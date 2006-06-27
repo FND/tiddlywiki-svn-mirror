6cfac409a2d00144eacd9625aeea71a7438233a3
UdoBorkowski <UdoBorkowski@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-06-27 19:02:15 +0000

    Created branch: TRY-ub-mini-#8
    git-svn-id: http://svn.tiddlywiki.org/Trunk@289 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/UdoBorkowski/branches/TRY-ub-mini-#8/core/js/Formatter.js
@@ -0,0 +1,584 @@
+// ---------------------------------------------------------------------------------
+// Standard formatters
+// ---------------------------------------------------------------------------------
+
+config.formatters = [
+{
+	name: "table",
+	match: "^\\|(?:[^\\n]*)\\|(?:[fhck]?)$",
+	lookahead: "^\\|([^\\n]*)\\|([fhck]?)$",
+	rowTerminator: "\\|(?:[fhck]?)$\\n?",
+	cellPattern: "(?:\\|([^\\n\\|]*)\\|)|(\\|[fhck]?$\\n?)",
+	cellTerminator: "(?:\\x20*)\\|",
+	rowTypes: {"c": "caption", "h": "thead", "": "tbody", "f": "tfoot"},
+	handler: function(w)
+	{
+		var table = createTiddlyElement(w.output,"table");
+		w.nextMatch = w.matchStart;
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		var currRowType = null, nextRowType;
+		var rowContainer, rowElement;
+		var prevColumns = [];
+		var rowCount = 0;
+		do {
+			lookaheadRegExp.lastIndex = w.nextMatch;
+			var lookaheadMatch = lookaheadRegExp.exec(w.source);
+			var matched = lookaheadMatch && lookaheadMatch.index == w.nextMatch;
+			if(matched)
+				{
+				nextRowType = lookaheadMatch[2];
+				if(nextRowType == "k")
+					{
+					table.className = lookaheadMatch[1];
+					w.nextMatch += lookaheadMatch[0].length+1;
+					continue;
+					}
+				if(nextRowType != currRowType)
+					rowContainer = createTiddlyElement(table,this.rowTypes[nextRowType]);
+				currRowType = nextRowType;
+				if(currRowType == "c")
+					{
+					if(rowCount == 0)
+						rowContainer.setAttribute("align","top");
+					else
+						rowContainer.setAttribute("align","bottom");
+					w.nextMatch = w.nextMatch + 1;
+					w.subWikify(rowContainer,this.rowTerminator);
+					table.insertBefore(rowContainer,table.firstChild);
+					}
+				else
+					{
+					var rowClass = (rowCount & 1) ? "oddRow" : "evenRow";
+					rowElement = createTiddlyElement(rowContainer,"tr",null,rowClass);
+					this.rowHandler(w,rowElement,prevColumns);
+					}
+				rowCount++;
+				}
+		} while(matched);
+	},
+	rowHandler: function(w,e,prevColumns)
+	{
+		var col = 0;
+		var currColCount = 1;
+		var matched;
+		var cellRegExp = new RegExp(this.cellPattern,"mg");
+		do {
+			cellRegExp.lastIndex = w.nextMatch;
+			var cellMatch = cellRegExp.exec(w.source);
+			matched = cellMatch && cellMatch.index == w.nextMatch;
+			if(matched)
+				{
+				if(cellMatch[1] == "~")
+					{
+					var last = prevColumns[col];
+					if(last)
+						{
+						last.rowCount++;
+						last.element.setAttribute("rowSpan",last.rowCount);
+						last.element.setAttribute("rowspan",last.rowCount);
+						last.element.valign = "center";
+						}
+					w.nextMatch = cellMatch.index + cellMatch[0].length-1;
+					}
+				else if(cellMatch[1] == ">")
+					{
+					currColCount++;
+					w.nextMatch = cellMatch.index + cellMatch[0].length-1;
+					}
+				else if(cellMatch[2])
+					{
+					w.nextMatch = cellMatch.index + cellMatch[0].length;;
+					break;
+					}
+				else
+					{
+					var spaceLeft = false, spaceRight = false;
+					w.nextMatch++;
+					var styles = config.formatterHelpers.inlineCssHelper(w);
+					while(w.source.substr(w.nextMatch,1) == " ")
+						{
+						spaceLeft = true;
+						w.nextMatch++;
+						}
+					var cell;
+					if(w.source.substr(w.nextMatch,1) == "!")
+						{
+						cell = createTiddlyElement(e,"th");
+						w.nextMatch++;
+						}
+					else
+						cell = createTiddlyElement(e,"td");
+					prevColumns[col] = {rowCount: 1, element: cell};
+					if(currColCount > 1)
+						{
+						cell.setAttribute("colSpan",currColCount);
+						cell.setAttribute("colspan",currColCount);
+						currColCount = 1;
+						}
+					config.formatterHelpers.applyCssHelper(cell,styles);
+					w.subWikify(cell,this.cellTerminator);
+					if(w.matchText.substr(w.matchText.length-2,1) == " ")
+						spaceRight = true;
+					if(spaceLeft && spaceRight)
+						cell.align = "center";
+					else if (spaceLeft)
+						cell.align = "right";
+					else if (spaceRight)
+						cell.align = "left";
+					w.nextMatch = w.nextMatch-1;
+					}
+				col++;
+				}
+		} while(matched);		
+	}
+},
+
+{
+	name: "rule",
+	match: "^----+$\\n?",
+	handler: function(w)
+	{
+		createTiddlyElement(w.output,"hr");
+	}
+},
+
+{
+	name: "heading",
+	match: "^!{1,5}",
+	terminator: "\\n",
+	handler: function(w)
+	{
+		var e = createTiddlyElement(w.output,"h" + w.matchLength);
+		w.subWikify(e,this.terminator);
+	}
+},
+
+{
+	name: "monospacedByLine",
+	match: "^\\{\\{\\{\\n",
+	lookahead: "^\\{\\{\\{\\n((?:^[^\\n]*\\n)+?)(^\\}\\}\\}$\\n?)",
+	handler: config.formatterHelpers.monospacedByLineHelper
+},
+
+{
+	name: "monospacedByLineForCSS",
+	match: "^/\\*[\\{]{3}\\*/\\n",
+	lookahead: "/\\*[\\{]{3}\\*/\\n*((?:^[^\\n]*\\n)+?)(\\n*^/\\*[\\}]{3}\\*/$\\n?)", 
+	handler: config.formatterHelpers.monospacedByLineHelper
+},
+
+{
+	name: "monospacedByLineForPlugin",
+	match: "^//\\{\\{\\{\\n",
+	lookahead: "^//\\{\\{\\{\\n\\n*((?:^[^\\n]*\\n)+?)(\\n*^//\\}\\}\\}$\\n?)",
+	handler: config.formatterHelpers.monospacedByLineHelper
+},
+
+{
+	name: "monospacedByLineForTemplate",
+	match: "^<!--[\\{]{3}-->\\n",
+	lookahead: "<!--[\\{]{3}-->\\n*((?:^[^\\n]*\\n)+?)(\\n*^<!--[\\}]{3}-->$\\n?)", 
+	handler: config.formatterHelpers.monospacedByLineHelper
+},
+
+{
+	name: "wikifyCommentForPlugin", 
+	match: "^/\\*\\*\\*\\n",
+	terminator: "^\\*\\*\\*/\\n",
+	handler: function(w)
+	{
+		w.subWikify(w.output,this.terminator);
+	}
+},
+
+{
+	name: "wikifyCommentForTemplate", 
+	match: "^<!---\\n",
+	terminator: "^--->\\n",
+	handler: function(w) 
+	{
+		w.subWikify(w.output,this.terminator);
+	}
+},
+
+{
+	name: "quoteByBlock",
+	match: "^<<<\\n",
+	terminator: "^<<<(\\n|$)",
+	handler: function(w)
+	{
+		var e = createTiddlyElement(w.output,"blockquote");
+		w.subWikify(e,this.terminator);
+	}
+},
+
+{
+	name: "quoteByLine",
+	match: "^>+",
+	terminator: "\\n",
+	element: "blockquote",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.match,"mg");
+		var placeStack = [w.output];
+		var currLevel = 0;
+		var newLevel = w.matchLength;
+		var t;
+		do {
+			if(newLevel > currLevel)
+				{
+				for(t=currLevel; t<newLevel; t++)
+					placeStack.push(createTiddlyElement(placeStack[placeStack.length-1],this.element));
+				}
+			else if(newLevel < currLevel)
+				{
+				for(t=currLevel; t>newLevel; t--)
+					placeStack.pop();
+				}
+			currLevel = newLevel;
+			w.subWikify(placeStack[placeStack.length-1],this.terminator);
+			createTiddlyElement(placeStack[placeStack.length-1],"br");
+			lookaheadRegExp.lastIndex = w.nextMatch;
+			var lookaheadMatch = lookaheadRegExp.exec(w.source);
+			var matched = lookaheadMatch && lookaheadMatch.index == w.nextMatch;
+			if(matched)
+				{
+				newLevel = lookaheadMatch[0].length;
+				w.nextMatch += lookaheadMatch[0].length;
+				}
+		} while(matched);
+	}
+},
+
+{
+	name: "list",
+	match: "^(?:(?:(?:\\*)|(?:#)|(?:;)|(?::))+)",
+	lookahead: "^(?:(?:(\\*)|(#)|(;)|(:))+)",
+	terminator: "\\n",
+	handler: function(w)
+	{
+		var placeStack = [w.output];
+		var currLevel = 0, currType = null;
+		var listLevel, listType, itemType;
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		w.nextMatch = w.matchStart;
+		lookaheadRegExp.lastIndex = w.nextMatch;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source);
+		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch)
+			{
+			if(lookaheadMatch[1])
+				{
+				listType = "ul";
+				itemType = "li";
+				}
+			else if(lookaheadMatch[2])
+				{
+				listType = "ol";
+				itemType = "li";
+				}
+			else if(lookaheadMatch[3])
+				{
+				listType = "dl";
+				itemType = "dt";
+				}
+			else if(lookaheadMatch[4])
+				{
+				listType = "dl";
+				itemType = "dd";
+				}
+			listLevel = lookaheadMatch[0].length;
+			w.nextMatch += lookaheadMatch[0].length;
+			if(listLevel > currLevel)
+				{
+				for(var t=currLevel; t<listLevel; t++)
+					placeStack.push(createTiddlyElement(placeStack[placeStack.length-1],listType));
+				}
+			else if(listLevel < currLevel)
+				{
+				for(var t=currLevel; t>listLevel; t--)
+					placeStack.pop();
+				}
+			else if(listLevel == currLevel && listType != currType)
+				{
+				placeStack.pop();
+				placeStack.push(createTiddlyElement(placeStack[placeStack.length-1],listType));
+				}
+			currLevel = listLevel;
+			currType = listType;
+			var e = createTiddlyElement(placeStack[placeStack.length-1],itemType);
+			w.subWikify(e,this.terminator);
+			lookaheadRegExp.lastIndex = w.nextMatch;
+			lookaheadMatch = lookaheadRegExp.exec(w.source);
+		}
+	}
+},
+
+{
+	name: "wikiLink",
+	match: config.textPrimitives.wikiLink,
+	badPrefix: config.textPrimitives.anyLetter,
+	handler: function(w)
+	{
+		var preRegExp = new RegExp(config.textPrimitives.anyLetter,"mg");
+		var preMatch = null;
+		if(w.matchStart > 0)
+			{
+			preRegExp.lastIndex = w.matchStart-1;
+			preMatch = preRegExp.exec(w.source);
+			}
+		if(preMatch && preMatch.index == w.matchStart-1)
+			w.outputText(w.output,w.matchStart,w.nextMatch);
+		else if(w.matchText.substr(0,1) == config.textPrimitives.unWikiLink)
+			w.outputText(w.output,w.matchStart + 1,w.nextMatch);
+		else
+			{
+			var link = createTiddlyLink(w.output,w.matchText,false);
+			w.outputText(link,w.matchStart,w.nextMatch);
+			}
+	}
+},
+
+{
+	name: "prettyLink",
+	match: "\\[\\[",
+	lookahead: "\\[\\[([^\\|\\]]*?)(?:(\\]\\])|(\\|(.*?)\\]\\]))",
+	terminator: "\\|",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source)
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
+			{
+			var e;
+			var text = lookaheadMatch[1];
+			if (lookaheadMatch[2]) // Simple bracketted link
+				{
+				e = createTiddlyLink(w.output,text,false);
+				}
+			else if(lookaheadMatch[3]) // Pretty bracketted link
+				{
+				var link = lookaheadMatch[4];
+				e = config.formatterHelpers.isExternalLink(link) ? createExternalLink(w.output,link) : createTiddlyLink(w.output,link,false);
+				}
+			createTiddlyText(e,text);
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			}
+	}
+},
+
+{
+	name: "urlLink",
+	match: config.textPrimitives.urlPattern,
+	handler: function(w)
+	{
+		var e = createExternalLink(w.output,w.matchText);
+		w.outputText(e,w.matchStart,w.nextMatch);
+	}
+},
+
+{
+	name: "image",
+	match: "\\[(?:[<]{0,1})(?:[>]{0,1})[Ii][Mm][Gg]\\[",
+	lookahead: "\\[([<]{0,1})([>]{0,1})[Ii][Mm][Gg]\\[(?:([^\\|\\]]+)\\|)?([^\\[\\]\\|]+)\\](?:\\[([^\\]]*)\\]?)?(\\])",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source);
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) // Simple bracketted link
+			{
+			var e = w.output;
+			if(lookaheadMatch[5])
+				{
+				if(store.tiddlerExists(lookaheadMatch[5]) || store.isShadowTiddler(lookaheadMatch[5]))
+					e = createTiddlyLink(w.output,lookaheadMatch[5],false);
+				else
+					e = createExternalLink(w.output,lookaheadMatch[5]);
+				addClass(e,"imageLink");
+				}
+			var img = createTiddlyElement(e,"img");
+			if(lookaheadMatch[1])
+				img.align = "left";
+			else if(lookaheadMatch[2])
+				img.align = "right";
+			if(lookaheadMatch[3])
+				img.title = lookaheadMatch[3];
+			img.src = lookaheadMatch[4];
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			}
+	}
+},
+
+{
+	name: "macro",
+	match: "<<",
+	lookahead: "<<([^>\\s]+)(?:\\s*)((?:[^>]|(?:>(?!>)))*)>>",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source)
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart && lookaheadMatch[1])
+			{		
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			invokeMacro(w.output,lookaheadMatch[1],lookaheadMatch[2],w,w.tiddler);
+			}
+	}
+},
+
+{
+	name: "html",
+	match: "<[Hh][Tt][Mm][Ll]>",
+	lookahead: "<[Hh][Tt][Mm][Ll]>((?:.|\\n)*?)</[Hh][Tt][Mm][Ll]>",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source)
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
+			{
+			var e = createTiddlyElement(w.output,"span");
+			e.innerHTML = lookaheadMatch[1];
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			}
+	}
+},
+
+{
+	name: "commentByBlock",
+	match: "/%",
+	lookahead: "/%((?:.|\\n)*?)%/",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source)
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+	}
+},
+
+{
+	name: "boldByChar",
+	match: "''",
+	terminator: "''",
+	element: "strong",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "strikeByChar",
+	match: "==",
+	terminator: "==",
+	element: "strike",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "underlineByChar",
+	match: "__",
+	terminator: "__",
+	element: "u",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "italicByChar",
+	match: "//",
+	terminator: "//",
+	element: "em",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "subscriptByChar",
+	match: "~~",
+	terminator: "~~",
+	element: "sub",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "superscriptByChar",
+	match: "\\^\\^",
+	terminator: "\\^\\^",
+	element: "sup",
+	handler: config.formatterHelpers.charFormatHelper
+},
+
+{
+	name: "monospacedByChar",
+	match: "\\{\\{\\{",
+	lookahead: "\\{\\{\\{((?:.|\\n)*?)\\}\\}\\}",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"mg");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source)
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
+			{
+			var e = createTiddlyElement(w.output,"code",null,null,lookaheadMatch[1]);
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			}
+	}
+},
+
+{
+	name: "styleByChar",
+	match: "@@",
+	terminator: "@@",
+	lookahead: "(?:([^\\(@]+)\\(([^\\)]+)(?:\\):))|(?:([^:@]+):([^;]+);)",
+	handler:  function(w)
+	{
+		var e = createTiddlyElement(w.output,"span",null,null,null);
+		var styles = config.formatterHelpers.inlineCssHelper(w);
+		if(styles.length == 0)
+			e.className = "marked";
+		else
+			config.formatterHelpers.applyCssHelper(e,styles);
+		w.subWikify(e,this.terminator);
+	}
+},
+
+{
+	name: "lineBreak",
+	match: "\\n",
+	handler: function(w)
+	{
+		createTiddlyElement(w.output,"br");
+	}
+},
+
+{
+	name: "htmlEntitiesEncoding",
+	match: "&#?[a-zA-Z0-9]{2,8};",
+	handler: function(w)
+		{
+		var e = createTiddlyElement(w.output,"span");
+		e.innerHTML = w.matchText ;
+		}
+},
+
+{
+	name: "customClasses",
+	match: "\\{\\{",
+	terminator: "\\}\\}\\}",
+	lookahead: "\\{\\{[\\s]*([\\w]+[\\s\\w]*)[\\s]*\\{(\\n{0,1})",
+	handler: function(w)
+	{
+		var lookaheadRegExp = new RegExp(this.lookahead,"g");
+		lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = lookaheadRegExp.exec(w.source);
+		if(lookaheadMatch)
+			{
+			var isByLine = lookaheadMatch[2] == "\n";
+			var p = createTiddlyElement(w.output,isByLine ? "div" : "span",null,lookaheadMatch[1]);
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+			w.subWikify(p,this.terminator);
+			}
+	}
+}
+
+];
+
---
+++ contributors/UdoBorkowski/branches/TRY-ub-mini-#8/core/js/LoaderSaver.js
@@ -0,0 +1,109 @@
+
+// The format a TiddlyWiki's storeArea is saved can be given through the 
+// 'saveFormat' startup parameter, passing in the format id (e.g. 'tw21'). E.g.: 
+//
+//       http://www.tiddlywiki.com/index.html#saveFormat:tw21
+//
+// If no save format is explicitly specified the file is saved in its original format it was
+// loaded from, if that format is supported and not the config.store.LEGACY_FORMAT. 
+// Otherwise the config.store.defaultFormat is used.
+
+config.store =  {
+	LEGACY_FORMAT: 'tw20', // The format identifying the pre 2.1 file format
+	
+	//TODO: make this 'tw21' as soon ginsu/cook can understand that format
+	defaultFormat: 'tw20',  
+	
+	// Map a storage format name to a loader/saver.
+	loader: {},
+	saver: {}
+};
+
+function getLoaderForFormat(format) 
+{
+	var loader = config.store.loader[format];
+	if (!loader)
+		throw config.messages.unsupportedTWFormat.format([format]);
+	return new loader();
+}
+
+function getSaverForFormat(format) 
+{
+	var saver = config.store.saver[format];
+	if (!saver)
+		throw config.messages.unsupportedTWFormat.format([format]);
+	return new saver();
+}
+
+function getFormatOfStore(storeElem) 
+{
+	var format = storeElem.className;
+	return format ? format : config.store.LEGACY_FORMAT;
+}
+
+// Returns a saver for the preferredFormat or the default saver if the format is not supported.
+// When noAlert is false a dialog is displayed when the preferredFormat is not supported.
+function getSaver(preferredFormat, noAlert) {
+	try {
+		return getSaverForFormat(preferredFormat);
+	} catch(e) {
+		if (!noAlert)
+			alert(config.messages.wrongSaveFormat.format([preferredFormat]));
+		return getSaverForFormat(config.store.defaultFormat);
+	}
+}
+
+config.paramifiers.saveFormat = {
+	oninit: function(v) {
+		TiddlyWiki.prototype.requestedSaver = getSaver(v);
+	}
+};
+
+
+
+//-------------------------
+// LoaderByElems: A (abstract) storage loader that loads the tiddlers from a list of HTML elements.
+//# The format of the elements is defined by subclasses of this loader through the initTiddler implementation.
+//# Subclasses must implement:
+//# 			function getTitle(store, e)
+//#
+//# store must implement:
+//# 			function createTiddler(title).
+//#
+
+function LoaderByElems() {};
+
+LoaderByElems.prototype.loadTiddler = function(store, e, tiddlers) {
+	var title = this.getTitle(store, e);
+	if (title) {
+		var tiddler = store.createTiddler(title);
+		this.initTiddler(store, tiddler, title, e);
+		tiddlers.push(tiddler);
+	}
+}
+
+LoaderByElems.prototype.loadTiddlers = function(store, nodes) {
+	var tiddlers = [];
+	for (var t = 0; t < nodes.length; t++) {
+		try {
+			this.loadTiddler(store, nodes[t], tiddlers);
+		} catch (e) {
+			showException(e, config.messages.tiddlerLoadError.format([this.getTitle(store, e)]));
+		}
+	}
+	return tiddlers;
+};
+	
+//-------------------------
+// AbstractSaver: a storage saver that serializes all tiddlers into a string, 
+// with every tiddler individually serialized and joined with newlines 
+
+function AbstractSaver() {};
+
+AbstractSaver.prototype.serialize = function(store) {
+	var results = [];
+	var tiddlers = store.getTiddlers("title");
+	for (var t = 0; t < tiddlers.length; t++)
+		results.push(this.serializeTiddler(store, tiddlers[t]));
+	return results.join("\n");
+};
