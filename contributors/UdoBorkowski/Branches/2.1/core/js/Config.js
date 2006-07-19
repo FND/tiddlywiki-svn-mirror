857f7dc0bbeb18d843801ad8308422412f9fce08
UdoBorkowski <UdoBorkowski@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-07-19 06:32:05 +0000

    Performance Tuning: removed named notifier "ColorPalette".
    git-svn-id: http://svn.tiddlywiki.org/Trunk@386 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/UdoBorkowski/Branches/2.1/core/js/Config.js
+++ contributors/UdoBorkowski/Branches/2.1/core/js/Config.js
@@ -41,7 +41,6 @@ config.options = {
 	
 // List of notification functions to be called when certain tiddlers are changed or deleted
 config.notifyTiddlers = [
-	{name: "ColorPalette", notify: refreshEverything},
 	{name: "StyleSheetLayout", notify: refreshStyles},
 	{name: "StyleSheetColors", notify: refreshStyles},
 	{name: "StyleSheet", notify: refreshStyles},
