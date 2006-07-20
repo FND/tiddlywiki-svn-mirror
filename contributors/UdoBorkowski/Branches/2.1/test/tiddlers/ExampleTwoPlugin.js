36f1e416ab7c19085a16ce2600ace8b54c828eb0
UdoBorkowski <UdoBorkowski@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-07-20 06:40:40 +0000

    [Merge with Main branch]
    git-svn-id: http://svn.tiddlywiki.org/Trunk@410 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/UdoBorkowski/Branches/2.1/test/tiddlers/ExampleTwoPlugin.js
+++ contributors/UdoBorkowski/Branches/2.1/test/tiddlers/ExampleTwoPlugin.js
@@ -1,12 +1,12 @@
 /***
-|''Name:''|ExamplePlugin|
+|''Name:''|ExampleTwoPlugin|
 |''Description:''|To demonstrate how to write TiddlyWiki plugins|
-|''Version:''|2.0.2|
-|''Date:''|Jul 12, 2006|
+|''Version:''|2.0.3|
+|''Date:''|Jul 17, 2006|
 |''Source:''|http://www.tiddlywiki.com/#ExamplePlugin|
 |''Author:''|JeremyRuston (jeremy (at) osmosoft (dot) com)|
 |''License:''|[[BSD open source license]]|
-|''CoreVersion:''|2.1.0|
+|''CoreVersion:''|2.2.0|
 |''Browser:''|Firefox 1.0.4+; Firefox 1.5; InternetExplorer 6.0|
 ***/
 
@@ -14,4 +14,3 @@
 // Don't do anything, this is just an example
 //}}}
 
-deliberateError();
