9154fe95ab48c4638bf3289856a28151a890c9b1
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-08-14 19:10:50 +0000

    Renamed getFingerprint to calculateFingerprint to reflect the fact that it is not a simple get operation (and can take an extended length of time to calculate).
    git-svn-id: http://svn.tiddlywiki.org/Trunk@538 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/Branches/2.1/core/js/Tiddler.js
+++ contributors/MartinBudden/Branches/2.1/core/js/Tiddler.js
@@ -139,7 +139,7 @@ Tiddler.prototype.autoLinkWikiWords = function()
 	return !this.isTagged("systemConfig") && !this.isTagged("excludeMissing");
 }
 
-Tiddler.prototype.getFingerprint = function()
+Tiddler.prototype.calculateFingerprint = function()
 {
 	return "0x" + Crypto.hexSha1Str(this.text);
 }
