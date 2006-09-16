817ecdb42bec844621e74d090edb599899e83422
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-09-16 05:45:52 +0000

    Improved JavaScript compliance.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@729 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/martinsplugins/DisableWikiLinksPlugin.js
+++ verticals/martinsplugins/DisableWikiLinksPlugin.js
@@ -3,7 +3,7 @@
 |''Description:''|Pre-release allows you to disable TiddlyWiki's automatic linking of WikiWords|
 |''Source:''|http://martinswiki.com/martinsprereleases.html#DisableWikiLinksPlugin - for pre-release|
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
-|''Version:''|0.1.1|
+|''Version:''|0.1.2|
 |''Status:''|beta pre-release|
 |''Date:''|Aug 5, 2006|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
@@ -18,17 +18,17 @@ if(!version.extensions.DisableWikiLinksPlugin) {
 version.extensions.DisableWikiLinksPlugin = {installed:true};
 
 if(version.major < 2 || (version.major == 2 && version.minor < 1))
-	alertAndThrow("DisableWikiLinksPlugin requires TiddlyWiki 2.1 or newer.");
+	{alertAndThrow("DisableWikiLinksPlugin requires TiddlyWiki 2.1 or newer.");}
 
 if (config.options.chkDisableWikiLinks==undefined)
-	config.options.chkDisableWikiLinks = false;
+	{config.options.chkDisableWikiLinks = false;}
 
 Tiddler.prototype.autoLinkWikiWords = function()
 {
 	if(config.options.chkDisableWikiLinks==true)
-		return false;
+		{return false;}
 	return !this.isTagged("systemConfig") && !this.isTagged("excludeMissing");
-}
+};
 
 } // end of "install only once"
 //}}}
