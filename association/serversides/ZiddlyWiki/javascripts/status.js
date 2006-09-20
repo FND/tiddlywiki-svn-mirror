29402b1590ada96110d05d5e79b526a579789607
BobMcElrath <BobMcElrath@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-09-20 10:31:19 +0000

    Fix history: 1) Search through history for latest edit (not a lock) 2) Search back 200 items in the history (previously 10 empty edits will make the history be blank) 3) Properly highlight the current revision 4) Update get* scripts to work properly with revisions
    git-svn-id: http://svn.tiddlywiki.org/Trunk@748 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/ZiddlyWiki/javascripts/status.js
+++ association/serversides/ZiddlyWiki/javascripts/status.js
@@ -9,14 +9,18 @@ config.options.txtUserName = '<dtml-var "REQUEST.AUTHENTICATED_USER.getUserName(
 version.extensions.ZiddlyWiki = '<dtml-var "REQUEST.PARENTS[0].ZiddlyWiki.version">';
 zw.tiddlerList = {
 <dtml-try>
-<dtml-in expr="REQUEST.PARENTS[0].tiddlers.objectValues(['DTML Document', 'File'])" skip_unauthorized>
+<dtml-in expr="REQUEST.PARENTS[0].tiddlers.objectValues(['DTML Document', 'File'])" skip_unauthorized prefix="tiddler">
 <dtml-try>
-    <dtml-if "_.string.split(tags).index('deleted')"></dtml-if>
+    <dtml-if "_.string.split(tiddler_tags).index('deleted')"></dtml-if>
 <dtml-except>
 <dtml-try>
-    "<dtml-var "title">": "<dtml-var "manage_change_history()[0]['key']">",
+<dtml-call "REQUEST.set('break',0)"><dtml-in "tiddler_item.manage_change_history()" prefix="hist">
+<dtml-unless "REQUEST['break']"><dtml-unless "hasattr(tiddler_item.HistoricalRevisions[hist_item['key']],'ignore_revision')">
+"<dtml-var "tiddler_item.title">": "<dtml-var "hist_item['key']">",
+<dtml-call "REQUEST.set('break',1)"></dtml-unless></dtml-unless>
+</dtml-in>
 <dtml-except>
-    "Unauthorized to list tiddler": 1,
+"Unauthorized to list tiddler": "<dtml-var "tiddler_item.title">",
 </dtml-try>
 </dtml-try>
 </dtml-in>
