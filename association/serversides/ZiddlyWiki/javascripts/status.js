a7a0e04e6ff825bf18c68db2f8a6b249206be047
BobMcElrath <BobMcElrath@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-09-20 07:18:23 +0000

    Fix error where editing a new tiddler would not have a 'modifier', add text/javascript header to this file.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@743 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/ZiddlyWiki/javascripts/status.js
+++ association/serversides/ZiddlyWiki/javascripts/status.js
@@ -1,3 +1,4 @@
+<dtml-call "REQUEST.RESPONSE.setHeader('Content-Type', 'text/javascript')">
 if(typeof zw == "undefined") var zw = {};
 zw.loggedIn = <dtml-var "('Authenticated' in REQUEST.AUTHENTICATED_USER.getRoles()) and 'true' or 'false'">;
 zw.anonEdit = <dtml-var "getattr(_, 'zw_anon_edit', None) and 'true' or 'false'">;
@@ -5,6 +6,7 @@ zw.ziddlyPath = '<dtml-var "REQUEST.URL">';
 zw.isAdmin = <dtml-var "('Manager' in REQUEST.AUTHENTICATED_USER.getRoles()) and 'true' or 'false'">;
 zw.latestTiddler = <dtml-var "REQUEST.PARENTS[0].ZiddlyWiki.actions.get_timestamp(root=REQUEST.PARENTS[0])">;
 zw.username = '<dtml-var "REQUEST.AUTHENTICATED_USER.getUserName()">';
+config.options.txtUserName = '<dtml-var "REQUEST.AUTHENTICATED_USER.getUserName()">';
 version.extensions.ZiddlyWiki = '<dtml-var "REQUEST.PARENTS[0].ZiddlyWiki.version">';
 zw.tiddlerList = {
 <dtml-try>
