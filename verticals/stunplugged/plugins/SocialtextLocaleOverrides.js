fcbb35916816bf701999b9cab23670f76b7c2477
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-11-07 19:49:07 +0000

    Text overrides to better conform with Socialtext style.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@979 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/stunplugged/plugins/SocialtextLocaleOverrides.js
@@ -0,0 +1,10 @@
+/***
+|''Name:''|SocialtextLocaleOverrides|
+|''Description:''|Text changes for Socialtext|
+***/
+
+//{{{
+merge(config.views.wikified.tag,{
+	labelNoTags: "No Tags",
+	labelTags: "Tags"});
+//}}}
