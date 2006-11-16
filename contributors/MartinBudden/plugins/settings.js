df81280852bd91486dc413dce992e6b80883b7d0
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-11-16 07:15:32 +0000

    Initial submission.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1008 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/plugins/settings.js
@@ -0,0 +1,11 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = "";
+config.options.chkAnimate = false;
+config.options.chkDisableWikiLinks = true;
+/*}}}*/
