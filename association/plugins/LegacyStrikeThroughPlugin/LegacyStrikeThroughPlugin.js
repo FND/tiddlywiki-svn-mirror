6b1847213bdf03bf9220391587a757e672f69f3f
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-11-29 21:56:06 +0000

    Minor tidy up and moved to standard form of ensuring only installed once.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1071 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/LegacyStrikeThroughPlugin/LegacyStrikeThroughPlugin.js
+++ association/plugins/LegacyStrikeThroughPlugin/LegacyStrikeThroughPlugin.js
@@ -1,22 +1,18 @@
 /***
 |''Name:''|LegacyStrikeThroughPlugin|
-|''Description:''|Support for  legacy (pre 2.1) strike through formatting|
-|''Version:''|1.0.1|
+|''Description:''|Support for legacy (pre 2.1) strike through formatting|
+|''Version:''|1.0.2|
 |''Date:''|Jul 21, 2006|
 |''Source:''|http://www.tiddlywiki.com/#LegacyStrikeThroughPlugin|
 |''Author:''|MartinBudden (mjbudden (at) gmail (dot) com)|
 |''License:''|[[BSD open source license]]|
 |''CoreVersion:''|2.1.0|
-|''Browser:''|Firefox 1.0.4+; Firefox 1.5; InternetExplorer 6.0|
-
 ***/
 
 //{{{
-
 // Ensure that the LegacyStrikeThrough Plugin is only installed once.
-if(!version.extensions.LegacyStrikeThroughPlugin)
-	{
-	version.extensions.LegacyStrikeThroughPlugin = true;
+if(!version.extensions.LegacyStrikeThroughPlugin) {
+version.extensions.LegacyStrikeThroughPlugin = {installed:true};
 
 config.formatters.push(
 {
@@ -27,5 +23,5 @@ config.formatters.push(
 	handler: config.formatterHelpers.createElementAndWikify
 });
 
-} // end of "install only once"
+} //# end of "install only once"
 //}}}
