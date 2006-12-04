32ef8774cf2cb2baac4037d7137f0831e1d4e6cd
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-12-04 16:17:05 +0000

    Stunplugged updates
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1094 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/stunplugged/plugins/SocialtextTweaksPlugin.js
@@ -0,0 +1,29 @@
+/***
+|''Name:''|SocialtextTweaksPlugin|
+|''Description:''|Allows changes to be synchronised with a Socialtext server|
+|''Source:''|http://stunplugged.tiddlywiki.com/#SocialtextTweaksPlugin|
+|''Author:''|JeremyRuston (jeremy (at) osmosoft (dot) com)|
+|''Version:''|1.0.0|
+|''Date:''|Dec 4, 2006|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
+|''License:''|[[BSD open source license]]|
+|''~CoreVersion:''|2.1.3|
+
+Make minor configuration tweaks specific to Socialtext Unplugged
+
+***/
+
+//{{{
+
+// Ensure that the SocialtextTweaksPlugin is only installed once.
+if(!version.extensions.SocialtextTweaksPlugin) {
+version.extensions.SocialtextTweaksPlugin = {installed:true};
+// Check version number of core code
+if(version.major < 2 || (version.major == 2 && version.minor < 1))
+	{alertAndThrow("SocialtextTweaksPlugin requires TiddlyWiki 2.1 or later.");}
+
+config.options.chkSinglePageMode = true;
+config.options.chkEnableAnimations = true;
+
+} // end of "install only once"
+//}}}
