3d5b6eacd6f51f44c5b503fbc2ad9dc1861dad65
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-12-10 09:22:22 +0000

    Changed to K&R layout. Added wikiformat string.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1131 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/PmWikiFormatterPlugin.js
+++ contributors/MartinBudden/plugins/PmWikiFormatterPlugin.js
@@ -52,28 +52,42 @@ pmDebug = function(out,str)
 	createTiddlyElement(out,"br");
 };
 
+PmWikiFormatter.directives = function(w)
+{
+	this.lookaheadRegExp.lastIndex = w.matchStart;
+	var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
+	if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
+		var lm1 = lookaheadMatch[1];
+		var lm2 = lookaheadMatch[2];
+		switch(lm1) {
+		case "directive":
+			break;
+		default:
+			w.outputText(w.output,w.matchStart,w.nextMatch);
+			return;
+		}
+		w.nextMatch = this.lookaheadRegExp.lastIndex;
+	} else {
+		w.outputText(w.output,w.matchStart,w.nextMatch);
+	}
+};
+
 PmWikiFormatter.setFromParams = function(w,p)
 {
 	var r = {};
 	var re = /\s*(.*?)=(?:(?:"(.*?)")|(?:'(.*?)')|((?:\w|%|#)*))/mg;
 	var match = re.exec(p);
-	while(match)
-		{
+	while(match) {
 		var s = match[1].unDash();
-		if(match[2])
-			{
+		if(match[2]) {
 			r[s] = match[2];
-			}
-		else if(match[3])
-			{
+		} else if(match[3]) {
 			r[s] = match[3];
-			}
-		else
-			{
+		} else {
 			r[s] = match[4];
-			}
-		match = re.exec(p);
 		}
+		match = re.exec(p);
+	}
 	return r;
 };
 
@@ -84,27 +98,21 @@ config.formatterHelpers.setAttributesFromParams = function(e,p)
 	while(match)
 		{
 		var s = match[1].unDash();
-		if(s=="bgcolor")
-			{
+		if(s=="bgcolor") {
 			s = "backgroundColor";
-			}
+		}
 		try {
-		if(match[2])
-			{
-			e.setAttribute(s,match[2]);
-			}
-		else if(match[3])
-			{
-			e.setAttribute(s,match[3]);
-			}
-		else
-			{
-			e.setAttribute(s,match[4]);
+			if(match[2]) {
+				e.setAttribute(s,match[2]);
+			} else if(match[3]) {
+				e.setAttribute(s,match[3]);
+			} else {
+				e.setAttribute(s,match[4]);
 			}
 		}
 		catch(ex) {}
 		match = re.exec(p);
-		}
+	}
 };
 
 config.pmWikiFormatters = [
@@ -129,26 +137,24 @@ all men are created equal.
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
-			{
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			var text = lookaheadMatch[1];
-			if(config.browser.isIE)
-				{
+			if(config.browser.isIE) {
 				text = text.replace(/\n/g,"\r");
-				}
+			}
 			var t = createTiddlyElement(w.output,"table",null,"markup vert");
 			var tr1 = createTiddlyElement(t,"tr");
 			var td1 = createTiddlyElement(tr1,"td",null,"markup1");
 			var tr2 = createTiddlyElement(t,"tr");
 			var td2 = createTiddlyElement(tr2,"td",null,"markup2");
-				
+
 			createTiddlyElement(td1,"pre",null,null,text);
 			var oldSource = w.source;
 			w.source = text; w.nextMatch = 0;
 			w.subWikifyUnterm(td2);
 			w.source = oldSource;
 			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
-			}
+		}
 	}
 },
 
@@ -171,13 +177,12 @@ all men are created equal.
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
-			{
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			/*if(!w.tableParams)
 				w.tableParams = {};
 			w.tableParams = PmWikiFormatter.setFromParams(w,lookaheadMatch[1]);*/
 			w.nextMatch = this.lookaheadRegExp.lastIndex;
-			}
+		}
 	}
 },
 
@@ -192,22 +197,20 @@ all men are created equal.
 	{
 //this.debug = createTiddlyElement(w.output,"p");
 		var table = createTiddlyElement(w.output,"table");
-		/*for(var i in w.tableParams)
-			{
+		/*for(var i in w.tableParams) {
 			table.setAttribut(i,w.tableParams[i])
-			}*/
+		}*/
 		var rowContainer = createTiddlyElement(table,"tbody");
 		var rowCount = 0;
 		w.nextMatch = w.matchStart;
 		this.lookaheadRegExp.lastIndex = w.nextMatch;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch)
-			{
+		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch) {
 			this.rowHandler(w,createTiddlyElement(rowContainer,"tr",null,(rowCount&1)?"oddRow":"evenRow"));
 			rowCount++;
 			this.lookaheadRegExp.lastIndex = w.nextMatch;
 			lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-			}
+		}
 	},//# end handler
 	rowHandler: function(w,e)
 	{
@@ -216,69 +219,57 @@ all men are created equal.
 		var prevCell = null;
 		this.cellRegExp.lastIndex = w.nextMatch;
 		var cellMatch = this.cellRegExp.exec(w.source);
-		while(cellMatch && cellMatch.index == w.nextMatch)
-			{
-			if(w.source.substr(w.nextMatch,4) == "||||")
-				{// Colspan
+		while(cellMatch && cellMatch.index == w.nextMatch) {
+			if(w.source.substr(w.nextMatch,4) == "||||") {
+				// Colspan
 //pmDebug(this.debug,w.source.substr(w.nextMatch,10));
 //pmDebug(this.debug,"nm:"+w.nextMatch+" li:"+this.cellRegExp.lastIndex);
 				colSpanCount++;
 				w.nextMatch += 2;
-				}
-			else if(cellMatch[2])
-				{// End of row
-				if(colSpanCount > 1)
-					{
+			} else if(cellMatch[2]) {// End of row
+				if(colSpanCount > 1) {
 					prevCell.setAttribute("colspan",colSpanCount);
 					prevCell.setAttribute("colSpan",colSpanCount); // Needed for IE
-					}
+				}
 				w.nextMatch = this.cellRegExp.lastIndex;
 				break;
-				}
-			else
-				{// Cell
+			} else {
+				// Cell
 				w.nextMatch += 2; //skip over ||
 				var chr = w.source.substr(w.nextMatch,1);
 				var cell;
-				if(chr == "!")
-					{
+				if(chr == "!") {
 					cell = createTiddlyElement(e,"th");
 					w.nextMatch++;
 					chr = w.source.substr(w.nextMatch,1);
-					}
-				else
-					{
+				} else {
 					cell = createTiddlyElement(e,"td");
-					}
+				}
 				var spaceLeft = false;
-				while(chr == " ")
-					{
+				while(chr == " ") {
 					spaceLeft = true;
 					w.nextMatch++;
 					chr = w.source.substr(w.nextMatch,1);
-					}
-				if(colSpanCount > 1)
-					{
+				}
+				if(colSpanCount > 1) {
 					cell.setAttribute("colspan",colSpanCount);
 					cell.setAttribute("colSpan",colSpanCount); // Needed for IE
 					colSpanCount = 1;
-					}
+				}
 				w.subWikifyTerm(cell,this.cellTermRegExp);
-				if(w.matchText.substr(w.matchText.length-3,1) == " ") // spaceRight
-					{
+				if(w.matchText.substr(w.matchText.length-3,1) == " ") {
+					// spaceRight
 					cell.align = spaceLeft ? "center" : "left";
-					}
-				else if(spaceLeft)
-					{
+				} else if(spaceLeft) {
 					cell.align = "right";
-					}
+				}
 				prevCell = cell;
 				w.nextMatch -= 2;
-				}
+			}
 			col++;
 			this.cellRegExp.lastIndex = w.nextMatch;
 			cellMatch = this.cellRegExp.exec(w.source);
-			}
+		}
 	}//# end rowHandler
 },
 
@@ -295,42 +286,33 @@ all men are created equal.
 		w.nextMatch = w.matchStart;
 		this.lookaheadRegExp.lastIndex = w.nextMatch;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch)
-			{
-			if(lookaheadMatch[1])
-				{
+		while(lookaheadMatch && lookaheadMatch.index == w.nextMatch) {
+			if(lookaheadMatch[1]) {
 				listType = "ul";
 				itemType = "li";
-				}
-			else if(lookaheadMatch[2])
-				{
+			} else if(lookaheadMatch[2]) {
 				listType = "ol";
 				itemType = "li";
-				}
+			}
 			listLevel = lookaheadMatch[0].length;
 			w.nextMatch += lookaheadMatch[0].length;
-			if(listLevel > currLevel)
-				{
+			if(listLevel > currLevel) {
 				for(var i=currLevel; i<listLevel; i++)
 					{placeStack.push(createTiddlyElement(placeStack[placeStack.length-1],listType));}
-				}
-			else if(listLevel < currLevel)
-				{
+			} else if(listLevel < currLevel) {
 				for(i=currLevel; i>listLevel; i--)
 					{placeStack.pop();}
-				}
-			else if(listLevel == currLevel && listType != currType)
-				{
+			} else if(listLevel == currLevel && listType != currType) {
 				placeStack.pop();
 				placeStack.push(createTiddlyElement(placeStack[placeStack.length-1],listType));
-				}
+			}
 			currLevel = listLevel;
 			currType = listType;
 			var e = createTiddlyElement(placeStack[placeStack.length-1],itemType);
 			w.subWikifyTerm(e,this.termRegExp);
 			this.lookaheadRegExp.lastIndex = w.nextMatch;
 			lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-			}
+		}
 	}
 },
 
@@ -374,21 +356,17 @@ all men are created equal.
 	handler: function(w)
 	{
 		var e = createTiddlyElement(w.output,"pre");
-		while(true)
-			{
+		while(true) {
 			w.subWikifyTerm(e,this.termRegExp);
 			createTiddlyElement(e,"br");
 			this.lookaheadRegExp.lastIndex = w.nextMatch;
 			var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-			if(lookaheadMatch && lookaheadMatch.index == w.nextMatch)
-				{
+			if(lookaheadMatch && lookaheadMatch.index == w.nextMatch) {
 				w.nextMatch += lookaheadMatch[0].length;
-				}
-			else
-				{
+			} else {
 				break;
-				}
 			}
+		}
 	}
 },
 
@@ -400,22 +378,20 @@ all men are created equal.
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
-			{
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			var link = lookaheadMatch[1];
-			if(lookaheadMatch[2])
-				{// Titled link
+			if(lookaheadMatch[2]) {
+				//# Titled link
 				var e = config.formatterHelpers.isExternalLink(link) ? createExternalLink(w.output,link) : createTiddlyLink(w.output,link,false,null,w.isStatic);
 				var text = lookaheadMatch[2];
-				}
-			else
-				{// Simple bracketted link
+			} else {
+				//# Simple bracketted link
 				e = createTiddlyLink(w.output,link,false,null,w.isStatic);
 				text = link;
-				}
+			}
 			createTiddlyText(e,text);
 			w.nextMatch = this.lookaheadRegExp.lastIndex;
-			}
+		}
 	}
 },
 
@@ -433,22 +409,19 @@ all men are created equal.
 	match: config.textPrimitives.wikiLink,
 	handler: function(w)
 	{
-		if(w.matchStart > 0)
-			{
+		if(w.matchStart > 0) {
 			var preRegExp = new RegExp(config.textPrimitives.anyLetter,"mg");
 			preRegExp.lastIndex = w.matchStart-1;
 			preMatch = preRegExp.exec(w.source);
-			if(preMatch.index == w.matchStart-1)
-				{
+			if(preMatch.index == w.matchStart-1) {
 				w.outputText(w.output,w.matchStart,w.nextMatch);
 				return;
-				}
 			}
+		}
 		var output = w.output;
-		if(w.autoLinkWikiWords == true || store.isShadowTiddler(w.matchText))
-			{
+		if(w.autoLinkWikiWords == true || store.isShadowTiddler(w.matchText)) {
 			output = createTiddlyLink(w.output,w.matchText,false,null,w.isStatic);
-			}
+		}
 		w.outputText(output,w.matchStart,w.nextMatch);
 	}
 },
@@ -567,10 +540,9 @@ all men are created equal.
 	handler: function(w)
 	{
 		createTiddlyElement(w.output,"br");
-		if(w.matchLength==4)
-			{
+		if(w.matchLength==4) {
 			createTiddlyElement(w.output,"br");
-			}
+		}
 	}
 },
 
@@ -609,11 +581,17 @@ all men are created equal.
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		if(lookaheadMatch && lookaheadMatch.index == w.matchStart)
-			{
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			w.nextMatch = this.lookaheadRegExp.lastIndex;
-			}
+		}
 	}
+},
+
+{
+	name: "pmWikiDirectives",
+	match: "\\(:(?:[a-z]{2,16}):\\)",
+	lookaheadRegExp: /\(:(?:[a-z]{2,16}):\)/mg,
+	handler: PmWikiFormatter.directives
 },
 
 {
@@ -628,6 +606,7 @@ all men are created equal.
 ];
 
 config.parsers.pmWikiFormatter = new Formatter(config.pmWikiFormatters);
+config.parsers.pmWikiFormatter.format = "PmWiki";
 config.parsers.pmWikiFormatter.formatTag = "PmWikiFormat";
 } // end of "install only once"
 //}}}
