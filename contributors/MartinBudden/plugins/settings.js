3d0221a78cf193c44bc80fe98d7846ebec379273
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2006-12-21 13:18:51 +0000

    Set MaxEditRows option to 20.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1187 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/settings.js
+++ contributors/MartinBudden/plugins/settings.js
@@ -8,5 +8,6 @@
 config.views.editor.defaultText = '';
 config.options.chkAnimate = false;
 config.options.chkDisableWikiLinks = true;
+config.options.txtMaxEditRows = 20;
 config.usePreForStorage = true;
 /*}}}*/
