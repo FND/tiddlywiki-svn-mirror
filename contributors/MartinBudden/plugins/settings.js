2fa6abeb5a122264a301d8b522171e591940982a
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-01-11 20:57:52 +0000

    Minor updates.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1282 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/settings.js
+++ contributors/MartinBudden/plugins/settings.js
@@ -1,13 +1,18 @@
 /***
 |''Name:''|settings|
-|''Description:''|set preferences|
+|''Description:''|Set preferences|
 |''~CoreVersion:''|2.1.0|
 ***/
 
 /*{{{*/
+config.usePreForStorage = true;
+//config.displayStartupTime = true;
+
 config.views.editor.defaultText = '';
 config.options.chkAnimate = false;
 config.options.chkDisableWikiLinks = true;
 config.options.txtMaxEditRows = 20;
-config.usePreForStorage = true;
+
+//config.options.chkSinglePageMode = false;
+//config.options.chkTopOfPageMode = false;
 /*}}}*/
