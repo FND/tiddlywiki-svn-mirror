41c2abec5e1b3a3c57b94935a0133f2ab3413745
seanhess <seanhess@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-01-19 22:19:48 +0000

    created branch to work on selective tiddler uploads.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1331 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/MiniTiddlyServer/Branches/SelectiveSaving/config.php
@@ -0,0 +1,141 @@
+<?php session_start(); 
+    session_unset();
+    session_destroy();
+?>
+<head>
+<title>Admin Configuration Script for AjaxTiddlyWiki</title>
+<style>
+   td, div, body, p {
+	   font-family:verdana;
+       font-size:10pt;
+   }
+   
+   h1 {
+        color:red;
+   }
+</style>
+<script>
+
+function saveAndComplete() {
+
+    var form = document.getElementById("settings");
+    
+    if (form.adminpass.value != form.adminpass2.value || form.adminpass.value == "" || form.adminpass2.value == "") {
+        alert("Error: the passwords to not match or have not been entered");
+        
+        return; 
+    }
+    
+    else if ( form.wrapperpath.value.indexOf(".php") == -1 || form.sourcepath.value.indexOf(".html") == -1) {
+        alert("Error: Please make sure the wrapper path has a .php extension, and the sourcepath has a .html extension");
+        
+        return;
+    }
+    form.submit();
+}
+
+<?php
+
+/*
+Configuration and Installation::
+Run this file after copying everything to your install server. (In a unique folder!)
+
+
+*/
+$configfilepath = "Source/config.txt";
+$templatename = "Source/empty.html";
+$wikiframe = "Source/wikiframe.php";
+$userspath = "Source/users.php";
+
+$adminpass = $_GET['adminpass'];
+$wrapperpath = $_GET['wrapperpath']; 
+$sourcepath = $_GET['sourcepath']; 
+//~ $htmlPrefix = "Data/";
+
+
+if (isset($adminpass) && isset($wrapperpath) && isset($sourcepath) && $adminpass != "" && $wrapperpath != "" && $sourcepath != "") {
+
+        // 1 // Create a new config.txt with the admin password 
+            $usersfile = file($userspath);
+            $usersstr = join("", $usersfile);
+            $usersstr = preg_replace ( '/"ADMINPASS"/',"\"$adminpass\"",$usersstr);
+            writeToFile($userspath, $usersstr);
+        
+        // 2 // Create a new wiki of that name.. 
+        
+            // a // Copy the template wiki
+            if (!copy($templatename, $sourcepath)) {
+               echo "failed to copy $file...\n";
+               exit;
+            }
+            
+            // IMPORT ORIG WIKI // Open the wikiframe, put in the new filename and go.
+            
+            if (!$handle = fopen($wikiframe, 'r')) 
+                echo "error:true, message:'Cannot open file ($wikiframe)',";
+                   
+            if (!$output = fread($handle, filesize($wikiframe))) 
+                echo "error:true, message:'Cannot read file ($wikiframe)',";
+
+            fclose($handle);
+            
+            $output = preg_replace ( '/"WIKIPATH"/i',"\"$sourcepath\"",$output);
+            
+            writeToFile($wrapperpath, $output);
+            
+        // 3 // Delete Config File
+            unlink("config.php");
+            
+        // 4 // Redirect 
+            echo "window.location = \"$wrapperpath\"";
+    
+}
+    
+
+?>
+</script>
+
+</head>
+
+<body>
+<form id="settings" method="GET" action="config.php">
+<h2>AjaxTiddlyWiki</h2>
+
+<h4>Administrator Account</h4>
+<table>
+    <tr><td>user: </td><td>admin</td></tr>
+    <tr><td>password: </td><td><input type="password" size="10" name="adminpass"/></td></tr>
+    <tr><td>repeat pass: </td><td><input type="password" size="10" name="adminpass2"/></td></tr>
+</table>
+
+<h4>WikiFileName</h4>
+<div> Don't forget the extensions below!  The first must be ".php" and the second ".html";</div>
+<table>
+    <tr><td>Wrapper Path (ie: "test.php"): </td><td><input type="text" name="wrapperpath"/></td></tr>
+    <tr><td>Wiki Path (ie: "source.html"): </td><td><input type="text" name="sourcepath"/></td></tr>
+</table>
+
+<h4>Save and Complete</h4>
+<div>By clicking submit below, this config file will be deleted and you will be taken to your new wiki!</div>
+<p><input type="submit" value="Save and Complete" onclick="saveAndComplete()"/></p>
+</form>
+            
+</body>
+
+<?php 
+    function writeToFile($file, $str) {
+        if (!$handle = fopen($file, 'w')) 
+            $data .= "error:true, message:'Cannot open file ($file)',";
+            
+        if (fwrite($handle, $str) === FALSE)
+            $data .= "error:true, message:'Cannot write to file ($file)',";
+
+        else
+            $data .= "saved:true,";
+              
+        fclose($handle);
+    }
+?>
+
+
+
