2a23740b2ebb2ea529796493fa73b25c40c67952
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-01-20 18:17:42 +0000

    Additional commands for demonstration and testing.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1338 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/experimental/HostedCommands2Plugin.js
@@ -0,0 +1,86 @@
+/***
+|''Name:''|HostedCommands2Plugin|
+|''Description:''|Commands to access hosted TiddlyWiki data|
+|''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
+|''Source:''|http://martinswiki.com/martinsprereleases.html#HostedCommandsPlugin|
+|''Subversion:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/experimental|
+|''Version:''|0.1.0|
+|''Date:''|Jan 20, 2007|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
+|''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]]|
+|''~CoreVersion:''|2.2.0|
+***/
+
+//{{{
+// Ensure that the plugin is only installed once.
+if(!version.extensions.HostedCommands2Plugin) {
+version.extensions.HostedCommands2Plugin = {installed:true};
+
+config.macros.list.hosted = {};
+config.macros.list.hosted.prompt = "Tiddlers on the host";
+config.macros.list.hosted.handler = function(params,wikifier,paramString,tiddler)
+{
+//#displayMessage("listHosted:"+params);
+	return store.getHosted(tiddler.fields['server.host'],tiddler.fields['server.workspace']);
+};
+
+// Return an array of tiddler titles  that are on the given workspace on the host
+TiddlyWiki.prototype.getHosted = function(host,workspace)
+{
+//#displayMessage("host:"+host+" ws:"+workspace);
+	var results = [];
+	if(!this.hostedTiddlers)
+		return results;
+	if(!this.hostedTiddlers[host])
+		return results;
+	var list = this.hostedTiddlers[host][workspace];
+	if(list) {
+		for(i=0; i<list.length; i++) {
+			results.push(list[i].title);
+		}
+		results.sort();
+	}
+	return results;
+};
+
+config.macros.updateHostedList = {};
+merge(config.macros.updateHostedList,{
+	label: "update hosted list",
+	prompt: "Update list of hosted tiddlers",
+	done: "List updated"});
+
+config.macros.updateHostedList.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	params = paramString.parseParams("anon",null,true,false,false);
+	var customFields = getParam(params,"fields",false);
+	var btn = createTiddlyButton(place,this.label,this.prompt,this.onClick);
+	btn.setAttribute("customFields",customFields);
+};
+
+config.macros.updateHostedList.onClick = function(e)
+{
+	clearMessage();
+	var customFields = this.getAttribute("customFields");
+	var fields = convertCustomFieldsToHash(customFields);
+	var serverType = store.getServerType(fields);
+	var fn = config.hostFunctions.getTiddlerList[serverType];
+	if(fn) {
+		if(!params)
+			params = {};
+		params.serverHost = fields['server.host'];
+		params.serverWorkspace = fields['server.workspace'];
+		params.callback = config.macros.updateHostedList.onDone;
+		fn(params);
+		return true;
+	}
+	return false;
+};
+
+config.macros.updateHostedList.onDone = function(e)
+{
+	displayMessage(config.macros.updateHostedList.done);
+	story.displayTiddler(null,'ListHosted');// for demo
+};
+
+} // end of 'install only once'
+//}}}
