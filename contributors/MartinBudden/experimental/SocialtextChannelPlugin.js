dc239be3d1ff1bfb01872dc532dd54ccf1adba89
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-01-20 18:16:58 +0000

    Improved handling of page names. Added first revision of a getTiddlerList function.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1337 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/experimental/SocialtextChannelPlugin.js
+++ contributors/MartinBudden/experimental/SocialtextChannelPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
 |''Source:''|http://martinswiki.com/martinsprereleases.html#SocialtextChannelPlugin|
 |''Subversion:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins|
-|''Version:''|0.1.0|
+|''Version:''|0.1.1|
 |''Date:''|Jan 20, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]]|
@@ -41,9 +41,24 @@ SocialtextChannel = {}; // 'namespace' for local functions
 //#SocialtextChannel.mimeType = 'text/vnd.socialtext.wiki';
 SocialtextChannel.mimeType = 'text/x.socialtext-wiki';
 
+SocialtextChannel.canonicalId = function(title)
+{
+	var id = title.toLowerCase();
+	id = id.replace(/\s/g,'_').replace(/\//g,'_').replace(/\./g,'_').replace(/:/g,'').replace(/\?/g,'');
+	if(id.charAt(0)=='_')
+		id = id.substr(1);
+//#displayMessage("title:"+title+" id:"+id);
+	return String(id);
+};
+
+SocialtextChannel.dateFromEditTime = function(editTime)
+{
+	var dt = editTime;
+	return new Date(Date.UTC(dt.substr(0,4),dt.substr(5,2)-1,dt.substr(8,2),dt.substr(11,2),dt.substr(14,2)));
+};
+
 SocialtextChannel.getTiddler = function(title,params)
 {
-//# http://www.socialtext.net/data/workspaces/st-rest-docs/pages
 //# http://www.socialtext.net/data/workspaces/st-rest-docs/pages/socialtext_2_0_preview
 //# http://www.socialtext.net/data/workspaces/st-rest-docs/pages/representation?accept=application/json
 
@@ -51,7 +66,8 @@ SocialtextChannel.getTiddler = function(title,params)
 	var urlTemplate = '%0/data/workspaces/%1/pages/%2?accept=application/json';
 	if(!params.serverHost.match(/:\/\//))
 		urlTemplate = 'http://' + urlTemplate;
-	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,title]);
+	var pageId = SocialtextChannel.canonicalId(title);
+	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,pageId]);
 //#displayMessage('getSocialtext url: '+url);
 
 	params.title = title;
@@ -91,21 +107,19 @@ SocialtextChannel.getTiddlerCallback = function(status,params,responseText,xhr)
 		params.serverPageId = queryResult.page_id;
 		params.serverPageName = queryResult.name;
 		params.modifier = queryResult.last_editor;
-		var dt = queryResult.last_edit_time;
-		var modified = new Date(Date.UTC(dt.substr(0,4),dt.substr(5,2)-1,dt.substr(8,2),dt.substr(11,2),dt.substr(14,2)));
+		params.modified = SocialtextChannel.dateFromEditTime(queryResult.last_edit_time)
 //#displayMessage('modified:'+queryResult.last_edit_time);
-//#displayMessage('modified2:'+modified);
-		params.modified = modified;
+//#displayMessage('modified2:'+params.modified);
 	} catch (ex) {
 		//alert('SocialtextChannel.getTiddlerCallback: JSON error');
 		displayMessage('Response:'+responseText.substr(0,50));
+		return;
 	}
 	// request the page's text
-	//#var urlTemplate = 'http://%0/data/workspaces/%1/pages/%2?accept=text/vnd.socialtext.wiki';
 	var urlTemplate = '%0/data/workspaces/%1/pages/%2?accept=%3';
 	if(!params.serverHost.match(/:\/\//))
 		urlTemplate = 'http://' + urlTemplate;
-	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,params.title,SocialtextChannel.mimeType]);
+	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,params.serverPageId,SocialtextChannel.mimeType]);
 	var req = doHttp('GET',url,null,null,null,null,SocialtextChannel.getTiddlerCallback2,params);
 //#displayMessage('req:'+req);
 };
@@ -124,13 +138,15 @@ SocialtextChannel.getTiddlerCallback2 = function(status,params,responseText,xhr)
 
 SocialtextChannel.putTiddler = function(title,params)
 {
-	var text = store.fetchTiddler(title).text;
+	var tiddler = store.fetchTiddler(title);
+	var text = tiddler.text;
+	var pageId = tiddler.fields.serverPageId ? tiddler.fields.serverPageId : SocialtextChannel.canonicalId(title);
 //# data/workspaces/:ws/pages/:pname
 	var urlTemplate = '%0/data/workspaces/%1/pages/%2';
 	if(!params.serverHost.match(/:\/\//))
 		urlTemplate = 'http://' + urlTemplate;
-	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,title,text]);
-//#displayMessage('putUrl:'+url);
+	var url = urlTemplate.format([params.serverHost,params.serverWorkspace,pageId,text]);
+//#displayMessage('url:'+url);
 
 	params.title = title;
 	var req = doHttp('POST',url,text,SocialtextChannel.mimeType,null,null,SocialtextChannel.putTiddlerCallback,params,{"X-Http-Method": "PUT"});
@@ -148,8 +164,71 @@ SocialtextChannel.putTiddlerCallback = function(status,params,responseText,xhr)
 		params.callback(params);
 };
 
+SocialtextChannel.getTiddlerList = function(params)
+{
+//#displayMessage('getTiddlerList');
+//# http://www.socialtext.net/data/workspaces/st-rest-docs/pages?accept=application/json
+//# data/workspaces/:ws/pages/:pname
+	var urlTemplate = '%0/data/workspaces/%1/pages?accept=application/json';
+	if(!params.serverHost.match(/:\/\//))
+		urlTemplate = 'http://' + urlTemplate;
+	var url = urlTemplate.format([params.serverHost,params.serverWorkspace]);
+//#displayMessage('url:'+url);
+	var req = doHttp('GET',url,null,null,null,null,SocialtextChannel.getTiddlerListCallback,params);
+//#displayMessage('req:'+req);
+};
+
+/*
+[
+{"page_uri":"http://www.socialtext.net/st-rest-docs/index.cgi?rest_api_version_0_9x",
+"name":"REST API version 0.9x",
+"page_id":"rest_api_version_0_9x",
+"modified_time":1162933753,
+"uri":"rest_api_version_0_9x",
+"tags":[],
+"revision_id":20061107210913,
+"last_edit_time":"2006-11-07 21:09:13 GMT",
+"revision_count":1,
+"last_editor":
+"chris.dent@socialtext.com"},
+...
+]
+*/
+
+SocialtextChannel.getTiddlerListCallback = function(status,params,responseText,xhr)
+{
+	//#displayMessage('getTiddlerListCallback status:'+status);
+	//#displayMessage('rt:'+responseText.substr(0,50));
+	//#displayMessage('xhr:'+xhr);
+	if(!status)
+		displayMessage('Status text:'+xhr.statusText);
+	try {
+		//# convert the downloaded data into a javascript object
+		eval('var queryResult=' + responseText);
+		var list = [];
+		for(var i=0; i< queryResult.length; i++) {
+			list[i] = {title:queryResult[i].name,
+						id:queryResult[i].page_id,
+						modified:SocialtextChannel.dateFromEditTime(queryResult[i].last_edit_time),
+						revCount:queryResult[i].revision_count};
+			//list[i] = {title:queryResult[i].page_id};
+		}
+		if(!store.hostedTiddlers)
+			store.hostedTiddlers = {};
+		if(!store.hostedTiddlers[params.serverHost])
+			store.hostedTiddlers[params.serverHost] = {};
+		store.hostedTiddlers[params.serverHost][params.serverWorkspace] = list;
+	} catch (ex) {
+		//alert('SocialtextChannel.getTiddlerCallback: JSON error');
+		displayMessage('Response:'+responseText.substr(0,50));
+	}
+	if(params.callback)
+		params.callback(params);
+};
+
 config.hostFunctions.getTiddler['socialtext'] = SocialtextChannel.getTiddler;
 config.hostFunctions.putTiddler['socialtext'] = SocialtextChannel.putTiddler;
+config.hostFunctions.getTiddlerList['socialtext'] = SocialtextChannel.getTiddlerList;
 
 } // end of 'install only once'
 //}}}
