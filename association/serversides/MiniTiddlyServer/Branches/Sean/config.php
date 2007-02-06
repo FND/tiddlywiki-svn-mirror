38fa1fb330772fe716070ac5bfff7d46742c6c89
seanhess <seanhess@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-06 22:49:28 +0000

    Fixed passwords not matching in config.php
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1457 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/MiniTiddlyServer/Branches/Sean/config.php
+++ association/serversides/MiniTiddlyServer/Branches/Sean/config.php
@@ -31,6 +31,7 @@ function saveAndComplete() {
         
         return;
     }
+    
     form.submit();
 }
 
@@ -106,8 +107,8 @@ if (isset($adminpass) && isset($wrapperpath) && isset($sourcepath) && $adminpass
 
 <h4>Save and Complete</h4>
 <div>By clicking submit below, this config file will be deleted and you will be taken to your new wiki!</div>
-<p><input type="submit" value="Save and Complete" onclick="saveAndComplete()"/></p>
 </form>
+<p><input type="submit" value="Save and Complete" onclick="saveAndComplete()"/></p>
             
 </body>
 
