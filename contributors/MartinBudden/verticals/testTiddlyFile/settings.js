18fed87d6d869ae1d4538f32e6a9d8cb52dacc49
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-16 00:00:17 +0000

    Initial submission.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1574 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/testTiddlyFile/settings.js
@@ -0,0 +1,28 @@
+/***
+|''Name:''|settings|
+|''Description:''|Set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = "backup";
+config.options.txtMaxEditRows = 20;
+
+config.options.chkDisableWikiLinks = true;
+
+config.usePreForStorage = true;
+config.maxTiddlerImportCount = 10;
+
+config.defaultCustomFields = {
+	'server.type':'file',
+};
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+
+config.displayStartupTime = false;
+/*}}}*/
