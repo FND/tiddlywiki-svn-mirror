949c65e17e554d34d8e3a748186caad93acdf60d
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-21 23:09:48 +0000

    Minor fix.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1616 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/experimental/ccTiddlyAdaptorPlugin.js
+++ contributors/MartinBudden/experimental/ccTiddlyAdaptorPlugin.js
@@ -27,9 +27,9 @@ if(!config.options.txtccTiddlyPassword)
 if(!version.extensions.ccTiddlyAdaptorPlugin) {
 version.extensions.ccTiddlyAdaptorPlugin = {installed:true};
 
-function doHttpGET(url,callback,params,headers,data,contentType,username,password)
+function doHttpGET(uri,callback,params,headers,data,contentType,username,password)
 {
-	return doHttp('GET',url,data,contentType,username,password,callback,params,headers);
+	return doHttp('GET',uri,data,contentType,username,password,callback,params,headers);
 }
 
 function ccTiddlyAdaptor()
@@ -101,16 +101,16 @@ ccTiddlyAdaptor.prototype.getTiddlerRevision = function(title,revision,context,u
 	context.tiddler.fields['server.host'] = ccTiddlyAdaptor.minHostName(host);
 	context.tiddler.fields['server.type'] = ccTiddlyAdaptor.serverType;
 	if(revision) {
-		var urlTemplate = '%0msghandle.php?action=revisionDisplay&title=%1&revision=%2';
-		var url = urlTemplate.format([host,title,revision]);
-		//#displayMessage('url: '+url);
-		var req = doHttpGET(url,ccTiddlyAdaptor.getTiddlerCallback2,context);
+		var uriTemplate = '%0msghandle.php?action=revisionDisplay&title=%1&revision=%2';
+		var uri = uriTemplate.format([host,title,revision]);
+//#displayMessage('urir: '+uri);
+		var req = doHttpGET(uri,ccTiddlyAdaptor.getTiddlerCallback2,context);
 	} else {
 		// first get the revision list
-		urlTemplate = '%0msghandle.php?action=revisionList&title=%1';
-		url = urlTemplate.format([host,title]);
-		//#displayMessage('url: '+url);
-		req = doHttpGET(url,ccTiddlyAdaptor.getTiddlerCallback1,context);
+		uriTemplate = '%0msghandle.php?action=revisionList&title=%1';
+		uri = uriTemplate.format([host,title]);
+//#displayMessage('uri: '+uri);
+		req = doHttpGET(uri,ccTiddlyAdaptor.getTiddlerCallback1,context);
 	}
 //#displayMessage("req:"+req);
 	return typeof req == 'string' ? req : true;
@@ -136,12 +136,12 @@ ccTiddlyAdaptor.getTiddlerCallback1 = function(status,context,responseText,xhr)
 		var tiddlerRevision = parts[1];
 		//# http://cctiddly.sourceforge.net/msghandle.php?action=revisionDisplay&title=About&revision=6
 		// now get the latest revision
-		var urlTemplate = '%0msghandle.php?action=revisionDisplay&title=%1&revision=%2';
-		var host = this && this.host ? this.host : ccTiddlyAdaptor.fullHostName(context.tiddler.fields['server.host']);
-		var url = urlTemplate.format([host,context.tiddler.title,tiddlerRevision]);
-//#displayMessage('url1: '+url);
-		var req = doHttpGET(url,ccTiddlyAdaptor.getTiddlerCallback2,context);
-		//#displayMessage("req1:"+req);
+		var uriTemplate = '%0msghandle.php?action=revisionDisplay&title=%1&revision=%2';
+		var host = ccTiddlyAdaptor.fullHostName(context.adaptor.host);
+		var uri = uriTemplate.format([host,context.tiddler.title,tiddlerRevision]);
+//#displayMessage('uri: '+uri);
+		var req = doHttpGET(uri,ccTiddlyAdaptor.getTiddlerCallback2,context);
+//#displayMessage("req1:"+req);
 	} else {
 		context.statusText = xhr.statusText;
 	if(context.callback)
@@ -152,7 +152,7 @@ ccTiddlyAdaptor.getTiddlerCallback1 = function(status,context,responseText,xhr)
 //# http://cctiddly.sourceforge.net/msghandle.php?action=revisionDisplay&title=About&revision=6
 //#0 About
 //#1 About
-//#2 !About\nccTiddly is a tiddly adaptation based on PHP and MySQL to store tiddlers. This is a server side adaptation which allow its user to change their TiddlyWiki over HTTP. It is also possible to generate a standalone version with this!\n\n!Target audiance/uses\n*Online edittable TW\n**Blog\n**news site\n**personal ToDo list\n**online bookmark (with TiddlySnip, still in development)\n*As a collaboration tool\n\n!Features\n|!Add/Edit/Save tiddler over the web|basic tiddly wiki functions allow user to create, edit and delete tiddler but over the internet|\n|!Standalone generation|allow generation of standalone version if required, which is the one available on http://tiddlywiki.com|\n|!Access control|password protect the tiddly so only certain user can change its content over the net, hide ones you don't want anyone but you to see. All in the new privilege system!|\n|!Multiple config file|multiple config file for multiple ccT. One setup, multiple ccTiddly!|\n|!Select particular config file|ability to use a certain config file, which allow multiple tiddly host with one URL|\n|!Versioning|All versions of tiddlers kept|\n|!Import your TW|you can import your TW into ccT fo use and convert back to original TW when you need to bring it around on flashdisk|\n|!Multi-lingual|It may not be in your language now but you can easily translate to your own language|\n|!Tag filter|You can filter out your tiddlers by tag|
+//#2 !About\nccTiddly is a tiddly adaptation based on PHP and MySQL to store tiddlers. This is a server side adaptation which allow its user to change their TiddlyWiki over HTTP. It is also possible to generate a standalone version with this!\n\n!Target audiance/uses\n*Online edittable TW\n**Blog\n**news site\n**personal ToDo list\n**online bookmark (with TiddlySnip, still in development)\n*As a collaboration tool\n\n!Features\n|!Add/Edit/Save tiddler over the web|basic tiddly wiki functions allow user to create, edit and delete tiddler but over the internet|\n|!Standalone generation|allow generation of standalone version if required, which is the one available on http://tiddlywiki.com|\n|!Access control|password protect the tiddly so only certain user can change its content over the net, hide ones you don't want anyone but you to see. All in the new privilege system!|\n|!Multiple config file|multiple config file for multiple ccT. One setup, multiple ccTiddly!|\n|!Select particular config file|ability to use a certain config file, which allow multiple tiddly host with one uri|\n|!Versioning|All versions of tiddlers kept|\n|!Import your TW|you can import your TW into ccT fo use and convert back to original TW when you need to bring it around on flashdisk|\n|!Multi-lingual|It may not be in your language now but you can easily translate to your own language|\n|!Tag filter|You can filter out your tiddlers by tag|
 //#3 ccTiddly
 //#4 200610221408
 //#5 200601140131
@@ -196,10 +196,10 @@ ccTiddlyAdaptor.prototype.getTiddlerRevisionList = function(title,context,userPa
 	title = encodeURIComponent(title);
 //#displayMessage('getTiddlerRevisionList:'+title);
 //# http://cctiddly.sourceforge.net/msghandle.php?action=revisionList&title=About
-	var urlTemplate = '%0msghandle.php?action=revisionList&title=%1';
+	var uriTemplate = '%0msghandle.php?action=revisionList&title=%1';
 	var host = ccTiddlyAdaptor.fullHostName(this.host);
-	var url = urlTemplate.format([host,title]);
-//#displayMessage('url: '+url);
+	var uri = uriTemplate.format([host,title]);
+//#displayMessage('uri: '+uri);
 	if(!context) context = {};
 	context.userParams = userParams;
 	context.adaptor = this;
@@ -207,11 +207,11 @@ ccTiddlyAdaptor.prototype.getTiddlerRevisionList = function(title,context,userPa
 	context.tiddler = new Tiddler(title);
 	context.tiddler.fields['server.host'] = ccTiddlyAdaptor.minHostName(host);
 	context.tiddler.fields['server.type'] = ccTiddlyAdaptor.serverType;
-	var req = doHttpGET(url,ccTiddlyAdaptor.getTiddlerRevisionListCallback,context);
+	var req = doHttpGET(uri,ccTiddlyAdaptor.getTiddlerRevisionListCallback,context);
 //#displayMessage("req:"+req);
 };
 
-ccTiddlyAdaptor.getTiddlerRevisionListCallback = function(status,context,responseText,url,xhr)
+ccTiddlyAdaptor.getTiddlerRevisionListCallback = function(status,context,responseText,uri,xhr)
 {
 //#displayMessage('getTiddlerRevisionListCallback status:'+status);
 //#displayMessage('rt:'+responseText.substr(0,50));
@@ -222,14 +222,14 @@ ccTiddlyAdaptor.getTiddlerRevisionListCallback = function(status,context,respons
 		var r =  responseText;
 		if(r != '-') {
 			var revs = r.split('\n');
+			var list = [];
 			for(var i=0; i<revs.length; i++) {
 				var parts = revs[i].split(' ');
 				if(parts.length>1) {
-					var tiddler = new Tiddler(info[i].name);
-					list[i] = {};
+					var tiddler = new Tiddler(context.tiddler.title);
 					tiddler.modified = Date.convertFromYYYYMMDDHHMM(parts[0]);
-					tiddler.fields['server.page.revision'] = parts[1];
-					tiddler.fields['server.page.version'] = parts[1];//!! here temporarily for compatibility
+					tiddler.fields['server.page.revision'] = String(parts[1]);
+					tiddler.fields['server.page.version'] = tiddler.fields['server.page.revision'];//!! here temporarily for compatibility
 					list.push(tiddler);
 				}
 			}
@@ -250,9 +250,9 @@ ccTiddlyAdaptor.prototype.putTiddler = function(tiddler,context,callback)
 	var title = encodeURIComponent(tiddler.title);
 //#displayMessage('putTiddler:'+title);
 	var host = this && this.host ? this.host : ccTiddlyAdaptor.fullHostName(tiddler.fields['server.host']);
-	var urlTemplate = '%0RPC2/';
-	var url = urlTemplate.format([host,title]);
-//#displayMessage('url: '+url);
+	var uriTemplate = '%0RPC2/';
+	var uri = uriTemplate.format([host,title]);
+//#displayMessage('uri: '+uri);
 
 	if(!context) context = {};
 	context.userParams = userParams;
@@ -261,12 +261,12 @@ ccTiddlyAdaptor.prototype.putTiddler = function(tiddler,context,callback)
 	context.tiddler = tiddler;
 	context.tiddler.fields['server.host'] = ccTiddlyAdaptor.minHostName(host);
 	context.tiddler.fields['server.type'] = ccTiddlyAdaptor.serverType;
-	var req =doHttp('POST',url,payload,null,this.username,this.password,ccTiddlyAdaptor.putTiddlerCallback,tiddler.text);
+	var req =doHttp('POST',uri,payload,null,this.username,this.password,ccTiddlyAdaptor.putTiddlerCallback,tiddler.text);
 //#displayMessage("req:"+req);
 	return typeof req == 'string' ? req : true;
 };
 
-ccTiddlyAdaptor.putTiddlerCallback = function(status,context,responseText,url,xhr)
+ccTiddlyAdaptor.putTiddlerCallback = function(status,context,responseText,uri,xhr)
 {
 //#displayMessage('putTiddlerCallback status:'+status);
 //#displayMessage('rt:'+responseText.substr(0,50));
