677011f959f0b30247bccaaa8f3f03354bd56cce
BobMcElrath <BobMcElrath@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-27 08:15:25 +0000

    Mozilla fix for "unterminated character class", and 2.0/2.1 core compatability fixes.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1693 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/BobMcElrath/plugins/jsmath.js
+++ contributors/BobMcElrath/plugins/jsmath.js
@@ -2,8 +2,8 @@
 |Name|Plugin: jsMath|
 |Created by|BobMcElrath|
 |Email|my first name at my last name dot org|
-|Location|http://bob.mcelrath.org/tiddlyjsmath-2.0.3.html|
-|Version|1.4|
+|Location|http://bob.mcelrath.org/tiddlyjsmath.html|
+|Version|1.5.1|
 |Requires|[[TiddlyWiki|http://www.tiddlywiki.com]] &ge; 2.0.3, [[jsMath|http://www.math.union.edu/~dpvc/jsMath/]] &ge; 3.0|
 !Description
 LaTeX is the world standard for specifying, typesetting, and communicating mathematics among scientists, engineers, and mathematicians.  For more information about LaTeX itself, visit the [[LaTeX Project|http://www.latex-project.org/]].  This plugin typesets math using [[jsMath|http://www.math.union.edu/~dpvc/jsMath/]], which is an implementation of the TeX math rules and typesetting in javascript, for your browser.  Notice the small button in the lower right corner which opens its control panel.
@@ -26,6 +26,8 @@ If you had used a previous version of [[Plugin: jsMath]], it is no longer necess
 * 24-Jan-06, version 1.2, fixes for Safari, Konqueror
 * 27-Jan-06, version 1.3, improved error handling, detect if ajax was already defined (used by ZiddlyWiki)
 * 12-Jul-06, version 1.4, fixed problem with not finding image fonts
+* 26-Feb-07, version 1.5, fixed problem with Mozilla "unterminated character class".
+* 27-Feb-07, version 1.5.1, Runs compatibly with TW 2.1.0+, by Bram Chen
 !Examples
 |!Source|!Output|h
 |{{{The variable $x$ is real.}}}|The variable $x$ is real.|
@@ -52,7 +54,7 @@ if(typeof(window["ajax"]) == "undefined") {
 // Load jsMath
 jsMath = {
   Setup: {inited: 1},          // don't run jsMath.Setup.Body() yet
-  Autoload: {root: new String(document.location).replace(/[^/]*$/,'jsMath/')}  // URL to jsMath directory, change if necessary
+  Autoload: {root: new String(document.location).replace(/[^\/]*$/,'jsMath/')}  // URL to jsMath directory, change if necessary
 };
 var jsMathstr;
 try {
@@ -91,7 +93,8 @@ config.formatterHelpers.mathFormatHelper = function(w) {
 config.formatters.push({
   name: "displayMath1",
   match: "\\\$\\\$",
-  terminator: "\\\$\\\$\\n?",
+  terminator: "\\\$\\\$\\n?", // 2.0 compatability
+  termRegExp: "\\\$\\\$\\n?",
   element: "div",
   className: "math",
   handler: config.formatterHelpers.mathFormatHelper
@@ -100,7 +103,8 @@ config.formatters.push({
 config.formatters.push({
   name: "inlineMath1",
   match: "\\\$", 
-  terminator: "\\\$",
+  terminator: "\\\$", // 2.0 compatability
+  termRegExp: "\\\$",
   element: "span",
   className: "math",
   handler: config.formatterHelpers.mathFormatHelper
@@ -111,7 +115,8 @@ var backslashformatters = new Array(0);
 backslashformatters.push({
   name: "inlineMath2",
   match: "\\\\\\\(",
-  terminator: "\\\\\\\)",
+  terminator: "\\\\\\\)", // 2.0 compatability
+  termRegExp: "\\\\\\\)",
   element: "span",
   className: "math",
   handler: config.formatterHelpers.mathFormatHelper
@@ -120,7 +125,8 @@ backslashformatters.push({
 backslashformatters.push({
   name: "displayMath2",
   match: "\\\\\\\[",
-  terminator: "\\\\\\\]\\n?",
+  terminator: "\\\\\\\]\\n?", // 2.0 compatability
+  termRegExp: "\\\\\\\]\\n?",
   element: "div",
   className: "math",
   handler: config.formatterHelpers.mathFormatHelper
@@ -129,7 +135,8 @@ backslashformatters.push({
 backslashformatters.push({
   name: "displayMath3",
   match: "\\\\begin\\{equation\\}",
-  terminator: "\\\\end\\{equation\\}\\n?",
+  terminator: "\\\\end\\{equation\\}\\n?", // 2.0 compatability
+  termRegExp: "\\\\end\\{equation\\}\\n?",
   element: "div",
   className: "math",
   handler: config.formatterHelpers.mathFormatHelper
@@ -139,7 +146,8 @@ backslashformatters.push({
 backslashformatters.push({
   name: "displayMath4",
   match: "\\\\begin\\{eqnarray\\}",
-  terminator: "\\\\end\\{eqnarray\\}\\n?",
+  terminator: "\\\\end\\{eqnarray\\}\\n?", // 2.0 compatability
+  termRegExp: "\\\\end\\{eqnarray\\}\\n?",
   element: "div",
   className: "math",
   keepdelim: true,
@@ -163,9 +171,13 @@ config.formatters=backslashformatters.concat(config.formatters);
 window.wikify = function(source,output,highlightRegExp,tiddler)
 {
     if(source && source != "") {
-        var wikifier = new
-        Wikifier(source,formatter,highlightRegExp,tiddler);
-        wikifier.subWikify(output,null);
+        if(version.major == 2 && version.minor > 0) {
+            var wikifier = new Wikifier(source,getParser(tiddler),highlightRegExp,tiddler);
+            wikifier.subWikifyUnterm(output);
+        } else {
+            var wikifier = new Wikifier(source,formatter,highlightRegExp,tiddler);
+            wikifier.subWikify(output,null);
+        }
         jsMath.ProcessBeforeShowing();
     }
 }
