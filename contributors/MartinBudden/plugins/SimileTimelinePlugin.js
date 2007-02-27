9736184cb1a6ed44460db0511d4f9e835ea1207b
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-27 23:43:20 +0000

    More work.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1701 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/SimileTimelinePlugin.js
+++ contributors/MartinBudden/plugins/SimileTimelinePlugin.js
@@ -11,24 +11,27 @@
 ***/
 
 /*{{{*/
-Timeline.loadTiddlers = function(url,f,tag)
+Timeline.loadTiddlers = function(data,f)
 {
-/*    var fError = function(statusText,status,xmlhttp) {
-        alert("Failed to load tiddlers from " + url + "\n" + statusText);
-    };
-    var fDone = function(xmlhttp) {
-        f(xmlhttp.responseText,url,tag);
-    };
-    Timeline.XmlHttp.get(url,fError,fDone);
-*/
-	f(null,url,tag);
+	f(data);
 };
 
-Timeline.DefaultEventSource.prototype.loadTiddlers = function(data,url,tag)
+Timeline.GregorianDateLabeller.labelIntervalFunctions["en"] = function(date,intervalUnit)
 {
+	if(intervalUnit==Timeline.DateTime.WEEK) {
+		var date2 = Timeline.DateTime.removeTimeZoneOffset(date,this._timeZone);
+		var text = "" + date2.getWeek();
+		return { text: text, emphasized: false };
+	} else {
+		return this.defaultLabelInterval(date,intervalUnit);
+	}
+};
+
+Timeline.DefaultEventSource.prototype.loadTiddlers = function(data)
+{
+	var tag = data.tag;
+	var url = data.url ? data.url : "dummy";
 //#displayMessage('loadTiddlers:'+tag);
-//#tag = 'CubismTimeline';
-//#displayMessage('loadTiddlers2:'+tag);
     var base = this._getBaseURL(url);
     var added = false;  
     var wikiURL = null;
@@ -50,45 +53,55 @@ Timeline.DefaultEventSource.prototype.loadTiddlers = function(data,url,tag)
             event.isDuration || false,
             event.title,
             event.description,
-            this._resolveRelativeURL(event.image, base),
-            this._resolveRelativeURL(event.link, base),
-            this._resolveRelativeURL(event.icon, base),
+            this._resolveRelativeURL(event.image,base),
+            this._resolveRelativeURL(event.link,base),
+            this._resolveRelativeURL(event.icon,base),
             event.color,
             event.textColor);
         evt._obj = event;
         evt.getProperty = function(name) {
             return this._obj[name];
         };
-        evt.setWikiInfo(wikiURL, wikiSection);
+        evt.setWikiInfo(wikiURL,wikiSection);
 
         this._events.add(evt);
         added = true;
     }
    
     if (added) {
-        this._fire('onAddMany', []);
+        this._fire('onAddMany',[]);
     }
 };
 
 config.macros.SimileTimeline = {};
 
+// to allow loading from tiddlers with differently named fields
+config.macros.SimileTimeline.eventFields = {
+	start:'start',latestStart:'latestStart',end:'end',earliestEnd:'earliestEnd',
+	isDuration:'isDuration',title:'title',description:'description',image:'image',link:'link',
+	icon:'icon',color:'color',textColor:'textColor'
+	};
+
 config.macros.SimileTimeline.getEvent = function(title)
 {
 //#displayMessage('getEvent:'+title);
 	var t = title;
+	var f = config.macros.SimileTimeline.eventFields;
+//#displayMessage('f:'+f.start);
 	var ev = {};
-	ev.start = store.getTiddlerSlice(t,'start');
-	ev.latestStart = store.getTiddlerSlice(t,'latestStart');
-	ev.end = store.getTiddlerSlice(t,'end');
-	ev.earliestEnd = store.getTiddlerSlice(t,'earliestEnd');
-	ev.isDuration = store.getTiddlerSlice(t,'isDuration');
-	ev.title = store.getTiddlerSlice(t,'title');
-	ev.description = store.getTiddlerSlice(t,'description');
-	ev.image = store.getTiddlerSlice(t,'image');
-	ev.link = store.getTiddlerSlice(t,'link');
-	ev.icon = store.getTiddlerSlice(t,'icon');
-	ev.color = store.getTiddlerSlice(t,'color');
-	ev.textColor = store.getTiddlerSlice(t,'textColor');
+
+	ev.start = store.getTiddlerSlice(t,f.start);
+	ev.latestStart = store.getTiddlerSlice(t,f.latestStart);
+	ev.end = store.getTiddlerSlice(t,f.end);
+	ev.earliestEnd = store.getTiddlerSlice(t,f.earliestEnd);
+	ev.isDuration = store.getTiddlerSlice(t,f.isDuration);
+	ev.title = store.getTiddlerSlice(t,f.title);
+	ev.description = store.getTiddlerSlice(t,f.description);
+	ev.image = store.getTiddlerSlice(t,f.image);
+	ev.link = store.getTiddlerSlice(t,f.link);
+	ev.icon = store.getTiddlerSlice(t,f.icon);
+	ev.color = store.getTiddlerSlice(t,f.color);
+	ev.textColor = store.getTiddlerSlice(t,f.textColor);
 //#displayMessage('t:'+ev.title+' s:'+ev.start+' e:'+ev.end);
 	return ev;
 };
@@ -96,69 +109,45 @@ config.macros.SimileTimeline.getEvent = function(title)
 
 config.macros.SimileTimeline.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
-	var title = params[0];
+	var spec = params[0];
 	var eventSource = new Timeline.DefaultEventSource();
-  /*var bandInfos = [
-    Timeline.createBandInfo({
-           eventSource:    eventSource,
-        date:           'Jun 28 2006 00:00:00 GMT',
-	     width:          '70%', 
-        intervalUnit:   Timeline.DateTime.MONTH, 
-        intervalPixels: 100
-    }),
-    Timeline.createBandInfo({
-           eventSource:    eventSource,
-        date:           'Jun 28 2006 00:00:00 GMT',
-        showEventText:  false,
-        trackHeight:    0.5,
-        trackGap:       0.2,
-        width:          '30%', 
-        intervalUnit:   Timeline.DateTime.YEAR, 
-        intervalPixels: 200
-    })
-  ];
-	bandInfos[1].syncWith = 0;
-	bandInfos[1].highlight = true;
-*/
     var theme = Timeline.ClassicTheme.create();
-    theme.event.bubble.width = 320;
-    theme.event.bubble.height = 220;
-
-    var d = Timeline.DateTime.parseGregorianDateTime('1920');
+	var bwidth = store.getTiddlerSlice(spec,'BubbleWidth');
+	var bheight = store.getTiddlerSlice(spec,'BubbleHeight');
+    theme.event.bubble.width = bwidth ? bwidth : 320;
+    theme.event.bubble.height = bheight ? bheight : 220;
 
 	var bandInfos = [];
-
-	var bandParams = {};
-//#bandParams.width = '100%';
-	bandParams = config.macros.SimileTimeline.getBandParams(title,'1',eventSource,theme);
+	var bandParams = config.macros.SimileTimeline.getBandParams(spec,'0',eventSource,theme);
 	var bi = Timeline.createBandInfo(bandParams);
-
 	bandInfos.push(bi);
+
 	var timeline = createTiddlyElement(place,'div',null,'simileTimeline');// simileTimeline css class
-	timeline.style['height'] = '150px';
-	timeline.style['border'] = '1px';
-
-	config.macros.SimileTimeline.tl = Timeline.create(timeline,bandInfos);
-//#displayMessage('title:'+title);
-	var tag = store.getTiddlerSlice(title,'SimileTimelineTag');
-//#	displayMessage('tag:'+tag);
-	Timeline.loadTiddlers('cubism.js', function(data,url,tag) { eventSource.loadTiddlers(data,url,tag); },tag);
-	//Timeline.loadJSON('cubism.js', function(data, url) { eventSource.loadJSON(data, url); });
-	//Timeline.loadXML('example1.xml', function(xml, url) { eventSource.loadXML(xml, url); });
- };
-
+	var theight = store.getTiddlerSlice(spec,'timelineHeight');
+	timeline.style['height'] = theight ? theight + 'px' : '150px';
+	var tborder = store.getTiddlerSlice(spec,'timelineBorder');
+	if(tborder)
+		timeline.style['border'] = tborder + 'px';
+	config.macros.SimileTimeline.timeline = Timeline.create(timeline,bandInfos);
+	var data = {};
+	data.tag = store.getTiddlerSlice(spec,'timelineTag');
+	//#	displayMessage("handler tag:"+data.tag);
+	Timeline.loadTiddlers(data, function(data,url) { eventSource.loadTiddlers(data,url); });
+};
 
 config.macros.SimileTimeline.getBandParams = function(title,n,eventSource,theme)
 {
-//#displayMessage('getBandParams:'+title);
 	var t = title;
+	var width = store.getTiddlerSlice(t,'width'+n);
+//#displayMessage("width:"+width);
+	if(!width)
+		return null;
 	var bp = {};
+	bp.width = width;
 	if(eventSource)
 		bp.eventSource = eventSource;
 	if(theme)
 		bp.theme = theme;
-	bp.width = store.getTiddlerSlice(t,'width'+n);
-displayMessage('bp.width:'+bp.width);
 	var intervalUnit = store.getTiddlerSlice(t,'intervalUnit'+n);
 	switch(intervalUnit) {
 	case 'MILLISECOND':
