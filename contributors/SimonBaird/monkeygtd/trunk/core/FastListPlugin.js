4f173959cae1aaa68b75f245ab5068f0e2f3c719
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-28 14:53:39 +0000

    Added viewType param for fastList. Also project link for action view.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1710 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/monkeygtd/trunk/core/FastListPlugin.js
+++ contributors/SimonBaird/monkeygtd/trunk/core/FastListPlugin.js
@@ -1,6 +1,12 @@
 
 // this must be used in conjunction with IndexedTagsPlugin
 
+merge(Tiddler.prototype,{
+	fastGet: function(what) {
+		return config.indexedTags.indexes[this.title][what];
+	}
+});
+
 merge(config.macros,{
 
 	fastList: {
@@ -14,8 +20,10 @@ merge(config.macros,{
 			// not scanning every tiddler each time
 			// will use the indexes from IndexedTagsPlugin
 			var startTag = getParam(parsedParams,"startTag");
-			var tagExpr = getParam(parsedParams,"tagExpr");
-			var sortBy = getParam(parsedParams,"sortBy","title");
+			var tagExpr = getParam(parsedParams,"tags");
+			var sortBy = getParam(parsedParams,"sort","title");
+			var viewType = getParam(parsedParams,"view","plain");
+			var listTitle = getParam(parsedParams,"title","List");
 			var mode = getParam(parsedParams,"local");
 
 			if (!startTag)
@@ -25,16 +33,17 @@ merge(config.macros,{
 	
 			var listDiv = createTiddlyElement(place,"div",null,"mList");
 			var listTitleSpan = createTiddlyElement(listDiv,"span",null,"mListTitle");
-			createTiddlyText(listTitleSpan,"Hey");
+			createTiddlyText(listTitleSpan,listTitle);
 
 			var listRefreshContainer = createTiddlyElement(listDiv,"div");
 
 			listRefreshContainer.setAttribute("refresh","macro");
 			listRefreshContainer.setAttribute("macroName",macroName);
 
-       		listRefreshContainer.setAttribute("startTag",startTag);
-       		listRefreshContainer.setAttribute("tagExpr",tagExpr);
-       		listRefreshContainer.setAttribute("sortBy",sortBy);
+   		listRefreshContainer.setAttribute("startTag",startTag);
+   		listRefreshContainer.setAttribute("tagExpr",tagExpr);
+   		listRefreshContainer.setAttribute("sortBy",sortBy);
+   		listRefreshContainer.setAttribute("viewType",viewType);
 
 			this.refresh(listRefreshContainer);
 
@@ -44,17 +53,26 @@ merge(config.macros,{
 			var startTag = place.getAttribute("startTag");
 			var tagExpr = place.getAttribute("tagExpr");
 			var sortBy = place.getAttribute("sortBy");
+			var viewType = place.getAttribute("viewType");
 			var list = this.tagList(startTag,tagExpr,sortBy);
 			var markup = "";
 			for (var i=0;i<list.length;i++) {
-				markup += this.renderTiddler["action"](list[i],true);
+				markup += this.renderTiddler[viewType](list[i],true);
 			}
 			removeChildren(place);
 			wikify(markup,place,null,tiddler);
 		},
 
 		renderTiddler: {
+
 			action: function(t,showProject) {
+
+				var projText = "";
+				if (showProject) {
+					var p = t.fastGet("Project");
+					if (p)
+						projText = "[/%%/[[P|"+p+"]]/%%/]";
+				}
 				return "{{action2{"+
 					"@@font-size:80%;"+
 					"<<toggleTag Done [["+t.title+"]] ->>"+
@@ -63,8 +81,14 @@ merge(config.macros,{
 					"<<tTag tag:[[Starred]] mode:text text:{{config.mGTD.star}} title:[["+t.title+"]]>>"+
 					" @@"+
 					"[["+t.title+"]] "+
+					projText +
 					"}}}\n";
+			},
+
+			plain: function(t) {
+				return "*[["+t.title+"]]\n";
 			}
+
 		},
 
 		tagList: function(startTag,tagExpr,sortBy) {
