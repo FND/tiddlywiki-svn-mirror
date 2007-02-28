24ad6bf746cb974eb160ae501ccbcf9438e93e3b
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-02-28 22:39:47 +0000

    Started adding support for multiple bands in a timeline.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1711 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/SimileTimelinePlugin.js
+++ contributors/MartinBudden/plugins/SimileTimelinePlugin.js
@@ -11,17 +11,17 @@
 ***/
 
 /*{{{*/
-Timeline.loadTiddlers = function(data,f)
+Timeline.loadTiddlers = function(data,fn)
 {
-	f(data);
+	fn(data);
 };
 
 Timeline.GregorianDateLabeller.labelIntervalFunctions["en"] = function(date,intervalUnit)
 {
 	if(intervalUnit==Timeline.DateTime.WEEK) {
 		var date2 = Timeline.DateTime.removeTimeZoneOffset(date,this._timeZone);
-		var text = "" + date2.getWeek();
-		return { text: text, emphasized: false };
+		var text = '' + date2.getWeek();
+		return {text:text,emphasized:false};
 	} else {
 		return this.defaultLabelInterval(date,intervalUnit);
 	}
@@ -29,48 +29,48 @@ Timeline.GregorianDateLabeller.labelIntervalFunctions["en"] = function(date,inte
 
 Timeline.DefaultEventSource.prototype.loadTiddlers = function(data)
 {
+//#displayMessage('loadTiddlers:'+data.tag);
 	var tag = data.tag;
 	var url = data.url ? data.url : "dummy";
-//#displayMessage('loadTiddlers:'+tag);
-    var base = this._getBaseURL(url);
-    var added = false;  
-    var wikiURL = null;
-    var wikiSection = null;
+	var base = this._getBaseURL(url);
+	var added = false;  
+	var wikiURL = null;
+	var wikiSection = null;
 
-    var dateTimeFormat = null;
-    var parseDateTimeFunction = this._events.getUnit().getParser(dateTimeFormat);
+	var dateTimeFormat = null;
+	var parseDateTimeFunction = this._events.getUnit().getParser(dateTimeFormat);
    
 	var sortField = 'title';
 	var tagged = store.getTaggedTiddlers(tag,sortField);
 //#displayMessage('length:'+tagged.length);
 	for(var i=0; i<tagged.length; i++) {
 		var event = config.macros.SimileTimeline.getEvent(tagged[i].title);
-        var evt = new Timeline.DefaultEventSource.Event(
+		var evt = new Timeline.DefaultEventSource.Event(
 			parseDateTimeFunction(event.start),
 			parseDateTimeFunction(event.end),
 			parseDateTimeFunction(event.latestStart),
 			parseDateTimeFunction(event.earliestEnd),
-            event.isDuration || false,
-            event.title,
-            event.description,
-            this._resolveRelativeURL(event.image,base),
-            this._resolveRelativeURL(event.link,base),
-            this._resolveRelativeURL(event.icon,base),
-            event.color,
-            event.textColor);
-        evt._obj = event;
-        evt.getProperty = function(name) {
-            return this._obj[name];
-        };
-        evt.setWikiInfo(wikiURL,wikiSection);
+			event.isDuration || false,
+			event.title,
+			event.description,
+			this._resolveRelativeURL(event.image,base),
+			this._resolveRelativeURL(event.link,base),
+			this._resolveRelativeURL(event.icon,base),
+			event.color,
+			event.textColor);
+		evt._obj = event;
+		evt.getProperty = function(name) {
+			return this._obj[name];
+		};
+		evt.setWikiInfo(wikiURL,wikiSection);
 
-        this._events.add(evt);
-        added = true;
-    }
-   
-    if (added) {
-        this._fire('onAddMany',[]);
-    }
+		this._events.add(evt);
+		added = true;
+	}
+
+	if (added) {
+		this._fire('onAddMany',[]);
+	}
 };
 
 config.macros.SimileTimeline = {};
@@ -111,40 +111,56 @@ config.macros.SimileTimeline.handler = function(place,macroName,params,wikifier,
 {
 	var spec = params[0];
 	var eventSource = new Timeline.DefaultEventSource();
-    var theme = Timeline.ClassicTheme.create();
+	var theme = Timeline.ClassicTheme.create();
+
 	var bwidth = store.getTiddlerSlice(spec,'BubbleWidth');
+	if(bwidth)
+		theme.event.bubble.width = bwidth;
 	var bheight = store.getTiddlerSlice(spec,'BubbleHeight');
-    theme.event.bubble.width = bwidth ? bwidth : 320;
-    theme.event.bubble.height = bheight ? bheight : 220;
+	if(bheight)
+		theme.event.bubble.height = bheight;
 
+	var defaultDate = Timeline.DateTime.parseGregorianDateTime('2000')
 	var bandInfos = [];
-	var bandParams = config.macros.SimileTimeline.getBandParams(spec,'0',eventSource,theme);
-	var bi = Timeline.createBandInfo(bandParams);
+	var bandParams = config.macros.SimileTimeline.getBandParams(spec,'0',eventSource,theme,defaultDate);
+	defaultDate = bandParams.date;
+	var bi = bandParams.zones ? Timeline.createHotZoneBandInfo(bandParams) : Timeline.createBandInfo(bandParams);
 	bandInfos.push(bi);
-
+	
+	var i = 1;
+	bandParams = config.macros.SimileTimeline.getBandParams(spec,'1',eventSource,theme,defaultDate);
+	while(bandParams) {
+		bi = bandParams.zones ? Timeline.createHotZoneBandInfo(bandParams) : Timeline.createBandInfo(bandParams);
+		bandInfos.push(bi);
+		bandInfos[i].syncWith = 0;
+		bandInfos[i].highlight = true;
+		i++;
+		bandParams = config.macros.SimileTimeline.getBandParams(spec,String(i),eventSource,theme,defaultDate);
+	}
 	var timeline = createTiddlyElement(place,'div',null,'simileTimeline');// simileTimeline css class
-	var theight = store.getTiddlerSlice(spec,'timelineHeight');
-	timeline.style['height'] = theight ? theight + 'px' : '150px';
-	var tborder = store.getTiddlerSlice(spec,'timelineBorder');
-	if(tborder)
-		timeline.style['border'] = tborder + 'px';
+	var tHeight = store.getTiddlerSlice(spec,'timelineHeight');
+	timeline.style['height'] = tHeight ? tHeight + 'px' : '150px';
+	var tBorder = store.getTiddlerSlice(spec,'timelineBorder');
+	if(tBorder)
+		timeline.style['border'] = tBorder + 'px';
 	config.macros.SimileTimeline.timeline = Timeline.create(timeline,bandInfos);
 	var data = {};
-	data.tag = store.getTiddlerSlice(spec,'timelineTag');
-	//#	displayMessage("handler tag:"+data.tag);
-	Timeline.loadTiddlers(data, function(data,url) { eventSource.loadTiddlers(data,url); });
+	data.tag = store.getTiddlerSlice(spec,'eventSourceTag0');
+//#displayMessage("handler tag:"+data.tag);
+	if(data.tag)
+		Timeline.loadTiddlers(data, function(data,url) { eventSource.loadTiddlers(data,url); });
 };
 
-config.macros.SimileTimeline.getBandParams = function(title,n,eventSource,theme)
+config.macros.SimileTimeline.getBandParams = function(title,n,eventSource,theme,defaultDate)
 {
 	var t = title;
 	var width = store.getTiddlerSlice(t,'width'+n);
-//#displayMessage("width:"+width);
+//#displayMessage("width"+n+":"+width);
 	if(!width)
 		return null;
 	var bp = {};
 	bp.width = width;
-	if(eventSource)
+	if(eventSource && store.getTiddlerSlice(title,'eventSourceTag'+n))
 		bp.eventSource = eventSource;
 	if(theme)
 		bp.theme = theme;
@@ -189,34 +205,22 @@ config.macros.SimileTimeline.getBandParams = function(title,n,eventSource,theme)
 	case 'ERA':
 		bp.intervalUnit = -2;
 		break;
+	default:
+		bp.intervalUnit = 7;
+		break;
 	}
-//#displayMessage('bp.intervalUnit:'+bp.intervalUnit);
-	bp.intervalPixels = store.getTiddlerSlice(t,'intervalPixels'+n);
-//#displayMessage('bp.intervalPixels:'+bp.intervalPixels);
-	bp.date = store.getTiddlerSlice(t,'date'+n);
-	if(bp.date)
-	    bp.date = Timeline.DateTime.parseGregorianDateTime(bp.date);
-	//bp.trackHeight = store.getTiddlerSlice(t,'trackHeight'+n);
-	//bp.trackGap = store.getTiddlerSlice(t,'trackGap'+n);
-	//bp.showEventText = store.getTiddlerSlice(t,'showEventText'+n);
+	var intervalPixels = store.getTiddlerSlice(t,'intervalPixels'+n);
+	bp.intervalPixels = eval(intervalPixels);
+	var date = store.getTiddlerSlice(t,'date'+n);
+	bp.date = date ? Timeline.DateTime.parseGregorianDateTime(date) : defaultDate;
+	var showEventText = store.getTiddlerSlice(t,'showEventText'+n);
+	bp.showEventText = showEventText ? eval(showEventText) : true;
+	var trackHeight = store.getTiddlerSlice(t,'trackHeight'+n);
+	if(trackHeight)
+		bp.trackHeight = eval(trackHeight);
+	var trackGap = store.getTiddlerSlice(t,'trackGap'+n);
+	if(trackGap)
+		bp.trackGap = eval(trackGap);
 	return bp;
 };
-/*
-from http://simile.mit.edu/repository/timeline/trunk/src/webapp/api/scripts/util/date-time.js
-Timeline.DateTime.MILLISECOND    = 0;
-Timeline.DateTime.SECOND         = 1;
-Timeline.DateTime.MINUTE         = 2;
-Timeline.DateTime.HOUR           = 3;
-Timeline.DateTime.DAY            = 4;
-Timeline.DateTime.WEEK           = 5;
-Timeline.DateTime.MONTH          = 6;
-Timeline.DateTime.YEAR           = 7;
-Timeline.DateTime.DECADE         = 8;
-Timeline.DateTime.CENTURY        = 9;
-Timeline.DateTime.MILLENNIUM     = 10;
-
-Timeline.DateTime.EPOCH          = -1;
-Timeline.DateTime.ERA            = -2;
-*/
-
 /*}}}*/
