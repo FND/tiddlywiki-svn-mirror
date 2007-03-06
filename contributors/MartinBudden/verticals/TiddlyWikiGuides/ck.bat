b24494de10ecf968e84b3b1462e557e19743cf77
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-03-06 08:29:13 +0000

    Initial submission.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1751 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/TiddlyWikiGuides/ck.bat
@@ -0,0 +1,1 @@
+cook TiddlyWikiGuides.html.recipe
---
+++ contributors/MartinBudden/verticals/TiddlyWikiGuides/settings.js
@@ -0,0 +1,30 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = "backup";
+config.options.txtMaxEditRows = 20;
+
+config.options.chkDisableWikiLinks = true;
+
+config.usePreForStorage = true;
+config.maxTiddlerImportCount = 10;
+
+config.defaultCustomFields = {
+	'server.type':'mediawiki',
+	'server.host':'tiddlywikiguides.org/',
+	wikiformat:'mediawiki'
+};
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+
+config.displayStartupTime = false;
+/*}}}*/
