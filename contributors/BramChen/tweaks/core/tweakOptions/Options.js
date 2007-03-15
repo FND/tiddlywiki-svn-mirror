3cb3d29882cd5c1e00564c5b60097606776487ea
BramChen <BramChen@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-03-15 04:36:31 +0000

    Followed the last trunk (Changeset #1813, #1824)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1837 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/BramChen/tweaks/core/tweakOptions/Options.js
+++ contributors/BramChen/tweaks/core/tweakOptions/Options.js
@@ -1,30 +1,33 @@
-// ---------------------------------------------------------------------------------
-// Options cookie stuff
-// ---------------------------------------------------------------------------------
+//--
+//-- Options cookie stuff
+//--
+
+config.optionHandlers = {
+	'txt': {
+		get: function(name) {return encodeURIComponent(config.options[name].toString());},
+		set: function(name,value) {config.options[name] = decodeURIComponent(value);}
+	},
+	'chk': {
+		get: function(name) {return config.options[name] ? "true" : "false";},
+		set: function(name,value) {config.options[name] = value == "true";}
+	}
+};
 
 function loadOptionsCookie()
 {
 	if(safeMode)
 		return;
 	var cookies = document.cookie.split(";");
-	for(var c=0; c<cookies.length; c++)
-		{
+	for(var c=0; c<cookies.length; c++) {
 		var p = cookies[c].indexOf("=");
-		if(p != -1)
-			{
+		if(p != -1) {
 			var name = cookies[c].substr(0,p).trim();
 			var value = cookies[c].substr(p+1).trim();
-			switch(name.substr(0,3))
-				{
-				case "txt":
-					config.options[name] = decodeURIComponent(value);
-					break;
-				case "chk":
-					config.options[name] = value == "true";
-					break;
-				}
-			}
+			var optType = name.substr(0,3);
+			if(config.optionHandlers[optType] && config.optionHandlers[optType].set)
+				config.optionHandlers[optType].set(name,value);
 		}
+	}
 }
 
 function saveOptionCookie(name)
@@ -32,15 +35,9 @@ function saveOptionCookie(name)
 	if(safeMode)
 		return;
 	var c = name + "=";
-	switch(name.substr(0,3))
-		{
-		case "txt":
-			c += encodeURIComponent(config.options[name].toString());
-			break;
-		case "chk":
-			c += config.options[name] ? "true" : "false";
-			break;
-		}
+	var optType = name.substr(0,3);
+	if(config.optionHandlers[optType] && config.optionHandlers[optType].get)
+		c += config.optionHandlers[optType].get(name);
 	c += "; expires=Fri, 1 Jan 2038 12:00:00 UTC; path=/";
 	document.cookie = c;
 }
