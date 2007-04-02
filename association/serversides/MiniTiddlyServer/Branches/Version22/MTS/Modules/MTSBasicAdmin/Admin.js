591684d5a9c2b6254b15a7abe65fbf085bb1bc9d
seanhess <seanhess@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-04-02 10:46:08 +0000

    Version 2.1 compatibility: BasicAdmin Admin.js
    git-svn-id: http://svn.tiddlywiki.org/Trunk@1985 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/MiniTiddlyServer/Branches/Version22/MTS/Modules/MTSBasicAdmin/Admin.js
+++ association/serversides/MiniTiddlyServer/Branches/Version22/MTS/Modules/MTSBasicAdmin/Admin.js
@@ -7,38 +7,42 @@ config.shadowTiddlers.MTSAdminPanel = "|<<mtsadduser>>|<<mtscreatewiki>>|\n|<<mt
 
 // Create a handle for the backstage //
 
-config.backstageTasks.push("MTSAdmin");
-config.tasks.MTSAdmin = {
-    text: "MTS Admin", 
-    tooltip: "MTS Controls and Admin Panel", 
-    content: ""
+if ( config.backstageTasks ) {
+
+    config.backstageTasks.push("MTSAdmin");
+    config.tasks.MTSAdmin = {
+        text: "MTS Admin", 
+        tooltip: "MTS Controls and Admin Panel", 
+        content: ""
+    }
+
+    //config.backstageTasks = ["save", "tidy","sync","importTask","copy","tweak","plugins"];
+
+
+    for (var i in config.backstageTasks) {
+        if (config.backstageTasks[i] == "save")
+            config.backstageTasks.splice(i,6);
+    }
+
+
+    var adminOldIn = adminLoggedIn;
+    adminLoggedIn = function () {
+        adminOldIn.apply(this,arguments);
+        config.tasks.MTSAdmin.content = config.shadowTiddlers.MTSAdminPanel
+    }
+
+    var adminOldOut = adminLoggedOut;
+    adminLoggedOut = function () {
+        adminOldOut.apply(this,arguments);
+        config.tasks.MTSAdmin.content = "Please log in as an admin before accessing this panel";
+    }
+
+    if (isAdmin)
+        adminLoggedIn();
+    else
+        adminLoggedOut();
+
 }
-
-//config.backstageTasks = ["save", "tidy","sync","importTask","copy","tweak","plugins"];
-
-
-for (var i in config.backstageTasks) {
-    if (config.backstageTasks[i] == "save")
-        config.backstageTasks.splice(i,6);
-}
-
-
-var adminOldIn = adminLoggedIn;
-adminLoggedIn = function () {
-    adminOldIn.apply(this,arguments);
-    config.tasks.MTSAdmin.content = config.shadowTiddlers.MTSAdminPanel
-}
-
-var adminOldOut = adminLoggedOut;
-adminLoggedOut = function () {
-    adminOldOut.apply(this,arguments);
-    config.tasks.MTSAdmin.content = "Please log in as an admin before accessing this panel";
-}
-
-if (isAdmin)
-    adminLoggedIn();
-else
-    adminLoggedOut();
 
 /*
 // Will only have one instance, or should anyway // 
