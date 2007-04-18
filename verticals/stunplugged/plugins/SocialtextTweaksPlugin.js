6e26258682bc3d121d9fc644e7ccea6c67609f0c
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-04-18 21:45:27 +0000

    Minor updates for 2.2
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2084 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/stunplugged/plugins/SocialtextTweaksPlugin.js
+++ verticals/stunplugged/plugins/SocialtextTweaksPlugin.js
@@ -3,24 +3,27 @@
 |''Description:''|Allows changes to be synchronised with a Socialtext server|
 |''Source:''|http://stunplugged.tiddlywiki.com/#SocialtextTweaksPlugin|
 |''Author:''|JeremyRuston (jeremy (at) osmosoft (dot) com)|
-|''Version:''|1.0.0|
+|''Version:''|1.0.1|
 |''Date:''|Dec 4, 2006|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
 |''License:''|[[BSD open source license]]|
-|''~CoreVersion:''|2.1.3|
+|''~CoreVersion:''|2.2|
 
 Make minor configuration tweaks specific to Socialtext Unplugged
-
 ***/
 
 //{{{
-
 // Ensure that the SocialtextTweaksPlugin is only installed once.
 if(!version.extensions.SocialtextTweaksPlugin) {
 version.extensions.SocialtextTweaksPlugin = {installed:true};
 // Check version number of core code
-if(version.major < 2 || (version.major == 2 && version.minor < 1))
-	{alertAndThrow("SocialtextTweaksPlugin requires TiddlyWiki 2.1 or later.");}
+if(version.major < 2 || (version.major == 2 && version.minor < 2))
+	{alertAndThrow("SocialtextTweaksPlugin requires TiddlyWiki 2.2 or later.");}
+
+merge(config.defaultCustomFields,{
+	'server.type':'socialtext',
+	wikiformat:'socialtext'
+});
 
 config.options.chkSinglePageMode = true;
 config.options.chkEnableAnimations = true;
