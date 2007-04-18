fed249b8f15d665402438db8c02cb6f9e2947a83
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-04-18 22:19:09 +0000

    Minor updates for 2.2
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2087 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/verticals/SocialtextOSS/settings.js
+++ contributors/MartinBudden/verticals/SocialtextOSS/settings.js
@@ -1,7 +1,7 @@
 /***
 |''Name:''|settings|
 |''Description:''|Set preferences|
-|''~CoreVersion:''|2.1.0|
+|''~CoreVersion:''|2.2.0|
 ***/
 
 /*{{{*/
@@ -9,23 +9,21 @@ config.views.editor.defaultText = '';
 config.options.chkAnimate = false;
 config.options.chkSaveBackups = false;
 config.options.chkAutoSave = false;
-config.options.txtBackupFolder = "backup";
+config.options.txtBackupFolder = 'backup';
 config.options.txtMaxEditRows = 20;
-
 config.options.chkDisableWikiLinks = true;
 
-config.usePreForStorage = true;
 config.maxTiddlerImportCount = 10;
 
-config.defaultCustomFields = {
+merge(config.defaultCustomFields,{
 	'server.type':'socialtext',
 	'server.host':'www.eu.socialtext.net',
 	'server.workspace':'stoss',
 	wikiformat:'socialtext'
-};
+});
 
 config.options.chkSinglePageMode = false;
 config.options.chkTopOfPageMode = false;
 
-config.displayStartupTime = true;
+//#config.displayStartupTime = true;
 /*}}}*/
