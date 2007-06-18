cb523cd2d4a5fb07d6e2988e4a20f09fd03a995f
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-06-18 12:43:20 +0000

    stop putting empty into repos. also added lessbackupsplugin.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2321 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/build.rb
+++ contributors/SimonBaird/mptw/trunk/build.rb
@@ -6,10 +6,8 @@ def get_rev
   `svnversion .`.split(':').last
 end
 
-# using 2.2 empty
-#
 make_tw {
-  source_file           'empty22.html'
+  source_file           'empties/empty.html'
   remove_tiddler        'LegacyStrikeThroughPlugin'
   add_tiddlers_from_dir 'core'
   package_as_from_dir   'misc/MptwLayoutPlugin.js', 'layout'
@@ -31,32 +29,4 @@ make_tw {
   add_tiddler_from_file('layout/MptwEditTemplate.html').rename("EditTemplate")
   to_file               'upload/empty_nolayout.html'
 }
-
-# using 2.1 empty
-#
-
-exit
-
-make_tw {
-  source_file           'empty.html'
-  add_tiddlers_from_dir 'core'
-  package_as_from_dir   'misc/MptwLayoutPlugin.js', 'layout'
-  add_tiddlers_from_dir 'noupgrade'
-  to_file               'upload/empty21.html'
-
-  # make a no layout version
-  remove_tiddler        'PageTemplate'
-  remove_tiddler        'StyleSheet'
-  remove_tiddler        'MptwLayoutPlugin'
-  remove_tiddler        'MptwUpgradeURL'
-  remove_tiddler        'MptwUpgradeTsURL'
-  add_tiddler_from_file('misc/MptwUpgrade.pub')
-  add_tiddler_from_file('misc/MptwUpgradeTs.pub')
-  remove_tiddler        'ViewTemplate'
-  add_tiddler_from_file('layout/MptwViewTemplate.html').rename("ViewTemplate")
-  remove_tiddler        'EditTemplate'
-  add_tiddler_from_file('layout/MptwEditTemplate.html').rename("EditTemplate")
-  to_file               'upload/empty21_nolayout.html'
-}
-
 
