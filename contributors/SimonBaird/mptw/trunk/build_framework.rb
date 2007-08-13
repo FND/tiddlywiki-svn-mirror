2139ce548178f64e5c59b3911e43385c12beade4
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-08-13 12:49:18 +0000

    build script and upload props tweak
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2409 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/build_framework.rb
+++ contributors/SimonBaird/mptw/trunk/build_framework.rb
@@ -7,5 +7,16 @@ make_tw {
   package_as_from_dir   'misc/MptwLayoutPlugin.js', 'layout'
   add_tiddlers_from_dir 'noupgrade'
   add_tiddlers_from_dir 'framework'
+
+
+  add_tiddler_from_scratch('tiddler' => 'Mow Lawn', 'tags' => ['Project','Yard','Personal','Active'])
+  add_tiddler_from_scratch('tiddler' => 'Remove Stump', 'tags' => ['Project','Yard','Personal','Active'])
+  add_tiddler_from_scratch('tiddler' => 'Buy Skis', 'tags' => ['Project','Sports','Personal','Someday/Maybe'])
+  add_tiddler_from_scratch('tiddler' => 'Look in phone book for ski shops', 'tags' => ['Action','Next','Personal','Buy Skis'])
+  add_tiddler_from_scratch('tiddler' => 'Call tree stumper', 'tags' => ['Action','Next','Personal','Remove Stump'])
+  add_tiddler_from_scratch('tiddler' => 'Buy Petrol', 'tags' => ['Action','Next','Personal','Mow Lawn'])
+  add_tiddler_from_scratch('tiddler' => 'Clear palm branches', 'tags' => ['Action','Next','Personal','Mow Lawn'])
+
+
   to_file               'upload/emptydev.html'
 }
