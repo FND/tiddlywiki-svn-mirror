80ba92949cd9c198d5e3515d5b93f64cba303955
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-09-27 14:16:20 +0000

    Initial submission
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2535 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/SimonMcManus/verticals/testFacebook/settings.js
@@ -0,0 +1,20 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = 'backup';
+config.options.txtMaxEditRows = 20;
+
+config.options.chkDisableWikiLinks = true;
+config.defaultCustomFields = {x:17};
+config.displayStartupTime = true;
+//#config.options.chkSinglePageMode = false;
+//#config.options.chkTopOfPageMode = false;
+/*}}}*/
