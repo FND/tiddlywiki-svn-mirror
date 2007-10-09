3b67aab33b8902354d67f0dd8a67a099728790d9
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-09 09:20:34 +0000

    Explicitly set to import workspace on startup.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2570 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/sharedrecords/plugins/settings.js
@@ -0,0 +1,14 @@
+/***
+|''Name:''|settings|
+|''Description:''|Set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkDisableWikiLinks = true;
+config.options.txtMaxEditRows = 20;
+
+config.options.chkImportWorkspaceOnStartup = true;
+/*}}}*/
