f1e59f5e19acb53d0a02b6cb7de6e872780e2f84
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-15 11:59:17 +0000

    Back button and attempts to streamline code
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2608 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/Verticals/iPhone/iPhoneHacksPlugin.js
+++ contributors/PhilHawksworth/Verticals/iPhone/iPhoneHacksPlugin.js
@@ -1,65 +1,63 @@
 //{{{
 
-// Check if we're on an iPhone
-config.browser.isiPhone = config.userAgent.indexOf("iphone") != -1;
+	// Check if we're on an iPhone
+	config.browser.isiPhone = config.userAgent.indexOf("iphone") != -1;
 
-// Hack to scroll the address bar off the screen after startup
-window._origRestart = window.restart;
-window.restart = function() {
-	window._origRestart();
-	window.setTimeout(window.scrollTo,200,0,1);
-	//showIndexPane();
-}
+	config.options.chkAnimate = false;
 
-// Display the list screen
-function showIndexPane() {
-	var e = document.getElementById("displayPane");
-	if(e)
-		e.style.display = "none";
-	var e = document.getElementById("indexPane");
-	if(e)
-		e.style.display = "block";
-}
+	// Hack to scroll the address bar off the screen after startup
+	window._origRestart = window.restart;
+	window.restart = function() {
+		window._origRestart();
+		window.setTimeout(window.scrollTo,200,0,1);
+	};
+	
+	//Provide sensible history functionality
+	Story.prototype.history = {
+		buffer : new Array(),
+		bufferSize : 20,
+		p : 0,
+		add : function(t){
+			this.buffer.push(t);
+			if(this.buffer.length > this.bufferSize){
+				this.buffer.slice(this.buffer.length);
+			}
+			p = this.buffer.length -1;
+		},
+		go : function(){
+			p--;
+			if (p < 0) p = 0;
+			story.displayTiddler(null,this.buffer[p],null,null,null,true);
+		}
+	};
 
-window._origDisplayTiddler = Story.prototype.displayTiddler;
-Story.prototype.displayTiddler = function(srcElement,title,template,animate,slowly)
-{
-	var e = document.getElementById("displayPane");
-	if(e)
-		e.style.display = "block";
-	e = document.getElementById("indexPane");
-	if(e)
-		e.style.display = "none";
-	story.closeAllTiddlers();
-	window._origDisplayTiddler.apply(this,arguments);
+	window._origDisplayTiddler = Story.prototype.displayTiddler;
+	Story.prototype.displayTiddler = function(srcElement,title,template,animate,slowly,skiphistory)	{
+		story.closeAllTiddlers(title);
+		if(!skiphistory) story.history.add(title);
+		window._origDisplayTiddler.apply(this,arguments);
+	};
 
-}
+	config.commands.home = { text: "home", tooltip: "go back to the home page"};
+	config.commands.back = { text: "back", tooltip: "go back to the previous page"};
+	config.commands.showindex = { text: "index", tooltip: "view the index of this TiddlyWiki"};
 
-config.commands.home = { text: "home", tooltip: "go back to the home page"};
-config.commands.back = { text: "back", tooltip: "go back to the previous page"};
-config.commands.showindex = { text: "index", tooltip: "view the index of this TiddlyWiki"};
+	config.commands.home.handler = function(event,src,title) {
+		story.displayTiddler(null,'Home');
+		return false;
+	};
 
-config.commands.home.handler = function(event,src,title)
-{
-	story.displayTiddler(null,'Home');
-	return false;
-};
+	config.commands.back.handler = function(event,src,title) {
+		story.history.go();
+		return false;
+	};
 
-config.commands.back.handler = function(event,src,title)
-{
-	story.displayTiddler(null,'Back');
-	return false;
-};
+	config.commands.showindex.handler = function(event,src,title) {
+		story.displayTiddler(null,'IndexPane');
+		return false;
+	};
 
-config.commands.showindex.handler = function(event,src,title)
-{
-	//showIndexPane();
-	story.displayTiddler(null,'IndexPane');
-	return false;
-};
-
-
-// Tell TiddlyWiki to display startup timings
-config.options.chkDisplayStartupTime = true;
+	// Tell TiddlyWiki to display startup timings
+	config.options.chkDisplayStartupTime = true;
 
 //}}}
