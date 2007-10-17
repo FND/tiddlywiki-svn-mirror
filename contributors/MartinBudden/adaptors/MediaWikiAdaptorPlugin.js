e95f2eac18620731a2704b0938b3e727b117b8ac
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-17 16:49:25 +0000

    Upped version number.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2630 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js
+++ contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
 |''Source:''|http://www.martinswiki.com/#MediaWikiAdaptorPlugin |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js |
-|''Version:''|0.5.5|
+|''Version:''|0.5.6|
 |''Date:''|Jul 27, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
