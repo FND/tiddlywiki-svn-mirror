b09f1ec527486963be9bee84a31c125669e5f50e
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-17 19:27:56 +0000

    Initial submission.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2631 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/experimental/BackupTiddlerPlugin.js
@@ -0,0 +1,38 @@
+/***
+|''Name:''|BackupTiddlerPlugin|
+|''Description:''|Backs up tiddlers individually if chkAutoSave is set|
+|''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/BackupTiddlerPlugin.js |
+|''Version:''|0.0.1|
+|''Date:''|Oct 17, 2007|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
+|''~CoreVersion:''|2.3.0|
+
+***/
+
+//{{{
+//# Ensure that the DisableWikiLinksPlugin is only installed once.
+if(!version.extensions.BackupTiddlerPlugin) {
+version.extensions.BackupTiddlerPlugin = {installed:true};
+
+if(version.major < 2 || version.major < 3)
+	{alertAndThrow('BackupTiddlerPlugin requires TiddlyWiki 2.3 or newer.');}
+
+var saveBackupOriginal = saveBackup;
+function saveBackup(localPath,original,tiddlers)
+{
+	if(tiddlers) {
+		for(var i=0;i<tiddlers.length;i++) {
+			var t = tiddlers[i];
+			var backupPath = getBackupPath(localPath,t.title,"tiddler");
+			//# displayMessage("t:"+t.title);
+			//# displayMessage("b:"+backupPath);
+			saveFile(backupPath,store.getSaver().externalizeTiddler(store,t));
+		}
+	}
+	saveBackupOriginal.apply(this,arguments);
+}
+
+} //# end of 'install only once'
+//}}}
