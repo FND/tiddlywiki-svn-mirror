38cfbf0a22214c86dd526777024825bb7c123c2d
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-18 11:28:18 +0000

    Changed backstage bar.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2644 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/martinswiki/settings.js
@@ -0,0 +1,21 @@
+/***
+|''Name:''|settings|
+|''Description:''|Set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+//#config.displayStartupTime = true;
+
+//#config.macros.toolbar.moreLabel = '»';
+
+config.backstageTasks = ["save","tweak","plugins"];
+
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkDisableWikiLinks = true;
+config.options.txtMaxEditRows = 20;
+
+//#config.options.chkSinglePageMode = false;
+//#config.options.chkTopOfPageMode = false;
+/*}}}*/
