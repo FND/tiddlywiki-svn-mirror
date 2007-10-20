e7dd78a64aeff9dddf517ed4fea2ca4265933347
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-20 12:44:51 +0000

    Further development
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2664 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/adaptors/ZiddlyWikiAdaptorPlugin.js
+++ contributors/MartinBudden/adaptors/ZiddlyWikiAdaptorPlugin.js
@@ -27,11 +27,6 @@ if(!config.options.txtZiddlyWikiPassword)
 if(!version.extensions.ZiddlyWikiAdaptorPlugin) {
 version.extensions.ZiddlyWikiAdaptorPlugin = {installed:true};
 
-function doHttpGET(url,callback,params,headers,data,contentType,username,password)
-{
-	return doHttp('GET',url,data,contentType,username,password,callback,params,headers);
-}
-
 config.messages.serverParsingError = "Error parsing result from server";
 
 function ZiddlyWikiAdaptor()
@@ -43,6 +38,31 @@ function ZiddlyWikiAdaptor()
 	this.password = config.options.txtZiddlyWikiPassword;
 	return this;
 }
+
+ZiddlyWikiAdaptor.serverType = 'ziddlywiki';
+
+ZiddlyWikiAdaptor.HttpGET = function(uri,callback,params,headers,data,contentType,username,password)
+{
+	return doHttp('GET',uri,data,contentType,username,password,callback,params,headers);
+};
+
+ZiddlyWikiAdaptor.HttpPUT = function(uri,callback,params,headers,data,contentType,username,password)
+{
+	return doHttp('PUT',uri,data,contentType,username,password,callback,params,headers);
+};
+
+ZiddlyWikiAdaptor.prototype.setContext = function(context,userParams,callback)
+{
+	if(!context) context = {};
+	context.userParams = userParams;
+	if(callback) context.callback = callback;
+	context.adaptor = this;
+	if(!context.host)
+		context.host = this.host;
+	if(!context.workspace)
+		context.workspace = this.workspace;
+	return context;
+};
 
 ZiddlyWikiAdaptor.fullHostName = function(host)
 {
@@ -60,43 +80,70 @@ ZiddlyWikiAdaptor.minHostName = function(host)
 	return host ? host.replace(/^http:\/\//,'').replace(/\/$/,'') : '';
 };
 
-ZiddlyWikiAdaptor.prototype.openHost = function(host,context)
+ZiddlyWikiAdaptor.normalizedTitle = function(title)
 {
-displayMessage("openHost:"+host);
+	return title;
+};
+
+ZiddlyWikiAdaptor.prototype.openHost = function(host,context,userParams,callback)
+{
+//#displayMessage("openHost:"+host);
 	this.host = ZiddlyWikiAdaptor.fullHostName(host);
+	context = this.setContext(context,userParams,callback);
 //#displayMessage("host:"+this.host);
-	if(context && context.callback)
-		window.setTimeout(context.callback,0,true,this,context);
+	if(context.callback) {
+		context.status = true;
+		window.setTimeout(context.callback,10,context,userParams);
+	}
 	return true;
 };
 
-ZiddlyWikiAdaptor.prototype.openWorkspace = function(workspace,context)
+ZiddlyWikiAdaptor.prototype.openWorkspace = function(workspace,context,userParams,callback)
 {
-displayMessage("openWorkspace:"+workspace);
+//#displayMessage("openWorkspace:"+workspace);
 	this.workspace = workspace;
-	if(context && context.callback)
-		window.setTimeout(context.callback,0,true,this,context);
+	context = this.setContext(context,userParams,callback);
+	if(context.callback) {
+		context.status = true;
+		window.setTimeout(context.callback,10,context,userParams);
+	}
 	return true;
 };
 
-ZiddlyWikiAdaptor.prototype.getTiddlerList = function(context)
+ZiddlyWikiAdaptor.prototype.getWorkspaceList = function(context,userParams,callback)
 {
-//#displayMessage('getTiddlerList');
-//# http://www.ziddlywiki.com/ZiddlyWiki?action=get&id=News
-	var urlTemplate = '%0data/workspaces/%1/pages';
-	var host = this && this.host ? this.host : ZiddlyWikiAdaptor.fullHostName(context.tiddler.fields['server.host']);
-	var url = urlTemplate.format([host]);
-//#displayMessage('url:'+url);
-	context.adaptor = this;
-	var req = doHttpGET(url,ZiddlyWikiAdaptor.getTiddlerListCallback,context);
+//#displayMessage("getWorkspaceList");
+	context = this.setContext(context,userParams,callback);
+	var list = [];
+	list.push({title:"Main",name:"Main"});
+	context.workspaces = list;
+	if(context.callback) {
+		context.status = true;
+		window.setTimeout(context.callback,10,context,userParams);
+	}
+	return true;
+};
+
+ZiddlyWikiAdaptor.prototype.getTiddlerList = function(context,userParams,callback,filter)
+{
+displayMessage('getTiddlerList');
+	context = this.setContext(context,userParams,callback);
+//# http://zw.mcelrath.org/ziddlywiki.com/?action=get_all
+	var uriTemplate = '%0%1/?action=get_all';
+	var host = ZiddlyWikiAdaptor.fullHostName(context.host);
+	var uri = uriTemplate.format([host,context.workspace]);
+displayMessage('uri:'+uri);
+	var req = ZiddlyWikiAdaptor.HttpGET(uri,ZiddlyWikiAdaptor.getTiddlerListCallback,context);
 //#displayMessage('req:'+req);
 	return typeof req == 'string' ? req : true;
 };
 
-ZiddlyWikiAdaptor.getTiddlerListCallback = function(status,context,responseText,url,xhr)
+ZiddlyWikiAdaptor.getTiddlerListCallback = function(status,context,responseText,uri,xhr)
 {
-//#displayMessage('getTiddlerListCallback status:'+status);
-//#displayMessage('rt:'+responseText.substr(0,50));
+displayMessage('getTiddlerListCallback status:'+status);
+if(!status)
+	displayMessage('http status:'+xhr.status);
+displayMessage('rt:'+responseText ? responseText.substr(0,100) : 'none');
 //#displayMessage('xhr:'+xhr);
 	context.status = false;
 	if(status) {
@@ -140,6 +187,7 @@ ZiddlyWikiAdaptor.prototype.generateTiddlerInfo = function(tiddler)
 	return info;
 };
 
+
 //# News
 //# [[Version 2.0.11.4]] has been released.\n\n[[Version 2.0.11.3]] has been released.\n\n[[Version 2.0.11.2]] has been released.\n\nWith this version the maintainership has changed.  Tim Morgan has retired and development has merged with the main TiddlyWiki systems at http://trac.tiddlywiki.org.  We are thankful to retain hosting by [[Zettai|http://zettai.net]].\n
 //# BobMcElrath
@@ -149,46 +197,60 @@ ZiddlyWikiAdaptor.prototype.generateTiddlerInfo = function(tiddler)
 //#
 //# 871.64174.18161.45875
 
-ZiddlyWikiAdaptor.prototype.getTiddler = function(context)
+//#1. 2007 Sep 21 16:20 r:880.32920.53259.36590
+//#2. 2006 Oct 28 04:29 r:878.7151.38480.2986
+// http://zw.mcelrath.org/ziddlywiki.com/?action=get&title=GetYourOwn&revision=880.32920.53259.36590&1192874996515
+
+ZiddlyWikiAdaptor.prototype.getTiddler = function(title,context,userParams,callback)
 {
-	title = encodeURIComponent(context.tiddler.title);
-displayMessage('ZiddlyWikiAdaptor.getTiddler:'+title);
-//# http://www.ziddlywiki.com/ZiddlyWiki?action=get&id=News
+//#displayMessage('getTiddler:'+title);
+	context = this.setContext(context,userParams,callback);
+	if(title)
+		context.title = title;
+//# http://zw.mcelrath.org/ziddlywiki.com/?action=get&title=News
+//# http://zw.mcelrath.org/ziddlywiki.com/?action=get_revisions&title=News
 
-	var urlTemplate = '%0ZiddlyWiki?action=get&id=%1';
-	var host = this && this.host ? this.host : ZiddlyWikiAdaptor.fullHostName(context.tiddler.fields['server.host']);
-	var url = urlTemplate.format([host,title]);
-displayMessage('getZiddlyWwiki url: '+url);
-//# http://www.ziddlywiki.org/ZiddlyWiki?action=get&id=ZiddlyWiki
+	var uriTemplate = '%0%1/?action=get&title=%2';
+	if(context.revision)
+		uriTemplate += '&revision=%3';
+	var host = ZiddlyWikiAdaptor.fullHostName(context.host);
+	var uri = uriTemplate.format([host,context.workspace,title,context.revision]);
+//#displayMessage('uri: '+uri);
 
-	context.tiddler.fields['server.type'] = 'ziddlywiki';
-	context.adaptor = this;
-	var req = doHttpGET(url,ZiddlyWikiAdaptor.getTiddlerCallback,context);
-displayMessage("req:"+req);
+	context.tiddler = new Tiddler(context.title);
+	context.tiddler.fields['server.type'] = ZiddlyWikiAdaptor.serverType;
+	context.tiddler.fields['server.host'] = ZiddlyWikiAdaptor.minHostName(host);
+	context.tiddler.fields['server.workspace'] = context.workspace;
+	var req = ZiddlyWikiAdaptor.HttpGET(uri,ZiddlyWikiAdaptor.getTiddlerCallback,context);
+//#displayMessage("req:"+req);
 	return typeof req == 'string' ? req : true;
 };
 
-ZiddlyWikiAdaptor.getTiddlerCallback = function(status,context,responseText,url,xhr)
+ZiddlyWikiAdaptor.getTiddlerCallback = function(status,context,responseText,uri,xhr)
 {
-displayMessage('Ziddly.getTiddlerCallback status:'+status);
-//#displayMessage('rt:'+responseText.substr(0,50));
+//#displayMessage('getTiddlerCallback status:'+status);
+//#displayMessage('rt:'+responseText.substr(0,100));
 //#displayMessage('xhr:'+xhr);
 	context.status = false;
-	if(status && responseText.substr(0,1) != '-') {
-		var x = responseText.split('\n');
-		context.tiddler.text = x[1].unescapeLineBreaks();
-		try {
-			context.tiddler.modifier = x[2];
-			context.tiddler.tags = x[5];
-			if(x[3])
-				context.tiddler.created = Date.convertFromYYYYMMDDHHMM(x[3]);
-			if(x[4])
-				context.tiddler.modified = Date.convertFromYYYYMMDDHHMM(x[4]);
-		} catch(ex) {
-			context.statusText = exceptionText(ex,config.messages.serverParsingError);
-			if(context.callback)
-				context.callback(context);
-			return;
+	if(status) {
+		var tiddlerRegExp = /<div([^>]*)>(?:\s*)(<pre>)?([^<]*?)</mg;
+		tiddlerRegExp.lastIndex = 0;
+		match = tiddlerRegExp.exec(responseText);
+		if(match) {
+			//#displayMessage("m1:"+match[1]);
+			//#displayMessage("m2:"+match[2]);
+			//#displayMessage("m3:"+match[3]);
+			var ft = match[1].replace(/\=\"/mg,':"');
+			var fields = ft.decodeHashMap();
+			var text = match[3] ? match[3] : '';
+			if(match[2]) {
+				text = text.replace(/\r/mg,'').htmlDecode();
+			} else {
+				text = text.unescapeLineBreaks().htmlDecode();
+			}
+			context.tiddler.text = text;
+			//#displayMessage('tt:'+text.substr(0,100));
+			context.status = true;
 		}
 		context.status = true;
 	} else {
@@ -202,30 +264,39 @@ displayMessage('Ziddly.getTiddlerCallback status:'+status);
 	//#}
 };
 
-ZiddlyWikiAdaptor.prototype.getTiddlerRevisionList = function(context)
+ZiddlyWikiAdaptor.prototype.getTiddlerRevision = function(title,revision,context,userParams,callback)
+{
+displayMessage('getTiddler:'+title+' revision:'+revision);
+	context = this.setContext(context,userParams,callback);
+	context.revision = revision;
+	return this.getTiddler(title,context,userParams,callback);
+};
+
+ZiddlyWikiAdaptor.prototype.getTiddlerRevisionList = function(title,limit,context,userParams,callback)
 // get a list of the revisions for a tiddler
 {
-	var title = encodeURIComponent(context.tiddler.title);
-//#displayMessage('ZiddlyWikiAdaptor.getTiddlerRevisionList:'+title);
-// http://www.ziddlywiki.com/ZiddlyWiki?action=get_revisions&id=News
+	context = this.setContext(context,userParams,callback);
+	context.title = title;
+//#displayMessage('getTiddlerRevisionList:'+title);
+//# http://zw.mcelrath.org/ziddlywiki.com/?action=get_revisions&title=ZiddlyWiki
+//# http://zw.mcelrath.org/ziddlywiki.com/?action=get_revisions&title=GetYourOwn
 
-	var urlTemplate = '%0ZiddlyWiki?action=get_revisions&id=%1';
-	var host = this && this.host ? this.host : ZiddlyWikiAdaptor.fullHostName(context.tiddler.fields['server.host']);
-	var url = urlTemplate.format([host,title]);
-//#displayMessage('getZiddlyWwiki url: '+url);
-// http://www.ziddlywiki.org/ZiddlyWiki?action=get_revisions&id=ZiddlyWiki
+	var uriTemplate = '%0%1/?action=get_revisions&title=%2';
+	var host = ZiddlyWikiAdaptor.fullHostName(context.host);
+	var uri = uriTemplate.format([host,context.workspace,title]);
+//#displayMessage('getZiddlyWwiki uri: '+uri);
 
-	context.tiddler.fields['server.type'] = 'ziddlywiki';
 	context.adaptor = this;
-	var req = doHttpGET(url,ZiddlyWikiAdaptor.getTiddlerRevisionListCallback,context);
+	var req = ZiddlyWikiAdaptor.HttpGET(uri,ZiddlyWikiAdaptor.getTiddlerRevisionListCallback,context);
 //#displayMessage("req:"+req);
 	return typeof req == 'string' ? req : true;
 };
 
-ZiddlyWikiAdaptor.getTiddlerRevisionListCallback = function(status,context,responseText,url,xhr)
+//# 200609051750 878.7151.38480.2986 mcelrath
+ZiddlyWikiAdaptor.getTiddlerRevisionListCallback = function(status,context,responseText,uri,xhr)
 {
-//#displayMessage('Ziddly.getTiddlerRevisionListCallback status:'+status);
-//#displayMessage('rt:'+responseText.substr(0,50));
+//#displayMessage('getTiddlerRevisionListCallback status:'+status);
+//#displayMessage('rt:'+responseText.substr(0,100));
 //#displayMessage('xhr:'+xhr);
 //#displayMessage('callback:'+context.callback);
 	var revisions = [];
@@ -234,79 +305,52 @@ ZiddlyWikiAdaptor.getTiddlerRevisionListCallback = function(status,context,respo
 		for(var i=0; i<revs.length; i++) {
 			var parts = revs[i].split(' ');
 			if(parts.length>1) {
-				revisions[i] = {};
-				revisions[i].modified = Date.convertFromYYYYMMDDHHMM(parts[0]);
-				revisions[i].key = parts[1];
+				var tiddler = new Tiddler(context.title);
+				tiddler.modified = Date.convertFromYYYYMMDDHHMM(parts[0]);
+				tiddler.fields['server.page.revision'] = parts[1];
+				tiddler.modifier = parts[2];
+				tiddler.fields['server.page.name'] = context.title;
+				//tiddler.fields['server.page.id'] = ZiddlyWikiAdaptor.normalizedTitle(title);
+				revisions.push(tiddler);
 			}
 		}
 	}
+	context.status = true;
 	context.revisions = revisions;
 	if(context.callback)
-		context.callback(context);
+		context.callback(context,context.userParams);
 };
+
+/*
+ajax.post(zw.get_url(), callback, 'action=save&id=' + encodeURIComponent(title) + '&title=' 
+	+ encodeURIComponent(newTitle) + '&body=' + encodeURIComponent(newBody) + '&tags=' 
+	+ encodeURIComponent(tags) + '&modified=' 
+	+ encodeURIComponent((modified||store.fetchTiddler(title).modified).convertToYYYYMMDDHHMM()) 
+	+ '&' + zw.no_cache());
+*/
 
 //# placeholder, not working yet
-ZiddlyWikiAdaptor.getTiddlerRevision = function(context)
+ZiddlyWikiAdaptor.prototype.putTiddler = function(tiddler,context,userParams,callback)
 {
-//#displayMessage('getTiddlerRevision:'+context.tiddler.title);
-	title = encodeURIComponent(context.tiddler.title);
-	if(context.tiddler.fields['server.page.revision'] == revision)
-		return true;
-	zw.status('loading...');
-	revision = revision ? '&revision=%2' + revision : '';
-	updateTimeline = updateTimeline ? '&updatetimeline=1' : '';
-	//ajax.get('?action=get&id=' + encodeURIComponent(title) + revision + updateTimeline + '&' + zw.no_cache(),displayTiddlerRevisionCallback)
-	//return typeof req == 'string' ? req : true;
-	var urlTemplate = '%0ZiddlyWiki?action=get&id=%1' + context.revision + context.updatetimeline;
-	var host = this && this.host ? this.host : ZiddlyWikiAdaptor.fullHostName(context.tiddler.fields['server.host']);
-	var url = urlTemplate.format([host,title,revision]);
-	context.tiddler.fields['server.type'] = 'ziddlywiki';
-	context.adaptor = this;
-	var req = doHttpGET(url,ZiddlyWikiAdaptor.getTiddlerRevisionCallback,context);
-//#displayMessage("req:"+req);
-	return typeof req == 'string' ? req : true;
+displayMessage('putTiddler:'+tiddler.title);
+	context = this.setContext(context,userParams,callback);
+	context.tiddler = tiddler;
+	context.title = tiddler.title;
+	//var title = encodeURIComponent(context.tiddler.title);
+	var uriTemplate = '%0%1/action=save&id=%2&title=%3&body=%4&tags=%5&modified=%6';
+	var id = tiddler.title;
+	var tags = '';
+	var modified = tiddler.modified.convertToYYYYMMDDHHMM();
+	var uri = uriTemplate.format([context.host,context.workspace,id,tiddler.title,tiddler.text,tags,modified]);
+displayMessage('uri: '+uri);
 
-};
-
-//# placeholder, not working yet
-ZiddlyWikiAdaptor.getTiddlerRevisionCallback = function(status,context,responseText,url,xhr)
-{
-//#displayMessage('getTiddlerRevisionCallback status:'+status);
-//#displayMessage('rt:'+responseText.substr(0,50));
-//#displayMessage('xhr:'+xhr);
-	var encoded = responseText;
-	if(encoded.indexOf('\n') > -1) {
-		var parts = encoded.split('\n');
-		var title = parts[0];
-		context.tiddler.set(title,Tiddler.unescapeLineBreaks(parts[1].htmlDecode()),parts[2],
-				Date.convertFromYYYYMMDDHHMM(parts[3]),parts[5],
-				Date.convertFromYYYYMMDDHHMM(parts[4]));
-		tiddler.revisionKey = parts[7];
-		store.addTiddler(tiddler);
-		story.refreshTiddler(title,DEFAULT_VIEW_TEMPLATE,true);
-		if(parts[6] == 'update timeline')
-			store.notify('TabTimeline',true);
-	} else if(encoded != '-') {
-		alert(encoded); // error message
-	}
-	zw.status(false);
-};
-
-ZiddlyWikiAdaptor.prototype.putTiddler = function(context)
-{
-	var title = encodeURIComponent(context.tiddler.title);
-	var urlTemplate = '%0RPC2/';
-	var host = this && this.host ? this.host : ZiddlyWikiAdaptor.fullHostName(context.tiddler.fields['server.host']);
-	var url = urlTemplate.format([host,encodeURIComponent(title)]);
-//#displayMessage('putZiddlyWwiki url: '+url);
-
-	context.tiddler.fields['server.type'] = 'ziddlywiki';
-	var req =doHttp('POST',url,payload,null,this.username,this.password,ZiddlyWikiAdaptor.putTiddlerCallback,context);
+	context.tiddler.fields['server.type'] = ZiddlyWikiAdaptor.serverType;
+	var req = ZiddlyWikiAdaptor.HttpPUT(uri,ZiddlyWikiAdaptor.putTiddlerCallback,context);
 //#displayMessage("req:"+req);
 	return typeof req == 'string' ? req : true;
 };
 
-ZiddlyWikiAdaptor.putTiddlerCallback = function(status,context,responseText,url,xhr)
+ZiddlyWikiAdaptor.putTiddlerCallback = function(status,context,responseText,uri,xhr)
 {
 //#displayMessage('putTiddlerCallback status:'+status);
 //#displayMessage('rt:'+responseText.substr(0,50));
@@ -321,9 +365,8 @@ ZiddlyWikiAdaptor.putTiddlerCallback = function(status,context,responseText,url,
 		context.callback(context);
 };
 
-ZiddlyWikiAdaptor.prototype.getWorkspaceList = function(context) {return false;};
 ZiddlyWikiAdaptor.prototype.close = function() {return true;};
 
-config.adaptors['ziddlywiki'] = ZiddlyWikiAdaptor;
+config.adaptors[ZiddlyWikiAdaptor.serverType] = ZiddlyWikiAdaptor;
 } // end of 'install only once'
 //}}}
