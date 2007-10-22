8c061ce5e1fb28e17148613ff53c4549e76cc297
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-22 19:05:20 +0000

    Initial submission
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2693 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/testTiddlyDAV/settings.js
@@ -0,0 +1,26 @@
+/***
+|''Name:''|settings|
+|''Description:''|Set preferences|
+|''~CoreVersion:''|2.1.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = "backup";
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+
+config.maxTiddlerImportCount = 10;
+
+merge(config.defaultCustomFields,{
+	'server.type':'dav',
+});
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+
+//#config.displayStartupTime = true;
+/*}}}*/
