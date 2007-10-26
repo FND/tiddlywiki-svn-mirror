7adf4283991d0a930f913d6861302cb3f5ffab85
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-26 12:07:02 +0000

    typo dammit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2721 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/misc/MptwLayoutPlugin.js
+++ contributors/SimonBaird/mptw/trunk/misc/MptwLayoutPlugin.js
@@ -21,7 +21,7 @@ merge(config.annotations,{
 	MptwEditTemplate: "Contains the default MPTW EditTemplate. If you want to customise this rename it to EditTemplate",
 	MptwViewTemplate: "Contains the default MPTW ViewTemplate. If you want to customise this rename it to ViewTemplate",
 	MptwPageTemplate: "Contains the default MPTW PageTemplate. If you want to customise this rename it to PageTemplate",
-	MptwStyleSheet:   "Contains the default MPTW ~StyleSheet. Designed to be included in StyleSheet tiddler using the double square bracked notation like this: {{{[[MptwStyleSheet]]}}}"
+	MptwStyleSheet:   "Contains the default MPTW ~StyleSheet. Designed to be included in StyleSheet tiddler using the double square bracketted notation like this: {{{[[MptwStyleSheet]]}}}"
 });
 
 //}}}
