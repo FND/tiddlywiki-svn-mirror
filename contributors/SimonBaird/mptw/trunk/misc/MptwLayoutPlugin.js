b16ab5053dec77249a7e43eb3c12a4c37707beb9
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-26 11:51:35 +0000

    Added the missing annotations to the MptwEditTemplate.html. Minor css tweakage.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2720 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/misc/MptwLayoutPlugin.js
+++ contributors/SimonBaird/mptw/trunk/misc/MptwLayoutPlugin.js
@@ -17,5 +17,12 @@ config.mptwJournalFormat = 'Journal DD/MM/YY';
 
 config.shadowTiddlers.GettingStarted += "\n\nSee also MonkeyPirateTiddlyWiki.";
 
+merge(config.annotations,{
+	MptwEditTemplate: "Contains the default MPTW EditTemplate. If you want to customise this rename it to EditTemplate",
+	MptwViewTemplate: "Contains the default MPTW ViewTemplate. If you want to customise this rename it to ViewTemplate",
+	MptwPageTemplate: "Contains the default MPTW PageTemplate. If you want to customise this rename it to PageTemplate",
+	MptwStyleSheet:   "Contains the default MPTW ~StyleSheet. Designed to be included in StyleSheet tiddler using the double square bracked notation like this: {{{[[MptwStyleSheet]]}}}"
+});
+
 //}}}
 
