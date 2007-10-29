bb1958b42e5f2c96fdc40ee0026872a88da12873
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-10-29 14:13:16 +0000

    Corrected code that ensured plugin was only installed once
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2728 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/SimileTimelinePlugin.js
+++ contributors/MartinBudden/plugins/SimileTimelinePlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden ( mjbudden [at] gmail [dot] com)|
 |''Source:''|http://www.martinswiki.com/#SimileTimelineBundlePlugin |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/SimileTimelinePlugin.js |
-|''Version:''|0.1.0|
+|''Version:''|0.1.1|
 |''Date:''|Mar 4, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
@@ -24,7 +24,7 @@
 /*{{{*/
 // Ensure that the SimileTimelinePlugin is only installed once.
 if(!version.extensions.SimileTimelinePlugin) {
-version.extensions.SimileTimelineBundlePlugin = {installed:true};
+version.extensions.SimileTimelinePlugin = {installed:true};
 
 if(version.major < 2 || (version.major == 2 && version.minor < 2))
 	{alertAndThrow('SimileTimelineBundlePlugin requires TiddlyWiki 2.2 or newer.');}
