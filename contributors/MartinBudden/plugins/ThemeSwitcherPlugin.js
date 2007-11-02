f9d675828ff0e55fc202aaf5ec657e30e87e2f34
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-02 13:15:48 +0000

    Removed core code (to be submitted as patch to core).
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2760 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
+++ contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden|
 |''Source:''|http://www.martinswiki.com/#ThemeSwitcherPlugin |
 |''~CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js |
-|''Version:''|0.0.5|
+|''Version:''|0.0.4|
 |''Status:''|Not for release - still under development|
 |''Date:''|Oct 31, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
@@ -51,61 +51,6 @@ config.macros.selectTheme.onClick = function(ev)
 config.macros.selectTheme.onClickTheme = function(ev)
 {
 	story.switchTheme(this.getAttribute("theme"));
-};
-
-Story.prototype.switchTheme = function(theme)
-{
-	isTiddler = function(title) {
-		var s = title ? title.indexOf(config.textPrimitives.sectionSeparator) : -1;
-		if(s!=-1)
-			title = title.substr(0,s);
-		return store.tiddlerExists(title) || store.isShadowTiddler(title);
-	};
-
-	getSlice = function(theme,slice) {
-		var r = store.getTiddlerSlice(theme,slice);
-		if(r && r.indexOf(config.textPrimitives.sectionSeparator)==0)
-			r = theme + r;
-		return isTiddler(r) ? r : slice;
-	};
-
-	replaceNotification = function(index,name,newName) {
-		if(name==newName)
-			return name;
-		if(store.namedNotifications[index].name == name) {
-			store.namedNotifications[index].name = newName;
-			return newName;
-		}
-		return name;
-	};
-
-	for(var i=0; i<config.notifyTiddlers.length; i++) {
-		var name = config.notifyTiddlers[i].name;
-		switch(name) {
-		case "PageTemplate":
-			config.refreshers.pageTemplate = replaceNotification(i,config.refreshers.pageTemplate,getSlice(theme,name));
-			break;
-		case "StyleSheet":
-			var styleElement = document.getElementById(config.refreshers.styleSheet);
-			if(styleElement)
-				styleElement.parentNode.removeChild(styleElement);
-			config.refreshers.styleSheet = replaceNotification(i,config.refreshers.styleSheet,getSlice(theme,name));
-			break;
-		case "ColorPalette":
-			config.refreshers.colorPalette = replaceNotification(i,config.refreshers.colorPalette,getSlice(theme,name));
-			break;
-		default:
-			break;
-		}
-	}
-	config.tiddlerTemplates[DEFAULT_VIEW_TEMPLATE] = getSlice(theme,"ViewTemplate");
-	config.tiddlerTemplates[DEFAULT_EDIT_TEMPLATE] = getSlice(theme,"EditTemplate");
-	if(!startingUp) {
-		refreshAll();
-		story.refreshAllTiddlers(true);
-	}
-	config.options.txtTheme = theme;
-	saveOptionCookie("txtTheme");
 };
 
 } //# end of 'install only once'
