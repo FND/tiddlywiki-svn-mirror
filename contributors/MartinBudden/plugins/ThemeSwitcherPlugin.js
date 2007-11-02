3443be38f632826166f1ad4061b41effd48fe38a
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-02 15:04:39 +0000

    Better internationalization
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2761 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
+++ contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden|
 |''Source:''|http://www.martinswiki.com/#ThemeSwitcherPlugin |
 |''~CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js |
-|''Version:''|0.0.4|
+|''Version:''|0.0.5|
 |''Status:''|Not for release - still under development|
 |''Date:''|Oct 31, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
@@ -18,12 +18,14 @@
 if(!version.extensions.ThemeSwitcherPlugin) {
 version.extensions.ThemeSwitcherPlugin = {installed:true};
 
-config.macros.selectTheme = {};
+config.macros.selectTheme = {
+	label: "select theme",
+	prompt: "Select the current theme"
+};
+
 config.macros.selectTheme.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
-	var label = "select theme";
-	var prompt = "select the current theme";
-	var btn = createTiddlyButton(place,label,prompt,this.onClick);
+	var btn = createTiddlyButton(place,this.label,this.prompt,this.onClick);
 	btn.setAttribute("place",place);
 };
 
@@ -31,27 +33,27 @@ config.macros.selectTheme.onClick = function(ev)
 {
 	var e = ev ? ev : window.event;
 	var popup = Popup.create(this);
-	var tiddlers = store.getTaggedTiddlers("systemTheme");
+	var tiddlers = store.getTaggedTiddlers('systemTheme');
 	for(var i=0; i<tiddlers.length; i++) {
 		var title = tiddlers[i].title;
-		var name = store.getTiddlerSlice(title,"Name");
+		var name = store.getTiddlerSlice(title,'Name');
 		var label = name ? name : title;
-		var desc = store.getTiddlerSlice(title,"Description");
+		var desc = store.getTiddlerSlice(title,'Description');
 		var prompt = desc ? desc : label;
-		var li = createTiddlyElement(popup,"li");
+		var li = createTiddlyElement(popup,'li');
 		var btn = createTiddlyButton(li,label,prompt,config.macros.selectTheme.onClickTheme);
-		btn.setAttribute("theme",title);
+		btn.setAttribute('theme',title);
 	}
 	Popup.show();
 	e.cancelBubble = true;
 	if(e.stopPropagation) e.stopPropagation();
+	return false;
 };
-
 
 config.macros.selectTheme.onClickTheme = function(ev)
 {
-	story.switchTheme(this.getAttribute("theme"));
+	story.switchTheme(this.getAttribute('theme'));
+	return false;
 };
-
 } //# end of 'install only once'
 //}}}
