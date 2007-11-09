bfd783ea896aa3afd6b009b84d466bba4b65b28e
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-09 13:59:22 +0000

    refactor of TiddleLeWeb code to not use shadow tiddlers or agenda object
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2783 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/tiddleleweb/plugins/Agenda.js
@@ -0,0 +1,215 @@
+/*******************
+ * Agenda
+ *******************/
+Agenda = {};
+// flag for whether agenda is up to date
+Agenda.isCurrent = false;
+// params array for the tab macro, filled by create_tab_tiddlers
+Agenda.tabsParams = [];
+
+// function to gather agenda items
+Agenda.getItems = function() {
+	var filter = "[tag[session]]";
+	return store.filterTiddlers(filter);
+};
+
+// function to return array of names of tracks the agenda covers
+Agenda.getTracks = function() {
+	var tracks = [];
+	var items = Agenda.getItems();
+	for (var i=0;i<items.length;i++) {
+		tracks.pushUnique(store.getTiddlerSlice(items[i].title,"track"));
+	}
+	return tracks;
+};
+
+// function to return agenda items for a single track
+Agenda.getTrack = function(track) {
+	var trackItems = [];
+	var items = Agenda.getItems();
+	for (var i=0;i<items.length;i++) {
+		if (store.getTiddlerSlice(items[i].title,"track") == track) {
+			trackItems.push(items[i]);
+		}
+	}
+	return trackItems;
+};
+
+// create the right tiddlers to populate the tabs in the agendaMenu
+// returns params array for tab macro
+Agenda.create_tab_tiddlers = function() {
+	var params = [];
+	var titlePrefix = "TabAgenda";
+	var textPrefix = "<<agendaMenuByTrack ";
+	var textSuffix = ">>";
+	var tracks = Agenda.getTracks();
+	// start by adding the selected tab param
+	params.push("txtAgendaTab");
+	for (var i;i<tracks.length;i++) {
+		var title = titlePrefix+n;
+		var text = textPrefix+n+textSuffix;
+		if(!store.tiddlerExists(title)) {
+			store.saveTiddler(title,title,text);
+			params.push(tracks[i]);
+			params.push(tracks[i]);
+			params.push(title);
+		}
+	}
+	return params;
+};
+
+Agenda.init = function() {
+	// check to see if Agenda is current before updating
+	if (!Agenda.isCurrent) {
+		// set up the tiddlers for the tabs macro in agendaMenu
+		Agenda.tabsParams = Agenda.create_tab_tiddlers();
+		// set default selected tab in agendaMenu
+		config.options.txtAgendaTab = "";
+		Agenda.isCurrent = !Agenda.isCurrent;
+	}
+};
+Agenda.init();
+
+/*******************
+ * agendaMenu
+ * agendaMenuByTrack
+ *******************/
+config.macros.agendaMenu = {};
+
+config.macros.agendaMenu.handler = function(place,macroName) {
+	// set up the variables and tiddlers for running the tabs macro
+	config.macros.tabs.handler(place,macroName,Agenda.tabsParams);
+};
+
+config.macros.agendaMenuByTrack = {};
+
+config.macros.agendaMenuByTrack.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	// create the agenda menu for a single track
+	// params[0] is the day
+	var trackItems = Agenda.getTrack(params[0]);
+	if (!trackItems) {
+		displayMessage("error with agendaMenuByTrack for day: " + params[0]);
+		return;
+	}
+	for (var i=0;i<trackItems.length;i++) {
+		this.buildAgendaItem(place,trackItems[i]);
+	}
+};
+
+config.macros.agendaMenuByTrack.buildAgendaItem = function(place,item) {
+	// collect information from the session tiddler
+	var title = item.title;
+	var speaker = store.getTiddlerSlice(title,"speaker");
+	var start = store.getTiddlerSlice(title,"start");
+	var end = store.getTiddlerSlice(title,"end");
+	var blurb = store.getTiddlerSlice(title,"blurb");
+	// build the entry in the agendaMenu
+	var wrapper = createTiddlyElement(place,"div",null,"agendaItem");
+	var header = createTiddlyLink(createTiddlyElement(wrapper,"div",null,"agendaItemHeader"),title);
+	createTiddlyElement(header,"span",null,"agendaItemHeaderTime",start + " - " + end);
+	createTiddlyElement(header,"span",null,"agendaItemHeaderSpeaker",speaker);
+	var text = createTiddlyElement(wrapper,"div",null,"agendaItemText");
+	createTiddlyElement(text,"span",null,"agendaItemTextTitle",title);
+	createTiddlyElement(text,"span",null,"agendaItemTextBlurb",blurb);
+};
+
+/*******************
+* currentSession
+*******************/
+config.macros.currentSession = {};
+
+config.macros.currentSession.handler = function(place) {
+	// find out which agenda items are on now
+	var sessions = config.macros.currentSession.find();
+	var currentSessionButton = createTiddlyElement(place,"span","currentSessionButton");
+	if (sessions.length === 0) {
+		createTiddlyText(currentSessionButton,"no sessions on now");
+	} else if (sessions.length == 1) {
+		createTiddlyText(createTiddlyLink(currentSessionButton,sessions[0].title),"Jump to current session");
+	} else {
+		var options = [];
+		options.push({caption:"Jump to current sessions",name:""});
+		for (var i=0;i<sessions.length;i++) {
+			options.push({caption:sessions[i].title,name:sessions[i].title});
+		}
+		createTiddlyDropDown(currentSessionButton,config.macros.currentSession.onchange,options,"");
+	}
+};
+
+config.macros.currentSession.onchange = function() {
+	// open up the selected tiddler and move the agendaMenu to the right session
+	var title = this.value;
+	if (title) {
+		story.displayTiddler(this,title);
+	}
+	// move active agenda item to title
+};
+
+config.macros.currentSession.find = function() {
+	// returns an array of title for sessions on now
+	// assumes agenda items have dates of the form dd/mm/yyyy GMT
+	// and that item start is of the form hhmm
+	var items = Agenda.getItems();
+	var now = new Date();
+	var sessions = [];
+	for (var i=0;i<items.length;i++) {
+		var then = config.macros.currentSession.getTimes(items[i]);
+		// check for now being between start and end of session 
+		if (then.start.getTime() < now.getTime() && then.end.getTime() > now.getTime()) {
+			sessions.push(items[i]);
+		}
+	}
+	return sessions;
+};
+
+config.macros.currentSession.getTimes = function(item) {
+	// returns two dates for agenda item start and finish
+	var then = {};
+	var title = item.title;
+	var date = store.getTiddlerSlice(title,"date");
+	var start = store.getTiddlerSlice(title,"start");
+	var end = store.getTiddlerSlice(title,"end");
+	// guard against single digit entries in the date
+	date = date.replace(/(\b)(\w)(\b)/g,"$10$2$3");
+	// convert YY to YYYY, assuming everything to be after 2000
+	date = date.replace(/(\b)(\d\d)$/g,"$120$2");
+	// convert UK standard date of dd/mm/yyyy to expected YYYYMMDD
+	var d = date.split("/").reverse().join("");
+	var d1 = d+start;
+	var d2 = d+end;
+	then.start = Date.convertFromYYYYMMDDHHMM(d1);
+	then.end = Date.convertFromYYYYMMDDHHMM(d2);
+	return then;
+};
+
+/*******************
+* crossBrowserHelp
+*******************/
+config.macros.crossBrowserHelp = {};
+
+config.macros.crossBrowserHelp.handler = function(place) {
+	// detect different help messages depending on which browser is being used
+	var text = "";
+	if(config.browser.isIE) {
+		// IE
+		text = "You're using IE";
+	} else if (config.browser.isGecko) {
+		// FF
+		text = "You're using Firefox";
+	} else if (config.browser.isSafari || config.browser.isBadSafari) {
+		// Safari
+		text = "You're using Safari";
+	} else if (config.browser.isOpera) {
+		// Opera
+		text = "You're using Opera";
+	}
+	/* Other variables I have to play with:
+	 * ieVersion: // config.browser.ieVersion[1], if it exists, will be the IE version string, eg "6.0"
+	 * firefoxDate: // config.browser.firefoxDate[1], if it exists, will be Firefox release date as "YYYYMMDD"
+	 * isMac:
+	 * isWindows:
+	 * isLinux:
+	 * isUnix
+	 */
+	 createTiddlyText(place,text);
+};
