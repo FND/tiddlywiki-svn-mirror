298405469637f764c830e21de64908e79793e77a
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-20 18:31:13 +0000

    RSS bug fix
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2901 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/plugins/PushAndPullPlugin.js
+++ verticals/ripplerap/plugins/PushAndPullPlugin.js
@@ -261,6 +261,22 @@ function generateRss(filterTag)
 	return s.join("\n");
 }
 
+// Override built-in saveToRss to include author and wikitext fields
+Tiddler.prototype.saveToRss = function(url)
+{
+	var s = [];
+	s.push("<item>");
+	s.push("<title" + ">" + this.title.htmlEncode() + "</title" + ">");
+	s.push("<description>" + wikifyStatic(this.text,null,this).htmlEncode() + "</description>");
+	for(var t=0; t<this.tags.length; t++)
+		s.push("<category>" + this.tags[t] + "</category>");
+	s.push("<link>" + url + "#" + encodeURIComponent(String.encodeTiddlyLink(this.title)) + "</link>");
+	s.push("<pubDate>" + this.modified.toGMTString() + "</pubDate>");
+	s.push("<author>" + this.modifier + "</author>");
+	s.push("</item>");
+	return s.join("\n");
+};
+
 /*************
  * Test macro *
  **************/
