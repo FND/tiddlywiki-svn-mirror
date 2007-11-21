3b19043ceed7998f82761bedcad4b9ee6a8b0507
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-21 12:36:55 +0000

    added skinkers plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2921 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/ripplerap/plugins/SkinkersAlertPlugin.js
@@ -0,0 +1,51 @@
+/***
+|''Name:''|SkinkersAlertPlugin|
+|''Description:''|Send an alert using Skinkers|
+|''Version:''|1.0.0|
+|''Date:''|Nov 21, 2007|
+|''Source:''|http://www.tiddlywiki.com/#SkinkersAlertPlugin|
+|''Author:''|PaulDowney (paul.downey (at) whatfettle (dot) com)|
+|''License:''|[[BSD open source license]]|
+|''CoreVersion:''|2.2|
+***/
+
+//{{{
+// Ensure that the SkinkersAlertPlugin Plugin is only installed once.
+if(!version.extensions.SkinkersAlertPlugin) {
+version.extensions.SkinkersAlertPlugin = {installed:true};
+
+config.macros.skinkersAlert = {
+
+	debug: false,
+	offline: false,
+	
+	alert: function (title, text) {
+
+	        var macro = config.macros.skinkersAlert;
+	
+	        var user = config.options.txtMojoUserName;
+	
+		    var body = encodeURIComponent('message_title') + '=' + title 
+				+ '&' + encodeURIComponent('message_text') + '=' + encodeURIComponent(text)
+				+ '&' + encodeURIComponent('message_link') + '=' + encodeURIComponent('http://lifestream.whatfettle.com/ripplerap');
+				 
+		if(macro.debug)
+			displayMessage('message info:' + body );
+
+		var callback = function(status,params,responseText,url,xhr) {
+			displayMessage(status?'message sent':'message failed');
+			if(config.macros.skinkersAlert.debug)
+				displayMessage(responseText);
+		};
+
+		if(!macro.offline) {
+			var req = doHttp('POST', 'http://10.29.2.182/rest/', body, null, null,null, callback);
+			if(macro.debug)
+				displayMessage(req);
+		}
+	}
+
+}
+
+} //# end of "install only once"
+//}}}
