2fce3d343a8252c6cc2fdd102c820f052709ea90
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-27 15:37:50 +0000

    Initial submission of refactoring of JonathanLister's RSS adaptor
    git-svn-id: http://svn.tiddlywiki.org/Trunk@2984 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JonathanLister/adaptors/RSSAdaptor.js
@@ -0,0 +1,303 @@
+/***
+|''Name:''|RSSAdaptor|
+|''Description:''|Adaptor for talking to RSS 2.0 files|
+|''Author''|Jon Lister|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/JonathanLister/adaptors/RSSAdaptor.js |
+|''Version:''|0.1.1|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''~CoreVersion:''|2.2.6|
+***/
+
+//{{{
+if(!version.extensions.RSSAdaptor) {
+version.extensions.RSSAdaptor = {installed:true};
+
+function RSSAdaptor()
+{
+	this.host = null;
+	this.workspace = null;
+	return this;
+}
+
+RSSAdaptor.NotLoadedError = "RSS file has not been loaded";
+RSSAdaptor.serverType = 'rss';
+
+RSSAdaptor.prototype.setContext = function(context,userParams,callback)
+{
+	if(!context) context = {};
+	context.userParams = userParams;
+	if(callback) context.callback = callback;
+	context.adaptor = this;
+	if(!context.host)
+		context.host = this.host;
+	context.host = RSSAdaptor.fullHostName(context.host);
+	if(!context.workspace)
+		context.workspace = this.workspace;
+	return context;
+};
+
+RSSAdaptor.fullHostName = function(host)
+{
+	if(!host)
+		return '';
+	if(!host.match(/:\/\//))
+		host = 'http://' + host;
+	//if(host.substr(-1) != '/')
+	//	host = host + '/';
+	return host;
+};
+
+RSSAdaptor.prototype.openHost = function(host,context,userParams,callback)
+{
+//#displayMessage("openHost");
+	this.host = RSSAdaptor.fullHostName(host);
+	context = this.setContext(context,userParams,callback);
+	context.status = true;
+	if(callback)
+		window.setTimeout(function() {callback(context,userParams);},10);
+	return true;
+};
+
+RSSAdaptor.loadRssCallback = function(status,context,responseText,url,xhr)
+{
+//#displayMessage("loadRssCallback:"+status);
+	context.status = status;
+	if(!status) {
+		context.statusText = "Error reading file: " + xhr.statusText;
+	} else {
+		context.tiddlers = RSSAdaptor.rssToTiddlers(responseText,context.filter);
+	}
+	context.complete(context,context.userParams);
+};
+
+// Gets the list of workspaces on a given server
+//# Sets context.workspaces, which contains a single default workspace, since RSS files do not have workspaces
+//# Returns true if successful, error string if not
+RSSAdaptor.prototype.getWorkspaceList = function(context,userParams,callback)
+{
+	context = this.setContext(context,userParams,callback);
+	context.workspaces = [{title:"(default)"}];
+	context.status = true;
+	window.setTimeout(function() {callback(context,userParams);},10);
+	return true;
+};
+
+// Open the specified workspace
+//# Returns true if successful, error string if not (or it should)
+//# Trivial in the case of RSS file where we don't have a workspace
+RSSAdaptor.prototype.openWorkspace = function(workspace,context,userParams,callback)
+{
+	this.workspace = workspace;
+	context = this.setContext(context,userParams,callback);
+	context.status = true;
+	if(callback)
+		window.setTimeout(function() {callback(context,userParams);},10);
+	return true;
+};
+
+// Gets the list of tiddlers within a given workspace
+//# Returns true if successful, error string if not
+//# Sets context.tiddlers, which is an array of tiddlers. Each tiddler corresponds to an RSS item
+//# Set these variables if possible:
+//# title: tiddler.title, modified: tiddler.modified, modifier: tiddler.modifier, text: tiddler.text, tags: tiddler.tags, size: tiddler.text
+RSSAdaptor.prototype.getTiddlerList = function(context,userParams,callback,filter)
+{
+//#displayMessage("getTiddlerList");
+	context = this.setContext(context,userParams,callback);
+	if(!context.filter)
+		context.filter = filter;
+	context.complete = RSSAdaptor.getTiddlerListComplete;
+	return context.tiddlers ? 
+		context.complete(context,context.userParams) :
+		loadRemoteFile(context.host,RSSAdaptor.loadRssCallback,context);
+};
+
+RSSAdaptor.getTiddlerListComplete = function(context,userParams)
+{
+	context.status = true;
+	if(context.callback)
+		window.setTimeout(context.callback,10,context,userParams);
+	return true;
+};
+
+RSSAdaptor.prototype.generateTiddlerInfo = function(tiddler)
+{
+	var info = {};
+	info.uri = tiddler.fields['server.host'] + "#" + tiddler.title;
+	return info;
+};
+
+RSSAdaptor.prototype.getTiddler = function(title,context,userParams,callback)
+{
+//#displayMessage("getTiddler:"+title);
+	context = this.setContext(context,userParams,callback);
+	context.title = title;
+	context.complete = RSSAdaptor.getTiddlerComplete;
+	return context.tiddlers ? 
+		context.complete(context,context.userParams) :
+		loadRemoteFile(context.host,RSSAdaptor.loadRssCallback,context);
+};
+
+RSSAdaptor.getTiddlerComplete = function(context,userParams)
+{
+	//# Retrieve the tiddler from the this.context.tiddlers array
+	for(var i=0; i<context.tiddlers.length; i++) {
+		if(context.tiddlers[i].title == context.title) {
+			context.tiddler = context.tiddlers[i];
+			break;
+		}
+	}
+	if(context.tiddler) {
+		context.status = true;
+	} else {
+		context.status = false;
+		context.statusText = "error retrieving tiddler: " + title;
+		return context.statusText;
+	}
+	if(context.allowSynchronous) {
+		context.isSynchronous = true;
+		context.callback(context,userParams);
+	} else {
+		window.setTimeout(function() {callback(context,userParams);},10);
+	}
+	return true;
+};
+
+RSSAdaptor.prototype.close = function()
+{
+};
+
+RSSAdaptor.rssToTiddlers = function(rss,filter)
+{
+	var tiddlers = [];
+	rss = rss.replace(/\r+/mg,"");
+	// regex_item matches on the items 
+	var regex_item = /<item>(.|\n)*?<\/item>/mg;
+	// regex_title matches for the titles
+	var regex_title = /<title>(.|\n)*?<\/title>/mg;
+	var regex_guid = /<guid>(.|\n)*?<\/guid>/mg;
+	var regex_wiki = /<tw:wikitext>(.|\n)*?<\/tw:wikitext>/mg;
+	var regex_desc = /<description>(.|\n)*?<\/description>/mg;
+	var regex_category = /<category>(.|\n)*?<\/category>/mg;
+	var regex_link = /<link>(\S|\n)*?<\/link>/mg;
+	var regex_pubDate = /<pubDate>(.|\n)*?<\/pubDate>/mg;
+	var regex_author = /<author>(.|\n)*?<\/author>/mg;
+	var item_match = rss.match(regex_item);
+	for(var i=0;i<item_match.length;i++) {
+		// create a new Tiddler in context.tiddlers with the finished item object
+		// grab a title
+		var item = {};
+		var title = item_match[i].match(regex_title);
+		if(title) {
+			item.title = title[0].replace(/^<title>|<\/title>$/mg,"");
+		} else {
+			// something went wrong grabbing the title, grab the guid instead
+			title = item_match[i].match(regex_guid);
+			// TEMP FIX FOR DEMO 21/11/07
+			if(title) {
+				item.title = title[0].replace(/^<guid>|<\/guid>$/mg,"");
+			} else {
+				item.title = new Date();
+				displayMessage("problem with getting title AND guid: " + item_match[i]);
+			}
+		}
+		// This line makes sure any html-encoding in the title is reversed
+		item.title = item.title.htmlDecode();
+		// There is a problem with the title, which is that it is not formatted, so characters like &apos; are not converted at render time
+		// renderHtmlText() extends String and sorts out the problem
+		item.title = item.title.renderHtmlText();
+		// grab original wikitext if it is there as an extended field
+		wikitext = item_match[i].match(regex_wiki);
+		if(wikitext) {
+			item.text = wikitext[0].replace(/^<tw:wikitext>|<\/tw:wikitext>$/mg,"");
+			item.text = item.text.htmlDecode();
+		} else {
+			// grab a description
+			desc = item_match[i].match(regex_desc);
+			if(desc) {
+				item.text = desc[0].replace(/^<description>|<\/description>$/mg,"");
+			} else {
+				item.text = "empty, something seriously wrong with this item";
+			}
+		}
+		var t = new Tiddler(item.title);
+		if(wikitext) {
+			t.text = item.text;
+		} else {
+			t.text = "<html>" + item.text.renderHtmlText() + "</html>";
+		}
+		// grab the categories
+		category = item_match[i].match(regex_category);
+		if(category) {
+			item.categories = [];
+			for(var j=0;j<category.length;j++) {
+				item.categories[j] = category[j].replace(/^<category>|<\/category>$/mg,"");
+			}
+			t.tags = item.categories;
+		}
+		// grab the link and put it in a custom field (assumes this is sensible)
+		// regex_link assumes you can never have whitespace in a link
+		link = item_match[i].match(regex_link);
+		if(link) {
+			item.link = link[0].replace(/^<link>|<\/link>$/mg,"");
+		} else {
+			item.link = "#";
+		}
+		t.fields["linktooriginal"] = item.link;
+		// grab date created
+		pubDate = item_match[i].match(regex_pubDate);
+		if(pubDate) {
+			pubDate = pubDate[0].replace(/^<pubDate>|<\/pubDate>$/mg,"");
+			item.pubDate = new Date(pubDate);
+		} else {
+			item.pubDate = new Date();
+		}
+		t.created = item.pubDate;
+		// grab author
+		author = item_match[i].match(regex_author);
+		if(author) {
+			author = author[0].replace(/^<author>|<\/author>$/mg,"");
+			item.author = author;
+		} else {
+			item.author = "anonymous";
+		}
+		t.modifier = item.author;
+		tiddlers.push(t);
+		}
+	/*if(filter) {
+		var tw = new TiddlyWiki();
+		tw.tiddlers = tiddlers;
+		tiddlers = tw.filterTiddlers(filter);
+	}*/
+	return tiddlers;
+};
+
+config.adaptors[RSSAdaptor.serverType] = RSSAdaptor;
+
+// renderHtmlText puts a string through the browser render process and then extracts the text
+// useful to turn HTML entities into literals such as &apos; to '
+// this method has two passes at the string - the first to convert it to html and the second
+// to selectively catch the ASCII-encoded characters without losing the rest of the html
+String.prototype.renderHtmlText = function() {
+	var text = this;
+	var regex_cdata = /<!\[CDATA\[((?:.| )*?)\]\]>/mg;
+	regex_cdata.lastIndex = 0;
+	var match = regex_cdata.exec(this);
+	if(match) {
+		text = match[1];
+	}
+	var e = createTiddlyElement(document.body,"div");
+	e.innerHTML = text;
+	text = getPlainText(e);
+	text = text.replace(/&#[\w]+?;/g,function(word) {
+		var ee = createTiddlyElement(e,"div");
+		ee.innerHTML = word;
+		return getPlainText(ee);
+	});
+	removeNode(e);
+	return text;
+};
+
+} //# end of 'install only once'
+//}}}
