e6d1aa70b5b3650602638ed4bdd37bc9f39c6a41
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-11-30 12:39:38 +0000

    wip
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3074 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/server/test/mkusers.sh
+++ verticals/ripplerap/server/test/mkusers.sh
@@ -3,18 +3,68 @@
 #  usage mkusers [n]
 #  blanks out user directories
 
-declare -i n=$1
-[ $n -le 0 ] && n=100
+declare -i nusers=$1
+declare -i nitems=$2
+
+[ $nusers -le 0 ] && nusers=100
+[ $nitems -le 0 ] && nitems=5
 
 [ -d "users" ] || mkdir "users"
+[ -d "notes" ] || mkdir "notes"
+
+uri="http://www.tiddlywiki.com/"
 
 let i=0
-while [ $i -lt $n ] ; do
+while [ $i -lt $nusers ] ; do
     let i=i+1
     user=$(printf "TestUser%03d" $i)
     userdir="users/$user"
     [ -d "$userdir" ] || mkdir "$userdir"
-    sed -e "s/PaulDowney/$user/" < index.xml > $userdir/index.xml
+    date=$(date)
+    echo $i 1>&2
+{
+    cat <<EOF 
+<?xml version="1.0"?>
+<rss version="2.0">
+<channel>
+<title></title>
+<link>http://www.tiddlywiki.com/</link>
+<description>the social conferencing tool</description>
+<language>en-us</language>
+<copyright>Copyright 2007 PaulDowney</copyright>
+<pubDate>$date</pubDate>
+<lastBuildDate>$date</lastBuildDate>
+<docs>http://blogs.law.harvard.edu/tech/rss</docs>
+<generator>TiddlyWiki 2.2.6</generator>
+EOF
+
+    let j=0
+    while [ $j -lt $nitems ] ; do
+	let j=j+1
+
+	title=$(printf "Testing's \`Number\`: %03d" $j)
+	escaped_title=$(echo "$title" | sed 's/ /%20/g')
+
+	cat <<EOF 
+<item>
+<title>$title from $user</title>
+<description>some stuff on $title from $user ..</description>
+<category>note</category>
+<category>shared</category>
+<link>$uri/#$escaped_title</link>
+<pubDate>$date</pubDate>
+<author>$user</author>
+</item>
+EOF
+done
+
+    cat <<EOF 
+</channel>
+</rss>
+EOF
+
+} > $userdir/index.xml
+
 done
 
 exit 0
---
+++ verticals/ripplerap/server/test/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+touch users/TestUser*1/index.xml
+
+export ripplerapdir=..
+../ripplerap.sh 
