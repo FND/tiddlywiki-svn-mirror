7e827a06a0788fffee4818c2897cf295fe15e040
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-03 11:55:51 +0000

    button style
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3109 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/plugins/MakeNotesControlPlugin.js
+++ verticals/ripplerap/plugins/MakeNotesControlPlugin.js
@@ -20,9 +20,29 @@ version.extensions.MakeNotesControlPlugin = {installed:true};
 	config.macros.MakeNotesControl.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 	{
 		createTiddlyButton(place,"make notes","Make notes on this session",config.macros.MakeNotesControl.onClick);
+		createTiddlyCheckbox(place,'private',false,config.macros.MakeNotesControl.togglePrivate);
+		
 	};
 
 	config.macros.MakeNotesControl.onClick = function()
+	{
+		var after =  story.findContainingTiddler(this);
+		var sessionTitle = after.id.substr(8);
+		var title = sessionTitle + " from " + config.options.txtUserName;
+		var template = "NotesEditTemplate";
+		story.displayTiddler(after,title,template,false,null,null);
+	
+		var text = "your notes... " + title;
+		story.getTiddlerField(title,"text").value = text.format([title]);
+			
+		story.setTiddlerTag(title,'notes',+1);
+		story.setTiddlerTag(title,'shared',+1);
+		story.focusTiddler(title,focus);
+		
+		return false;
+	};
+	
+	config.macros.MakeNotesControl.togglePrivate = function()
 	{
 		var after =  story.findContainingTiddler(this);
 		var sessionTitle = after.id.substr(8);
