42fd4aa22a347313a661e3335218eae59c7a22ef
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-03 10:23:56 +0000

    Tiddler styling and default values
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3103 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/settings.js
+++ verticals/ripplerap/settings.js
@@ -2,5 +2,7 @@
 
 config.options.txtMainTab = "AgendaDay2";
 config.options.txtMojoTelno = "+3368...";
+config.options.txtRipplerapAccountPassword = "password";
+config.options.chkRipplerapShare = true;
 
 //}}}
