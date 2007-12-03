759932b526ddd0787a9d69c8e2cfae15fbdd5823
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-03 10:47:28 +0000

    ripplerap flags
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3104 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/settings.js
+++ verticals/ripplerap/settings.js
@@ -4,5 +4,6 @@ config.options.txtMainTab = "AgendaDay2";
 config.options.txtMojoTelno = "+3368...";
 config.options.txtRipplerapAccountPassword = "password";
 config.options.chkRipplerapShare = true;
+config.options.chkRipplerapReadyToUse = false;
 
 //}}}
