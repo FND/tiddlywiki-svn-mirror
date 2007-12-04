9bf2f3f9312a6d4e3415f38d1983847d8f2d1c0c
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-04 11:35:04 +0000

    full path for user file
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3151 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/server/reg/index.php
+++ verticals/ripplerap/server/reg/index.php
@@ -69,7 +69,7 @@
 	$f = fopen($userDir."/.htaccess", 'w') or die("can't open users' .htaccess file");
 	fwrite($f, "AuthType Basic\n");
 	fwrite($f, "AuthName 'TiddleLeWeb'\n");
-	fwrite($f, "AuthUserFile .htpasswd\n");
+	fwrite($f, "AuthUserFile $userDir/.htpasswd\n");
 	fwrite($f, "Require user $username\n");
 	fclose($f);	
 
