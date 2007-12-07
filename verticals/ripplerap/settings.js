7f44714649d5dbb11b73013975e1c8c20c7b4ab4
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-07 11:25:38 +0000

    autosave disabled, animations enabled
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3246 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/settings.js
+++ verticals/ripplerap/settings.js
@@ -2,6 +2,9 @@
 
 config.options.txtMainTab = "AgendaDay2";
 config.options.txtMojoTelno = "+3368...";
+config.options.chkAutoSave = false;
+config.options.chkAnimate = true;
+
 //config.options.chkRipplerapShare = true;
 //config.options.chkRipplerapReadyToUse = false;
 
