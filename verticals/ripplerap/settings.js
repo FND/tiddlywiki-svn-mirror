38167894683fc41e0a946fc09f1bb86ca64e4bc4
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-11 00:18:12 +0000

    autosave
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3281 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/settings.js
+++ verticals/ripplerap/settings.js
@@ -2,8 +2,9 @@
 
 config.options.txtMainTab = "AgendaDay2";
 config.options.txtMojoTelno = "+3368...";
-config.options.chkAutoSave = false;
+config.options.chkAutoSave = true;
 config.options.chkAnimate = true;
+config.options.chkSaveBackups = false;
 
 config.options.chkRipplerapShare = true;
 config.options.chkRipplerapReadyToUse = false;
