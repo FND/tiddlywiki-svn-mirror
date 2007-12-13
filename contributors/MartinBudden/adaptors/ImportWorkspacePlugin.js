ebeb809274c94cbdb968b4042e8522f1f921362f
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2007-12-13 16:17:15 +0000

    Changed so that tiddlers for all feeds are imported on startup (if import on startup option is set).
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3292 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/adaptors/ImportWorkspacePlugin.js
+++ contributors/MartinBudden/adaptors/ImportWorkspacePlugin.js
@@ -3,7 +3,7 @@
 |''Description:''|Commands to access hosted TiddlyWiki data|
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/adaptors/ImportWorkspacePlugin.js |
-|''Version:''|0.0.3|
+|''Version:''|0.0.4|
 |''Date:''|Aug 23, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
@@ -28,7 +28,7 @@ if(config.options.chkImportWorkspaceOnStartup == undefined)
 config.messages.hostOpened = "Host '%0' opened";
 config.messages.workspaceOpened = "Workspace '%0' opened";
 config.messages.workspaceTiddlers = "%0 tiddlers in workspace, importing %1 of them";
-config.messages.tiddlerImported = 'Tiddler: "%0" imported';
+config.messages.tiddlerImported = "Tiddler: \"%0\" imported";
 
 
 // import all the tiddlers from a given workspace on a given host
@@ -46,18 +46,19 @@ config.macros.importWorkspace.init = function()
 		//# no server set so get the server from the specified feed
 		var title = config.options.txtImportFeed;
 		if(title=='') {
-			//# no feed specified, so just use the first feed found
-			var tiddlers = store.getTaggedTiddlers("systemServer");
-			if(tiddlers.length==0)
-				return;
-			title = tiddlers[0].title;
+			//# no feed specified, so just use the first feed found to set the defaultCustomFields
+			var tiddlers = store.getTaggedTiddlers('systemServer');
+			if(tiddlers.length>0)
+				title = tiddlers[0].title;
 		}
-		config.defaultCustomFields['server.type'] = store.getTiddlerSlice(title,'Type');
-		config.defaultCustomFields['server.host'] = store.getTiddlerSlice(title,'URL');
-		config.defaultCustomFields['server.workspace'] = store.getTiddlerSlice(title,'Workspace');
+		if(title) {
+			config.defaultCustomFields['server.type'] = store.getTiddlerSlice(title,'Type');
+			config.defaultCustomFields['server.host'] = store.getTiddlerSlice(title,'URL');
+			config.defaultCustomFields['server.workspace'] = store.getTiddlerSlice(title,'Workspace');
+		}
 	}
 	if(config.options.chkImportWorkspaceOnStartup)
-		config.macros.importWorkspace.getTiddlers(customFields);
+		config.macros.importWorkspace.getTiddlersForAllFeeds();
 };
 
 config.macros.importWorkspace.handler = function(place,macroName,params,wikifier,paramString,tiddler)
@@ -70,7 +71,7 @@ config.macros.importWorkspace.handler = function(place,macroName,params,wikifier
 	if(!customFields['server.type']) {
 		var title = getParam(params,'anon');
 		if(!title) {
-			var tiddlers = store.getTaggedTiddlers("systemServer");
+			var tiddlers = store.getTaggedTiddlers('systemServer');
 			if(tiddlers.length>0)
 				title = tiddlers[0].title;
 		}
@@ -98,30 +99,52 @@ config.macros.importWorkspace.onClick = function(e)
 	config.macros.importWorkspace.getTiddlers(fields);
 };
 
-config.macros.importWorkspace.getTiddlers = function(fields)
+config.macros.importWorkspace.getTiddlersForAllFeeds = function()
 {
-	if(!fields['server.type']) {
-		var tiddlers = store.getTaggedTiddlers("systemServer");
+	var tiddlers = store.getTaggedTiddlers('systemServer');
+	for(var i=0;i<tiddlers.length;i++) {
+		config.macros.importWorkspace.getTiddlersForFeed(tiddlers[i].title);
+	}
+};
+
+config.macros.importWorkspace.getTiddlersForFeed = function(feed)
+{
+	var fields = {};
+	fields['server.type'] = store.getTiddlerSlice(feed,'Type');
+	fields['server.host'] = store.getTiddlerSlice(feed,'URL');
+	fields['server.workspace'] = store.getTiddlerSlice(feed,'Workspace');
+	var filter = store.getTiddlerSlice(feed,'TiddlerFilter');
+	config.macros.importWorkspace.getTiddlers(fields,filter);
+};
+
+config.macros.importWorkspace.getTiddlers = function(fields,filter)
+{
+	/*if(!fields['server.type']) {
+		var tiddlers = store.getTaggedTiddlers('systemServer');
 		var title = tiddlers[0].title;
 		fields = {};
 		fields['server.type'] = store.getTiddlerSlice(title,'Type');
 		fields['server.host'] = store.getTiddlerSlice(title,'URL');
 		fields['server.workspace'] = store.getTiddlerSlice(title,'Workspace');
 		context.filter = store.getTiddlerSlice(title,'TiddlerFilter');
-	}
+	}*/
 	var serverType = fields['server.type'];
 	if(!serverType)
 		serverType = fields['wikiformat'];
 	if(!serverType)
 		return false;
 	var adaptor = new config.adaptors[serverType];
+	if(!adaptor)
+		return false;
 	if(adaptor) {
 		var context = {};
 		context.host = fields['server.host'];
 		context.workspace = fields['server.workspace'];
+		context.filter = filter;
 		context.adaptor = adaptor;
 		adaptor.openHost(context.host,context,null,config.macros.importWorkspace.openHostCallback);
 	}
+	return true;
 };
 
 config.macros.importWorkspace.openHostCallback = function(context,userParams)
