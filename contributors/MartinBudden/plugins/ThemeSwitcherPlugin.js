0d8920200eb39050fbb55b0a3c881ee0b75c3fa7
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-01-11 15:18:52 +0000

    Documentation update
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3364 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
+++ contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js
@@ -4,12 +4,20 @@
 |''Author:''|Martin Budden|
 |''Source:''|http://www.martinswiki.com/#ThemeSwitcherPlugin |
 |''~CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/ThemeSwitcherPlugin.js |
-|''Version:''|0.0.6|
+|''Version:''|0.0.7|
 |''Status:''|Not for release - still under development|
 |''Date:''|Oct 31, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
 |''~CoreVersion:''|2.3|
+
+!!Description
+This plugin defines a theme selector button that allows you to select a theme from a list of tiddlers tagged with "systemTheme".
+
+!!Usage
+Include
+{{{<<selectTheme>>}}}
+in any tiddler to create a select theme button.
 
 ***/
 
@@ -25,7 +33,7 @@ config.macros.selectTheme = {
 
 config.macros.selectTheme.handler = function(place)
 {
-	var btn = createTiddlyButton(place,this.label,this.prompt,this.onClick);
+	createTiddlyButton(place,this.label,this.prompt,this.onClick);
 };
 
 config.macros.selectTheme.onClick = function(ev)
@@ -41,9 +49,7 @@ config.macros.selectTheme.onClick = function(ev)
 		btn.setAttribute('theme',t);
 	}
 	Popup.show();
-	e.cancelBubble = true;
-	if(e.stopPropagation) e.stopPropagation();
-	return false;
+	return stopEvent(e);
 };
 
 config.macros.selectTheme.onClickTheme = function(ev)
