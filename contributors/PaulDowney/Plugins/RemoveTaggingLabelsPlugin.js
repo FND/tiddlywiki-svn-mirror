937d4a61afc479cef083859391fe83cfb781e5fd
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-01-25 16:54:45 +0000

    RemoveTaggingLabelsPlugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3419 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/Plugins/RemoveTaggingLabelsPlugin.js
@@ -0,0 +1,20 @@
+/***
+|''Name:''|RemoveTaggingLabelsPlugin|
+|''Description:''|Remove the spurious text labels put out by the 'tagging' macro|
+|''Version:''|0.0.1|
+|''Date:''|Jan 25, 2008|
+|''Source:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/Plugins/RemoveTaggingLabelsPlugin|
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
+|''License:''|[[BSD open source license]]|
+|''~CoreVersion:''|2.1.0|
+|''Browser:''|Firefox 1.0.4+; Firefox 1.5; InternetExplorer 6.0|
+***/
+
+//{{{
+
+merge(config.macros.tagging,{
+	label: "",
+	labelNotTag: "",
+	tooltip: "List of tiddlers tagged with '%0'"});
+
+//}}}
