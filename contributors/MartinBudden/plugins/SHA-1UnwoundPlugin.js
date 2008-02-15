4a39fde7e3b307bdc5b5b38b3cd3215c7d277c66
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-02-15 18:36:20 +0000

    Efficiency improvements
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3520 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/SHA-1UnwoundPlugin.js
+++ contributors/MartinBudden/plugins/SHA-1UnwoundPlugin.js
@@ -25,34 +25,31 @@ if(version.major < 2 || (version.major == 2 && version.minor < 1))
 Crypto.sha1 = function(x,blen)
 {
 	if(blen==null)
-		{return null;}
+		return null;
 
 	// Add 32-bit integers, wrapping at 32 bits
 	//# Uses 16-bit operations internally to work around bugs in some JavaScript interpreters.
-	a32 = function(a,b)
+	function a32(a,b)
 	{
 		var lsw = (a&0xFFFF)+(b&0xFFFF);
 		var msw = (a>>16)+(b>>16)+(lsw>>16);
 		return (msw<<16)|(lsw&0xFFFF);
-	};
-	// Add five 32-bit integers, wrapping at 32 bits
-	//# Uses 16-bit operations internally to work around bugs in some JavaScript interpreters.
-	a32x5 = function(a,b,c,d,e)
+	}
+	//# Cryptographic round helper function. Add five 32-bit integers, wrapping at 32 bits, second parameter is rotated left 5 bits before the addition
+	function AA(a,b,c,d,e)
 	{
-		var lsw = (a&0xFFFF)+(b&0xFFFF)+(c&0xFFFF)+(d&0xFFFF)+(e&0xFFFF);
-		var msw = (a>>16)+(b>>16)+(c>>16)+(d>>16)+(e>>16)+(lsw>>16);
+		b=(b>>>27)|(b<<5);
+		var lsw=(a&0xFFFF)+(b&0xFFFF)+(c&0xFFFF)+(d&0xFFFF)+(e&0xFFFF);
+		var msw=(a>>16)+(b>>16)+(c>>16)+(d>>16)+(e>>16)+(lsw>>16);
 		return (msw<<16)|(lsw&0xFFFF);
-	};
-	// Bitwise rotate left a 32-bit integer by 1 bit.
-	r32 = function(n)
-	{
-		return (n>>>31)|(n<<1);
-	};
+	}
+	//# rotate left 1 bit
+	function R1(n) { return (n<<1)|(n>>>31); }
 
 	var len=blen*8;
-	// Append padding so length in bits is 448 mod 512
+	//# Append padding so length in bits is 448 mod 512
 	x[len>>5] |= 0x80<<(24-len%32);
-	// Append length
+	//# Append length
 	x[((len+64>>9)<<4)+15] = len;
 
 	var k1=0x5A827999;
@@ -67,104 +64,104 @@ Crypto.sha1 = function(x,blen)
 	var h4=0xC3D2E1F0;
 
 	var w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,wA,wB,wC,wD,wE,wF;
-	for(var i=0;i<x.length;i+=16)
-		{
+	for(var i=0;i<x.length;i+=16) {
 		var a=h0;
 		var b=h1;
 		var c=h2;
 		var d=h3;
 		var e=h4;
-		w0=x[i];   e=a32x5(e,(a>>>27)|(a<<5),d^(b&(c^d)),w0,k1);b=(b>>>2)|(b<<30);
-		w1=x[i+1]; d=a32x5(d,(e>>>27)|(e<<5),c^(a&(b^c)),w1,k1);a=(a>>>2)|(a<<30);
-		w2=x[i+2]; c=a32x5(c,(d>>>27)|(d<<5),b^(e&(a^b)),w2,k1);e=(e>>>2)|(e<<30);
-		w3=x[i+3]; b=a32x5(b,(c>>>27)|(c<<5),a^(d&(e^a)),w3,k1);d=(d>>>2)|(d<<30);
-		w4=x[i+4]; a=a32x5(a,(b>>>27)|(b<<5),e^(c&(d^e)),w4,k1);c=(c>>>2)|(c<<30);
-		w5=x[i+5]; e=a32x5(e,(a>>>27)|(a<<5),d^(b&(c^d)),w5,k1);b=(b>>>2)|(b<<30);
-		w6=x[i+6]; d=a32x5(d,(e>>>27)|(e<<5),c^(a&(b^c)),w6,k1);a=(a>>>2)|(a<<30);
-		w7=x[i+7]; c=a32x5(c,(d>>>27)|(d<<5),b^(e&(a^b)),w7,k1);e=(e>>>2)|(e<<30);
-		w8=x[i+8]; b=a32x5(b,(c>>>27)|(c<<5),a^(d&(e^a)),w8,k1);d=(d>>>2)|(d<<30);
-		w9=x[i+9]; a=a32x5(a,(b>>>27)|(b<<5),e^(c&(d^e)),w9,k1);c=(c>>>2)|(c<<30);
-		wA=x[i+10];e=a32x5(e,(a>>>27)|(a<<5),d^(b&(c^d)),wA,k1);b=(b>>>2)|(b<<30);
-		wB=x[i+11];d=a32x5(d,(e>>>27)|(e<<5),c^(a&(b^c)),wB,k1);a=(a>>>2)|(a<<30);
-		wC=x[i+12];c=a32x5(c,(d>>>27)|(d<<5),b^(e&(a^b)),wC,k1);e=(e>>>2)|(e<<30);
-		wD=x[i+13];b=a32x5(b,(c>>>27)|(c<<5),a^(d&(e^a)),wD,k1);d=(d>>>2)|(d<<30);
-		wE=x[i+14];a=a32x5(a,(b>>>27)|(b<<5),e^(c&(d^e)),wE,k1);c=(c>>>2)|(c<<30);
-		wF=x[i+15];e=a32x5(e,(a>>>27)|(a<<5),d^(b&(c^d)),wF,k1);b=(b>>>2)|(b<<30);
-		w0=r32(wD^w8^w2^w0);d=a32x5(d,(e>>>27)|(e<<5),c^(a&(b^c)),w0,k1);a=(a>>>2)|(a<<30);
-		w1=r32(wE^w9^w3^w1);c=a32x5(c,(d>>>27)|(d<<5),b^(e&(a^b)),w1,k1);e=(e>>>2)|(e<<30);
-		w2=r32(wF^wA^w4^w2);b=a32x5(b,(c>>>27)|(c<<5),a^(d&(e^a)),w2,k1);d=(d>>>2)|(d<<30);
-		w3=r32(w0^wB^w5^w3);a=a32x5(a,(b>>>27)|(b<<5),e^(c&(d^e)),w3,k1);c=(c>>>2)|(c<<30);
 
-		w4=r32(w1^wC^w6^w4);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,w4,k2);b=(b>>>2)|(b<<30);
-		w5=r32(w2^wD^w7^w5);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,w5,k2);a=(a>>>2)|(a<<30);
-		w6=r32(w3^wE^w8^w6);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,w6,k2);e=(e>>>2)|(e<<30);
-		w7=r32(w4^wF^w9^w7);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,w7,k2);d=(d>>>2)|(d<<30);
-		w8=r32(w5^w0^wA^w8);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,w8,k2);c=(c>>>2)|(c<<30);
-		w9=r32(w6^w1^wB^w9);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,w9,k2);b=(b>>>2)|(b<<30);
-		wA=r32(w7^w2^wC^wA);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,wA,k2);a=(a>>>2)|(a<<30);
-		wB=r32(w8^w3^wD^wB);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,wB,k2);e=(e>>>2)|(e<<30);
-		wC=r32(w9^w4^wE^wC);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,wC,k2);d=(d>>>2)|(d<<30);
-		wD=r32(wA^w5^wF^wD);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,wD,k2);c=(c>>>2)|(c<<30);
-		wE=r32(wB^w6^w0^wE);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,wE,k2);b=(b>>>2)|(b<<30);
-		wF=r32(wC^w7^w1^wF);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,wF,k2);a=(a>>>2)|(a<<30);
-		w0=r32(wD^w8^w2^w0);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,w0,k2);e=(e>>>2)|(e<<30);
-		w1=r32(wE^w9^w3^w1);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,w1,k2);d=(d>>>2)|(d<<30);
-		w2=r32(wF^wA^w4^w2);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,w2,k2);c=(c>>>2)|(c<<30);
-		w3=r32(w0^wB^w5^w3);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,w3,k2);b=(b>>>2)|(b<<30);
-		w4=r32(w1^wC^w6^w4);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,w4,k2);a=(a>>>2)|(a<<30);
-		w5=r32(w2^wD^w7^w5);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,w5,k2);e=(e>>>2)|(e<<30);
-		w6=r32(w3^wE^w8^w6);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,w6,k2);d=(d>>>2)|(d<<30);
-		w7=r32(w4^wF^w9^w7);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,w7,k2);c=(c>>>2)|(c<<30);
+		w0=x[i];   e=AA(e,a,d^(b&(c^d)),w0,k1);b=(b>>>2)|(b<<30);
+		w1=x[i+1]; d=AA(d,e,c^(a&(b^c)),w1,k1);a=(a>>>2)|(a<<30);
+		w2=x[i+2]; c=AA(c,d,b^(e&(a^b)),w2,k1);e=(e>>>2)|(e<<30);
+		w3=x[i+3]; b=AA(b,c,a^(d&(e^a)),w3,k1);d=(d>>>2)|(d<<30);
+		w4=x[i+4]; a=AA(a,b,e^(c&(d^e)),w4,k1);c=(c>>>2)|(c<<30);
+		w5=x[i+5]; e=AA(e,a,d^(b&(c^d)),w5,k1);b=(b>>>2)|(b<<30);
+		w6=x[i+6]; d=AA(d,e,c^(a&(b^c)),w6,k1);a=(a>>>2)|(a<<30);
+		w7=x[i+7]; c=AA(c,d,b^(e&(a^b)),w7,k1);e=(e>>>2)|(e<<30);
+		w8=x[i+8]; b=AA(b,c,a^(d&(e^a)),w8,k1);d=(d>>>2)|(d<<30);
+		w9=x[i+9]; a=AA(a,b,e^(c&(d^e)),w9,k1);c=(c>>>2)|(c<<30);
+		wA=x[i+10];e=AA(e,a,d^(b&(c^d)),wA,k1);b=(b>>>2)|(b<<30);
+		wB=x[i+11];d=AA(d,e,c^(a&(b^c)),wB,k1);a=(a>>>2)|(a<<30);
+		wC=x[i+12];c=AA(c,d,b^(e&(a^b)),wC,k1);e=(e>>>2)|(e<<30);
+		wD=x[i+13];b=AA(b,c,a^(d&(e^a)),wD,k1);d=(d>>>2)|(d<<30);
+		wE=x[i+14];a=AA(a,b,e^(c&(d^e)),wE,k1);c=(c>>>2)|(c<<30);
+		wF=x[i+15];e=AA(e,a,d^(b&(c^d)),wF,k1);b=(b>>>2)|(b<<30);
+		w0=R1(wD^w8^w2^w0);d=AA(d,e,c^(a&(b^c)),w0,k1);a=(a>>>2)|(a<<30);
+		w1=R1(wE^w9^w3^w1);c=AA(c,d,b^(e&(a^b)),w1,k1);e=(e>>>2)|(e<<30);
+		w2=R1(wF^wA^w4^w2);b=AA(b,c,a^(d&(e^a)),w2,k1);d=(d>>>2)|(d<<30);
+		w3=R1(w0^wB^w5^w3);a=AA(a,b,e^(c&(d^e)),w3,k1);c=(c>>>2)|(c<<30);
 
-		w8=r32(w5^w0^wA^w8);e=a32x5(e,(a>>>27)|(a<<5),(b&c)|(d&(b|c)),w8,k3);b=(b>>>2)|(b<<30);
-		w9=r32(w6^w1^wB^w9);d=a32x5(d,(e>>>27)|(e<<5),(a&b)|(c&(a|b)),w9,k3);a=(a>>>2)|(a<<30);
-		wA=r32(w7^w2^wC^wA);c=a32x5(c,(d>>>27)|(d<<5),(e&a)|(b&(e|a)),wA,k3);e=(e>>>2)|(e<<30);
-		wB=r32(w8^w3^wD^wB);b=a32x5(b,(c>>>27)|(c<<5),(d&e)|(a&(d|e)),wB,k3);d=(d>>>2)|(d<<30);
-		wC=r32(w9^w4^wE^wC);a=a32x5(a,(b>>>27)|(b<<5),(c&d)|(e&(c|d)),wC,k3);c=(c>>>2)|(c<<30);
-		wD=r32(wA^w5^wF^wD);e=a32x5(e,(a>>>27)|(a<<5),(b&c)|(d&(b|c)),wD,k3);b=(b>>>2)|(b<<30);
-		wE=r32(wB^w6^w0^wE);d=a32x5(d,(e>>>27)|(e<<5),(a&b)|(c&(a|b)),wE,k3);a=(a>>>2)|(a<<30);
-		wF=r32(wC^w7^w1^wF);c=a32x5(c,(d>>>27)|(d<<5),(e&a)|(b&(e|a)),wF,k3);e=(e>>>2)|(e<<30);
-		w0=r32(wD^w8^w2^w0);b=a32x5(b,(c>>>27)|(c<<5),(d&e)|(a&(d|e)),w0,k3);d=(d>>>2)|(d<<30);
-		w1=r32(wE^w9^w3^w1);a=a32x5(a,(b>>>27)|(b<<5),(c&d)|(e&(c|d)),w1,k3);c=(c>>>2)|(c<<30);
-		w2=r32(wF^wA^w4^w2);e=a32x5(e,(a>>>27)|(a<<5),(b&c)|(d&(b|c)),w2,k3);b=(b>>>2)|(b<<30);
-		w3=r32(w0^wB^w5^w3);d=a32x5(d,(e>>>27)|(e<<5),(a&b)|(c&(a|b)),w3,k3);a=(a>>>2)|(a<<30);
-		w4=r32(w1^wC^w6^w4);c=a32x5(c,(d>>>27)|(d<<5),(e&a)|(b&(e|a)),w4,k3);e=(e>>>2)|(e<<30);
-		w5=r32(w2^wD^w7^w5);b=a32x5(b,(c>>>27)|(c<<5),(d&e)|(a&(d|e)),w5,k3);d=(d>>>2)|(d<<30);
-		w6=r32(w3^wE^w8^w6);a=a32x5(a,(b>>>27)|(b<<5),(c&d)|(e&(c|d)),w6,k3);c=(c>>>2)|(c<<30);
-		w7=r32(w4^wF^w9^w7);e=a32x5(e,(a>>>27)|(a<<5),(b&c)|(d&(b|c)),w7,k3);b=(b>>>2)|(b<<30);
-		w8=r32(w5^w0^wA^w8);d=a32x5(d,(e>>>27)|(e<<5),(a&b)|(c&(a|b)),w8,k3);a=(a>>>2)|(a<<30);
-		w9=r32(w6^w1^wB^w9);c=a32x5(c,(d>>>27)|(d<<5),(e&a)|(b&(e|a)),w9,k3);e=(e>>>2)|(e<<30);
-		wA=r32(w7^w2^wC^wA);b=a32x5(b,(c>>>27)|(c<<5),(d&e)|(a&(d|e)),wA,k3);d=(d>>>2)|(d<<30);
-		wB=r32(w8^w3^wD^wB);a=a32x5(a,(b>>>27)|(b<<5),(c&d)|(e&(c|d)),wB,k3);c=(c>>>2)|(c<<30);
+		w4=R1(w1^wC^w6^w4);e=AA(e,a,b^c^d,w4,k2);b=(b>>>2)|(b<<30);
+		w5=R1(w2^wD^w7^w5);d=AA(d,e,a^b^c,w5,k2);a=(a>>>2)|(a<<30);
+		w6=R1(w3^wE^w8^w6);c=AA(c,d,e^a^b,w6,k2);e=(e>>>2)|(e<<30);
+		w7=R1(w4^wF^w9^w7);b=AA(b,c,d^e^a,w7,k2);d=(d>>>2)|(d<<30);
+		w8=R1(w5^w0^wA^w8);a=AA(a,b,c^d^e,w8,k2);c=(c>>>2)|(c<<30);
+		w9=R1(w6^w1^wB^w9);e=AA(e,a,b^c^d,w9,k2);b=(b>>>2)|(b<<30);
+		wA=R1(w7^w2^wC^wA);d=AA(d,e,a^b^c,wA,k2);a=(a>>>2)|(a<<30);
+		wB=R1(w8^w3^wD^wB);c=AA(c,d,e^a^b,wB,k2);e=(e>>>2)|(e<<30);
+		wC=R1(w9^w4^wE^wC);b=AA(b,c,d^e^a,wC,k2);d=(d>>>2)|(d<<30);
+		wD=R1(wA^w5^wF^wD);a=AA(a,b,c^d^e,wD,k2);c=(c>>>2)|(c<<30);
+		wE=R1(wB^w6^w0^wE);e=AA(e,a,b^c^d,wE,k2);b=(b>>>2)|(b<<30);
+		wF=R1(wC^w7^w1^wF);d=AA(d,e,a^b^c,wF,k2);a=(a>>>2)|(a<<30);
+		w0=R1(wD^w8^w2^w0);c=AA(c,d,e^a^b,w0,k2);e=(e>>>2)|(e<<30);
+		w1=R1(wE^w9^w3^w1);b=AA(b,c,d^e^a,w1,k2);d=(d>>>2)|(d<<30);
+		w2=R1(wF^wA^w4^w2);a=AA(a,b,c^d^e,w2,k2);c=(c>>>2)|(c<<30);
+		w3=R1(w0^wB^w5^w3);e=AA(e,a,b^c^d,w3,k2);b=(b>>>2)|(b<<30);
+		w4=R1(w1^wC^w6^w4);d=AA(d,e,a^b^c,w4,k2);a=(a>>>2)|(a<<30);
+		w5=R1(w2^wD^w7^w5);c=AA(c,d,e^a^b,w5,k2);e=(e>>>2)|(e<<30);
+		w6=R1(w3^wE^w8^w6);b=AA(b,c,d^e^a,w6,k2);d=(d>>>2)|(d<<30);
+		w7=R1(w4^wF^w9^w7);a=AA(a,b,c^d^e,w7,k2);c=(c>>>2)|(c<<30);
 
-		wC=r32(w9^w4^wE^wC);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,wC,k4);b=(b>>>2)|(b<<30);
-		wD=r32(wA^w5^wF^wD);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,wD,k4);a=(a>>>2)|(a<<30);
-		wE=r32(wB^w6^w0^wE);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,wE,k4);e=(e>>>2)|(e<<30);
-		wF=r32(wC^w7^w1^wF);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,wF,k4);d=(d>>>2)|(d<<30);
-		w0=r32(wD^w8^w2^w0);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,w0,k4);c=(c>>>2)|(c<<30);
-		w1=r32(wE^w9^w3^w1);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,w1,k4);b=(b>>>2)|(b<<30);
-		w2=r32(wF^wA^w4^w2);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,w2,k4);a=(a>>>2)|(a<<30);
-		w3=r32(w0^wB^w5^w3);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,w3,k4);e=(e>>>2)|(e<<30);
-		w4=r32(w1^wC^w6^w4);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,w4,k4);d=(d>>>2)|(d<<30);
-		w5=r32(w2^wD^w7^w5);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,w5,k4);c=(c>>>2)|(c<<30);
-		w6=r32(w3^wE^w8^w6);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,w6,k4);b=(b>>>2)|(b<<30);
-		w7=r32(w4^wF^w9^w7);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,w7,k4);a=(a>>>2)|(a<<30);
-		w8=r32(w5^w0^wA^w8);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,w8,k4);e=(e>>>2)|(e<<30);
-		w9=r32(w6^w1^wB^w9);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,w9,k4);d=(d>>>2)|(d<<30);
-		wA=r32(w7^w2^wC^wA);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,wA,k4);c=(c>>>2)|(c<<30);
-		wB=r32(w8^w3^wD^wB);e=a32x5(e,(a>>>27)|(a<<5),b^c^d,wB,k4);b=(b>>>2)|(b<<30);
-		wC=r32(w9^w4^wE^wC);d=a32x5(d,(e>>>27)|(e<<5),a^b^c,wC,k4);a=(a>>>2)|(a<<30);
-		wD=r32(wA^w5^wF^wD);c=a32x5(c,(d>>>27)|(d<<5),e^a^b,wD,k4);e=(e>>>2)|(e<<30);
-		wE=r32(wB^w6^w0^wE);b=a32x5(b,(c>>>27)|(c<<5),d^e^a,wE,k4);d=(d>>>2)|(d<<30);
-		wF=r32(wC^w7^w1^wF);a=a32x5(a,(b>>>27)|(b<<5),c^d^e,wF,k4);c=(c>>>2)|(c<<30);
+		w8=R1(w5^w0^wA^w8);e=AA(e,a,(b&c)|(d&(b|c)),w8,k3);b=(b>>>2)|(b<<30);
+		w9=R1(w6^w1^wB^w9);d=AA(d,e,(a&b)|(c&(a|b)),w9,k3);a=(a>>>2)|(a<<30);
+		wA=R1(w7^w2^wC^wA);c=AA(c,d,(e&a)|(b&(e|a)),wA,k3);e=(e>>>2)|(e<<30);
+		wB=R1(w8^w3^wD^wB);b=AA(b,c,(d&e)|(a&(d|e)),wB,k3);d=(d>>>2)|(d<<30);
+		wC=R1(w9^w4^wE^wC);a=AA(a,b,(c&d)|(e&(c|d)),wC,k3);c=(c>>>2)|(c<<30);
+		wD=R1(wA^w5^wF^wD);e=AA(e,a,(b&c)|(d&(b|c)),wD,k3);b=(b>>>2)|(b<<30);
+		wE=R1(wB^w6^w0^wE);d=AA(d,e,(a&b)|(c&(a|b)),wE,k3);a=(a>>>2)|(a<<30);
+		wF=R1(wC^w7^w1^wF);c=AA(c,d,(e&a)|(b&(e|a)),wF,k3);e=(e>>>2)|(e<<30);
+		w0=R1(wD^w8^w2^w0);b=AA(b,c,(d&e)|(a&(d|e)),w0,k3);d=(d>>>2)|(d<<30);
+		w1=R1(wE^w9^w3^w1);a=AA(a,b,(c&d)|(e&(c|d)),w1,k3);c=(c>>>2)|(c<<30);
+		w2=R1(wF^wA^w4^w2);e=AA(e,a,(b&c)|(d&(b|c)),w2,k3);b=(b>>>2)|(b<<30);
+		w3=R1(w0^wB^w5^w3);d=AA(d,e,(a&b)|(c&(a|b)),w3,k3);a=(a>>>2)|(a<<30);
+		w4=R1(w1^wC^w6^w4);c=AA(c,d,(e&a)|(b&(e|a)),w4,k3);e=(e>>>2)|(e<<30);
+		w5=R1(w2^wD^w7^w5);b=AA(b,c,(d&e)|(a&(d|e)),w5,k3);d=(d>>>2)|(d<<30);
+		w6=R1(w3^wE^w8^w6);a=AA(a,b,(c&d)|(e&(c|d)),w6,k3);c=(c>>>2)|(c<<30);
+		w7=R1(w4^wF^w9^w7);e=AA(e,a,(b&c)|(d&(b|c)),w7,k3);b=(b>>>2)|(b<<30);
+		w8=R1(w5^w0^wA^w8);d=AA(d,e,(a&b)|(c&(a|b)),w8,k3);a=(a>>>2)|(a<<30);
+		w9=R1(w6^w1^wB^w9);c=AA(c,d,(e&a)|(b&(e|a)),w9,k3);e=(e>>>2)|(e<<30);
+		wA=R1(w7^w2^wC^wA);b=AA(b,c,(d&e)|(a&(d|e)),wA,k3);d=(d>>>2)|(d<<30);
+		wB=R1(w8^w3^wD^wB);a=AA(a,b,(c&d)|(e&(c|d)),wB,k3);c=(c>>>2)|(c<<30);
+
+		wC=R1(w9^w4^wE^wC);e=AA(e,a,b^c^d,wC,k4);b=(b>>>2)|(b<<30);
+		wD=R1(wA^w5^wF^wD);d=AA(d,e,a^b^c,wD,k4);a=(a>>>2)|(a<<30);
+		wE=R1(wB^w6^w0^wE);c=AA(c,d,e^a^b,wE,k4);e=(e>>>2)|(e<<30);
+		wF=R1(wC^w7^w1^wF);b=AA(b,c,d^e^a,wF,k4);d=(d>>>2)|(d<<30);
+		w0=R1(wD^w8^w2^w0);a=AA(a,b,c^d^e,w0,k4);c=(c>>>2)|(c<<30);
+		w1=R1(wE^w9^w3^w1);e=AA(e,a,b^c^d,w1,k4);b=(b>>>2)|(b<<30);
+		w2=R1(wF^wA^w4^w2);d=AA(d,e,a^b^c,w2,k4);a=(a>>>2)|(a<<30);
+		w3=R1(w0^wB^w5^w3);c=AA(c,d,e^a^b,w3,k4);e=(e>>>2)|(e<<30);
+		w4=R1(w1^wC^w6^w4);b=AA(b,c,d^e^a,w4,k4);d=(d>>>2)|(d<<30);
+		w5=R1(w2^wD^w7^w5);a=AA(a,b,c^d^e,w5,k4);c=(c>>>2)|(c<<30);
+		w6=R1(w3^wE^w8^w6);e=AA(e,a,b^c^d,w6,k4);b=(b>>>2)|(b<<30);
+		w7=R1(w4^wF^w9^w7);d=AA(d,e,a^b^c,w7,k4);a=(a>>>2)|(a<<30);
+		w8=R1(w5^w0^wA^w8);c=AA(c,d,e^a^b,w8,k4);e=(e>>>2)|(e<<30);
+		w9=R1(w6^w1^wB^w9);b=AA(b,c,d^e^a,w9,k4);d=(d>>>2)|(d<<30);
+		wA=R1(w7^w2^wC^wA);a=AA(a,b,c^d^e,wA,k4);c=(c>>>2)|(c<<30);
+		wB=R1(w8^w3^wD^wB);e=AA(e,a,b^c^d,wB,k4);b=(b>>>2)|(b<<30);
+		wC=R1(w9^w4^wE^wC);d=AA(d,e,a^b^c,wC,k4);a=(a>>>2)|(a<<30);
+		wD=R1(wA^w5^wF^wD);c=AA(c,d,e^a^b,wD,k4);e=(e>>>2)|(e<<30);
+		wE=R1(wB^w6^w0^wE);b=AA(b,c,d^e^a,wE,k4);d=(d>>>2)|(d<<30);
+		wF=R1(wC^w7^w1^wF);a=AA(a,b,c^d^e,wF,k4);c=(c>>>2)|(c<<30);
 
 		h0=a32(h0,a);
 		h1=a32(h1,b);
 		h2=a32(h2,c);
 		h3=a32(h3,d);
 		h4=a32(h4,e);
-		}
-	return Array(h0,h1,h2,h3,h4);
+	}
+	return [h0,h1,h2,h3,h4];
 };
 
 } // end of "install only once"
