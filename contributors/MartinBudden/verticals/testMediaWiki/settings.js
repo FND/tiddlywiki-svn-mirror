daa2dce22e9d333a00c8c9547942ab0267aaeeb4
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-02-25 18:59:47 +0000

    Initial submission
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3631 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/testMediaWiki/settings.js
@@ -0,0 +1,26 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.2.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = "backup";
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+
+config.maxTiddlerImportCount = 10;
+
+merge(config.defaultCustomFields,{
+	wikiformat:'mediawiki'
+});
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+
+//#config.displayStartupTime = true;
+/*}}}*/
