85a8698337421deb12176a518d2362e8e07f6201
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-02-26 12:57:16 +0000

    RippleRap: generic version, added settings.js
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3641 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/ripplerap/settings.js
@@ -0,0 +1,12 @@
+//{{{
+
+config.options.txtMainTab = "AgendaDay1";
+config.options.txtMojoTelno = "+3368...";
+config.options.chkAutoSave = true;
+config.options.chkAnimate = true;
+config.options.chkSaveBackups = false;
+
+config.options.chkRipplerapShare = true;
+config.options.chkRipplerapReadyToUse = false;
+
+//}}}
