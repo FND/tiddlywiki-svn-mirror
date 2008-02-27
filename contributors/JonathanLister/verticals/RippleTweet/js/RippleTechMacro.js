39bd3a6f616f781dd670cec36f6129e9b352d73c
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-02-27 16:16:42 +0000

    removing console.log calls
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3683 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/verticals/RippleTweet/js/RippleTechMacro.js
+++ contributors/JonathanLister/verticals/RippleTweet/js/RippleTechMacro.js
@@ -7,8 +7,7 @@ config.macros.RippleTech.handler = function(place,macroName,params,wikifier,para
 		config.macros.Microblog.settings(platform);
 
 		var uri = microblogs[platform].ListenURI;
-		console.log("uri " + uri);
-		var count = params[2];
+				var count = params[2];
 		var avatars = params[3] =='avatars' ? true : false;
 		var makeTiddlers = params[4] =='makeTiddlers' ? true : false;
 		var context = {
@@ -20,10 +19,7 @@ config.macros.RippleTech.handler = function(place,macroName,params,wikifier,para
 				makeTiddlers:makeTiddlers,
 				rssUseRawDescription:true
 				};
-				
-		console.log(context);
-		console.log(microblogs[platform]);
-	
+					
 		var adaptor = new RSSAdaptor();
 		var ret = adaptor.openHost(context.host,context,params,config.macros.RippleTech.onOpenHost);
 		if (typeof(ret) == "string") {
@@ -37,8 +33,7 @@ config.macros.RippleTech.handler = function(place,macroName,params,wikifier,para
 			
 config.macros.RippleTech.onOpenHost = function(context,params) {
 
-	console.log("in onOpenHost");
-	var ret = context.adaptor.getTiddlerList(context,params,config.macros.RippleTech.convertItemsToJSON);
+		var ret = context.adaptor.getTiddlerList(context,params,config.macros.RippleTech.convertItemsToJSON);
 	if (typeof(ret) == "string") {
 		displayMessage("problem opening tiddlers: " + ret);
 		return false;
@@ -51,15 +46,12 @@ config.macros.RippleTech.onOpenHost = function(context,params) {
 config.macros.RippleTech.convertItemsToJSON = function(context,params) {
 // Requirement: convert Tiddlers to the minimal twitter format needed:
 // [{created_at:modified,text:text,user:{name:modifier}},...]
-	console.log("in convertItemsToJSON");
-	var items = context.tiddlers;
-	console.log(items);
-	var JSONarray = [];
+		var items = context.tiddlers;
+		var JSONarray = [];
 	for (var i in items) {
 		var item = items[i];
 		if (item instanceof Tiddler) {
-			console.log(item);
-			var itemObject = {};
+						var itemObject = {};
 			itemObject.created_at = item.created.formatString("ddd, DD MMM YYYY 0hh:0hh:0ss TZD");
 			itemObject.text = item.text;
 			itemObject.user = {};
@@ -73,6 +65,5 @@ config.macros.RippleTech.convertItemsToJSON = function(context,params) {
 		}
 	}
 	var JSONstring = JSONarray.toJSONString();
-	console.log("going into microblog land");
-	config.macros.Microblog.listenHandler(true,context,JSONstring,context.host,null);
+		config.macros.Microblog.listenHandler(true,context,JSONstring,context.host,null);
 };
