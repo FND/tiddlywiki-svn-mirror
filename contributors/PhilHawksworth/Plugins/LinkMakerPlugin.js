1faf9be24bc6d0f18d328a9256b86ee5b6894d7d
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-02-29 13:56:01 +0000

    extended link maker to support templates
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3745 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/Plugins/LinkMakerPlugin.js
+++ contributors/PhilHawksworth/Plugins/LinkMakerPlugin.js
@@ -13,6 +13,8 @@
 Usage:
 <<LinkMaker DisplayedText TargetTiddler [className]>>
 
+<<LinkMaker type:'text|image|img' display:'tiddler field|image path' linkto:'href|TiddlerTitle' [title:'title'] [alt:'alt'] [classname:'classname'] >>
+
 ***/
 
 //{{{
@@ -21,14 +23,53 @@ version.extensions.LinkMakerPlugin = {installed:true};
 	
 	config.macros.LinkMaker = {};
 	config.macros.LinkMaker.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-		if(params.length < 2) {
-			return;
+		
+		params = paramString.parseParams("anon",null,true,false,false);
+		var linktype = getParam(params,"type",'text');
+		var linkto = store.getValue(tiddler,getParam(params,"linkto",""));
+		// var title = getParam(params,"title","");
+		// var alt = getParam(params,"alt","");
+		var display =  store.getValue(tiddler,getParam(params,"display",""));
+		var classname = getParam(params,"classname",null);
+		var dateformat = getParam(params,'dateformat',null);
+
+		if(dateformat) {
+			var handler = config.macros.view.views['date'];
+			var fakeParams = [null,null,dateformat];
 		}
-		var text = store.getValue(tiddler,params[0]);
-		var title = store.getValue(tiddler,params[1]);
-		var className = params[2] ? store.getValue(tiddler,params[2]) : null;
-		var e = createTiddlyLink(place,title,false,className);
-		createTiddlyText(e,text);
+		
+		
+		
+		if(linktype == 'image' || linktype ==  'img') {
+			var a = createTiddlyElement(place,'a',null,classname);
+			// a.setAttribute('title',title);
+			// a.setAttribute('alt',alt);
+			a.setAttribute('href',linkto);
+			var i = createTiddlyElement(a,'img');
+			i.setAttribute('src',display);
+		}
+		else if (config.formatterHelpers.isExternalLink(linkto)) {
+			var a = createTiddlyElement(place,'a',null,classname,null);
+			if (handler)
+				handler(display,a,fakeParams);
+			else 
+				createTiddlyText(a,display);
+			// a.setAttribute('title',title);
+			// a.setAttribute('alt',alt);
+			a.setAttribute('href',linkto);
+		}
+		else {			
+			var e = createTiddlyLink(place,linkto,false,classname);
+			if (handler)
+				handler(display,e,fakeParams);
+			else 
+				createTiddlyText(e,display);
+		}
+
+
+
+
+
 	};
 
 } //# end of 'install only once'
