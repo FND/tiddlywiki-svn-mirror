01cf4297bf59d5cf2f5c5901f5e6f31303688976
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-03 10:01:17 +0000

    SpeedGeeking plugins
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3810 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/Plugins/SimpleMojoPlugin.js
@@ -0,0 +1,104 @@
+/***
+|''Name:''|SimpleMojoPlugin|
+|''Description:''|Click to call/SMS Telnos using BT Mojo|
+|''Version:''|1.0.0|
+|''Date:''|Nov 21, 2007|
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/Plugins/SimpleMojoPlugin.js |
+|''Version:''|0.1|
+|''License:''|[[BSD open source license]]|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.2|
+
+uses browser session cookie, so you need to sign-in to  http://mojo.bt.com
+from another tab/window
+
+***/
+
+//{{{
+// Ensure that the SimpleMojoPlugin Plugin is only installed once.
+if(!version.extensions.SimpleMojoPlugin) {
+version.extensions.SimpleMojoPlugin = {installed:true};
+
+config.macros.mojo = {
+
+	debug: false,
+	offline: false,
+
+	makeCall: function (callee) {
+
+	        var macro = config.macros.mojo;
+	
+	        var user = config.options.txtMojoUserName;
+	        var caller = config.options.txtMojoTelno;
+	
+		    var body = encodeURIComponent('call[username]') + '=' + user 
+				+ '&' + encodeURIComponent('call[gadgetkey]') + '=' + '1234567891' 
+				+ '&' + encodeURIComponent('call[caller]') + '=' + encodeURIComponent(caller)
+				+ '&' + encodeURIComponent('call[callee]') + '=' + encodeURIComponent(callee); 
+				 
+		if(macro.debug)
+			displayMessage('call info:' + body );
+
+		var callback = function(status,params,responseText,url,xhr) {
+			displayMessage(status?'call made':'call failed');
+			if(config.macros.mojo.debug)
+				displayMessage(responseText);
+		};
+
+		if(!macro.offline) {
+			var req = doHttp('POST', 'http://mojo.bt.com/calls', body, null, null,null, callback);
+			if(macro.debug)
+				displayMessage(req);
+		}
+	},
+	
+	
+	sendSMS: function (recipient, text) {
+
+	        var macro = config.macros.mojo;
+	
+	        var user = config.options.txtMojoUserName;
+	
+		    var body = encodeURIComponent('message[username]') + '=' + user 
+				+ '&' + encodeURIComponent('message[gadgetkey]') + '=' + '1234567891' 
+				+ '&' + encodeURIComponent('message[recipient]') + '=' + encodeURIComponent(recipient)
+				+ '&' + encodeURIComponent('message[text]') + '=' + encodeURIComponent(text); 
+				 
+		if(macro.debug)
+			displayMessage('message info:' + body );
+
+		var callback = function(status,params,responseText,url,xhr) {
+			displayMessage(status?'message sent':'message failed');
+			if(config.macros.mojo.debug)
+				displayMessage(responseText);
+		};
+
+		if(!macro.offline) {
+			var req = doHttp('POST', 'http://mojo.bt.com/messages', body, null, null,null, callback);
+			if(macro.debug)
+				displayMessage(req);
+		}
+	},
+	
+	getElementByClass:  function (node,className) {
+		var re = new RegExp('\\b'+className+'\\b');
+		var element = node.getElementsByTagName('*');
+		for(var i = 0; i < element.length; i++) {
+			var classes = element[i].className;
+			if(re.test(classes)) return element[i];
+		}
+		return 0;
+	},
+	
+	onClickDial: function (e) {
+	        var macro = config.macros.mojo;
+	        var caller = macro.telno;
+	        var callee = macro.getElementByClass(this.previousSibling,'value').firstChild.data;
+	    macro.makeCall(caller, callee);
+	}
+
+}
+
+} //# end of "install only once"
+//}}}
---
+++ contributors/PaulDowney/Plugins/SpeedGeekingPlugin.js
@@ -0,0 +1,108 @@
+/***
+|''Name:''|SpeedGeekingPlugin|
+|''Description:''|Shuffle, pair-up, and ring numbers found in contact tiddlers|
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/Plugins/SpeedGeekingPlugin.js |
+|''Version:''|1.0.0|
+|''License:''|[[BSD open source license]]|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''CoreVersion:''|2.2|
+
+a contact tiddler matches the given tagged and has the slice 'Mobile', eg.:
+
+
+- uses SimpleMojoPlugin to place phone calls
+
+***/
+
+//{{{
+// Ensure that this Plugin is only installed once.
+if(!version.extensions.SpeedGeekingPlugin) {
+version.extensions.SpeedGeekingPlugin = {installed:true};
+
+config.macros.speedGeekDial = {
+
+	handler: function (place,macroName,params,wikifier,paramString,tiddler) {
+		params = paramString.parseParams('tag',null,true,false,false);
+		var tag = getParam(params,'tag',null);
+	        var button = createTiddlyButton(place,'RING!','Click here to make these calls!',this.onClickDail);
+		button.setAttribute("tiddlerName",tiddler.title);
+	},
+
+	onClickDail: function (e) {
+		var macro = config.macros.speedGeekDial;
+		var pairs = macro.getPairs(this);
+
+		for(var n=0;n<pairs.length;n++) {
+			var callingParty = store.getTiddlerSlice(pairs[n][0],"Mobile");
+			var calledParty = store.getTiddlerSlice(pairs[n][1],"Mobile");
+			config.macros.mojo.makeCall(callingParty, calledParty);
+		}
+        },
+
+	getPairs: function (button) {
+		title = button.getAttribute('tiddlerName');
+		text = store.getTiddlerText(title);
+		lines = text.split("|\n");
+		pairs = [];
+		for(var n=0;n<lines.length;n++) {
+			var parts = lines[n].split("|");
+			if (parts[2]) {
+				pairs.push([parts[1],parts[2]]);
+			}
+		}
+		return pairs;
+	}
+},
+
+config.macros.speedGeek = {
+
+	round: 0,
+	tag: 'geek',
+
+	handler: function (place,macroName,params,wikifier,paramString,tiddler) {
+		params = paramString.parseParams('tag',null,true,false,false);
+		var tag = getParam(params,'tag',null);
+		var limit = params[2].value;
+
+	        var button = createTiddlyButton(place,'ROUND!','Click here to pair up tiddlers tagged with \''+tag+'\'',this.onClickDeal);
+		button.setAttribute("limit",limit);
+	},
+
+	onClickDeal: function (e) {
+	        var macro = config.macros.speedGeek;
+		macro.round++;
+
+		var tag = config.macros.speedGeek.tag;
+		var title = "SpeedGeekRound" + macro.round;
+		var tiddlers = macro.shuffle(store.getTaggedTiddlers(tag));
+
+		var body = '<<speedGeekDial '+tag+'>>\n';
+
+		var limit = this.getAttribute('limit')*2;
+		if (limit > tiddlers.length) { limit = tiddlers.length; }
+
+		for(var n=0;n<limit;n+=2) {
+			body = body + '|' + tiddlers[n].title + '|' + (tiddlers[n+1]?tiddlers[n+1].title : "") + '|\n'
+		}
+
+		tiddler = store.saveTiddler(title,title,body,config.options.txtUserName);
+		story.displayTiddlers(this,[title]);
+	},
+
+	shuffle: function(list) {
+		var i = list.length;
+		if (i == 0) return false;
+		while (--i) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var ti = list[i];
+			var tj = list[j];
+			list[i] = tj;
+			list[j] = ti;
+		}
+		return list;
+	}
+
+}
+} //# end of "install only once"
+//}}}
