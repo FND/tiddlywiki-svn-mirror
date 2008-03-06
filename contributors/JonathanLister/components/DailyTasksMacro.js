9a3185b8bce0e4dfc383f455a27116ded65c75c9
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-06 12:54:57 +0000

    DailyTasks: second commit, code written, example vertical created, not entirely debugged. Error in config.macros.dailyTasks.handler, last line (as commented)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3845 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/components/DailyTasksMacro.js
+++ contributors/JonathanLister/components/DailyTasksMacro.js
@@ -18,25 +18,64 @@ Getting all tiddler slices will return only those tasks have not been marked up 
 ***/
 
 config.macros.dailyTasks = {
-	titlePrefix = 'DailyTasks_',
-	sliceName = 'task'
-	tag = 'dailytask'
+	titlePrefix: 'DailyTasks_',
+	sliceName: 'task',
+	tag: 'dailytask'
 };
 
 config.macros.dailyTasks.getDailyTasks = function() {
 	var taskTiddlers = store.getTaggedTiddlers(this.tag);
 	var tasks = [];
 	for (var i=0; i<taskTiddlers.length; i++) {
-		var taskTiddlerSlices = store.calcAllSlices(taskTiddlers[i]);
+		var taskTiddlerSlices = store.calcAllSlices(taskTiddlers[i].title);
 		tasks.push(taskTiddlerSlices[this.sliceName]);
 	}
 	return tasks;
 };
 
 config.macros.dailyTasks.createDailyTaskTiddler = function(date) {
+	var day = date.formatString("DDD");
+	var title = this.titlePrefix + day;
+	if(!store.tiddlerExists(title)) {
+		var t = new Tiddler(title);
+		var tasks = this.getDailyTasks();
+		var body = createTiddlyElement(null,"span");
+		for (var i=0; i<tasks.length; i++) {
+			var task = tasks[i];
+			createTiddlyText(createTiddlyElement(body,"span"),task+": ");
+			createTiddlyCheckbox(body,null,null,this.completeTask);
+		}
+		t.text = "<html>"+body.innerHTML+"</html>";
+		console.log(t.text);
+		store.saveTiddler(t.title,t.title,t.text,config.options.txtUserName);
+	}
+	return title;
+};
 
+config.macros.dailyTasks.completeTask = function(ev) {
+	var e = ev ? ev : window.event;
+	var target = resolveTarget(e);
+	if(target.checked)
+		target.checked = false;
+	else
+		target.checked = true;
+	// this now refers to the clicked element
+	var tiddlerElem = story.findContainingTiddler(this);
+	var title = tiddlerElem.getAttribute("tiddler");
+	var textSpan = this.previousSibling;
+	var taskCompleted = textSpan.textContent;
+	// change the tiddler text and refresh the tiddler
+	var t = store.getTiddler(title);
+	var regexString = taskCompleted;
+	var taskRegex = new Regex(regexString,"m");
+	t.text.replace(taskRegex,"---$1---");
+	store.saveTiddler(t.title,t.title,t.text,config.options.txtUserName,new Date(),t.tags,t.fields,true,t.created);
+	story.refreshTiddler(title);
 };
 
 config.macros.dailyTasks.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	
+	var today = new Date();
+	var todayTiddler = this.createDailyTaskTiddler(today);
+	// THIS LINE ISN'T WORKING
+	story.displayTiddler(todayTiddler);
 };
