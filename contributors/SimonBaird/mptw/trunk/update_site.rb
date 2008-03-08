281513bd2605044bf914799b0fe4cf822b4e84cb
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-08 00:56:45 +0000

    mptw: fixed update_site script
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3863 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/update_site.rb
+++ contributors/SimonBaird/mptw/trunk/update_site.rb
@@ -2,9 +2,9 @@
 require 'r4tw'
 
 make_tw {
-  source_file            'http://mopi.tiddlyspot.com/'
+  source_file            'http://mptw.tiddlyspot.com/'
   copy_all_tiddlers_from 'upload/upgrade.html'
-  tiddlers.each { |t| t.fields['modifier'] = 'Mopi' }
+  tiddlers.each { |t| t.fields['modifier'] = 'MPTW' }
   to_file                'upload/index.html'
 }
 
