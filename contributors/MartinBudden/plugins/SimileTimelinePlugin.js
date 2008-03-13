10d40dac21ee212b995c4a7f1d8b5f2dd6674f61
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-13 17:30:45 +0000

    Minor update
    git-svn-id: http://svn.tiddlywiki.org/Trunk@3942 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/plugins/SimileTimelinePlugin.js
+++ contributors/MartinBudden/plugins/SimileTimelinePlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden ( mjbudden [at] gmail [dot] com)|
 |''Source:''|http://www.martinswiki.com/#SimileTimelineBundlePlugin |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/SimileTimelinePlugin.js |
-|''Version:''|0.1.4|
+|''Version:''|0.1.5|
 |''Date:''|Mar 4, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
@@ -255,9 +255,6 @@ Timeline.loadJSONFile = function(filePath,f) {
 	}
 };
 
-
-
-
 Tiddler.prototype.getSimileTimelineEvent = function(type,eventFields)
 {
 //#displayMessage('getEvent:'+this.title);
@@ -402,6 +399,7 @@ config.macros.SimileTimeline.handler = function(place,macroName,params,wikifier,
 			}
 		}
 	}
+	eventSource.addMany([]);
 };
 
 config.macros.SimileTimeline.tick = function()
