5aa0fb2acd2bf258310662974a2ce9829b2c9218
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-20 20:04:13 +0000

    Minor updates
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4086 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/verticals/testGeneral/testSaving.js
+++ contributors/MartinBudden/verticals/testGeneral/testSaving.js
@@ -3,7 +3,7 @@
 |''Description:''|tests saving using templates|
 |''Author:''|Martin Budden ( mjbudden [at] gmail [dot] com)|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/verticals/testGeneral/testSaving.js |
-|''Version:''|0.0.4|
+|''Version:''|0.0.6|
 |''Date:''|Mar 15, 2008|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]] |
@@ -18,8 +18,7 @@ config.templateFormatters = [
 {
 	name: "templateElement",
 	match: '<!--(?:<<|@@)',
-	//lookaheadRegExp:   /<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>/mg,
-	lookaheadRegExp: /<!--<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>-->|<!--@@([a-z]*)@@-->\n/mg,
+	lookaheadRegExp: /<!--<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>-->|<!--@@([a-z]*)@@-->/mg,
 	handler: function(w)
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
@@ -34,43 +33,46 @@ config.templateFormatters = [
 			var text = '';
 			switch(match) {
 			case 'version':
-				//# drop through
 			case 'js':
-				text = document.getElementById(match+"Area").innerHTML.replace(/^\s*\/\/<!\[CDATA\[\s*|\s*\/\/\]\]>\s*$/g,"");
+				var e = document.getElementById(match+'Area')
+				if(e)
+					text = e.innerHTML.replace(/^\s*\/\/<!\[CDATA\[\s*|\s*\/\/\]\]>\s*$/g,'');
 				break;
 			case 'title':
 				text = getPageTitle();
 				break;
+			case 'copyright':
+				e = document.getElementsByName('copyright');
+				if(e)
+					text = e[0].content;
+				break;
 			case 'shadow':
 				var saver = new TW21Saver();
 				var shadows = new TiddlyWiki();
-				shadows.loadFromDiv("shadowArea","shadows",true);
+				shadows.loadFromDiv('shadowArea','shadows',true);
 				shadows.forEachTiddler(function(title,tiddler) {
 					tiddler.created = tiddler.modified = version.date;
-					text += saver.externalizeTiddler(store,tiddler) + "\n";
+					text += saver.externalizeTiddler(store,tiddler) + '\n';
 				});
 				delete shadows;
 				break;
 			case 'tiddler':
-				if(!w.tiddler.isTagged("empty"))
+				if(!w.tiddler.isTagged('empty'))
 					text = convertUnicodeToUTF8(store.allTiddlersAsHtml());
 				break;
 			case 'prehead':
-				//var = s = "Markup"+{prehead:"PreHead",posthead:"PostHead",prebody:"PreBody",postscript:"PostBody"}[lookaheadMatch[1]];
-				text = store.getRecursiveTiddlerText("MarkupPreHead");
-				break;
 			case 'posthead':
-				text = store.getRecursiveTiddlerText("MarkupPostHead");
-				break;
 			case 'prebody':
-				text = store.getRecursiveTiddlerText("MarkupPreBody");
-				break;
 			case 'postscript':
-				text = store.getRecursiveTiddlerText("MarkupPostBody");
+				var s = {prehead:'PreHead',posthead:'PostHead',prebody:'PreBody',postscript:'PostBody'};
+				text = store.getRecursiveTiddlerText('Markup'+s[match]);
 				break;
 			}
 			if(text) {
-				createTiddlyText(w.output,text+"\n");
+				createTiddlyText(w.output,text);
+			} else {
+				if(w.source.substr(w.nextMatch,1)=='\n')
+					w.nextMatch++;
 			}
 		}
 	}
@@ -81,75 +83,12 @@ config.parsers.templateFormatter = new Formatter(config.templateFormatters);
 config.parsers.templateFormatter.format = 'template';
 config.parsers.templateFormatter.formatTag = 'TemplateFormat';
 
-expandTemplate2 = function(template,filter,data,tiddler)
-{
-	var tempTiddlerName = "temp";
-	var defaultTemplate = "<<view text>>";
-	template = template ? store.getTiddlerText(template,defaultTemplate) : defaultTemplate;
-	var tiddlers = [];
-	// METHOD4 - 24/1/08 - using these calls:
-	// <<ListTemplate filter:"[tag[docs]]" template:"RssItemTemplate">>
-	// <<ListTemplate data:"tags" template:"RssItemCategoryTemplate">>
-	// <<view text>> (for the tags)
-	// This method for passing data through to subTemplates works by creating "pseudo-tiddlers" (Tiddler objects not in the store) that each carry a part of the data array we want iterating through. We do this to keep the unit of data as the tiddler.
-	
-	if(filter) {
-		tiddlers = store.filterTiddlers(filter);
-	} else if(data) {
-		switch(data) {
-		case "tags":
-			// creates a new tiddler for each tag and has that tag as its tags
-			for(var i=0;i<tiddler.tags.length;i++) {
-				var t = new Tiddler(tiddler.title);
-				t.tags = new Array(tiddler.tags[i]);
-				tiddlers.push(t);
-			}
-			break;
-		default:
-			tiddlers.push(tiddler);
-			break;
-		}
-	} else {
-		// no data provided, so inherit
-		tiddlers.push(tiddler ? tiddler : new Tiddler(tempTiddlerName));
-	}
-	var output = "";
-	for(i=0; i<tiddlers.length; i++) {
-		output += wikifyStatic(template,null,tiddlers[i],'template').htmlDecode();
-	}
-	return output;	
-};
-
-config.macros.listTemplate = {};
-config.macros.listTemplate.handler = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-console.log('ll:'+paramString);
-console.log(params);
-	p = paramString.parseParams("anon",null,true,false,false);
-console.log(params);
-	var template = getParam(p,"template",null);
-console.log('t1:'+template);
-	if(!template)
-		template =params[0];
-console.log('t2:'+template);
-	var filter = getParam(p,"filter",null);
-	var data = getParam(p,"data",null);
-	createTiddlyText(place,expandTemplate2(template,filter,data,tiddler));
-};
-
-config.macros.permalink = {};
-config.macros.permalink.handler = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-	var t = encodeURIComponent(String.encodeTiddlyLink(tiddler.title));
-	createTiddlyText(place,window.location+"#"+t);
-};
-
-expandTemplate = function(template,format,tags)
+expandTemplate2 = function(template,format,tags)
 {
 	var tiddler = new Tiddler("temp");
 	if(tags)
 		tiddler.tags = (typeof tags == "string") ? tags.readBracketedList() : tags;
-	return wikifyStatic(store.getTiddlerText(template),null,tiddler,format ? format : 'template').htmlDecode();
+	return wikifyStatic(store.getTiddlerText(template),null,tiddler,format ? format : "template").htmlDecode();
 };
 
 function saveChanges(onlyIfDirty,tiddlers)
@@ -157,7 +96,7 @@ function saveChanges(onlyIfDirty,tiddlers)
 	if(onlyIfDirty && !store.isDirty())
 		return;
 	clearMessage();
-	var t1,t0 = new Date();
+	var t0 = new Date();
 	//# Get the URL of the document
 	var originalPath = document.location.toString();
 	//# Check we were loaded from a file URL
@@ -173,7 +112,7 @@ function saveChanges(onlyIfDirty,tiddlers)
 	saveEmpty(localPath);
 	saveMain(localPath);
 	if(config.options.chkDisplayInstrumentation)
-		displayMessage("saveChanges " + (t1-t0) + " ms");
+		displayMessage("saveChanges " + (new Date()-t0) + " ms");
 }
 
 function saveBackup(localPath)
@@ -210,7 +149,7 @@ function saveEmpty(localPath)
 		else
 			emptyPath = localPath + ".";
 		emptyPath += "empty.html";
-		var empty = expandTemplate("TiddlyWikiTemplate",null,"empty");
+		var empty = expandTemplate2("TiddlyWikiTemplate",null,"empty");
 		if(saveFile(emptyPath,empty))
 			displayMessage(config.messages.emptySaved,"file://" + emptyPath);
 		else
@@ -221,7 +160,7 @@ function saveEmpty(localPath)
 function saveMain(localPath)
 {
 	try {
-		var revised = expandTemplate("TiddlyWikiTemplate");
+		var revised = expandTemplate2("TiddlyWikiTemplate");
 		revised = updateLanguageAttribute(revised);
 		var save = saveFile(localPath,revised);
 	} catch (ex) {
@@ -249,31 +188,8 @@ function saveRss(localPath)
 
 function generateRss()
 {
-	return expandTemplate("RssTemplate");
+	return expandTemplate2("RssTemplate");
 }
-
-/*
-config.templateFormatters = [
-{
-	name: 'templateElement',
-	match: '<!--<<',
-	lookaheadRegExp: /<!--<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>-->/mg,
-	handler: function(w)
-	{
-		this.lookaheadRegExp.lastIndex = w.matchStart;
-		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
-		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
-			invokeMacro(w.output,lookaheadMatch[1],lookaheadMatch[2],w,w.tiddler);
-			w.nextMatch = this.lookaheadRegExp.lastIndex;
-		}
-	}
-}
-];
-
-config.parsers.templateFormatter = new Formatter(config.templateFormatters);
-config.parsers.templateFormatter.format = 'template';
-config.parsers.templateFormatter.formatTag = 'TemplateFormat';
-*/
 
 //} //# end of 'install only once'
 //}}}
