c687d1225f16a0c6e5ca6eedcc8a71af2fd62f20
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-20 18:16:48 +0000

    Split out ListTemplatePlugin into a separate file. Changed name of macro from ListTemplate to listTemplate. Made template: qualifier optional.
    Updated RSS templates accordingly.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4082 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JonathanLister/plugins/ListTemplatePlugin.js
@@ -0,0 +1,113 @@
+/***
+|''Name:''|ListTemplatePlugin|
+|''Description:''|Renders a set of tiddlers through a template|
+|''Author:''|JonathanLister (based on ListRelatedPlugin by JeremyRuston)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/JonathanLister/plugins/ListTemplatePlugin.js |
+|''Version:''|0.0.3|
+|''Date:''|Jan 21, 2008|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''~CoreVersion:''|2.3|
+
+The listTemplate macro finds a set of tiddlers and renders them once each through a template. The template can contain additional
+calls to the macro to allow for e.g. looping inside a template (think RSS items).
+
+It needs to be able to support recursion, so that sub-templates make sense. The content is passed in either through a filter for tiddlers,
+a tag to get tiddlers, or a space-separated list. We'll support more as we go along, but this is what we need for RSS.
+
+Usage:
+{{{
+<<listTemplate template:"RssTemplate" filter:"[tag[!excludeLists]]">>
+<<listTemplate RssTemplate filter:"[tag[!excludeLists]]">> // template qualification is optional
+}}}
+
+Parameters can be:
+template - the name of the template
+filter - a tiddler filter
+data - the part of a tiddler to use in the subTemplate
+raw - if set to true in a top-level template, renders the output as text suitable for saving to a file; otherwise, outputs as HTML into place
+
+If a parameter does not have a qualifier, it is assumed to be the template name
+***/
+
+//{{{
+if(!version.extensions.ListTemplatePlugin) {
+version.extensions.ListTemplatePlugin = {installed:true};
+expandTemplate = function(template,filter,data,tiddler)
+{
+	//	<<ListTemplate filter:"[tag[docs]]" template:"RssItemTemplate">>
+	//	<<ListTemplate data:"tags" template:"RssItemCategoryTemplate">>
+	//	<<view text>> (for the tags)
+	//	This method for passing data through to subTemplates works by creating "pseudo-tiddlers" (Tiddler objects not in the store)
+	//  that each carry a part of the data array we want iterating through. We do this to keep the unit of data as the tiddler.
+	var tempTiddlerName = "temp";
+	var defaultTemplate = "<<view text>>";
+	template = template ? store.getTiddlerText(template,defaultTemplate) : defaultTemplate;
+	var tiddlers = [];
+	if(filter) {
+		tiddlers = store.filterTiddlers(filter);
+	} else if(data) {
+		switch(data) {
+		case "tags":
+			// creates a new tiddler for each tag and has that tag as its tags
+			for(var i=0;i<tiddler.tags.length;i++) {
+				var t = new Tiddler(tiddler.title);
+				t.tags = new Array(tiddler.tags[i]);
+				tiddlers.push(t);
+			}
+			break;
+		default:
+			tiddlers.push(tiddler);
+			break;
+		}
+	} else {
+		// no data provided, so inherit
+		tiddlers.push(tiddler ? tiddler : new Tiddler(tempTiddlerName));
+	}
+	var output = "";
+	for(i=0; i<tiddlers.length; i++) {
+		output += wikifyStatic(template,null,tiddlers[i],'template').htmlDecode();
+		//displayMessage(">>>>>>>>>>>");
+		//displayMessage(output);
+		//displayMessage("<<<<<<<<");
+	}
+	return output;	
+};
+
+config.macros.listTemplate = {};
+config.macros.listTemplate.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	p = paramString.parseParams("anon",null,true,false,false);
+	var template = getParam(p,"template",null);
+	if(!template)
+		template =params[0];
+	var filter = getParam(p,"filter",null);
+	var data = getParam(p,"data",null);
+	var raw = getParam(p,"raw",null);
+	if(raw=="true")
+		createTiddlyText(place,expandTemplate(template,filter,data,tiddler));
+	else {
+		//displayMessage("---before expand---");
+		var output = expandTemplate(template,filter,data,tiddler);
+		//displayMessage("---after expand---");
+		//displayMessage("---innerHTML before---");
+		//displayMessage(place.innerHTML);
+		//displayMessage("---going to add this output---");
+		//displayMessage(output);
+		place.innerHTML += output;
+		//displayMessage("---innerHTML with output---");
+		//displayMessage(place.innerHTML);
+	}
+};
+
+config.macros.permalink = {};
+config.macros.permalink.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	var t = encodeURIComponent(String.encodeTiddlyLink(tiddler.title));
+	createTiddlyText(place,window.location+"#"+t);
+};
+
+} //# end of 'install only once'
+//}}}
+
+
--- contributors/JonathanLister/plugins/TiddlyTemplatingPlugin.js
+++ contributors/JonathanLister/plugins/TiddlyTemplatingPlugin.js
@@ -9,31 +9,6 @@
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''~CoreVersion:''|2.3|
 
-!Macros included in the plugin
-|''Name:''|ListTemplateMacro|
-|''Description:''|Renders a set of tiddlers through a template|
-|''Author:''|JonathanLister (based on ListRelatedPlugin by JeremyRuston)|
-|''CodeRepository:''| |
-|''Version:''|0.0.2|
-|''Date:''|Jan 21, 2008|
-|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
-|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
-|''~CoreVersion:''|2.3|
-
-ListTemplateMacro finds a set of tiddlers and renders them once each through a template. The template can contain additional calls to the macro to allow for e.g. looping inside a template (think RSS items).
-
-It needs to be able to support recursion, so that sub-templates make sense. The content is passed in either through a filter for tiddlers, a tag to get tiddlers, or a space-separated list. We'll support more as we go along, but this is what we need for RSS.
-
-Usage:
-{{{
-<<ListTemplate filter:"[tag[!excludeLists]]" template:"RssTemplate">>
-}}}
-
-Parameters can be:
-filter - a tiddler filter
-data - the part of a tiddler to use in the subTemplate
-raw - if set to true in a top-level template, renders the output as text suitable for saving to a file; otherwise, outputs as HTML into place
-
 |''Name:''|TiddlyTemplatingMacro|
 |''Description:''|Renders a template and saves the output to a local file|
 |''Author:''|JonathanLister|
@@ -65,72 +40,6 @@ Usage:
 if(!version.extensions.TiddlyTemplating) {
 version.extensions.TiddlyTemplating = {installed:true};
 // NOTE: FOR NOW, THIS PLUGIN ALSO OVERRIDES THE RSS SAVING AND PRODVIDES A MACRO CALLED TIDDLYTEMPLATING TO SAVE AN EXTERNAL FILE
-
-expandTemplate = function(template,filter,data,tiddler)
-{
-	var tempTiddlerName = "temp";
-	var defaultTemplate = "<<view text>>";
-	template = template ? store.getTiddlerText(template,defaultTemplate) : defaultTemplate;
-	var tiddlers = [];
-	/* METHOD4 - 24/1/08 - using these calls:
-		<<ListTemplate filter:"[tag[docs]]" template:"RssItemTemplate">>
-		<<ListTemplate data:"tags" template:"RssItemCategoryTemplate">>
-		<<view text>> (for the tags)
-		This method for passing data through to subTemplates works by creating "pseudo-tiddlers" (Tiddler objects not in the store) that each carry a part of the data array we want iterating through. We do this to keep the unit of data as the tiddler. */
-	if(filter) {
-		tiddlers = store.filterTiddlers(filter);
-	} else if(data) {
-		switch(data) {
-		case "tags":
-			// creates a new tiddler for each tag and has that tag as its tags
-			for(var i=0;i<tiddler.tags.length;i++) {
-				var t = new Tiddler(tiddler.title);
-				t.tags = new Array(tiddler.tags[i]);
-				tiddlers.push(t);
-			}
-			break;
-		default:
-			tiddlers.push(tiddler);
-			break;
-		}
-	} else {
-		// no data provided, so inherit
-		tiddlers.push(tiddler ? tiddler : new Tiddler(tempTiddlerName));
-	}
-	var output = "";
-	for(i=0; i<tiddlers.length; i++) {
-		output += wikifyStatic(template,null,tiddlers[i],'template').htmlDecode();
-		//displayMessage(">>>>>>>>>>>");
-		//displayMessage(output);
-		//displayMessage("<<<<<<<<");
-	}
-	return output;	
-};
-
-config.macros.ListTemplate = {};
-
-config.macros.ListTemplate.handler = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-	params = paramString.parseParams("anon",null,true,false,false);
-	var filter = getParam(params,"filter",null);
-	var template = getParam(params,"template",null);
-	var data = getParam(params,"data",null);
-	var raw = getParam(params,"raw",null);
-	if(raw=="true")
-		createTiddlyText(place,expandTemplate(template,filter,data,tiddler));
-	else {
-		//displayMessage("---before expand---");
-		var output = expandTemplate(template,filter,data,tiddler);
-		//displayMessage("---after expand---");
-		//displayMessage("---innerHTML before---");
-		//displayMessage(place.innerHTML);
-		//displayMessage("---going to add this output---");
-		//displayMessage(output);
-		place.innerHTML += output;
-		//displayMessage("---innerHTML with output---");
-		//displayMessage(place.innerHTML);
-	}
-};
 
 config.templateFormatters = [
 {
@@ -250,34 +159,8 @@ config.macros.TiddlyTemplating.handler = function(place,macroName,params,wikifie
 		alert(config.messages.fileFailed,"file://"+savePath);
 };
 
-config.macros.permalink = {};
-config.macros.permalink.handler = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-	var t = encodeURIComponent(String.encodeTiddlyLink(tiddler.title));
-	createTiddlyText(place,window.location+"#"+t);
-};
-
 } //# end of 'install only once'
 //}}}
-
-/*** NB: Rest of this code is to be moved to a saving plugin ***/
-//{{{
-function generateRss()
-{
-	return expandTemplate("RssTemplate");
-}
-
-//# from Config.js
-config.shadowsLoaded = [];
-
-//# from main.js
-function loadShadowTiddlers()
-{
-	var shadows = new TiddlyWiki();
-	shadows.loadFromDiv("shadowArea","shadows",true);
-	shadows.forEachTiddler(function(title,tiddler){config.shadowTiddlers[title] = tiddler.text;config.shadowsLoaded.push(title);});
-	delete shadows;
-}
 
 /***
 Experimental override of saveRSS using templating
