4cb2c76add847587ed4edee361db9cbdc82d3b82
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-26 13:30:54 +0000

    TiddlyTemplating: split apart ListTemplate into TemplateTiddlers and TemplateTags, and updated templates and splits
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4142 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JonathanLister/plugins/TemplatePlugin.js
@@ -0,0 +1,109 @@
+/***
+|''Name:''|templateTiddlersPlugin |
+|''Description:''|Renders a set of tiddlers through a template |
+|''Author:''|JonathanLister (based on ListRelatedPlugin by JeremyRuston) |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/JonathanLister/plugins/templateTiddlersPlugin.js |
+|''Version:''|0.0.4|
+|''Date:''|25/3/08|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''~CoreVersion:''|2.3|
+
+The templateTiddlers macro finds a set of tiddlers and renders them once each through a template. The template can contain additional
+calls to the macro to allow for e.g. looping inside a template (think RSS items).
+
+It needs to be able to support recursion, so that sub-templates make sense. The content is passed in either through a filter for tiddlers,
+a tag to get tiddlers, or a space-separated list. We'll support more as we go along, but this is what we need for RSS.
+
+Usage:
+{{{
+<<templateTiddlers template:RssTemplate filter:"[tag[!excludeLists]]">>
+<<templateTiddlers RssTemplate filter:"[tag[!excludeLists]]">> // template qualification is optional
+}}}
+
+Parameters can be:
+template - the name of the template
+filter - a tiddler filter
+data - the part of a tiddler to use in the subTemplate
+
+If a parameter does not have a qualifier, it is assumed to be the template name
+
+|''Name:''|PermalinkMacro |
+|''Description:''|Creates a permalink to the tiddler |
+
+Usage:
+{{{
+<<permalink>>
+}}}
+***/
+
+//{{{
+if(!version.extensions.templateTiddlersPlugin) {
+version.extensions.templateTiddlersPlugin = {installed:true};
+
+expandTemplate = function(template,tiddlers)
+{
+	var defaultTemplate = "<<view text>>";
+	template = template ? store.getTiddlerText(template,defaultTemplate) : defaultTemplate;
+	if(!tiddlers) {
+		// no tiddlers provided, so create a temporary tiddler
+		tiddlers = [];
+		tiddlers.push(new Tiddler("temp"));
+	}
+	var output = "";
+	for(var i=0; i<tiddlers.length; i++) {
+		output += wikifyStatic(template,null,tiddlers[i],'template').htmlDecode();
+	}
+	return output;
+};
+
+config.macros.templateTiddlers = {};
+config.macros.templateTiddlers.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	p = paramString.parseParams("anon",null,true,false,false);
+	var template = getParam(p,"template",null);
+	if(!template)
+		template = getParam(p,"anon",null);
+	var filter = getParam(p,"filter",null);
+	var tiddlers = [];
+	if(filter) {
+		tiddlers = store.filterTiddlers(filter);
+	} else {
+		// no filter provided, so inherit or create temp tiddler
+		tiddlers.push(tiddler ? tiddler : new Tiddler("temp"));
+	}
+	var output = expandTemplate(template,tiddlers);
+	place.innerHTML += output.htmlEncode();
+};
+
+//	<<templateTags RssItemCategoryTemplate tags:"tags" ">>
+//	<<view text>> (for the tags)
+
+config.macros.templateTags = {};
+config.macros.templateTags.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	//	This method for passing data through to sub-templates works by creating "pseudo-tiddlers" (Tiddler objects not in the store)
+	//  that each carry a part of the data array we want iterating through. We do this to keep the unit of data as the tiddler.
+	p = paramString.parseParams("anon",null,true,false,false);
+	var template = getParam(p,"template",null);
+	var tiddlers = [];
+	if(!template)
+		template = getParam(p,"anon",null);
+	for(var i=0;i<tiddler.tags.length;i++) {
+		var t = new Tiddler(tiddler.title);
+		t.tags = [tiddler.tags[i]];
+		tiddlers.push(t);
+	}
+	var output = expandTemplate(template,tiddlers);
+	place.innerHTML += output.htmlEncode();
+};
+
+config.macros.permalink = {};
+config.macros.permalink.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	var t = encodeURIComponent(String.encodeTiddlyLink(tiddler.title));
+	createTiddlyText(place,window.location+"#"+t);
+};
+
+} //# end of 'install only once'
+//}}}
