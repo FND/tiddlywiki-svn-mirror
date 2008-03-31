add7f62665a50e1681eab6847d3ab302a6fc2a90
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-31 10:31:28 +0000

    Updated formatter. Removed unnecessary saveChanges code.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4191 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/experimental/FastSavePlugin.js
+++ contributors/MartinBudden/experimental/FastSavePlugin.js
@@ -3,7 +3,7 @@
 |''Author:''|Martin Budden ( mjbudden [at] gmail [dot] com)|
 |''Description:''|Plug to speed up saving by avoiding loading tiddlywiki before saving|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/FastSavePlugin.js |
-|''Version:''|0.0.4|
+|''Version:''|0.0.5|
 |''Date:''|Mar 19, 2008|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]] |
@@ -28,7 +28,7 @@ config.templateFormatters = [
 {
 	name: 'templateElement',
 	match: '<!--(?:<<|@@)',
-	lookaheadRegExp: /<!--<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>-->|<!--@@([a-z]*)@@-->/mg,
+	lookaheadRegExp: /<!--<<([^>\s]+)(?:\s*)((?:[^>]|(?:>(?!>)))*)>>-->|<!--@@([^@]*)@@-->/mg,
 	handler: function(w)
 	{
 		this.lookaheadRegExp.lastIndex = w.matchStart;
@@ -85,46 +85,6 @@ function loadOriginal(localPath)
 	var tiddler = new Tiddler('temp');
 	var template = store.getTiddlerText('TiddlyWikiTemplate');
 	return template ? wikifyStatic(template,null,tiddler,'template').htmlDecode() : loadFile(localPath);
-}
-
-// Save this tiddlywiki with the pending changes
-function saveChanges(onlyIfDirty,tiddlers)
-{
-console.log('ss');
-	if(onlyIfDirty && !store.isDirty())
-		return;
-	clearMessage();
-	var t0 = new Date();
-	//# Get the URL of the document
-	var originalPath = document.location.toString();
-	//# Check we were loaded from a file URL
-	if(originalPath.substr(0,5) != 'file:') {
-		alert(config.messages.notFileUrlError);
-		if(store.tiddlerExists(config.messages.saveInstructions))
-			story.displayTiddler(null,config.messages.saveInstructions);
-		return;
-	}
-	var localPath = getLocalPath(originalPath);
-	//# Load the original file
-	var original = loadOriginal(localPath);
-	if(!original) {
-		alert(config.messages.cantSaveError);
-		if(store.tiddlerExists(config.messages.saveInstructions))
-			story.displayTiddler(null,config.messages.saveInstructions);
-		return;
-	}
-	//# Locate the storeArea div's
-	var posDiv = locateStoreArea(original);
-	if(!posDiv) {
-		alert(config.messages.invalidFileError.format([localPath]));
-		return;
-	}
-	saveBackup(localPath,original);
-	saveRss(localPath);
-	saveEmpty(localPath,original,posDiv);
-	saveMain(localPath,original,posDiv);
-	if(config.options.chkDisplayInstrumentation)
-		displayMessage('saveChanges ' + (new Date()-t0) + ' ms');
 }
 
 } //# end of 'install only once'
