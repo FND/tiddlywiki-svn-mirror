19d90b7d09ef0ec00e69b3ebe750f8f633708c53
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-01 16:37:06 +0000

    Minor update
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4223 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/experimental/BackupTiddlerPlugin.js
+++ contributors/MartinBudden/experimental/BackupTiddlerPlugin.js
@@ -3,16 +3,16 @@
 |''Description:''|Backs up tiddlers individually if chkAutoSave is set|
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/plugins/BackupTiddlerPlugin.js |
-|''Version:''|0.0.1|
+|''Version:''|0.0.2|
 |''Date:''|Oct 17, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
-|''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
+|''License:''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]] |
 |''~CoreVersion:''|2.3.0|
 
 ***/
 
 //{{{
-//# Ensure that the DisableWikiLinksPlugin is only installed once.
+//# Ensure that the BackupTiddlerPlugin is only installed once.
 if(!version.extensions.BackupTiddlerPlugin) {
 version.extensions.BackupTiddlerPlugin = {installed:true};
 
@@ -26,8 +26,6 @@ function saveBackup(localPath,original,tiddlers)
 		for(var i=0;i<tiddlers.length;i++) {
 			var t = tiddlers[i];
 			var backupPath = getBackupPath(localPath,t.title,"tiddler");
-			//# displayMessage("t:"+t.title);
-			//# displayMessage("b:"+backupPath);
 			saveFile(backupPath,store.getSaver().externalizeTiddler(store,t));
 		}
 	}
