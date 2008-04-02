f7e95f96ff203ec5341456494a04af1174f1f0d9
EricShulman <EricShulman@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-02 14:28:14 +0000

    when single-page mode is in use and a tiddler is being edited, ask for permission to save-and-close that tiddler, instead of just leaving it open.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4242 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/EricShulman/plugins/SinglePageModePlugin.js
+++ contributors/EricShulman/plugins/SinglePageModePlugin.js
@@ -2,7 +2,7 @@
 |Name|SinglePageModePlugin|
 |Source|http://www.TiddlyTools.com/#SinglePageModePlugin|
 |Documentation|http://www.TiddlyTools.com/#SinglePageModePluginInfo|
-|Version|2.6.0|
+|Version|2.9.0|
 |Author|Eric Shulman - ELS Design Studios|
 |License|http://www.TiddlyTools.com/#LegalStatements <br>and [[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]]|
 |~CoreVersion|2.1|
@@ -16,24 +16,40 @@ This plugin allows you to configure TiddlyWiki to navigate more like a tradition
 !!!!!Configuration
 <<<
 <<option chkSinglePageMode>> Display one tiddler at a time
-<<option chkSinglePagePermalink>> Automatically permalink current tiddler
-<<option chkSinglePageAutoScroll>> Automatically scroll tiddler into view (if needed)
+><<option chkSinglePageKeepFoldedTiddlers>> Don't auto-close folded tiddlers
+><<option chkSinglePagePermalink>> Automatically permalink current tiddler
 <<option chkTopOfPageMode>> Always open tiddlers at the top of the page
 <<option chkBottomOfPageMode>> Always open tiddlers at the bottom of the page
-//Note: if both 'top' and 'bottom' settings are selected, "top of page" will be used.  Also, in Apple's Safari browser, automatically setting the permalink causes an error and is disabled.//
+<<option chkSinglePageAutoScroll>> Automatically scroll tiddler into view (if needed)
+
+Notes:
+* The "display one tiddler at a time" option can also be //temporarily// set/reset by including a 'paramifier' in the document URL: {{{#SPM:true}}} or {{{#SPM:false}}}.
+* If more than one display mode is selected, 'one at a time' display takes precedence over both 'top' and 'bottom' settings, and if 'one at a time' setting is not used, 'top of page' takes precedence over 'bottom of page'.
+* When using Apple's Safari browser, automatically setting the permalink causes an error and is disabled.
 <<<
 !!!!!Revisions
 <<<
-2008.03.01 [2.6.0] in hijack of displayTiddler(), added 'title' argument to closeAllTiddlers() so that target tiddler isn't closed-and-reopened if it was already displayed.  Also, added config.options.chkSinglePageAutoScrolloption to bypass automatic 'scroll into view' logic (note: core still does it's own ensureVisible() handling)
+2008.04.02 [2.9.0] in displayTiddler(), when single-page mode is in use and a tiddler is being edited, ask for permission to save-and-close that tiddler, instead of just leaving it open.
 | Please see [[SinglePageModePluginInfo]] for previous revision details |
 2005.08.15 [1.0.0] Initial Release.  Support for BACK/FORWARD buttons adapted from code developed by Clint Checketts.
 <<<
 !!!!!Code
 ***/
 //{{{
-version.extensions.SinglePageMode= {major: 2, minor: 6, revision: 0, date: new Date(2008,3,1)};
-
+version.extensions.SinglePageMode= {major: 2, minor: 9, revision: 0, date: new Date(2008,4,2)};
+//}}}
+//{{{
+config.paramifiers.SPM = { onstart: function(v) {
+	config.options.chkSinglePageMode=eval(v);
+	if (config.options.chkSinglePageMode && config.options.chkSinglePagePermalink && !config.browser.isSafari) {
+		config.lastURL = window.location.hash;
+		if (!config.SPMTimer) config.SPMTimer=window.setInterval(function() {checkLastURL();},1000);
+	}
+} };
+//}}}
+//{{{
 if (config.options.chkSinglePageMode==undefined) config.options.chkSinglePageMode=false;
+if (config.options.chkSinglePageKeepFoldedTiddlers==undefined) config.options.chkSinglePageKeepFoldedTiddlers=true;
 if (config.options.chkSinglePagePermalink==undefined) config.options.chkSinglePagePermalink=true;
 if (config.options.chkTopOfPageMode==undefined) config.options.chkTopOfPageMode=false;
 if (config.options.chkBottomOfPageMode==undefined) config.options.chkBottomOfPageMode=false;
@@ -41,6 +57,7 @@ if (config.options.chkSinglePageAutoScroll==undefined) config.options.chkSingleP
 
 if (config.optionsDesc) {
 	config.optionsDesc.chkSinglePageMode="Display one tiddler at a time";
+	config.optionsDesc.chkSinglePageKeepFoldedTiddlers="Don't auto-close folded tiddlers";
 	config.optionsDesc.chkSinglePagePermalink="Automatically permalink current tiddler";
 	config.optionsDesc.chkSinglePageAutoScroll="Automatically scroll tiddler into view (if needed)";
 	config.optionsDesc.chkTopOfPageMode="Always open tiddlers at the top of the page";
@@ -48,64 +65,101 @@ if (config.optionsDesc) {
 } else {
 	config.shadowTiddlers.AdvancedOptions += "\
 		\n<<option chkSinglePageMode>> Display one tiddler at a time \
+		\n<<option chkSinglePageKeepFoldedTiddlers>> Don't auto-close folded tiddlers \
 		\n<<option chkSinglePagePermalink>> Automatically permalink current tiddler \
 		\n<<option chkSinglePageAutoScroll>> Automatically scroll tiddler into view (if needed) \
 		\n<<option chkTopOfPageMode>> Always open tiddlers at the top of the page \
 		\n<<option chkBottomOfPageMode>> Always open tiddlers at the bottom of the page";
 }
-
+//}}}
+//{{{
 config.SPMTimer = 0;
 config.lastURL = window.location.hash;
 function checkLastURL()
 {
 	if (!config.options.chkSinglePageMode)
 		{ window.clearInterval(config.SPMTimer); config.SPMTimer=0; return; }
-	if (config.lastURL == window.location.hash)
-		return;
-	var tiddlerName = convertUTF8ToUnicode(decodeURIComponent(window.location.hash.substr(1)));
-	tiddlerName=tiddlerName.replace(/\[\[/,"").replace(/\]\]/,""); // strip any [[ ]] bracketing
-	if (tiddlerName.length) story.displayTiddler(null,tiddlerName,1,null,null);
+	if (config.lastURL == window.location.hash) return; // no change in hash
+	var tids=convertUTF8ToUnicode(decodeURIComponent(window.location.hash.substr(1))).readBracketedList();
+	if (tids.length==1) // permalink (single tiddler in URL)
+		story.displayTiddler(null,tids[0]);
+	else { // restore permaview or default view
+		config.lastURL = window.location.hash;
+		if (!tids.length) tids=store.getTiddlerText("DefaultTiddlers").readBracketedList();
+		story.closeAllTiddlers();
+		story.displayTiddlers(null,tids);
+	}
 }
 
-if (Story.prototype.SPM_coreDisplayTiddler==undefined) Story.prototype.SPM_coreDisplayTiddler=Story.prototype.displayTiddler;
-Story.prototype.displayTiddler = function(srcElement,title,template,animate,slowly)
+if (Story.prototype.SPM_coreDisplayTiddler==undefined)
+	Story.prototype.SPM_coreDisplayTiddler=Story.prototype.displayTiddler;
+Story.prototype.displayTiddler = function(srcElement,tiddler,template,animate,slowly)
 {
-	if (config.options.chkSinglePageMode)
-		story.closeAllTiddlers(title);
-	else if (config.options.chkTopOfPageMode)
+	var title=(tiddler instanceof Tiddler)?tiddler.title:tiddler;
+	var tiddlerElem=document.getElementById(story.idPrefix+title); // ==null unless tiddler is already displayed
+	var opt=config.options;
+	if (opt.chkSinglePageMode) {
+		story.forEachTiddler(function(tid,elem) {
+			// skip current tiddler and, optionally, tiddlers that are folded.
+			if (	tid==title
+				|| (opt.chkSinglePageKeepFoldedTiddlers && elem.getAttribute("folded")=="true"))
+				return;
+			// if a tiddler is being edited, ask before closing
+			if (elem.getAttribute("dirty")=="true") {
+				// if tiddler to be displayed is already shown, then leave active tiddlers editors as is
+				// (occurs when switching between view and edit modes)
+				if (tiddlerElem) return;
+				// otherwise, ask for permission
+				var msg="'"+tid+"' is currently being edited.\n\n";
+				msg+="Press OK to save and close this tiddler\nor press Cancel to leave it opened";
+				if (!confirm(msg)) return; else story.saveTiddler(tid);
+			}
+			story.closeTiddler(tid);
+		});
+	}
+	else if (opt.chkTopOfPageMode)
 		arguments[0]=null;
-	else if (config.options.chkBottomOfPageMode)
+	else if (opt.chkBottomOfPageMode)
 		arguments[0]="bottom";
-	if (config.options.chkSinglePageMode && config.options.chkSinglePagePermalink && !config.browser.isSafari) {
+	if (opt.chkSinglePageMode && opt.chkSinglePagePermalink && !config.browser.isSafari) {
 		window.location.hash = encodeURIComponent(convertUnicodeToUTF8(String.encodeTiddlyLink(title)));
 		config.lastURL = window.location.hash;
 		document.title = wikifyPlain("SiteTitle") + " - " + title;
 		if (!config.SPMTimer) config.SPMTimer=window.setInterval(function() {checkLastURL();},1000);
 	}
-	this.SPM_coreDisplayTiddler.apply(this,arguments); // let CORE render tiddler
+	if (tiddlerElem && tiddlerElem.getAttribute("dirty")=="true") { // editing... move tiddler without re-rendering
+		var isTopTiddler=(tiddlerElem.previousSibling==null);
+		if (!isTopTiddler && (opt.chkSinglePageMode || opt.chkTopOfPageMode))
+			tiddlerElem.parentNode.insertBefore(tiddlerElem,tiddlerElem.parentNode.firstChild);
+		else if (opt.chkBottomOfPageMode)
+			tiddlerElem.parentNode.insertBefore(tiddlerElem,null);
+		else this.SPM_coreDisplayTiddler.apply(this,arguments); // let CORE render tiddler
+	} else
+		this.SPM_coreDisplayTiddler.apply(this,arguments); // let CORE render tiddler
 	var tiddlerElem=document.getElementById(story.idPrefix+title);
-	if (tiddlerElem&&config.options.chkSinglePageAutoScroll) {
+	if (tiddlerElem&&opt.chkSinglePageAutoScroll) {
 		var yPos=ensureVisible(tiddlerElem); // scroll to top of tiddler
 		var isTopTiddler=(tiddlerElem.previousSibling==null);
-		if (config.options.chkSinglePageMode||config.options.chkTopOfPageMode||isTopTiddler)
+		if (opt.chkSinglePageMode||opt.chkTopOfPageMode||isTopTiddler)
 			yPos=0; // scroll to top of page instead of top of tiddler
-		if (config.options.chkAnimate) // defer scroll until 200ms after animation completes
+		if (opt.chkAnimate) // defer scroll until 200ms after animation completes
 			setTimeout("window.scrollTo(0,"+yPos+")",config.animDuration+200); 
 		else
 			window.scrollTo(0,yPos); // scroll immediately
 	}
 }
 
-if (Story.prototype.SPM_coreDisplayTiddlers==undefined) Story.prototype.SPM_coreDisplayTiddlers=Story.prototype.displayTiddlers;
-Story.prototype.displayTiddlers = function(srcElement,titles,template,unused1,unused2,animate,slowly)
-{
+if (Story.prototype.SPM_coreDisplayTiddlers==undefined)
+	Story.prototype.SPM_coreDisplayTiddlers=Story.prototype.displayTiddlers;
+Story.prototype.displayTiddlers = function() {
 	// suspend single-page mode (and/or top/bottom display options) when showing multiple tiddlers
-	var saveSPM=config.options.chkSinglePageMode; config.options.chkSinglePageMode=false;
-	var saveTPM=config.options.chkTopOfPageMode; config.options.chkTopOfPageMode=false;
-	var saveBPM=config.options.chkBottomOfPageMode; config.options.chkBottomOfPageMode=false;
+	var opt=config.options;
+	var saveSPM=opt.chkSinglePageMode; opt.chkSinglePageMode=false;
+	var saveTPM=opt.chkTopOfPageMode; opt.chkTopOfPageMode=false;
+	var saveBPM=opt.chkBottomOfPageMode; opt.chkBottomOfPageMode=false;
 	this.SPM_coreDisplayTiddlers.apply(this,arguments);
-	config.options.chkBottomOfPageMode=saveBPM;
-	config.options.chkTopOfPageMode=saveTPM;
-	config.options.chkSinglePageMode=saveSPM;
+	opt.chkBottomOfPageMode=saveBPM;
+	opt.chkTopOfPageMode=saveTPM;
+	opt.chkSinglePageMode=saveSPM;
 }
 //}}}
