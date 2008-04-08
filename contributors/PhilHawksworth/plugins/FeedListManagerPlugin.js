4d7e4ab143178b4ca0179ee574e8adfd69ec8f24
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-08 17:13:30 +0000

    FeedListManagerPlugin now explodes OPML files to include their rss uris. Introduced a dependency on the TextToXMLDOM Plugin for aiding in the OPML parsing. Scope remains for abstracting the OPML parsing further into its own plugin.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4290 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/plugins/FeedListManagerPlugin.js
+++ contributors/PhilHawksworth/plugins/FeedListManagerPlugin.js
@@ -3,6 +3,7 @@
 |''Description:''|Manage a list of uris to be called in a round-robin stlye|
 |''Author:''|PhilHawksworth|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PhilHawksworth/plugins/FeedListManagerPlugin.js |
+|''Dependencies:''| TextToXMLDOMPlugin|
 |''Version:''|0.1|
 |''Date:''|Mar 03, 2008|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
@@ -19,7 +20,7 @@ var feeds = new FeedListManager();
 
 Add a uri to an existing feed list manager.
 {{{
-feeds.add(uri);
+feeds.add(uri, [name], [type]);
 }}}
 
 Remove a uri from an existing feed list manager.
@@ -49,7 +50,7 @@ i = feeds.registered(uri);
 
 Repopulate the uri list in an existing feed list manager.
 {{{
-feeds.refresh();
+feeds.populate();
 }}}
 
 Clear out the list of registered uris.
@@ -74,6 +75,7 @@ function FeedListManager(){
 	this.currentPosition = 0;
 	this.lastIncrement = null;
 	this.updating = false;
+	this.requests = [];
 };
 
 
@@ -90,17 +92,19 @@ FeedListManager.log = function(x)
 // Add a uri to the list of those managed (no dupes).
 // uri : the uri,
 // [name] : friendly name. (defaults to 'anon')
-// [type] : uri | opml (defaults to 'uri')
+// [type] : rss | opml (defaults to 'rss')
 FeedListManager.prototype.add = function(uri, name, type) {
 	var exists = this.registered(uri);
 	if(exists != null)
 		return;
 	if(!type)
-		type = 'uri';
+		type = 'rss';
 	if(!name)
 		name = 'anon';
 	var uriObj = {'name':name, 'uri':uri, 'type':type, 'callCount':0, 'lastCall':null};
 	this.uris.push(uriObj);
+	if(type == 'opml'|'include')
+		this.populate();
 };
 
 
@@ -114,40 +118,51 @@ FeedListManager.prototype.remove = function(uri) {
 
 // Add a uri to the list of those managed (no dupes).
 FeedListManager.prototype.populate = function() {
-	
-	//flag that the update is happening.
-	this.updating = true;
-	
-	//get and call the opml links.
-	var opmllinks = [];
-	for(var u=0; u<this.uris.length; u++) {
-		if(this.uris[u].type == 'opml') {
-			// var params = {};
-			// 	params.platform = platform;
-			// 	sparams.tiddlerTitle = tiddlerTitle;
-			doHttp("GET",this.uris[u].uri,null,null,null,null,this.examineopml);
+	var feedList = this;
+	for(var u=0; u<feedList.uris.length; u++) {
+		if(feedList.uris[u].type == 'opml') {
+			feedList.updating = true;
+			var params = {'feedList': feedList};
+			var req = doHttp("GET",feedList.uris[u].uri,null,null,null,null,feedList.examineopml,params);
+			feedList.requests.push(req);
 		}	
 	}
-	//flag that the update is complete.
 };
 
 
 //examine an opml file to gather the feed links within.
 FeedListManager.prototype.examineopml = function(status,params,responseText,url,xhr) {
+
+	var feedList = params.feedList;
+
+	// Remove the record of this request for tracking purposes.
+	var pos = feedList.requests.indexOf(xhr);
+	if(pos != -1)
+		feedList.requests.splice(pos,1);
+
+	// Stop if there is not a suitable response.
 	if(xhr.status != 200) {
 		FeedListManager.log("Unable to get OPML");
 		return;
 	}
-	
-	FeedListManager.log(xhr);
-	
-	/*
-		TODO Complete population from OPML
-	*/
-	parseOPML(responseText);
-	
-	//add each uri found.
-	//flag that the update is complete.
+
+	// transform the response into XMLDOM for parsing.
+	var xml = getXML(responseText);
+	var outlines = xml.getElementsByTagName("outline");
+
+	// Add any rss links found to the manager.
+	var xmlUrl, type, attr, xmlUrl, title = null;
+	for (var i=0; i < outlines.length; i++) {
+		type = outlines[i].getAttribute("type") || null;
+		xmlUrl = outlines[i].getAttribute("xmlUrl") || null;
+		title = outlines[i].getAttribute("title") || null;		
+		if(type && xmlUrl)
+			feedList.add(xmlUrl, title, type);
+	};
+
+	//flag if all updates are complete.
+	if(feedList.requests.length == 0)
+		feedList.updating = false;
 };
 
 
@@ -187,14 +202,14 @@ FeedListManager.prototype.next = function() {
 	var now = new Date();
 	now = now.convertToYYYYMMDDHHMMSSMMM();
 	this.lastIncrement = now;
-	if(uri.type != 'uri')
+	if(uri.type != 'rss')
 		this.next();
 	else
 		return uri;
 };
 
 
-// Make a note of a call to a given yri
+// Make a note of a call to a given uri
 FeedListManager.prototype.logCall = function(uri) {
 	var p = this.registered(uri);
 	if(p == null)
@@ -223,7 +238,7 @@ FeedListManager.prototype.stats = function(uri) {
 		/*
 			TODO Make the stats output to a tiddler rather than to the console.
 		*/
-		FeedListManager.log(uris[u].uri + ", ("+ uris[u].type +"),  called "+ uris[u].callCount + " times, last called at " + uris[u].lastCall);
+		FeedListManager.log(uris[u].name + ", " + uris[u].uri + ", ("+ uris[u].type +"),  called "+ uris[u].callCount + " times, last called at " + uris[u].lastCall);
 	}
 };
 
@@ -232,27 +247,6 @@ FeedListManager.prototype.stats = function(uri) {
 FeedListManager.prototype.purge = function() {
 	this.uris = [];
 	this.currentPosition = 0;
-};
-
-
-
-/*
-	TODO Abstract the OPML parser into a seperate plugin.
-*/
-function getXML(opml) {
-	if(window.ActiveXObject) {
-		var doc = new ActiveXObject("Microsoft.XMLDOM");
-		doc.async="false";
-		doc.loadXML(opml);
-	}
-	else {
-		var parser=  new DOMParser();
-		var doc = parser.parseFromString(opml,"text/xml");	
-	}
-
-
-	FeedListManager.log(opml);
-
 };
 
 
