e520e3b7cffefd323e8ced8d67fe25635c70fa2f
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-08 17:09:32 +0000

    TextToXMLDOM plugin provides an easy way to create an XMLDOM from a text string. This then allows easy parsing of the XML DOM with established standard methods.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4289 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/plugins/TextToXMLDOMPlugin.js
+++ contributors/PhilHawksworth/plugins/TextToXMLDOMPlugin.js
@@ -1,17 +1,27 @@
 /***
-|''Name:''|TestToXMLDOMPlugin|
+|''Name:''|TextToXMLDOMPlugin|
 |''Description:''|Convert a chunk of text into XML DOM for traversing using standard XML DOM methods|
 |''Author:''|PhilHawksworth |
-|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PhilHawksworth/Plugins/TestToXMLDOMPlugin.js |
-|''Version:''|0.1|
-|''Date:''|Mar 18, 2008|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PhilHawksworth/plugins/TestToXMLDOMPlugin.js |
+|''Version:''|0.2|
+|''Date:''|April 8, 2008|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''~CoreVersion:''|2.3|
 
 
 ''Usage examples:''
+{{{
+// Create an XMLDOM object from some text 
+var xml = getXML(text);
 
+// Use standars XML DOM methods to parse the object. For example:
+// Get all of the 'thing' tag elements.
+var things = xml.getElementsByTagName("thing");
+
+// Get the uri attribute of the first 'thing' element.
+var thingUri = things[0].getAttribute("uri");
+}}}
 
 
 ***/
@@ -20,20 +30,6 @@
 if(!version.extensions.TextToXMLDOMPlugin) {
 version.extensions.TextToXMLDOMPlugin = {installed:true};
 
-config.macros.TextToXMLDOM = {};
-config.macros.TextToXMLDOM.log = function(x) {
-	if(window.console) {
-		console.log(x);	
-		displayMessage(x);
-	}
-};
-
-
-
-
-/*
-	TODO Abstract the OPML parser into a seperate plugin.
-*/
 function getXML(opml) {
 	if(window.ActiveXObject) {
 		var doc = new ActiveXObject("Microsoft.XMLDOM");
@@ -44,9 +40,11 @@ function getXML(opml) {
 		var parser=  new DOMParser();
 		var doc = parser.parseFromString(opml,"text/xml");	
 	}
-
-	FeedListManager.log(opml);
-
+	if(doc){
+		return doc;
+	}
+	else
+		return null;	
 };
 
 
