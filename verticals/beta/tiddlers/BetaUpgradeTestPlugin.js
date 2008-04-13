be57ee18473c91432b05120313ef08cd16594964
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-13 18:37:59 +0000

    Content updates for TiddlyWiki 2.4.0 beta 1
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4338 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/beta/tiddlers/BetaUpgradeTestPlugin.js
@@ -0,0 +1,16 @@
+/***
+|''Name''|BetaUpgradeTestPlugin |
+|''Author''|JeremyRuston |
+|''Version''|1.0|
+|''Date''|13 Apr 2008|
+|''CoreVersion''|2.4.0 |
+!Description
+
+This plugin overrides the upgrade source location to enable testing of the new upgrade functionality.
+
+***/
+//{{{
+
+config.macros.upgrade.source = "http://www.tiddlywiki.com/beta/empty.html";
+
+//}}}
