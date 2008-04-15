14477011eb340449b83f8f34b3ad7b8c31b50170
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-15 10:25:55 +0000

    Testing: updated tests
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4379 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/tests/jsspec/LiveColorPalettePlugin.jsspec.js
+++ contributors/PhilHawksworth/tests/jsspec/LiveColorPalettePlugin.jsspec.js
@@ -1,14 +1,40 @@
 // <![CDATA[
 
-describe('Live Color Palette', {
-	'getColor returns a value from the palette': function() {
-		var c = config.macros.LiveColorPalette.getColor('ColorPalette', 'Background');
-		value_of(c).should_not_be(null);
+	
+describe('LiveColorPalettePlugin', {
+	
+	before_each: function(){
+		store = new TiddlyWiki();
+		loadShadowTiddlers();
 	},
-	'getColor returns null if the color requested does not exist in the palette': function() {
-		var c = config.macros.LiveColorPalette.getColor('Jibberish');
-		value_of(c).should_be(null);
+
+	'given a valid color name, getColor returns a value from the palette': function() {
+			
+		var c = config.macros.LiveColorPalette.getColor('ColorPalette', 'Background');
+		value_of(c).should_be_undefined();
+	},
+	
+	'given an invalid color name, getColor returns a undefined ' : function() {
+		var c = config.macros.LiveColorPalette.getColor('ColorPalette', 'jibberish');
+		value_of(c).should_be_undefined();
+	},
+	
+	'given an invalid palette name, getColor returns undefined ' : function() {
+		var c = config.macros.LiveColorPalette.getColor('Jibberish', 'Background');
+		value_of(c).should_be_undefined();	
+	},
+	
+	'given a valid palette, color name and value, setColor writes the color to the palette : ' : function() {
+		var color = '#f00';
+		config.macros.liveColorPalette.setColor('ColorPalette', 'Background', color );
+		var c = config.macros.LiveColorPalette.getColor('ColorPalette', 'Background');
+		value_of(c).should_be(color);
 	}
+	
 });
+
+
+
+
 
 // ]]>
