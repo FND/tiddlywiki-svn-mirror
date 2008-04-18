6c36ba27b3e04e4538375fd3e2dc9ebe05e20823
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-18 10:08:33 +0000

    HTMLPreviewMacro testing: more tests
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4500 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/tests/jsspec/HTMLTemplatePreviewMacro.jsspec.js
+++ contributors/JonathanLister/tests/jsspec/HTMLTemplatePreviewMacro.jsspec.js
@@ -8,6 +8,29 @@ describe('HTMLTemplatePreview : handler()', {
 		params = ["testTemplate"];
 	},
 
+	'it should return false if no template parameter is provided': function() {
+		params = [];
+		var actual = config.macros.HTMLTemplatePreview.handler(place,null,params);
+		value_of(actual).should_be_false();
+		displayMessage = function() {
+			return;
+		};
+	},
+	
+	'it should not call expandTemplate if no template parameter is provided': function() {
+		var called = false;
+		expandTemplate = function(html) {
+			called = true;
+			return html;
+		};
+		displayMessage = function() {
+			return;
+		};
+		params = [];
+		var actual = config.macros.HTMLTemplatePreview.handler(place,null,params);
+		value_of(called).should_be(false);
+	},
+
 	'it should call the expandTemplate function': function() {
 		var called = false;
 		expandTemplate = function(html) {
