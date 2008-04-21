cbcd756369404b2d702974ebabd3d1b4490fba27
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-21 16:19:52 +0000

    updated include file
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4564 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/tiddlers/include.php
+++ association/serversides/cctiddly/tiddlers/include.php
@@ -1,12 +1,63 @@
 <?php
+include_once($cct_base."tiddlers/ccVariables.php");
 
-include_once($cct_base."tiddlers/ccWorkspace.php");
-include_once($cct_base."tiddlers/SiteProxy.php");
-include_once($cct_base."tiddlers/ccLoadRemoteFileThroughProxy.php");
-include_once($cct_base."tiddlers/ccAbout.php");
-include_once($cct_base."tiddlers/ccUpload.php");
-include_once($cct_base."tiddlers/ccLogin.php");
-include_once($cct_base."tiddlers/ccStyleSheet.tiddler");
+
+
+
+$dir = $cct_base."ccPlugins/";
+
+// Open a known directory, and proceed to read its contents
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+       while (($file = readdir($dh)) !== false) {
+			$ext = substr($file, strrpos($file, '.') + 1); 
+			if ($ext == "js")
+			{
+				$tiddler_name = str_replace('.js', '', $file);
+				echo "<div tiddler='".$tiddler_name."' modifier='ccTiddly'  modified='200804211142' created='200804211142' tags='systemConfig excludeLists excludeSearch'>\n";
+            	include_once($cct_base."ccPlugins/".$file);
+				echo "</div>\n";
+    		}else if ($ext == "tiddler")
+			{		
+					$tiddler_name = str_replace('.tiddler', '', $file);
+					include_once($cct_base."ccPlugins/".$file);
+			}
+    	}
+        closedir($dh);
+    }
+}
 include_once($cct_base."tiddlers/ccTiddly_debug_time.php");
 
 ?>
+<div title="CreateWorkspace" tags="simple">
+<pre>&lt;&lt;ccCreateWorkspace&gt;&gt;</pre>
+</div>
+
+<div title="CreateWorkspace">
+<pre><<ccCreateWorkspace>></pre>
+</div>
+
+<div title="AnonDefaultTiddlers" modifier="ccTiddly">
+<pre>
+[[Login]] [[GettingStarted]]
+</pre>
+</div>
+
+
+<div title="LoginStatus" modifier="ccTiddly" tags="simple">
+<pre>
+&lt;&lt;ccLoginStatus&gt;&gt;
+</pre>
+</div>
+<div title="Login" modifier="ccTiddly">
+<pre>
+&lt;&lt;ccLogin&gt;&gt;
+</pre>
+</div>
+<div title="Forgotten Details" modifier="ccTiddly">
+<pre>
+If you've forgotten your username and / or password, please contact the administrator:
+
+simon@osmosoft.com
+</pre>
+</div>
