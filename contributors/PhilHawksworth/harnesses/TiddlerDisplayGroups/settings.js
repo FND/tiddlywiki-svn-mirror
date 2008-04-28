5d0d4a59f9c0752c95369e20c920e499236bb0e6
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-04-28 16:43:01 +0000

    TiddlerDisplayGroupsPlugin test harness. This demonstrates the simple functionality of the TiddlerDisplayGroupsPlugin with a very simple set of tiddlers and an example template.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4726 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PhilHawksworth/harnesses/TiddlerDisplayGroups/settings.js
@@ -0,0 +1,18 @@
+//{{{
+
+config.options.chkAutoSave = true;
+config.options.chkAnimate = true;
+config.options.chkSaveBackups = false;
+
+// Initialise the session display groupings
+var test_group = new TiddlerDisplayGroup();
+
+var test_pattern = [
+	{label:'header', tag:'session', count:1, require:null, openAt:null},
+	{label:'mynote', tag:'notes', count:1, require:'header', openAt:null},
+	{label:'notes', tag:'discovered_notes', count:0, require:'header', openAt:'bottom'}];
+
+test_group.setPattern(test_pattern); 
+test_group.setGroupField('rr_session_id');
+
+//}}}
