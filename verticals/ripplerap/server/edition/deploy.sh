eceac6f22e6b74786be549d5ea1473d0f0c2230f
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-06 16:13:32 +0000

    ripplerap - added edition deploy script
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4796 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/ripplerap/server/edition/deploy.sh
@@ -0,0 +1,5 @@
+#
+#  deploy edition to riplerap.com
+#
+scp index.php ripmaster@ripplerap.com:/data/vhost/www.ripplerap.com/html/edition/index.php
+scp ../../editions/blank/index.html ripmaster@ripplerap.com:/data/vhost/www.ripplerap.com/html/edition/ripplerap.html
