cd4f3b75eb34266a9126bcce5caaea167f362c0d
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-06 11:51:10 +0000

    RippleRap: Recipe refactoring.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4788 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/core/settings.js
+++ verticals/ripplerap/core/settings.js
@@ -9,12 +9,12 @@ config.options.txtTheme = "RippleRapSkin";
 
 
 // Setup the session notes display groupings.
-var session_notes_groups = new TiddlerDisplayGroup();
-var session_notes_pattern = [
-	{label:'header', tag:'session', count:1, require:null, openAt:null},
-	{label:'mynote', tag:'notes', count:1, require:'header', openAt:null},
-	{label:'notes', tag:'discovered_notes', count:0, require:'header', openAt:'bottom'}];
-session_notes_groups.setPattern(session_notes_pattern); 
-session_notes_groups.setGroupField('rr_session_id');
+// var session_notes_groups = new TiddlerDisplayGroup();
+// var session_notes_pattern = [
+// 	{label:'header', tag:'session', count:1, require:null, openAt:null},
+// 	{label:'mynote', tag:'notes', count:1, require:'header', openAt:null},
+// 	{label:'notes', tag:'discovered_notes', count:0, require:'header', openAt:'bottom'}];
+// session_notes_groups.setPattern(session_notes_pattern); 
+// session_notes_groups.setGroupField('rr_session_id');
 
 //}}}
