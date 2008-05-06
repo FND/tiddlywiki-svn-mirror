f706cccd3894f8afce80eb000091076f210b6456
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-06 10:52:22 +0000

    ripplerap - added RippleRap agenda parameters
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4781 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/editions/blank/settings.js
+++ verticals/ripplerap/editions/blank/settings.js
@@ -1,12 +1,15 @@
 //{{{
 
 config.options.txtMainTab = "AgendaDay1";
-config.options.txtMojoTelno = "+3368...";
 config.options.chkAutoSave = true;
 config.options.chkAnimate = true;
 config.options.chkSaveBackups = false;
 
 config.options.chkRipplerapShare = true;
-config.options.chkRipplerapReadyToUse = false;
+config.options.chkRipplerapEnjoy= true;
+
+config.options.chkRipplerapConferenceName= "";
+config.options.chkRipplerapConferenceURI= "";
+config.options.chkRipplerapConferenceURIType= "";
 
 //}}}
