f9c2c0aec6d0afd930cea98f85e6b44def539841
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-12 14:06:28 +0000

    cct - modified recipe to include view template
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4896 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/ccPlugins/ccSave.js
@@ -0,0 +1,13 @@
+/***
+|''Name''|ccSave|
+|''Description''|Currently only being used to stop the upgrade option |
+|''Author''|[[Simon McManus|http://simonmcmanus.com]] |
+|''Version''|1.0.1|
+|''Date''|12/05/2008|
+
+!Code
+
+***/
+//{{{
+	
+//}}}
--- association/serversides/cctiddly/index/posttiddlers.php
+++ association/serversides/cctiddly/index/posttiddlers.php
@@ -1,5 +1,5 @@
 <?php
-include_once($cct_base."tiddlers/include.php");
+include_once($cct_base."includes/include.php");
 ?>
 
 <?php /*print cct_print_form($standalone);/*cct*/ ?>
