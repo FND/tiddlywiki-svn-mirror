c81647f54a2bb4b2b674731be519ae147ce178e6
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-12 11:34:29 +0000

    cct - file renaming
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4887 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/handle/revisionList.php
+++ association/serversides/cctiddly/handle/revisionList.php
@@ -25,17 +25,19 @@
 		//returnResult("013");
 		sendHeader(400,$ccT_msg['misc']['no_title'],"",1);
 	}
-	
-	if( !isset($_GET['revision']) ) {
-		//returnResult("013");
-		sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
-	}
-	
+
 	$title = formatParametersGET($_GET['title']);
-	$revision = formatParametersGET($_GET['revision']);
 	
 //////////////////////////////////////////////////////////start of code//////////////////////////////////////////////////////////////
 
+	//get user and privilege and set variables
+	/*if( strlen($username)==0 && strlen($password)==0 )
+	{
+		$user = user_create();		//get username password from cookie
+	}else{
+		$user = user_create($username,"",0,"",$password,1);
+	}*/
+	
 	//get tiddler with certain title to obtain its tiddler_id
 	$tiddler = db_tiddlers_mainSelectTitle($title);
 	if( $tiddler === FALSE ) {//not found
@@ -46,26 +48,35 @@
 	//use tiddler_id to obtain list of tiddler for revision
 	$tiddler_list = db_tiddlers_backupSelectOid($tiddler['id']);
 	
-	//find revision
+	//check privilege for each tiddler
+	$tmp = array();
 	foreach( $tiddler_list as $t ) {
-		if( $revision == $t['revision'] ) {		//if revision equals, check privilege
-			if( user_readPrivilege(user_tiddlerPrivilegeOfUser($user,$t['tags'])) ) {	//if read privilege ok, output
-				$output = $title."\n";
-				$output .= $t['title']."\n";
-				$output .= $t['body']."\n";
-				$output .= $t['modifier']."\n";
-				$output .= $t['modified']."\n";
-				$output .= $tiddler['created']."\n";
-				$output .= $t['tags']."\n";
-				$output .= $t['revision']."\n";
-				$output .= $t['fields'];
-				sendHeader(200,"", $output,1);
-				//returnResult("007");
-			}else{		//if no read privilege, stop
-				sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
-				//returnResult("014");
-			}
+		if( user_readPrivilege(user_tiddlerPrivilegeOfUser($user,$t['tags'])) )
+		{
+			$tmp[] = $t;
 		}
 	}
-	sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
+	$tiddler_list = $tmp;
+	
+	if( sizeof($tiddler_list)==0 ) {		//if empty
+		sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
+	}
+	
+	//print revision list
+	$output = "";
+	foreach($tiddler_list as $t) {
+		$output .= $t['modified']." ".$t['revision']." ".$t['modifier']."\n";
+	}
+	//print substr( $output, 0, strlen($output) - 1 );
+	/*for( $i=sizeof($tiddler_list)-1; $i>=0; $i-- ) {
+		print $tiddler_list[$i]['modified']." ".$tiddler_list[$i]['version']." ".$tiddler_list[$i]['modifier'];
+		if( $i != 0 )
+		{
+			print "\n";
+		}
+	}*/
+	
+	//remove terminating "/n" using substr
+	sendHeader(200,"", substr( $output, 0, strlen($output) - 1 ),1);
+	//returnResult("007");
 ?>
