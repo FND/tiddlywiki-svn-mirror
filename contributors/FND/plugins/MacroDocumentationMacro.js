70b34e7466e39716f5e4c58322fb2b7a6945856b
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-12 20:14:36 +0000

    MacroDocumentationMacro: various fixes and adjustments
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4911 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/MacroDocumentationMacro.js
+++ contributors/FND/plugins/MacroDocumentationMacro.js
@@ -1,41 +1,24 @@
 /***
 |''Name''|MacroDocumentationMacro|
 |''Description''|<...>|
-|''Author''|<...>|
-|''Contributors''|<...>|
-|''Version''|<...>|
-|''Date''|<...>|
-|''Status''|<//unknown//; @@experimental@@; @@beta@@; //obsolete//; stable>|
+|''Author''|FND|
+|''Version''|0.1|
+|''Status''|@@experimental@@|
 |''Source''|<...>|
 |''CodeRepository''|<...>|
-|''Copyright''|<...>|
 |''License''|<...>|
 |''CoreVersion''|<...>|
-|''Requires''|<...>|
-|''Overrides''|<...>|
-|''Feedback''|<...>|
 |''Documentation''|<...>|
 |''Keywords''|<...>|
 !Description
 <...>
 !Notes
 <...>
-!Usage
-{{{
-<<macroDoc>>
-}}}
-!!Parameters
-<...>
-!!Examples
-<<macroDoc>>
-!Configuration Options
-<...>
+<<macroDoc "macroDoc">>
 !Revision History
-!!v<#.#> (<yyyy-mm-dd>)
-* <...>
+!!v0.1 (2008-05-12)
+* initial proof-of-concept implementation
 !To Do
-* parameter for indentation (heading level) // DEBUG: obsolete due to blockquote?
-* 
 * l10n support (e.g. parameters table's column headings)
 !Code
 ***/
@@ -43,10 +26,10 @@
 config.macros.macroDoc = {};
  
 config.macros.macroDoc.handler = function(place, macroName, params, wikifier, paramString, tiddler) {
+	var output = "!Macro Documentation\n";
 	if(params[0]) {
 		output += this.generateDoc(config.macros[params[0]].doc);
 	} else {
-		var output = "";
 		for each(var macro in config.macros) {
 			if(macro.doc)
 				output += this.generateDoc(macro.doc);
@@ -56,54 +39,42 @@ config.macros.macroDoc.handler = function(place, macroName, params, wikifier, pa
 };
 
 config.macros.macroDoc.generateDoc = function(doc) {
-	output = "<<<\n";
-	output += "!Description\n" + doc.desc + "\n" // DEBUG: missing macro name!?
-		+ "!Usage\n{{{\n" + doc.usage + "\n}}}\n";
+	output = "; " + "[macroName]" + "\n" // DEBUG: how to retrieve macro name?; use heading?
+		+ "!!Description\n" + doc.desc + "\n"
+		+ "!!Usage\n{{{\n" + doc.usage + "\n}}}\n";
 	if(doc.params) {
-		output += "!!Parameters\n"
+		output += "!!!Parameters\n"
 			+ "|!Name/Index|!Description|!Type|!Optional|!Default Value|h\n";
 		for(var i = 0; i < doc.params.length; i++) {
 			var p = doc.params[i];
-			if(p.named)
-				output += "|" + p.name;
-			else
-				output += "|" + (i + 1);
-			output += "|" + p.desc;
-			output += "|" + p.type;
-			output += "|" + p.optional;
-			output += "|" + p.defaultValue
+			output += p.named ? "|" + p.name : "|" + (i + 1)
+				+ "|" + p.desc
+				+ "|" + p.type
+				+ "|" + (p.optional ? "yes" : "no")
+				+ "|" + (p.defaultValue ? p.defaultValue : "N/A")
 				+ "|\n";
 		}
 	}
 	if(doc.examples) {
 		// DEBUG: to do
 	}
-	output += "<<<\n";
-	console.log(output); // DEBUG
 	return output;
 };
 
 config.macros.macroDoc.doc = {
 	desc: "automatically renders built-in macro documentation",
-	usage: "<<macroDoc [macroName] [headingLevel]>>",
+	usage: "<<macroDoc [macroName]>>",
 	params: [
 		{
 			named: false,
-			desc: "macro name",
+			desc: "name of the macro to document",
 			type: "string",
 			optional: true,
 			defaultValue: null
-		}, {
-			named: false,
-			desc: "heading level",
-			type: "integer",
-			optional: true,
-			defaultValue: 0
 		}
-		],
+	],
 	examples: [
-		"<<macroDoc>>",
-		"<<macroDoc 'macroDoc' '2'>>"
+		"<<macroDoc 'macroDoc'>>"
 	]
 };
 //}}}
