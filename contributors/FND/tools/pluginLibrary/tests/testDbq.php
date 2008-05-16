b4b57e96ee90224e9b05a201c793b6a25d484d37
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-16 11:06:14 +0000

    Plugin Library: sample tests
    git-svn-id: http://svn.tiddlywiki.org/Trunk@4971 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/tools/pluginLibrary/tests/testDbq.php
+++ contributors/FND/tools/pluginLibrary/tests/testDbq.php
@@ -2,12 +2,18 @@
 
 require_once("../dbq.php");
 
-class TestOfDbq extends UnitTestCase {
-    function testDatabaseConnection() {
+class testDatabaseConnection extends UnitTestCase {
+	function testReturnTrueOnSuccess() {
 		$dbq = new dbq();
 		$db = $dbq->connectToDB();
-        $this->assertTrue($db);
-    }
+		$this->assertTrue($db);
+	}
+
+	function testReturnFalseOnFailure() {
+		$dbq = new dbq();
+		$db = $dbq->connectToDB("fakeHost"); // DEBUG: leads to die()
+		$this->assertFalse($db);
+	}
 }
 
 ?>
