c7a0ac8e5d97c7defafe6f86da17944eed0e2833
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-20 14:55:01 +0000

    Ripplerap: Further skinning and refinements
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5069 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/core/plugins/RippleRapPlugin.js
+++ verticals/ripplerap/core/plugins/RippleRapPlugin.js
@@ -29,6 +29,37 @@ config.macros.RippleRap.init = function(){
 	
 };
 
+
+
+config.macros.RippleRap = {};
+config.macros.RippleRap.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	var params = paramString.parseParams("anon",null,true,false,false);
+	var option = getParam(params,"option",null);
+	switch(option) { 
+	   case "makeNote" : 
+	      this.makeNoteButton(place);
+	      break; 
+	   case "setSharingPrefs" : 
+	      break; 
+	   default : 
+	      break;
+	} 
+};
+
+
+// Create a button for the makeNote feature
+config.macros.RippleRap.makeNoteButton = function(place){
+	createTiddlyButton(place,"make notes","Make notes on this session",this.makeNoteButtonClick);
+};
+
+// make a note. Possibly for sharing.
+config.macros.RippleRap.makeNoteButtonClick = function(ev){
+	var e = ev ? ev : window.event;
+	var target = resolveTarget(e);
+	alert("Let's make some notes");
+
+};
+
 // Display discovered noted in the agenda UI
 config.macros.RippleRap.displayNotesLinks = function(){
 
@@ -43,12 +74,7 @@ config.macros.RippleRap.setSharingPreferences = function(){
 
 };
 
-// make a note. Possibly for sharing.
-config.macros.RippleRap.makeNote = function(){
 
-	
-
-};
 
 
 }
