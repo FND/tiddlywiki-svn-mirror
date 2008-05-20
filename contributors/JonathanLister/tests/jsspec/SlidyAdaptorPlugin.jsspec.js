db1bfdd6b950cf402c5ada4e0d94a7258aa46817
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-20 14:24:25 +0000

    SlidyTiddly: adding recipe for test harness
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5067 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/tests/jsspec/SlidyAdaptorPlugin.jsspec.js
+++ contributors/JonathanLister/tests/jsspec/SlidyAdaptorPlugin.jsspec.js
@@ -6,13 +6,29 @@ describe('SlidyTiddly: slidyToTiddlers(slidy)', {
 		value_of(actual).should_be(expected);
 	},
 	
-	'it should convert div\'s with a class of "slide" into tiddlers': function() {
-		var actual = tiddlers.length;
-		var expected = 3;
+	'it should convert div\'s with a class of "slide" into tiddlers tagged "slide" unless the div also has a class of "cover"': function() {
+		var actual = 0;
+		for(var i=0;i<tiddlers.length;i++) {
+			if(tiddlers[i].tags.contains("slide") && tiddlers[i].tags.contains("cover")!=true) {
+				actual++;
+			}
+		}
+		var expected = 2;
 		value_of(actual).should_be(expected);
 	},
 	
-	'it should use the first h1 inside the div for a tiddler title': function() {
+	'it should convert a div with the classes "slide" and "cover" into a tiddler tagged "coverslide"': function() {
+		var actual = 0;
+		for(var i=0;i<tiddlers.length;i++) {
+			if(tiddlers[i].tags.contains("slide") && tiddlers[i].tags.contains("cover")) {
+				actual++;
+			}
+		}
+		var expected = 1;
+		value_of(actual).should_be(expected);
+	},
+	
+	'it should use the first h1 inside the slide div for a tiddler title': function() {
 		var expected = [
 			"HTML Slidy: Slide Shows in XHTML",
 			"Slide Shows in XHTML",
@@ -21,27 +37,37 @@ describe('SlidyTiddly: slidyToTiddlers(slidy)', {
 		var actual;
 		value_of(tiddlers.length === 0).should_be_false();
 		for(var i=0;i<tiddlers.length;i++) {
-			actual = tiddlers[i].title;
-			value_of(actual).should_be(expected[i]); 
+			if(tiddlers[i].tags.contains("slide")) {
+				actual = tiddlers[i].title;
+				value_of(actual).should_be(expected[i]);
+			}
 		}
 	},
 	
-	'it should use the rest of the div content as the text of the tiddler': function() {
-		var expected = [
-			"<p><a href=\"http://www.w3.org/People/Raggett/\">Dave Raggett</a>, &lt;<a href=\"mailto:dsr@w3.org\">dsr@w3.org</a>&gt;<br /><br /><br /><br /><br /><em>Hit the space bar for next slide</em></p>",
-			"<ul><li>You can now create accessible slide shows with ease</li><li>Works across browsers and is operated like PowerPoint  <ul><li>Advance to next slide with mouse click or space bar</li><li>Move forward/backward between slides with Cursor Left, Cursor Right, <strong>Pg Up</strong> and <strong>Pg Dn</strong> keys</li><li><strong>Home</strong> key for first slide, <strong>End</strong>   key for last slide</li><li>The \"<strong>C</strong>\" key for an automatically generated table of contents (or click on \"contents\" on the toolbar)</li><li>Function <strong>F11</strong> to go full screen and back</li><li>The \"<strong>F</strong>\" key toggles the display of the footer</li><li>The \"<strong>A</strong>\" key toggles display of current vs all slides  <ul><li>Use the \"A\" key when you want to view or print all slides</li><li>Try it now to see how to include notes for handouts (this is explained in the notes following this slide)</li></ul></li><li>Font sizes automatically adapt to browser window size  <ul><li>use <strong>S</strong> and <strong>B</strong> keys for manual control (or &lt; and &gt;, or the <strong>-</strong> and  <strong>+</strong> keys on the number pad</li><li>See also the <a href=\"#(13)\">use of the meta element for adjusting the default Slidy behavior</a></li></ul></li><li>Switching off JavaScript reveals all slides</li></ul></li><li><em>Now move to next slide to see how it works</em></li></ul><p class=\"copyright\"><a rel=\"Copyright\" href= \"http://www.w3.org/Consortium/Legal/ipr-notice#Copyright\" shape= \"rect\">Copyright</a> &copy; 2005-2006 <a href=\"/\"><acronym title= \"World Wide Web Consortium\">W3C</acronym></a><sup>&reg;</sup>  (<a href=\"http://www.csail.mit.edu/\"><acronym title= \"Massachusetts Institute of Technology\">MIT</acronym></a>, <a href= \"http://www.ercim.org/\"><acronym title= \"European Research Consortium for Informatics and Mathematics\">ERCIM</acronym></a>, <a href=\"http://www.keio.ac.jp/\">Keio</a>), All Rights Reserved.</p>",
-			"<ul><li>Each presentation is a single XHTML file</li><li>Each slide is enclosed in <em>&lt;div class=\"slide\"&gt; ...  &lt;/div&gt;</em><ul><li>The div element will be created automatically for h1 elements that are direct children of the body element.</li></ul></li><li>Use regular markup within each slide</li><li>The document head includes two links: <ul><li>The slide show style sheet: <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.css\">http://www.w3.org/Talks/Tools/Slidy/slidy.css</a></li><li>The slide show script: <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.js\">http://www.w3.org/Talks/Tools/Slidy/slidy.js</a></li><li>Or you can link to the compressed version of the script which is about one seventh the size, see <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.js.gz\">http://www.w3.org/Talks/Tools/Slidy/slidy.js.gz</a></li><li>If you are using XHTML, remember to use &lt;/script&gt; and &lt;/style&gt; as per <a href=\"http://www.w3.org/TR/xhtml1/#C_3\">Appendix C.3</a></li></ul></li></ul><pre> &lt;?xml version=\"1.0\" encoding=\"utf-8\"?&gt;  &lt;!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"&gt; &lt;html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\"&gt;  &lt;head&gt;    &lt;title&gt;Slide Shows in XHTML&lt;/title&gt;    &lt;meta name=\"copyright\"     content=\"Copyright &amp;#169; 2005 your copyright notice\" /&gt;    &lt;link rel=\"stylesheet\" type=\"text/css\" media=\"screen, projection, print\"     href=\"http://www.w3.org/Talks/Tools/Slidy/slidy.css\" /&gt;    &lt;script src=\"http://www.w3.org/Talks/Tools/Slidy/slidy.js\"     charset=\"utf-8\" type=\"text/javascript\"&gt;&lt;/script&gt;    &lt;style type=\"text/css\"&gt;      &lt;!-- your custom style rules --&gt;    &lt;/style&gt;    &lt;/head&gt; &lt;body&gt;    ... your slides marked up in XHTML ... &lt;/body&gt; &lt;/html&gt; </pre>"
-		];
+	'it should use the rest of the slide div content as the text of the tiddler': function() {
+		var expected = [];
+		var slide = document.createElement("div");
+		slide.innerHTML = "<p><a href=\"http://www.w3.org/People/Raggett/\">Dave Raggett</a>, &lt;<a href=\"mailto:dsr@w3.org\">dsr@w3.org</a>&gt;<br /><br /><br /><br /><br /><em>Hit the space bar for next slide</em></p>";
+		expected.push(slide.innerHTML);
+		slide.innerHTML = "<ul><li>You can now create accessible slide shows with ease</li><li>Works across browsers and is operated like PowerPoint  <ul><li>Advance to next slide with mouse click or space bar</li><li>Move forward/backward between slides with Cursor Left, Cursor Right, <strong>Pg Up</strong> and <strong>Pg Dn</strong> keys</li><li><strong>Home</strong> key for first slide, <strong>End</strong>   key for last slide</li><li>The \"<strong>C</strong>\" key for an automatically generated table of contents (or click on \"contents\" on the toolbar)</li><li>Function <strong>F11</strong> to go full screen and back</li><li>The \"<strong>F</strong>\" key toggles the display of the footer</li><li>The \"<strong>A</strong>\" key toggles display of current vs all slides  <ul><li>Use the \"A\" key when you want to view or print all slides</li><li>Try it now to see how to include notes for handouts (this is explained in the notes following this slide)</li></ul></li><li>Font sizes automatically adapt to browser window size  <ul><li>use <strong>S</strong> and <strong>B</strong> keys for manual control (or &lt; and &gt;, or the <strong>-</strong> and  <strong>+</strong> keys on the number pad</li><li>See also the <a href=\"#(13)\">use of the meta element for adjusting the default Slidy behavior</a></li></ul></li><li>Switching off JavaScript reveals all slides</li></ul></li><li><em>Now move to next slide to see how it works</em></li></ul><p class=\"copyright\"><a rel=\"Copyright\" href= \"http://www.w3.org/Consortium/Legal/ipr-notice#Copyright\" shape= \"rect\">Copyright</a> &copy; 2005-2006 <a href=\"/\"><acronym title= \"World Wide Web Consortium\">W3C</acronym></a><sup>&reg;</sup>  (<a href=\"http://www.csail.mit.edu/\"><acronym title= \"Massachusetts Institute of Technology\">MIT</acronym></a>, <a href= \"http://www.ercim.org/\"><acronym title= \"European Research Consortium for Informatics and Mathematics\">ERCIM</acronym></a>, <a href=\"http://www.keio.ac.jp/\">Keio</a>), All Rights Reserved.</p>";
+		expected.push(slide.innerHTML);
+		slide.innerHTML = "<ul><li>Each presentation is a single XHTML file</li><li>Each slide is enclosed in <em>&lt;div class=\"slide\"&gt; ...  &lt;/div&gt;</em><ul><li>The div element will be created automatically for h1 elements that are direct children of the body element.</li></ul></li><li>Use regular markup within each slide</li><li>The document head includes two links: <ul><li>The slide show style sheet: <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.css\">http://www.w3.org/Talks/Tools/Slidy/slidy.css</a></li><li>The slide show script: <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.js\">http://www.w3.org/Talks/Tools/Slidy/slidy.js</a></li><li>Or you can link to the compressed version of the script which is about one seventh the size, see <a href= \"http://www.w3.org/Talks/Tools/Slidy/slidy.js.gz\">http://www.w3.org/Talks/Tools/Slidy/slidy.js.gz</a></li><li>If you are using XHTML, remember to use &lt;/script&gt; and &lt;/style&gt; as per <a href=\"http://www.w3.org/TR/xhtml1/#C_3\">Appendix C.3</a></li></ul></li></ul><pre> &lt;?xml version=\"1.0\" encoding=\"utf-8\"?&gt;  &lt;!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"&gt; &lt;html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\"&gt;  &lt;head&gt;    &lt;title&gt;Slide Shows in XHTML&lt;/title&gt;    &lt;meta name=\"copyright\"     content=\"Copyright &amp;#169; 2005 your copyright notice\" /&gt;    &lt;link rel=\"stylesheet\" type=\"text/css\" media=\"screen, projection, print\"     href=\"http://www.w3.org/Talks/Tools/Slidy/slidy.css\" /&gt;    &lt;script src=\"http://www.w3.org/Talks/Tools/Slidy/slidy.js\"     charset=\"utf-8\" type=\"text/javascript\"&gt;&lt;/script&gt;    &lt;style type=\"text/css\"&gt;      &lt;!-- your custom style rules --&gt;    &lt;/style&gt;    &lt;/head&gt; &lt;body&gt;    ... your slides marked up in XHTML ... &lt;/body&gt; &lt;/html&gt; </pre>";
+		expected.push(slide.innerHTML);
+		delete slide;
 		var actual;
 		value_of(tiddlers.length === 0).should_be_false();
 		for(var i=0;i<tiddlers.length;i++) {
-			actual = tiddlers[i].text;
-			value_of(actual).should_be(expected[i]); 
+			if(tiddlers[i].tags.contains("slide")) {
+				actual = tiddlers[i].text;
+				value_of(actual).should_be(expected[i]);
+			}
 		}
 	},
 	
 	'it should use a meta tag named "copyright" to create a copyright tiddler': function() {
-		var expected = "Copyright &#169; 2005 W3C (MIT, ERCIM, Keio)";
+		var copyright = document.createElement("div");
+		copyright.innerHTML = "Copyright &#169; 2005 W3C (MIT, ERCIM, Keio)";
+		var expected = copyright.innerHTML;
 		var actual = "";
 			for(var i=0;i<tiddlers.length;i++) {
 				if(tiddlers[i].title == "copyright") {
