f99be728e4f9fcd78294eb3706db39e94f6f5100
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-21 19:30:27 +0000

    AgendaTrack macro - SpeakerSessions works with session_title field
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5125 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/AgendaTrack.js
+++ contributors/PaulDowney/plugins/AgendaTrack.js
@@ -35,7 +35,7 @@ version.extensions.AgendaTrack = {installed:true};
 		wikify(text,place);
 	};
 
-	config.macros.AgendaTrackSessions= {};
+	config.macros.AgendaTrackSessions = {};
 	config.macros.AgendaTrackSessions.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 
 		var track = store.getValue(tiddler,'rr_session_tag');
@@ -47,7 +47,7 @@ version.extensions.AgendaTrack = {installed:true};
 		wikify(text,place);
 	};
 
-	config.macros.Speaker= {};
+	config.macros.Speaker = {};
 	config.macros.Speaker.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 		var co = store.getValue(tiddler,'speaker_co');
 		var text = "[img["+ store.getValue(tiddler,'speaker_img') + "]]"
@@ -58,29 +58,31 @@ version.extensions.AgendaTrack = {installed:true};
 		wikify(text,place);
 	};
 
-	config.macros.SpeakerSession= {};
-	config.macros.SpeakerSession.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	config.macros.SpeakerSessions = {};
+	config.macros.SpeakerSessions.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 
 		var tagged = store.getTaggedTiddlers("session");
 
 		// search for sessions where speaker list contains speaker
-		// TBD: there must be a better way of doing this!
 		var sess = [];
 		for(var t=0; t<tagged.length; t++) {
-		    var speakers = store.getValue(tagged[t],'rr_session_speaker');
+		    var speakers = store.getValue(tagged[t],'rr_session_speakers');
 		    if (speakers) {
 			var slist = speakers.split(/\s*,\s*/);
 			for(var s=0; s<slist.length; s++) {
 			    if (slist[s].trim() == tiddler.title) {
-				sess.push(tagged[t].title);
+				sess.push("[[" + store.getValue(tagged[t],'rr_session_title') + "|" + tagged[t].title + "]]");
 			    }
 			}
 		    }
 		}
 
-		var text = (sess.length ? "*[[" + sess.join("]]\n*[[") + "]]" : "")
-			+ "\n<html><i>Taken from: <a href='" + store.getValue(tiddler,'speaker_bio', '') + "'>"
-			+ store.getValue(tiddler,'speaker_bio', '') + "</a></i></html>";
+		var text = sess.length ? "*" + sess.join("\n*") + "" : "";
+
+		var bio = store.getValue(tiddler,'speaker_bio', '');
+		if (bio) {
+			text = text + "\n//Taken from: "+ bio + "//";
+		}
 		wikify(text,place);
 	};
 
