0651a3dbf98f300f1faf53c266bbd103dd97031e
SaqImtiaz <SaqImtiaz@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-01 04:17:00 +0000

    WikispacesOffline vertical: recipe updates Added new vertical: Hesperian
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5298 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/SaqImtiaz/verticals/Hesperian/settings.js
@@ -0,0 +1,6 @@
+config.options.chkImportWorkspaceOnStartup = true;
+//config.options.chkBackstage = true;
+
+//config.macros.sync.syncStatusList.none.display = 'none';
+//config.macros.sync.syncStatusList.changedServer.display = 'none';
+//config.macros.sync.syncStatusList.changedLocally.display = 'none';
