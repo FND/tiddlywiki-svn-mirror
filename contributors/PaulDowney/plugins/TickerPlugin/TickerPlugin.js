6bba38daf88600f2974f62734a00e65a07463a7d
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-02 14:46:00 +0000

    TickerPlugin - refreshing, maths out of the spout
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5317 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/TickerPlugin/TickerPlugin.js
+++ contributors/PaulDowney/plugins/TickerPlugin/TickerPlugin.js
@@ -8,7 +8,7 @@
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.2|
 
-depends upon YYYYMMDDHHMMSSMMMPlugin.js
+depends upon YYYYMMDDHHMMSSMMMPlugin.js or core fixes to Date functions
 
 ticker tiddlers are:
 * tagged "ticker"
@@ -19,7 +19,7 @@ ticker tiddlers are:
 if(!version.extensions.TickerPlugin) {
 version.extensions.TickerPlugin = {installed:true};
 
-config.options.txtTickerInterval = undefined;
+config.options.txtTickerInterval = 60;
 config.optionsDesc.txtTickerInterval = "~Ticker interval in seconds";
             
 config.macros.Ticker = {
@@ -43,59 +43,57 @@ config.macros.Ticker = {
 		var interval = config.macros.Ticker.getInterval();
 
 		for(var i=0;i<tiddlers.length;i++){
-			var remaining = me.checkTiddler(tiddlers[i]);
-			if(remaining && remaining < interval) {
+			var remaining = me.checkTiddler(tiddlers[i], new Date());
+			if(remaining > 0 && remaining < interval) {
 				interval = remaining;
 			}
 		}
 		if (interval <= 0) {
 			interval = me.getInterval(me.min_interval);
 		}
-		console.log("remaining: ", interval);
 		window.setTimeout(arguments.callee, interval);
 	},
 
 	getInterval: function(interval) {
 		var me = config.macros.Ticker;
-		if(isNaN(parseInt(interval,10))) {
-			interval = parseInt(me.txtTickerInterval,10);
+		if(isNaN(interval)) {
+			interval = parseInt(config.options.txtTickerInterval,10);
 			if(isNaN(interval)) {
-				interval = 2;
+				interval = 60;
 			}
 		}
 		return interval * 1000;
 	},
 
-	checkTiddler: function(tiddler) {
+	checkTiddler: function(tiddler, now) {
 		var me = config.macros.Ticker;
-		var now = new Date();
 		var interval = me.getInterval(tiddler.fields.ticker_interval);
 		var lastcalled;
 
-		if (!isNaN(tiddler.fields.ticker_lastcalled)){
-		    lastcalled = Date.convertFromYYYYMMDDHHMMSSMMM(tiddler.fields.ticker_lastcalled);
-		}
-		if (isNaN(lastcalled)){
-		    lastcalled = 0;
-		}
 		if (isNaN(tiddler.fields.ticker_count)){
 			tiddler.fields.ticker_count = 0;
 		}
-		//if (now <= (lastcalled+interval)){
+		if (!isNaN(tiddler.fields.ticker_lastcalled)){
+			lastcalled = Number(Date.convertFromYYYYMMDDHHMMSSMMM(tiddler.fields.ticker_lastcalled));
+		}
+		if (isNaN(lastcalled)){
+			lastcalled = 0;
+		}
+		if (Number(now) >= (lastcalled+interval)){
 			me.invokeTiddler(tiddler);
 			tiddler.fields.ticker_lastcalled = now.convertToYYYYMMDDHHMMSSMMM();
 			tiddler.fields.ticker_count++;
 			return interval;
-		//}
-		return (interval + lastcalled - now);
+		}
+		return (interval + lastcalled - Number(now));
 	},
 
 	invokeTiddler: function(tiddler) {
-	    var s = story.refreshTiddler(tiddler.title,null,true);
-	    if (!story.refreshTiddler(tiddler.title,null,true)) {
-		    var me = config.macros.Ticker;
-		    wikify(tiddler.text,null,null,null);
-	    }
+		var s = story.refreshTiddler(tiddler.title,null,true);
+		if (!story.refreshTiddler(tiddler.title,null,true)) {
+			var me = config.macros.Ticker;
+			wikify(tiddler.text,null,null,null);
+		}
 	}
 };
 
--- contributors/PaulDowney/plugins/TickerPlugin/TickerPlugin.jsspec.js
+++ contributors/PaulDowney/plugins/TickerPlugin/TickerPlugin.jsspec.js
@@ -41,13 +41,13 @@ describe('TickerPlugin getInterval', {
 	'config.macros.Ticker.getInterval by default returns one minute in milliseconds' : function() {
 		value_of(config.macros.Ticker.getInterval()).should_be(60000);
 	},
-	'config.macros.Ticker.getInterval with a config.options.txtTickerInterval value of 10 returns 10000 milliseconds' : function() {
+	'config.macros.Ticker.getInterval with a config.options.txtTickerInterval value of 10 should return 10000 milliseconds' : function() {
 		tests_mock.save("config.options.txtTickerInterval");
 		config.options.txtTickerInterval = 10;
 		value_of(config.macros.Ticker.getInterval()).should_be(10000);
 		tests_mock.restore();
 	},
-	'config.macros.Ticker.getInterval with a config.options.txtTickerInterval value of "foo" returns 60000 milliseconds' : function() {
+	'config.macros.Ticker.getInterval with a config.options.txtTickerInterval value of "foo" should return 60000 milliseconds' : function() {
 		tests_mock.save("config.options.txtTickerInterval");
 		config.options.txtTickerInterval = "foo";
 		value_of(config.macros.Ticker.getInterval()).should_be(60000);
@@ -67,13 +67,20 @@ describe('TickerPlugin checkTiddler', {
 		__main();
 		store.create
 		tests_mock.before('config.macros.Ticker.invokeTiddler');
-		tiddler = store.createTiddler("tickerTest");
+		var tiddler = store.createTiddler("tickerTest");
+		tiddler.tags.pushUnique("ticker");
+		tiddler.fields.ticker_interval = 10;
+		__t = tiddler;
         },
         after_each : function() {
 		tests_mock.after('config.macros.Ticker.invokeTiddler');
 	},
 	'config.macros.Ticker.checkTiddler is a function' : function() {
 		value_of(typeof config.macros.Ticker.checkTiddler).should_be("function");
+	},
+	'config.macros.Ticker.checkTiddler is a function' : function() {
+		__t.fields.ticker_lastcalled = "20080101000000000";
+		value_of(config.macros.Ticker.checkTiddler(__t,new Date(Date.UTC(2008,0,1,0,0,0)))).should_be(10000);
 	},
 });
 
