b2d14e4f8c4a964cdc88c46bb9b60cb6fefa343e
SaqImtiaz <SaqImtiaz@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-03 12:29:40 +0000

    WikispacesOffline vertical and Hesperian vertical: further syn and import improvements
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5347 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SaqImtiaz/verticals/Hesperian/settings.js
+++ contributors/SaqImtiaz/verticals/Hesperian/settings.js
@@ -1,8 +1,7 @@
-config.options.chkImportWorkspaceOnStartup = true;
+//config.options.chkImportWorkspaceOnStartup = true;
 //config.options.chkBackstage = true;
 
-config.macros.quicksync.label = 'synchronize';
+//config.macros.quicksync.label = 'synchronize';
 config.macros.sync.syncStatusList.none.display = 'none';
 //config.macros.sync.syncStatusList.changedServer.display = 'none';
 //config.macros.sync.syncStatusList.changedLocally.display = 'none';
-//config.shadowTiddlers.home='foo';
