2b9d007e22edfc42164fe429285771fd22c2240e
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-04 15:31:45 +0000

    GenerateRss - general tidyness
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5391 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/GenerateRssPlugin.js
+++ contributors/PaulDowney/plugins/GenerateRssPlugin.js
@@ -1,6 +1,6 @@
 /***
 |''Name:''|GenerateRssPlugin|
-|''Description:'generate RSS from set of tiddlers'|HTML||
+|''Description:''|generate RSS from set of tiddlers|
 |''Version:''|0.0.1|
 |''Date:''|April 14, 2008|
 |''Source:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/GenerateAtomPlugin|
@@ -22,11 +22,14 @@ GenerateRss = {};
  *  - orignally from RippleRap
  *  - core function saves *all* tiddlers, so this could be moved to the core?
  */
-GenerateRss.serialize = function(tiddlers,uri)
+GenerateRss.serialize = function(tiddlers,options)
 {
 	var s = [];
 	var now = new Date();
-	var uri = uri || store.getTiddlerText('SiteUrl');
+	if (!options){
+		options = {};
+	}
+	var uri = options.uri || store.getTiddlerText('SiteUrl');
 
 	s.push('<' + '?xml version="1.0"?' + '>');
 	s.push('<rss version="2.0">');
@@ -42,7 +45,7 @@ GenerateRss.serialize = function(tiddlers,uri)
 	s.push('<lastBuildDate>' + now.toGMTString() + '</lastBuildDate>');
 	s.push('<docs>http://blogs.law.harvard.edu/tech/rss</docs>');
 	s.push('<generator>TiddlyWiki ' + version.major + '.' + version.minor + '.' + version.revision + ' (Notes)</generator>');
-	//# The body
+
 	for (var i=0;i<tiddlers.length;i++) {
 		var t = tiddlers[i];
 		s.push('<item>');
@@ -53,17 +56,12 @@ GenerateRss.serialize = function(tiddlers,uri)
 		s.push('<link>' + uri + '#' + encodeURIComponent(String.encodeTiddlyLink(t.title)) + '</link>');
 		s.push('<pubDate>' + t.modified.toGMTString() + '</pubDate>');
 		s.push('<author>' + t.modifier + '</author>');
+		s.push('<tw:wikitext>\n' + t.text.htmlEncode() + '\n</tw:wikitext>');
 		s.push('</item>');
-		/*var item = t.toRssItem(u);
-		if(t.modifier)
-			item += '\n<author>' + t.modifier + '</author>\n';
-		item += '<tw:wikitext>\n' + t.text..htmlEncode() + '\n</tw:wikitext>';
-		s.push('<item>\n' + item + '\n</item>');*/
 	}
-	//# And footer
+
 	s.push('</channel>');
 	s.push('</rss>');
-	//# Save it all
 	return s.join('\n');
 };
 
