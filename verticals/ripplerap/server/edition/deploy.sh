bf01fd4317c6d704da21a3b099bbd3b38280347f
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-04 10:23:03 +0000

    RippleRap deploy script now cooks
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5364 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/server/edition/deploy.sh
+++ verticals/ripplerap/server/edition/deploy.sh
@@ -1,5 +1,9 @@
 #
 #  deploy edition to riplerap.com
 #
+(
+    cd ../../core
+    cook
+)
 scp index.php ripmaster@ripplerap.com:/data/vhost/www.ripplerap.com/html/edition/index.php
 scp ../../core/index.html ripmaster@ripplerap.com:/data/vhost/www.ripplerap.com/html/edition/ripplerap.html
