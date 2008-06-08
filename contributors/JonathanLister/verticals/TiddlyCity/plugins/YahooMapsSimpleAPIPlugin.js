e0a291506efded3ea8173be2fc321a64b058695d
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-08 17:38:31 +0000

    TiddlyCity:added most of the code for the YahooMapsSimpleAPIPlugin; need to sort out how I'm managing fields vs. slices to store information about the reviews
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5454 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/verticals/TiddlyCity/plugins/YahooMapsSimpleAPIPlugin.js
+++ contributors/JonathanLister/verticals/TiddlyCity/plugins/YahooMapsSimpleAPIPlugin.js
@@ -15,33 +15,86 @@ if(!version.extensions.YahooMapsSimpleAPIPlugin) {
 version.extensions.YahooMapsSimpleAPIPlugin = {installed:true};
 
 var YahooMapsSimpleAPI = function(query,context) {
-	this.geoRssXML = "";
+	this.items = [];
 	this.base = "http://api.maps.yahoo.com/Maps/V1/annotatedMaps";
-	this.appid = "SzdB7ATV34GOxwkm9FJKLa8MEcpIX13E0Sbjm.eL1CE7NPnReIlPpvQxqRvptZ7dIQ--";
+	this.namespace = "yahoo:maps";
+	this.params = {
+		appid:"SzdB7ATV34GOxwkm9FJKLa8MEcpIX13E0Sbjm.eL1CE7NPnReIlPpvQxqRvptZ7dIQ--",
+		xmlsrc:""
+	}
 };
 
 YahooMapsSimpleAPI.prototype.makeURL = function() {
-	var url = this.base+'?';
-	for (var i in this.params) {
-		url += '&' + i + '=' + encodeURIComponent(this.params[i]);
-	}
+	var url = this.base;
 	return url;
 };
 
-YahooMapsSimpleAPI.prototype.addElement = function(fields) {
-
+YahooMapsSimpleAPI.prototype.makeDataString = function() {
+	var data = "";
+	this.makeGeoRss();
+	for (var i in this.params) {
+		data += '&' + i + '=' + encodeURIComponent(this.params[i]);
+	}
+	return data;
 };
 
-YahooMapsSimpleAPI.prototype.createGeoRss = function(fields) {
-	// NOTE: can get a lot of the code from http://developer.yahoo.com/maps/simple/V1/jspost.js
-	// create the geoRss wrapper
-	// loop through the geoRss elements
+YahooMapsSimpleAPI.prototype.addElement = function(tiddler) {
+	// makeGeoRss expects:
+	// item.title (don't know what to use for this), item.link (missing at the moment), item.desc (missing at the moment, but will be the body of the popup so should be the text of the review)
+	// item.streetAddress, item.city, item.country (these are all in the fields)
+	var item = {};
+	item.title = ""; // fix
+	item.link = ""; // fix
+	item.desc = ""; // fix
+	item.fields = tiddler.fields;
+	this.items.push(item);
 };
 
 YahooMapsSimpleAPI.prototype.displayMap = function() {
-	// bring in the IFrame plugin
-	// make POST to this.makeURL() with this.geoRssXML as the data
-	// set responseText to the IFrame contents using ifr.modify(html)
+	var ifr = new IFrame();
+	console.log(ifr);
+	var url = this.makeURL();
+	console.log(url);
+	var data = this.makeDataString();
+	console.log(data);
+	var context = {};
+	context.ifr = ifr;
+	doHttp(url,"POST",data,null,null,null,this.displayMapCallback,context,null,true);
+};
+
+YahooMapsSimpleAPI.prototype.displayMapCallback = function(status,context,responseText,url,xhr) {
+	var ifr = context.ifr;
+	console.log(responseText);
+	//ifr.modify(responseText);
+};
+
+YahooMapsSimpleAPI.prototype.makeGeoRss = function() {
+
+	var xml = "";
+	var items = this.items;
+	var namespace = this.namespace;
+	if(items.length > 0) {
+		xml += "<rss version = '2.0'>\n";
+		xml += "<channel xmlns:" + namespace +"='"+this.base+"'>\n";
+		xml += "<title>Your DIY City Guide updates</title>\n";
+		xml += "<link>http://www.tiddlywiki.com/</link>\n";
+		xml += "<description>Locations that you selected to view</description>\n";
+		var item = {};
+		for(var i=0;i<items.length;i++) {
+			item = items[i];
+			xml += "<item>\n";
+			xml += "<title>" + item.title.htmlEncode() + "</title>\n";
+			xml += "<link>" + item.link.htmlEncode() + "</link>\n";
+			xml += "<description>" + item.desc.htmlEncode() + "</description>\n";
+			xml += "<" + namespace +":Address>" + item.streetAddress.htmlEncode() + "</" +namespace + ":Address>\n";
+			xml += "<" + namespace + ":CityState>" + item.city.htmlEncode() + "</" + namespace + ":CityState>\n";
+			xml += "<" + namespace + ":Country>" + item.country.htmlEncode() + "</" + namespace + ":Country>\n";
+			xml += "</item>\n";
+		}
+		xml += "</channel>\n";
+		xml += "</rss>";
+	}
+	this.params.xmlsrc = xml;
 };
 
 } //# end of 'install only once'
