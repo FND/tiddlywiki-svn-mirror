a567c58c8e9108389f136604cbd44b0e6d8f2d67
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-10 13:31:39 +0000

    mptw: doneclose now works if you rename the tiddler
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5502 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mptw/trunk/core/SaveCloseTiddlerPlugin.js
+++ contributors/SimonBaird/mptw/trunk/core/SaveCloseTiddlerPlugin.js
@@ -14,20 +14,21 @@ merge(config.commands,{
 	saveCloseTiddler: {
 		text: 'done/close',
 		tooltip: 'Save changes to this tiddler and close it',
-		handler: function(e,src,title) {
-			config.commands.saveTiddler.handler(e,src,title);
-			config.commands.closeTiddler.handler(e,src,title);
-			return false;
+		handler: function(ev,src,title) {
+			var closeTitle = title;
+			var newTitle = story.saveTiddler(title,ev.shiftKey);
+			if (newTitle)
+				closeTitle = newTitle;
+			return config.commands.closeTiddler.handler(ev,src,closeTitle);
 		}
 	},
 
 	cancelCloseTiddler: {
 		text: 'cancel/close',
 		tooltip: 'Undo changes to this tiddler and close it',
-		handler: function(e,src,title) {
-			config.commands.cancelTiddler.handler(e,src,title);
-			config.commands.closeTiddler.handler(e,src,title);
-			return false;
+		handler: function(ev,src,title) {
+			// the same as closeTiddler now actually
+			return config.commands.closeTiddler.handler(ev,src,title);
 		}
 	}
 
