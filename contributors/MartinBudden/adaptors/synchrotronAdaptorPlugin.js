6ddc472f904307f4fee315b74093f86ee99c01be
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-11 17:58:08 +0000

    Minor fixes
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5546 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/adaptors/synchrotronAdaptorPlugin.js
+++ contributors/MartinBudden/adaptors/synchrotronAdaptorPlugin.js
@@ -154,7 +154,7 @@ synchrotronAdaptor.prototype.getWorkspaceList = function(context,userParams,call
 
 synchrotronAdaptor.prototype.getTiddlerList = function(context,userParams,callback)
 {
-//#console.log('synchrotronAdaptor.getTiddlerList');
+console.log('synchrotronAdaptor.getTiddlerList');
 	context = this.setContext(context,userParams,callback);
 	var path = synchrotronAdaptor.contentPath();
 	var entries = this.dirList(path);
@@ -187,30 +187,25 @@ synchrotronAdaptor.prototype.getTiddlerList = function(context,userParams,callba
 synchrotronAdaptor.prototype.getTiddlerRevisionList = function(title,limit,context,userParams,callback)
 // get a list of the revisions for a tiddler
 {
-//#displayMessage('synchrotronAdaptor.getTiddlerRevisionList');
+console.log('synchrotronAdaptor.getTiddlerRevisionList');
 	context = this.setContext(context,userParams,callback);
-	var path = synchrotronAdaptor.revisionPath();
+	var tiddler = store.getTiddler(title);
+	context.revisions = [];
+	context.status = true;
+	var entries = null;
+	if(tiddler.fields.guid)
+		entries = repo.fileRevisions(tiddler.fields.guid);
 //#displayMessage('revisionPath:'+path);
-	var entries = this.dirList(path);
 	if(entries) {
 		var list = [];
 		for(var i=0; i<entries.length; i++) {
-			var name = entries[i].name;
-			//# need to match name with
-			//# dirlist/<title>.<nnnn>.tiddler
-			var matchRegExp = /(.*?)\.([0-9]*)\.([0-9]*)\.tiddler/;
-			matchRegExp.lastIndex = 0;
-			var match = matchRegExp.exec(name);
-			if(match) {
-				if(match[1]==title) {
-					var tiddler = new Tiddler(title);
-					tiddler.modified = Date.convertFromYYYYMMDDHHMM(match[2]);
-					//#tiddler.modified = new Date();
-					//#tiddler.modified.setTime(entries[i].modified);
-					tiddler.fields['server.page.revision'] = match[3];
-					list.push(tiddler);
-				}
-			}
+			var body = repo.getBody(entries[i], tiddler.fields.guid);
+			title = body.title;
+			tiddler = new Tiddler(title+i);
+			tiddler.modified = entries[i].timestamp;
+			tiddler.text = body.text.join("\n");
+			tiddler.fields['server.page.revision'] = i;
+			list.push(tiddler);
 		}
 		context.revisions = list;
 		context.status = true;
@@ -322,129 +317,6 @@ synchrotronAdaptor.prototype.getTiddler = function(title,context,userParams,call
 	if(context.callback)
 		window.setTimeout(function() {callback(context,userParams);},0);
 	return context.status;
-};
-
-synchrotronAdaptor.prototype.putTiddler = function(tiddler,context,userParams,callback)
-{
-//#clearMessage();
-//#displayMessage('synchrotronAdaptor.putTiddler:' + tiddler.title);
-	context = this.setContext(context,userParams,callback);
-	context.title = tiddler.title;
-	var revision = tiddler.fields['server.page.revision'] || synchrotronAdaptor.baseRevision;
-	++revision;
-	if(synchrotronAdaptor.revisionsDirectory)
-		tiddler.fields['server.page.revision'] = String(revision);
-	else
-		delete tiddler.fields['server.page.revision'];
-		
-	tiddler.clearChangeCount();
-	context.tiddler = tiddler;
-	//# save the tiddler as a revision
-	//#if(synchrotronAdaptor.revisionsDirectory)
-	//#	this.saveTiddlerAsDiv(context,true);
-	context = this.saveTiddlerAsDiv(context);
-	if(context.callback)
-		window.setTimeout(function() {callback(context,userParams);},0);
-	return context.status;
-};
-
-synchrotronAdaptor.prototype.putTiddlerRevision = function(tiddler,context,userParams,callback)
-{
-//#clearMessage();
-//#displayMessage('synchrotronAdaptor.putTiddler:' + tiddler.title);
-	if(!synchrotronAdaptor.revisionsDirectory)
-		return;
-	context = this.setContext(context,userParams,callback);
-	context.title = tiddler.title;
-	if(!tiddler.fields['server.page.revision'])
-		tiddler.fields['server.page.revision'] = String(synchrotronAdaptor.baseRevision);
-		
-	tiddler.clearChangeCount();
-	context.tiddler = tiddler;
-	// save the tiddler as a revision
-	this.saveTiddlerAsDiv(context,true);
-	if(context.callback)
-		window.setTimeout(function() {callback(context,userParams);},0);
-	return context.status;
-};
-
-synchrotronAdaptor.prototype.saveTiddlerAsDiv = function(context,isRevision)
-{
-//#displayMessage('synchrotronAdaptor.saveTiddlerAsDiv:'+context.tiddler.title);
-	var tiddler = context.tiddler;
-	if(isRevision) {
-		if(!synchrotronAdaptor.revisionsDirectory)
-			return;
-		var path = synchrotronAdaptor.revisionPath();
-		var uriTemplate = '%0%1.%2.%3.tiddler';
-		var revision = tiddler.fields['server.page.revision'];
-		var modified = tiddler.modified.convertToYYYYMMDDHHMM();
-	} else {
-		path = synchrotronAdaptor.contentPath();
-		uriTemplate = '%0%1.tiddler';
-	}
-	var uri = uriTemplate.format([path,tiddler.title,modified,revision]);
-//#displayMessage('uri:'+uri);
-	var extendedAttributes = "";
-	store.forEachField(tiddler,
-		function(tiddler,fieldName,value) {
-			if(typeof value != "string")
-				value = "";
-			if(!fieldName.match(/^temp\./))
-				extendedAttributes += ' %0="%1"'.format([fieldName,value.escapeLineBreaks().htmlEncode()]);
-		},true);
-	var created = tiddler.created;
-	var modified = tiddler.modified;
-	var attributes = tiddler.modifier ? ' modifier="' + tiddler.modifier.htmlEncode() + '"' : "";
-	attributes += (created == version.date) ? "" :' created="' + created.convertToYYYYMMDDHHMM() + '"';
-	attributes += (modified == created) ? "" : ' modified="' + modified.convertToYYYYMMDDHHMM() +'"';
-	var tags = tiddler.getTags();
-	if(tags)
-		attributes += ' tags="' + tags.htmlEncode() + '"';
-	var text = synchrotronAdaptor.toFileFormat(tiddler.text);
-	var div = ('<div title="%0"%1%2>%3</div>').format([tiddler.title.htmlEncode(),attributes,extendedAttributes,"\n<pre>" + text + "</pre>\n"]);
-	context.status = saveFile(uri,div);
-	if(context.status) {
-		if(isRevision) {
-			displayMessage(synchrotronAdaptor.revisionSavedMessage.format([revision]));
-		}
-	} else {
-		alert(config.messages.backupFailed);
-	}
-	return context;
-};
-
-synchrotronAdaptor.prototype.saveTiddlerAsJs = function(context)
-{
-	var tiddler = context.tiddler;
-	var path = synchrotronAdaptor.contentPath();
-	var uriTemplate = '%0%1';
-	var uri = uriTemplate.format([path,synchrotronAdaptor.normalizedTitle(tiddler.title)]);
-//#displayMessage('uri:'+uri);
-	var meta = 'title: ' + tiddler.title + '\n';
-	if(tiddler.modifier)
-		meta += 'modifier: ' + tiddler.modifier + '\n';
-	if(tiddler.created)
-		meta += 'created: ' + tiddler.created.convertToYYYYMMDDHHMM() + '\n';
-	if(tiddler.modified)
-		meta += 'modified: ' + tiddler.modified.convertToYYYYMMDDHHMM() + '\n';
-	if(tiddler.getTags())
-		meta += 'tags: ' + tiddler.getTags() + '\n';
-	var fields = tiddler.fields;
-	for(var i in fields) {
-		if(!TiddlyWiki.isStandardField(i) && i != 'anon' && i != 'changecount') {
-			meta += i + ': ' + fields[i] + '\n';
-		}
-	}
-	context.status = saveFile(uri + '.js',tiddler.text);
-	if(context.status) {
-		context.status = saveFile(uri + '.js.meta',meta);
-	}
-	if(context.status)
-		displayMessage(config.messages.backupSaved,uri);
-	else
-		alert(config.messages.backupFailed);
-	return context;
 };
 
 synchrotronAdaptor.prototype.close = function() {return true;};
