df28768bec520c1c9ffd21ce6ba006f1f881da0a
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-12 12:44:43 +0000

    RelatedTagsPlugin: changed declaration of event handlers so that they are correctly inserted into the global scope on IE and Safari; removed relatedtags macro
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5569 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/plugins/RelatedTagsPlugin.js
+++ contributors/JonathanLister/plugins/RelatedTagsPlugin.js
@@ -1,43 +1,10 @@
-config.views.wikified.relatedtag = {
-	labelNoRelatedTags:"no tags",
-	labelRelatedTags:"related tags: "
-};
-
 config.views.wikified.tag.relatedTagsText = "Show related tags";
 config.views.wikified.tag.relatedTagsTooltip = "Show related tags";
-
-config.macros.relatedtags = {};
-config.macros.relatedtags.handler = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-	params = paramString.parseParams("anon",null,true,false,false);
-	var ul = createTiddlyElement(place,"ul");
-	var relatedtags = [];
-	var tags = [];
-	// go and get tags from tag
-	var tag = getParam(params,"anon","");
-	var tiddlers = store.getTaggedTiddlers(tag);
-	for(var i=0; i<tiddlers.length; i++) {
-		tags = tiddlers[i].tags;
-		for(var j=0; j<tags.length; j++) {
-			relatedtags.pushUnique(tags[j]);
-		}
-	}
-	//end go and get tags from tag
-	var sep = getParam(params,"sep"," ");
-	var lingo = config.views.wikified.relatedtag;
-	var prompt = relatedtags.length == 0 ? lingo.labelNoRelatedTags : lingo.labelRelatedTags;
-	createTiddlyElement(ul,"li",null,"listTitle",prompt.format([tag]));
-	for(var t=0; t<relatedtags.length; t++) {
-		if(relatedtags[t]!==tag) {
-			createTagButton(createTiddlyElement(ul,"li"),relatedtags[t],tag);
-			if(t<relatedtags.length-1)
-				createTiddlyText(ul,sep);
-		}
-	}
-};
+config.views.wikified.tag.labelNoRelatedTags = "no tags";
+config.views.wikified.tag.labelRelatedTags = "related tags: ";
 
 // Event handler for clicking on a tiddler tag
-function onClickTag(ev)
+window.onClickTag = function(ev)
 {
 	var e = ev ? ev : window.event;
 	var popup = Popup.create(this);
@@ -75,11 +42,28 @@ function onClickTag(ev)
 }
 
 // Event handler for 'Show related tags' on a tiddler popup
-function onClickRelatedTags(ev)
+window.onClickRelatedTags = function(ev)
 {
 	var e = ev ? ev : window.event;		
-	var popup = Popup.create(this,"ol","popup");
-	wikify("<<relatedtags "+this.getAttribute("tag")+">>",popup);
+	var popup = Popup.create(this);
+	var relatedtags = [];
+	var tags = [];
+	var tag = this.getAttribute("tag");
+	var tiddlers = store.getTaggedTiddlers(tag);
+	for(var i=0; i<tiddlers.length; i++) {
+		tags = tiddlers[i].tags;
+		for(var j=0; j<tags.length; j++) {
+			relatedtags.pushUnique(tags[j]);
+		}
+	}
+	var lingo = config.views.wikified.tag;
+	var prompt = relatedtags.length == 0 ? lingo.labelNoRelatedTags : lingo.labelRelatedTags;
+	createTiddlyElement(popup,"li",null,"listTitle",prompt.format([tag]));
+	for(var t=0; t<relatedtags.length; t++) {
+		if(relatedtags[t]!==tag) {
+			createTagButton(createTiddlyElement(popup,"li"),relatedtags[t],tag);
+		}
+	}
 	Popup.show();
 	e.cancelBubble = true;
 	if (e.stopPropagation) e.stopPropagation();
