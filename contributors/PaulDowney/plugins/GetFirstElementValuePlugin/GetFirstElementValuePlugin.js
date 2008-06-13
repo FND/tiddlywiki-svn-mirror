a1ae73730c0947b8551489a021436536d01cab13
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-13 21:05:00 +0000

    GetFirstElementValuePlugin - DOM helper functions
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5605 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/GetFirstElementValuePlugin/GetFirstElementValuePlugin.js
@@ -0,0 +1,28 @@
+/***
+|''Name:''|GetFirstElementValuePlugin|
+|''Description:''|DOM helper functions to return node value with default |
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/GetFirstElementValuePlugin |
+|''Version:''|0.1|
+|''License:''|[[BSD open source license]]|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.2|
+
+***/
+
+//{{{
+if(!version.extensions.GetFirstElementValuePlugin) {
+version.extensions.GetFirstElementValuePlugin = {installed:true};
+
+getFirstElementByTagNameValue = function (node, tag, def) {
+        if (node){
+                var e = node.getElementsByTagName(tag);
+                if (e && e.length){
+                        def = e[0].textContent || e[0].text || def;
+                }
+        }
+        return def;
+};
+
+} //# end of 'install only once'
+//}}}
