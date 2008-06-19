a48dd2b5fe39a60f90a1fd7444aac6921dead02b
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-19 17:00:12 +0000

    RelatedTagsPluginWithExtendablePopup: broken plugin into three plugins - CorePopupPlugin, to provide extendable, layout template-based popups; RelatedTagsPlugin; and FilterTagsPopupPlugin, which allows for drilling down through related tags building up a list of tiddlers tagged with a AND b AND c etc.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5687 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JonathanLister/plugins/CorePopupPlugin.js
@@ -0,0 +1,122 @@
+// Extra properties and methods for popup
+// Enables extendable, layout template-based popups
+Popup.layouts = {};
+Popup.handlers = {};
+
+Popup.layout = function(popup,label,params)
+{
+	var layout = this.layouts[label];
+	if(layout && typeof layout == 'function') {
+		layout(popup,params,this.handlers);
+	}
+	if(layout.isExtendable) {
+		for(var i=0;i<layout.extras.length;i++) {
+			layout.extras[i](popup,params,this.handlers);
+		}
+	}
+};
+
+Popup.makeExtendable = function(label) {
+	var layout = this.layouts[label];
+	if(layout) {
+		layout.isExtendable = true;
+		layout.extras = [];
+	}
+};
+
+Popup.extend = function(label,func) {
+	var layout = this.layouts[label];
+	if(layout.isExtendable) {
+		layout.extras.push(func);
+	}
+};
+
+
+// defaults to handle onClickTag
+merge(Popup.handlers, {
+	listTitles: function(popup,titles) {
+		if(titles) {
+			for(r=0; r<titles.length; r++) {
+				createTiddlyLink(createTiddlyElement(popup,"li"),titles[r],true);
+			}
+		}
+	},
+	noTitles: function(popup,label) {
+		createTiddlyText(createTiddlyElement(popup,"li",null,"disabled"),label);
+	},
+	listBreak: function(popup) {
+		createTiddlyElement(createTiddlyElement(popup,"li",null,"listBreak"),"div");
+	},
+	button: function(popup,label,tooltip,callback,attribs) {
+		var btn = createTiddlyButton(createTiddlyElement(popup,"li"),label,tooltip,callback,null,null,null,attribs);
+	},
+	link: function(popup,title,text) {
+		if(title) {
+			var h = createTiddlyLink(createTiddlyElement(popup,"li"),title,false);
+			createTiddlyText(h,text);
+		}
+	}
+});
+
+merge(Popup.layouts, {
+	onClickTag: function(popup,params,handlers) {
+		var lingo = params.lingo;
+		var titles = params.titles;
+		var tag = params.tag;
+		if(popup && titles) {
+			if(titles.length > 0) {
+				handlers.button(popup,lingo.openAllText.format([tag]),lingo.openAllTooltip,onClickTagOpenAll,{tag:tag});
+				handlers.listBreak(popup);
+				handlers.listTitles(popup,titles);
+			} else {
+				handlers.noTitles(popup,lingo.popupNone.format([tag]));
+				handlers.listBreak(popup);
+			}
+			handlers.link(popup,tag,lingo.openTag.format([tag]));
+		}
+	}
+});
+
+Popup.makeExtendable('onClickTag');
+
+window.onClickTag = function(ev)
+{
+	var e = ev || window.event;
+	var popup = Popup.create(this);
+	var tag = this.getAttribute("tag");
+	var title = this.getAttribute("tiddler");
+	var excludeTags = this.getAttribute("excludeTags");
+	var filterFunc = excludeTags ? 'filterTiddlers' : 'getTaggedTiddlers';
+	var filter = "";
+	var tags = [];
+	if(excludeTags) {
+		tags = excludeTags.readBracketedList();
+		filter += "[";
+		for(var i=0;i<tags.length;i++) {
+			filter += "tag["+tags[i]+"]";
+		}
+		filter += "]";
+	} else {
+		filter = tag;
+	}
+	if(popup && filter) {
+		var tagged = store[filterFunc](filter);
+		var titles = [];
+		var li,r;
+		for(r=0;r<tagged.length;r++) {
+			if(tagged[r].title != title)
+				titles.push(tagged[r].title);
+		}
+		var lingo = config.views.wikified.tag;
+		var params = {
+			titles:titles,
+			lingo:lingo,
+			tag:tag
+		};
+		Popup.layout(popup,'onClickTag',params);
+	}
+	Popup.show();
+	e.cancelBubble = true;
+	if(e.stopPropagation) e.stopPropagation();
+	return false;
+};
--- contributors/JonathanLister/plugins/RelatedTagsPlugin.js
+++ contributors/JonathanLister/plugins/RelatedTagsPlugin.js
@@ -1,47 +1,36 @@
+// Extensions specific for adding 'show related tags', as if the above were in the core
+
 config.views.wikified.tag.relatedTagsText = "Show related tags";
 config.views.wikified.tag.relatedTagsTooltip = "Show related tags";
 config.views.wikified.tag.labelNoRelatedTags = "no tags";
 config.views.wikified.tag.labelRelatedTags = "related tags: ";
 
-// Event handler for clicking on a tiddler tag
-window.onClickTag = function(ev)
-{
-	var e = ev ? ev : window.event;
-	var popup = Popup.create(this);
-	var tag = this.getAttribute("tag");
-	var title = this.getAttribute("tiddler");
-	if(popup && tag) {
-		var tagged = store.getTaggedTiddlers(tag);
-		var titles = [];
-		var li,r;
-		for(r=0;r<tagged.length;r++) {
-			if(tagged[r].title != title)
-				titles.push(tagged[r].title);
-		}
-		var lingo = config.views.wikified.tag;
-		if(titles.length > 0) {
-			var openAll = createTiddlyButton(createTiddlyElement(popup,"li"),lingo.openAllText.format([tag]),lingo.openAllTooltip,onClickTagOpenAll);
-			openAll.setAttribute("tag",tag);
-			createTiddlyElement(createTiddlyElement(popup,"li",null,"listBreak"),"div");
-			for(r=0; r<titles.length; r++) {
-				createTiddlyLink(createTiddlyElement(popup,"li"),titles[r],true);
-			}
-		} else {
-			createTiddlyText(createTiddlyElement(popup,"li",null,"disabled"),lingo.popupNone.format([tag]));
-		}
-		createTiddlyElement(createTiddlyElement(popup,"li",null,"listBreak"),"div");
-		var relatedTags = createTiddlyButton(createTiddlyElement(popup,"li"),lingo.relatedTagsText,lingo.relatedTagsTooltip,onClickRelatedTags);
-		relatedTags.setAttribute("tag",tag);
-		var h = createTiddlyLink(createTiddlyElement(popup,"li"),tag,false);
-		createTiddlyText(h,lingo.openTag.format([tag]));
-	}
-	Popup.show();
-	e.cancelBubble = true;
-	if(e.stopPropagation) e.stopPropagation();
-	return false;
-}
+Popup.handlers.text = function(popup,text) {
+	createTiddlyElement(popup,"li",null,null,text);
+};
 
-// Event handler for 'Show related tags' on a tiddler popup
+// Instead of overriding Popup.layouts.onClickTag, make use of the fact it is extendable
+Popup.extend('onClickTag', function(popup,params,handlers) {
+	var lingo = params.lingo;
+	var tag = params.tag;
+	handlers.button(popup,lingo.relatedTagsText,lingo.relatedTagsTooltip,onClickRelatedTags,{tag:tag});
+});
+
+// New layout for popup to be displayed when "show related tags" is clicked
+Popup.layouts.onClickRelatedTags = function(popup,params,handlers) {
+	var lingo = params.lingo;
+	var tag = params.tag;
+	var relatedtags = params.relatedtags;
+	var prompt = relatedtags.length == 0 ? lingo.labelNoRelatedTags : lingo.labelRelatedTags;
+	handlers.text(popup,prompt.format([tag]));
+	for(var t=0; t<relatedtags.length; t++) {
+		if(relatedtags[t]!==tag) {
+			createTagButton(createTiddlyElement(popup,"li"),relatedtags[t],tag);
+		}
+	}
+};
+
+// Event handler for 'Show related tags' on a tiddler tag popup
 window.onClickRelatedTags = function(ev)
 {
 	var e = ev ? ev : window.event;		
@@ -57,15 +46,14 @@ window.onClickRelatedTags = function(ev)
 		}
 	}
 	var lingo = config.views.wikified.tag;
-	var prompt = relatedtags.length == 0 ? lingo.labelNoRelatedTags : lingo.labelRelatedTags;
-	createTiddlyElement(popup,"li",null,"listTitle",prompt.format([tag]));
-	for(var t=0; t<relatedtags.length; t++) {
-		if(relatedtags[t]!==tag) {
-			createTagButton(createTiddlyElement(popup,"li"),relatedtags[t],tag);
-		}
-	}
+	var params = {
+		lingo:lingo,
+		relatedtags:relatedtags,
+		tag:tag
+	};
+	Popup.layout(popup,'onClickRelatedTags',params);
 	Popup.show();
 	e.cancelBubble = true;
 	if (e.stopPropagation) e.stopPropagation();
 	return false;
-}
+};
