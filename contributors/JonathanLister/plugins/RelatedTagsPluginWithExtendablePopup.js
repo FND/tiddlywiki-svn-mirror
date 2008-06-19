ec03892305c7407dcb405720e9fe6a577b67f9d8
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-19 12:22:14 +0000

    RelatedTagsPluginWithExtendablePopup: refactored the Popup code so that handlers are generic and the layout acts as a variable separation between variables set in click handler and variables passed to popup handlers. This makes it even more generic.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5675 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonathanLister/plugins/RelatedTagsPluginWithExtendablePopup.js
+++ contributors/JonathanLister/plugins/RelatedTagsPluginWithExtendablePopup.js
@@ -1,66 +1,59 @@
-/* extra functions for Popup */
+// extra properties and layout method for Popup
 Popup.layouts = {};
+Popup.handlers = {};
 
-Popup.doLayout = function(popup,label,params)
+Popup.layout = function(popup,label,params)
 {
-	if(this.layouts[label] && typeof this.layouts[label].layout == 'function') {
-		this.layouts[label].layout(popup,params);
+	// can we introduce a sort of if-then-else bit to the layouts, so the layouts become genuinely extendable
+	// that way we can keep onClickTag extendable because we add to rather than override the layout
+	if(this.layouts[label] && typeof this.layouts[label] == 'function') {
+		this.layouts[label](popup,params,this.handlers);
 	}
 };
 
-/* extra properties specific to overriding onClickTag */
-Popup.layouts.onClickTag = {};
-Popup.layouts.onClickTag.layout = function(popup,params) {
-	var titles = params.titles;
-	var handlers = this.handlers;
-	if(popup && titles) {
-		if(titles.length > 0) {
-			handlers.openAll(popup,params);
-			handlers.separator(popup);
-			handlers.titles(popup,params);
-		} else {
-			handlers.noTitles(popup,params);
-			handlers.separator(popup,params);
-		}
-		handlers.openTag(popup,params);
-	}
-};
-Popup.layouts.onClickTag.handlers = {
-	openAll: function(popup,params) {
-		var lingo = params.lingo;
-		var tag = params.tag;
-		if(lingo && tag) {
-			var openAll = createTiddlyButton(createTiddlyElement(popup,"li"),lingo.openAllText.format([tag]),lingo.openAllTooltip,onClickTagOpenAll);
-			openAll.setAttribute("tag",tag);
-		}
-	},
-	titles: function(popup,params) {
-		var titles = params.titles;
+merge(Popup.handlers, {
+	listTitles: function(popup,titles) {
 		if(titles) {
 			for(r=0; r<titles.length; r++) {
 				createTiddlyLink(createTiddlyElement(popup,"li"),titles[r],true);
 			}
 		}
 	},
-	noTitles: function(popup,params) {
-		var lingo = params.lingo;
-		var tag = params.tag;
-		if(lingo && tag) {
-			createTiddlyText(createTiddlyElement(popup,"li",null,"disabled"),lingo.popupNone.format([tag]));
-		}
+	noTitles: function(popup,label) {
+		createTiddlyText(createTiddlyElement(popup,"li",null,"disabled"),label);
 	},
-	openTag: function(popup,params) {
-		var lingo = params.lingo;
-		var tag = params.tag;
-		if(lingo && tag) {
-			var h = createTiddlyLink(createTiddlyElement(popup,"li"),tag,false);
-			createTiddlyText(h,lingo.openTag.format([tag]));
-		}
-	},
-	separator: function(popup) {
+	listBreak: function(popup) {
 		createTiddlyElement(createTiddlyElement(popup,"li",null,"listBreak"),"div");
+	},
+	button: function(popup,label,tooltip,callback,attribs) {
+		var btn = createTiddlyButton(createTiddlyElement(popup,"li"),label,tooltip,callback,null,null,null,attribs);
+	},
+	link: function(popup,title,text) {
+		if(title) {
+			var h = createTiddlyLink(createTiddlyElement(popup,"li"),title,false);
+			createTiddlyText(h,text);
+		}
 	}
-};
+});
+
+merge(Popup.layouts, {
+	onClickTag: function(popup,params,handlers) {
+		var lingo = params.lingo;
+		var titles = params.titles;
+		var tag = params.tag;
+		if(popup && titles) {
+			if(titles.length > 0) {
+				handlers.button(popup,lingo.openAllText.format([tag]),lingo.openAllTooltip,onClickTagOpenAll,{tag:tag});
+				handlers.listBreak(popup);
+				handlers.listTitles(popup,titles);
+			} else {
+				handlers.noTitles(popup,lingo.popupNone.format([tag]));
+				handlers.listBreak(popup);
+			}
+			handlers.link(popup,tag,lingo.openTag.format([tag]));
+		}
+	}
+});
 
 // Event handler for clicking on a tiddler tag
 window.onClickTag = function(ev)
@@ -83,7 +76,7 @@ window.onClickTag = function(ev)
 			lingo:lingo,
 			tag:tag
 		};
-		Popup.doLayout(popup,'onClickTag',params);		
+		Popup.layout(popup,'onClickTag',params);
 	}
 	Popup.show();
 	e.cancelBubble = true;
@@ -92,12 +85,13 @@ window.onClickTag = function(ev)
 };
 
 /* Extensions specific for adding 'show related tags', as if the above were in the core */
+/*
 config.views.wikified.tag.relatedTagsText = "Show related tags";
 config.views.wikified.tag.relatedTagsTooltip = "Show related tags";
 config.views.wikified.tag.labelNoRelatedTags = "no tags";
 config.views.wikified.tag.labelRelatedTags = "related tags: ";
 
-Popup.layouts.onClickTag.handlers.relatedTags = function(popup,params) {
+Popup.handlers.relatedTags = function(popup,params) {
 	var lingo = params.lingo;
 	var tag = params.tag;
 	if(lingo && tag) {
@@ -106,7 +100,7 @@ Popup.layouts.onClickTag.handlers.relatedTags = function(popup,params) {
 	}
 };
 
-Popup.layouts.onClickTag.layout = function(popup,params) {
+Popup.layouts.onClickTag = function(popup,params) {
 	var titles = params.titles;
 	var handlers = this.handlers;
 	if(popup && titles) {
@@ -151,3 +145,4 @@ window.onClickRelatedTags = function(ev)
 	if (e.stopPropagation) e.stopPropagation();
 	return false;
 };
+*/
