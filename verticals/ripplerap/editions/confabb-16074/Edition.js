2966c958793742204fbd0b5e8b0f3f194fa0d999
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-22 17:04:56 +0000

    Confabb - made test edition localhost
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5735 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/editions/confabb-16074/Edition.js
+++ verticals/ripplerap/editions/confabb-16074/Edition.js
@@ -1,5 +1,6 @@
 {{
 config.options.txtRippleRapType = "confabb";
 config.options.txtRippleRapConferenceName = "Web 2.0 Conference";
-config.options.txtRippleRapConferenceURI = "http://staging.confabb.com/conferences/16074-web-2-0-conference-2006/";
+//#config.options.txtRippleRapConferenceURI = "http://staging.confabb.com/conferences/16074-web-2-0-conference-2006/";
+config.options.txtRippleRapConferenceURI = "http://localhost/src/tiddlywiki.org/Trunk/verticals/ripplerap/editions/confabb-16074/";
 }}
