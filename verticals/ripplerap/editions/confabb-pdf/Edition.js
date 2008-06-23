de4b9b2ecd6a9c786095cce5f640aed61d616e12
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-23 05:27:28 +0000

    Confabb - test PDF edition
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5748 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/ripplerap/editions/confabb-pdf/Edition.js
@@ -0,0 +1,5 @@
+{{
+config.options.txtRippleRapType = "confabb";
+config.options.txtRippleRapConferenceName = "PDF Conference";
+config.options.txtRippleRapConferenceURI = "http://tiddlywiki.localhost/verticals/ripplerap/editions/confabb-pdf/";
+}}
