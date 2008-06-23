709b39c605654ace5e67b9d7f97234c4536ee15d
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-23 06:39:58 +0000

    Confabb - fixed subtitle, uses non wikiword
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5753 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ripplerap/editions/Confabb/Edition.js
+++ verticals/ripplerap/editions/Confabb/Edition.js
@@ -4,5 +4,5 @@ config.options.pasSharedNotesPassword = "";
 config.options.txtRippleRapType = "confabb";
 config.options.txtRippleRapConferenceName = "";
 config.options.txtRippleRapConferenceURI = "";
-config.options.txtPoweredBy = " using Confabb NoteShare";
+config.options.txtPoweredBy = " using Confabb ~NoteShare";
 }}
