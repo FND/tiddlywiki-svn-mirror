97bb21038b641f45f635754a2b2e6023fe478886
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-30 07:53:24 +0000

    first attempt at automating ccTiddly $1 release
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5794 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Tags/1.6.4/handle/login.php
@@ -0,0 +1,16 @@
+<?php 
+
+$cct_base = "../";
+include_once($cct_base."includes/header.php");
+
+if( isset($_REQUEST['cctuser']) && isset($_REQUEST['cctpass']) )		//set cookie for login
+{	
+	user_login(formatParametersPOST($_POST['cctuser']),formatParametersPOST($_POST['cctpass']));
+}
+
+if (isset($_POST['logout']) || isset($_REQUEST['logout']))
+{
+	user_logout('You have logged out.');
+}
+
+?>
