fc28dc560aedd915730470545640da21bfc4d1c0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-30 13:18:26 +0000

    Automated ccTiddly release : $1
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5800 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Tags/1.6.4/handle/revisionList.php
@@ -0,0 +1,82 @@
+<?php
+	$cct_base = "../";
+	include_once($cct_base."includes/header.php");
+	
+	//return result/message
+	function returnResult($str)
+	{
+		global $ccT_msg;
+		db_close();
+		switch($str) {
+			case "007":		//successful
+				break;
+			case "013":		//no title passed
+				exit("\n".$ccT_msg['misc']['no_title']);
+				break;
+			case "014":		//revision not found
+				exit("\n".$ccT_msg['error']['revision_not_found']);
+				break;
+			default:
+				exit("\n".$ccT_msg['word']['error']);
+		}
+	}
+//////////////////////////////////////////////////////////preformat tiddler data//////////////////////////////////////////////////////////////
+	if( !isset($_GET['title']) ) {
+		//returnResult("013");
+		sendHeader(400,$ccT_msg['misc']['no_title'],"",1);
+	}
+
+	$title = formatParametersGET($_GET['title']);
+	
+//////////////////////////////////////////////////////////start of code//////////////////////////////////////////////////////////////
+
+	//get user and privilege and set variables
+	/*if( strlen($username)==0 && strlen($password)==0 )
+	{
+		$user = user_create();		//get username password from cookie
+	}else{
+		$user = user_create($username,"",0,"",$password,1);
+	}*/
+	
+	//get tiddler with certain title to obtain its tiddler_id
+	$tiddler = db_tiddlers_mainSelectTitle($title);
+	if( $tiddler === FALSE ) {//not found
+		//returnResult("014");
+		sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
+	}
+
+	//use tiddler_id to obtain list of tiddler for revision
+	$tiddler_list = db_tiddlers_backupSelectOid($tiddler['id']);
+	
+	//check privilege for each tiddler
+	$tmp = array();
+	foreach( $tiddler_list as $t ) {
+		if( user_readPrivilege(user_tiddlerPrivilegeOfUser($user,$t['tags'])) )
+		{
+			$tmp[] = $t;
+		}
+	}
+	$tiddler_list = $tmp;
+	
+	if( sizeof($tiddler_list)==0 ) {		//if empty
+		sendHeader(204,$ccT_msg['error']['revision_not_found'],"",1);
+	}
+	
+	//print revision list
+	$output = "";
+	foreach($tiddler_list as $t) {
+		$output .= $t['modified']." ".$t['revision']." ".$t['modifier']."\n";
+	}
+	//print substr( $output, 0, strlen($output) - 1 );
+	/*for( $i=sizeof($tiddler_list)-1; $i>=0; $i-- ) {
+		print $tiddler_list[$i]['modified']." ".$tiddler_list[$i]['version']." ".$tiddler_list[$i]['modifier'];
+		if( $i != 0 )
+		{
+			print "\n";
+		}
+	}*/
+	
+	//remove terminating "/n" using substr
+	sendHeader(200,"", substr( $output, 0, strlen($output) - 1 ),1);
+	//returnResult("007");
+?>
---
+++ association/serversides/cctiddly/Tags/1.6.4/handle/workspaceAdmin.php
@@ -0,0 +1,106 @@
+<?php
+// THIS WILL BECOME A MODULE IN 1.7
+
+$cct_base = "../";
+include_once($cct_base."includes/header.php");
+
+$a = $_POST['action']?$_POST['action']:$_REQUEST['action'];
+$u = $_POST['username']?$_POST['username']:$_REQUEST['username'];
+$w = $_POST['workspace_name']?$_POST['workspace_name']:$_REQUEST['workspace_name'];
+
+
+if(!user_session_validate())
+{
+//	sendHeader("403");
+	echo '<b>You do not appear to be logged in. You may need to refresh the page to recieve the login prompt.</b>';
+	exit;	
+}
+
+if ($a =="LISTWORKSPACES")
+{
+	$data['workspace_name']=$w;
+	
+	
+	$result = db_workspace_selectOwnedBy(user_getUsername());  
+$out = "";
+while ($r = db_fetch_assoc($result))
+	{
+		$out .= "'".$r['workspace_name']."', ";
+	}
+	echo substr_replace($out ,"",-2)." ";	
+ 	exit;
+}
+
+
+
+if (!user_isAdmin(user_getUsername(), $w))
+{
+
+	echo user_getUsername();
+//	sendHeader("401");
+	echo '<b> You are not an Admin rights on this workspace.</b>';
+	exit;
+}
+
+if($a == "DELETEADMIN")
+{
+
+	$users = explode( ",", $u);
+	foreach($users as $user)
+	{
+		if ($user)
+		{
+			$data['username'] = $user;
+			$data['workspace_name'] =  $w; 
+			$r = db_record_delete($tiddlyCfg['table']['admin'],$data);	
+		}	
+	}	
+	exit;
+}
+
+
+
+
+if ($a =="LISTALL")
+{
+	$data['workspace_name']=$w;
+ 	$r = db_record_select($tiddlyCfg['table']['admin'], $data);	
+ 	$out = "[";	
+    $count = 0;
+	foreach ($r as $k=>$v)
+	{
+	//	$out .="'".$count++."':'".$v[workspace_name]."',";
+	//	$out .="'".$v[username]."',";
+	
+		$out .="{'username':'".$v[username]."',";
+		$data1['username']=$v[username];
+		$data1['workspace']=$w;
+	 	$r1 = db_record_select($tiddlyCfg['table']['workspace_view'], $data1, null ,"order by id limit 1");	
+		$out .= "'lastVisit':'".$r1[0]['time']."'},";
+	}	
+echo substr_replace($out ,"",-1)."]";		
+	exit;
+}
+
+if ($u && $w)
+{
+	$data['username'] = $u;
+	$data['workspace_name'] = $w;
+}else
+{
+	echo 'Please enter a workspace name and username.';
+	exit;
+}
+
+$res = db_record_insert($tiddlyCfg['table']['admin'],$data);
+
+if ($res !=1)
+{
+	echo 'User could not be added. ';
+	exit;
+}else
+{
+	sendHeader("201");
+	echo "created";
+}
+?> 
