35c08678844b40f39a9a5da574582c0dbb7a84e8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-30 07:57:40 +0000

    Automated ccTiddly release : $1
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5796 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Tags/1.6.4/handle/register.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$cct_base = "../";
+include_once($cct_base."includes/header.php");
+debug('cccc');
+
+
+if ($tiddlyCfg['can_create_account'] !=1)
+{
+	sendHeader("403");
+	echo 'This is not allowed on this server. ';
+}
+
+
+$data['username'] = $_POST['username'];
+$data['password'] = $_POST['password'];
+
+
+
+// if the user is checking if the username is available.
+if($_POST['free'] ==1 )
+{
+	echo count(db_record_select($tiddlyCfg['table']['user'],$data));
+	exit;
+}
+
+
+$res = db_record_insert($tiddlyCfg['table']['user'],$data);
+
+
+if ($res !=1)
+{
+	echo 'User not crated, please try again with a different username.';
+}else
+{
+	sendHeader("201");
+	echo "created";
+}
+?> 
