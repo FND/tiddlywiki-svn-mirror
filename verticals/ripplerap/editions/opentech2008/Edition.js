7a2e5884df25cc219252901d82a9792316d97952
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-03 10:36:25 +0000

    RippleRap: New openTech edition
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5823 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/ripplerap/editions/opentech2008/Edition.js
@@ -0,0 +1,15 @@
+{{
+config.options.txtRippleRapType = "confabb";
+config.options.txtRippleRapConferenceName = "OpenTech2008";
+// config.options.txtRippleRapConferenceURI = "http://tiddlywiki.localhost/verticals/ripplerap/editions/confabb-pdf/";
+config.options.txtRippleRapConferenceURI = "file://localhost/Users/Phil/Projects/tiddlywiki/Trunk/verticals/ripplerap/editions/confabb-pdf/";
+
+// SharedNotes Plugin
+config.options.chkSharedNotesPutEnabled = false;
+config.options.chkSharedNotesGetEnabled = false;
+
+// Ticker config
+config.options.chkTickerEval = false;
+config.options.chkTickerRefresh = false;
+
+}}
---
+++ verticals/ripplerap/editions/opentech2008/settings.js
@@ -0,0 +1,43 @@
+//{{{
+
+// RippleRap Edition Specific settings are in Edition.js
+
+config.options.chkAutoSave = true;
+config.options.chkAnimate = true;
+config.options.chkSaveBackups = false;
+config.options.txtTheme = "RippleRapSkin";
+
+// SharedNotes Plugin
+config.options.chkSharedNotesPutEnabled = false;
+config.options.chkSharedNotesGetEnabled = false;
+
+// Ticker config
+config.options.chkTickerEval = false;
+config.options.chkTickerRefresh = false;
+
+// Agenda
+config.options.txtMainTab = "";
+config.options.txtDisplayTimeFormat = "00hh:00mm";
+config.options.txtDisplayTimezone = "UTC";
+
+// Logging
+config.options.chkLogMessageEnabled = false;
+config.options.chkLogMessageConsole = false;
+config.options.chkLogMessageWindow = false;
+config.options.chkLogMessageDisplayMessage = false;
+
+// Setup the session notes display groupings.
+var session_notes_groups = new TiddlerDisplayGroup();
+var session_notes_pattern = [
+	{label:'header', tag:'session', count:1, require:null, openAt:null},
+	{label:'mynote', tag:'notes', count:1, require:'header', openAt:null},
+	{label:'notes', tag:'discovered_notes', count:0, require:'header', openAt:'bottom'}];
+session_notes_groups.setPattern(session_notes_pattern); 
+session_notes_groups.setGroupField('rr_session_id');
+
+// Label the message area close button differently
+merge(config.messages.messageClose,{
+	text: "clear",
+	tooltip: "clear this message area"});
+
+//}}}
