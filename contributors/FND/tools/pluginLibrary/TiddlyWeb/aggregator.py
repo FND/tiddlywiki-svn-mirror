e1b9e2a8784008f2249acd27e70d94894e8a860d
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-10 14:34:41 +0000

    Plugin Library: plugin-origin handling, filtering exclude* tags, reading list of repositories from file, various adjustments
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5921 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/tools/pluginLibrary/TiddlyWeb/aggregator.py
+++ contributors/FND/tools/pluginLibrary/TiddlyWeb/aggregator.py
@@ -4,6 +4,8 @@
 retrieve TiddlyWiki plugins from authors' repositories
 """
 
+from __future__ import with_statement
+
 import sys
 import os
 import shutil
@@ -13,27 +15,37 @@ from urllib import urlopen
 from tiddlyweb.store import Store
 
 def main(args):
-	global repos
-	repos = [
-		{
-			"name": "Core Plugins",
-			"URI": "../coreplugins.html", #"http://www.tiddlywiki.com/coreplugins.html", # DEBUG'd
-			"type": "TiddlyWiki"
-		}, {
-			"name": "TiddlyTools",
-			"URI": "", #"http://www.tiddlytools.com", # DEBUG'd
-			"type": "TiddlyWiki"
-		}, {
-			"name": "Martin's Wiki",
-			"URI": "", #"http://martinswiki.com", # DEBUG'd
-			"type": "TiddlyWiki"
-		}
-	] # DEBUG: read list from file
+	repos = getRepositories("repos.lst")
 	for repo in repos:
 		print "processing " + repo["name"] + " (" + repo["URI"] + ")" # DEBUG: log
 		getPlugins(repo)
 	bags = [repo["name"] for repo in repos]
 	generateRecipe(bags)
+
+def getRepositories(filepath):
+	"""
+	retrieve list of repositories from file
+
+	file structure:
+	* one repository per line
+	* three components per line (pipe-delimited):
+	  URI|type|name
+
+	@param filepath: full path to source file
+	@return: repository objects
+	@rtype : list
+	"""
+	repos = list()
+	with open(filepath, "r") as f:
+		for line in f:
+			if line.startswith("#"): # skip commented lines -- DEBUG: skip blank lines
+				repo = dict()
+				components = line.split("|", 2)
+				repo["URI"] = components[0].strip()
+				repo["type"] = components[1].strip()
+				repo["name"] = components[2].strip()
+				repos.append(repo)
+	return repos
 
 def generateRecipe(bags):
 	"""
@@ -64,11 +76,11 @@ def getPlugins(repo):
 		from tiddlyweb.bag import Bag
 		from tiddlyweb.importer import import_wiki
 		try:
-			doc = urlopen(repo["URI"]).read() # DEBUG: caching?!
+			doc = urlopen(repo["URI"]).read() # DEBUG: caching, deferred processing?!
 		except IOError:  # DEBUG: doesn't include 404!?
 			return False # DEBUG: log error
 		bagName = repo["name"] # DEBUG: escape invalid path chars
-		# delete existing bag -- DEBUG: temporary hack?
+		# delete existing bag -- DEBUG: temporary(?) hack to circumvent excessive revision creation
 		bagPath = "store" + os.sep + "bags" + os.sep + bagName # DEBUG: hardcoded root - evil!?
 		if(os.path.exists(bagPath)):
 			shutil.rmtree(bagPath)
@@ -79,12 +91,26 @@ def getPlugins(repo):
 		# import plugins
 		tw = TiddlyWiki(doc)
 		tw.convertStoreFormat()
-		import_wiki(tw.getPluginTiddlers(), repo["name"]) # DEBUG: creates a new revision per cycle
+		import_wiki(tw.getPluginTiddlers(repo), repo["name"]) # DEBUG: creates a new revision per cycle
 		return True
 	elif repo["type"] == "SVN":
 		pass # DEBUG: to be implemented
 	else:
 		pass # DEBUG: TBD
+
+def normalizeURL(URL):
+	"""
+	strip non-essential trailing characters from URL
+
+	@param URL: URL
+	@type  URL: str
+	@return: uniform URL
+	@rtype : str
+	"""
+	URL = URL.split("#", 1)[0]
+	if URL[-1] == "/":
+		URL = URL[:-1] # DEBUG: use rstrip()?
+	return URL
 
 class TiddlyWiki():
 	"""
@@ -100,24 +126,83 @@ class TiddlyWiki():
 		self.doc = doc
 		self.dom = BeautifulSoup(doc) # DEBUG: rename!?
 
-	def getPluginTiddlers(self): # DEBUG: universal-ize; rename to getTiddlers() and use key-value pair(s?) for matching attribute(s?)
+	def getPluginTiddlers(self, repo): # DEBUG: universal-ize; rename to getTiddlers() and use key-value pair(s?) for matching attribute(s?)
 		"""
 		retrieve plugin tiddlers
 
+		@param repo: current repository
+		@type  repo: dict
 		@return: plugin tiddlers (pure-store format)
 		@rtype : str
 		"""
-		store = self.dom.find("div", id="storeArea")
+		self.store = self.dom.find("div", id="storeArea")
 		tag = "systemConfig" # includes "systemConfigDisable" -- DEBUG: include systemTheme tiddlers?
 		# remove non-plugin tiddlers
-		[tiddler.extract() for tiddler in store.findAll("div", title=True) \
+		[tiddler.extract() for tiddler in self.store.findAll("div", title=True) \
 			if (not tiddler.has_key("tags")) or (tag not in tiddler["tags"])]
 		# disable plugins -- DEBUG: move into separate function
 		pattern = re.compile(r"\b(systemConfig|excludeLists|excludeSearch)\b\s?")
-		for plugin in store.findAll("div", title=True, tags=True):
+		for plugin in self.store.findAll("div", title=True, tags=True):
 			plugin["tags"] = re.sub(pattern, "", plugin["tags"]).strip()
+		# remove non-originating plugins
+		self.removeDuplicates(repo["URI"])
 		# return pure-store format
-		return "<html><body><div id='storeArea'>" + store.renderContents() + "</div></body></html>"
+		return "<html><body><div id='storeArea'>" + self.store.renderContents() + "</div></body></html>"
+
+	def removeDuplicates(self, repo): # DEBUG: rename!?
+		"""
+		remove plugins whose origin is not the repository specified
+
+		@param repo: repository URL
+		@type  repo: str
+		@return: None
+		"""
+		repo = normalizeURL(repo)
+		for plugin in self.store.findAll("div", title=True): # DEBUG: limit to direct child nodes!?
+			slices = self.getSlices(plugin.pre.renderContents())
+			if slices.has_key("Source"): # N.B.: plugin accepted if Source slice not present
+				source = normalizeURL(slices["Source"])
+				if source != repo:
+					plugin.extract()
+
+	def getSlices(self, text):
+		"""
+		retrieve plugin meta-slices
+
+		@param text: tiddler text
+		@type  text: str
+		"""
+		pattern = r"(?:^([\'\/]{0,2})~?([\.\w]+)\:\1\s*([^\n]+)\s*$)|(?:^\|([\'\/]{0,2})~?([\.\w]+)\:?\4\|\s*([^\|\n]+)\s*\|$)"; # RegEx origin: TiddlyWiki core
+		pattern = re.compile(pattern, re.M + re.I)
+		matches = re.findall(pattern, text)
+		slices = dict()
+		for match in matches:
+			if match[1]: # colon notation
+				slices[match[1].capitalize()] = match[2]
+			else: # table notation
+				slices[match[4].capitalize()] = match[5];
+		return slices
+
+	def getVersion(self):
+		"""
+		retrieve TiddlyWiki version number
+
+		@return: [TBD]
+		@rtype : [TBD]
+		"""
+		version = self.dom.html.head.script.renderContents()
+		pattern = re.compile("major: (\d), minor: (\d), revision: (\d)")
+		matches = re.search(pattern, version);
+		if matches:
+			major = int(matches.groups()[0])
+			minor = int(matches.groups()[1])
+			revision = int(matches.groups()[2])
+			if major + minor + revision > 0: # DEBUG: dirty hack?
+				return [major, minor, revision]
+			else:
+				return None
+		else:
+			return None
 
 	def convertStoreFormat(self):
 		"""
@@ -128,21 +213,6 @@ class TiddlyWiki():
 		version = self.getVersion()
 		if version[0] + (version[1] / 10.0) < 2.2: # DEBUG: only works if minor < 10
 			pass # DEBUG: to do
-
-	def getVersion(self):
-		"""
-		retrieve TiddlyWiki version number
-		"""
-		version = self.dom.html.head.script.renderContents()
-		pattern = re.compile("major: (\d), minor: (\d), revision: (\d)")
-		matches = re.search(pattern, version);
-		major = int(matches.groups()[0])
-		minor = int(matches.groups()[1])
-		revision = int(matches.groups()[2])
-		if major + minor + revision > 0: # DEBUG: dirty hack?
-			return [major, minor, revision]
-		else:
-			return None
 
 # startup
 
