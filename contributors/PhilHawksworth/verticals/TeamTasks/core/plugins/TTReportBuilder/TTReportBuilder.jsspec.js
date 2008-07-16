34fb5690a7085f3a18ea4fe5f4f12e32152195cd
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-16 15:24:08 +0000

    TeamTasks - Adding testing structure for upcoming ReportBuilder plugin and UI
    git-svn-id: http://svn.tiddlywiki.org/Trunk@5971 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PhilHawksworth/verticals/TeamTasks/core/plugins/TTReportBuilder/TTReportBuilder.jsspec.js
@@ -0,0 +1,116 @@
+// <![CDATA[
+
+describe('TTReportBuilder : Filtering results', {
+
+	'Providing a resultLimit value should modify the value of the resultsLimit parameter' : function() {
+		
+	},
+	
+	'the addShowFilter handler should create a new' : function() {
+		
+	},
+
+	
+
+});
+
+describe('TTReportBuilder : Results limit', {
+
+	'it should return no more results than specified' : function() {
+		
+	},
+	
+	'it should not limit results if no value is specified' : function() {
+		
+	}
+
+});
+
+describe('TTReportBuilder : Add column button', {
+
+	'it should create a new column when clicked' : function() {
+		
+	},
+	
+	'it should create a new column in edit mode' : function() {
+		
+	},
+	
+	'it should display a message instructing the user to add a column if there are no columns displayed' : function() {
+		
+	}
+
+});
+
+describe('TTReportBuilder : Delete column button', {
+
+	'it should prompt the user with a confirm box' : function() {
+		
+	},
+
+	'it should remove the column if the confirmation is given' : function() {
+		
+	}
+	
+});
+
+describe('TTReportBuilder : Column headers', {
+
+	'it should display the edit and delete buttons when it is moused-over' : function() {
+		
+	},
+
+	'it should change to edit mode when the edit button is clicked' : function() {
+		
+	},
+	
+	'it should leave edit mode if a click event occurs elsewhere' : function() {
+		
+	},
+	
+	'it should leave edit mode when a value is chosen from the dropdown' : function() {
+		
+	}
+
+});
+
+describe('TTReportBuilder : Column header edit mode', {
+
+	'it should display a dropdown populated with the values of the taskDefinitions ' : function() {
+		
+	},
+	
+	'it should set the field for display to the selected option when a different option is selected' : function() {
+		
+	},
+
+	'it should exit edit mode when a different option is selected' : function() {
+		
+	}
+	
+});
+
+describe('TTReportBuilder : The Done checkbox', {
+
+	'it should be checked if the task is done' : function() {
+		
+	},
+
+	'it should not be checked if the task is not done' : function() {
+		
+	},
+
+	'it should mark the task as done when checked' : function() {
+		
+	},
+	
+	'it should mark the task as not done when unchecked' : function() {
+		
+	}
+
+});
+
+
+
+
+// ]]>
