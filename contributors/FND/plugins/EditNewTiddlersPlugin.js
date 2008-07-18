1e70ea87eb3374e94921315ed054a708162c10f4
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-18 17:51:38 +0000

    EditNewTiddlersPlugin: added metadata
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6001 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/EditNewTiddlersPlugin.js
+++ contributors/FND/plugins/EditNewTiddlersPlugin.js
@@ -1,3 +1,17 @@
+/***
+|''Name''|EditNewTiddlersPlugin|
+|''Description''|non-existing tiddlers are automatically opened in edit mode|
+|''Author''|FND|
+|''Version''|0.1|
+|''Status''|@@experimental@@|
+|''Source''|http://devpad.tiddlyspot.com/#EditNewTiddlersPlugin|
+|''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
+|''License''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]]|
+!Revision History
+!!v0.1 (2008-07-18)
+* initial release
+!Code
+***/
 //{{{
 // hijack chooseTemplateForTiddler()
 Story.prototype.chooseTemplateForTiddler_editNewTiddlers = Story.prototype.chooseTemplateForTiddler;
