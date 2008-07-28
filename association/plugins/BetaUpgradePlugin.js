5bcc2f5deef0a553d26e783f78362b90434266ca
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-28 12:24:51 +0000

    BetaUpgradePlugin: initial commit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6106 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/plugins/BetaUpgradePlugin.js
@@ -0,0 +1,10 @@
+/***
+|''Name''|BetaUpgradePlugin|
+|''Description''|change the upgrade source to the beta location|
+|''~CoreVersion''|2.4.0|
+|''Source''|http://svn.tiddlywiki.org/Trunk/associations/plugins/|
+|''CodeRepository''|http://svn.tiddlywiki.org|
+***/
+//{{{
+config.macros.upgrade.source = "http://www.tiddlywiki.com/beta/empty.html";
+//}}}
