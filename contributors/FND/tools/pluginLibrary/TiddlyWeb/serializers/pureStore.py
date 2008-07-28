4cc1f54fb996e99bddc375163a00a5f2005b7c27
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-28 14:18:12 +0000

    Plugin Library: minor correction (missing module)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6116 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/tools/pluginLibrary/TiddlyWeb/serializers/pureStore.py
+++ contributors/FND/tools/pluginLibrary/TiddlyWeb/serializers/pureStore.py
@@ -4,14 +4,15 @@ Pure-store format serializer.
 
 from tiddlyweb.serializations import SerializationInterface
 from tiddlyweb.serializations.wiki import Serialization as WikiSerialization
+from tiddlyweb.serializations.wiki import splitter
 
 empty_html = "lib/pureStore.html"
 
 class Serialization(WikiSerialization):
 	def _split_empty_html(self):
 		f = open(empty_html)
-		wiki = f.read()
+		store = f.read()
 		f.close()
-		wiki = unicode(wiki, "utf-8")
-		return wiki.split(splitter)
+		store = unicode(wiki, "utf-8")
+		return store.split(splitter)
 
