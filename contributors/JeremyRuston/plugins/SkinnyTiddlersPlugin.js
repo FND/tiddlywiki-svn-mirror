24a43f0a6bf0b58bbe78fa4c4e30ffb1d1f3f085
JeremyRuston <JeremyRuston@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-31 09:34:16 +0000

    [skinnyTiddlers] Initial submission
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6158 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JeremyRuston/plugins/SkinnyTiddlersPlugin.js
@@ -0,0 +1,46 @@
+/***
+|''Name:''|SkinnyTiddlersPlugin.js|
+|''Description:''|Adds support for config.skinnyTiddlers which show up timelines and other contexts |
+|''Author:''|Jeremy Ruston (jeremy (at) osmosoft (dot) com)|
+|''Source:''|http://svn.tiddlywiki.org/Trunk/contributors/JeremyRuston/plugins/SkinnyTiddlersPlugin.js|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/JeremyRuston/plugins/SkinnyTiddlersPlugin.js|
+|''Version:''|0.0.9|
+|''Status:''|Under Development|
+|''Date:''|July 31, 2008|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev|
+|''License:''|BSD|
+|''~CoreVersion:''|2.4.0|
+***/
+
+//{{{
+//# Ensure that the plugin is only installed once.
+if(!version.extensions.SkinnyTiddlersPlugin) {
+version.extensions.SkinnyTiddlersPlugin = {installed:true};
+
+config.skinnyTiddlers = {}; // Hashmap of {title,modifier,created,modified,tags[]}
+
+config.skinnyTiddlers.SkinnyTiddler = {
+	title: "SkinnyTiddler", // Also should to initialise modifier,created,modified,tags[]
+};
+
+var old_getTiddlyLinkInfo = window.getTiddlyLinkInfo;
+
+window.getTiddlyLinkInfo = function(title,currClasses)
+{
+	var result = old_getTiddlyLinkInfo.apply(window,arguments);
+	var classes = result.classes.split(" ");
+	if(classes.indexOf("tiddlyLinkNonExisting") !== -1 && config.skinnyTiddlers[title]) {
+		classes.remove("tiddlyLinkNonExisting");
+		classes.pushUnique("tiddlyLinkExisting");
+		var skinny = config.skinnyTiddlers[title];
+		result.subTitle = "'" + title + "' has not yet been loaded from the server";
+	}
+	result.classes = classes.join(" ");
+	return result;
+}
+
+
+
+} // if(!version.extensions.SkinnyTiddlersPlugin)
+
+//}}}
