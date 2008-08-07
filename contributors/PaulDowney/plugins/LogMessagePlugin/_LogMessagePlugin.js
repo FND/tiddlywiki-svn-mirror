d87366a252476ed0989e31ed9caefdaa74d1d6fa
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-07 16:33:30 +0000

    LogMessagePlugin - documentation typeos
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6272 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/LogMessagePlugin/_LogMessagePlugin.js
+++ contributors/PaulDowney/plugins/LogMessagePlugin/_LogMessagePlugin.js
@@ -2,7 +2,7 @@
 |''Name:''|LogMessagePlugin|
 |''Description:''|Browser independent message logging|
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
-|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/LogMessagePlugin.js |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/LogMessagePlugin/ |
 |''Version:''|0.2|
 |''License:''|[[BSD open source license]]|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
