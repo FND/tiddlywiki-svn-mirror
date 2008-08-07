c015f6cd811639a3ba0896692c07c3f95a974314
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-07 15:45:56 +0000

    LogMessagePlugin - documentation and changes suggesed by Xavier - missing options box and spurious return
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6270 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/LogMessagePlugin/_LogMessagePlugin.js
+++ contributors/PaulDowney/plugins/LogMessagePlugin/_LogMessagePlugin.js
@@ -3,16 +3,26 @@
 |''Description:''|Browser independent message logging|
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/LogMessagePlugin.js |
-|''Version:''|0.1|
+|''Version:''|0.2|
 |''License:''|[[BSD open source license]]|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
-|''~CoreVersion:''|2.4|
+|''~CoreVersion:''|2.4.1|
+!!Documentation
+This plugin provides a log() function with the same semantics as the [[Firebug|http://getfirebug.com/]] [[console.log|http://getfirebug.com/console.html]] function. Calling console.log() is likely to fail in browsers without Firebug installed. 
 
-Options:
+Depending upon the Options, the message will be sent to the Firebug console, as well as to an external popup window, and TiddlyWiki display message. As of TiddlyWiki release 2.4.1, the core provides a <[[default log() function|http://trac.tiddlywiki.org/ticket/674]], so code containing log() calls will continue to function without generating errors, even without this plugin installed.
+
+TiddlyWiki initializes plugin tiddlers in collation sequence order, so in order to make the log() function available for use in other plugins during initialization, the tiddler has been named _LogMessagePlugin.
+
+An example TiddlyWiki containing the latest released version with a simple example is available from from http://whatfettle.com/2008/07/LogMessagePlugin/
+
+!!Options
 |<<option chkLogMessageEnabled>>|<<message config.optionsDesc.chkLogMessageEnabled>>|
 |<<option chkLogMessageConsole>>|<<message config.optionsDesc.chkLogMessageConsole>>|
 |<<option chkLogMessageWindow>>|<<message config.optionsDesc.chkLogMessageWindow>>|
+|<<option chkLogMessageDisplayMessage>>|<<message config.optionsDesc.chkLogMessageDisplayMessage>>|
 
+!!Code:
 ***/
 
 //{{{
@@ -34,32 +44,34 @@ version.extensions.LogMessagePlugin = {installed:true};
 	config.macros.LogMessage = { 
 
 		log: function(){
-			if (!config.options.chkLogMessageEnabled){
+			if(!config.options.chkLogMessageEnabled){
 				return;
 			}
-			if (config.options.chkLogMessageConsole){
+			if(config.options.chkLogMessageConsole){
 				if (window.console){
 					console.log(arguments);
-					return;
 				}
+			}
+			if(!(config.options.chkLogMessageWindow||config.options.chkLogMessageDisplayMessage)){
+				return;
 			}
 			var message = (function(a){var x=[]; for(var i=0; i<a.length; i++)
 				x.push(a[i]); return x.join(', ');})(arguments);
-			if (config.options.chkLogMessageWindow){
+			if(config.options.chkLogMessageWindow){
 				var me = config.macros.LogMessage;
 				me.logWindow(message);
 			}
-			if (config.options.chkLogMessageDisplayMessage){
+			if(config.options.chkLogMessageDisplayMessage){
 				displayMessage(message);
 			}
 		},
 
 		logWindow: function(message){
 			var me = config.macros.LogMessage;
-			if (!me.window_ || me.window_.closed){
+			if(!me.window_ || me.window_.closed){
 				var win = window.open("", null, "width=400,height=200,scrollbars=yes,"+
 					"resizable=yes,status=no,location=no,menubar=no,toolbar=no");
-				if (!win) 
+				if(!win) 
 					return;
 				var doc = win.document;
 				doc.write("<html><head><title>Log</title></head><body></body></html>");
