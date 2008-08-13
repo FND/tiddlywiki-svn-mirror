42ef26ee11a00ae5093e4da79ea7c6416af70b8d
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-13 15:03:08 +0000

    improvements to download button
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6314 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlywiki.com/plugins/DownloadTiddlyWikiPlugin.js
+++ verticals/tiddlywiki.com/plugins/DownloadTiddlyWikiPlugin.js
@@ -1,8 +1,8 @@
 /***
 |''Name:''|DownloadTiddlyWikiPlugin|
 |''Description:''|Download TiddlyWiki according to browser type|
-|''Version:''|0.0.1|
-|''Date:''|Sep 22, 2006|
+|''Version:''|0.0.2|
+|''Date:''|Aug 13, 2008|
 |''Source:''|http://www.tiddlywiki.com/#DownloadTiddlyWikiPlugin|
 |''License:''|[[BSD open source license]]|
 |''~CoreVersion:''|2.4.1|
@@ -20,14 +20,37 @@ merge(config.macros.download,{
 	prompt: "Download TiddlyWiki",
 	className: "chunkyButton"});
 
-config.macros.download.handler = function(place)
+config.macros.download.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
 	var span = createTiddlyElement(place,"span",null,this.className);
-	createTiddlyButton(span,this.label,this.prompt,this.onClick);
+	createTiddlyButton(span,params[0]||this.label,params[1]||this.prompt,this.onClick);
+
 };
 
 config.macros.download.onClick = function(e)
 {
+	// start the download
+	var url = config.browser.isSafari || config.browser.isOpera ? 'http://www.tiddlywiki.com/empty.zip' :'http://www.tiddlywiki.com/empty.download';
+	document.location.href = url;
+	// and display the tiddler containing the instructions
+	var title;
+	if(config.browser.isMac) {
+		if(config.browser.isSafari) {
+			title = "Safari on Mac OS X"
+		} else if(config.browser.isOpera) {
+			title = "Opera on Mac OS X"
+		} else {
+			title = "Firefox on Mac OS X"
+		}
+	} else if(config.browser.isWindows) {
+		if(config.browser.isIE) {
+			title = "Internet Explorer on Windows Vista"
+		} else {
+			title = "Firefox on Windows Vista"
+		}
+	} 
+	var target = resolveTarget(e);
+	story.displayTiddler(target,title);
 	return false;
 };
 
