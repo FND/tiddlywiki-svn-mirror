2edd0538c4a0114a9006eca4339d390b55429033
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-15 08:04:36 +0000

    ToDoTogglePlugin: added metadata
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6335 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/ToDoTogglePlugin.js
+++ contributors/FND/plugins/ToDoTogglePlugin.js
@@ -1,6 +1,32 @@
+/***
+|''Name''|TodoTogglePlugin|
+|''Description''|toggles a tiddler's tag based on contining checkboxes' status|
+|''Authors''|FND, PhilHawksworth|
+|''Version''|0.1.0|
+|''Status''|@@experimental@@|
+|''Source''|http://svn.tiddlywiki.org/Trunk/contributors/FND/plugins/ToDoTogglePlugin.js|
+|''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/plugins/ToDoTogglePlugin.js|
+|''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
+|''Requires''|[[CheckboxPlugin|http://www.tiddlytools.com/#CheckboxPlugin]]|
+|''Keywords''|tasks|
+!Description
+<...>
+!Usage
+{{{
+[_] task
+}}}
+!Revision History
+!!v0.1.0 (2008-08-14)
+* initial release
+!To Do
+* re-enable tag when unchecking task
+* documentation
+!Code
+***/
 //{{{
-if(!version.extensions.TodoTogglePlugin) {
+if(!version.extensions.TodoTogglePlugin) { //# ensure that the plugin is only installed once
 version.extensions.TodoTogglePlugin = {
+	installed: true,
 	cueTag: "todo",
 
 	checkStatus: function(title) {
