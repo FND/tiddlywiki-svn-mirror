c5e8f6e74b276802ff14e11a67abf628c4031f57
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-17 19:54:18 +0000

    EnhancedSearchPlugin: initial commit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6354 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/FND/plugins/EnhancedSearchPlugin.js
@@ -0,0 +1,83 @@
+/***
+|''Name''|EnhancedSearchPlugin|
+|''Description''|displays search results as a simple list of matching tiddlers|
+|''Authors''|FND|
+|''Version''|0.1.0|
+|''Status''|@@experimental@@|
+|''Source''|http://svn.tiddlywiki.org/Trunk/contributors/FND/plugins/EnhancedSearchPlugin.js|
+|''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/plugins/EnhancedSearchPlugin.js|
+|''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
+|''Keywords''|search|
+!Configuration Options
+<<option chkClassicSearch>> Use classic search behavior
+!Revision History
+!!v0.1.0 (2008-08-17)
+* initial release
+!To Do
+* default styling
+* animations for container creation and removal
+!Code
+***/
+//{{{
+if(!version.extensions.EnhancedSearchPlugin) { //# ensure that the plugin is only installed once
+version.extensions.EnhancedSearchPlugin = {
+	installed: true,
+	btnLabel: "close",
+	btnTooltip: "dismiss search results",
+	id: "searchResults",
+
+	displayResults: function(matches, query) {
+		var el = document.getElementById(this.id);
+		if(el) {
+			removeChildren(el);
+		} else { //# fallback: use displayArea as parent
+			var container = document.getElementById("displayArea");
+			el = document.createElement("div");
+			el.setAttribute("id", this.id);
+			el = container.insertBefore(el, container.firstChild);
+		}
+		if(matches.length > 0) {
+			var msg = config.macros.search.successMsg.format([matches.length.toString(), query]) + "\n";
+			for(var i = 0 ; i < matches.length; i++) {
+				msg += "# [[" + matches[i].title + "]]\n";
+			}
+		} else {
+			msg = config.macros.search.failureMsg.format([query]);
+		}
+		createTiddlyButton(el, this.btnLabel, this.btntooltip,
+			function() { removeNode(this.parentNode); },
+			"button", null, null);
+		wikify(msg, el);
+	}
+};
+
+config.optionsDesc.chkClassicSearch = "Use classic search behavior";
+
+config.shadowTiddlers.StyleSheetEnhancedSearch = "/*{{{*/\n"
+	+ "#" + version.extensions.EnhancedSearchPlugin.id + " {\n"
+	+ "\tborder: 1px solid #eee;\n"
+	+ "\tpadding: 5px;\n"
+	+ "}\n\n"
+	+ "#" + version.extensions.EnhancedSearchPlugin.id + " .button {\n"
+	+ "\tfloat: right;\n"
+	+ "\tbackground-color: #eee;\n"
+	+ "}\n"
+	+ "/*}}}*/";
+store.addNotification("StyleSheetEnhancedSearch", refreshStyles);
+
+// hijack story.search()
+Story.prototype.search_enhancedSearch = Story.prototype.search;
+Story.prototype.search = function(text, useCaseSensitive, useRegExp) {
+	if(config.options.chkClassicSearch) {
+		Story.prototype.search_enhancedSearch.apply(this, arguments);
+	} else {
+		highlightHack = new RegExp(useRegExp ? text : text.escapeRegExp(), useCaseSensitive ? "mg" : "img");
+		var matches = store.search(highlightHack, "title", "excludeSearch");
+		highlightHack = null;
+		var q = useRegExp ? "/" : "'";
+		version.extensions.EnhancedSearchPlugin.displayResults(matches, q + text + q);
+	}
+};
+
+} //# end of "install only once"
+//}}}
