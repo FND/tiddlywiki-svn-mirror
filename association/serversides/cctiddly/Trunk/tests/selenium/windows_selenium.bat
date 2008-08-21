77092f029c7d953e353e32e6c3c17dc3e6d5770a
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-21 16:06:26 +0000

    cct - se
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6464 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -4,4 +4,4 @@ svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ s
 c:\xampp\mysql\bin\mysqladmin -u root drop cctiddly17 -f
 c:\xampp\mysql\bin\mysql -u root<selenium_testssinstall.sql
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive
-C:\Program Files\Mozilla Firefox\firefox.exe -url http://127.0.0.1/selenium/GoogleTest.php
+"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/selenium/GoogleTest.php
