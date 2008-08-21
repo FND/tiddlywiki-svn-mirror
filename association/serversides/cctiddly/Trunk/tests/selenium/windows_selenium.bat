5b4bd947fb58929439eb5b9c158dc8324ab58a54
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-21 15:37:05 +0000

    cct - improved script
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6458 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -1,4 +1,7 @@
 mkdir selenium_tests\
+mkdir selenium_core\
+svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
 svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
 c:\xampp\mysql\bin\mysqladmin -u root drop cctiddly17 -f
 c:\xampp\mysql\bin\mysql -u root<selenium_tests\install.sql
+start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive
