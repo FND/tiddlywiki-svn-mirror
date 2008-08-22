678bafaec8686e71ac44893cbdbd8dd309dbe396
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-22 09:34:13 +0000

    JigglyWiki - Updated the macro engine to try out a new method for calling plugins which uses valid, visible html. Some tiny styling and tweaks too
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6479 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/experimental/jigglywiki/proto/js/display.js
+++ contributors/PhilHawksworth/experimental/jigglywiki/proto/js/display.js
@@ -1,5 +1,4 @@
 
-
 jw.displayTiddler = function(options) {
 
 	var defaults = {
@@ -11,12 +10,10 @@ jw.displayTiddler = function(options) {
 		overflow: false
 	};
 	var opts = $.extend(defaults, options);
-		
-	// get the tiddler from the store
 	var t = jw.getTiddler(opts.name, 'store');
 	
 	// get the templated html of the tiddler
-	var templatedTiddler = jw.getTemplatedTiddlerNEW(t, opts.template);
+	var templatedTiddler = jw.getTemplatedTiddler(t, opts.template);
 	
 	// switching a template
 	if(opts.position == 'replace') {
@@ -49,58 +46,33 @@ jw.displayTiddler = function(options) {
 
 
 // Map the tiddler values onto a given template structure.
-jw.getTemplatedTiddlerNEW = function(tiddler,template) {
+jw.getTemplatedTiddler = function(tiddler,template) {
 	
 	// get a copy of the template html structure from template tiddler.
 	var html = $(jw.getTiddlerData(template,'store').text).clone();
 	html.find(":first-child").prepend("<a class='tiddlerName' name='tiddler:"+ jw.getTiddlerData(tiddler,'store').tiddlerName +"'></a>");
 	
-	// // // find macros in this tiddler.
+	// find macros in this tiddler.
 	html.find('code.macro').each(function(n,e){
-	
 		// build an object for calling the macro handler.
-		var t = $.trim($(e).text());
-		var pairs = t.split(',');
 		var opts = {
 			tiddler:tiddler,
 			place:$(this)
 		};
+		var t = $.trim($(e).text());
+		var pairs = t.split(',');
 		for (var p=0; p < pairs.length; p++) {
 			nv = pairs[p].split(':');
 			opts[$.trim(nv[0])] = $.trim(nv[1]);
 		};	
-	
 		// pass the macro name directly and remove it fro the arguments passed to the macro caller.
 		var m = opts.macro;
 		delete opts.macro;
-		jw.callMacro(m, opts);
-	
+		jw.callMacro(m, opts);	
 	});
 	
 	return html.html();
 };
 
-
-
-// Map the tiddler values onto a given template structure.
-// jw.getTemplatedTiddler = function(tiddler,template) {
-// 	
-// 	// get a copy of the template html structure from template tiddler.
-// 	var html = $(jw.getTiddlerData(template,'store').text).clone();
-// 	
-// 
-// 	// //use the macros in the templates to populate them.
-// 	html.find('*').each(function(i,n) {	
-// 		var macro = $(n).attr('macro');
-// 		if(macro !== undefined) {
-// 			jw.applyMacros($(n), macro, tiddler);
-// 		}
-// 		if(i==0){
-// 			$(n).prepend("<a class='tiddlerName' name='tiddler:"+ jw.getTiddlerData(tiddler,'store').tiddlerName +"'></a>");
-// 		}
-// 	});
-// 	
-// 	return html.html();
-// };
 
 
