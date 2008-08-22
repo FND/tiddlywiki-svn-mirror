b0360ecd9f6f26cf26c19e433a0074dec3426737
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-22 19:16:52 +0000

    Plugin Library: test coverage (requires coverage module)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6507 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/services/pluginLibrary/test_all.py
+++ association/services/pluginLibrary/test_all.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import coverage
 
 def main(args = []):
 	modules = [
@@ -9,10 +10,24 @@ def main(args = []):
 	]
 	if len(args) > 1:
 		modules = args[1:]
-	suites = unittest.TestLoader().loadTestsFromNames(modules)
-	result = unittest.TextTestRunner().run(suites)
+	initCoverage()
+	suite = unittest.TestLoader().loadTestsFromNames(modules)
+	result = unittest.TextTestRunner().run(suite)
+	endCoverage()
 	return result.wasSuccessful()
 
+def initCoverage():
+	coverage.erase()
+	coverage.use_cache(0)
+	coverage.start()
+
+def endCoverage():
+	coverage.stop()
+	import tiddlywiki, dirScraper, utils # TODO: avoid manual listing
+	modules = [tiddlywiki, dirScraper, utils] # TODO: avoid manual listing
+	coverage.report(modules)
+
 if __name__ == "__main__":
-	sys.exit(not main(sys.argv))
+	status = main(sys.argv)
+	sys.exit(not status)
 
