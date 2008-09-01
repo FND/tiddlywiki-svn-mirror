114ee81245d954a34a190299c9479ff59a6dafa6
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-01 11:07:00 +0000

    cct - working login
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6571 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/ccLoginStatus.js
+++ association/serversides/cctiddly/Trunk/tiddlers/ccLoginStatus.js
@@ -28,7 +28,21 @@ Show the current user their login status.
 	
 	
 config.macros.saveChanges.handler=function(place,macroName,params,wikifier,paramString,tiddler){
-		wikify("<<ccLoginStatus>>", place);
+	
+
+
+	if (isLoggedIn()){
+		createTiddlyButton(place,"logout" , "Click to logout", function()  {
+				if (window.fullUrl.indexOf("?") >0)
+					window.location = window.fullUrl+"&logout=1";
+				else
+					window.location = window.fullUrl+"?logout=1";
+		},null,null,this.accessKey);
+	}else{
+		createTiddlyButton(place,"login" , "Click to login", function()  {
+			story.displayTiddler(null, "Login");
+		},null,null,this.accessKey);
+	}
 };
 
 
@@ -45,32 +59,15 @@ config.macros.ccLoginStatus.refresh=function(place,errorMsg){
 	removeChildren(loginDivRef);
 	var wrapper=createTiddlyElement(place,"div");
 	if (isLoggedIn()){
-		createTiddlyElement(wrapper,"br");
 		name = cookieString(document.cookie).txtUserName;
 		if(workspace == "")
 			var str = wikify("You are viewing the default workspace and  are logged in as "+decodeURIComponent(name)+"\r\n\r\n",wrapper);
 		else
 			var str = wikify("You are viewing the workspace "+workspace+" and  are logged in as "+decodeURIComponent(name)+"\r\n\r\n",wrapper);
-		var btn = createTiddlyElement(wrapper,"input",null,"button", null, {
-			type: "button",
-			value: "Logout" });
-		btn.setAttribute("type", "button");
-		btn.onclick=function() {
-			
-			// append url function required 
-			if (window.fullUrl.indexOf("?") >0)
-				window.location = window.fullUrl+"&logout=1";
-			else
-				window.location = window.fullUrl+"?logout=1";
-		};
-		wrapper.appendChild(btn);
+
 		if (workspacePermission.owner==1){
-			createTiddlyElement(wrapper, "br");
-			createTiddlyElement(wrapper, "br");
 			createTiddlyText(wrapper,"You are an admin of this workspace");
 		}
-	}else{
-		wikify("Hi Guest\r\n\r\n Please [[login|Login]] ",wrapper);
 	}
 };
 
