597176dfafcf73981b8906d743a36a6052f5ffb1
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-03 15:58:04 +0000

    Expenses - Evolving the UI. Added basic generation of claims and claim items. These will need to be populated by the correct 'forms' and claim reference ids
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6590 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
+++ contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
@@ -2,37 +2,55 @@
 |''Name:''|CreateClaimItemPlugin |
 |''Description:''|Creat a new claim item in the EasyExpenses system|
 |''Author:''|Phil Hawksworth|
-|''Version:''|0.1|
+|''Version:''|0.2|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''~CoreVersion:''|2.4.1|
 ***/
 
 //{{{
-if(!version.extensions.CreateClaimItemPlugin) {
-version.extensions.CreateClaimItemPlugin = {installed:true};
+if(!version.extensions.CreateClaimPlugin) {
+version.extensions.CreateClaimPlugin = {installed:true};
 
+config.macros.CreateClaim = {};
 config.macros.CreateClaimItem = {};
 
-config.macros.CreateClaimItem.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	
-	var tiddler = story.findContainingTiddler(place);
-	var t = store.getTiddler(tiddler.getAttribute('tiddler'));
-	
-	//make a form to create new claim items. Populate the drop down by finding all the 'form definition' tiddlers.
-	var btn = createTiddlyButton(place,"new claim item","add a new claim item",this.CreateClaimItem);
-	btn.claim_id = t.fields.claim_id;
-
-	
+config.macros.CreateClaim.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	var btn = createTiddlyButton(place,"New claim","start a new claim",this.doCreate);
 };
 
-config.macros.CreateClaimItem.CreateClaimItem = function(ev) {
-	var e = ev ? ev : window.event;
+config.macros.CreateClaim.doCreate = function(ev) {
+	var dt = new Date();
+	var claim_id = 'GENERATED_CLAIM_ID_'+ dt.getTime();
+	var title = "Your reference";
+	var t = store.createTiddler(title);
+	t.tags.pushUnique('claimHeader');
+	t.fields['claim_id'] = claim_id;
+	t.text = 'this is a new claim. It\'s ~GENERATED_CLAIM_ID will need to be provided by the expenses system  <<CreateClaimItem>>';
+	story.displayTiddler(null,title,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);		
+};
 
-	console.log('Create new item. claim_id:', this.claim_id);
-	
+
+config.macros.CreateClaimItem.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	var tiddler = story.findContainingTiddler(place);
+	var t = store.getTiddler(tiddler.getAttribute('tiddler'));
+	var btn = createTiddlyButton(place,"new claim item","add a new claim item",this.doCreate);
+	btn.claim_id = t.fields.claim_id;
+};
+
+config.macros.CreateClaimItem.doCreate = function(ev) {
+	var e = ev ? ev : window.event;
+	var dt = new Date();
+	var uniqueID = 'claim_' + this.claim_id + '_' + dt.getTime();
+
 	// create tiddler and associate it with this claim.
-	// refelct the form elements into this claim report.
+	var t = store.createTiddler(uniqueID);
+	t.tags.pushUnique('claimItem');
+	t.fields['claim_id'] = this.claim_id;
+	t.text = 'defined by the FORM';
+
+	// refelct the form elements into this claim report.		
+	story.displayTiddler(null,uniqueID,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);	
 };
 
 }
