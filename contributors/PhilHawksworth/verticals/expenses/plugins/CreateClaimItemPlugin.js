facac2830daa7ad363c56d83a7b658ee7f324da7
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-09 11:46:10 +0000

    Expenses - added quick claim form ready for a quick addition of incomplete expense claim items
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6634 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
+++ contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
@@ -13,7 +13,6 @@ if(!version.extensions.CreateClaimPlugin) {
 version.extensions.CreateClaimPlugin = {installed:true};
 
 config.macros.CreateClaim = {};
-config.macros.CreateClaimItem = {};
 
 config.macros.CreateClaim.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 	var btn = createTiddlyButton(place,"New claim","start a new claim",this.doCreate);
@@ -28,6 +27,62 @@ config.macros.CreateClaim.doCreate = function(ev) {
 	t.fields['claim_id'] = claim_id;
 	story.displayTiddler(null,title,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);		
 };
+
+
+config.macros.CreateClaimItem = {};
+config.macros.CreateClaimItem.newItemType = 'simple';
+
+config.macros.CreateClaimItem.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+
+	var options = [];
+	var slices = store.calcAllSlices('expenseTypeValues');
+
+	// test data to be replaced later when the tiddler exists.
+	options.push({'caption':'just get it up there', 'name':'Simple'});
+	options.push({'caption':'An Airfare', 'name':'Airfare'});
+
+	for(s in slices) {
+		var arg =  store.getTiddlerSlice('AirfareForm',s);
+		options.push({'caption':arg, 'value':arg});
+	}
+	createTiddlyDropDown(place,this.setItemType,options,'simple');
+	createTiddlyButton(place,"add item","add another item to this claim",this.doCreate);
+};
+
+config.macros.CreateClaimItem.doCreate = function(ev) {
+
+	var e = ev ? ev : window.event;
+	var t = story.findContainingTiddler(e.target);
+	var claimTiddler = store.getTiddler(t.getAttribute('tiddler'));
+	var dt = new Date();
+	var uniqueID = 'claim_' + claimTiddler.fields['claim_id'] + '_' + dt.getTime();
+
+	// create tiddler and associate it with this claim.
+	var valuesTiddlerTitle = config.macros.CreateClaimItem.newItemType + 'Form';	
+	var formDefinitionTiddler = store.getTiddler(valuesTiddlerTitle);
+	var newTiddler = store.createTiddler(uniqueID);
+	newTiddler.tags.pushUnique('claimItem');
+	newTiddler.fields['claim_id'] = claimTiddler.fields['claim_id'];
+
+	// add fields as described in the form template for this claim type
+	var slices = store.calcAllSlices(formDefinitionTiddler);
+	for(s in slices) {
+		var arg =  store.getTiddlerSlice(formDefinitionTiddler,s);
+		newTiddler.fields[s.toLowerCase()] = "";
+		// newTiddler.fields[s.toLowerCase()] = {'label':s, 'arg':arg, 'value':null};
+	}
+	console.log('newTiddler.fields',newTiddler.fields);
+
+	// refelct the form elements into this claim report.		
+	story.displayTiddler(null,uniqueID,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);
+};
+
+config.macros.CreateClaimItem.setItemType = function(ev) {
+	var e = ev ? ev : window.event;
+	var t = e.target;
+	config.macros.CreateClaimItem.newItemType = t[t.selectedIndex].value;
+};
+
 
 
 // Extend the commands so that we can use our own commands on tiddlers.
@@ -59,6 +114,8 @@ config.commands.newClaimItem = {
 		story.displayTiddler(null,uniqueID,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);
 	}
 };
+
+
 config.commands.cloneClaimItem = {
 	text: "clone",
 	tooltip: "add another claim item just like this",
@@ -79,13 +136,19 @@ config.commands.cloneClaimItem = {
 config.macros.view.views.complexField = function(value,place,params,wikifier,paramString,tiddler) {
 	var results = value[params[2]];
 	highlightify(results,place,highlightHack,tiddler);
-	// console.log('complex field',arguments, results);
 };
 
-	// text: function(value,place,params,wikifier,paramString,tiddler) {
-	// 	highlightify(value,place,highlightHack,tiddler);
-	// },
 
+
+config.macros.SubmitQuickClaim = {};
+
+config.macros.SubmitQuickClaim.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	var btn = createTiddlyButton(place,"submit quick claim","quickly submit this item to the system to tweak later",this.doSubmit);
+};
+
+config.macros.SubmitQuickClaim.doSubmit = function(ev) {
+	alert("submitted");
+};
 
 
 }
