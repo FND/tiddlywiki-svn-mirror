1b96d8da461f226b1bea0e65a0035ec9b19bd278
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-10 14:07:40 +0000

    Expenses - adding the ability to list all non standard (i.e. common) fields for an item form via a new macro with a suspect name: ModeledDataFormBuilder. This macro now also populates all claim item tiddlers via their ViewTemplates
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6653 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
+++ contributors/PhilHawksworth/verticals/expenses/plugins/CreateClaimItemPlugin.js
@@ -35,23 +35,24 @@ config.macros.CreateClaim.doCreate = function(ev) {
 // ==================
 
 config.macros.CreateClaimItem = {};
-config.macros.CreateClaimItem.newItemType = 'simple';
+config.macros.CreateClaimItem.newItemType = 'AirfareForm';
 config.macros.CreateClaimItem.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 
 	var options = [];
 	var slices = store.calcAllSlices('expenseTypeValues');
 
 	// test data to be replaced later when the tiddler exists.
-	options.push({'caption':'just get it up there', 'name':'Simple'});
-	options.push({'caption':'An Airfare', 'name':'Airfare'});
+	options.push({'caption':'just get it up there', 'name':'SimpleForm'});
+	options.push({'caption':'An Airfare', 'name':'AirfareForm'});
 
 	for(s in slices) {
 		var arg =  store.getTiddlerSlice('AirfareForm',s);
 		options.push({'caption':arg, 'value':arg});
 	}
-	createTiddlyDropDown(place,this.setItemType,options,'simple');
+	createTiddlyDropDown(place,this.setItemType,options,'SimpleForm');
 	createTiddlyButton(place,"add item","add another item to this claim",this.doCreate);
 };
+
 
 config.macros.CreateClaimItem.doCreate = function(ev) {
 
@@ -62,24 +63,23 @@ config.macros.CreateClaimItem.doCreate = function(ev) {
 	var uniqueID = 'claim_' + claimTiddler.fields['claim_id'] + '_' + dt.getTime();
 
 	// create tiddler and associate it with this claim.
-	var valuesTiddlerTitle = config.macros.CreateClaimItem.newItemType + 'Form';	
-	var formDefinitionTiddler = store.getTiddler(valuesTiddlerTitle);
+	var valuesTiddlerTitle = config.macros.CreateClaimItem.newItemType;	
 	var newTiddler = store.createTiddler(uniqueID);
 	newTiddler.tags.pushUnique('claimItem');
 	newTiddler.fields['claim_id'] = claimTiddler.fields['claim_id'];
+	newTiddler.fields['expense_type'] = config.macros.CreateClaimItem.newItemType.substr(0,config.macros.CreateClaimItem.newItemType.length-4);
 
 	// add fields as described in the form template for this claim type
-	var slices = store.calcAllSlices(formDefinitionTiddler);
+	var slices = store.calcAllSlices(valuesTiddlerTitle);
 	for(s in slices) {
-		var arg =  store.getTiddlerSlice(formDefinitionTiddler,s);
+		// newTiddler.fields[s] = "";
 		newTiddler.fields[s.toLowerCase()] = "";
-		// newTiddler.fields[s.toLowerCase()] = {'label':s, 'arg':arg, 'value':null};
-	}
-	console.log('newTiddler.fields',newTiddler.fields);
+	}	
 
-	// refelct the form elements into this claim report.		
-	story.displayTiddler(null,uniqueID,DEFAULT_VIEW_TEMPLATE,false,null,null,false,null);
+	//display the new tiddler
+	story.displayTiddler(null,uniqueID,DEFAULT_EDIT_TEMPLATE,false,null,null,false,null);
 };
+
 
 config.macros.CreateClaimItem.setItemType = function(ev) {
 	var e = ev ? ev : window.event;
@@ -125,19 +125,108 @@ config.macros.SubmitQuickClaim.handler = function(place,macroName,params,wikifie
 };
 
 config.macros.SubmitQuickClaim.doSubmit = function(ev) {
+
 	console.log('submitting quick claim item');
-	
-	//call out to the particular systems 
+
+	//call out to the particular systems authentication and claim generation functions.
 	config.macros.SiteminderLogin.doLogin();
+};
+
+
+// ===========================================
+// = Build form elements from the data model =
+// ===========================================
+config.macros.ModeledDataFormBuilder = {};
+
+config.macros.ModeledDataFormBuilder.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+
+	var options = paramString.parseParams()[0];	
+	var referenceForm = tiddler.fields[options.formIdentifier] + 'Form';	
 	
-	// config.macros.SubmitQuickClaim.handleResponse();
+	if(options.field) {
+		// we are displaying the contents of a field
+		var field = options.field + 'Field';
+		var type = store.getTiddler(field).fields['field_type'];
+		var value = tiddler.fields[field.toLowerCase()];
+		var label = options.field;			
+		var param = store.getTiddlerSlice(referenceForm,field);
+		this.createFormItem(place,{
+			type: type,
+			value: value,
+			label: label,
+			param: param,
+			readonly: options.readonly ? options.readonly : null,
+			mandatory: options.mandatory ? options.mandatory : null
+		});
+	} else if(options.fields && options.fields == 'nonStandard') {
+		// we are displaying all fields not specified as being standard.
+
+		// get these from psd
+		var standardFields = ['claim_id','expense_type','StartDateField', 'AmountFields', 'JustificationField'];
+
+		// sadly we need to sanitise the field names because the tiddler fields seem to neef to be lower case.
+		for (var s=0; s < standardFields.length; s++) {
+			standardFields[s] = standardFields[s].toLowerCase();
+		};
+		
+		for(var f in tiddler.fields) {
+			if(!standardFields.contains(f)) {
+				console.log('Output ' + f);
+				var field = f + 'Field';
+				var type = store.getTiddler(field).fields['field_type'];
+				var value = tiddler.fields[field.toLowerCase()];
+				var label = f;			
+				var param = store.getTiddlerSlice(referenceForm,field);
+				this.createFormItem(place,{
+					type: type,
+					value: value,
+					label: label,
+					param: param,
+					readonly: options.readonly ? options.readonly : null,
+					mandatory: options.mandatory ? options.mandatory : null
+				});
+			}
+		}
+		
+		
+	} else {
+		//we are displaying the form reference that we are using
+		var type = 'text';
+		var value = tiddler.fields[options.formIdentifier];
+		var label = 'Claim Item Type'; // thinking that this should be paramaterised somehow.
+		var param = tiddler.fields[options.formIdentifier]; // is this submitted ?
+		this.createFormItem(place,{
+			type: type,
+			value: value,
+			label: label,
+			param: param,
+			readonly: options.readonly ? options.readonly : null,
+			mandatory: options.mandatory ? options.mandatory : null
+		});
+	}
+
 };
 
-config.macros.SubmitQuickClaim.handleResponse = function() {
-	console.log('handling response from quick claim form submission');
-	displayMessage("Your quick claim item has been submitted");
-};
 
+config.macros.ModeledDataFormBuilder.createFormItem = function(place, options) {
+	
+	// test for an empty mandatory element.
+	var className = null;
+	if(options.mandatory && options.value.length === 0) {
+		className = 'flagMandatory';
+	}
+
+	// create the label elelment
+	createTiddlyElement(place,'label',null,className,options.label,{'for': options.param});	
+
+	// create the form input element
+	var attributes = {'value':options.value, 'name':options.param};
+	if(options.readonly) {
+		attributes.readonly = true;
+	}
+	createTiddlyElement(place,'input',null,className,null,attributes);	
+
+};
 
 }
 //}}}
