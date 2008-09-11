6fcb3fccf3bf747e8f8cf43a8f75f8bf873e432a
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-11 14:09:41 +0000

    WordpressAdaptor - test TiddlyWiki
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6671 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/testWordpress/settings.js
@@ -0,0 +1,28 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.4.1|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = 'backup';
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+
+config.maxTiddlerImportCount = 500;
+
+/*merge(config.defaultCustomFields,{
+	'server.type':'wordpress',
+	'server.host':'changeme.wordpress.com'
+});*/
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+config.options.chkUseHostImages = true;
+
+//#config.displayInstrumentation = true;
+/*}}}*/
