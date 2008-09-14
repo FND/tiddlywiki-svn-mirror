bc8bc922a6df84c6cd9450764b20285c1d57d752
SaqImtiaz <SaqImtiaz@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-14 15:18:37 +0000

    POLS250 vertical: IE fixes and changed location of structure feed
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6692 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SaqImtiaz/verticals/POLS250/studentnotebook/plugins/ReadOnlyTiddlersPlugin.js
+++ contributors/SaqImtiaz/verticals/POLS250/studentnotebook/plugins/ReadOnlyTiddlersPlugin.js
@@ -17,7 +17,7 @@ config.commands.editTiddler.hideShadow = true;
 
 setStylesheet("#backstageButton #backstageShow {position:absolute;top:-30em;right:0;}","hideBackStageStyles");
 
-setStylesheet("#mysidebar #sidebarOptions .bigbutton .button {padding:0.2em 0.3em; font-size:120%; font-weight:bold; border:1px solid #000; display:block; background-color:#0044BB; color:#fff; margin-bottom:0.2em;} #mysidebar #sidebarOptions .bigbutton br {display:none;}","savebuttonStyles");
+setStylesheet("#mysidebar #sidebarOptions .bigbutton .button, #mysidebar #sidebarOptions .bigbutton .button:hover {padding:0.2em 0.3em; font-size:100%; font-weight:bold; border:1px solid #000; display:block; background-color:#0044BB; color:#fff; margin-bottom:0.2em;} #mysidebar #sidebarOptions .bigbutton br {display:none;}","savebuttonStyles");
 
 config.options.chkPrefillNotes = true;
 merge(config.defaultCustomFields,{'wikiformat':'wikispaces'});
