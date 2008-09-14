0265816df5c420330a9fb91038dd8148d80a9fd7
SaqImtiaz <SaqImtiaz@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-14 17:28:42 +0000

    batchNavigationMacro: first commit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6694 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/SaqImtiaz/plugins/batchNavigationMacro.js
@@ -0,0 +1,11 @@
+config.macros.batchNavigation = {
+	handler : function(place,macroName,params,wikifier,paramString,tiddler) {
+		if(!config.macros.navigation)
+			return false;
+		var sets = store.getTaggedTiddlers(params[0]);
+		for (var s=0; s<sets.length; s++){
+			var p = "tiddlers:{{store.getTiddlerText('" + sets[s].title + "').readBracketedList()}}";
+			invokeMacro(place,'navigation',p,wikifier,tiddler);
+		}
+	}
+}
