35489e6b24eb207c594f53a9e9ca269c996f9492
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-16 08:16:03 +0000

    cct - ccAbout can now be translated easily
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6705 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/ccAbout.js
+++ association/serversides/cctiddly/Trunk/tiddlers/ccAbout.js
@@ -20,22 +20,26 @@
 
 ***/
 //{{{
-config.backstageTasks.push("about");
-
-		
-merge(config.tasks,{about:{text: "about",tooltip: "Find out more about ccTiddly ",content: '<<ccAbout>>'}});
-
+	
+	
 config.macros.ccAbout={};
-config.macros.ccAbout.handler=function(place,macroName,params,wikifier,paramString,tiddler,errorMsg){
+merge(config.macros.ccAbout,{
+	buttonBackstageText:"about",
+	buttonBackstageTooltip:"Find out more about ccTiddly ",
+	stepAboutTitle:"About",
+	stepAboutTextStart:"You are running ccTiddly ",
+	stepAboutTextEnd:"More info about ccTiddly can be found  at <a  target=new href=http://www.tiddlywiki.org/wiki/CcTiddly>http://www.tiddlywiki.org/wiki/CcTiddly</a><br/><br/>  More information about TiddlyWiki can be found at <a target=new href=http://www.tiddlywiki.com>http://www.tiddlywiki.com</a><br/>"
+});
 
-	
+
+config.backstageTasks.push(config.macros.ccAbout.buttonBackstageText);
+
+merge(config.tasks,{about:{text: config.macros.ccAbout.buttonBackstageText,tooltip: config.macros.ccAbout.buttonBackstageTooltip,content: '<<ccAbout>>'}});
+
+config.macros.ccAbout.handler=function(place,macroName,params,wikifier,paramString,tiddler,errorMsg){
 	var w = new Wizard();
-	w.createWizard(place,"About");
-	var str="You are running ccTiddly " + window.ccTiddlyVersion+"<br /><br />";
-	str+="More info about ccTiddly can be found  at <a  target=new  href=http://www.tiddlywiki.org/wiki/CcTiddly>http://www.tiddlywiki.org/wiki/CcTiddly</a><br/><br/>  More information about TiddlyWiki can be found at <a target=new href=http://www.tiddlywiki.com>http://www.tiddlywiki.com</a><br/>" ;
-	//var link=createExternalLink(str,'http://tiddlywiki.org/wiki/CcTiddly');
-	//link.textContent='http://tiddlywiki.org/wiki/CcTiddly';
-	w.addStep(null, str);
-	
+	var me = config.macros.ccAbout;
+	w.createWizard(place,me.stepAboutTitle);
+	w.addStep(null, me.stepAboutTextStart + window.ccTiddlyVersion + "<br /><br />" + me.stepAboutTextEnd);
 };
 //}}}
