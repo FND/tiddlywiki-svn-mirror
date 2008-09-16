da9898b673e6653a8040160ee6d94a7403df06e0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-16 20:28:45 +0000

    cct - adding more style to graphs
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6727 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/instanceStats.php
+++ association/serversides/cctiddly/Trunk/handle/instanceStats.php
@@ -29,18 +29,18 @@ function displayGraph($SQL, $title)
 	}
 	$r2 = round(max($values), -2)/2;
 
-	echo "\r\n\r<img src=http://chart.apis.google.com/chart?chtt=".urlencode($title)."&cht=lc&chs=800x375&chd=".chart_data($values)."&chxt=x,y&chxl=0:|".$labels."|1:|0|".$r2."|".round(max($values))."&chf=c,lg,90,DDDDDD,0.5,ffffff,20|bg,s,FFFFFF&&chg=100.0,25.0&chf=c,ls,0,CCCCCC,0.2,FFFFFF,0.2>";
+	echo "\r\n\r<img src=http://chart.apis.google.com/chart?chtt=".urlencode($title)."&cht=lc&chs=800x375&chd=".chart_data($values)."&chxt=x,y&chxl=0:|".$labels."1:|0|".$r2."|".round(max($values))."&chf=c,lg,90,DDDDDD,0.5,ffffff,20|bg,s,FFFFFF&&chg=100.0,25.0&chf=c,ls,0,CCCCCC,0.2,FFFFFF,0.2>";
 }
 
 $SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k:%i') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
 displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over the past day by minutes");
 
-$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k:%i') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
-displayGraph($SQL, "Instance views by month over the past day by minutes");
+$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
+displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over the past day by hour");
 
-	$SQL ="SELECT DATE_FORMAT(time, '%m/%Y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 2 YEAR  GROUP BY Date order by time";
+	$SQL ="SELECT DATE_FORMAT(time, '%m/%Y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 2 YEAR AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
 	displayGraph($SQL, "Instance views by month over the past two years.");
-	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 10 DAY  GROUP BY Date order by time";
+	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 10 DAY AND workspace='".$_REQUEST['workspace']."'  GROUP BY Date order by time";
 	displayGraph($SQL, "Instance views in the last 10 days.");
 
 
