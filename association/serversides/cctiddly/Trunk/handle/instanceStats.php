44e18ef3c93c3fc2406b98b0d385341b1b50c5bc
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-17 15:25:36 +0000

    cct - making graphs look nice
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6732 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/instanceStats.php
+++ association/serversides/cctiddly/Trunk/handle/instanceStats.php
@@ -24,14 +24,16 @@ $chartData.=substr($simpleEncoding,61*($currentValue/$maxValue),1);
 
 
 function displayGraphFull($chartTitle, $values, $xLabels, $yValues)
-{
-	echo "http://chart.apis.google.com/chart?chtt=".urlencode($chartTitle)."&cht=lc&chs=800x375&chd=".chart_data($values)."&chxt=x,y&chxl=0:|". $yLabels."1:|".$xValues."&chf=c,lg,90,EEEEEE,0.5,ffffff,20|bg,s,FFFFFF&&chg=10.0,10.0&";	
+{	
+	  echo "http://chart.apis.google.com/chart?chtt=".urlencode($chartTitle)."&cht=lc&chs=800x375&chd=".chart_data($values)."&chxt=x,y&chxl=0:1:|".$yValues."&chf=c,lg,90,EEEEEE,0.5,ffffff,20|bg,s,FFFFFF&&chg=10.0,10.0&";
+	//echo "http://chart.apis.google.com/chart?chtt=".urlencode($chartTitle)."&cht=lc&chs=800x375&chd=".chart_data($values)."&chxt=x,y&chxl=0:|". $yLabels."1:|".$xValues."&chf=c,lg,90,EEEEEE,0.5,ffffff,20|bg,s,FFFFFF&&chg=10.0,10.0&";	
 }
 
 
 function displayGraphThumb($values)
 {
-	echo "\r\n\rhttp://chart.apis.google.com/chart?cht=lc&chs=100x75&chd=".chart_data($values)."&chxt=x,y&chxl=0:||1:|";
+	echo "http://chart.apis.google.com/chart?cht=lc&chs=100x75&chd=".chart_data($values)."&chxt=x,y&chxl=0:||1:|";
+	
 	
 }
 
@@ -48,10 +50,10 @@ function displayGraph($SQL, $title)
 	sort($labelValues);
 	foreach($labelValues as $label)
 		$lv .=$label.'|';
-	$lv = substr($lv,0,strlen($lv)-1);
+echo 	$lv = substr($lv,0,strlen($lv)-1);
 	$r2 = round(max($values), -2)/2;
 	if ($_REQUEST['full']==1)
-		displayGraphFull("blah ", $values, $labels, $lv);
+		displayGraphFull($_REQUEST['desc'], $values, "$label", $lv);
 	else
 		displayGraphThumb($values);
 }
@@ -59,37 +61,24 @@ function displayGraph($SQL, $title)
 
 
 
-if ($_REQUEST['graph']==1){
-	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k:%i') AS Date,  COUNT(*) AS 
-numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day 
-AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
-displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over 
-the past day by minutes");
+if ($_REQUEST['graph']=="minute"){
+	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k:%i') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
+displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over the past day by minutes");
 }
-if ($_REQUEST['graph']==2){
-	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k') AS Date,  COUNT(*) AS 
-numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day 
-AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
-displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over 
-the past day by hour");
+if ($_REQUEST['graph']=="hour"){
+	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%Y-%k') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 1 day AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
+displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over the past day by hour");
 }
-if ($_REQUEST['graph']==3){
-	$SQL ="SELECT DATE_FORMAT(time, '%m/%Y') AS Date,  COUNT(*) AS 
-numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 2 
-YEAR AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by 
-time";
-	displayGraph($SQL, "Instance views by month over the past two 
-years.");
+if ($_REQUEST['graph']=="day"){
+echo 	$SQL ="SELECT DATE_FORMAT(time, '%m/%Y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 7 DAY GROUP BY Date order by time";
+	displayGraph($SQL, "Instance views by month over the past seven Days.");
 }
-if ($_REQUEST['graph']==4){
-
-	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%y') AS Date,  COUNT(*) 
-AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 10 
-DAY AND workspace='".$_REQUEST['workspace']."'  GROUP BY Date order by 
-time";
-	displayGraph($SQL, "Instance views in the last 10 days.");
+if ($_REQUEST['graph']=="month"){
+	$SQL ="SELECT DATE_FORMAT(time, '%d/%m/%y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 12 MONTH AND workspace='".$_REQUEST['workspace']."'  GROUP BY Date order by time";
+	displayGraph($SQL, "Instance views by month over the past year.");
 }
-
-
-
+if ($_REQUEST['graph']=="year"){
+	$SQL ="SELECT DATE_FORMAT(time, '%Y') AS Date,  COUNT(*) AS numRows FROM workspace_view  where time >CURRENT_DATE() - INTERVAL 5 YEAR AND workspace='".$_REQUEST['workspace']."' GROUP BY Date order by time";
+displayGraph($SQL, "Views of workspace ".$_REQUEST['workspace']." over the past 5 years.");
+}
 ?>
