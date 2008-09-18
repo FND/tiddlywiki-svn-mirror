f170e84800f3b0a3d3f6428592e05e826f4dcc1a
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-18 09:54:18 +0000

    HTMLParserPlugin - lame monolithic but works
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6739 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/HTMLParserPlugin/testHTMLParserPlugin.jsspec.js
@@ -0,0 +1,106 @@
+// <![CDATA[
+
+function __main() {
+	store = new TiddlyWiki();
+	loadShadowTiddlers();
+	store.loadFromDiv("storeArea","store",true);
+	loadPlugins();
+}
+
+describe('HTMLParser: calcAllSlices()', {
+	before_each: function() {
+		__main();
+	},
+
+	'should return a single slice (table notation) with end of line comment': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|-- end of line comment--";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar" };
+		value_of(actual).should_be(expected);
+	},
+	'should return a single slice (table notation) with start of line comment': function() {
+		var title = "tiddler";
+		var text = "-- start of line comment --|foo|bar|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar" };
+		value_of(actual).should_be(expected);
+	},
+	'should return a single slice (table notation) with start and end of line comments': function() {
+		var title = "tiddler";
+		var text = "-- start of line comment --|foo|bar|-- end of line comment--";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar" };
+		value_of(actual).should_be(expected);
+	},
+	'should return slices (table notation) with end of line comment in middle of table': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|-- comment in middle --\n"
+			+ "|lorem|ipsum|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar", lorem: "ipsum" };
+		value_of(actual).should_be(expected);
+	},
+	'should return slices (table notation) with commented out table row': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|\n"
+			+ "--|middle|row|--\n"
+			+ "|lorem|ipsum|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar", lorem: "ipsum" };
+		value_of(actual).should_be(expected);
+	},
+	'should return slices (table notation) with commented out multiple table rows': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|\n"
+			+ "--|middle|row|\n"
+			+ "|another|row|\n"
+			+ "|yetanother|row|--\n"
+			+ "|lorem|ipsum|";
+		store.saveTiddler(title, title, text);
+		var actual = store.calcAllSlices(title);
+		var expected = { foo: "bar", lorem: "ipsum" };
+		value_of(actual).should_be(expected);
+	}
+});
+
+describe('HTMLParser: getTiddlerSlice()', {
+	before_each: function() {
+		__main();
+	},
+
+	'should return a single slice (table notation) with end of line comment': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|-- end of line comment--";
+		store.saveTiddler(title, title, text);
+		var actual = store.getTiddlerSlice(title, "foo");
+		var expected = "bar";
+		value_of(actual).should_be(expected);
+	},
+	'should return a single slice (table notation) with start of line comment': function() {
+		var title = "tiddler";
+		var text = "-- start of line comment --|foo|bar|";
+		store.saveTiddler(title, title, text);
+		var actual = store.getTiddlerSlice(title, "foo");
+		var expected = "bar";
+		value_of(actual).should_be(expected);
+	},
+	'should return slices (table notation) with commented out multiple table rows': function() {
+		var title = "tiddler";
+		var text = "|foo|bar|\n"
+			+ "--|middle|row|\n"
+			+ "|another|row|\n"
+			+ "|yetanother|row|--\n"
+			+ "|lorem|ipsum|";
+		store.saveTiddler(title, title, text);
+		value_of(store.getTiddlerSlice(title, "foo")).should_be("bar");
+		value_of(store.getTiddlerSlice(title, "lorem")).should_be("ipsum");
+	}
+});
+
+// ]]>
