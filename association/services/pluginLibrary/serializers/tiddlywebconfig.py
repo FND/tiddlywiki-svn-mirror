82803c1735e53d177f59107f6e37f0e628670454
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-23 13:56:25 +0000

    Plugin Library: directory reorganization (dedicated directory for aggregation script)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6810 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/services/pluginLibrary/serializers/tiddlywebconfig.py
+++
@@ -1,5 +0,0 @@
-import tiddlyweb.config
-
-config["extension_types"]["store"] = "text/x-tiddlystore"
-config["serializers"]["text/x-tiddlystore"] = ["tiddlyweb.serializations.pureStore",
-	"text/html; charset=UTF-8"]
