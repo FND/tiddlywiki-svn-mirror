439b397ae519b3beeacc0d46eb78c4fb62732b32
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-24 12:24:30 +0000

    cct - preparing selenium environment
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6849 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -1,11 +1,10 @@
 mkdir selenium_tests\
 mkdir selenium_core\
 svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
-c:\xampp\mysql\bin\mysqladmin -u root drop cctiddly_selenium -f
-c:\xampp\mysql\bin\mysqladmin -u root create cctiddly_selenium 
+c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
+c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
 cd c:\xampp\htdocs\Trunk\tests\selenium\selenium_tests
-
-c:\xampp\mysql\bin\mysql -u root cctiddly_selenium  < install.sql
+c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
 //start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive
 																						
 "C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/
