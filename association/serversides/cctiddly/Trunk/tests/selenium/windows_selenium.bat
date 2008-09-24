5d3168714f8b8fca7c99d9094d2004527ad75983
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-24 12:25:38 +0000

    cct - preparing selenium environment
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6850 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -1,7 +1,7 @@
 mkdir selenium_tests\
 mkdir selenium_core\
 svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
-c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
+//c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
 cd c:\xampp\htdocs\Trunk\tests\selenium\selenium_tests
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
