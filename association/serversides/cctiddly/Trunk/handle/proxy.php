3b7914b06016dc36cdef0001f2a580ac8511a198
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-24 10:13:38 +0000

    cct - returned proxy.php to work with php4 ;
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6839 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/proxy.php
+++ association/serversides/cctiddly/Trunk/handle/proxy.php
@@ -6,34 +6,8 @@ if(!isset($cct_base)) {
 include_once($cct_base."includes/header.php");
 include_once($cct_base."includes/config.php");
 	$feed = $_REQUEST['feed'];
- function readExternalFile($feed) {
-    //    $host = parse_url($feed, PHP_URL_HOST);
-    //  echo   $file = substr(parse_url($feed, PHP_URL_PATH), 1);
+	
 
-
-	$url = parse_url($feed);
-		$host = $url[host];
-
-        $fp = fsockopen($host, 80, $errno, $errstr, 30);
-        if (!$fp) {
-           $result = "$errstr ($errno)<br />\n";
-        } else {
-      $out = "GET /".$file." HTTP/1.1\r\n";
-           $out .= "Host: ".$host."\r\n";
-           $out .= "Connection: Close\r\n\r\n";
-
-           fwrite($fp, $out);
-           while (!feof($fp)) {
-                   $result .= fgets($fp);
-           }
-           fclose($fp);
-        }
-
-        ob_start();
-       print ("$result");
-}
-
-$feed = $_REQUEST['feed'];
 $url = parse_url($feed);
 if(!in_array($url[host], $tiddlyCfg['allowed_proxy_list']))
 {
@@ -42,7 +16,7 @@ if(!in_array($url[host], $tiddlyCfg['allowed_proxy_list']))
 }
 if($feed != '' && strpos($feed, 'http') === 0)
 {
-	readExternalFile($feed);
+	readfile($feed);
 	return;
 }
 ?>
