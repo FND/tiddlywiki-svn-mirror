5ab015b82f21f126c7e08b451ada9f41dd3b50c0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-24 11:43:10 +0000

    cct - preparing selenium environment
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6845 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -3,6 +3,6 @@ mkdir selenium_core\
 svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
 c:\xampp\mysql\bin\mysqladmin -u root drop cctiddly_selenium -f
 c:\xampp\mysql\bin\mysqladmin -u root create cctiddly_selenium 
-c:\xampp\mysql\bin\mysql -u root<selenium_tests\install.sql
+c:\xampp\mysql\bin\mysql -u root -d cctiddly_selenium  < C:\xampp\htdocs\Trunk\tests\selenium_tests\install.sql
 //start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive
 "C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/
