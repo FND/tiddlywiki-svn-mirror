2a3aa0d969ed68260763e99e9c1f87e7fe28d3e6
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-28 20:06:09 +0000

    cct - seo module working nicely
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6961 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/modules/seo/files/createHtml.php
+++ association/serversides/cctiddly/Trunk/modules/seo/files/createHtml.php
@@ -1,19 +1,11 @@
 <?php
-
-
-error_log("SEO PLUGGIN OUTPUT PLUGIn");
-
-
 $dir = dirname(dirname(dirname($tiddlyCfg['pref']['upload_dir'])))."/uploads/workspace/".$tiddlyCfg['workspace_name']."/tiddlers";
-	
-error_log("UPLOAD DIR : ".$dir);
-
 	mkdir($dir, 0700, true);
-
 	$myFile = $dir."/".$ntiddler['title'].".html";
 	$fh = fopen($myFile, 'w+') or die("can't open file");
-
-	if(fwrite($fh, formatParametersPOST($_POST['wikifiedBody']))){
+	$doc = "<html>\r\n<head>\r\n<script language='javascript'><!-- \r\nlocation.replace('".dirname(getUrl())."/".$tiddlyCfg['workspace_name']."#".$ntiddler['title']."') \r\n //--></script>\r\n</head>\r\n<body>\r\n";
+	$doc .= $_POST['wikifiedBody']."\r\n</body>\r\n</html>";
+	if(fwrite($fh, formatParametersPOST($doc))){
 	}
 	fclose($fh);		
 
