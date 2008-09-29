285dbb857d90026c2eb440554b9f286b87dd454e
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-29 14:19:25 +0000

    Plugin Library: UI; minor adjustments to build script
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6980 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/services/pluginLibrary/UI/build.py
+++ association/services/pluginLibrary/UI/build.py
@@ -4,9 +4,9 @@ from subprocess import call, Popen, PIPE
 
 cmd = "git svn info ./association/services/pluginLibrary/" # XXX: future Git versions interpret path as relative to $PWD
 info = Popen(cmd.split(" "), stdout = PIPE).communicate()[0]
-rev = re.compile(r"Rev: ([0-9]+)").search(info).groups()[0]
-timestamp = re.compile(r"Date: (.+?)\s\+").search(info).groups()[0]
-build = "alpha demo (rev %s, %s)" % (rev, timestamp)
+rev = re.search(r"Rev: ([0-9]+)", info).groups()[0]
+timestamp = re.search(r"Date: (.+?)\s\+", info).groups()[0]
+build = "alpha demo (revision %s, %s)" % (rev, timestamp)
 
 filepath = "shadows/SiteSubtitle.tiddler"
 f = open(filepath, "r")
