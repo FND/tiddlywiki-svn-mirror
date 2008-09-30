3bdfff7dd434171bc473dc526ac59bd8b146bdf8
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-30 09:47:58 +0000

    - Moved make to new scripts/ dir - Some wording and layout fixes in the portal site
    git-svn-id: http://svn.tiddlywiki.org/Trunk@6985 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/scripts/make.sh
+++ contributors/MichaelMahemoff/scripts/make.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+root=`dirname $0`'/..'
+cd $root
 for recipe in `find . -name *.recipe` ; do
   recipebase=`echo $recipe | sed -e s/\.html\.recipe$//g`
   (cd `dirname $recipebase` ; cook `basename $recipebase`)
 done
 
-pub=`dirname $0`'/pub'
+pub="$root/pub"
 if [ -e $pub ] ; then
   mv $pub /tmp/pub$$
 fi
 mkdir $pub
 
-cp plugins/comments/CommentsPlugin.html verticals/tiddlyguv/TiddlyGuv.html verticals/tiddlyguv/portal/*.html pub
+cp plugins/CommentsPlugin/CommentsPlugin.html verticals/tiddlyguv/TiddlyGuv.html verticals/tiddlyguv/portal/*.html pub
 rsync -avz --delete -e ssh $pub/* $TIDDLYGUV_PUB
