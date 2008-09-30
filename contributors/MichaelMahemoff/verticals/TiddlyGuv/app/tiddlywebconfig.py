7995b192913ac821dd85003518a769bb224c662d
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-09-30 18:11:20 +0000

    TiddlyGuv: Added app/ dir, where I expect the actual entire app to live. This is basically a big tiddlyweb store.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7004 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MichaelMahemoff/verticals/TiddlyGuv/app/tiddlywebconfig.py
@@ -0,0 +1,3 @@
+config = {
+  'server_store': ['text', {'store_root': 'store'}]
+}
---
+++ contributors/MichaelMahemoff/verticals/TiddlyGuv/app/tw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# export PYTHONPATH='../../../../ChrisDent/experimental/TiddlyWeb/'
+export PYTHONPATH=`pwd`
+../../../../ChrisDent/experimental/TiddlyWeb/manager server 0.0.0.0 8080
