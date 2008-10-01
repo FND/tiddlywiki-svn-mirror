e98149d3f5fab5589dfae35a9d8300c1af2a93ba
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-01 10:53:11 +0000

    TiddlyGuv: thanks to tiddlyweb changes, no longer need store/empty
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7019 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/verticals/TiddlyGuv/app/tiddlywebconfig.py
+++ contributors/MichaelMahemoff/verticals/TiddlyGuv/app/tiddlywebconfig.py
@@ -1,3 +1,7 @@
+tiddlyweb_root = "../../../../ChrisDent/experimental/TiddlyWeb"
 config = {
-  'server_store': ['text', {'store_root': 'store'}]
+  'server_store': ['text', {'store_root': 'store'}],
+  'urls_map': tiddlyweb_root + "/urls.map", 
+  'base_tiddlywiki': tiddlyweb_root + "/lib/empty.html"
 }
+
