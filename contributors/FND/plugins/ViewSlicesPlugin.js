5009d52c1a7ddc654bfd75185f6ce50c034e095a
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-01 10:07:08 +0000

    ViewSlicesPlugin: initial commit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7018 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/FND/plugins/ViewSlicesPlugin.js
@@ -0,0 +1,31 @@
+/***
+|''Name''|ViewSlicesPlugin|
+|''Description''|extends the built-in view macro to display slices|
+|''Author''|FND|
+|''Version''|0.1.0|
+|''Status''|@@experimental@@|
+|''Source''|http://devpad.tiddlyspot.com/#ViewSlicesPlugin|
+|''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
+|''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
+!Usage
+{{{
+<<view text slice [name] [defaultValue]>>
+}}}
+!!Examples
+<<view text slice Description "N/A">>
+!Revision History
+!!v0.1 (2008-10-01)
+* initial release
+!Code
+***/
+//{{{
+if(!version.extensions.ViewSlicesPlugin) {
+version.extensions.ViewSlicesPlugin = { installed: true };
+
+config.macros.view.views.slice = function(value, place, params, wikifier, paramString, tiddler) {
+		var text = store.getTiddlerSlice(tiddler.title, params[2]) || params[3];
+		wikify(text, place);
+};
+
+} //# end of "install only once"
+//}}}
