40819014156f72c55674bf5c515882143af75e8b
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-05 14:37:02 +0000

    cct - adding flickr adaptor
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7095 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/flickrAdaptor.js
+++ association/serversides/cctiddly/Trunk/tiddlers/flickrAdaptor.js
@@ -9,7 +9,6 @@ flickrAdaptor.serverType = 'flickr';
 flickrAdaptor.doHttpGET = function(uri,callback,params,headers,data,contentType,username,password)
 {
 		uri = "release/handle/proxy.php?feed="+uri;
-		displayMessage(uri);
 	    return doHttp('GET',uri,data,contentType,username,password,callback,params,headers);
 };
 
@@ -22,27 +21,38 @@ flickrAdaptor.prototype.getWorkspaceList = function(context,userParams,callback)
 	return typeof req == 'string' ? req : true;
 };
 
-function jsonFlickrFeed(o){
-  var i=0;
-  while(o.items[i]){
-		
-		//tiddler.created = convertTimestamp(timestamp).convertToLocalYYYYMMDDHHMM().toString();
-		
-		var d = new Date();
-			d.setTime(Date.parse(o.items[i].published));
-			//d = d.convertToYYYYMMDDHHMM(); 
-		var date = d;
-		tiddler.set(o.items[i].title,"[img["+ o.items[i].title +"|"+ o.items[i].media.m+"]]","modifier",date,"",date,"");
+
+function createTiddler(i){
+		var date = "";
+		var tiddler = new Tiddler(i.title);
+		tiddler.set(i.title,"[img["+ i.title +"|"+ i.media.m+"]]","modifier",date,"",date,"");
 		store.addTiddler(tiddler);
-		console.log(o.items[i]);	
+	    displayMessage("<img src=" + i.media.m + " alt=" + i.title +">’");
+		console.log(tiddler);
+	
+}
+
+function jsonFlickrFeed(o){
+
+	return o; 
+ var i=0;
+  while(o.items[i]){
+	createTiddler(o.items[i]);
     i++;
   }
 }
 
 
+
+
 flickrAdaptor.getWorkspaceListCallback = function(status,context,responseText,uri,xhr)
 {
-	eval(" { " + responseText + " } ");
+	var pics = eval(" { " + responseText + " } ");
+	var i=0;
+  while(pics.items[i]){
+	createTiddler(pics.items[i]);
+    i++;
+  }
 };
 
 
