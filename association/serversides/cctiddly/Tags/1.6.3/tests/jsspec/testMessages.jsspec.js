c28eae4d6e429974e694aaebba6356fe876ce817
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-06 15:46:21 +0000

    cct - removing releases from the Trunk
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7127 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Tags/1.6.3/tests/jsspec/testMessages.jsspec.js
+++
@@ -1,9 +0,0 @@
-// <![CDATA[
-describe('displayMessage', {
-	'should fail if the messageArea element does not exist': function() {
-		var actual = displayMessage('test value');
-		var expected = undefined;
-		value_of(actual).should_be(expected);
-	}
-});
-// ]]>
--- association/serversides/cctiddly/Tags/1.7.1/tiddlers/SiteProxy.tiddler
+++
@@ -1,3 +0,0 @@
-<div title="SiteProxy" modifier="ccTiddly" tags="systemTiddler ProxyService  excludeLists excludeSearch">
-<pre>handle/proxy.php?feed=</pre>
-</div>
