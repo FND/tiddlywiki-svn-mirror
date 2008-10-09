8044ad8cdd01bfbb5f731eab067dfd064c5870e8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-09 16:54:54 +0000

    cct - tweaks to lifestream
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7160 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/lifeStream.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/lifeStream.js
@@ -2,7 +2,6 @@ config.macros.lifeStream = {};
 config.macros.lifeStream.handler = function(place,macroName,params)
 {
 	var context = {};
-	window.ls_callbackCount = 0;
 	context.callback = function() {
 		config.macros.lifeStream.display(place, params);
 	};
@@ -10,24 +9,29 @@ config.macros.lifeStream.handler = function(place,macroName,params)
 	var twitter = new twitterAdaptor();
 	twitter.openHost();
 	twitter.getWorkspaceList(context);
-	var flickr = new flickrAdaptor();
+
+var flickr = new flickrAdaptor();
 	flickr.openHost();
 	context.host = "http://api.flickr.com";
 	flickr.getWorkspaceList(context);
-	var delicious = new deliciousAdaptor();
+		var delicious = new deliciousAdaptor();
 	delicious.openHost();
 	context.host = "http://feeds.delicious.com";
 	delicious.getWorkspaceList(context);
-config.macros.lifeStream.display(place, params);	
+
+
 };
 
 config.macros.lifeStream.display = function (place, params)
 {
-	setStylesheet(".tiddler .button, .tiddler .button:hover {background-repeat:no-repeat;   background-color:#111; margin:20px}"+
+	setStylesheet(".tiddler .button, .tiddler .button:hover {background-repeat:no-repeat;   background-color:#111; margin:20px; float:none}"+
 	".stream { background-repeat:no-repeat; display: block; color:white; padding:10px; margin:10px ; width750px; border:1px solid #111;}"+	
 	".slider { background-color:#111;color:white; margin-left:20px; margin-top:-20px; padding:10px 10px 10px 50px; width:625px;border:1px solid #111;}"+	
+	".imgClass {float:left; display:block; padding-right:10px}"+
 	".tiddler a.deliciousStream,a.delicousStream:hover{padding-left:50px;background:url(http://ransom.redjar.org/images/delicious_icon.gif);background-repeat:no-repeat;  background-color:#111;}"+
-	".textSpace {margin-left:10px; position:relative}"+
+	".textSpace {padding-left:60px; }"+
+	
+	".noFloat {float:none; background-color:red;}"+
 	".tiddler .button:hover	 { background-color:#111; border:1px solid #111;}");
 
 	var tiddlers = store.reverseLookup("tags","excludeLists",false,"modified");
@@ -36,7 +40,7 @@ config.macros.lifeStream.display = function (place, params)
 	for(var t=tiddlers.length-1; t>=last; t--) {
 		switch(tiddlers[t].fields['server.type']){
 			case "flickr":
-				var img = createTiddlyElement(null, "img");
+				var img = createTiddlyElement(null, "img", null, "imgClass");
 				img.src = tiddlers[t].text;
 				img.width = "50";
 				img.height = "50";
@@ -47,9 +51,11 @@ config.macros.lifeStream.display = function (place, params)
 				createTiddlyText(sliderButton, tiddlers[t].title);
 				addClass(sliderButton,"stream flickrStream");
 				wikify("\n\r[img["+tiddlers[t].text+"]]\n\r"+tiddlers[t].created,slider);
+				
+				createTiddlyElement(sliderButton, "div", null, "noFloat");
 			break;
 			case "twitter":
-				var img = createTiddlyElement(null, "img");
+				var img = createTiddlyElement(null, "img", null, "imgClass");
 				img.src = tiddlers[t].fields['user_img'];
 				img.width = "50";
 				img.height = "50";
@@ -59,7 +65,8 @@ config.macros.lifeStream.display = function (place, params)
 				sliderButton.appendChild(img);
 				createTiddlyElement(sliderButton, "div", null, "textSpace",  wikifyStatic(tiddlers[t].text));
 				addClass(sliderButton,"stream twitterStream");
-				wikify("\n\r"+"\n\r"+tiddlers[t].created,slider);
+				createTiddlyElement(sliderButton, "div", null, "noFloat");
+				wikify("\n\r"+tiddlers[t].created,slider);
 			break;
 			case "delicious":
 				var slider = config.macros.slider.createSlider(place, "", tiddlers[t].title);
