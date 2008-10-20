c987a2c15ee242182d4d14e10d540e9f676f23be
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 14:48:08 +0000

    ccTiddly - selenium - adding setup_test.php
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7272 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Trunk/tests/selenium/tests/setup_test.php
@@ -0,0 +1,30 @@
+<?php
+
+set_include_path(get_include_path() . PATH_SEPARATOR . './PEAR/');
+require_once 'Testing/Selenium.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+class ccTests extends PHPUnit_Framework_TestCase
+{
+	private $selenium;
+	public function setUp()
+	{
+		global $fail_count;
+		$fail_count=0;
+		$this->selenium = new Testing_Selenium("*firefox", "http://127.0.0.1/Trunk/");
+		$this->selenium->start();
+		$this->selenium->setSpeed("1");
+	}
+
+	public function tearDown()
+	{
+		$this->selenium->stop();
+	}
+
+
+	$a = new ccTests();
+	$a->setUp();
+
+	//$a->runTests();
+	
+
+?>
