a7e61667fdad686d0ba9795d6a4dfd019199e293
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 14:23:20 +0000

    ccTiddly - selenium - install.sql now called using absolute path
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7267 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -3,6 +3,6 @@ svn update
 c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
 COPY /Y C:\xampp\htdocs\tests\selenium\configs\default_config.php  C:\xampp\htdocs\includes\config.php
-c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
+c:\xampp\mysql\bin\mysql -u root alpha_upgrade<C:\xampp\htdocs\install.sql
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
 "C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php
