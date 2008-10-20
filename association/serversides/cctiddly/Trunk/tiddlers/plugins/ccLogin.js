6194e8add83e3a7639c967fd240da707583640d8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 16:38:19 +0000

    ccTiddly - selenium - adding id to login
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7282 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/ccLogin.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/ccLogin.js
@@ -36,7 +36,7 @@ merge(config.macros.ccLogin,{
 	usernameRequest:"Username",
 	passwordRequest:"Password",
 	stepLoginTitle:null,
-	stepLoginIntroTextHtml:"<table border=0px><tr><td>username</td><td><input name=username id=username tabindex='1'></td></tr><tr><td>password</td><td><input type='password' tabindex='2' class='txtPassword'><input   name='password'></td></tr></table>",
+	stepLoginIntroTextHtml:"<table border=0px><tr><td>username</td><td><input name=username id=username tabindex='1'></td></tr><tr><td>password</td><td><input type='password' id='password' tabindex='2' class='txtPassword'><input   name='password'></td></tr></table>",
 	stepDoLoginTitle:"Logging you in",
 	stepDoLoginIntroText:"we are currently trying to log you in.... ",
 	stepForgotPasswordTitle:"Password Request",
