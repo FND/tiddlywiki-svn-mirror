de75d5931d3f457eb2f655444b9ebcd6848e2051
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 14:12:45 +0000

    ccTiddly - updating window_selenium.bat
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7265 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -2,14 +2,7 @@ cd ../../
 svn update
 c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
-<<<<<<< .mine
-COPY C:\xampp\htdocs\Trunk\tests\selenium\configs\default_config.php  C:\xampp\htdocs\Trunk\includes\config.php
-=======
-COPY tests\selenium\configs\default_config.php  includes/config.php
->>>>>>> .r7263
+COPY C:\xampp\htdocs\tests\selenium\configs\default_config.php  C:\xampp\htdocs\includes\config.php
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
-<<<<<<< .mine
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
-"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/test_login.phps=======
-start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
-"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php>>>>>>> .r7263
+"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php
