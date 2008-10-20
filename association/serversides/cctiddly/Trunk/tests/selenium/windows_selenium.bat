54f9ec57829357a043bdb68d53457175bc3d3442
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 13:57:01 +0000

    ccTiddly - reverting default config.php
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7264 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -2,7 +2,14 @@ cd ../../
 svn update
 c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
+<<<<<<< .mine
+COPY C:\xampp\htdocs\Trunk\tests\selenium\configs\default_config.php  C:\xampp\htdocs\Trunk\includes\config.php
+=======
 COPY tests\selenium\configs\default_config.php  includes/config.php
+>>>>>>> .r7263
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
+<<<<<<< .mine
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
-"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php
+"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/test_login.phps=======
+start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
+"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php>>>>>>> .r7263
