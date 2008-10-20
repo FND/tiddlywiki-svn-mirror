275d5bbdde0775beb097814815fa6c6a21573367
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 14:18:15 +0000

    ccTiddly - forcing override of config.php
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7266 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -2,7 +2,7 @@ cd ../../
 svn update
 c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
-COPY C:\xampp\htdocs\tests\selenium\configs\default_config.php  C:\xampp\htdocs\includes\config.php
+COPY /Y C:\xampp\htdocs\tests\selenium\configs\default_config.php  C:\xampp\htdocs\includes\config.php
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
 "C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php
