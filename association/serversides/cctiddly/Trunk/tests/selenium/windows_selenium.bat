2ada6f369676fd3b18fac38f0cf9a93fd1744522
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 12:16:37 +0000

    ccTiddly - updated windows_selenium.bat files to use update not co
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7261 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -1,8 +1,8 @@
-mkdir selenium_tests\
-svn co http://svn.tiddlywiki.org/Trunk/association/serversides/cctiddly/Trunk/ selenium_tests/
+cd ../../
+svn update
 c:\xampp\mysql\bin\mysqladmin -u root drop alpha_upgrade -f
 c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade 
-cd c:\xampp\htdocs\Trunk\tests\selenium\selenium_tests
+COPY tests\selenium\configs\default_config.php  includes/config.php
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<install.sql
-//start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
+start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
 "C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/selenium_tests/tests/selenium/tests/test_login.php
