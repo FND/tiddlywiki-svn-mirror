91d525717c3990f419f41d14921f8481ae0ed197
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-20 15:21:57 +0000

    ccTiddly - selenium - now using setup test
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7275 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
+++ association/serversides/cctiddly/Trunk/tests/selenium/windows_selenium.bat
@@ -5,4 +5,4 @@ c:\xampp\mysql\bin\mysqladmin -u root create alpha_upgrade
 c:\xampp\mysql\bin\mysql -u root alpha_upgrade<C:\xampp\htdocs\Trunk\install.sql
 COPY /Y C:\xampp\htdocs\tests\selenium\configs\default_config.php  C:\xampp\htdocs\includes\config.php
 start java -jar C:\xampp\htdocs\Trunk\tests\selenium\selenium-server.jar -interactive																						
-"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/tests/test_login.php
+"C:\Program Files\Mozilla Firefox\firefox.exe"  -url http://127.0.0.1/Trunk/tests/selenium/tests/setup_test.php
