60c55f9bcd471c552682161950c4c2d382371220
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-21 12:57:13 +0000

    FatSlicePlugin - couple more noddy test cases
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7301 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/FatSlicePlugin/testFatSlicePlugin.jsspec.js
+++ contributors/PaulDowney/plugins/FatSlicePlugin/testFatSlicePlugin.jsspec.js
@@ -26,7 +26,18 @@ describe('Slice: calcFatSlices()', {
 		var title = "tiddler";
 		store.saveTiddler(title, title, "blah, blah\n" + "||col1|col2|\n" + "|row|val1|val2|\n" + "ya yak yak");
 		value_of(store.calcFatSlices(title)).should_be({row:{col1:'val1',col2:'val2'}});
-	}
+	},
+	'one row, three columns FatSlice table': function() {
+		var title = "tiddler";
+		store.saveTiddler(title, title, "||col1|col2|col3|\n" + "|row|val1|val2|val3|");
+		value_of(store.calcFatSlices(title)).should_be({row:{col1:'val1',col2:'val2',col3:'val3'}});
+	},
+	'two rows, three columns FatSlice table': function() {
+		var title = "tiddler";
+		store.saveTiddler(title, title, "||col1|col2|col3|\n" + "|row1|val1|val2|val3|\n" + "|row2|val4|val5|val6|"
+);
+		value_of(store.calcFatSlices(title)).should_be({ row1:{col1:'val1',col2:'val2',col3:'val3'}, row2:{col1:'val4',col2:'val5',col3:'val6'} });
+	},
 
 });
 
