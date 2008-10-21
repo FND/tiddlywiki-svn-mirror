faf8b5b00ae5a3521d3479e7222f8a356835f6c0
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-21 13:50:32 +0000

    FatSlicePlugin - slice macro defaults to built in slice functionality if only 2 params are provided
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7306 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/FatSlicePlugin/FatSlicePlugin.js
+++ contributors/PaulDowney/plugins/FatSlicePlugin/FatSlicePlugin.js
@@ -19,48 +19,48 @@ version.extensions.FatSlicePlugin = {installed:true};
 TiddlyWiki.prototype.fatSlicesRE = /^\|(.*)\|$/gm;
 
 // @internal
-TiddlyWiki.prototype.calcFatSlices = function(title)
-{
-        var slices = {};
+TiddlyWiki.prototype.calcFatSlices = function(title) {
+	var slices = {};
 	var cols = [];
-        var text = this.getTiddlerText(title,"");
-        this.slicesRE.lastIndex = 0;
-        var m = this.fatSlicesRE.exec(text);
-        while(m) {
+	var text = this.getTiddlerText(title,"");
+	this.slicesRE.lastIndex = 0;
+	var m = this.fatSlicesRE.exec(text);
+	while(m) {
 		var slice = m[1].split('|');
 		var key = slice.shift();
-		if (!cols.length){
+		if (!cols.length) {
 			cols = slice;
 			for(var i=0;i<cols.length;i++){
 				cols[i] = cols[i].replace(/[^\w]/g, '');
 			}
 		} else {
 			row = {};
-			for(var i=0;i<cols.length;i++){
+			for(var i=0; i<cols.length; i++){
 				row[cols[i]]=slice[i];
 			}
 			slices[key] = row;
 		}
-                m = this.fatSlicesRE.exec(text);
-        }
-        return slices;
+		m = this.fatSlicesRE.exec(text);
+	}
+	return slices;
 };
 
-TiddlyWiki.prototype.getFatSlice = function(title,row,col)
-{
-console.log(arguments);
+TiddlyWiki.prototype.getFatSlice = function(title,row,col) {
 	var slices = this.calcFatSlices(title);
-console.log(slices);
 	return slices[row][col];
-}
+};
 
+
+// Slice macro.
 config.macros.slice = {};
 config.macros.slice.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	if(params.length>2){
-		
+	if(params.length > 2){
 		createTiddlyText(place,store.getFatSlice(params[0],params[1],params[2]));
+	} else {
+		// use the builtn in slice functionality.
+		createTiddlyText(place,store.getTiddlerSlice(params[0],params[1]));		
 	}
-}
+};
 
 } //# end of 'install only once'
 //}}}
