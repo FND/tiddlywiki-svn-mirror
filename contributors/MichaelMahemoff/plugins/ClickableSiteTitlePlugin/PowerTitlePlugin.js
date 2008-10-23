cef5efc993cf03d608aac59025cb7a3e1c358f7a
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-23 13:59:24 +0000

    New plugin - makes title clickable. Pretty complete for tiddlyguv purposes afaict
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7352 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MichaelMahemoff/plugins/ClickableSiteTitlePlugin/PowerTitlePlugin.js
@@ -0,0 +1,43 @@
+/***
+|Name|PowerTitlePlugin|
+|Description|Make the title clickable|
+|Source|http://tiddlyguv.org/PowerTitle.html#PowerTitlePlugin|
+|Documentation|http://tiddlyguv.org/PowerTitlePlugin.html#PowerTitlePluginInfo|
+|Version|0.1|
+|Author|Michael Mahemoff, Osmosoft|
+|''License:''|[[BSD open source license]]|
+|~CoreVersion|2.2|
+***/
+
+var stylesheet = store.getTiddlerText(tiddler.title + "##StyleSheet");
+config.shadowTiddlers["PowerTitleStylesheet"] = stylesheet;
+store.addNotification("PowerTitleStylesheet", refreshStyles);
+
+var origRestart = restart;
+window.restart = function() {
+  origRestart();
+
+  // Rewrite site title. This (a) adds onclick event; (b) suppresses tiddler
+  var pageTemplate = store.createTiddler("PageTemplate");
+  pageTemplate.text =
+    store.getShadowTiddlerText("PageTemplate").replace(
+      /<span class='siteTitle' refresh='content' tiddler='SiteTitle'><\/span>/g,
+      "<span onclick='PowerTitlePlugin.onTitleClick();' class='siteTitle' tiddler='SiteTitle'>" + store.getTiddlerText("SiteTitle") + "</span>"
+    );
+  store.saveTiddler("PageTemplate");
+
+} 
+
+PowerTitlePlugin = {
+  onTitleClick: function() { story.closeAllTiddlers(); origRestart(); }
+}
+
+/***
+!StyleSheet
+
+.siteTitle { cursor: pointer; }
+.siteTitle:hover { background: [[ColorPalette::Background]]; color: [[ColorPalette::PrimaryDark]]; }
+
+!(end of StyleSheet)
+
+***/
