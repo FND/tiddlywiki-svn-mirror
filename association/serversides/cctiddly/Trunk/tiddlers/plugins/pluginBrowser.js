90db81ae429538184a5209072c62bdb00c981c39
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-24 11:29:36 +0000

    ccTiddly - pluginBrowser now shows which plguins are installed
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7373 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/pluginBrowser.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/pluginBrowser.js
@@ -4,6 +4,8 @@ merge(config.macros.pluginBrowser,{
        listTemplate: {
        columns: [
        {name: 'Selected', field: 'url', rowName: 'url', type: 'Selector'},
+       {name: 'wiki text', field: 'installed', title: 'installed', type: 'WikiText'},
+       {name: 'wiki text', field: 'saved', title: 'saved', type: 'WikiText'},
 	   {name: 'wiki text', field: 'title', title: "Plugin", type: 'WikiText'},
 	   {name: 'wiki text', field: 'description', title: "Description", type: 'WikiText'}
 	   ],
@@ -29,34 +31,55 @@ config.macros.pluginBrowser.handler=function(place,macroName,params,wikifier,par
 	var me = config.macros.pluginBrowser;
 	var w = new Wizard();
 	w.createWizard(place,me.wizardTitleText);
+	
+
+	
 	w.addStep(null, me.wizardStepText);
 	var markList = w.getElement("markList");
 	var listWrapper = document.createElement("div");
 	markList.parentNode.insertBefore(listWrapper,markList);
-	var listView =
-	ListView.create(listWrapper,me.plugins,me.listTemplate);
+	for(t=0; t<me.plugins.length; t++){
+		p = me.plugins[t];
+		var tiddler = store.getTiddler(me.getFileName(p.url));
+		if(tiddler){
+			p.url = false;
+			p.saved = " true ";
+			if(tiddler.isTagged("systemConfig"))
+				p.installed =" true";
+		}
+	}
+	var listView = ListView.create(listWrapper,me.plugins,me.listTemplate);
 	w.setValue("listView",listView);
+	me.refresh(w);
+};
+
+config.macros.pluginBrowser.refresh=function(w){
+	var me = config.macros.pluginBrowser;
 	w.setButtons([
 		{caption: me.wizardButtonInstallText, tooltip: me.wizardButtonInstallTooltip, onClick: function() {me.install(w);}
 	}]);
 };
-
+	
 config.macros.pluginBrowser.install = function(w){
 	var listView = w.getValue("listView");
 	var rowNames = ListView.getSelectedRows(listView);
 	for(var e=0; e < rowNames.length; e++){
-	var postUri = url+"handle/proxy.php?feed="+rowNames[e];
+		var postUri = url+"handle/proxy.php?feed="+rowNames[e];
 		doHttp('POST', postUri, null, null, null, null, config.macros.pluginBrowser.installCallback);
 	}
 };
 
 config.macros.pluginBrowser.installCallback = function(status,params,responseText,uri,xhr){
-	uri = uri.substring(uri.lastIndexOf('/'));
-	var title = uri.substring(1, uri.indexOf('.js'));
+	var title = config.macros.pluginBrowser.getFileName(uri);
 	var now = new Date();
 	if(!store.getTiddler(title)){
 		store.saveTiddler(title,title,responseText,'ccTiddly',now,'systemConfig');
     }
 	store.resumeNotifications();
 	refreshDisplay();
+};
+
+config.macros.pluginBrowser.getFileName = function(str){
+		str = str.substring(str.lastIndexOf('/'));
+		return str.substring(1, str.indexOf('.js'));
 };
