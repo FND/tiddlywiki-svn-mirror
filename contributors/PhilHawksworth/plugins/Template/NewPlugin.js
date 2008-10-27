563baa2f9f1fa96a3360cf881efc3c8452ead647
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-27 11:29:03 +0000

    plugin template added for convenience
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7395 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PhilHawksworth/plugins/Template/NewPlugin.js
@@ -0,0 +1,14 @@
+/***
+|''Name:''| |
+|''Description:''|Description|
+|''Author:''|Phil Hawksworth|
+|''Version:''|0.1|
+|''Date:''|Tue 21 Oct 2008 10:13:55 BST|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''~CoreVersion:''|2.4.1|
+***/
+
+//{{{
+	
+//}}}
