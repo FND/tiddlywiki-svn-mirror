26b424b0931a2e5aa83bd0f3d0a32dbbe6e5a199
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-28 17:41:33 +0000

    ccTiddly - adding #!/bin/sh to .sh file
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7435 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tests/selenium/mac_selenium.sh
+++ association/serversides/cctiddly/Trunk/tests/selenium/mac_selenium.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
 cd /Applications/xampp/xamppfiles/htdocs/Trunk
 svn update
 /Applications/xampp/xamppfiles/bin/mysqladmin -u root drop alpha_upgrade -f
 /Applications/xampp/xamppfiles/bin/mysqladmin -u root create alpha_upgrade 
 /Applications/xampp/xamppfiles/bin/mysql -u root alpha_upgrade< /Applications/xampp/xamppfiles/htdocs/install.sql
 cp  /Applications/xampp/xamppfiles/htdocs/tests/selenium/configs/default_config.php   /Applications/xampp/xamppfiles/htdocs/Trunk/includes/config.php
-java -jar /Users/simonmcmanus/selenium-server/selenium-server.jar -interactive
+java -jar /Users/simonmcmanus/selenium-server/selenium-server.jar -interactive &
 "/Applications/Firefox.app/Contents/MacOS/firefox"  -url http://127.0.0.1/Trunk/tests/selenium/tests/setup_test.php
-
