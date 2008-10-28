ed4ea7266d9654c8742fd38f07231b9a661e07b4
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-28 16:49:44 +0000

    JigglyWiki - establishing core style for tiddlers and also changing the html structure of tiddlers to utilise hAtom
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7431 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/jigglywiki/js/jigglywiki.js
+++ contributors/PhilHawksworth/jigglywiki/js/jigglywiki.js
@@ -1,6 +1,45 @@
+// create a jigglwiki namsespace.
+var jw = jq.fn.jw = {};
 
+
+// DOM ready. Let's go!
 jq(document).ready(function() {
 	
 	loadJQueryExtensions();
+	jw.init();
 	
 });
+
+
+jw.log = function(str) {
+	if(window.console && window.console.log) {
+		console.log(str);
+	} else {
+		alert(str);
+	}
+};
+
+
+
+jw.init = function() {
+	
+	jq('div.tiddler').hide();
+	// jw.showDefaultTiddlers();
+	// jw.addEventHandlers();
+	// jw.pagemap.render();
+};
+
+
+// Show the default tiddlers
+jw.showDefaultTiddlers = function(container) {
+	var container = container ? container : "story1";
+	var links = jw.getTiddlerData('DefaultTiddlers', 'store').tiddlerLinks;	
+	links.each(function(){	
+		jw.displayTiddler( jq(this).attr('href'), { 
+			container: container,
+			template: 'ViewTemplate',
+			overflow: true,
+			animate: false
+		});	
+	});	
+};
