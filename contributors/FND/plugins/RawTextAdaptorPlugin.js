fe9d591810a66bc3c58e0282acc8223fb4491f14
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-29 09:21:54 +0000

    RawTextAdaptorPlugin: stripping file extension from tiddler title
    N.B.: This version requires TiddlyWiki v2.5 (cf. changeset:7427).
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7452 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/RawTextAdaptorPlugin.js
+++ contributors/FND/plugins/RawTextAdaptorPlugin.js
@@ -2,17 +2,17 @@
 |''Name''|RawTextAdaptorPlugin|
 |''Description''|adaptor for importing plain-text files (e.g. from Subversion) as tiddlers|
 |''Author''|FND|
-|''Version''|0.1.0|
-|''Status''|@@experimental@@|
+|''Version''|0.2.0|
+|''Status''|@@beta@@|
 |''Source''|http://devpad.tiddlyspot.com/#RawTextAdaptorPlugin|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
+|''CoreVersion''|2.5|
 !Revision History
 !!v0.1 (2008-10-28)
 * initial release
 !To Do
-* strip file extension in title
-* properly normalize URIs (cf. trimURI)
+* properly normalize URIs (cf. {{{trimURI}}})
 !Code
 ***/
 //{{{
@@ -21,6 +21,7 @@ function RawTextAdaptor() {}
 RawTextAdaptor.prototype = new AdaptorBase();
 RawTextAdaptor.serverType = "rawtext";
 RawTextAdaptor.serverLabel = "Raw Text";
+RawTextAdaptor.defaultModifier = "..."; // XXX: use empty string?
 
 RawTextAdaptor.prototype.getWorkspaceList = function(context, userParams, callback) {
 	context = this.setContext(context, userParams, callback);
@@ -37,7 +38,7 @@ RawTextAdaptor.prototype.getTiddlerList = function(context, userParams, callback
 	if(context.callback) {
 		context.status = true;
 		var filename = trimURI(context.host).split("/").pop();
-		context.tiddlers = [new Tiddler(filename)];
+		context.tiddlers = [RawTextAdaptor.createTiddler(filename)];
 		window.setTimeout(function() { callback(context, context.userParams); }, 0);
 	}
 	return true;
@@ -45,18 +46,11 @@ RawTextAdaptor.prototype.getTiddlerList = function(context, userParams, callback
 
 RawTextAdaptor.prototype.getTiddler = function(title, context, userParams, callback) {
 	context = this.setContext(context, userParams, callback);
-	context.title = title; // XXX: unused?
 	if(!context.tiddler) {
-		context.tiddler = new Tiddler(title);
+		context.tiddler = RawTextAdaptor.createTiddler(title);
 	}
-	context.created = new Date(); // XXX: ??
-	context.modified = context.created;
-	context.modified = ""; // XXX: ??
 	context.tiddler.fields["server.type"] = RawTextAdaptor.serverType;
 	context.tiddler.fields["server.host"] = AdaptorBase.minHostName(context.host);
-	if(title.substring(title.length - 3) == ".js") {
-		context.tiddler.tags = ["systemConfig"];
-	}
 	var req = httpReq("GET", context.host, RawTextAdaptor.getTiddlerCallback, context);
 	return typeof req == "string" ? req : true;
 };
@@ -71,6 +65,22 @@ RawTextAdaptor.getTiddlerCallback = function(status, context, responseText, uri,
 	if(context.callback) {
 		context.callback(context, context.userParams);
 	}
+};
+
+// create tiddler from filename
+RawTextAdaptor.createTiddler = function(filename) {
+	var tiddler = new Tiddler(filename);
+	var pos = filename.lastIndexOf(".");
+	if(pos != -1) { // strip file extension
+		tiddler.title = title.substr(0, pos); //# N.B.: altering title is acceptable since it's not used as URI component
+		if(title.substr(pos) == ".js") { // treat as plugin
+			tiddler.tags = ["systemConfig"];
+		}
+	}
+	context.created = new Date();
+	context.modified = context.created;
+	context.modifier = RawTextAdaptor.defaultModifier;
+	return tiddler;
 };
 
 config.adaptors[RawTextAdaptor.serverType] = RawTextAdaptor;
