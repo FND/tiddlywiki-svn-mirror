e3029950f65d624b26ebec1d07dc1f98f8d7a53c
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-31 12:56:22 +0000

    Phil's changes
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7512 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/jigglywiki/js/jigglywiki.js
+++ contributors/PhilHawksworth/jigglywiki/js/jigglywiki.js
@@ -23,48 +23,42 @@ jw.log = function() {
 
 jw.init = function() {
 	
-	jw.store = jq('#store').tiddlerStore();
+	jw.store = jq('#store').hide().tiddlerStore();
 	
-	var t;
-
-	t = jw.store.fetch();
-	jw.log('all tiddlers:', t);
-
-	t = jw.store.fetch({modifier: 'PhilHawksworth'});
-	jw.log('all tiddlers modified by PhilHawksworth:', t);
-	
-	t = jw.store.fetch('PhilHawksworth');
-	jw.log('The PhilHawksworth tiddler:', t);
-
-	t = jw.store.fetch('PhilHawksworth', {modifier: 'PhilHawksworth'});
-	jw.log('the tiddlers of a set of one, modified by PhilHawksworth:', t);
-	
-	t = jw.store.fetch('PhilHawksworth','JigglyWiki','NotARealTiddler');
-	jw.log('2 valid tiddlers (from 3 requested):', t);
-		
-	t = jw.store.fetch('PhilHawksworth','JigglyWiki','NotARealTiddler', {modifier: 'PhilHawksworth'});
-	jw.log('the tiddlers of a set of 3, modified by PhilHawksworth:', t);
-
-	
-
-
-	
-	// jw.showDefaultTiddlers();
+	jw.showDefaultTiddlers();
 	// jw.addEventHandlers();
 	// jw.pagemap.render();
 };
 
 
 // Show the default tiddlers
-jw.showDefaultTiddlers = function(container) {
-	var container = container ? container : "story1";
-	var links = jw.getTiddlerData('DefaultTiddlers', 'store').tiddlerLinks;	
-	links.each(function(){	
-		jw.displayTiddler( jq(this).attr('href'), { 
-			container: container,
-			template: 'ViewTemplate',
-			overflow: true,
-			animate: false
-		});	
-	});	
+jw.showDefaultTiddlers = function() {
+
+	var tiddler = jw.store.fetch('DefaultTiddlers');
+	tiddler.find('div.entry-content a.tiddlerLink').each(function(){
+		jw.displayTiddler(jq(this));
+	});
+	
+	// var links = jw.getTiddlerData('DefaultTiddlers', 'store').tiddlerLinks;	
+	// links.each(function(){	
+	// 	jw.displayTiddler( jq(this).attr('href'), { 
+	// 		container: container,
+	// 		template: 'ViewTemplate',
+	// 		overflow: true,
+	// 		animate: false
+	// 	});	
+	// });	
 };
+
+
+jw.displayTiddler = function(link, options) {
+	
+	// TODO: handler either tiddler names or tiddlerLinks.
+
+	var name = link.text();
+	var t = jw.store.fetch(name).clone();
+	// jq(t).expandMacros();
+	jq('#story1').append(t);
+};
+
+
