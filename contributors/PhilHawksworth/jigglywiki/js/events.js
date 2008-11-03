9542d4b923a3f8dd35b957cb69c4b7ed24af56d4
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-03 14:21:46 +0000

    JigglyWiki - initial event handling
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7556 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PhilHawksworth/jigglywiki/js/events.js
@@ -0,0 +1,42 @@
+// Event handlers.
+
+
+// handle a click on any element within a story
+jq('div.story').click(function(ev){
+
+	var target = jq(ev.target);
+
+	if( target.is('a.tiddlerLink') ) {
+		// tiddlerlink button clicks
+		
+		ev.preventDefault();
+		jw.log('tiddlerLink clicked', target.text());
+	
+	} else if( target.is('a.command') ) {
+		// Command link clicks
+		
+		ev.preventDefault();
+		var tiddler = target.parents('div.hentry');
+		var command = target.attr('class').replace('command ','');
+
+		jw.log('command clicked', tiddler, command);
+	
+	}
+	
+});
+
+
+// doublicking
+jq('div.story').dblclick(function(ev){
+	
+	var target = jq(ev.target);
+	
+
+	if( target.is('div.entry-content') ) {
+		// doubleclick tiddler text
+		
+		jw.log('tiddler dblclick');
+	
+	}
+	
+});
