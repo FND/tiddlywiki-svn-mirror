ffa99cc7b1b1cfdf3e6048453a886b33642323b5
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-07 15:52:51 +0000

    ccTiddly - edit password functionality now works nicely
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7670 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/changePassword.php
+++ association/serversides/cctiddly/Trunk/handle/changePassword.php
@@ -1,5 +1,26 @@
 <?php
 
 
+$cct_base = "../";
+include_once($cct_base."includes/header.php");
+debug($_SERVER['PHP_SELF'], "handle");	
 
+if($_POST['new1']!=$_POST['new2'])
+{
+	debug("Passwords do not match", "params");
+	exit;
+}
+
+if(!user_login($user['username'],$_POST['old1']))
+{
+	debug("Permissions denied when changing password. Did you enter the correct details?");
+	exit;
+}
+
+$odata['username'] = $user['username'];
+$ndata['password'] = $_POST['new2'];
+if(db_record_update($tiddlyCfg['table']['user'],$odata,$ndata))
+echo "all went well";
+else
+	sendHeader(304);
 ?>
--- association/serversides/cctiddly/Trunk/tiddlers/plugins/changePassword.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/changePassword.js
@@ -2,13 +2,15 @@ config.macros.changePassword={};
 merge(config.macros.changePassword,{
 	title:"Change Password", 
 	subTitle : "for user ", 
-	step1Html: "Old Password : <input name='old' /><br/> New Password : <input name='new1' /><br /> New Password Again : <input name='new2' /> ",   
+	step1Html: "Old Password : <input name='old' type='password'/><br/> New Password : <input name='new1' type='password' /><br /> New Password Again : <input name='new2' type='password' /> ",   
 	buttonChangeText:"Change Password",
 	buttonChangeToolTip:"Click to change your password", 
 	buttonCancelText:"Cancel",
 	buttonCancelToolTip:"Click to cancel",
 	noticePasswordsNoMatch : "Your new passwords do not match", 
-	noticePasswordWrong : "Your password is incorrect." 
+	noticePasswordWrong : "Your password is incorrect.",
+	noticePasswordUpdated : "Your Password has been updated", 
+	noticePasswordUpdateFailed : "Your Password was NOT updated." 
 });
 
 config.macros.changePassword.handler=function(place,macroName,params,wikifier,paramString,tiddler,errorMsg){
@@ -27,12 +29,14 @@ config.macros.changePassword.doPost = function (w) {
 		alert(me.noticePasswordsNoMatch);
 		return false;
 	}
-	doHttp("POST", url+"handle/changePassword.php", "?new1="+w.formElem.new1.value+"&new2="+w.formElem.new2.value+"&old="+w.formElem.old.value,null,null,null,config.macros.changePassword.callback);	
+	doHttp("POST", url+"handle/changePassword.php", "&new1="+Crypto.hexSha1Str(w.formElem.new1.value)+"&new2="+Crypto.hexSha1Str(w.formElem.new2.value)+"&old1="+Crypto.hexSha1Str(w.formElem.old.value),null,null,null,config.macros.changePassword.callback);	
 }
 
-
 config.macros.changePassword.callback = function(status,context,responseText,uri,xhr) {
-	alert("herer");
+	if(xhr.status == 304)
+		displayMessage(me.noticePasswordUpdateFailed);
+	else
+		displayMessage(me.noticePasswordUpdated);
 }
 
 //}}}
