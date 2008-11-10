d6cdd9378a6946a8fb97299f91a3f25c692e86c2
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-10 17:13:17 +0000

    ccTiddly - more plugin goodness
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7689 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/RSS/index.php
+++ association/serversides/cctiddly/Trunk/plugins/RSS/index.php
@@ -2,5 +2,5 @@
 $m = new Plugin('RSS 2.0','0.1','simonmcmanus.com');
 
 // before returning file not found check if the file exists in the uploads/tiddlers/ directory. 
-$p->addTiddler('/Applications/xampp/xamppfiles/htdocs/plugins/RSS/files/MarkupPostBody.tiddler');
+$p->addTiddler(null, getcwd().'/plugins/RSS/files/MarkupPostBody.tiddler');
 ?>
