fefd75cdc86bb2fc9d74eecdae7500256613e11b
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-10 17:18:04 +0000

    ccTiddly - more plugin goodness
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7690 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/OpenID/index.php
+++ association/serversides/cctiddly/Trunk/plugins/OpenID/index.php
@@ -1,15 +1,10 @@
 <?php
 $p = new Plugin('OpenID Plugin','0.1','simonmcmanus.com');
-$p->addEvent("postSetLoginPerm", './plugins/OpenID/files/openid/common.php');
-
-
-
+$p->addEvent("postSetLoginPerm", getcwd().'/plugins/OpenID/files/openid/common.php');
 $data['tags'] = 'systemConfig';
-$p->addTiddler($data, '/Applications/xampp/xamppfiles/htdocs/plugins/OpenID/files/OpenIDPlugin.js');
-
-
+$p->addTiddler($data, getcwd().'/plugins/OpenID/files/OpenIDPlugin.js');
 $data1['tags'] = 'loginBox';
-$p->addTiddler($data1, '/Applications/xampp/xamppfiles/htdocs/plugins/OpenID/files/OpenID.tiddler');
+$p->addTiddler($data1, getcwd().'/plugins/OpenID/files/OpenID.tiddler');
 
 
 ?>
