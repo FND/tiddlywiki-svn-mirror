03da83ed3ef49654e695943a3fa35d4f65865b6e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-11 17:13:10 +0000

    ccTiddly - changes to rss plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7721 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/RSS/index.php
+++ association/serversides/cctiddly/Trunk/plugins/RSS/index.php
@@ -4,9 +4,9 @@ $p = new Plugin('RSS 2.0','0.1','simonmcmanus.com');
 // before returning file not found check if the file exists in the uploads/tiddlers/ directory. 
 $tiddler['title'] = "MarkupPostBody";
 if($tiddlyCfg['use_mod_rewrite'] ==1)
-	$tiddler['body'] = "<link rel='alternate' type='application/rss+xml' title='RSS' href='".getURL()."?workspace='".$tiddlyCfg['workspace_name']."'/>";
+	$tiddler['body'] = "<link rel='alternate' type='application/rss+xml' title='RSS Feed for ccTiddly workspace : ".$tiddlyCfg['workspace_name']."' href='".getURL()."?workspace='".$tiddlyCfg['workspace_name']."'/>";
 else
-	$tiddler['body'] = "<link rel='alternate' type='application/rss+xml' title='RSS' href='".getURL().$tiddlyCfg['workspace_name']."/index.xml'/>";
+	$tiddler['body'] = "<link rel='alternate' type='application/rss+xml' title='RSS Feed for ccTiddly workspace : ".$tiddlyCfg['workspace_name']."' href='".getURL()."/".$tiddlyCfg['workspace_name']."/index.xml'/>";
 
 $p->addTiddler($tiddler);
 $p->addEvent("returnNotFound", 'RSS/files/URImapping.php');
