67811dcb13244f8dd00d75b304a1032c9e2968fc
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-12 12:27:26 +0000

    ccTiddly - disabling more plugins in preperation for 1.7.5
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7736 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/lifestream/DISABLEDindex.php
+++ association/serversides/cctiddly/Trunk/plugins/lifestream/DISABLEDindex.php
@@ -8,4 +8,11 @@ $m->addEvent("outputTiddlers", 'lifestream/files/include.php');
 $p = new Plugin('Life Steaminging Plugin','0.1','simonmcmanus.com');
 $p->addTiddlersFolder('/Applications/xampp/xamppfiles/htdocs/plugins/lifestream/files');
 */
+
+
+/*
+$p = new Plugin('Life Steaminging Plugin','0.1','simonmcmanus.com');
+$p->addTiddlersFolder(getcwd().'/plugins/lifestream/files');
+*/
+
 ?>
--- association/serversides/cctiddly/Trunk/plugins/lifestream/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
-$p = new Plugin('Life Steaminging Plugin','0.1','simonmcmanus.com');
-$p->addTiddlersFolder(getcwd().'/plugins/lifestream/files');
-*/
-
-?>
