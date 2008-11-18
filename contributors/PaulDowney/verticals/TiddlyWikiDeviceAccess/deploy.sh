731f0f499f8f1c6895fcd612ae78ca5ddc996ba8
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-18 13:28:49 +0000

    W3C paper - static html
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7826 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyWikiDeviceAccess/deploy.sh
+++ contributors/PaulDowney/verticals/TiddlyWikiDeviceAccess/deploy.sh
@@ -1,1 +1,2 @@
 scp index.html osmodot@www.osmosoft.com:/data/vhost/www.osmosoft.com/html/~psd/TiddlyWikiDeviceAccess/index.html
+scp paper.html osmodot@www.osmosoft.com:/data/vhost/www.osmosoft.com/html/~psd/TiddlyWikiDeviceAccess/paper.html
