dd36c8b511108323fd876938c8a2607d29b06756
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-19 14:41:12 +0000

    ccTiddly - start of templating work - tiddlers being inserted into the database
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7867 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/templating.php
+++ association/serversides/cctiddly/Trunk/templating.php
@@ -1,8 +1,11 @@
 <?php
+include_once('includes/header.php');
+
 //extract tiddlers
 $locations['PRE_HEAD']['start'] = '<!--PRE-HEAD-START-->';
 $locations['PRE_HEAD']['end'] = '<!--PRE-HEAD-END-->';
 $locations['PRE_HEAD']['type'] = 'text';
+$locations['PRE_HEAD']['tiddler'] = 'MarkUpPreHead';
 $locations['POST_HEAD']['start'] = '<!--POST-HEAD-START-->';
 $locations['POST_HEAD']['end'] = '<!--POST-HEAD-END-->';
 $locations['PRE_HEAD']['type'] = 'text';
@@ -13,8 +16,9 @@ foreach($locations as $loc)
 {
 	$start_pos = strpos($file, $loc['start'])+strlen($loc['start']);
 	$end_pos = strpos($file, $loc['end']) - $start_pos;
-	echo substr($file, $start_pos, $end_pos);	
-	echo '<hr />';
+	$body = substr($file, $start_pos, $end_pos);	
+	$tiddler = tiddler_create($loc['tiddler'], $body);
+	tiddler_insert($tiddler);
 }
 	
 exit;
