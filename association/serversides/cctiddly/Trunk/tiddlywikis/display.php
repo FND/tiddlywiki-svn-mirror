8583390420ab8509c54948ca9a891f37c939766e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-19 20:13:14 +0000

    ccTiddly - templates now working from the cctiddly tiddlers array
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7875 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlywikis/display.php
+++ association/serversides/cctiddly/Trunk/tiddlywikis/display.php
@@ -1,9 +1,12 @@
 <?php
-$cct_base = '../';
-include_once('../includes/header.php');
 
-
-
+/*
+ * // Is this needed?
+$locations['SITE_TITLE']['start'] = '<title>';
+$locations['SITE_TITLE']['end'] = '</title>';
+$locations['SITE_TITLE']['type'] = 'text';
+$locations['SITE_TITLE']['tiddler'] = 'SiteTitle';
+$locations['SITE_TITLE']['text'] = ' new title from simon';
 
 $locations['STORE_AREA']['start'] = '<!--POST-SHADOWAREA-->
 <div id="storeArea">';
@@ -13,42 +16,37 @@ $locations['POST_HEAD']['text'] = '<div id="storeArea"><div title="SiteTitle" mo
 <pre>//teamtasks//</pre>
 </div></div>';
 
-$locations['SITE_TITLE']['start'] = '<title>';
-$locations['SITE_TITLE']['end'] = '</title>';
-$locations['SITE_TITLE']['type'] = 'text';
-$locations['SITE_TITLE']['tiddler'] = 'SiteTitle';
-$locations['SITE_TITLE']['text'] = ' new title from simon';
+* */
+
+$cct_base = '../';
+include_once('../includes/header.php');
+ 	$tiddlers = getAllTiddlers();
+
+
+$locations['STORE_AREA']['start'] = '<!--POST-SHADOWAREA-->';
+$locations['STORE_AREA']['end'] = '<!--POST-STOREAREA-->';
+$locations['STORE_AREA']['tiddlers'] = $tiddlers;
+
 
 $locations['PRE_HEAD']['start'] = '<!--PRE-HEAD-START-->';
 $locations['PRE_HEAD']['end'] = '<!--PRE-HEAD-END-->';
-$locations['PRE_HEAD']['type'] = 'text';
-$locations['PRE_HEAD']['tiddler'] = 'MarkUpPreHead';
+$locations['PRE_HEAD']['tiddlers'] = $tiddlers['MarkupPreHead'];
 
 $locations['POST_HEAD']['start'] = '<!--POST-HEAD-START-->';
 $locations['POST_HEAD']['end'] = '<!--POST-HEAD-END-->';
-$locations['POST_HEAD']['type'] = 'text';
-$locations['POST_HEAD']['tiddler'] = 'MarkUpPostHead';
+$locations['POST_HEAD']['tiddlers'] = $tiddlers['MarkupPostHead'];
 
 $locations['PRE_BODY']['start'] = '<!--PRE-BODY-START-->';
 $locations['PRE_BODY']['end'] = '<!--PRE-BODY-END-->';
-$locations['PRE_BODY']['type'] = 'text';
-$locations['PRE_BODY']['tiddler'] = 'MarkUpPostHead';
+$locations['PRE_BODY']['tiddlers'] = $tiddlers['MarkupPreBody'];
 
 $locations['POST_SCRIPT']['start'] = '<!--POST-SCRIPT-START-->';
 $locations['POST_SCRIPT']['end'] = '<!--POST-SCRIPT-END-->';
-$locations['POST_SCRIPT']['type'] = 'text';
-$locations['POST_SCRIPT']['tiddler'] = 'MarkUpPostHead';
- 
+$locations['POST_SCRIPT']['tiddlers'] = $tiddlers['MarkupPostBody'];
+
+
 $file = file_get_contents('teamtasks.html');
 
-/*
-$locations['STORE_AREA']['start'] = '<b>';
-$locations['STORE_AREA']['end'] = '</b>';
-$locations['STORE_AREA']['type'] = 'store';
-$locations['STORE_AREA']['text'] = 'BOB';
-*/
-
-//$file = 'helly my name is <b>something</b> ... ';
 foreach($locations as $loc)
 {
 	$str_start = $loc['start'];
@@ -58,13 +56,16 @@ foreach($locations as $loc)
 	$end_pos = strpos($string, $str_end);
 	$part1 = substr($string, 0, $start_pos);
 	$part2 = substr($string, $end_pos, strlen($string));
-	if($loc['text'])
-		echo  $part1.$loc['text'].$part2;
-//exit;
-	//$tiddler = tiddler_create($loc['tiddler'], $body);
-	//tiddler_insert($tiddler);
-	//e	cho $loc['tiddler'].'create </br>';	
+	if($loc['tiddlers'])
+	{
+
+		foreach($loc['tiddlers'] as $tiddler)
+		print_r($part1.tiddler_bodyDecode($tiddler['body'])).$part2;
+	
+	
+
+	}
 }
 
+echo $file;
 ?>
-	
