7537049959107b4e75954b7f9523330aade33149
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-19 14:31:57 +0000

    ccTiddly - start of templating work
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7866 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/templating.php
+++ association/serversides/cctiddly/Trunk/templating.php
@@ -1,15 +1,32 @@
 <?php
-
 //extract tiddlers
+$locations['PRE_HEAD']['start'] = '<!--PRE-HEAD-START-->';
+$locations['PRE_HEAD']['end'] = '<!--PRE-HEAD-END-->';
+$locations['PRE_HEAD']['type'] = 'text';
+$locations['POST_HEAD']['start'] = '<!--POST-HEAD-START-->';
+$locations['POST_HEAD']['end'] = '<!--POST-HEAD-END-->';
+$locations['PRE_HEAD']['type'] = 'text';
 
 $file = file_get_contents('tiddlywikis/empty.html');
 
+foreach($locations as $loc)
+{
+	$start_pos = strpos($file, $loc['start'])+strlen($loc['start']);
+	$end_pos = strpos($file, $loc['end']) - $start_pos;
+	echo substr($file, $start_pos, $end_pos);	
+	echo '<hr />';
+}
+	
+exit;
+
+
+
 $start = '<!--PRE-HEAD-START-->';
 $end = '<!--PRE-HEAD-END-->';
-$start_pos = strpos($file, $start);
-$end_pos = strpos($file, $end);
+$start_pos = strpos($file, $start)+strlen($start);
+$end_pos = strpos($file, $end) - $start_pos - strlen($start);
 
-echo substr($file, $start_pos+strlen($start), $end_pos - $start_pos - strlen($start));
+echo substr($file, $start_pos, $end_pos);
 
 ?>
 
