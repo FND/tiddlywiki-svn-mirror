f50032b1bbd44a24fdea9db324e9eecd21a020c1
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-22 14:45:57 +0000

    HTTPSavingPlugin: fixing JSLint errors and warnings
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7905 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/Zooko/HTTPSavingPlugin.js
+++ contributors/Zooko/HTTPSavingPlugin.js
@@ -3,7 +3,7 @@
 |''Description''|<...>|
 |''Author''|Zooko|
 |''Contributors''|FND|
-|''Version''|0.1.1|
+|''Version''|0.1.2|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/contributors/Zooko/HTTPSavingPlugin.js|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/Zooko/HTTPSavingPlugin.js|
@@ -28,18 +28,19 @@ readOnly = false;
 config.options.chkHttpReadOnly = false;
 showBackstage = true;
 
-function saveTest() {
+saveTest = function() {
 	var s = document.getElementById("saveTest");
 	/*if(s.hasChildNodes()) {
 		alert(config.messages.savedSnapshotError);
 	}*/
 	s.appendChild(document.createTextNode("savetest"));
-}
+};
 
 // Save this TiddlyWiki with the pending changes
-function saveChanges(onlyIfDirty,tiddlers) {
-	if(onlyIfDirty && !store.isDirty())
+saveChanges = function(onlyIfDirty,tiddlers) {
+	if(onlyIfDirty && !store.isDirty()) {
 		return;
+	}
 	clearMessage();
 	// Get the URL of the document
 	var originalPath = getPath(document.location.toString());
@@ -48,10 +49,11 @@ function saveChanges(onlyIfDirty,tiddlers) {
 		//log("loaded remote file from ", originalPath);
 		/*log("got callback status ", status, "\n", context: ", context, "\n",
 			URL: ", url, "\n", XHR: ", xhr);*/
-		if(original == null) {
+		if(original === null) {
 			alert(config.messages.cantSaveError);
-			if(store.tiddlerExists(config.messages.saveInstructions))
+			if(store.tiddlerExists(config.messages.saveInstructions)) {
 				story.displayTiddler(null,config.messages.saveInstructions);
+			}
 			return;
 		}
 		// Locate the storeArea div's
@@ -63,57 +65,60 @@ function saveChanges(onlyIfDirty,tiddlers) {
 		saveRss(originalPath);
 		saveEmpty(originalPath,original,posDiv);
 		saveMain(originalPath,original,posDiv);
-	}
+	};
 	var result = loadRemoteFile(originalPath, localCallback);
 	//log("result from loadRemoteFile: ", result);
 	return true;
-}
+};
 
 // override and disable saveBackup()
-function saveBackup(localPath,original) {}
+saveBackup = function(localPath,original) {};
 
 // override and disable getLocalPath()
-function getLocalPath(origPath) {}
+getLocalPath = function(origPath) {};
 
 // override getPath()
-function getPath(origPath) {
+getPath = function(origPath) {
 	var originalPath = convertUriToUTF8(origPath,config.options.txtFileSystemCharSet);
 	// Remove any location or query part of the URL
 	var argPos = originalPath.indexOf("?");
-	if(argPos != -1)
+	if(argPos != -1) {
 		originalPath = originalPath.substr(0,argPos);
+	}
 	var hashPos = originalPath.indexOf("#");
-	if(hashPos != -1)
+	if(hashPos != -1) {
 		originalPath = originalPath.substr(0,hashPos);
+	}
 	// Convert file://localhost/ to file:///
-	if(originalPath.indexOf("file://localhost/") == 0)
+	if(originalPath.indexOf("file://localhost/") === 0) {
 		originalPath = "file://" + originalPath.substr(16);
+	}
 	// Convert to a native file format
 	var resultPath;
-	if(originalPath.indexOf("http://") == 0) // HTTP file
-		resultPath = originalPath
+	if(originalPath.indexOf("http://") === 0) { // HTTP file
+		resultPath = originalPath;
 	} else if(originalPath.charAt(9) == ":") { // PC local file
 		resultPath = unescape(originalPath.substr(8)).replace(new RegExp("/","g"),"\\");
-	} else if(originalPath.indexOf("file://///") == 0) { // Firefox PC network file
+	} else if(originalPath.indexOf("file://///") === 0) { // Firefox PC network file
 		resultPath = "\\\\" + unescape(originalPath.substr(10)).replace(new RegExp("/","g"),"\\");
-	} else if(originalPath.indexOf("file:///") == 0) { // *nix local file
+	} else if(originalPath.indexOf("file:///") === 0) { // *nix local file
 		resultPath = unescape(originalPath.substr(7));
-	} else if(originalPath.indexOf("file:/") == 0) { // *nix local file
+	} else if(originalPath.indexOf("file:/") === 0) { // *nix local file
 		resultPath = unescape(originalPath.substr(5));
 	} else { // PC local file
 		resultPath = "\\\\" + unescape(originalPath.substr(7)).replace(new RegExp("/","g"),"\\");
 	}
 	return resultPath;
-}
+};
 
 // override saveFile()
-function saveFile(fileUrl,content,callb) {
+saveFile = function(fileUrl,content,callb) {
 	//alert("whee! about to save to " + fileUrl);
 	var localCallback = function(status,params,responseText,url,xhr) {
 		//alert("whee! got callback status " + status + ", params " + params + ", url " + url + ", xhr " + xhr);
-	}
+	};
 	return doHttp('PUT',fileUrl,content,"tahoe tiddly type",null,null,localCallback);
-}
+};
 
 } //# end of "install only once"
 //}}}
