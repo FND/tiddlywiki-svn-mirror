7a636640bf72bb0a43d72c973fc38889daf4dc95
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-24 16:51:43 +0000

    ccTiddly - adding freds autosave plugin- importing working too
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7916 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/ccTiddlyPlugins.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/ccTiddlyPlugins.js
@@ -169,6 +169,7 @@ config.extensions.ServerSideSavingPlugin = {
 	adaptor: config.adaptors.cctiddly,
 
 	saveTiddler: function(tiddler) {
+		displayMessage("acW RUSSLER ");
 		var adaptor = new this.adaptor();
 		context = {
 			tiddler: tiddler,
@@ -204,6 +205,39 @@ saveChanges = function(onlyIfDirty, tiddlers) {
 
 } //# end of "install only once"
 //}}}
+
+
+// Import Override - ensures imported tiddlers have cctiddly server type. 
+config.macros.importTiddlers.onGetTiddler = function(context,wizard)
+{
+	if(!context.status)
+		displayMessage("Error in importTiddlers.onGetTiddler: " + context.statusText);
+	var tiddler = context.tiddler;
+	store.suspendNotifications();
+	tiddler.fields['server.type'] = 'cctiddly';
+	tiddler.fields['server.host'] = window.url;
+	tiddler.fields['workspace']= window.workspace;
+	store.saveTiddler(tiddler.title, tiddler.title, tiddler.text, tiddler.modifier, tiddler.modified, tiddler.tags, tiddler.fields, true, tiddler.created);// local 
+	config.extensions.ServerSideSavingPlugin.saveTiddler(tiddler); // remote save. 
+	if(!wizard.getValue("sync")) {
+		store.setValue(tiddler.title,'server',null);
+	}
+	store.resumeNotifications();
+	if(!context.isSynchronous)
+		store.notify(tiddler.title,true);
+	var remainingImports = wizard.getValue("remainingImports")-1;
+	wizard.setValue("remainingImports",remainingImports);
+	if(remainingImports == 0) {
+		if(context.isSynchronous) {
+			store.notifyAll();
+			refreshDisplay();
+		}
+		wizard.setButtons([
+				{caption: config.macros.importTiddlers.doneLabel, tooltip: config.macros.importTiddlers.donePrompt, onClick: config.macros.importTiddlers.onClose}
+			],config.macros.importTiddlers.statusDoneImport);
+		autoSaveChanges();
+	}
+};
 
 
 
