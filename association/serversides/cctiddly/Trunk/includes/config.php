d2de9d737a7f517146bac25f43ea648d83556ee6
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-26 16:57:50 +0000

    ccTiddly - adding a theme
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7966 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/includes/config.php
+++ association/serversides/cctiddly/Trunk/includes/config.php
@@ -61,6 +61,7 @@ $tiddlyCfg['pref']['offline']['js'] = array("plugins/ccTiddlyAdaptor");
 
 $tiddlyCfg['allowed_proxy_list'] = 
 array(	'wikipedia.org', 
+		'aiddlywiki.sourceforge.net',
 		'tiddlywiki.org',
 		'nathanbowers.com',
 		'tiddlyfolio.tiddlyspot.com',
