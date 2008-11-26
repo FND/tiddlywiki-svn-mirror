a601c837cc105f2860c02009a6c3fc7a663a041c
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-26 17:57:25 +0000

    Initial submission
    git-svn-id: http://svn.tiddlywiki.org/Trunk@7969 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/CHDK/settings.js
@@ -0,0 +1,29 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.4.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = 'backup';
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+config.options.txtTheme = 'MediaWikiTheme';
+
+config.maxTiddlerImportCount = 500;
+
+merge(config.defaultCustomFields,{
+	'server.host':'chdk.wikia.com',
+	wikiformat:'mediawiki'
+});
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+config.options.chkUseHostImages = true;
+
+//#config.displayInstrumentation = true;
+/*}}}*/
