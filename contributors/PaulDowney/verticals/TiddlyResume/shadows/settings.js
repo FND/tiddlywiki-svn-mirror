edf5778bfba8d78f894622a24ce10252edc21efa
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-29 17:18:27 +0000

    TiddlyResume - first commit, work in progress
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8017 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/verticals/TiddlyResume/shadows/settings.js
@@ -0,0 +1,21 @@
+//{{{
+
+/*
+ *  Theme
+ */
+config.options.txtTheme = "DefaultTheme";
+
+/*
+ *  Groups of tiddlers
+ */
+/*
+var educationGroup = new TiddlerDisplayGroup();
+educationGroup.setGroupField('section');
+educationGroup.setPattern([
+        {label:'heading', tag:'heading', count:1, require:null, openAt:null},
+        {label:'education', tag:'education', count:0, require:'heading', openAt:null}
+]);
+*/
+
+//}}}
+
