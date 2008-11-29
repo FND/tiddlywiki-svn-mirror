2077ff940ebeeee9ca7d8d67f5c7a1c775265a1f
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-29 13:37:38 +0000

    LinkMaker: added support for Microformats - rel and classname for photos
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8016 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PhilHawksworth/plugins/LinkMakerPlugin.js
+++ contributors/PhilHawksworth/plugins/LinkMakerPlugin.js
@@ -3,16 +3,15 @@
 |''Description:''|Create a link from a Template, accessing a tiddlers properties|
 |''Author:''|PhilHawksworth|
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PhilHawksworth/plugins/LinkMakerPlugin.js |
-|''Version:''|0.0.3|
+|''Version:''|0.1|
 |''Date:''|Feb 29, 2008|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]] |
 |''~CoreVersion:''|2.3|
 
-
 Usage:
 
-<<LinkMaker type:'text|image|img' display:'tiddler field|image path' linkto:'href|TiddlerTitle' [classname:'classname'] [tooltip:'tooltip']>>
+<<LinkMaker type:'text|image|img' display:'tiddler field|image path' linkto:'href|TiddlerTitle' [classname:'classname'] [tooltip:'tooltip'] [rel:'relvalue']>>
 
 ***/
 
@@ -25,10 +24,12 @@ version.extensions.LinkMakerPlugin = {installed:true};
 		
 		params = paramString.parseParams("anon",null,true,false,false);
 		var linktype = getParam(params,"type",'text');
-		var linkto = store.getValue(tiddler,getParam(params,"linkto",null));
 		var display =  store.getValue(tiddler,getParam(params,"display",""));
-		var classname = getParam(params,"classname",null);
+		var classname = getParam(params,'classname',null);
 		var dateformat = getParam(params,'dateformat',null);
+		var rel = getParam(params,'rel',null);
+		var linkto = getParam(params,"linkto",null);
+		linkto = linkto ? store.getValue(tiddler, linkto) : null;
 		var tip = getParam(params,'tooltip',null);
 		var tooltip = tip ? store.getValue(tiddler, tip) : null;
 
@@ -36,29 +37,41 @@ version.extensions.LinkMakerPlugin = {installed:true};
 			var handler = config.macros.view.views['date'];
 			var fakeParams = [null,null,dateformat];
 		}
+
+		function createAnchor() {
+			var a = createTiddlyElement(place,'a',null,null);
+			a.setAttribute('href',linkto);
+			a.setAttribute('target',"_blank");
+			if(classname){
+				a.setAttribute('class',classname);
+			}
+			if(tooltip){
+				a.setAttribute('title',tooltip);				
+			}
+			if(rel){
+				a.setAttribute('rel',rel);				
+			}
+			return a;
+		}
 		
-		if(linktype == 'image' || linktype ==  'img') {
-			
+		if(linktype == 'image' || linktype == 'img') {
+			var a = place;
 			if(linkto) {
-				var a = createTiddlyElement(place,'a',null,null);
-				a.setAttribute('href',linkto);
-				a.setAttribute('target',"_blank");
-				var i = createTiddlyElement(a,'img');
-				i.setAttribute('src',display);
+				a = createAnchor();
+			}
+			var i = createTiddlyElement(a,'img');
+			i.setAttribute('src',display);
+			if(classname){
+				i.setAttribute('class',classname);
 			}
 		}
 		
 		else if (linkto && config.formatterHelpers.isExternalLink(linkto)) {
-			var a = createTiddlyElement(place,'a',null,classname,null);
+			var a = createAnchor();
 			if (handler)
 				handler(display,a,fakeParams);
 			else 
 				createTiddlyText(a,display);
-			a.setAttribute('href',linkto);
-			a.setAttribute('target',"_blank");
-			if(tooltip){
-				a.setAttribute('title', tooltip);				
-			}
 		}
 		else if (linkto) {			
 			var e = createTiddlyLink(place,linkto,false,classname);
