157cffb1cbb9f800c7ad0470f0b79d0ab7daf6fe
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-01 17:55:57 +0000

    ServerSideSavingPlugin: corrected handling of bags/recipes
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8042 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/ServerSideSavingPlugin.js
+++ association/plugins/ServerSideSavingPlugin.js
@@ -2,7 +2,7 @@
 |''Name''|ServerSideSavingPlugin|
 |''Description''|server-side saving|
 |''Author''|FND|
-|''Version''|0.2.1|
+|''Version''|0.2.3|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
@@ -46,9 +46,14 @@ plugin = {
 		var adaptor = new this.adaptor();
 		context = {
 			tiddler: tiddler,
-			changecount: tiddler.fields.changecount,
-			workspace: tiddler.fields["server.workspace"] // XXX: bag?
+			changecount: tiddler.fields.changecount
 		};
+		var bag = tiddler.fields["server.bag"];
+		if(bag) {
+			context.bag = bag;
+		} else {
+			context.workspace = tiddler.fields["server.workspace"];
+		}
 		var req = adaptor.putTiddler(tiddler, context, {}, this.saveTiddlerCallback);
 		return req ? tiddler : false;
 	},
@@ -63,7 +68,7 @@ plugin = {
 			}
 			displayMessage(plugin.locale.saved.format([tiddler.title]));
 			store.setDirty(false);
-		} else { // TODO: handle 412 etc.
+		} else {
 			displayMessage(plugin.locale.saveError.format([tiddler.title, context.statusText]));
 		}
 	},
@@ -71,9 +76,14 @@ plugin = {
 	removeTiddler: function(tiddler) {
 		var adaptor = new this.adaptor();
 		context = {
-			tiddler: tiddler,
-			workspace: tiddler.fields["server.workspace"] // XXX: bag?
+			tiddler: tiddler
 		};
+		var bag = tiddler.fields["server.bag"];
+		if(bag) {
+			context.bag = bag;
+		} else {
+			context.workspace = tiddler.fields["server.workspace"];
+		}
 		var req = adaptor.deleteTiddler(tiddler, context, {}, this.removeTiddlerCallback);
 		return req ? tiddler : false;
 	},
@@ -88,7 +98,7 @@ plugin = {
 			}
 			displayMessage(plugin.locale.deleted.format([tiddler.title]));
 			store.setDirty(false);
-		} else { // TODO: handle 412 etc.
+		} else {
 			displayMessage(plugin.locale.deleteLocalError.format([tiddler.title, context.statusText]));
 		}
 	}
