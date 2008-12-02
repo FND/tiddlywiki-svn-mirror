64f94d4914e199a5f578c19a664f356bd8954120
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-02 17:13:12 +0000

    ccTiddly - rss works nicely'
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8068 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/RSS/files/rss.php
+++ association/serversides/cctiddly/Trunk/plugins/RSS/files/rss.php
@@ -1,5 +1,6 @@
 <?php
 sendHeader(200);
+header('Content-type: application/xml; charset="utf-8"',true);
 include_once($cct_base."includes/header.php");
 //force anonymous due to security concerns
 $user['verified']=0;
@@ -7,19 +8,18 @@ $tiddlyCfg['workspace_name'] = '';
 
 $data = db_tiddlers_mainSelectSiteConfig();
 $tmp=array();
-while( $d=db_fetch_assoc($data) ) {
-	$tmp[$d['title']] = $d;
+while( $d=db_fetch_assoc($data) ) {	$tmp[$d['title']] = $d;
 }
 $data = $tmp;
-
 //$siteUrl = isset($data['SiteUrl'])?htmlspecialchars($data['SiteUrl']['body']):"http://www.tiddlywiki.com/";
-$siteUrl = dirname(dirname(dirname(getUrl())))."/".$tiddlyCfg['workspace_name'];
-$result = '<?xml version="1.0"?><rss version="2.0"><channel>
+$siteUrl ="http://".$_SERVER['SERVER_NAME'];
+$result = '<?xml version="1.0"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel>
+<atom:link href="http://latest.simonmcmanus.com/index.xml" rel="self" type="application/rss+xml" />
 <title>'.(isset($data['SiteTitle'])?htmlspecialchars($data['SiteTitle']['body']):"My TiddlyWiki").'</title>
-<link>'.$siteUrl.'</link>
+<link>http://simonmcmanus.com</link>
 <description>'.(isset($data['SiteSubtitle'])?htmlspecialchars($data['SiteSubtitle']['body']):"a reusable non-linear personal web notebook").'</description>
-<pubDate>'.gmdate("D, j M Y h:i:s e").'</pubDate>
-<lastBuildDate>'.gmdate("D, j M Y h:i:s e").'</lastBuildDate>
+<pubDate>'.date("r").'</pubDate>
+<lastBuildDate>'.date("r").'</lastBuildDate>
 <generator>ccTiddly '.$tiddlyCfg['version'].'</generator>';
 
 //get required data from database
@@ -32,15 +32,16 @@ while( $d=db_fetch_assoc($data) )
 	{
 		$result .= '<item>
 		<title>'.htmlspecialchars($d['title']).'</title>
-		<description>'.htmlspecialchars($d['body']).'</description>';
+		<description><![CDATA['.htmlspecialchars($d['body']).']]></description>';
 		$tags = tiddler_breakTag($d['tags']);
 		foreach( $tags as $t ) {
 			$result .= '
 			<category>'.$t.'</category>';
 		}
 		$result .= '
-		<link>'.$siteUrl.'#'.htmlspecialchars($d['title']).'</link>
-		<pubDate>'.gmdate("D, j M Y h:i:s",TiddlyTimeToEpoch($d['modified'])).'</pubDate>
+		<link>'.$siteUrl.'#</link> 
+		<pubDate>'.date("r",TiddlyTimeToEpoch($d['modified'])).'</pubDate>
+		<guid isPermaLink="false">'.$siteUrl.rand().'</guid>
 		</item>';
 		$count++;
 	}
