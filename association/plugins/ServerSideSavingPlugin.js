7216661060f27e1e2bcf9ed13c10ae3339d1e055
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-03 16:41:59 +0000

    ServerSideSavingPlugin: overriding Story.prototype.saveTiddler to fix core bug
    This bug is scheduled to be fixed in TiddlyWiki release 2.4.2.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8090 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/ServerSideSavingPlugin.js
+++ association/plugins/ServerSideSavingPlugin.js
@@ -2,7 +2,7 @@
 |''Name''|ServerSideSavingPlugin|
 |''Description''|server-side saving|
 |''Author''|FND|
-|''Version''|0.3.0|
+|''Version''|0.3.1|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
@@ -153,6 +153,43 @@ TiddlyWiki.prototype.removeTiddler = function(title) { // XXX: should override d
 };
 
 })(config.extensions.ServerSideSavingPlugin); //# end of alias
+
+// override saveTiddler to fix core bug -- XXX: to be fixed in TiddlyWiki v2.4.2
+Story.prototype.saveTiddler = function(title,minorUpdate)
+{
+	var tiddlerElem = this.getTiddler(title);
+	if(tiddlerElem) {
+		var fields = {};
+		this.gatherSaveFields(tiddlerElem,fields);
+		var newTitle = fields.title || title;
+		if(!store.tiddlerExists(newTitle))
+			newTitle = newTitle.trim();
+		if(store.tiddlerExists(newTitle) && newTitle != title) {
+			if(!confirm(config.messages.overwriteWarning.format([newTitle.toString()])))
+				return null;
+		}
+		if(newTitle != title)
+			this.closeTiddler(newTitle,false);
+		tiddlerElem.id = this.tiddlerId(newTitle);
+		tiddlerElem.setAttribute("tiddler",newTitle);
+		tiddlerElem.setAttribute("template",DEFAULT_VIEW_TEMPLATE);
+		tiddlerElem.setAttribute("dirty","false");
+		if(config.options.chkForceMinorUpdate)
+			minorUpdate = !minorUpdate;
+		if(!store.tiddlerExists(newTitle))
+			minorUpdate = false;
+		var newDate = new Date();
+		var extendedFields = store.tiddlerExists(newTitle) ? store.fetchTiddler(newTitle).fields : (newTitle!=title && store.tiddlerExists(title) ? store.fetchTiddler(title).fields : merge({},config.defaultCustomFields));
+		for(var n in fields) {
+			if(!TiddlyWiki.isStandardField(n))
+				extendedFields[n] = fields[n];
+		}
+		var tiddler = store.saveTiddler(title,newTitle,fields.text,minorUpdate ? undefined : config.options.txtUserName,minorUpdate ? undefined : newDate,fields.tags,extendedFields);
+		autoSaveChanges(null,[tiddler]);
+		return newTitle;
+	}
+	return null;
+};
 
 } //# end of "install only once"
 //}}}
