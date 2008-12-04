a4193fbcf69fe623c276a15dce6f3324aface922
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-04 15:01:22 +0000

    ParagraphFormatter: various enhancements, usage examples
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8111 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/formatters/ParagraphFormatter.js
+++ contributors/FND/formatters/ParagraphFormatter.js
@@ -2,7 +2,7 @@
 |''Name''|ParagraphFormatter|
 |''Description''|formatter modifying TiddlyWiki's handling of line breaks|
 |''Author:''|FND|
-|''Version''|0.1.0|
+|''Version''|0.1.1|
 |''Status''|@@beta@@|
 |''Source''|http://devpad.tiddlyspot.com/#ParagraphFormatter|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
@@ -10,28 +10,44 @@
 !Description
 This formatter modifies the way line breaks in wiki markup are rendered;
 single line breaks are ignored, two line breaks result in a blank line being inserted.
+!Usage
+!!Examples
+{{{
+lorem
+ipsum
+
+dolor
+sit
+amet
+}}}
+<<<
+lorem
+ipsum
+
+dolor
+sit
+amet
+<<<
 !Revision History
 !!v0.1 (2008-12-03)
 * initial release
 !Code
 ***/
 //{{{
-(function() {
-    // modify line-break formatter
-    for(var i = 0; i < config.formatters.length; i++) {
-        var obj = config.formatters[i];
-        if(obj.name == "lineBreak") {
-            obj.match = "<br ?/?>";
-        }
-    }
-    // create paragraph formatter
-    config.formatters.push({
-        name: "paragraph",
-        match: "\\n\\n",
-        handler: function(w) {
-            createTiddlyElement(w.output, "br");
-            createTiddlyElement(w.output, "br");
-        }
-    });
-})();
+(function(formatters) { //# set up alias
+
+// modify line-break formatter
+var lineBreak = formatters[formatters.findByField("name", "lineBreak")];
+lineBreak.match = "<br ?/?>";
+// create paragraph formatter
+formatters.push({
+	name: "paragraph",
+	match: "\\n\\n",
+	handler: function(w) {
+		createTiddlyElement(w.output, "br");
+		createTiddlyElement(w.output, "br");
+	}
+});
+
+})(config.formatters); //# end of alias
 //}}}
