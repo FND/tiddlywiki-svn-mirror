2f72d045f9d6d1b13eec6d0fe806a00d2942681c
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-04 17:45:07 +0000

    ccTiddly - skinny tiddlers working with systemConfig tiddlers
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8116 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/files/removeBody.php
+++ association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/files/removeBody.php
@@ -1,15 +1,17 @@
 <?php
 $autoLoad = array("skinnyPlugin", "SiteTitle", "SiteSubtitle", "MainMenu", "002_ccTiddlyPlugins", "ColorPalette", "GettingStarted"); // set the tiddlers which will not be lazily loaded
 $defaultTiddler = ($user['verified']) ? "DefaultTiddlers" : "AnonDefaultTiddlers"; // if the user is logged in get default tiddlers, if the are not logged in get the anonDefaultTiddler
+
 $defaultTiddlers = getTiddler('AnonDefaultTiddlers', $tiddlyCfg['workspace_name']); // fetch the default tiddlers from the db
 foreach(explode(" ", $defaultTiddlers['body']) as $tiddler)
 	$defaults[] = str_replace("]]", "", str_replace("[[", "", $tiddler));	 // for each default tiddler add them to the autoLoad array. 
 
+$systemConfigTiddlers =  getTiddlersWithTags(array('systemConfig'), array());
+foreach($systemConfigTiddlers as $sysTiddler)
+	$defaults[] = $sysTiddler['title'];
 
-//$systemConfigTiddlers =  getTiddlersWithTags(array('systemConfig'), array());
-
-
-array_merge($defaults, $autoLoad);
+$autoLoad = array_merge($systemConfigTiddlers, $autoLoad);
+$autoLoad = array_merge($defaults, $autoLoad);
 
 if(!in_array($t['title'], $autoLoad)) // remove the body of all tiddlers which should be skinily loaded. 
 	$t['body'] = "";
