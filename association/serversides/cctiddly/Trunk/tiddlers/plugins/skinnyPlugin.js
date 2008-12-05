aa8172be7ad03ed3ed4bf45312d299fca9464209
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-05 10:46:37 +0000

    skinnyPlugin: using custom loadMissingTiddlerContents instead of overriding loadMissingTiddler
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8118 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/skinnyPlugin.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/skinnyPlugin.js
@@ -1,86 +1,65 @@
-if(!config.extensions) { config.extensions = {}; } //# obsolete from v2.4.2
+(function() {
 
-config.extensions.lazyLoading = {};
-
-(function(plugin) { //# set up alias
-
-config.defaultCustomFields = {
-	"server.host": "http://127.0.0.1/",	
-	"server.type": "cctiddly",
-	"server.workspace": "fredtest"  
-};
-	
 // hijack loadMissingTiddler to trigger lazy loading
 plugin.loadMissingTiddler = Story.prototype.loadMissingTiddler;
 Story.prototype.loadMissingTiddler = function(title, fields, tiddlerElem) {
-	var t = store.getTiddler(title);
-	if(t && t.text) {
-		return true; // XXX: ??
+	var tiddler = store.getTiddler(title);
+	if(tiddler && tiddler.text) {
+		return true;
+	} else if(tiddler) {
+		console.log("loading missing contents: " + title); // DEBUG
+		return plugin.loadMissingTiddlerContents(tiddler);
 	} else {
-		console.log("loading missing: " + title);
-		//store.removeTiddler(t.title); // XXX: wrong
+		console.log("loading missing tiddler: " + title); // DEBUG
 		return plugin.loadMissingTiddler.apply(this, arguments);
 	}
 };
 
-Story.prototype.createTiddler = function(place, before, title, template, customFields) {
-	var tiddlerElem = createTiddlyElement(null,"div",this.tiddlerId(title),"tiddler");
-	tiddlerElem.setAttribute("refresh","tiddler");
-	if(customFields)
-		tiddlerElem.setAttribute("tiddlyFields",customFields);
-	place.insertBefore(tiddlerElem,before);
-	var defaultText = null;
-	var tiddler = store.getTiddler(title);
-	console.log(tiddler, tiddler ? tiddler.text : "missing: " + title, tiddler ? (tiddler.text || true) : "skinny");
-	if((!store.tiddlerExists(title) && !store.isShadowTiddler(title)) || (tiddler && tiddler.text === ""))
-		defaultText = this.loadMissingTiddler(title,customFields,tiddlerElem);
-	this.refreshTiddler(title,template,false,customFields,defaultText);
-	return tiddlerElem;
-};
-
-Story.prototype.loadMissingTiddler = function(title,fields,tiddlerElem)
-{
-	var tiddler = new Tiddler(title);
-	tiddler.fields = typeof fields == "string" ? fields.decodeHashMap() : (fields || config.defaultCustomFields);
+Story.prototype.loadMissingTiddlerContents = function(tiddler) {
 	var serverType = tiddler.getServerType();
-	var host = tiddler.fields['server.host'];
-	var workspace = tiddler.fields['server.workspace'];
+	var host = tiddler.fields["server.host"];
+	var workspace = tiddler.fields["server.workspace"]; // XXX: bag?
 	if(!serverType || !host) {
-		console.log("ignoring " + title, tiddler, serverType, host, fields);
+		console.log("ignoring " + title);
 		return null;
 	}
-	var sm = new SyncMachine(serverType,{
-			start: function() {
-				return this.openHost(host,"openWorkspace");
-			},
-			openWorkspace: function() {
-				return this.openWorkspace(workspace,"getTiddler");
-			},
-			getTiddler: function() {
-				return this.getTiddler(title,"onGetTiddler");
-			},
-			onGetTiddler: function(context) {
-				var tiddler = context.tiddler;
-				console.log(tiddler);
+	var sm = new SyncMachine(serverType, {
+		start: function() {
+			return this.openHost(host,"openWorkspace");
+		},
 
-				if(tiddler && tiddler.text) {
-					var downloaded = new Date();
-					if(!tiddler.created)
-						tiddler.created = downloaded;
-					if(!tiddler.modified)
-						tiddler.modified = tiddler.created;
-					store.saveTiddler(tiddler.title,tiddler.title,tiddler.text,tiddler.modifier,tiddler.modified,tiddler.tags,tiddler.fields,true,tiddler.created);
-					autoSaveChanges();
+		openWorkspace: function() {
+			return this.openWorkspace(workspace,"getTiddler");
+		},
+
+		getTiddler: function() {
+			return this.getTiddler(title,"onGetTiddler");
+		},
+
+		onGetTiddler: function(context) {
+			var tiddler = context.tiddler;
+			if(tiddler && tiddler.text) {
+				if(!tiddler.created) {
+					tiddler.created = new Date();
 				}
-				delete this;
-				return true;
-			},
-			error: function(message) {
-				displayMessage("Error loading missing tiddler from %0: %1".format([host,message]));
+				if(!tiddler.modified) {
+					tiddler.modified = tiddler.created;
+				}
+				store.saveTiddler(tiddler.title, tiddler.title,
+					tiddler.text, tiddler.modifier, tiddler.modified,
+					tiddler.tags, tiddler.fields, true, tiddler.created);
+				autoSaveChanges();
 			}
-		});
+			delete this;
+			return true;
+		},
+
+		error: function(message) {
+			displayMessage("Error loading missing tiddler contents from %0: %1".format([host, message]));
+		}
+	});
 	sm.go();
-	return config.messages.loadingMissingTiddler.format([title,serverType,host,workspace]);
+	return config.messages.loadingMissingTiddler.format([title, serverType, host,workspace]);
 };
 
-})(config.extensions.lazyLoading); //# end of alias
+})(); //# end of alias
