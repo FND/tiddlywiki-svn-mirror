91975403a5ddb97e2f845bdeb274e78549dbcf7c
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-08 20:40:45 +0000

    ccTiddly - portlet: nolonger dislays comlpleted tiddlers
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8166 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/RSS/files/portlet.php
+++ association/serversides/cctiddly/Trunk/plugins/RSS/files/portlet.php
@@ -2,11 +2,16 @@
 $cct_base = "../../../";
 include_once($cct_base."includes/header.php");
 $tasks =  getTiddlersWithTags(array('task'), array());
-
-echo "<h1>".count($tasks)." Tasks </h1>";
-
 foreach($tasks as $task)
-	echo "<a href='".dirname(dirname(dirname(getURL())))."/".$tiddlyCfg['workspace_name']."#[[".$task['title']."]]' target=".rand().">".$task['title']."</a><br />";
+{
+	if(!stristr($task['fields'], "tt_status='Complete"))
+	{
+		$links .= "<a href='".dirname(dirname(dirname(getURL())))."/".$tiddlyCfg['workspace_name']."#[[".$task['title']."]]' target=".rand().">".$task['title']."</a><br />";
+		$count++;
+	}
+}
 
-
+echo "<h1>".$count." Tasks </h1>";
+echo $links;
+echo "<br /><a href='".dirname(dirname(dirname(getURL())))."/".$tiddlyCfg['workspace_name']."#newTiddler:NewTask'  target=".rand().">new task</a>";
 ?>
