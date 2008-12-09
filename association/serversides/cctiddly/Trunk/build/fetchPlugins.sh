aad0bed659ad9fec77b92cec65fa312111599e3d
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-09 16:21:51 +0000

    ccTiddly - adding script to fetch plugins from svn.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8179 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Trunk/build/fetchPlugins.sh
@@ -0,0 +1,9 @@
+svn export http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js ../tiddlers/importedPlugins/ServerSideSavingPlugin.js
+svn export http://svn.tiddlywiki.org/Trunk/contributors/EricShulman/plugins/TaggedTemplateTweak.js ../tiddlers/importedPlugins/TaggedTemplateTweak.js
+svn export http://svn.tiddlywiki.org/Trunk/contributors/BidiX/plugins/LoadRemoteFileThroughProxy.js ../tiddlers/importedPlugins/LoadRemoteFileThroughProxy.js
+svn export http://svn.tiddlywiki.org/Trunk/contributors/EricShulman/plugins/TaggedTemplateTweak.js ../tiddlers/importedPlugins/TaggedTemplateTweak.js
+
+
+
+
+
