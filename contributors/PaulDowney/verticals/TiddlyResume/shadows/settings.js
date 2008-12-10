5c086cdf31e34a9d832f5c63e3f07e69d4cb0426
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-10 14:51:47 +0000

    TiddlyResume - snapshot, svn unwravelling
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8211 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyResume/shadows/settings.js
+++ contributors/PaulDowney/verticals/TiddlyResume/shadows/settings.js
@@ -8,13 +8,14 @@ config.options.txtTheme = "DefaultTheme";
 /*
  *  Groups of tiddlers
  */
+
 /*
 var educationGroup = new TiddlerDisplayGroup();
-educationGroup.setGroupField('section');
 educationGroup.setPattern([
-        {label:'heading', tag:'heading', count:1, require:null, openAt:null},
-        {label:'education', tag:'education', count:0, require:'heading', openAt:null}
+        {label:'educationsection', tag:'educationSection', count:1, require:null, openAt:null},
+        {label:'education', tag:'education', count:0, require:'educationsection', openAt:'bottom'}
 ]);
+educationGroup.setGroupField('section');
 */
 
 //}}}
