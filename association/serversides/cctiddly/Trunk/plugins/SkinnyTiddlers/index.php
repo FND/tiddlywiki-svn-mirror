3471492894652b4718e97fd342c3159a12770c1e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-10 10:29:44 +0000

    ccTiddly - adding skinny tiddlers
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8202 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/index.php
+++ association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/index.php
@@ -1,16 +1,9 @@
 <?php
-/*
 
 $p = new Plugin('Skinny Tiddlers Plugin','0.1','simonmcmanus and FND');
-
-Loads all tiddlers into tiddlywiki file but does not load the body. 
-
-The body is loaded on request. 
-
 
 $p->addTiddler(null, getcwd()."/plugins/SkinnyTiddlers/files/skinnyPlugin.js");
 $p->addEvent('preOutputTiddler', getcwd()."/plugins/SkinnyTiddlers/files/removeBody.php")
 
-*/
 
 ?>
