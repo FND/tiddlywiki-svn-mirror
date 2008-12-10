2eecc1405550770fb22a9639be2d5c62bdae637c
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-10 10:53:30 +0000

    ccTiddly - removing debugging info from the tiddler_selectTitlte() funciton
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8203 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/includes/tiddler.php
+++ association/serversides/cctiddly/Trunk/includes/tiddler.php
@@ -332,7 +332,6 @@
 			$tiddler['workspace_name'] = $workspace;
 
 		$tiddlers = db_record_select($tiddlyCfg['table']['main'],$tiddler,1);
-		print_r($tiddlers);
 		//grab record and check if title are the same
 		//this is required since mysql is not binary safe unless deliberately configured in table
 		//result would be empty string if not found, array if found
