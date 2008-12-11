34411c3b84e0c442c66850e39ee1e2e7f2fce8be
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-11 22:38:50 +0000

    SegmentedFilterPlugin - first release
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8229 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/SegmentedFilterPlugin/SegmentedFilterPlugin.js
+++ contributors/PaulDowney/plugins/SegmentedFilterPlugin/SegmentedFilterPlugin.js
@@ -2,6 +2,7 @@
 |''Name:''|SegmentedFilterPlugin|
 |''Description:''|Treat a fliter as a series of filters, one per line|
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
+|''Source:''|http://whatfettle.com/2008/07/SegmentedFilterPlugin |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/SegmentedFilterPlugin |
 |''Version:''|0.1|
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
@@ -9,19 +10,20 @@
 |''~CoreVersion:''|2.4|
 |''Overrides''|TiddlyWiki.prototype.filterTiddlers() |
 !!Documentation
+Useful for separating a filter, such as the MainMenu and DefaultTiddlers, into a series of filters which may include sort and limit clauses.
 !!Code
 ***/
 //{{{
 if(!version.extensions.SegmentedFilterPlugin) {
 version.extensions.SegmentedFilterPlugin = {installed:true};
 
-Story.prototype._segmentedFilter_filterTiddlers = Story.prototype.filterTiddlers;
-Story.prototype.filterTiddlers = function(filter)
+TiddlyWiki.prototype._segmentedFilter_filterTiddlers = TiddlyWiki.prototype.filterTiddlers;
+TiddlyWiki.prototype.filterTiddlers = function(filter)
 {
 	var lines = filter.split("\n");
 	var tiddlers = [];
         for(var t=0; t<lines.length; t++){
-                tiddlers.push(this._segmentedFilter_filterTiddlers.apply(this,arguments));
+                tiddlers = tiddlers.concat(this._segmentedFilter_filterTiddlers(lines[t]));
 	}
 	return tiddlers;
 };
