960f87aabddf4e9ef7960ad15afd8a2acf9ab471
EricShulman <EricShulman@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-18 18:42:50 +0000

    v1.5.0 added handling for using tiddler title as prefix (e.g., SomeTiddlerViewTemplate).  Applied as a fallback only if no tag-matching templates are found.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8280 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/EricShulman/plugins/TaggedTemplateTweak.js
+++ contributors/EricShulman/plugins/TaggedTemplateTweak.js
@@ -2,7 +2,7 @@
 |Name|TaggedTemplateTweak|
 |Source|http://www.TiddlyTools.com/#TaggedTemplateTweak|
 |Documentation|http://www.TiddlyTools.com/#TaggedTemplateTweakInfo|
-|Version|1.4.1|
+|Version|1.5.0|
 |Author|Eric Shulman - ELS Design Studios|
 |License|http://www.TiddlyTools.com/#LegalStatements <br>and [[Creative Commons Attribution-ShareAlike 2.5 License|http://creativecommons.org/licenses/by-sa/2.5/]]|
 |~CoreVersion|2.1|
@@ -15,14 +15,14 @@ This tweak extends story.chooseTemplateForTiddler() so that ''whenever a tiddler
 >see [[TaggedTemplateTweakInfo]]
 !!!!!Revisions
 <<<
-2008.08.29 [1.4.1] corrected handling for tiddlers with no matching tagged template when non-default theme is in effect (e.g., use "MyTheme##ViewTemplate").
+2008.12.18 [1.5.0] added handling for using tiddler //title// as prefix (e.g., {{{SomeTiddlerViewTemplate}}}) 
 | please see [[TaggedTemplateTweakInfo]] for previous revision details |
 2007.06.11 [1.0.0] initial release
 <<<
 !!!!!Code
 ***/
 //{{{
-version.extensions.TaggedTemplateTweak= {major: 1, minor: 4, revision: 1, date: new Date(2008,8,29)};
+version.extensions.TaggedTemplateTweak= {major: 1, minor: 5, revision: 0, date: new Date(2008,12,18)};
 
 Story.prototype.taggedTemplate_chooseTemplateForTiddler = Story.prototype.chooseTemplateForTiddler
 Story.prototype.chooseTemplateForTiddler = function(title,template)
@@ -30,10 +30,8 @@ Story.prototype.chooseTemplateForTiddler = function(title,template)
 	// get default template from core
 	var coreTemplate=this.taggedTemplate_chooseTemplateForTiddler.apply(this,arguments);
 
-	// if the tiddler doesn't exist yet or is untagged, return core result
-	var tiddler=store.getTiddler(title);
-	if (!tiddler || !tiddler.tags.length)
-		return coreTemplate;
+	// if the tiddler doesn't exist yet, return core result
+	var tiddler=store.getTiddler(title); if (!tiddler) return coreTemplate;
 
 	// split core template into theme prefix and template name
 	var theme="";
@@ -43,15 +41,22 @@ Story.prototype.chooseTemplateForTiddler = function(title,template)
 	else theme=config.options.txtTheme||""; // fallback if theme is not specified
 	theme+=config.textPrimitives.sectionSeparator;
 
-	// look for template whose prefix matches a tag on this tiddler (if any)
+	// look for template whose prefix matches a *tag* on this tiddler (if any)
 	for (i=0; i<tiddler.tags.length; i++) {
 		var t=tiddler.tags[i]+template; // add tag prefix to template
 		var c=t.substr(0,1).toUpperCase()+t.substr(1); // capitalized for WikiWord title
 		if (store.getTiddlerText(theme+t))	{ return theme+t; } // theme##tagTemplate
 		if (store.getTiddlerText(theme+c))	{ return theme+c; } // theme##TagTemplate
-		if (store.getTiddlerText(t)) 		{ return t; }	     // tagTemplate
-		if (store.getTiddlerText(c))		{ return c; }	     // TagTemplate
+		if (store.getTiddlerText(t)) 		{ return t; }	    // tagTemplate
+		if (store.getTiddlerText(c))		{ return c; }	    // TagTemplate
 	}
-	return coreTemplate; // no matching tag, return core result
+
+	// if no tagged template found, and tiddler title is not itself a tag,
+	// then look for the template whose prefix matches the *title* of this tiddler
+	var isTag=store.getTaggedTiddlers(title).length;
+	if (!isTag && store.getTiddlerText(title+template)) { return title+template; }
+
+	// no matching tag OR title prefix... return core result
+	return coreTemplate;
 }
 //}}}
