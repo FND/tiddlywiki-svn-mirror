249dc58e7e4eb43df12670cf963f5b0485e52405
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-19 17:31:46 +0000

    TiddlerTable macro - uses eval() to create functions prior to rendering, instead of running an eval() (performance drain) each time a render occurs
    Note that TiddlerTable should eventually be merged into ListView
    
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8289 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/plugins/TiddlerTableMacro/TiddlerTableMacro.js
+++ contributors/MichaelMahemoff/plugins/TiddlerTableMacro/TiddlerTableMacro.js
@@ -7,25 +7,26 @@ if(!version.extensions.TiddlerTableMacro) {
 
     handler: function(place,macroName,params,wikifier,paramString,tiddler) {
       var macroParams = paramString.parseParams();
-      var filter =  (getParam(macroParams, "filter")) ?  getParam(macroParams, "filter") : "";
-      var tiddlers = store.filterTiddlers(filter);
+      var tiddlers =  store.filterTiddlers(getParam(macroParams, "filter"));
+      console.log(tiddlers);
       var table = createTiddlyElement(place, "table", null, "tiddlerTable");
       
       /* Process Defaults - headings */
       headings = ["Tiddler", "Summary"];
       if (getParam(macroParams, "headings")) headings = getParam(macroParams, "headings").split(",");
       var row = createTiddlyElement(table, "tr");
-
-      /* Process Defaults - summaries */
-      var summaryFuncs = [ "tiddler.title", "tiddler.text.substr(0,50)"];
-      for (var colCount=0; colCount<headings.length; colCount++) {
-        var summaryFunc = (getParam(macroParams, "col"+(colCount+1)));
-        if (summaryFunc) summaryFuncs[colCount] = summaryFunc;
-      }
-
       /* Show Headings */
       for (var colCount=0; colCount<headings.length; colCount++) {
         createTiddlyElement(row, "th", null, null, headings[colCount]);
+      }
+
+      /* Process Defaults - summaries */
+      var defaultSummaryFuncs = [ "'[['+tiddler.title+']]'", "wikifyPlainText(tiddler.text,50) + '...'"];
+      window.summary = {};
+      for (var colCount=0; colCount<headings.length; colCount++) {
+        var summaryFunc = (getParam(macroParams, "col"+(colCount+1))) || defaultSummaryFuncs[colCount];
+        log("thefunc", colCount, summaryFunc);
+        eval("window.summary["+colCount+"] = function(tiddler) { return "+summaryFunc+"; }");
       }
 
       /* Show Rows */
@@ -33,13 +34,14 @@ if(!version.extensions.TiddlerTableMacro) {
         row = createTiddlyElement(table, "tr");
         var tiddler = tiddlers[rowCount];
         for (var colCount=0; colCount<headings.length; colCount++) {
-          // createTiddlyElement(row, "td", null, null, eval(summaryFuncs[colCount]));
           var cell = createTiddlyElement(row, "td", null, null);
-          wikify(eval(summaryFuncs[colCount])+"", cell);
+          log("boom", cell, tiddler, window.summary[colCount]+"");
+          // cell.innerHTML = wikify(window.summary[colCount](tiddler)+"", cell);
+          wikify(window.summary[colCount](tiddler)+"", cell);
         }
       }
     }
   } // end macro
 } // end of 'install only once'
-// function log() { if (console) console.log.apply(console, arguments); }
+function log() { if (console) console.log.apply(console, arguments); }
 /*}}}*/
