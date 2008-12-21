ec21059b6ae452688b7b1a962f9b4811bba2999f
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-21 10:37:01 +0000

    FadingMessagesPlugin - added some pumped messages for the demo, ready to ship
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8305 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/FadingMessagesPlugin/tiddlers/PumpMessages.js
@@ -0,0 +1,26 @@
+//{{{
+if(!version.extensions.PumpMessagesPlugin) {
+version.extensions.PumpMessagesPlugin = {installed:true};
+
+pumpMessages = function(q){
+	displayMessage(q.shift());
+	if(q.length){
+		setTimeout(function(){pumpMessages(q);},900);
+	}
+};
+
+setTimeout(function(){pumpMessages([
+	"This is a demonstration",
+	"of the FadingMessagesPlugin",
+	"in ''action'' ..",
+	"The messages should disappear",
+	"automatically",
+	"Fading away, by themselves",
+	"You can also click on the cross and delete messages,",
+	"if you want",
+	"The length of time beofre the message disappears is an option.",
+	"See the FadingMessagesPlugin tiddler for more details.",
+	]);},3000);
+
+}
+//}}}
