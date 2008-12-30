b57e0ac30eae00a69d94da451ffe8a48134655d5
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-12-30 18:59:49 +0000

    TiddlyResume - canonicalisation of templates - they were getting messy
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8328 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyResume/shadows/settings.js
+++ contributors/PaulDowney/verticals/TiddlyResume/shadows/settings.js
@@ -10,18 +10,5 @@ config.options.txtTheme = "DefaultTheme";
  */
 config.options.txtFadingMessagesTimeout = 1;
 
-/*
- *  Groups of tiddlers
- */
-
-/*
-var educationGroup = new TiddlerDisplayGroup();
-educationGroup.setPattern([
-        {label:'educationsection', tag:'educationSection', count:1, require:null, openAt:null},
-        {label:'education', tag:'education', count:0, require:'educationsection', openAt:'bottom'}
-]);
-educationGroup.setGroupField('section');
-*/
-
 //}}}
 
