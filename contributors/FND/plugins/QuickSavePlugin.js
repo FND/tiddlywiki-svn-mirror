f9d6689200b169b246ce9a050ce5f63d14bd0603
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-10 12:03:19 +0000

    QuickSavePlugin: minor adjustments
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8396 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/QuickSavePlugin.js
+++ contributors/FND/plugins/QuickSavePlugin.js
@@ -2,14 +2,14 @@
 |''Name''|QuickSavePlugin|
 |''Description''|provides command for staying in edit mode when saving|
 |''Author''|FND|
-|''Version''|0.1.0|
+|''Version''|0.1.1|
 |''Status''|@@experimental@@|
 |''Source''|http://devpad.tiddlyspot.com/#QuickSavePlugin|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
 |''Keywords''|editing saving|
 !Usage
-Add {{{quickSaveTiddler}}} [[command|http://www.tiddlywiki.org/wiki/Tiddler_Toolbar]] to [[ToolbarCommands]].
+Add {{{quickSaveTiddler}}} [[command|http://www.tiddlywiki.org/wiki/Tiddler_Toolbar]] to [[ToolbarCommands]]' //EditToolbar// slice.
 !Revision History
 !!v0.1 (2009-01-09)
 * initial release
@@ -27,19 +27,19 @@ config.commands.quickSaveTiddler = {
 	text: "save",
 	tooltip: "Save changes to this tiddler",
 
-	handler: function(event,src,title) {
-		var newTitle = story.saveTiddler(title,event.shiftKey,true);
+	handler: function(event, src, title) {
+		var newTitle = story.saveTiddler(title, event.shiftKey, true);
 		return false;
 	}
 };
 
 // override saveTiddler to make template-switching optional
-Story.prototype.saveTiddler = function(title,minorUpdate,keepView) // TODO: rename template/view argument
+Story.prototype.saveTiddler = function(title, minorUpdate, keepView) // TODO: rename template/view argument
 {
 	var tiddlerElem = this.getTiddler(title);
 	if(tiddlerElem) {
 		var fields = {};
-		this.gatherSaveFields(tiddlerElem,fields);
+		this.gatherSaveFields(tiddlerElem, fields);
 		var newTitle = fields.title || title;
 		if(!store.tiddlerExists(newTitle)) {
 			newTitle = newTitle.trim();
@@ -50,14 +50,14 @@ Story.prototype.saveTiddler = function(title,minorUpdate,keepView) // TODO: rena
 			}
 		}
 		if(newTitle != title) {
-			this.closeTiddler(newTitle,false);
+			this.closeTiddler(newTitle, false);
 		}
 		tiddlerElem.id = this.tiddlerId(newTitle);
-		tiddlerElem.setAttribute("tiddler",newTitle);
+		tiddlerElem.setAttribute("tiddler", newTitle);
 		if(!keepView) {
-			tiddlerElem.setAttribute("template",DEFAULT_VIEW_TEMPLATE);
+			tiddlerElem.setAttribute("template", DEFAULT_VIEW_TEMPLATE);
 		}
-		tiddlerElem.setAttribute("dirty","false");
+		tiddlerElem.setAttribute("dirty", "false");
 		if(config.options.chkForceMinorUpdate) {
 			minorUpdate = !minorUpdate;
 		}
@@ -65,17 +65,17 @@ Story.prototype.saveTiddler = function(title,minorUpdate,keepView) // TODO: rena
 			minorUpdate = false;
 		}
 		var newDate = new Date();
-		var extendedFields = store.tiddlerExists(newTitle) ? store.fetchTiddler(newTitle).fields : (newTitle!=title && store.tiddlerExists(title) ? store.fetchTiddler(title).fields : merge({},config.defaultCustomFields));
+		var extendedFields = store.tiddlerExists(newTitle) ? store.fetchTiddler(newTitle).fields : (newTitle!=title && store.tiddlerExists(title) ? store.fetchTiddler(title).fields : merge({}, config.defaultCustomFields));
 		for(var n in fields) {
 			if(!TiddlyWiki.isStandardField(n)) {
 				extendedFields[n] = fields[n];
 			}
 		}
-		var tiddler = store.saveTiddler(title,newTitle,fields.text,minorUpdate ? undefined : config.options.txtUserName,minorUpdate ? undefined : newDate,fields.tags,extendedFields);
+		var tiddler = store.saveTiddler(title, newTitle, fields.text, minorUpdate ? undefined : config.options.txtUserName, minorUpdate ? undefined : newDate, fields.tags, extendedFields);
 		if(keepView && tiddler && !config.options.chkAutoSave) { // XXX: hacky!?
 			displayMessage(config.messages.draftSaved.format([tiddler.title])); // XXX: "saved" confusing; displayMessage unsuitable for feedback?
 		}
-		autoSaveChanges(null,[tiddler]);
+		autoSaveChanges(null, [tiddler]);
 		return newTitle;
 	}
 	return null;
