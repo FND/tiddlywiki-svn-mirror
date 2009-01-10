b87eeeecad099b4b98e51fb1bdd57eba9320a8f3
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-10 17:07:55 +0000

    unaforms - updates to help doc to note the change in the install process: twanager update now plucks a recipe out of svn.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8397 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
+++ contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
@@ -1,5 +1,9 @@
-# A default empty config, make your changes here.
 config = {
         'system_plugins': ['former'],
         'twanager_plugins': ['former'],
+# point to the recipe that contains the core needs, just for
+# bootstrapping and installation
+        'instance_tiddlers': [
+            'http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/verticals/UnaForms/forms.recipe',
+            ],
 }
