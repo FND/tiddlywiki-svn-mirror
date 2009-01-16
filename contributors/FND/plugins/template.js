a7c543bc8be70aa5c5f70af49d9f70314cbe7821
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-16 14:01:28 +0000

    bugfix: missing semicolon after jQuery inclusion
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8460 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/template.js
+++ contributors/FND/plugins/template.js
@@ -4,10 +4,10 @@
 |''Author''|FND|
 |''Version''|<#.#.#>|
 |''Status''|<//unknown//; @@experimental@@; @@beta@@; //obsolete//; stable>|
-|''Source''|http://devpad.tiddlyspot.com/#<...>|
+|''Source''|http://fnd.tiddlyspot.com/#<...>|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
-|''CoreVersion''|<...>|
+|''CoreVersion''|2.5|
 |''Requires''|<...>|
 |''Keywords''|<...>|
 !Description
@@ -32,7 +32,7 @@
 !Code
 ***/
 //{{{
-(function() { //# set up local scope
+(function($) { //# set up local scope
 
 if(!window.<...>) {
 	throw "Missing dependency: <...>";
@@ -41,9 +41,7 @@ if(!window.<...>) {
 if(!version.extensions.<...>) { //# ensure that the plugin is only installed once
 version.extensions.<...> = { installed: true };
 
-if(!config.extensions) { config.extensions = {}; } //# obsolete from v2.4.2
-
-config.extensions.<...> = {
+$.tw.<...> = {
 	sampleFunction: function() {
 		/* ... */
 	}
@@ -57,5 +55,5 @@ config.macros.<...> = {
 
 } //# end of "install only once"
 
-})(); //# end of local scope
+})(jQuery); //# end of local scope
 //}}}
