265a7b0bff77ea96041879e7092c2eb7febf0dfe
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-19 17:06:47 +0000

    plugin template: updated for TiddlyWiki 2.5's jQuery scheme
    N.B.: This scheme is still preliminary.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8482 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/template.js
+++ contributors/FND/plugins/template.js
@@ -4,10 +4,10 @@
 |''Author''|FND|
 |''Version''|<#.#.#>|
 |''Status''|<//unknown//; @@experimental@@; @@beta@@; //obsolete//; stable>|
-|''Source''|http://devpad.tiddlyspot.com/#<...>|
+|''Source''|http://fnd.tiddlyspot.com/#<...>|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
-|''CoreVersion''|<...>|
+|''CoreVersion''|2.5|
 |''Requires''|<...>|
 |''Keywords''|<...>|
 !Description
@@ -32,30 +32,26 @@
 !Code
 ***/
 //{{{
-(function() { //# set up local scope
+(function($) { //# set up local scope
 
 if(!window.<...>) {
 	throw "Missing dependency: <...>";
 }
 
-if(!version.extensions.<...>) { //# ensure that the plugin is only installed once
-version.extensions.<...> = { installed: true };
-
-if(!config.extensions) { config.extensions = {}; } //# obsolete from v2.4.2
-
-config.extensions.<...> = {
+// plugin namespace
+$.tw.<...> = {
 	sampleFunction: function() {
 		/* ... */
 	}
 };
 
-config.macros.<...> = {
-	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+// macro
+$.tw.fn.extend({
+	<...>: function(args) {
 		/* ... */
+		return this;
 	}
-};
+});
 
-} //# end of "install only once"
-
-})(); //# end of local scope
+})(jQuery); //# end of local scope
 //}}}
