36dc324e7c1791823820c57aba4136978c966857
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-22 17:49:48 +0000

    ccTiddly - tiddlerTree - adding printView macro which produces a standalone HTML version of the document
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8517 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/tiddlertree/files/printTree.js
+++ association/serversides/cctiddly/Trunk/plugins/tiddlertree/files/printTree.js
@@ -1,30 +1,24 @@
-// printTree //
-
 config.macros.printTree={};
 	
 config.macros.printTree.handler=function(place,macroName,params,wikifier,paramString,tiddler){
 
-
-var srcWindow = window.open("","sourceWindow","width=700,height=100,scrollbars=true");
-    var srcDocument = srcWindow.document;
-    // Jam in the text template
-    srcDocument.write("<html><head></head><body><div id='printedVersion'></div></body></html>");
-    srcDocument.close();
-
- var marker = srcDocument.getElementById("printedVersion");
-
-
+var srcWindow = window.open("","sourceWindow","width=700,height=900");
+	var srcDocument = srcWindow.document;
+	// Jam in the text template
+	srcDocument.write("<html><head></head><body><div id='printedVersion'></div></body></html>");
+	srcDocument.close();
+	var marker = srcDocument.getElementById("printedVersion");
 	var treeSpec = store.getTiddlerText(params[0]); 
 	if(treeSpec){
 		var sections = treeSpec.split("\n");
-		var parent = createTiddlyElement(marker, "ul","sortableList", "page-list");
+		var parent = createTiddlyElement(marker, "ul");
 		for(var i = 0; i < sections.length; i++) {
 			var matches = sections[i].match(/^(\*+) (.*)/)
 			if (matches) {
 				var level = matches[1].length;
 				var tiddlerTitle = matches[2];
 				if (level>prevLevel) {
-					parent = createTiddlyElement(parent, "ul","sortableList", "page-list");
+					parent = createTiddlyElement(parent, "ul");
 				} else if (level < prevLevel) {
 					parent = parent.parentNode;
 				}
@@ -36,10 +30,6 @@ var srcWindow = window.open("","sourceWindow","width=700,height=100,scrollbars=t
 	}
 	
 };
-
-config.macros.printTree.strip=function(s) {
-	return s.replace(" ",  "");}
-
 
 function log() { if (console) console.log.apply(console, arguments); };
 
