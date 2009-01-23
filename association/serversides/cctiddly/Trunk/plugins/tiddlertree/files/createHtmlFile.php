0ac48e6a51d23228623053803c6df8ffe997a77d
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-01-23 12:39:05 +0000

    ccTiddly - tiddlerTree - adding createHtmlFile.php
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8521 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Trunk/plugins/tiddlertree/files/createHtmlFile.php
@@ -0,0 +1,15 @@
+<?php
+
+$cct_base = "../../../";
+include_once($cct_base."includes/header.php");
+$dir = dirname(dirname(dirname($tiddlyCfg['pref']['upload_dir']))).'/uploads/documents';
+@mkdir($dir, 0777, true);
+formatParametersPOST($_POST['html']);
+
+$myFile = $dir."/".$_REQUEST['compositionTiddler'].".html";
+$fh = @fopen($myFile, 'w+');
+
+if(@fwrite($fh, $_POST['html'])){
+}
+@fclose($fh);
+?>
--- association/serversides/cctiddly/Trunk/plugins/tiddlertree/files/printTree.js
+++ association/serversides/cctiddly/Trunk/plugins/tiddlertree/files/printTree.js
@@ -1,35 +1,29 @@
 config.macros.printTree={};
 	
 config.macros.printTree.handler=function(place,macroName,params,wikifier,paramString,tiddler){
-
-var srcWindow = window.open("","sourceWindow","width=700,height=900");
-	var srcDocument = srcWindow.document;
-	// Jam in the text template
-	srcDocument.write("<html><head></head><body><div id='printedVersion'></div></body></html>");
-	srcDocument.close();
-	var marker = srcDocument.getElementById("printedVersion");
+	var htmlStack = ["<html><head></head><body>"];
 	var treeSpec = store.getTiddlerText(params[0]); 
 	if(treeSpec){
 		var sections = treeSpec.split("\n");
-		var parent = createTiddlyElement(marker, "ul");
 		for(var i = 0; i < sections.length; i++) {
 			var matches = sections[i].match(/^(\*+) (.*)/)
 			if (matches) {
 				var level = matches[1].length;
 				var tiddlerTitle = matches[2];
-				if (level>prevLevel) {
-					parent = createTiddlyElement(parent, "ul");
-				} else if (level < prevLevel) {
-					parent = parent.parentNode;
-				}
-			}
-			var heading = createTiddlyElement(marker, "h"+level, null, null,   tiddlerTitle);
-			wikify(store.getTiddlerText(tiddlerTitle),marker);
+			}		
+			htmlStack.push("<h"+level+">"+tiddlerTitle+"</h"+level+">");
+			htmlString += wikifyStatic(store.getTiddlerText(tiddlerTitle));
 			var prevLevel = level;			
 		}
+		htmlStack.push("</body></html>")
+		var htmlString = htmlStack.join("\n");
+		doHttp('POST',url+'/plugins/tiddlertree/files/createHtmlFile.php','workspace_name='+workspace+'&html="'+encodeURIComponent(htmlString)+'"&compositionTiddler='+params[0],null,null,null,config.macros.printTree.saveCallback,params);		
 	}
-	
 };
+
+config.macros.printTree.saveCallback=function(status,context,responseText,uri,xhr) {
+	log(responseText);
+}
 
 function log() { if (console) console.log.apply(console, arguments); };
 
