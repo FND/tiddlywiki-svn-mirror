348fa859f63f03dfacae09ba20d347fece7fbdeb
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-05 14:04:44 +0000

    TiddlyWebConfig: do not force AutoSave for local documents
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8648 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/TiddlyWebConfig.js
+++ association/plugins/TiddlyWebConfig.js
@@ -2,7 +2,7 @@
 |''Name''|TiddlyWebConfig|
 |''Description''|configuration settings for TiddlyWeb|
 |''Author''|FND|
-|''Version''|0.2.0|
+|''Version''|0.2.1|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/plugins/TiddlyWebConfig.js|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
@@ -18,7 +18,9 @@
 if(!version.extensions.TiddlyWebConfig) { //# ensure that the plugin is only installed once
 version.extensions.TiddlyWebConfig = { installed: true };
 
-config.options.chkAutoSave = true;
+if(window.location.protocol == "file:") {
+	config.options.chkAutoSave = true;
+}
 
 config.defaultCustomFields = {
 	"server.type": "tiddlyweb",
