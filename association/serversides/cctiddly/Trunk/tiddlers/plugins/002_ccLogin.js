13799e0914aafeb53634551fd4f3cc5a4efefccc
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-05 13:59:05 +0000

    ccTiddly - tiddlerTree(TiddlyDocs) - removing register button
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8647 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/tiddlers/plugins/002_ccLogin.js
+++ association/serversides/cctiddly/Trunk/tiddlers/plugins/002_ccLogin.js
@@ -88,7 +88,7 @@ config.macros.ccLogin.refresh=function(place, error){
 	createTiddlyButton(w.footElem,this.buttonLogin,this.buttonLoginToolTip,function() {
 		config.macros.ccLogin.doLogin(w.formElem["username"].value, w.formElem["password"].value, this, place);
 	},null, null, null,  {tabindex:'3'});
-	
+/*	
 	if(config.macros.register!==undefined){		
 		var li_register = createTiddlyElement(w.footElem, "li");
 		createTiddlyButton(li_register,config.macros.register.buttonRegister,config.macros.register.buttonRegisterToolTip,function() {
@@ -99,7 +99,7 @@ config.macros.ccLogin.refresh=function(place, error){
 	createTiddlyButton(li_forgotten,this.buttonForgottenPassword,this.buttonForgottenPasswordToolTip,function() {
 		config.macros.ccLogin.displayForgottenPassword(this, place);
 	},"nobox", null, null,  {tabindex:5});
-
+*/
 };
 
 config.macros.ccLogin.doLogin=function(username, password, item, place){
