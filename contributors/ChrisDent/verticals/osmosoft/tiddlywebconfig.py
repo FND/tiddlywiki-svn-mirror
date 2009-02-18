277ceb0d66560c5b3d77dd9b199aac8b41eaaca7
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-18 22:12:13 +0000

    tiddlyweb - the osmosoft vertical of TiddlyWeb now uses the cachinghoster plugin instead of osmosoft.py.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8798 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/verticals/osmosoft/tiddlywebconfig.py
+++ contributors/ChrisDent/verticals/osmosoft/tiddlywebconfig.py
@@ -1,16 +1,14 @@
 # A default config, make your own changes here.
 config = {
     'secret': '0e45a778ae3a45d05c04c2930467aa261b1a6c5d',
+    'server_prefix': '/wiki',
     'server_host': {
         'scheme': 'http',
         'host': 'labs.osmosoft.com',
         'port': '80',
     },
     'debug_level': 'DEBUG',
-    #'system_plugins': ['logout', 'static', 'gzipper', 'osmosoft'],
-    'system_plugins': ['static','logout', 'osmosoft'],
-    'static_dir': 'images',
-    'auth_systems': ['openid'],
+    'system_plugins': ['logout', 'cachinghoster'],
     'css_uri': 'http://peermore.com/tiddlyweb.css',
     'extension_types': {
         'atom': 'application/atom+xml',
