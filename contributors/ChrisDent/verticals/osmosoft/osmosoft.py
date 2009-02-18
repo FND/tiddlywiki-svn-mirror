36f0759dd0ad2f3971feb0caf2475e3747d06b4c
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-18 22:11:36 +0000

    tiddlyweb - update the cache validation routine so it is healthy in the face of someone switching between the two recipes: because the same url is used the browser will send the same Etag without knowing any better.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8794 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/verticals/osmosoft/osmosoft.py
+++ contributors/ChrisDent/verticals/osmosoft/osmosoft.py
@@ -1,48 +1,6 @@
 """
 Quick plugin to replace what's at /
 with a redirect to useful stuff.
-
-Includes some interesting cache handling:
-
-This is an interesting (I think) example of some HTTP, WSGI and
-Python, and filesystem fun:
-
-1 When a wiki is generated from the store, a static copy of
-  the wiki is dumped to disk, using its generated Etag as
-  the filename. A symlink from the recipe name to the etag
-  filename is made.
-
-2 When an incoming request comes in, if it has an If-None-Match
-  header (which will contain the Etag) we look on the disk for
-  a file with that name. If it is there, we respond with an
-  HTTP 304, and no response body: no data is sent, the browser
-  knows to use what it has in cache.
-
-3 If there is no If-None-Match header, we look on disk for a
-  file named with the recipe we are looking for. If this is
-  there we read it to get its content and determine the name of
-  the file to which it is symlinked. We send out the static content
-  from disk, and the etag filename (so future requests from the
-  same browser will send the If-None-Match header).
-
-4 If there's nothing matching in the cache dir, then we call
-  into the normal TiddlyWeb code to generate the wiki. We do a
-  bit of finagling to trap the Etag that gets generated. Before
-  we send the content out to the browser, we do step 1 above.
-
-5 The StorageInterface provides a tiddler_written() hook that
-  gets called any time a Tiddler is written to the store. We
-  override that code so that we flush the entire contents of
-  the cache whenever a Tiddler is updated, requiring the
-  cache to rebuild itself on the next request.
-
-6 Because we never actually read from the etag filename, only
-  the symlinked filey, and we only ever make the symlink to the
-  etag file after it is fully written, we should avoid the
-  situation where we read a partially written file from cache.
-
-For some other time: This code needs more testing and better comments
-to explain why it is doing what it is doing.
 """
 
 
@@ -93,7 +51,7 @@ def osmo_home(environ, start_response):
         start_response(status, headers)
 
     try:
-        _validate_cache(if_none_match)
+        _validate_cache(if_none_match, recipe_name)
         output, out_etag = _read_cache(recipe_name)
         start_response('200', [
             ('Content-Type', 'text/html; charset=UTF-8'),
@@ -108,12 +66,14 @@ def osmo_home(environ, start_response):
     return output
 
 
-def _validate_cache(etag):
+def _validate_cache(etag, name):
     if not etag:
         return
     path = os.path.join(WIKI_CACHE_DIR, etag)
+    link_filename = os.path.join(WIKI_CACHE_DIR, name)
+    linked_etag = os.path.basename(os.path.realpath(link_filename))
     logging.debug('attempting to validate %s' % path)
-    if os.path.exists(path):
+    if etag == linked_etag and os.path.exists(path):
         logging.debug('validated %s' % etag)
         raise HTTP304(etag)
     
