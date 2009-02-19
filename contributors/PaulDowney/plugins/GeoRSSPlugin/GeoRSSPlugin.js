0ff1617fc8775cafd6e886cc454a4dd5d6bf6d87
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-19 00:21:41 +0000

    GeoRSSPlugin - tidied up namespace and documentation
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8803 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/GeoRSSPlugin/GeoRSSPlugin.js
+++ contributors/PaulDowney/plugins/GeoRSSPlugin/GeoRSSPlugin.js
@@ -4,12 +4,12 @@
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
 |''Source:''|http://whatfettle.com/2008/07/GeoRSSPlugin/ |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/GeoRSSPlugin/ |
-|''Version:''|0.2|
+|''Version:''|0.3|
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.4|
 !!Documentation
-This plugin extends the core RSS saving to include [[W3C GeoRSS|http://en.wikipedia.org/wiki/GeoRSS]] values for tiddlers with latitude and longitude values as [[extended fields|http://www.tiddlywiki.org/wiki/Extended_fields]].  Coordinates are taken to be in the [[WGS|http://en.wikipedia.org/wiki/World_Geodetic_System]].  For example output see [[index.xml|http://whatfettle.com/2008/07/GeoRSSPlugin/index.xml]], and the result on [[Google Maps|http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=http:%2F%2Fwhatfettle.com%2F2008%2F07%2FGeoRSSPlugin%2Findex.xml&ie=UTF8&t=h&z=14]].
+This plugin extends the core RSS saving to include [[W3C GeoRSS|http://en.wikipedia.org/wiki/GeoRSS]] values for tiddlers with ''latitude'' and ''longitude'' [[extended fields|http://www.tiddlywiki.org/wiki/Extended_fields]].  Coordinates are taken to be in the [[WGS|http://en.wikipedia.org/wiki/World_Geodetic_System]] format. For example output, see [[index.xml|http://whatfettle.com/2008/07/GeoRSSPlugin/index.xml]], and the result when passed to[[Google Maps|http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=http:%2F%2Fwhatfettle.com%2F2008%2F07%2FGeoRSSPlugin%2Findex.xml&ie=UTF8&t=h&z=14]].
 
 For more examples on using geo data in TiddlyWiki, see [[Jon Robson|http://www.jonrobson.me.uk/]]'s [[The Web is Your Oyster|http://www.jonrobson.me.uk/workspaces//tiddlers/The%20Web%20Is%20Your%20Oyster%20-%20create%20and%20tweak%20travel%20itineraries%20for%20your%20trip%20through%20the%20web/]].
 !!Code
@@ -46,7 +46,7 @@ version.extensions.GeoRSSPlugin = {installed:true};
 		{
 			var me = config.extensions.geoRSS;
 			var s = me._generateRss();
-			return s.replace(/">/, '" xmlns:geo="'+me.geo+'" xmlns:georss="'+me.georss+'">');
+			return s.replace(/">/, '" xmlns:geo="'+me.geo+'">');
 		}
 	};
 
