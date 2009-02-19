7e67c32133dd3e3b1006adacb94d83aedb2510cf
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-19 00:04:28 +0000

    GeoRSSPlugin - standardised on the antiquated W3C GeoRSS and added documentation
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8801 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/GeoRSSPlugin/GeoRSSPlugin.js
+++ contributors/PaulDowney/plugins/GeoRSSPlugin/GeoRSSPlugin.js
@@ -4,11 +4,14 @@
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
 |''Source:''|http://whatfettle.com/2008/07/GeoRSSPlugin/ |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/GeoRSSPlugin/ |
-|''Version:''|0.1|
+|''Version:''|0.2|
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.4|
 !!Documentation
+This plugin extends the core RSS saving to include [[W3C GeoRSS|http://en.wikipedia.org/wiki/GeoRSS]] values for tiddlers with latitude and longitude values as [[extended fields|http://www.tiddlywiki.org/wiki/Extended_fields]].  Coordinates are taken to be in the [[WGS|http://en.wikipedia.org/wiki/World_Geodetic_System]].  For example output see [[index.xml|http://whatfettle.com/2008/07/GeoRSSPlugin/index.xml]], and the result on [[Google Maps|http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=http:%2F%2Fwhatfettle.com%2F2008%2F07%2FGeoRSSPlugin%2Findex.xml&ie=UTF8&t=h&z=14]].
+
+For more examples on using geo data in TiddlyWiki, see [[Jon Robson|http://www.jonrobson.me.uk/]]'s [[The Web is Your Oyster|http://www.jonrobson.me.uk/workspaces//tiddlers/The%20Web%20Is%20Your%20Oyster%20-%20create%20and%20tweak%20travel%20itineraries%20for%20your%20trip%20through%20the%20web/]].
 !!Code
 ***/
 //{{{
@@ -22,7 +25,6 @@ version.extensions.GeoRSSPlugin = {installed:true};
         config.extensions.geoRSS = {
 
 		geo: "http://www.w3.org/2003/01/geo/wgs84_pos#",
-		georss: "http://www.georss.org/georss",
 
                 _tiddlerToRssItem: tiddlerToRssItem,
 		tiddlerToRssItem: function(tiddler,uri)
@@ -30,11 +32,11 @@ version.extensions.GeoRSSPlugin = {installed:true};
 			var me = config.extensions.geoRSS;
 			var s = me._tiddlerToRssItem(tiddler,uri);
 			var lat = store.getValue(tiddler.title,"latitude");
-			var long = store.getValue(tiddler.title,"longitude");
-			if(lat&&long){
-				lat=lat.htmlEncode();
-				long=long.htmlEncode();
-				s = s + "<georss:point>"+lat+" "+long+"</georss:point>"+"<geo:lat>"+lat+"</geo:lat>"+"<geo:long>"+long+"</geo:long>";
+			if(lat){
+				var long = store.getValue(tiddler.title,"longitude");
+				if(long){
+					s = s + "<geo:lat>"+lat.htmlEncode()+"</geo:lat>"+"<geo:long>"+long.htmlEncode()+"</geo:long>";
+				}
 			}
 			return s;
 		},
@@ -50,6 +52,5 @@ version.extensions.GeoRSSPlugin = {installed:true};
 
         tiddlerToRssItem = config.extensions.geoRSS.tiddlerToRssItem;
         generateRss = config.extensions.geoRSS.generateRss;
-
 }
 //}}}
--- contributors/PaulDowney/plugins/GeoRSSPlugin/deploy.sh
+++ contributors/PaulDowney/plugins/GeoRSSPlugin/deploy.sh
@@ -1,1 +1,1 @@
-scp index.html pauldowney@amp.dreamhost.com:/home/pauldowney/whatfettle.com/2008/07/GeoRSSPlugin/
+scp index.html index.xml pauldowney@amp.dreamhost.com:/home/pauldowney/whatfettle.com/2008/07/GeoRSSPlugin/
