0f49f094e327138396f5ab4dafc2cd174cfc13f4
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-19 10:55:45 +0000

    TiddlyWeb: dev store; reordered class methods
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8805 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/TiddlyWeb/plugins/devtext.py
+++ contributors/FND/TiddlyWeb/plugins/devtext.py
@@ -33,79 +33,18 @@ class Store(Text):
 
     def list_bags(self):
         bags = self._dirs_in_dir(self._store_root())
-
         return [Bag(urllib.unquote(bag).decode('utf-8')) for bag in bags]
 
     def list_recipes(self):
         recipes = [file.replace('.recipe', '') for file in self._files_in_dir(self._store_root()) if file.endswith('.recipe')]
-
         return [Recipe(urllib.unquote(recipe).decode('utf-8')) for recipe in recipes]
 
     def list_users(self):
         users = [file.replace('.user', '') for file in self._files_in_dir(self._store_root()) if file.endswith('.user')]
-
         return [User(urllib.unquote(user).decode('utf-8')) for user in users]
-
-    def _dirs_in_dir(self, path):
-        return [dir for dir in self._files_in_dir(path) if os.path.isdir(os.path.join(path, dir))]
-
-    def _recipe_path(self, recipe):
-        return os.path.join(self._store_root(), _encode_filename(recipe.name) + '.recipe')
-
-    def _bag_path(self, bag_name):
-        try:
-            return os.path.join(self._store_root(), _encode_filename(bag_name))
-        except (AttributeError, StoreEncodingError), exc:
-            raise NoBagError('No bag name: %s' % exc)
-
-    def _user_path(self, user):
-        return os.path.join(self._store_root(), user.usersign + '.user')
-
-    def _tiddlers_dir(self, bag_name):
-        return self._bag_path(bag_name)
-
-    def _tiddler_base_filename(self, tiddler):
-        # should we get a Bag or a name here?
-        bag_name = tiddler.bag
-
-        store_dir = self._tiddlers_dir(bag_name)
-
-        if not os.path.exists(store_dir):
-            raise NoBagError('%s does not exist' % store_dir)
-
-        try:
-            return store_dir
-        except StoreEncodingError, exc:
-            raise NoTiddlerError(exc)
-
-    def _tiddler_full_filename(self, tiddler, revision):
-        return os.path.join(self._tiddlers_dir(tiddler.bag),
-            '%s.tid' % _encode_filename(tiddler.title))
-
-    def _tiddler_revision_filename(self, tiddler, index=0):
-        return 1
-
-    def _read_tiddler_file(self, tiddler, tiddler_filename):
-        try:
-            tiddler_file = codecs.open(tiddler_filename, encoding='utf-8')
-        except IOError:
-            tiddler_filename = tiddler_filename.replace('.tid', '.js')
-            tiddler_file = codecs.open(tiddler_filename, encoding='utf-8')
-        tiddler_string = tiddler_file.read()
-        tiddler_file.close()
-        if tiddler_filename.endswith('.js'):
-            header = 'tags: systemConfig'
-            notice = '/***\n> source: {{{%s}}}\n***/' % tiddler_filename
-            tiddler_string = '%s\n\n%s\n%s' % (header, notice, tiddler_string)
-        self.serializer.object = tiddler
-        tiddler = self.serializer.from_string(tiddler_string)
-        return tiddler
 
     def list_tiddler_revisions(self, tiddler):
         return [self._tiddler_revision_filename(tiddler)]
-
-    def _files_in_dir(self, path):
-        return [x for x in os.listdir(path) if not x.startswith('.') and not x == 'policy' and not x == 'description']
 
     def bag_get(self, bag):
         bag_path = self._bag_path(bag.name)
@@ -141,11 +80,9 @@ class Store(Text):
                 if lock_attempts > 4:
                     raise StoreLockError(exc)
                 time.sleep(.1)
-
         revision = 1
         tiddler_filename = self._tiddler_full_filename(tiddler, revision)
         tiddler_file = codecs.open(tiddler_filename, 'w', encoding='utf-8')
-
         if tiddler.type and tiddler.type != 'None':
             tiddler.text = b64encode(tiddler.text)
         self.serializer.object = tiddler
@@ -166,6 +103,54 @@ class Store(Text):
         except Exception, exc:
             raise IOError('unable to delete %s: %s' % (tiddler.title, exc))
 
+    def _bag_path(self, bag_name):
+        try:
+            return os.path.join(self._store_root(), _encode_filename(bag_name))
+        except (AttributeError, StoreEncodingError), exc:
+            raise NoBagError('No bag name: %s' % exc)
+
+    def _recipe_path(self, recipe):
+        return os.path.join(self._store_root(), _encode_filename(recipe.name) + '.recipe')
+
+    def _user_path(self, user):
+        return os.path.join(self._store_root(), user.usersign + '.user')
+
+    def _tiddlers_dir(self, bag_name):
+        return self._bag_path(bag_name)
+
+    def _tiddler_base_filename(self, tiddler):
+        bag_name = tiddler.bag
+        store_dir = self._tiddlers_dir(bag_name)
+        if not os.path.exists(store_dir):
+            raise NoBagError('%s does not exist' % store_dir)
+        try:
+            return store_dir
+        except StoreEncodingError, exc:
+            raise NoTiddlerError(exc)
+
+    def _tiddler_full_filename(self, tiddler, revision):
+        return os.path.join(self._tiddlers_dir(tiddler.bag),
+            '%s.tid' % _encode_filename(tiddler.title))
+
+    def _tiddler_revision_filename(self, tiddler, index=0):
+        return 1
+
+    def _read_tiddler_file(self, tiddler, tiddler_filename):
+        try:
+            tiddler_file = codecs.open(tiddler_filename, encoding='utf-8')
+        except IOError:
+            tiddler_filename = tiddler_filename.replace('.tid', '.js')
+            tiddler_file = codecs.open(tiddler_filename, encoding='utf-8')
+        tiddler_string = tiddler_file.read()
+        tiddler_file.close()
+        if tiddler_filename.endswith('.js'):
+            header = 'tags: systemConfig'
+            notice = '/***\n> source: {{{%s}}}\n***/' % tiddler_filename
+            tiddler_string = '%s\n\n%s\n%s' % (header, notice, tiddler_string)
+        self.serializer.object = tiddler
+        tiddler = self.serializer.from_string(tiddler_string)
+        return tiddler
+
     def _read_policy(self, bag_path):
         policy_filename = os.path.join(bag_path, 'policy')
         try:
@@ -179,3 +164,9 @@ class Store(Text):
         except IOError:
             policy = Policy()
         return policy
+
+    def _dirs_in_dir(self, path):
+        return [dir for dir in self._files_in_dir(path) if os.path.isdir(os.path.join(path, dir))]
+
+    def _files_in_dir(self, path):
+        return [x for x in os.listdir(path) if not x.startswith('.') and not x == 'policy' and not x == 'description']
