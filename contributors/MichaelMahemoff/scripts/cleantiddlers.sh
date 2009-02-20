209d8628d496bf494a571e16ac7bfa4a02eb6c4a
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-20 17:14:06 +0000

    updated scripts to allow easy upload of all complete plugins
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8828 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/scripts/cleantiddlers.sh
+++ contributors/MichaelMahemoff/scripts/cleantiddlers.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 export TRASH='/tmp/cleantiddlers.'"$$"
 mkdir $TRASH
-cd `dirname $0`
+  ls -la $TRASH
+cd `dirname $0`/../verticals/TiddlyGuv/app/store
 
-for f in `find ../verticals/TiddlyGuv/app/store -name '[0-9]*'` ; do
+for f in `find . -name '^[0-9]+$'` ; do
   if [ `basename $f` != '1' ] ; then
     echo $f
     mv $f $TRASH
   fi
 done
 
-for f in `find . -type d -name 'comment*'` ; do
+for f in `find . -type d -name 'comment_*'` ; do
+  echo $f
+  mv $f $TRASH
+done
+
+for f in `find . -name '*.sw?'` ; do
   echo $f
   mv $f $TRASH
 done
--- contributors/MichaelMahemoff/scripts/make.sh
+++ contributors/MichaelMahemoff/scripts/make.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 root=`dirname $0`'/..'
 cd $root
-for recipe in `find . -name *.recipe` ; do
-  echo 'COOKING '"$recipe"
-  recipebase=`echo $recipe | sed -e s/\.html\.recipe$//g`
-  (cd `dirname $recipebase` ; cook `basename $recipebase`)
-done
 
-pub="$root/pub"
-if [ -e $pub ] ; then
-  mv $pub /tmp/pub$$
-fi
-mkdir $pub
+function makepub() {
+  pub="$root/pub"
+  if [ -e $pub ] ; then
+    mv $pub /tmp/pub$$
+  fi
+  mkdir $pub
+}
 
-(exec maketiddler.sh)
-echo 'done maketiddler'
+function cookall() {
+  for recipe in `find . -name *.recipe` ; do
+    echo 'COOKING '"$recipe"
+    recipebase=`echo $recipe | sed -e s/\.html\.recipe$//g`
+    (cd `dirname $recipebase` ; cook `basename $recipebase`)
+  done
+}
 
-cp plugins/CommentsPlugin/CommentsPlugin.html plugins/PowerTitlePlugin/PowerTitlePlugin.html verticals/tiddlyguv/prototype/TiddlyGuv.html plugins/TiddlerTableMacro/TiddlerTableMacro.html verticals/tiddlyguv/portal/*.html pub
+function pub() {
+  while [ "$1" != "" ] ; do
+    plugin=$1
+    (cd plugins/$1 ; if [ -f make.rb ] ; then ./make.rb ; fi ; cook $1 ; )
+    cp plugins/$1/$1.html $pub
+    shift
+  done
+  ls -la $pub
+}
+
+makepub
+pub GuidPlugin CommentsPlugin DrawingPlugin TiddlerTableMacro PreferenceMacros
+cd $pub
+ls -latr 
+echo $MAHEMOFF_TIDDLYWIKI
+rsync -avz --delete -e ssh * $MAHEMOFF_TIDDLYWIKI
+exit
+
+# (exec maketiddler.sh)
+# echo 'done maketiddler'
+
+# cp plugins/CommentsPlugin/CommentsPlugin.html plugins/PowerTitlePlugin/PowerTitlePlugin.html verticals/tiddlyguv/prototype/TiddlyGuv.html plugins/TiddlerTableMacro/TiddlerTableMacro.html verticals/tiddlyguv/portal/*.html pub
+
+# cp verticals/tiddlyguv/prototype/TiddlyGuv.html verticals/tiddlyguv/portal/*.html pub
 
 rsync -avz --delete -e ssh pub/* $TIDDLYGUV_PUB
+
+# cp plugins/CommentsPlugin/CommentsPlugin.html plugins/TiddlerTableMacro/TiddlerTableMacro.html pub
+
+# rsync -avz --delete -e ssh pub/* $MAHEMOFF_PUB
