37aec27dc15e1d9cbe964440c725352b97310def
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-20 11:19:06 +0000

    unaforms - don't use a special base tiddlywiki, just put the special stuff in a bag in the recipe cascade.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8819 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
+++ contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
@@ -7,6 +7,5 @@ config = {
             'http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/verticals/UnaForms/forms.recipe',
             ],
         'debug_level': 'DEBUG',
-        'base_tiddlywiki': 'base_tiddlywiki.html',
         'css_uri': 'http://peermore.com/tiddlyweb.css',
 }
