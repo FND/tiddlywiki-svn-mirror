ce02922c9b0042b201fa4966667c47f9b5b726a1
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-23 15:44:52 +0000

    TiddlyWebConfig: properly resolve full host name
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8838 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/TiddlyWebConfig.js
+++ association/plugins/TiddlyWebConfig.js
@@ -2,7 +2,7 @@
 |''Name''|TiddlyWebConfig|
 |''Description''|configuration settings for TiddlyWeb|
 |''Author''|FND|
-|''Version''|0.2.2|
+|''Version''|0.2.3|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/plugins/TiddlyWebConfig.js|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
@@ -18,15 +18,21 @@
 if(!version.extensions.TiddlyWebConfig) { //# ensure that the plugin is only installed once
 version.extensions.TiddlyWebConfig = { installed: true };
 
+(function() { //# set up local scope
+
 if(window.location.protocol != "file:") {
 	config.options.chkAutoSave = true;
 }
 
+var adaptor = new config.adaptors.tiddlyweb();
+var minhost = store.getTiddler("TiddlyWebConfig").fields["server.host"];
 config.defaultCustomFields = {
 	"server.type": "tiddlyweb",
-	"server.host": store.getTiddler("TiddlyWebConfig").fields["server.host"],
+	"server.host": adaptor.fullHostName(minhost),
 	"server.workspace": "bags/common"
 };
+
+})(); //# end of local scope
 
 } //# end of "install only once"
 //}}}
