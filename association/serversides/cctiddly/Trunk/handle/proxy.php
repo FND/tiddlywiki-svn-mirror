aa950ae8268e8893ba26c02ae34d804ef64668ae
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-23 12:15:33 +0000

    ccTiddly - 1.8 proxy.php should now fail gracefully after attempting various different proxy fucnctions
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8834 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/proxy.php
+++ association/serversides/cctiddly/Trunk/handle/proxy.php
@@ -1,39 +1,43 @@
 <?php
-
-$ch = curl_init($_REQUEST['feed']);
-curl_exec($ch);
-curl_close($ch);
-
-exit;
-
-
-// This file requires refactoring....
+$cct_base = "../";
+include($cct_base."includes/header.php");
 $feed = $_REQUEST['feed'];
 $url = parse_url($feed);
-if(!in_array($url[host], $tiddlyCfg['allowed_proxy_list']))
+
+// security checks 
+
+if(!in_array($url['host'], $tiddlyCfg['allowed_proxy_list']))
 {
 	exit;
 }
 $url = $feed;
-$params = array('http' => array(
-'method' => 'GET',
-'header'=> 'accept:application/json',
-'content' => $data));
-$ctx = stream_context_create($params);
-$fp = fopen($url, 'rb', false, $ctx);
-echo $response = stream_get_contents($fp);
+
+// Attempt different proxy methods. 
+
+if(curl_version())
+{
+	$ch = curl_init($_REQUEST['feed']);
+	curl_exec($ch);
+	curl_close($ch);
+	exit;
+	
+} elseif(readfile()) 
+{
+	readfile($feed);
+} else 
+{
+	$params = array('http' => array(
+	'method' => 'GET',
+	'header'=> 'accept:application/json',
+	'content' => $data));
+	$ctx = stream_context_create($params);
+	$fp = fopen($url, 'rb', false, $ctx);
+	echo $response = stream_get_contents($fp);
+	
+}
 
 // in some situtations this needs to replace the above line. 
 //echo $response = readExternalFile($feed);
-exit;
 
-if($feed != '' && strpos($feed, 'http') === 0)
-{
-	if($_REQUEST['format'])
-		readfile($feed."&format=".$_REQUEST['format']);
-	else
-		readfile($feed);
-		
-	return;
-}
+
 ?>
