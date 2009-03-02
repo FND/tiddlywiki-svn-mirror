ee256d1bd80e8056748fb98edeeeae5ac5b6d1b0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-02 15:48:15 +0000

    ccTiddly - TiddlyDocs - css tweaks for a screen resizing
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8918 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/save.php
+++ association/serversides/cctiddly/Trunk/handle/save.php
@@ -10,63 +10,59 @@ if(!user_session_validate())
 	sendHeader("401");
 	exit;	
 }
-$tiddlyCfg['workspace_name'] = formatParametersPOST($_POST['workspace']);
 $tiddler = db_tiddlers_mainSelectTitle($_POST['title']);
-
-if($_POST['id']!="undefined")  // if the tiddler is being renamed we need ensure the id is correct. 
-	$tiddler['id'] = $_POST['id']; 
-
-$otiddler['revision'] = formatParametersPOST($_POST['revision']);
-
 $ntiddler['title'] = formatParametersPOST($_POST['title']);
+
+$tiddlyCfg['workspace_name'] = formatParametersPOST($_POST['workspace']);
 $ntiddler['modifier'] = formatParametersPOST($_POST['modifier']);
 $ntiddler['modified'] = formatParametersPOST($_POST['modified']);
 $ntiddler['created'] = formatParametersPOST($_POST['created']); 
+
 $ntiddler['tags'] = formatParametersPOST($_POST['tags']);
+
 $ntiddler['body'] =  formatParametersPOST($_POST['body']);
+$ntiddler['revision'] = formatParametersPOST($_POST['revision']);
+
 $ntiddler['fields'] = formatParametersPOST($_POST['fields']);
+$tiddler['id'] = $_POST['id'];
 
-
-// Plugin preSave Event.
-if(@$pluginsLoader->events['preSave']) {
-	foreach (@$pluginsLoader->events['preSave'] as $event) {
-		if(is_file($event)) 
+if(@$pluginsLoader->events['preSave']) 
+{
+	foreach (@$pluginsLoader->events['preSave'] as $event)
+	{
+		if(is_file($event)) {
 			include($event);
+		}	
+		
 	}
 }				
-
-//if($tiddler['id']!="undefined")
-
-error_log("revision : "+$_POST['revision']+" id : "+ $tiddler['id']);
-if($_POST['revision'] && $_POST['revision']!=0 && $tiddler['id']!="undefined")
+if($tiddler['id']!="undefined")
 {
-	if($tiddler['revision'] !== $_POST['revision'] ) {		//ask to reload if the tiddler has been edited it was last downloaded
+	error_log("prev r is : ".$ntiddler['revision']);
+	
+	if($tiddler['revision'] >= $_POST['revision'] ) {		//ask to reload if modified date differs
 		debug($ccT_msg['debug']['reloadRequired'], "save");
-		error_log("!!! tidldler revisions are ".$tiddler['revision']." and this was posted : ". $_POST['revision']." on tiddler ".$ntiddler['title']);
 		sendHeader(409);
 		exit;
 	}
 		//require edit privilege on new and old tags			
 	if(user_editPrivilege(user_tiddlerPrivilegeOfUser($user,$ntiddler['tags'])) && user_editPrivilege(user_tiddlerPrivilegeOfUser($user,$otiddler['tags'])))
 	{
+		$ntiddler['modified'] = $ntiddler['modified']; 
 		$ntiddler['creator'] = $otiddler['creator'];
 		$ntiddler['created'] = $otiddler['created'];
-		$ntiddler['revision'] = $otiddler['revision']+1;
-	//	error_log(print_r($ntiddler))
-		debug("Attempting to update server for tiddler...");
+		debug("Attempting to update server...");
 		unset($ntiddler['workspace_name']); 	// hack to remove the workspace being set twice. 
-		
-		error_log("sSSending SQL ids : ".$tiddler['id'] ." for ".$ntiddler['title']);
 		if(tiddler_update_new($tiddler['id'], $ntiddler)) {
 			sendHeader(201);
-			error_log("setting id to : ".$tiddler['id']);
+			echo $tiddler['id'];
 		}
 	}else{
 		debug("Permissions denied to save.", "save");
 		sendHeader(400);	
 	}
-}else
-{	//This Tiddler does not exist in the database.
+}else{
+	//This Tiddler does not exist in the database.
 	if( user_insertPrivilege(user_tiddlerPrivilegeOfUser($user,$ntiddler['tags'])) ) 
 	{
 		debug("Inserting New Tiddler...", "save");
@@ -78,11 +74,8 @@ if($_POST['revision'] && $_POST['revision']!=0 && $tiddler['id']!="undefined")
 		{
 			sendHeader(201);
 			echo $id;
-			error_log("TIS IS A NEW ~TIDLER - setting id to ".$id);
-			
 		}
 	}else{
-		debug("Permissions denied to save.", "save");
 		sendHeader(400);
 	}
 }
