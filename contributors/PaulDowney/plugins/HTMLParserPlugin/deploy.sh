f24770b8de09bdaf1b34c85e8b5a8d34f7001d80
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-05 10:29:03 +0000

    HTMLParserPlugin - deployed simple.html test file
    git-svn-id: http://svn.tiddlywiki.org/Trunk@8992 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/HTMLParserPlugin/deploy.sh
@@ -0,0 +1,2 @@
+#ssh pauldowney@amp.dreamhost.com mkdir /home/pauldowney/whatfettle.com/2008/07/HTMLParserPlugin/
+scp index.html simple.html HTMLParserPlugin.js pauldowney@amp.dreamhost.com:/home/pauldowney/whatfettle.com/2008/07/HTMLParserPlugin/
