e6ec0120906eb0701739027e9d2a3857dcb9c94e
SaqImtiaz <SaqImtiaz@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-08 12:53:26 +0000

    WikispacesOffline: recipe rename, demo generator
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9011 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SaqImtiaz/verticals/Wikispaces/WikispacesOffline/settingsNotebookPlugin.js
+++ contributors/SaqImtiaz/verticals/Wikispaces/WikispacesOffline/settingsNotebookPlugin.js
@@ -5,7 +5,7 @@ merge(config.commands.saveTiddler,{
 	text: "enter",
 	tooltip: "finish editing this page"});
 	
-setStylesheet("#mysidebar #sidebarOptions .bigbutton .button {padding:0.2em 0.3em; font-size:120%; font-weight:bold; border:1px solid #000; display:block; background-color:#0044BB; color:#fff; margin-bottom:0.2em;} #mysidebar #sidebarOptions .bigbutton br {display:none;}","savebuttonStyles");
+setStylesheet("#mysidebar #sidebarOptions .bigbutton .button {padding:0.2em 0.3em; font-size:110%; font-weight:bold; border:1px solid #000; display:block; background-color:#0044BB; color:#fff; margin-bottom:0.2em;} #mysidebar #sidebarOptions .bigbutton br {display:none;}","savebuttonStyles");
 
 
 setStylesheet("#backstageButton #backstageShow {position:absolute;top:-30em;right:0;}","hideBackStageStyles");
---
+++ contributors/SaqImtiaz/verticals/Wikispaces/WikispacesOfflineGenerator.cgi
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import urllib
+import cgi
+from urlparse import urlparse
+
+sourceurl = "http://studentnotebook.lewcid.org/WikispacesOffline-generic.html"
+sourcefile = urllib.urlopen(sourceurl)
+input = sourcefile.read()
+
+form = cgi.FieldStorage()
+
+wikiurl = form['wikiurl'].value
+wikiworkspace = (urlparse(wikiurl)[1].split("."))[0]
+
+output = input.replace('%(wiki_url)s',form['wikiurl'].value).replace('%(wiki_title)s',form['wikititle'].value).replace('%(wiki_workspace)s',wikiworkspace)
+
+print 'Content-Type: application/octet-stream'
+print 'Content-Disposition: attachment; filename=fname\n\n'.replace('fname',form['wikititle'].value.replace(" ","-")+".html") 
+print output
+
