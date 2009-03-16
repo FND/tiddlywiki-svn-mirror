c744e2a4c7290e0ab6d6b5c015d0df091a542f03
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-16 13:50:27 +0000

    tidldyweb - rename the tiddlyweb-plugins/twplugins confusion to tiddlywebplugins to help error message about it not being installed make more sense
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9115 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/plugins/setup.py
+++ association/serversides/tiddlyweb/plugins/setup.py
@@ -7,15 +7,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
-VERSION = 0.5
+VERSION = 0.6
 
-setup(name = 'tiddlyweb-plugins',
+setup(name = 'tiddlywebplugins',
         version = VERSION,
         description = 'Tools and methods for managing TiddlyWeb plugins',
         author = 'Chris Dent',
         author_email = 'cdent@peermore.com',
         url = 'http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/plugins',
-        packages = ['twplugins'],
+        packages = ['tiddlywebplugins'],
         platforms = 'Posix; MacOS X; Windows',
         install_requires = ['tiddlyweb'],
         )
