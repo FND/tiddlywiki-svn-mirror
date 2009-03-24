f5bb76c7dae39cad1e57889e4357b5a30d0a08d0
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-24 11:02:59 +0000

    TiddlyWeb; dev store script
    shell script for automated creation of a TiddlyWeb instance using devtext store
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9235 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/FND/TiddlyWeb/plugins/devinstance.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# set up TiddlyWeb instance using devtext store
+#
+# Usage:
+#  $ ./devinstance targetDir
+
+function quit {
+	echo "aborting: $1"
+	exit
+}
+
+if [ $# -gt 0 ]; then
+	cd $1 || quit "invalid target directory"
+else
+	quit "target directory not specified"
+fi
+
+# initialize configuration
+wget http://svn.tiddlywiki.org/Trunk/contributors/FND/TiddlyWeb/plugins/devtext.py
+tiddlywebconfig='{\n	"server_store": ["devtext", { "store_root": "store" }]\n}'
+echo -e "config = $tiddlywebconfig" > tiddlywebconfig.py
+
+#  create instance
+twanager instance dev &&
+	rm tiddlywebconfig.py tiddlyweb.log *.pyc &&
+	cd dev ||
+	quit "failed to create instance"
+
+# configure instance
+mv ../devtext.py ./
+echo -e "config.update($tiddlywebconfig)" >> tiddlywebconfig.py # XXX: odd way to merge with existing config!?
+
+# create sample tiddler
+twanager bag content < /dev/null
+echo -e "lorem ipsum dolor sit amet" | twanager tiddler lipsum content
+# create sample plugin
+twanager bag plugins < /dev/null
+echo -e 'alert("Hello world!");' > store/plugins/helloworld.js
+# create sample recipe
+echo -e "/bags/system/tiddlers\n/bags/plugins/tiddlers\n/bags/content/tiddlers" | twanager recipe sample
+
+echo 'dev instance created; run "twanager server"'
