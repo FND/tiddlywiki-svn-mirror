ca143cbb99e01e4cddb1d3b08f1e3ef33fb36f4e
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-25 23:35:29 +0000

    FancyMissingPlugin: initial commit
    simple plugin to sort missing tiddlers by their number of references
    based on cdent's exploratory efforts
    
    currently missing proper metadata (cf. plugin template)
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9276 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/FND/plugins/FancyMissingPlugin.js
@@ -0,0 +1,22 @@
+//{{{
+(function() {
+
+// hijack getMissingLinks to sort by number of references
+var getMissingLinks = TiddlyWiki.prototype.getMissingLinks;
+TiddlyWiki.prototype.getMissingLinks = function(sortField) {
+	var results = getMissingLinks.apply(this, arguments);
+	var index = results.map(function(item, i) {
+		return {
+			title: results[i],
+			count: store.getReferringTiddlers(results[i]).length
+		};
+	});
+	return index.sort(function(a, b) {
+		return a.count - b.count;
+	}).map(function(item, i) {
+		return item.title;
+	});
+};
+
+})();
+//}}}
