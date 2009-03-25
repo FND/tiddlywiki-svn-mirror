9bd1d023ee2c74af0b7404c36054a72464c861a4
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-25 13:11:39 +0000

    Wikispaces: generateUserFilesFromCSV; various adjustments
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9255 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SaqImtiaz/verticals/Wikispaces/generateUserFilesFromCSV.py
+++ contributors/SaqImtiaz/verticals/Wikispaces/generateUserFilesFromCSV.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python
-# argv
-## 1 : csv file
-## 2 : template html file
-## 3 : output dir, must exist
-## 4 : outputfilename
+
+"""
+generate user files from CSV
+
+Usage:
+ $ ./generateUserFilesFromCSV.py <CSV> <HTML> <targetDir> <targetFile>
+ * CSV       : CSV filename
+ * HTML      : template HTML file
+ * targetDir : output dir, must exist
+ * targetFile: output filename
+"""
+
 
 import sys
-import csv
 import os
+import csv
 
-def main(argv=None):
-	csvfile = argv[1]
-	template = argv[2]
-	dir = argv[3]
-	output_name = argv[4]
+
+def main(args=None):
+	csvfile = args[1]
+	template = args[2]
+	dir = args[3]
+	output_name = args[4]
 	try:
 		os.mkdir(dir)
-	except OSError: 
+	except OSError:
 		pass
-	csvReader = csv.reader(open(csvfile),delimiter=',')
+	csvReader = csv.reader(open(csvfile), delimiter=",")
 	for row in csvReader:
 		username = row[0]
 		password = row[2]
-		f = open(template);
+		f = open(template)
 		inputtext = f.read()
-		outputtext = inputtext.replace('%(wikispaces_username)s',username).replace('%(wikispaces_password)s',password)
+		outputtext = inputtext.replace("%(wikispaces_username)s", username).
+			replace("%(wikispaces_password)s", password)
 		f.close()
-		output = open(dir + "/" + output_name + "-" + username +".html","w")
+		output = open("%s/%s-%s.html" % (dir, output_name, username), "w")
 		output.write(outputtext)
 		output.close()
 	return True
-	
+
+
 if __name__ == "__main__":
 	status = not main(sys.argv)
 	sys.exit(status)
