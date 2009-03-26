d2086d37cde7158264ac7c6bfbee8087719d90f0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-26 13:04:10 +0000

    ccTiddly - renaming now works but fails on the second commit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9286 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/handle/save.php
+++ association/serversides/cctiddly/Trunk/handle/save.php
@@ -11,7 +11,7 @@ if(!user_session_validate())
 	exit;	
 }
 $tiddlyCfg['workspace_name'] = formatParametersPOST($_POST['workspace']);
-$tiddler = db_tiddlers_mainSelectTitle($_POST['title']);
+$tiddler = db_tiddlers_mainSelectId($_POST['id']);
 
 if($_POST['id']!="undefined")  // if the tiddler is being renamed we need ensure the id is correct. 
 	$tiddler['id'] = $_POST['id']; 
@@ -27,6 +27,7 @@ $ntiddler['body'] =  formatParametersPOST($_POST['body']);
 $ntiddler['fields'] = formatParametersPOST($_POST['fields']);
 
 
+
 // Plugin preSave Event.
 if(@$pluginsLoader->events['preSave']) {
 	foreach (@$pluginsLoader->events['preSave'] as $event) {
@@ -35,49 +36,68 @@ if(@$pluginsLoader->events['preSave']) {
 	}
 }				
 
-//if($tiddler['id']!="undefined")
-
-error_log("revision : "+$_POST['revision']+" id : "+ $tiddler['id']);
 
 // does the tiddler already exists?
-
-
 if(db_tiddlers_mainSelectTitle($ntiddler['title']) || is_numeric($_POST['id']))
-{
+{	
+	
 	if($tiddler['revision'] !== $_POST['revision']) {		//ask to reload if the tiddler has been edited it was last downloaded
 		debug($ccT_msg['debug']['reloadRequired'], "save");
 		error_log("!!! tidldler revisions are ".$tiddler['revision']." and this was posted : ". $_POST['revision']." on tiddler ".$ntiddler['title']." with id ".$ntiddler['id']);
 		sendHeader(409);
 		exit;
 	}
+
+	if($ntiddler['title']!=$tiddler['title']) // The Tiddler is being renamed.
+	{	
+		error_log("ITS A RENAME");
+		
+		if(@$pluginsLoader->events['preRename']) 
+		{
+
+			foreach (@$pluginsLoader->events['preRename'] as $event)
+			{
+				error_log($event);
+				if(is_file($event)) {
+					include($event);
+				}	
+
+			}
+		}
+
+		if(user_editPrivilege(user_tiddlerPrivilegeOfUser($user,$tiddler['tags'])))
+		{
+			if(tiddler_update_new($tiddler['id'], $ntiddler))
+			{
+				sendHeader(201);
+			}
+		}
+		exit;
+
+	}
+// end rename
+
+
 		//require edit privilege on new and old tags			
 	if(user_editPrivilege(user_tiddlerPrivilegeOfUser($user,$ntiddler['tags'])) && user_editPrivilege(user_tiddlerPrivilegeOfUser($user,$otiddler['tags'])))
 	{
 		$ntiddler['creator'] = $otiddler['creator'];
 		$ntiddler['created'] = $otiddler['created'];
 		$ntiddler['revision'] = $otiddler['revision']+1;
-	//	error_log(print_r($ntiddler))
 		debug("Attempting to update server for tiddler...");
 		unset($ntiddler['workspace_name']); 	// hack to remove the workspace being set twice. 
-		
 		error_log("sSSending SQL ids : ".$tiddler['id'] ." for ".$ntiddler['title']);
 		if(tiddler_update_new($tiddler['id'], $ntiddler)) {
 			sendHeader(201);
 			error_log("setting id to : ".$tiddler['id']);
 		}
 	}else{
-		debug("Permissions denied to save.", "save");
 		sendHeader(400);	
+		debug("Permissions denied to save.", "save");
 	}
 }else
 {	//This Tiddler does not exist in the database.
-	
-	
 	error_log("TIS IS A NEW ~TIDLER posted rev was :  ".$_POST['revision']." and title is : ".$ntiddler['title']);
-	
-	
-	
-	
 	if( user_insertPrivilege(user_tiddlerPrivilegeOfUser($user,$ntiddler['tags'])) ) 
 	{
 		debug("Inserting New Tiddler...", "save");
@@ -85,15 +105,13 @@ if(db_tiddlers_mainSelectTitle($ntiddler['title']) || is_numeric($_POST['id']))
 		$ntiddler['created'] = $ntiddler['modified'];
 		$ntiddler['revision'] = 1;
 		unset($ntiddler['workspace_name']); 	// hack to remove the workspace being set twice. 
-		
 			if($id = tiddler_insert_new($ntiddler))
 			{
 				sendHeader(201);
 				echo $id;
-			
 			}
-		
-	}else{
+	}else
+	{
 		debug("Permissions denied to save.", "save");
 		sendHeader(400);
 	}
