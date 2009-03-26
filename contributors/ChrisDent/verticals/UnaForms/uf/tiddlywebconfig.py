624d05876eead372ca96e936e23b4cde4363ddba
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-26 13:58:11 +0000

    unaforms - get rid of the comitted store, we're going to build one using instance_tiddlers instead
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9292 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
+++ contributors/ChrisDent/verticals/UnaForms/uf/tiddlywebconfig.py
@@ -1,10 +1,15 @@
 config = {
         'system_plugins': ['former'],
         'twanager_plugins': ['former', 'lister'],
-# point to the recipe that contains the core needs, just for
-# bootstrapping and installation
         'instance_tiddlers': [
-            'http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/verticals/UnaForms/forms.recipe',
+            ('system', [
+                'http://svn.tiddlywiki.org/Trunk/association/adaptors/TiddlyWebAdaptor.js',
+                'http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js',
+                'http://svn.tiddlywiki.org/Trunk/association/plugins/TiddlyWebConfig.js'
+                ]),
+            ('formtools', [
+                'http://svn.tiddlywiki.org/Trunk/contributors/SaqImtiaz/verticals/InputExForms/inputEx-subrecipe.recipe',
+                ])
             ],
         'log_level': 'DEBUG',
         'css_uri': 'http://peermore.com/tiddlyweb.css',
