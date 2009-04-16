90f2e8fcd014ce64cf3032ffaf97d3e866993214
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-04-16 12:18:51 +0000

    ccTiddly - LDAP Plugin - sha1 var is not auto overridden
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9515 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/LDAP/index.php
+++ association/serversides/cctiddly/Trunk/plugins/LDAP/index.php
@@ -1,20 +1,24 @@
 <?php
+$m = new Plugin('LDAP Plugin','0.1','simonmcmanus.com');
 
-// include the addition header files. 
-// change sha1 setting
+
+// Tiddler Definitions : 
+$tiddler['title'] = "LDAP Settings";
+$tiddler['body'] = "config.macros.ccLogin.sha1 = false;";
+$tiddler['tags'] = "systemConfig";
 
 // LDAP Plugin Definitions
 
-$m = new Plugin('LDAP Plugin','0.1','simonmcmanus.com');
 $m->addEvent("preUserInclude", 'LDAP/files/functions.php');
+$m->addTiddler($tiddler);
 
 // LDAP Config Settings 
-$tiddlyCfg['users_required_in_db'] = 0;  // means users are not required in the db to login.
+$tiddlyCfg['users_required_in_db'] = 0;  // means users are not required in the db to login/
 $tiddlyCfg['pref']['ldap_enabled'] = 1;	
-$tiddlyCfg['pref']['ldap_username']	= "blah ";
+$tiddlyCfg['pref']['ldap_username']	= "balh";
 $tiddlyCfg['pref']['ldap_base_dn']	="blah";
-$tiddlyCfg['pref']['ldap_password'] = "pass";
-$tiddlyCfg['pref']['ldap_filter'] = "indentifier=";
+$tiddlyCfg['pref']['ldap_password'] = "password";
+$tiddlyCfg['pref']['ldap_filter'] = "accountname=";
 $tiddlyCfg['pref']['ldap_connection_string'] = "ldap://server.com:389";
 
 //
