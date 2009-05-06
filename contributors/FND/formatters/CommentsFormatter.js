0449c853f31fe935f02b23e4eb3a71c1679aef0c
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-06 19:35:10 +0000

    CommentsFormatter: fixed missing line break
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9626 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/formatters/CommentsFormatter.js
+++ contributors/FND/formatters/CommentsFormatter.js
@@ -2,7 +2,7 @@
 |''Name''|CommentsFormatter|
 |''Description''|formatter adding comments syntax|
 |''Author:''|FND|
-|''Version''|0.1.0|
+|''Version''|0.1.1|
 |''Status''|@@experimental@@|
 |''Source''|http://devpad.tiddlyspot.com/#CommentsFormatter|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
@@ -11,9 +11,11 @@
 !!Examples
 {{{
 lorem /# foo
+ipsum /# bar
 }}}
 <<<
 lorem /# foo
+ipsum /# bar
 <<<
 !Revision History
 !!v0.1 (2009-05-06)
@@ -28,6 +30,7 @@ formatters.push({
 	match: "\/#.*?\n",
 	handler: function(w) {
 		createTiddlyElement(w.output, "span", null, "comment", w.matchText);
+		createTiddlyElement(w.output, "br");
 	}
 });
 
