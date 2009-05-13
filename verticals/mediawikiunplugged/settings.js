62a0081a9b0a87e04db7205798368e6bf00df5a4
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-13 10:54:00 +0000

    mediawikiunplugged: forked from contributors/
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9647 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/mediawikiunplugged/settings.js
@@ -0,0 +1,27 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.2.0|
+|''MediaWiki Host''|<<option txtServerHost>>|
+|''MediaWiki sync interval (in minutes)''|<<option txtMediawikiSyncIterval>>|
+|''Elements to display per page''|<<option txtElementsPerPage>>|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = "backup";
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+config.maxTiddlerImportCount = 500;
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+
+merge(config.defaultCustomFields,{
+	wikiformat:'mediawiki'
+});
+
+//#config.displayStartupTime = true;
+/*}}}*/
