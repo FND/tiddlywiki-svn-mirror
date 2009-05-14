d92453a0deadc7b8f3520506612ee970822964e9
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-14 07:32:32 +0000

    mediawikiunplugged - counter handles dreamhost log archiving - take#2
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9713 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/mediawikiunplugged/counter/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="30" >
+<style>
+#count {
+        text-align: center;
+        width: 60%;
+        font-size: 20em;
+        color: #fff;
+        background-color: #0248C4;
+        margin: 20px auto 0 auto;
+        border: 5px solid black;
+}
+</style>
+</head>
+<body>
+<div id="count">
+<?php
+
+$logdir='/home/pauldowney/logs/mediawikiunplugged.com/http/';
+system('cat '.$logdir.'access.log '.$logdir.'access.log.20* | awk \'$7 == "/MediaWikiUnplugged.zip" { print $9 }\' | grep 200 | wc -l');
+
+?>
+<div>
+</body>
+</html>
+
