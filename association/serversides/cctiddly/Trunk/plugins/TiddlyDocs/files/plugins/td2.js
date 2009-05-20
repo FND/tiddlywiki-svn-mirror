fc38f1563b36c109bb4dc8053ac84f9b2e0ace98
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-20 20:53:35 +0000

    ccTiddly - TiddlyDocs - adding deleteZone as seperate macro
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9749 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/td2.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/td2.js
@@ -22,52 +22,96 @@ config.macros.tdoc2Outline.strip=function(s) {
 }
 
 config.macros.tdoc2Outline.handler=function(place,macroName,params,wikifier,paramString,tiddler){
-	config.options.txtOpenType = "inline";
 	config.macros.tdoc2Outline.refresh(place,macroName,params,wikifier,paramString,tiddler);
 };
 
-
-
 config.macros.tdoc2Outline.renderSpec = function(place, spec) {
-	var ul = createTiddlyElement(place, "ul", "ul"+(window.ulCount++), "page-list");
-   	var li = createTiddlyElement(ul, "li", "li"+window.liCount++, "clear-element page-item1 left");
-	var sectionDiv = createTiddlyElement(li, "div", "div"+window.divCount++, "sort-handle");	
+	var ul = createTiddlyElement(place, "ul", "ul"+(window.ulCount++), "toc");
+   	var li = createTiddlyElement(ul, "li", spec.title, "clear-element toc-item left");
+	if(store.getTiddler(spec.title)==null){
+			store.saveTiddler(spec.title, spec.title, "", config.options.txtUserName, new Date(),"task" );
+			autoSaveChanges(false, spec.title);
+	}else{
+		if(store.getTiddler(spec.title).fields.tt_status == "Complete")
+			var sectionClass = "completed";
+		else 
+			var sectionClass = "incomplete";
+	}
+	var sectionDiv = createTiddlyElement(li, "div", "div"+window.divCount++, "toc-sort-handle "+sectionClass);	
 	var heading = createTiddlyElement(sectionDiv, "div",  config.macros.tdoc2Outline.strip(spec.title)+"HeadingView", "sectionHeading");		
 	createTiddlyText(heading, spec.title);
 	$.each(spec.children, function() {
 		config.macros.tdoc2Outline.renderSpec(li, this);
 	});
-	
 }
 
 config.macros.tdoc2Outline.refresh=function(place,macroName,params,wikifier,paramString,tiddler){
 	window.ulCount=0;
 	window.liCount=0;
 	window.divCount=0;
-	config.macros.tdoc2Outline.renderSpec(place, testSpec);
+	config.macros.tdoc2Outline.renderSpec(place, testSpec);	
+	$("#ul0").NestedSortable({
+		accept: 'toc-item',
+		noNestingClass: "no-nesting",
+		helperclass: 'helper',
+		onStop: function() {
+			// HACK TO REMOVE THE HELPER CLASS WHICH SHOULD REMOVE ITSELF
+			$(".helper").remove();
+		},
+		onChange: function(serialized) {
+			displayMessage("saving");
+			$('#left-to-right-ser').html("This can be passed as parameter to a GET or POST request: "+ serialized[0].hash);
+		},
+		autoScroll: true,
+		handle: '.toc-sort-handle'
+	});
 	
+	$("#ul0 li").mouseup(function() {
+		if(config.options.chkOpenEditView==true)
+			story.displayTiddler(null, this.id, DEFAULT_EDIT_TEMPLATE);
+		else
+			story.displayTiddler(null, this.id);
+	});
 	
-		$("#ul0").NestedSortable({
-			accept: 'page-item1',
-					helperclass: 'helper',
-			handle: '.sort-handle ',
-			onChange: function(serialized) {
-				console.log(serialized, this)
-			}
-		});
-		$(".sectionHeading").hover(
-			function() {
-				$(this).addClass("draggableOn");
-			}, 
-			function() {
-				
-					console.log(this);
-				$(this).removeClass("draggableOn");
-				$("#sortHelper").remove();
-			}
-		);
+	$(".sectionHeading").hover(
+		function() {
+			$(this).addClass("draggableOn");
+		}, 
+		function() {
+			$(this).removeClass("draggableOn");
+		}
+	);		
 }	
 
+// DELETE ZONE
+
+config.macros.deleteZone = {};
+config.macros.deleteZone.handler = function() {
+	var div = createTiddlyElement(place, "div","deleteZone", "deleteZoneClass");
+	var binContents = store.getTiddlerText(window.activeDocument+"Bin");
+	if(binContents)
+		wikify("Bin \n"+binContents, div);
+	else
+		div.innerHTML = "<b>Recycle Bin</b><br /><br /> You have an empty bin.";
+	div.style.height = "auto";
+	$("#deleteZone").Droppable(
+	{
+		hoverclass : "deleteHelper",
+		accept:"toc-item",
+			ondrop:	function (drag) {
+				config.macros.deleteZone.delete(drag.id, testSpec);
+			}
+	});
+};
+
+config.macros.deleteZone.delete = function(id, spec) {
+	if(spec.title == id) {
+		console.log("delete me ", id);		
+	}
+	console.log(spec.children, id);
+	if(spec.children != undefined)	
+		config.macros.deleteZone.delete(id, spec.children);
+};
 //}}}
 
 function log() { if (console) console.log.apply(console, arguments); };
