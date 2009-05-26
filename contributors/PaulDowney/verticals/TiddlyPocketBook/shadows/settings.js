34db4918b42161662c3d9ddfe3506ec100677b36
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-26 21:50:34 +0000

    TiddlyPocketBook - moved to mm measurements, sadly Webkit scales this when printing
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9780 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyPocketBook/shadows/settings.js
+++ contributors/PaulDowney/verticals/TiddlyPocketBook/shadows/settings.js
@@ -3,8 +3,9 @@
 /*
  *  Theme
  */
-config.options.txtTheme = "DinkyBookTheme";
-//config.options.txtTheme = "PocketBookPrintTheme";
+//config.options.txtTheme = "PocketBookTheme";
+//config.options.txtTheme = "DinkyBookTheme";
+config.options.txtTheme = "PrintTheme";
 
 /*              
  *  Faster fading messages!
@@ -20,5 +21,13 @@ config.options.txtBackupFolder = "backups";
  *  Hide backstage
  */
 config.options.chkBackstage = false;
+
+/*
+ *  calculate a fixed width container for pages
+ */
+config.options.txtFlexiWidthContainer = '#displayArea';
+config.options.txtFlexiWidthBox = '#tiddler1 > .page:first';
+config.options.txtFlexiWidthMinBoxes = '1';
+config.options.txtFlexiWidthMaxBoxes = '4';
 
 //}}}
