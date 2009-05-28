887692bfe2e9ade853eade56277e6edd598276e2
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-28 22:09:40 +0000

    TiddlyPocketBook - added download ZIP
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9804 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
+++ contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
@@ -1,5 +1,16 @@
 
 scp index.html pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/
-ssh pauldowney@amp.dreamhost.com mkdir -p /home/pauldowney/tiddlypocketbook.com/books/psd/
 
-scp TiddlyWikiPocketBook.pdf pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/books/psd
+#ssh pauldowney@amp.dreamhost.com mkdir -p /home/pauldowney/tiddlypocketbook.com/books/psd/
+#scp TiddlyWikiPocketBook.pdf pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/books/psd
+
+(
+	cd ..
+	(
+	    echo TiddlyPocketBook/index.html
+	    echo TiddlyPocketBook/TiddlySaver.jar
+	    echo TiddlyPocketBook/backups
+	) | zip -@ TiddlyPocketBook/TiddlyPocketBook.zip
+)
+
+scp TiddlyPocketBook.zip pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/
