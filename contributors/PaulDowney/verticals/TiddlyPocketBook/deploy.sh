8b9a097ac83c7e8f180828b3dcd1efa47999b678
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-05-28 21:25:40 +0000

    TiddlyPocketBook - deploy PDF
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9803 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
+++ contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
@@ -1,1 +1,5 @@
+
 scp index.html pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/
+ssh pauldowney@amp.dreamhost.com mkdir -p /home/pauldowney/tiddlypocketbook.com/books/psd/
+
+scp TiddlyWikiPocketBook.pdf pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/books/psd
