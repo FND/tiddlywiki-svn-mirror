cf7030ae85ca8787508483ae0a19a30a8a1d8dcb
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-01 18:33:15 +0000

    ccTiddly - SkinnyTiddlers - systemConfig tiddlers now automatically loaded
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9815 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/files/removeBody.php
+++ association/serversides/cctiddly/Trunk/plugins/SkinnyTiddlers/files/removeBody.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
-$autoLoad = array("skinnyPlugin"=>"", "SiteTitle"=>"","yourSearch"=>"" , "SiteSubtitle"=>"", "MainMenu"=>"", "002_ccTiddlyPlugins"=>"", "ColorPalette"=>"", "GettingStarted"=>"", "serverSideSearchPlugin"=>""); // set the tiddlers which will not be lazily loaded
-
+//$autoLoad = array("titleTitle"=>""); // set the tiddlers which will not be lazily loaded
 
 $defaultTiddler = ($user['verified']) ? "DefaultTiddlers" : "AnonDefaultTiddlers"; // if the user is logged in get default tiddlers, if the are not logged in get the anonDefaultTiddler
 
@@ -11,24 +8,17 @@ $defaultTiddlers = getTiddler('AnonDefaultTiddlers', $tiddlyCfg['workspace_name'
 foreach(explode(" ", $defaultTiddlers['body']) as $tiddler)
 	$defaults[str_replace("]]", "", str_replace("[[", "", $tiddler))] = "";	 // for each default tiddler add them to the autoLoad array. 
 
-/*
 // GET systemConfig tags
 $systemConfigTiddlers = getTiddlersWithTags(array('systemConfig'), array());
 
-var_dump($systemConfigTiddlers);
-
-foreach($systemConfigTiddlers as $sysTiddler1)
-echo $sysTiddler1['title'];
-
+foreach($systemConfigTiddlers as $sysTiddler)
+{
+//	echo $sysTiddler1['title']."\n";
 	$autoLoad[$sysTiddler['title']] = "";
-
-//echo	$defaults[$sysTiddler['title']] = "";
-//$autoLoad = array_merge($systemConfigTiddlers, $autoLoad);
-*/
+}
 
 $autoLoad = array_merge($defaults, $autoLoad);
 
-//if(!in_array($t['title'], $autoLoad)) // remove the body of all tiddlers which should be skinily loaded. 
 if(!isset($autoLoad[$t['title']])) // using isset instead of in_array to speed things up.
 	$t['body'] = "";
 
