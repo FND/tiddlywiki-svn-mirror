81a4e1dfc2996dcd16b213546bf0421a6da9df15
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-02 19:23:15 +0000

    ccTiddly - TiddlyDocs - adding tdocsSettings.js which is doing to tiddler > section renaming
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9827 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsSettings.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsSettings.js
@@ -3,6 +3,5 @@ config.commands.editHtml={
 	text: "edit",
 	tooltip: "Edit this section"
 }
-merge(config.views.wikified,{
-	defaultText: "The section '%0' doesn't yet exist. Double-click to create it"
-}
+
+config.views.wikified.defaultText=  "The section '%0' doesn't yet exist. Double-click to create it";
