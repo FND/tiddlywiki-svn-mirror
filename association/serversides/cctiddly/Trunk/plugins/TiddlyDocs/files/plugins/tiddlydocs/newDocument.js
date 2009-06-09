6f8effb58d7b43843b86b9d3b5ce753de14a1b75
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-09 22:13:50 +0000

    ccTiddly - TiddlyDocs - newDocument now works albeit with 409 error messages
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9853 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/newDocument.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/newDocument.js
@@ -1,16 +1,22 @@
 config.macros.newDocument = {};
 config.macros.newDocument.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	
-	
 	var w = new Wizard();
 	var me = config.macros.newDocument;
 	w.createWizard(place,"create new docuemnt");
-	w.addStep(null, '<div><input/><a href="javascript:;" title="Create New Document " class="btn"><span><span>Create</span></span></a></div>');
+	w.addStep(null, '<input name="documentName" />');
+	w.setButtons([
+		{caption: 'create', tooltip: 'create new document', onClick: function()  { config.macros.newDocument.createDocumentOnClick(this, w);}
+	}]);
 };
 
-config.macros.newDocument.createDocument = function() {
-	
-	alert("s");
-	console.log(this, this.parentNode, arguments);
-	
+config.macros.newDocument.createDocumentOnClick = function(e, w) {
+	var docName = w.formElem.documentName.value;
+		if(store.tiddlerExists(docName)) {
+			alert(docName+"Already Exists");
+		}else{
+			store.saveTiddler(docName, docName, "[]", null, null, "document", config.defaultCustomFields);
+			autoSaveChanges(true, docName);
+		}
+		window.activeDocument = docName;
+		refreshAll();
 }
