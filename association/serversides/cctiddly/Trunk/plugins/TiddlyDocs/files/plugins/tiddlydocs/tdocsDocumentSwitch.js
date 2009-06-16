e9601cdc339dc74f6f2864416b8ca75dca089e66
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 08:43:20 +0000

    ccTiddly - TiddlyDocs - switching docs now shows the preview of the switched to document
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9874 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsDocumentSwitch.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsDocumentSwitch.js
@@ -5,14 +5,12 @@ config.macros.docSwitcher.handler = function(place,macroName,params,wikifier,par
 	for (var i=0; i < values.length; i++) {
 		options.push({'caption': values[i].title, 'name': values[i].title});				
 	}
-	
 	options.push({'caption': 'add new', 'name': 'new'});
 	var dropDown = createTiddlyDropDown(place,this.setDropDownMetaData,options,window.activeDocument);
 }
 
 config.macros.docSwitcher.setDropDownMetaData = function(ev) {
 	if(this[this.selectedIndex].value=='new'){
-		//create new document
 		story.displayTiddler(null, "Create New Document");
 		return false;
 	}
@@ -20,4 +18,5 @@ config.macros.docSwitcher.setDropDownMetaData = function(ev) {
 	var option = this[this.selectedIndex].value;
 	window.activeDocument = option;
 	refreshAll();
+	story.displayTiddler(null, option);
 }
