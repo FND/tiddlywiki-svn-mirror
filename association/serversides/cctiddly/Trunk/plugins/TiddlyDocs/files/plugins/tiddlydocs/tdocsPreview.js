00870ab8ad503beec314567a00179ab63f9ed22d
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 10:34:45 +0000

    ccTiddly - TiddlyDocs - ;abe;s nearly work correctly
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9878 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
@@ -3,22 +3,20 @@ config.macros.docPreview.handler = function(place,macroName,params,wikifier,para
 	var spec = $.parseJSON(store.getTiddlerText(window.activeDocument));
 	createTiddlyElement(place, "br");
 	createTiddlyElement(place, "br");
-	config.macros.docPreview.recurse(place, spec, 0, [0]);
+	config.macros.docPreview.recurse(place, spec, 0, []);
 }
 
 config.macros.docPreview.recurse = function(place, item, level, label) {
-
+	level++;
 	for(var e=0; e < item.length; e++) {
 		if(label[level] == undefined)
 			label[level] = 1;
 		else
 			label[level] ++;
-	
-//		var title = createTiddlyElement(place,"h"+level, null, null, label.join(".")+" : "+item[e].title);
-	var title = createTiddlyElement(place,"h"+level, null, null, level+" : "+item[e].title);
+		var title = createTiddlyElement(place,"h"+level, null, null, label.join(".").substr(1)+" : "+item[e].title);
 		wikify(store.getTiddlerText(item[e].title), place);
 		if(typeof item[e].children == "object") {
-			config.macros.docPreview.recurse(place, item[e].children, level++, label);
+			config.macros.docPreview.recurse(place, item[e].children, level, label);
 		}		
 	}
 }
