4bc776a5a56120841954f114cab3f986e61552a7
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 08:37:36 +0000

    ccTiddly - TiddlyDocs - correcting colour of links in print view
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9873 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
@@ -4,18 +4,14 @@ config.macros.docPreview.handler = function(place,macroName,params,wikifier,para
 	createTiddlyElement(place, "br");
 	createTiddlyElement(place, "br");
 	config.macros.docPreview.recurse(place, spec);
-
 }
 
-
 config.macros.docPreview.recurse = function(place, item) {
-//	console.log(item, item.children);
-		for(var e=0; e < item.length; e++) {
-			var title = createTiddlyElement(place,"h1", null, null, item[e].title);
-			wikify(store.getTiddlerText(item[e].title), place);
-			if(typeof item[e].children == "object") {
-				config.macros.docPreview.recurse(place, item[e].children);
-			}
-			
+	for(var e=0; e < item.length; e++) {
+		var title = createTiddlyElement(place,"h1", null, null, item[e].title);
+		wikify(store.getTiddlerText(item[e].title), place);
+		if(typeof item[e].children == "object") {
+			config.macros.docPreview.recurse(place, item[e].children);
 		}
+	}
 }
