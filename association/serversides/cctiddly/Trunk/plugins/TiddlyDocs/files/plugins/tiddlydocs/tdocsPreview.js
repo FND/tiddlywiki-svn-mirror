cccea7d4b4d1dbdd875ff96fda5e2f0ff8702db5
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 07:55:54 +0000

    ccTiddly - TiddlyDocs - doc preview now work without needing to create a temp tiddler;
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9871 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
@@ -1,16 +1,10 @@
 config.macros.docPreview = {};
 config.macros.docPreview.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 	var spec = $.parseJSON(store.getTiddlerText(window.activeDocument));
-	var htmlStack = [];
-	htmlStack.push("<html><body>");
+	createTiddlyElement(place, "br");
+	createTiddlyElement(place, "br");
 	for(var t=0; t < spec.length; t++) {
-		htmlStack.push("<h1>"+spec[t].title+"</h1>");
-		htmlStack.push(wikifyStatic(store.getTiddlerText(spec[t].title)));
+		var title = createTiddlyElement(place,"h1", null, null, spec[t].title);
+		wikify(store.getTiddlerText(spec[t].title), place);
 	}
-	htmlStack.push("</body></html>");
-	var htmlString = htmlStack.join("\n");
-	newDate = new Date();
-	var name = "Please wait..";
-	store.saveTiddler("Document Preview", "Document Preview", htmlString, config.options.txtUserName, newDate,"",config.defaultCustomFields);
-	story.displayTiddler(null, "Document Preview");
 }
