d5fd3c5e5b68e1f545d8e2f44ccf07a96030f276
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 08:30:56 +0000

    ccTiddly - TiddlyDocs - doc preview now work works recursively
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9872 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
@@ -3,8 +3,19 @@ config.macros.docPreview.handler = function(place,macroName,params,wikifier,para
 	var spec = $.parseJSON(store.getTiddlerText(window.activeDocument));
 	createTiddlyElement(place, "br");
 	createTiddlyElement(place, "br");
-	for(var t=0; t < spec.length; t++) {
-		var title = createTiddlyElement(place,"h1", null, null, spec[t].title);
-		wikify(store.getTiddlerText(spec[t].title), place);
-	}
+	config.macros.docPreview.recurse(place, spec);
+
+}
+
+
+config.macros.docPreview.recurse = function(place, item) {
+//	console.log(item, item.children);
+		for(var e=0; e < item.length; e++) {
+			var title = createTiddlyElement(place,"h1", null, null, item[e].title);
+			wikify(store.getTiddlerText(item[e].title), place);
+			if(typeof item[e].children == "object") {
+				config.macros.docPreview.recurse(place, item[e].children);
+			}
+			
+		}
 }
