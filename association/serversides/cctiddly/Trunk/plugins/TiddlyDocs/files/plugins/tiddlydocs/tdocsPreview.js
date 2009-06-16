66a9e618884e62b3256036130f7913c25fc06140
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-16 08:48:15 +0000

    ccTiddly - TiddlyDocs - levels now work iwth different heading in the preview
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9875 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/tdocsPreview.js
@@ -3,15 +3,16 @@ config.macros.docPreview.handler = function(place,macroName,params,wikifier,para
 	var spec = $.parseJSON(store.getTiddlerText(window.activeDocument));
 	createTiddlyElement(place, "br");
 	createTiddlyElement(place, "br");
-	config.macros.docPreview.recurse(place, spec);
+	config.macros.docPreview.recurse(place, spec, 0);
 }
 
-config.macros.docPreview.recurse = function(place, item) {
+config.macros.docPreview.recurse = function(place, item, level) {
+	level ++;
 	for(var e=0; e < item.length; e++) {
-		var title = createTiddlyElement(place,"h1", null, null, item[e].title);
+		var title = createTiddlyElement(place,"h"+level, null, null, item[e].title);
 		wikify(store.getTiddlerText(item[e].title), place);
 		if(typeof item[e].children == "object") {
-			config.macros.docPreview.recurse(place, item[e].children);
+			config.macros.docPreview.recurse(place, item[e].children, level);
 		}
 	}
 }
