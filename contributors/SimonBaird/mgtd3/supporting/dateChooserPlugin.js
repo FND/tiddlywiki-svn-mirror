43a52830ccc744a178c0a1c4d83b51edbbdee549
simon <simon@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-22 12:57:25 +0000

    mgtd3: purging dos style line breaks. i think they are giving me a lot of headaches. sorry about giant diff
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9902 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonBaird/mgtd3/supporting/dateChooserPlugin.js
+++ contributors/SimonBaird/mgtd3/supporting/dateChooserPlugin.js
@@ -1,17 +1,15 @@
 //{{{
-//For demonstration purposes only
-// A port of a plugin by Simon Baird to use DatePicker library
 //requires DatePicker: http://svn.tiddlywiki.org/Trunk/contributors/SaqImtiaz/libraries/DatePicker.js
 
-// some additions by Simon Baird
+if (DatePicker) {
 
-if (DatePicker){
-    merge(config.macros, {
-        calendarPopup: {
-            handler: function(place,macroName,params,wikifier,paramString,tiddler) {
-                var dateBox = createTiddlyButton(place,params[0],params[1]);            
-                dateBox.style.cursor='pointer';
-                DatePicker.create(dateBox,(new Date()),function(el,objDate) {
+	merge(config.macros, {
+
+		calendarPopup: {
+			handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+				var dateBox = createTiddlyButton(place,params[0],params[1]);            
+				dateBox.style.cursor='pointer';
+				DatePicker.create(dateBox,(new Date()),function(el,objDate) {
 					// mostly copy/pasted from NewSavedTiddler. refactor please
 					var title = prompt("Enter name for new Tickler","");
 					if (title) {
@@ -25,43 +23,43 @@ if (DatePicker){
 			}
 		},
 
-        dateChooser: {
-            handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+		dateChooser: {
+			handler: function(place,macroName,params,wikifier,paramString,tiddler) {
     
-                var useTiddler = tiddler;
-                if (params[0])
-                    useTiddler = store.fetchTiddler(params[0]);
+				var useTiddler = tiddler;
+				if (params[0])
+					useTiddler = store.fetchTiddler(params[0]);
     
-                var curVal = useTiddler.fields['mgtd_date'] || undefined;
-                var startDate = curVal ? Date.convertFromYYYYMMDDHHMM(curVal) : null;
+				var curVal = useTiddler.fields['mgtd_date'] || undefined;
+				var startDate = curVal ? Date.convertFromYYYYMMDDHHMM(curVal) : null;
 
-                var dateBox = createTiddlyElement(place,'input',null,'dateBox');            
+				var dateBox = createTiddlyElement(place,'input',null,'dateBox');            
 
-                var dateFormat = config.mGTD.getOptTxt('ticklerdateformat');
+				var dateFormat = config.mGTD.getOptTxt('ticklerdateformat');
 				var defaultDateFormat = 'ddd, DD-mmm-YY';
 				if (!dateFormat) {
 					// makes it nicer for upgraders who don't have a format set
 					dateFormat = defaultDateFormat;
 					config.mGTD.setOptTxt('ticklerdateformat', defaultDateFormat);
 				}
-                dateBox.value = startDate ? startDate.formatString(dateFormat) : '(set date)';
+				dateBox.value = startDate ? startDate.formatString(dateFormat) : '(set date)';
     
-                var callback = function(el,objDate){
-                    el.value = objDate.formatString(dateFormat);
+				var callback = function(el,objDate){
+					el.value = objDate.formatString(dateFormat);
 					store.setValue(useTiddler, 'mgtd_date',objDate.convertToYYYYMMDDHHMM());
-                }
-                DatePicker.create(dateBox,startDate,callback);
-            }
-        },
+				}
+				DatePicker.create(dateBox,startDate,callback);
+			}
+		},
 
 		addDay: {
-			label:   {addDay:"+d", addWeek:"+w",  addMonth:"+m",   addYear:"+y"},
-			tooltip: {addDay:"day",addWeek:"week",addMonth:"month",addYear:"year"},
-            handler: function(place,macroName,params,wikifier,paramString,tiddler) {
-                var useTiddler = tiddler;
-                if (params[0]) useTiddler = store.fetchTiddler(params[0]);
-                var curVal = useTiddler.fields['mgtd_date'] || undefined;
-                var curDate = curVal ? Date.convertFromYYYYMMDDHHMM(curVal) : new Date();    
+			label:   {addDay:"+d",  addWeek:"+w",   addMonth:"+m",    addYear:"+y"   },
+			tooltip: {addDay:"day", addWeek:"week", addMonth:"month", addYear:"year" },
+			handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+				var useTiddler = tiddler;
+				if (params[0]) useTiddler = store.fetchTiddler(params[0]);
+				var curVal = useTiddler.fields['mgtd_date'] || undefined;
+				var curDate = curVal ? Date.convertFromYYYYMMDDHHMM(curVal) : new Date();    
 				// ensure ticklers don't have minutes/hours since new Date() has minutes/hours
 				curDate.setHours(0);
 				curDate.setMinutes(0);
@@ -75,10 +73,12 @@ if (DatePicker){
 
 			}
 		}
-    });
-}
+	});
 
-config.macros.addWeek  = config.macros.addDay;
-config.macros.addMonth = config.macros.addDay;
-config.macros.addYear  = config.macros.addDay;
+	config.macros.addWeek  = config.macros.addDay;
+	config.macros.addMonth = config.macros.addDay;
+	config.macros.addYear  = config.macros.addDay;
+
+} // if (DatePicker)
+
 //}}}
