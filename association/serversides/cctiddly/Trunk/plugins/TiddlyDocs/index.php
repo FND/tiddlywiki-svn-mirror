4638dfa8c8fdaefcf70d949ee0051a63f2f30da8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-23 19:41:21 +0000

    ccTiddly - TiddlyDocs - adding permissions to the tiddlydocs plugin definitions
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9914 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/index.php
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/index.php
@@ -2,20 +2,18 @@
 
 // Set up Permissions : 
 
-$tiddlyCfg['privilege_misc']['undefined_privilege'] = "D";		//defined what should undefined (U) be treated 
-$tiddlyCfg['privilege_misc']['default_privilege'] = "AUUU";		//default privilege for all group and tags
+$tiddlyCfg['privilege_misc']['undefined_privilege'] = "D";		
+$tiddlyCfg['privilege_misc']['default_privilege'] = "AUUU";		
 $tiddlyCfg['privilege_misc']['group_default_privilege']['anonymous'] = "DDDD";
-$tiddlyCfg['privilege_misc']['group_default_privilege']['user'] = "AAAA";
+$tiddlyCfg['privilege_misc']['group_default_privilege']['user'] = "AAAA"; // THIS MAY NEED TO BE CHANGED
 
 // Tag based permissions : 
-
 
 $tiddlyCfg['privilege']['admin']['systemConfig'] = "AAAA";
 $tiddlyCfg['privilege']['non_admin']['systemConfig'] = "ADDD";
 
 $tiddlyCfg['privilege']['admin']['document'] = "AAAA";
-$tiddlyCfg['privilege']['non_admin']['document'] = "AAAA";
-
+$tiddlyCfg['privilege']['non_admin']['document'] = "AUAA";
 
 $p = new Plugin('Tiddler Tree','0.1','simonmcmanus.com');
 $p->addTiddlersFolder(getcwd().'/plugins/TiddlyDocs/files');
