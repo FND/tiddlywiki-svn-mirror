9e13dd753850e3ec95df532f7c6f643b3089d179
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-25 15:19:30 +0000

    ccTiddly - TiddlyDocs - fixing qatch
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9938 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/QATCH_displayDefaultTiddlers.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/QATCH_displayDefaultTiddlers.js
@@ -3,5 +3,22 @@
 // also requires overide of restart. 
 
 Story.prototype.displayDefaultTiddlers = function(){
-	story.displayTiddler(DEFAULT_EDIT_TEMPLATE, $("#ul0 :first-child")[0].id);
+	if($("#ul0 :first-child").length !=0){
+		story.displayTiddler(DEFAULT_EDIT_TEMPLATE, $("#ul0 :first-child")[0].id);
+	}else{
+	 	var tiddlers="";
+		if(isLoggedIn()){        
+			var url = window.location;        
+			url = url.toString();        
+			var bits = url.split('#');        
+			if(bits.length == 1){            
+				tiddlers = store.filterTiddlers(store.getTiddlerText("DefaultTiddlers"));            
+				story.displayTiddlers(null, tiddlers);
+			}
+		
+		}else{         
+			tiddlers=store.filterTiddlers(store.getTiddlerText("AnonDefaultTiddlers"));        
+			story.displayTiddlers(null, tiddlers);   
+		}   
+	} 
 };
