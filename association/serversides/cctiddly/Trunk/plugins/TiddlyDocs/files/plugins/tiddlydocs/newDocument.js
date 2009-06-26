e861c231cc42c8f9c271414e30ac3eb3a47079d6
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-26 10:43:15 +0000

    ccTiddly - TiddlyDocs - admin can now delete docuemnts
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9952 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/newDocument.js
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/tiddlydocs/newDocument.js
@@ -2,7 +2,7 @@ config.macros.newDocument = {};
 config.macros.newDocument.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 	var w = new Wizard();
 	var me = config.macros.newDocument;
-	w.createWizard(place,"create new docuemnt");
+	w.createWizard(place,"create new document");
 	w.addStep(null, '<input name="documentName" />');
 	w.setButtons([
 		{caption: 'create', tooltip: 'create new document', onClick: function()  { config.macros.newDocument.createDocumentOnClick(this, w);}
