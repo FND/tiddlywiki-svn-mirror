b6be258d4a02ec88eb9ea9e286543a6203cb6ab0
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-06-26 14:01:47 +0000

    ccTiddly - TiddlyDocs
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9959 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/cctiddly/Trunk/plugins/TiddlyDocs/files/plugins/issuesPlugin.js
@@ -0,0 +1,13 @@
+config.macros.issuesPlugin = {};
+config.macros.issuesPlugin.handler = function(place,macroName,params)
+{
+	tiddlers = store.getTaggedTiddlers('issue');
+	if(tiddlers.length < 1){
+		wikify('there are no know issues.', place);		
+	}else {
+		for(var t=0; t < tiddlers.length; t++) {
+			console.log(tiddlers[t].title);
+			wikify('[['+tiddlers[t].title+']]\n', place);
+		}	
+	}	
+};
