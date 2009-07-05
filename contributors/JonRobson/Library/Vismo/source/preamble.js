84441b7da77587d7a3d40bb74c97d8ad7e1e152b
jdlrobson <jdlrobson@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-05 12:15:25 +0000

    [vismo] slight reorg
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9967 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/JonRobson/Library/Vismo/source/preamble.js
@@ -0,0 +1,16 @@
+/***
+|''Name:''|VismoLibrary|
+|''Description:''|An opensource library of javascript code designed to create clickable graphics in canvas with VML alternative for Internet Explorer browsers. The purpose of this is to provide hackable, extendable graphics based plugins without being locked in to consumer products such as Flash.|
+|''Author:''|JonRobson (http://www.jonrobson.me.uk/Vismo)|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/JonRobson/Library/Vismo/source/|
+|''Version:''|0.8 |
+|''Dependencies:''| Requires jQuery|
+|''Comments:''|Please raise questions and make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''Usage:''|
+Not much good on it's own - it provides some nice functions to create graphicsy plugins.
+Currently provides horsepower to the following plugins amongst others:
+GeoTiddlyWiki (http://www.jonrobson.me.uk/workspaces/tiddlers/GeoTiddlyWiki/), ImageTaggingPlugin, TiddlyTagMindMap (http://tiddlytagmindmap.tiddlyspot.com)
+!Wouldn't have been possible without..
+http://spatialreference.org/ref/sr-org/google-projection/ for help with google projection hack
+***/
