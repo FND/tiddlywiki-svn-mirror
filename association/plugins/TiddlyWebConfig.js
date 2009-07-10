38e93170ccdc723b4858df9e52417e33a6103db2
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-10 10:16:38 +0000

    TiddlyWebConfig: disabled selected toolbar commands for unauthorized users
    TiddlyWeb provides information on save and delete permissions.
    This does not apply to newly-created tiddlers.
    
    Simply hiding the commands seems a bit crude; there's certainly room for
    usability enhancements here.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9972 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/plugins/TiddlyWebConfig.js
+++ association/plugins/TiddlyWebConfig.js
@@ -2,7 +2,7 @@
 |''Name''|TiddlyWebConfig|
 |''Description''|configuration settings for TiddlyWeb|
 |''Author''|FND|
-|''Version''|0.4.0|
+|''Version''|0.5.0|
 |''Status''|stable|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/plugins/TiddlyWebConfig.js|
 |''License''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
@@ -17,6 +17,8 @@
 * sync username with server
 !!v0.4 (2009-05-23)
 * cache list of available login challengers
+!!v0.5 (2009-07-10)
+* disabled save and delete toolbar commands for unauthorized users
 !Code
 ***/
 //{{{
@@ -42,6 +44,25 @@ config.defaultCustomFields = {
 	"server.type": tiddler.getServerType(),
 	"server.host": adaptor.fullHostName(host),
 	"server.workspace": workspace
+};
+
+// disable selected toolbar commands for unauthorized users
+
+config.commands.saveTiddler.isEnabled = function(tiddler) {
+	return hasPermission("write", tiddler);
+};
+
+config.commands.deleteTiddler.isEnabled = function(tiddler) {
+	return hasPermission("delete", tiddler);
+};
+
+var hasPermission = function(type, tiddler) {
+	var perms = tiddler.fields["server.permissions"];
+	if(perms) {
+		return perms.split(", ").contains(type);
+	} else {
+		return true;
+	}
 };
 
 // retrieve server info
