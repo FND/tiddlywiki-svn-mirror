5ee9cf0a907a94f17f42a510a394c6fa02b4cf62
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-16 14:14:26 +0000

    [wikidata] PEP8-related adjustments
    git-svn-id: http://svn.tiddlywiki.org/Trunk@9993 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/wikidataSerializer.py
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/wikidataSerializer.py
@@ -1,36 +1,37 @@
+import urllib
+import logging
+
+import jinja2
+
 from jinja2 import Environment, FileSystemLoader
- 
-import logging, urllib, jinja2
- 
+
 from tiddlyweb.serializations import SerializationInterface
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.web.util import encode_name
- 
- 
+
+
 EXTENSION_TYPES = { 'wd': 'text/x-wd-html' }
 SERIALIZERS = {
-        'text/x-wd-html': ['wikidataSerializer', 'text/html; charset=UTF-8']
-        }
+    'text/x-wd-html': ['wikidataSerializer', 'text/html; charset=UTF-8']
+}
+
 
 class Serialization(SerializationInterface):
- 
+
     def __init__(self, environ=None):
         if environ is None:
             environ = {}
         self.environ = environ
         self._init()
- 
+
     def _init(self):
         template_env = Environment(loader=FileSystemLoader('templates'))
         self.template = template_env.get_template('collection.html')
- 
- 
+
     def list_tiddlers(self, bag):
         tiddlers = bag.list_tiddlers()
         print tiddlers[0].fields
         return self.template.render(tiddlers=tiddlers)
-
-
 
     def tiddler_as(self, tiddler):
         template_env = Environment(loader=FileSystemLoader('templates'))
@@ -38,8 +39,8 @@ class Serialization(SerializationInterface):
         bag = Bag('tmpbag', tmpbag=True)
         bag.add_tiddler(tiddler)
         return self.template.render(tiddler=tiddler)
- 
- 
+
+
 def init(config):
     config['extension_types'].update(EXTENSION_TYPES)
     config['serializers'].update(SERIALIZERS)
