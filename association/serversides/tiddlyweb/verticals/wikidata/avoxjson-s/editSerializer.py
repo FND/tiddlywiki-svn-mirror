9eae42da6b4d40b75ad6d5552ad4932ada123331
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-16 18:00:16 +0000

    wikidata
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10029 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/editSerializer.py
@@ -0,0 +1,51 @@
+import urllib
+import logging
+
+import jinja2
+
+from jinja2 import Environment, FunctionLoader
+
+from tiddlyweb.serializations import SerializationInterface
+from tiddlyweb.model.bag import Bag
+
+
+
+EXTENSION_TYPES = { 'edit': 'text/x-edit-html' }
+SERIALIZERS = {
+    'text/x-edit-html': ['editSerializer', 'text/html; charset=UTF-8']
+}
+
+templates_dir = 'templates'
+
+
+def init(config):
+    config['extension_types'].update(EXTENSION_TYPES)
+    config['serializers'].update(SERIALIZERS)
+
+
+class Serialization(SerializationInterface):
+
+    def __init__(self, environ=None):
+        if environ is None:
+            environ = {}
+        self.environ = environ
+        self.template_env = Environment(loader=FunctionLoader(_generate_template))
+
+    def tiddler_as(self, tiddler):
+        bag = Bag('tmpbag', tmpbag=True)
+        bag.add_tiddler(tiddler)
+        template = self.template_env.get_template("editCompany.html")
+        return template.render(tiddler=tiddler)
+
+
+def _generate_template(name):
+    components = ["header.html", name, "footer.html"]
+    return "%s\n%s\n%s" % tuple(_get_template(name) for name in components)
+
+
+def _get_template(name):
+    filepath = "%s/%s" % (templates_dir, name)
+    f = open(filepath)
+    contents = f.read()
+    f.close() # XXX: not required?
+    return contents
