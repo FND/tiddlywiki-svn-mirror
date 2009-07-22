0f6ac7cd3536cc482ce9cdc2e66c2db967365bba
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-22 01:54:04 +0000

    This is the steel thread for Scrumptious trails.
    Right now, trails works, all running on the server-side with standard WSGI processing. There is zero Javascript here. (The player works using iframe and the "target" attribute). The trail data structure is simply a list of URLs for now. See the URLs map at the bottom
    
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10064 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/verticals/scrumptious/tiddlywebconfig.py
+++ contributors/MichaelMahemoff/verticals/scrumptious/tiddlywebconfig.py
@@ -1,10 +1,13 @@
 import os
+import json
 config = {
+    'debug_level': 'DEBUG',
     'secret': 'bc483adf6e27220b50de493ab0a6550ee4fd943c',
-    'system_plugins': ['static', 'methodhack', 'pathinfohack'],
+    'system_plugins': ['static', 'methodhack', 'pathinfohack', 'trails'],
+    'twanager_plugins': ['wserver'],
     'server_store': ['simpletext', {'store_root': 'store'}],
     'static_dir': 'static',
-    'server_prefix': '/comments',
+    'server_prefix': '',
     'server_host':{
         'scheme': 'http',
         'host': 'comments.boz',
---
+++ contributors/MichaelMahemoff/verticals/scrumptious/trails.py
@@ -0,0 +1,72 @@
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.store import NoUserError, NoRecipeError, NoBagError, NoTiddlerError
+from tiddlywebplugins import do_html, entitle
+from jinja2 import Environment, FileSystemLoader
+from urllib import unquote_plus
+from string import whitespace
+import re
+
+import simplejson as json
+
+def make_trail_tiddler(environ):
+  trail_id=environ['wsgiorg.routing_args'][1]['trail_id']
+  trail_owner=environ['wsgiorg.routing_args'][1]['trail_owner']
+  tiddler = Tiddler(title=trail_id)
+  tiddler.bag = 'trails-'+trail_owner
+  tiddler.owner = trail_owner
+  try:
+    environ['tiddlyweb.store'].get(tiddler)
+    return tiddler
+  except NoTiddlerError:
+    return None
+
+@do_html()
+def trail_player(environ, start_response):
+  tiddler = make_trail_tiddler(environ)
+  if (not tiddler):
+    return ['no tiddlers']
+  trail = json.loads(tiddler.text)
+  print 'playing trail', trail
+  template = Environment(loader=FileSystemLoader('templates')).get_template("player.html")
+  return template.generate(tiddler=tiddler, trail=trail)
+  # TODO say there are no bags
+
+
+@do_html()
+def trail_editor(environ, start_response):
+  tiddler = make_trail_tiddler(environ)
+  if (not tiddler):
+    tiddler = Tiddler(title=environ['wsgiorg.routing_args'][1]['trail_id'], text='{"resources": []}')
+  trail = json.loads(tiddler.text)
+  template = Environment(loader=FileSystemLoader('templates')).get_template("editor.html")
+  return template.generate(tiddler=tiddler, trail=trail)
+
+def trail_updater(environ, start_response):
+  # http://is.gd/1GTCG
+  print 'hello'
+  tiddler = make_trail_tiddler(environ)
+  trail = dict()
+  print 'trail'
+  print 'name', environ['tiddlyweb.query']['name']
+  trail['name'] = environ['tiddlyweb.query']['name'][0]
+  # trail['resources'] = environ['tiddlyweb.query']['resources'][0].split(whitespace)
+  trail['resources'] = re.split('\s+',environ['tiddlyweb.query']['resources'][0])
+  print 'trail json', json.dumps(trail)
+  # edit_url=environ['HTTP_HOST']+'/trails/'+ environ['wsgiorg.routing_args']['owner']+ '/'+ trail['name']
+
+  tiddler = make_trail_tiddler(environ)
+  tiddler.text = json.dumps(trail)
+
+  environ['tiddlyweb.store'].put(tiddler)
+
+  edit_url="http://"+environ['HTTP_HOST']+"/trails/"+environ['wsgiorg.routing_args'][1]['trail_owner']+'/'+trail['name']+'.editor'
+  print 'EDIT', edit_url
+  start_response("303 See Other", [('Location', edit_url)]);
+  return ['---']
+
+def init(config):
+  config['selector'].add('/trails/{trail_owner}/{trail_id}.player', GET=trail_player)
+  config['selector'].add('/trails/{trail_owner}/{trail_id}.editor', GET=trail_editor)
+  config['selector'].add('/trails/{trail_owner}/{trail_id}', PUT=trail_updater)
+  # config['selector'].add('/trails/{trail_id}.player', GET=trail_player)
