4b1e1f0ef9d5a3a46884ccbb68442dcbb457a79c
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-07-31 11:10:24 +0000

    scrumptious - remove redundant public dir
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10128 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/verticals/scrumptious/public/injectionTest.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  var script = document.createElement("script");
-  script.src = "bookmarklet.js";
-  script.type = "text/javascript";
-  document.body.appendChild(script);
-}
