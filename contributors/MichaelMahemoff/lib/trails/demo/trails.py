e3a2c8ddbdbe940863df39f7be1751149af808e6
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-06 16:43:39 +0000

    trails - adding main plugin file, and two files for start and end that aren't currently used (as the dummy start and end nodes are now invisible/unclickable/unreachable)
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10223 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MichaelMahemoff/lib/trails/demo/trails.py
@@ -0,0 +1,78 @@
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.store import NoUserError, NoRecipeError, NoBagError, NoTiddlerError
+from tiddlyweb.serializer import Serializer
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlywebplugins import do_html, entitle
+from jinja2 import Environment, FileSystemLoader
+from urllib import unquote_plus
+from string import whitespace
+from trail import Serialization as TrailSerialization
+import re
+
+import simplejson as json
+
+def make_trail_tiddler(environ):
+  trail_id=environ['wsgiorg.routing_args'][1]['trail_id']
+  trail_owner=environ['wsgiorg.routing_args'][1]['trail_owner']
+  tiddler = Tiddler(title=trail_id)
+  tiddler.bag = 'trails-'+trail_owner
+  tiddler.owner = trail_owner
+  try:
+    environ['tiddlyweb.store'].get(tiddler)
+    return tiddler
+  except NoTiddlerError:
+    return None
+
+@do_html()
+def trail_player(environ, start_response):
+  tiddler = make_trail_tiddler(environ)
+  if (not tiddler):
+    return ['no tiddlers']
+  trail = json.loads(tiddler.text)
+  template = Environment(loader=FileSystemLoader('templates')).get_template("trail-player.html")
+  return template.generate(tiddler=tiddler, trail=trail, server_prefix=environ['tiddlyweb.config']['server_prefix'])
+  # TODO say there are no bags
+
+
+@do_html()
+def trail_editor(environ, start_response):
+  tiddler = make_trail_tiddler(environ)
+  if (not tiddler):
+    tiddler = Tiddler(title=environ['wsgiorg.routing_args'][1]['trail_id'], text='{"resources": []}')
+  trail = json.loads(tiddler.text)
+  template = Environment(loader=FileSystemLoader('templates')).get_template("trail-editor.html")
+  return template.generate(tiddler=tiddler, trail=trail, server_prefix=environ['tiddlyweb.config']['server_prefix'])
+
+def trail_updater(environ, start_response):
+  # http://is.gd/1GTCG
+  tiddler = make_trail_tiddler(environ)
+  trail = dict()
+  print '*** query', environ['tiddlyweb.query']
+  trail['name'] = environ['tiddlyweb.query']['name'][0]
+  # trail['resources'] = environ['tiddlyweb.query']['resources'][0].split(whitespace)
+  trail['resources'] = re.split('\s+',environ['tiddlyweb.query']['resources'][0].strip())
+  # edit_url=environ['HTTP_HOST']+'/trails/'+ environ['wsgiorg.routing_args']['owner']+ '/'+ trail['name']
+
+  tiddler = make_trail_tiddler(environ)
+  tiddler.text = json.dumps(trail)
+
+  environ['tiddlyweb.store'].put(tiddler)
+
+  edit_url="http://"+environ['HTTP_HOST']+environ['tiddlyweb.config']['server_prefix']+"/trails/"+environ['wsgiorg.routing_args'][1]['trail_owner']+'/'+trail['name']+'.editor'
+  start_response("303 See Other", [('Location', edit_url)]);
+  return ['---']
+
+@do_html()
+def serialize_trail(environ, start_response):
+  tiddler = make_trail_tiddler(environ)
+  return [TrailSerialization(environ).tiddler_as(tiddler)]
+
+def init(config):
+  config['selector'].add('/trails/{trail_owner}/{trail_id}.player', GET=trail_player)
+  config['selector'].add('/trails/{trail_owner}/{trail_id}.editor', GET=trail_editor)
+  config['selector'].add('/trails/{trail_owner}/{trail_id}', POST=trail_updater)
+  # config['selector'].add('/trails/{trail_id}.player', GET=trail_player)
+  # config['selector'].add('/trails/{trail_owner}/{trail_id}.trail', GET=serialize_trail)
+
+
