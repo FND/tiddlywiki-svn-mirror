8ff21bd87937d2a1d7b61390775d63a2856ab339
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-06 17:32:15 +0000

    tiddlydocs - adding tiddlyweb directories
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10224 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/SimonMcManus/tiddlyweb/tiddlydocs/setup.sh
@@ -0,0 +1,3 @@
+
+
+twanager --load tiddlywebwiki.config from_svn tdocs http://svn.tiddlywiki.org/Trunk/verticals/tiddlydocs/index.html.recipe
---
+++ contributors/SimonMcManus/tiddlyweb/tiddlydocs/tiddlywebconfig.py
@@ -0,0 +1,7 @@
+# A basic configuration.
+# Run 'pydoc tiddlyweb.config' for details on configuration items.
+config = {
+    'system_plugins': ['tiddlywebwiki.plugin', 'atomplugin'],
+    'secret': '50b2869542e4e2fc539146e0fe86acb37bb0e5a1',
+    'twanager_plugins': ['tiddlywebwiki.plugin']
+}
