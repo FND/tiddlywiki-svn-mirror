b0d89b3d42d25b6a1b914f10aafc9ece8145108e
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-06 15:40:05 +0000

    trails - made carousel next and prev select next and prev link, as user would expect
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10220 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
+++ contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
@@ -1,47 +1,31 @@
-// var currentSlotIndex = 0;
-$(function() {
-  $('#resources').jcarousel({visible: 3});
-  $("#closer").click(function() { document.location.href = $(".selected").attr("href"); });
-  $('#resources a').click(function() {
-    console.log("hello click");
-    $('#resourceView').attr("src", $(this).attr("href"));
+var currentIndex = 1;
+
+function select(index) {
+    var resource = $($("#resources a")[index]);
+    console.log("select", index, "res", resource);
+    $('#resourceView').attr("src", resource.attr("href"));
     $('#resources a').removeClass("selected");
-    $(this).addClass("selected");
-  });
-  var links = $('#resources a');
-  if (links.length) $(links[0]).click();
-  /*
-  setupSlots();
-  selectResource(0);
-  */
+    resource.addClass("selected");
+}
+
+function selectNewResource(na,na,index,action) {
+     if (action=="next") {
+        if (index!=$("#resources a").length) currentIndex++;
+     } else if (action=="prev") {
+        currentIndex--;
+     }
+     select(currentIndex);
+}
+
+$(function() {
+
+  $('#resources a').each(function(i) { $(this).data("index", i); });
+
+  $('#resources').jcarousel({visible: 3, scroll: 1, offset: 1, itemFirstInCallback: selectNewResource})
+  $('#resources a').click(function() { select($(this).data("index")); });
+
+  $("#closer").click(function() { document.location.href = $(".selected").attr("href"); });
+
 });
-
-function setupSlots() {
-  var resources = $(".resource");
-  if (!resources.length) return;
-  var slots = $("<div id='slots'></div>").appendTo($("#slotsWrapper"));
-  resources.each(function(i) {
-    // var slot = $(this).clone().addClass("slot").append("<div class='nav'></div>").data("index", i).appendTo(slots);
-    var slot = $("<a class='slot'></a>")
-      .attr("href", $(this).attr("href"))
-      .attr("target", $(this).attr("target"))
-      .append("<div class='nav'></div>")
-      .append($("<span>"+$(this).html()+"</span>"))
-      .appendTo(slots);
-    slot.click(function() { selectResource(i); });
-  });
-}
-
-function selectResource(slotIndex) {
-  $("#slots").css("marginLeft", slotIndex ? "0" : "270px"); // workaround
-  var slots = $(".slot");
-  $(".slot").hide().each(function() {
-    this.className="slot";
-    $(this).find(".nav").empty();
-  });
-  $(slots[slotIndex]).show().addClass("current");
-  $(slots[slotIndex]).prev().show().addClass("prev").find(".nav").append("&#171;");
-  $(slots[slotIndex]).next().show().addClass("next").find(".nav").append("&#187;");;
-}
 
 function log() { if (console) console.log.apply(console, arguments); }
