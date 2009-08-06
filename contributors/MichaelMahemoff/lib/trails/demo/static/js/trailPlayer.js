a13451a654b24b4df7990e7b4b26384310a16f98
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-06 16:41:54 +0000

    trails - basically, the carousel is now fully functional and shifting focus to other features like annotations and transitions
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10222 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
+++ contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
@@ -1,28 +1,34 @@
-var currentIndex = 1;
+// NOTE:
+// indexes are rather confusing, because:
+// (a) carousel confusingly starts counting at 1
+// (b) the code here adds an invisible start element and end element
+// The two things ~cancel each other out, if you see what I mean.
 
-function select(index) {
-    var resource = $($("#resources a")[index]);
-    console.log("select", index, "res", resource);
-    $('#resourceView').attr("src", resource.attr("href"));
-    $('#resources a').removeClass("selected");
-    resource.addClass("selected");
+var selectedSlot = 2;
+
+function select(slot) {
+  var resource = $($("#resources a")[slot-1]);
+  $('#resourceView').attr("src", resource.attr("href"));
+  $('#resources a').removeClass("selected");
+  resource.addClass("selected");
 }
 
 function selectNewResource(na,na,index,action) {
-     if (action=="next") {
-        if (index!=$("#resources a").length) currentIndex++;
-     } else if (action=="prev") {
-        currentIndex--;
-     }
-     select(currentIndex);
+   if (action=="next") {
+      if (index!=$("#resources a").length) selectedSlot++;
+   } else if (action=="prev") {
+      selectedSlot--;
+   }
+   select(selectedSlot);
 }
 
 $(function() {
 
-  $('#resources a').each(function(i) { $(this).data("index", i); });
+  $('#resources a').each(function(i) { $(this).data("slot", i+1); });
 
   $('#resources').jcarousel({visible: 3, scroll: 1, offset: 1, itemFirstInCallback: selectNewResource})
-  $('#resources a').click(function() { select($(this).data("index")); });
+  /* $('#resources a').click(function() { select($(this).data("slot")); }); */
+  $('#resources a').click(function() { return false; });
 
   $("#closer").click(function() { document.location.href = $(".selected").attr("href"); });
 
