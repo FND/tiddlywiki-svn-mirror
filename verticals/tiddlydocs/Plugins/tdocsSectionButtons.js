3f8a39ddd9440e6785be616f07997e6df383eb30
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-11 08:12:32 +0000

    tiddlydocs /mypage - adding new theme files
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10246 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -1,8 +1,12 @@
 config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'15px'});
+	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'12px'});
 	img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
-	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'15px'});
+	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'12px'});
 	img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
+	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'15px'});
+	img.src = 'http://www.homefinderextra.com/images/close-icon.png';
+
+
 } 
 
