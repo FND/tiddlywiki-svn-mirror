568cf4c502d25aaad8712159ce589b6ce45aa45e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-12 10:40:13 +0000

    tiddlydocs /mypage - css  refactoring
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10257 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/commentSlider.js
+++ verticals/tiddlydocs/Plugins/commentSlider.js
@@ -3,9 +3,9 @@ config.macros.commentSlider = {};
 config.macros.commentSlider.handler = function(place,macroName,params)
 {
 	var commentCount = wikifyStatic("<<commentsCount "+arguments[5].title+">>")
-	var holder = createTiddlyElement(place, "div", "", "", " ");
+	var holder = createTiddlyElement(place, "div", "", "commentStretch", " ");
 	var slider = config.macros.slider.createSlider(holder, "chkSlider"+arguments[5].title, "comments("+commentCount+")");
-	wikify("<<comments>>", slider);
+	wikify("<<comments tiddler: "+arguments[5].title+">>", slider);
 
 
 }
