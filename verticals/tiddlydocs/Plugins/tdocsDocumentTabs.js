203c7c8b976c0799d3c4fef61962eb245b0aa6d3
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-12 14:04:03 +0000

    tiddlydocs /mypage - making document switch tabs dynamic
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10260 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
+++ verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
@@ -1,12 +1,40 @@
 config.macros.docTabs = {};
 config.macros.docTabs.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	
-	var html = '<div id="tab_wrapper" style="background-color: rgb(128, 128, 128);"><ul class="tabs" id="tab_bar" style="visibility: visible;"><li class="tab spacer"/><li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801">The Internet</span></div></li><li class="tab tab_r tab_r_selected"/><li class="tab spacer"/><li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"><div><a title="Click here to add more pages" class="thickbox mis" href="addTabForm.do?TB_iframe=true&amp;height=80&amp;width=195&amp;modal=true" id="AddTabDialogue"><img src="static/wa/jarrita/skins/wholesale/images/icons/add_tab_normal.png" class="moreFunctionsImg addTabImg"/> Create New Document</a></div></li><li class="tab tab_r tab_r_add"></li><li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a title="Click to add gadgets from the catalogue" alt="Click to add gadgets from the catalogue" class="thickbox mis addFromCatalogue" href="/container/catalogue/index.do?target=http://www.btwholesale.com:80/container/jsp/tiles/container.jsp?null&amp;TB_iframe=true&amp;height=495&amp;width=800&amp;modal=true" id="AddFromCatalogueDialogue"><img style="border: medium none ;" src="http://www.btwholesale.com/container/static/wa/jarrita/skins/wholesale/images/buttons/add_gadgets_normal.gif" class="addGadgetCatImg"/></a></li></ul></div>';
+	var html = '<div id="tab_wrapper" style="background-color: rgb(128, 128, 128);"><ul class="tabs" id="tab_bar" style="visibility: visible;"><li class="tab spacer"/></li></ul></div>';
 	var d = createTiddlyElement(null, 'div', '', '', '');
 	d.innerHTML  = html;
 	place.appendChild(d);
+	config.macros.docTabs.refresh();
 }
 
+
+
+config.macros.docTabs.refresh = function() {
+	var html = '<li class="tab tab_l tab_l_selected"/>';
+
+
+var values = store.getTaggedTiddlers('document');
+	var selectedHtml = '';
+	for (var i=0; i < values.length; i++) {
+		if(values[i].title == window.activeDocument){
+			selectedHtml += '<li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span 		id="tab_name_6801">'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
+		} else {
+			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"><div><a title="Click here to add more pages" class="thickbox mis"  id="AddTabDialogue"><img src="static/wa/jarrita/skins/wholesale/images/icons/add_tab_normal.png" class="moreFunctionsImg addTabImg"/>'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
+	
+		}
+	selectedHtml += '<li class="tab spacer"/>';
+	}
+
+
+
+	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue"><img style="border: medium none ;" src="http://www.btwholesale.com/container/static/wa/jarrita/skins/wholesale/images/buttons/add_gadgets_normal.gif" class="addGadgetCatImg"/></a>';
+html = html + selectedHtml  + newdoc;
+	$('#tab_bar:first-child').html(html);
+	var addClick = function() {
+			story.displayTiddler(null, "Create New Document");
+	}
+	$('#AddFromCatalogueDialogue').click(addClick);
+}
 
 
 
