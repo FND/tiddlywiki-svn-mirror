1cde633134a10b6032e68a6b78c7efacae805b3c
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-13 18:14:57 +0000

    TiddlyWikiDeviceAccess - redeployed to psd home directory
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10285 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyWikiDeviceAccess/deploy.sh
+++ contributors/PaulDowney/verticals/TiddlyWikiDeviceAccess/deploy.sh
@@ -1,2 +1,3 @@
-scp index.html osmodot@www.osmosoft.com:/data/vhost/www.osmosoft.com/html/~psd/TiddlyWikiDeviceAccess/index.html
-scp paper.html osmodot@www.osmosoft.com:/data/vhost/www.osmosoft.com/html/~psd/TiddlyWikiDeviceAccess/paper.html
+#ssh psd@www.osmosoft.com mkdir ~psd/public_html/TiddlyWikiDeviceAccess
+scp index.html psd@www.osmosoft.com:~psd/public_html/TiddlyWikiDeviceAccess/index.html
+scp paper.html psd@www.osmosoft.com:~psd/public_html/TiddlyWikiDeviceAccess/paper.html
