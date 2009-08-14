ee6fda03773be306a5ba413e2b22a2eba176a1ac
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-14 15:55:18 +0000

    ccTiddly - static plugin now support various file types
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10293 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/static/files/static_check.php
+++ association/serversides/cctiddly/Trunk/plugins/static/files/static_check.php
@@ -5,12 +5,13 @@ $URI = explode('/', $_SERVER['REQUEST_URI']);
 if($URI[1] == 'static'){
 	array_shift($URI);// remove first item from array
 	array_shift($URI); // remove second item from the array
-//	echo var_dump($URI);
 	$path = getcwd().'/plugins/static/static/'.implode("/", $URI);
-	
-header("Content-type: ".mime_content_type($path));
-echo file_get_contents($path);
-	exit;
+	if(file_exists($path))
+	{	
+		header("Content-type: ".mime_content_type($path));
+		echo file_get_contents($path);
+		exit;
+	}
 }
 
 ?>
