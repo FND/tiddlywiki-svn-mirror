b40046d6550fe4ba287c213d593460d166562bb7
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-21 15:46:10 +0000

    tiddlydocs - changing path to image files
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10334 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
+++ verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
@@ -22,15 +22,15 @@ var values = store.getTaggedTiddlers('document');
 	var selectedHtml = '';
 	for (var i=0; i < values.length; i++) {
 		if(values[i].title == window.activeDocument){
-			selectedHtml += '<li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801"><img src="static/tiddlydocs_images/icons/"/>'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
+			selectedHtml += '<li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801"><img src="/static/tiddlydocs_images/icons/"/>'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
 		} else {
-			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"  onclick="config.macros.docTabs.switchDoc(\''+values[i].title+'\');"><div><a class="thickbox mis" href="#"><img src="static/wa/jarrita/skins/wholesale/images/icons/add_tab_normal.png" class="moreFunctionsImg addTabImg"/>'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
+			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"  onclick="config.macros.docTabs.switchDoc(\''+values[i].title+'\');"><div><a class="thickbox mis" href="#"><img src="/static/wa/jarrita/skins/wholesale/images/icons/add_tab_normal.png" class="moreFunctionsImg addTabImg"/>'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
 	
 		}
 	selectedHtml += '<li class="tab spacer"/>';
 	}
 
-	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue" href="#"><img style="border: medium none ;" src="static/tiddlydocs_images/NewDocuments.png" class="addGadgetCatImg"/></a>';
+	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue" href="#"><img style="border: medium none ;" src="/static/tiddlydocs_images/NewDocuments.png" class="addGadgetCatImg"/></a>';
 html = selectedHtml  + newdoc;
 	jQuery('#tab_bar:first-child').html(html);
 	var addClick = function() {
