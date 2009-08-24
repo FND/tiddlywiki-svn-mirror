67ad21bfc2bc786b6734026c663c4d5822758045
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-24 12:15:02 +0000

    tiddlydocs - moving failedNotifications.js
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10338 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/cctiddly/Trunk/plugins/static/files/static_check.php
+++ association/serversides/cctiddly/Trunk/plugins/static/files/static_check.php
@@ -6,6 +6,20 @@ if($URI[1] == 'static'){
 	array_shift($URI);// remove first item from array
 	array_shift($URI); // remove second item from the array
 	$path = getcwd().'/plugins/static/static/'.implode("/", $URI);
+	
+	
+	
+	echo $path;
+echo '<br />';
+
+echo $mime_type = $fi->file($path);
+exit;
+      $fi = new finfo(FILEINFO_MIME);
+      $mime_type = $fi->buffer(file_get_contents($file));
+
+
+	
+	exit;
 	if(file_exists($path))
 	{	
 		header("Content-type: ".mime_content_type($path));
---
+++ contributors/SimonMcManus/plugins/failedSaveNotification.js
@@ -0,0 +1,89 @@
+
+config.extensions.ServerSideSavingPlugin.reportSuccess = function() {
+}
+
+config.extensions.ServerSideSavingPlugin.reportFailed = function(tiddler, context){
+	alert('Your changes were not saved');
+}
+
+
+
+
+var original = TiddlyWiki.prototype.setDirty;
+TiddlyWiki.prototype.setDirty = function(dirty) {
+	original.apply(arguments);
+	config.macros.saveNotification.displayStatus(dirty);
+};
+
+config.macros.saveNotification = {};
+config.macros.saveNotification.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	createTiddlyElement(place, "div", "", "savingNotificationsDiv", "STATUS IS : ");
+	config.macros.saveNotification.displayStatus(store.isDirty());
+};
+
+config.macros.saveNotification.displayStatus = function(dirty) {
+	if(dirty) {
+			$('.savingNotificationsDiv').css('background', 'red');
+	} else {
+			$('.savingNotificationsDiv').css('background', 'green');
+	}
+};
+
+
+config.macros.saveNotification.center  = function(el){
+	var size = this.getsize(el);
+	el.style.left = (Math.round(findWindowWidth()/2) - (size.width /2) + findScrollX())+'px';
+	el.style.top = (Math.round(findWindowHeight()/2) - (size.height /2) + findScrollY())+'px';
+}
+
+config.macros.saveNotification.getsize = function (el){
+	var x ={};
+	x.width = el.offsetWidth || el.style.pixelWidth;
+	x.height = el.offsetHeight || el.style.pixelHeight;
+	return x;
+}
+
+config.macros.saveNotification.showCloak = function(){
+	var cloak = document.getElementById('backstageCloak');
+	if (config.browser.isIE){
+		cloak.style.height = Math.max(document.documentElement.scrollHeight,document.documentElement.offsetHeight);
+		cloak.style.width = document.documentElement.scrollWidth;
+	}
+	cloak.style.display = "block";
+}
+
+config.macros.saveNotification.hideError = function(){
+	var box = document.getElementById('errorBox');
+	box.parentNode.removeChild(box);
+	document.getElementById('backstageCloak').style.display = "";
+}
+
+config.macros.saveNotification.handleError = function(error_code){
+		setStylesheet(
+		"#errorBox .button{padding:0.5em 1em; border:1px solid #222; background-color:#ccc; color:black; margin-right:1em;}\n"+
+		"html > body > #backstageCloak{height:100%;}"+
+		"#errorBox{border:1px solid #ccc;background-color: #eee; color:#111;padding:1em 2em; z-index:9999;}",'errorBoxStyles');
+		var box = document.getElementById('errorBox') || createTiddlyElement(document.body,'div','errorBox');
+		var error = ccTiddlyAdaptor.errorTitleNotSaved;
+		switch(error_code){
+			case 401:
+				error += ccTiddlyAdaptor.errorTextSessionExpired;
+				break;
+			case 409:
+				error += "\n\n"+ccTiddlyAdaptor.errorTextConfig+"\n \n error code : "+error_code+" \n";
+				break;
+
+			default:
+				error += ccTiddlyAdaptor.errorTextUnknown+"<br />"+error_code;
+		}
+		box.innerHTML = " <a style='float:right' href='javascript:onclick=ccTiddlyAdaptor.hideError()'>"+ccTiddlyAdaptor.errorClose+"</a><p>"+error+"</p><br/><br/>";
+		createTiddlyButton(box,ccTiddlyAdaptor.buttonOpenNewWindow,null,function(e){ window.open (window.location,"mywindow");	 return false;});
+		createTiddlyElement(box,"br");
+		createTiddlyElement(box,"br");
+		createTiddlyButton(box,ccTiddlyAdaptor.buttonHideThisMessage,null,function(){ccTiddlyAdaptor.hideError();});
+		box.style.position = 'absolute';
+		ccTiddlyAdaptor.center(box);
+		ccTiddlyAdaptor.showCloak();
+}
+
+
