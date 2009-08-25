49f98343c7e8b34e39f96a1d7b45b85799b62535
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-25 09:52:18 +0000

    tiddlydocs - section buttons macro now only shows on tiddlyweb
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10364 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -2,7 +2,7 @@ config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 	if(typeof(tiddler.fields['server.host']) === "string") {
 		var a = createTiddlyElement(place, "a", null, null);
-		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/'+tiddler.title;
+		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki';
 		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
 		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
 		createTiddlyText(a, 'Permalink');
