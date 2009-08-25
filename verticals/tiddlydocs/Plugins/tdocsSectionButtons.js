afbb57469cc0a27ff1746fe6090c6ea7786352ab
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-25 18:16:55 +0000

    tiddlydocs - more theme tweaks
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10383 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -1,12 +1,12 @@
 config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 	if(typeof(tiddler.fields['server.host']) === "string") {
-		var a = createTiddlyElement(place, "a", null, null);
+		var a = createTiddlyElement(place, "a", null, 'button');
 		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki';
 		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
 		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
 		createTiddlyText(a, 'Permalink');
-		var a = createTiddlyElement(place, "a", null, null);
+		var a = createTiddlyElement(place, "a", null, 'button');
 		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom';
 		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
 		img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
