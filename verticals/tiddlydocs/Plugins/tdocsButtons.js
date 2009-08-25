5ad10f5898e6ce02ff76d0ed812f52b2ccc4d16d
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-25 07:52:24 +0000

    tiddlydocs - css updates
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10357 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsButtons.js
@@ -3,22 +3,14 @@
 config.macros.tdButtons = {};
 config.macros.tdButtons.handler=function(place,macroName,params,wikifier,paramString,tiddler){
 	var buttonHolder = createTiddlyElement(place, "div", "buttonHolder");
-	if(config.options.chkDrawings)
-		wikify("| [[Drawings]] | <<newDrawing>>  ", buttonHolder);
-		//	window.activeDocument = params[0];
 	wikify("<<docPrint "+window.activeDocuement+">>", buttonHolder);
-
 	var previewClick = function(place) {
 		story.displayTiddler(null, "Current Document Preview");
 	}
-	
 	var btn = createTiddlyButton(buttonHolder, "preview", "preview current document", previewClick, null, null, null, null, "/static/tiddlydocs_images/icons/Menu_24.png");
-	
 	var btn = createTiddlyButton(buttonHolder, "new", "New Section", config.macros.newTiddler.onClickNewTiddler, null, null, null, null, "/static/tiddlydocs_images/icons/Invitation_24.png");
-
 	btn.setAttribute("newTitle","New Section Title");
-	btn.setAttribute("newTemplate",getParam(params,"template","myPageTheme##newEditTemplate"));
-
+	btn.setAttribute("newTemplate", "myPageTheme##newEditTemplate");
 	var displaySettings= function () {
 		story.displayTiddler(null, "Settings");
 	};
--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -1,12 +1,18 @@
 config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'12px'});
-	img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
-	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'12px'});
-	img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
-	var img = createTiddlyElement(place, 'img', '', '', '', {'height':'15px'});
-	img.src = 'http://www.btwholesale.com/container/static/wa/jarrita/skins/wholesale/images/icons/close_normal.png';
 
-
-} 
-
+	if(tiddler.fields.server != undefined) {
+		var a = createTiddlyElement(place, "a", null, null);
+		a.href = tiddler.fields.server['host']+tiddler.fields.server['workspace']+'/'+tiddler.title;
+		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
+		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
+		createTiddlyText(a, 'Permalink');
+	}
+	if(tiddler.fields.server != undefined) {
+		var a = createTiddlyElement(place, "a", null, null);
+		a.href = tiddler.fields.server['host']+tiddler.fields.server['workspace']+'/'+tiddler.title+'.atom';
+		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
+		img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
+		createTiddlyText(a, 'RSS');
+	} 
+}
