cfa8cf3f18d8aea29787eee47593f8aa03ef436c
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-25 12:25:20 +0000

    modal UI enhancements - added keyboard shortcut to close it (ESC) - added redundant "X" control to close it (affordance ftw) - changed fade effect to washed out (light gray with transparency) - fade in / fade out
    would still need better support for custom CSS to make it a true blue useful jquery plugin
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10367 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/lib/modal/js/modal.js
+++ contributors/MichaelMahemoff/lib/modal/js/modal.js
@@ -3,27 +3,34 @@
   $.modal.show  = function(message) {
 
     $.fn.push = function(html) { return this.append(html).children(":last"); };
+    $.fn.destroy = function() { return this.fadeOut(FADE_DURATION, function() { $(this).remove(); }) }
 
     var DIALOG_WIDTH = 400;
     var DIALOG_HEIGHT = 300;
     var CUSHION_LENGTH = 20000;
-    $("<div/>") // Black mask
+    FADE_DURATION = 400;
+    var mask = $("<div/>") // Black mask
+    .animate({opacity: 0.85}, FADE_DURATION)
     .css({
       position: "absolute",
       top: -CUSHION_LENGTH,
       left: -CUSHION_LENGTH,
-      background: "#000",
+      background: "#999",
       zIndex: 999998,
       height: 2*CUSHION_LENGTH+$(window).height(),
-      width: 2*CUSHION_LENGTH+$(window).width()
+      width: 2*CUSHION_LENGTH+$(window).width(),
+      opacity: 0.01
     })
-    .click(function() {
-      $(this).remove();
+    .click(function(ev) {
+      $(this).destroy();
+      ev.stopPropagation();
     })
     .push("<div/>")
+      .fadeIn(FADE_DURATION)
       .css({
         position: "absolute",
         background: "#fff",
+        border: "1px solid #666",
         width: DIALOG_WIDTH,
         height: DIALOG_HEIGHT,
         zIndex: 999999,
@@ -39,8 +46,25 @@
         })
         .html(message)
       .end()
+      .push("<div/>")
+        .css({
+          position: "absolute",
+          top: "-1.2em",
+          right: "4px",
+          cursor: "pointer"
+        })
+        .html("X")
+        .click(function(ev) {
+          $(this).parents("div").destroy();
+        })
+      .end()
     .end()
     .appendTo($(document.body));
+
+    $(window.body).keyup(function(ev) { 
+      if (ev.charCode==27 || ev.keyCode==27) mask.destroy();
+      ev.stopPropagation();
+    });
 
   }
 })(jQuery);
