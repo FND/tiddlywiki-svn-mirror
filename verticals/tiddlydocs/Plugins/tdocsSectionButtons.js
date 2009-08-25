cff7a7b41948991a2997609d1d3aa1f8c9a94632
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-25 10:06:48 +0000

    tiddlydocs - section buttons now link to rss feed correctly
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10365 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -7,7 +7,7 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
 		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
 		createTiddlyText(a, 'Permalink');
 		var a = createTiddlyElement(place, "a", null, null);
-		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/'+tiddler.title+'.atom';
+		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom';
 		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
 		img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
 		createTiddlyText(a, 'RSS');
