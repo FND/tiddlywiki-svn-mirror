2283010156a5829cc91a5ed695ea955bd1b741b8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-26 21:31:26 +0000

    tiddlydocs - css - making the bin nice colours
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10394 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/commentSlider.js
+++ verticals/tiddlydocs/Plugins/commentSlider.js
@@ -5,5 +5,5 @@ config.macros.commentSlider.handler = function(place,macroName,params)
 	var commentCount = wikifyStatic("<<commentsCount "+arguments[5].title+">>")
 	var holder = createTiddlyElement(place, "div", "", "commentStretch", " ");
 	var slider = config.macros.slider.createSlider(holder, "chkSlider"+arguments[5].title, "comments("+commentCount+")");
-	wikify("<<comments tiddler: "+arguments[5].title+">>", slider);
+	wikify("<<comments tiddler: "+arguments[5].title+">>\n", slider);
 }
