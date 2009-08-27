ddc5224835ef5e6f107915d6252a2f9e7a8fde6c
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-08-27 17:14:50 +0000

    tiddlydocs - adding tiddlyweb install script for tiddlydocs
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10408 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/tiddlyweb/tiddlydocs/install.sh
+++ contributors/SimonMcManus/tiddlyweb/tiddlydocs/install.sh
@@ -4,6 +4,8 @@ rm ../tiddlydocs -R -f
 
 twanager --load tiddlywebwiki.config instance tiddlydocs
 
+cd tiddlydocs
+
 wget http://github.com/tiddlyweb/tiddlyweb/raw/master/apache.py
 wget http://github.com/tiddlyweb/tiddlyweb-plugins/raw/e7cdc9d91a7fe9e8483933f3e1275139718e321d/atom/atom.py
 wget http://github.com/tiddlyweb/tiddlyweb-plugins/raw/e7cdc9d91a7fe9e8483933f3e1275139718e321d/atom/atomplugin.py
@@ -12,8 +14,8 @@ wget http://github.com/tiddlyweb/tiddlyweb-plugins/raw/e7cdc9d91a7fe9e8483933f3e
 
 wget http://svn.tiddlywiki.org/Trunk/contributors/SimonMcManus/tiddlyweb/tiddlydocs/tiddlywebconfig.py
 
-twanager bag tdocs
-twanager bag documents
+twanager bag tdocs < /dev/null
+twanager bag documents < /dev/null
 
 twanager from_svn documents http://svn.tiddlywiki.org/Trunk/verticals/tiddlydocs/documents/TheInternet/split.recipe
 twanager from_svn tdocs http://svn.tiddlywiki.org/Trunk/verticals/tiddlydocs/index.html.recipe
