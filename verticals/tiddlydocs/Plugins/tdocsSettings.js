9d7a28a566324d3ab44b7bf0c2cf82bf58f10597
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-02 15:50:50 +0000

    tiddlydocs - changing ckeditor icons
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10472 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSettings.js
+++ verticals/tiddlydocs/Plugins/tdocsSettings.js
@@ -20,6 +20,24 @@ config.views.wikified.defaultText= "The section has not been edited yet.";
 config.options.txtFCKeditorPath = 'plugins/FCKEditor/files/fckeditor/';
 
 
+config.toolbar_Basic =
+[
+    ['Cut','Copy','Paste', 'SpellChecker'],
+    ['Undo','Redo','-','Find','Replace'],
+    '/',
+    ['Bold','Italic','Underline','Strike'],
+    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+    ['Image', 'Table','HorizontalRule','SpecialChar'],
+    '/',
+    ['Styles','Format','Font','FontSize'],
+    ['TextColor','BGColor'],
+];
+
+
+
+
+
 // set the document from the hash
 var hash =  window.location.hash.substring(1);
 if(hash!="")
