cd34441f94f0a300eb1a3680e156491416eec780
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-02 13:29:04 +0000

    tiddlydocs - defaulting tabs options to true
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10468 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSettings.js
+++ verticals/tiddlydocs/Plugins/tdocsSettings.js
@@ -3,7 +3,7 @@
 //config.options.chkAutoSave = true;
 config.options.chkAnimate = true;
 config.options.chkSaveBackups = false;
-
+config.options.chkInsertTabs = true;
 //config.options.chkBackstage = false;
 
 
