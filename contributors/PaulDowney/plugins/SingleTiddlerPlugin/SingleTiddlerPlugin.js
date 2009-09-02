6563967a5c8518d0ff53c0765348b4f05a2c7208
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-02 21:10:30 +0000

    SingleTiddlerPlugin - simplest possible single paged mode
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10481 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/SingleTiddlerPlugin/SingleTiddlerPlugin.js
@@ -0,0 +1,26 @@
+/***
+|''Name:''|SingleTiddlerPlugin|
+|''Description:''| Simple Single Tiddler Display |
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
+|''Source:''|http://whatfettle.com/2008/07/SingleTiddlerPlugin/ |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/SingleTiddlerPlugin/ |
+|''Version:''|0.1|
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.4|
+!!Documentation
+Display one tiddler at one time.
+!!Code
+***/
+//{{{
+if(!version.extensions.SingleTiddlerPlugin) {
+version.extensions.SingleTiddlerPlugin = {installed:true};
+
+	Story.prototype._displayTiddler = Story.prototype.displayTiddler;
+	Story.prototype.displayTiddler = function(srcElement,title,template,animate,slowly)
+	{
+		story.closeAllTiddlers();
+		this._displayTiddler(null,title,template,animate,slowly);
+	}
+}
+//}}}
