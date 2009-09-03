89f2e726bef821618684b825cee41885b8c1d0ce
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-03 13:55:58 +0000

    tiddlydocs - mypage - adding some more images
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10511 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -4,13 +4,13 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 var a = createTiddlyElement(place, "a", null, 'button');
                 a.setAttribute('href', tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki');
                 var img = document.createElement('img');
-                img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';
+                img.src = '/static/mypage_images/icon_link.png';
                 a.appendChild(img);
                 createTiddlyText(a, 'Permalink');
                 var a2 = createTiddlyElement(place, "a", null, 'button');
                 a2.setAttribute('href',  tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom');
                 var img = document.createElement('img');
-                img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
+                img.src = '/static/mypage_images/icon_rss.png';
                 a2.appendChild(img);
                 createTiddlyText(a2, 'RSS');
         }
