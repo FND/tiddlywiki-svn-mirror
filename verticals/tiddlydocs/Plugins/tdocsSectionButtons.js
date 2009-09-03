823466b953e445c0e5813f44201d757fab84a596
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-03 11:13:49 +0000

    tiddlydocs - ie6 nonsense
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10486 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -6,10 +6,10 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
 		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
 		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
 		createTiddlyText(a, 'Permalink');
-		var a = createTiddlyElement(place, "a", null, 'button');
-		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom';
-		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
-		img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
-		createTiddlyText(a, 'RSS');
+		var a2 = createTiddlyElement(place, "a", null, 'button');
+		a2.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom';
+		var img2 = createTiddlyElement(a2, 'img', '', '', '', {'height':'12px'});	
+		img2.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
+		createTiddlyText(a2, 'RSS');
 	}
-}
+};
