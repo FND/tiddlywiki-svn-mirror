7c68c9ac93050ea0f971f83adcaa9c00fc65242d
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-03 13:59:41 +0000

    tiddlydocs - mypage - adding some more images
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10512 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -4,13 +4,13 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 var a = createTiddlyElement(place, "a", null, 'button');
                 a.setAttribute('href', tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki');
                 var img = document.createElement('img');
-                img.src = '/static/mypage_images/icon_link.png';
+                img.src = '/static/mydocs_images/icon_link.png';
                 a.appendChild(img);
                 createTiddlyText(a, 'Permalink');
                 var a2 = createTiddlyElement(place, "a", null, 'button');
                 a2.setAttribute('href',  tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom');
                 var img = document.createElement('img');
-                img.src = '/static/mypage_images/icon_rss.png';
+                img.src = '/static/mydocs_images/icon_rss.png';
                 a2.appendChild(img);
                 createTiddlyText(a2, 'RSS');
         }
