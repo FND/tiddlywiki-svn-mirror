30ec723d076c64c86526912e379b7b7926278e30
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-09-03 13:49:00 +0000

    tiddlydocs - mypage IE shenanigans
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10509 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -1,16 +1,18 @@
 config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	if(typeof(tiddler.fields['server.host']) === "string") {
-		return;
-		var a = createTiddlyElement(place, "a", null, 'button');
-		a.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki';
-		var img = createTiddlyElement(a, 'img', '', '', '', {'height':'12px'});	
-		img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';	
-		createTiddlyText(a, 'Permalink');
-		var a2 = createTiddlyElement(place, "a", null, 'button');
-		a2.href = tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom';
-		var img2 = createTiddlyElement(a2, 'img', '', '', '', {'height':'12px'});	
-		img2.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
-		createTiddlyText(a2, 'RSS');
-	}
+        if(typeof(tiddler.fields['server.host']) === "string") {
+                var a = createTiddlyElement(place, "a", null, 'button');
+                a.setAttribute('href', tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki');
+                var img = document.createElement('img');
+                img.src = 'http://www.sheffieldhealthyschools.co.uk/images/icons/link_icon.png';
+                a.appendChild(img);
+                createTiddlyText(a, 'Permalink');
+                var a2 = createTiddlyElement(place, "a", null, 'button');
+                a2.setAttribute('href',  tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom');
+                var img = document.createElement('img');
+                img.src = 'http://www.acdivoca.org/852571DC00681414/Lookup/RSS-feed-small-PNG/$file/RSS-feed-small-PNG.png';
+                a2.appendChild(img);
+                createTiddlyText(a2, 'RSS');
+        }
 };
+
