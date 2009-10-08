20021cc2f76b662cd419724ed99fdc4fba4ff2f1
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:59:56 +0000

    SlideControlsPlugin: initial commit
    From: FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10790 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/TiddlySlidy/plugins/SlideControlsPlugin.js
@@ -0,0 +1,70 @@
+/***
+|''Name''|<...>|
+|''Description''|<...>|
+|''Author''|FND|
+|''Version''|<#.#.#>|
+|''Status''|<//unknown//; @@experimental@@; @@beta@@; //obsolete//; stable>|
+|''Source''|http://devpad.tiddlyspot.com/#<...>|
+|''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
+|''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
+|''CoreVersion''|<...>|
+|''Requires''|<...>|
+|''Keywords''|<...>|
+!Description
+<...>
+!Notes
+<...>
+!Usage
+{{{
+<<...>>
+}}}
+!!Parameters
+<...>
+!!Examples
+<<...>>
+!Configuration Options
+<...>
+!Revision History
+!!v<#.#> (<yyyy-mm-dd>)
+* <...>
+!To Do
+<...>
+!Code
+***/
+//{{{
+(function() {
+
+config.macros.prevSlide = {
+	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+		var btn = createTiddlyButton(place, "prev", "previous", function(ev) {
+			changeSlide(-1, place);
+		});
+		
+	}
+};
+
+config.macros.nextSlide = {
+	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+		var btn = createTiddlyButton(place, "next", "next", function(ev) {
+			changeSlide(1, place);
+		});
+	}
+};
+
+var changeSlide = function(delta, place) {
+	var slides = version.extensions.InclusifierPlugin.getTiddlersForShow("Outline");
+	// XXX: DEBUG'd
+	//var currentSlide = story.findContainingTiddler(place);
+	//var title = currentSlide ? currentSlide.getAttribute("tiddler") : slides[0];
+	//var index = slides.indexOf(title) + delta;
+	if(window.currentSlide === undefined) {
+		currentSlide = -1; // TODO
+	}
+	currentSlide = currentSlide + delta;
+	currentSlide = currentSlide.clamp(0, slides.length - 1);
+	story.closeAllTiddlers();
+	story.displayTiddler(null, slides[currentSlide]);
+};
+
+})();
+//}}}
