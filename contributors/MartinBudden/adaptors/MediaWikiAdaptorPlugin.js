6fdd1e5d9b34b42c08729bc66628e1a4bb74fe36
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:37:51 +0000

    MediaWikiAdaptor - fixed minor bug in getTiddlerInfo
    From: MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10637 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js
+++ contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|Martin Budden (mjbudden (at) gmail (dot) com)|
 |''Source:''|http://www.martinswiki.com/#MediaWikiAdaptorPlugin |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/MartinBudden/adaptors/MediaWikiAdaptorPlugin.js |
-|''Version:''|0.8.14|
+|''Version:''|0.8.15|
 |''Date:''|Jul 27, 2007|
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''License:''|[[Creative Commons Attribution-ShareAlike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]] |
@@ -526,7 +526,7 @@ adaptor.prototype.generateTiddlerInfo = function(tiddler)
 	} else {
 		uriTemplate = '%0/index.php?title=%2';
 	}
-	info.uri = uriTemplate.format([host,this.workspace,tiddler.title]);
+	info.uri = uriTemplate.format([host,this.workspace,adaptor.normalizedTitle(tiddler.title)]);
 	return info;
 };
 
