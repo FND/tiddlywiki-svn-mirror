0436bd4d2af839b5e7374463db67663554f0626e
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:52:20 +0000

    tiddlydocs - adding new tiddler button to the backstage
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10743 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
+++ verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
@@ -6,4 +6,8 @@ else
 
 
 config.backstageTasks.push("tiddlers");
-merge(config.tasks,{about:{text: 'tiddlers',tooltip: 'view the tiddlers in the TiddlyWiki.',content: '<<tiddler SideBarTabs>>'}});
+merge(config.tasks,{tiddlers:{text: 'tiddlers',tooltip: 'view the tiddlers in the TiddlyWiki.',content: '<<tiddler SideBarTabs>>'}});
+
+
+config.backstageTasks.push("new tiddler");
+merge(config.tasks,{'new tiddler':{text: 'new tiddler',tooltip: 'create a new tiddler',content: '<<newTiddler template:"MyDocsTheme##tiddlerEditTemplate">>'}});
