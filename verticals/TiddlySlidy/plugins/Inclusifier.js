a274b3b59c3b11e3713b92dbda6c35592463179a
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:59:01 +0000

    tiddlyslidy = inclusifier has dummy method to return list of slides to show
    From: mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10783 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/TiddlySlidy/plugins/Inclusifier.js
+++ verticals/TiddlySlidy/plugins/Inclusifier.js
@@ -1,5 +1,16 @@
 (function() {
 
+if (!version.extensions.InclusifierPlugin) {
+
+  var plugin = version.extensions.InclusifierPlugin = {installed:true};
+
+  plugin.getTiddlersForShow = function(trailTiddler) {
+    return ["Introduction", "Background", "The Gettysburg Address"];
+    var trailText = store.getTiddlerText(trailTiddler);
+    var resources = version.extensions.TrailsPlugin.flattenTree(trailText);
+    return resources;
+  }
+
   var stylesheet = store.getTiddlerText(tiddler.title + "##InclusifierStyleSheet");
   if (stylesheet) { // check necessary because it happens more than once for some reason
     config.shadowTiddlers["InclusifierStyleSheet"] = stylesheet;
@@ -18,18 +29,35 @@
         var checkbox = 
           $("<input type='checkbox' />")
           .attr("checked", !tiddler.isTagged("noShow"))
+          .change(function() {
+            // store.setTiddlerTag(tiddler.title,! $(this).attr("checked"),"noShow");
+            saveTiddler(tiddler);
+          })
           .insertBefore($(this));
       });
     }
 
   };
 
-})();
+  function saveTiddler(tiddler) {
+    var tiddler = (typeof(tiddler)=="string") ? store.getTiddler(tiddler) : tiddler; 
+    store.saveTiddler(tiddler.title, tiddler.title, tiddler.text, tiddler.modifier, tiddler.modified, tiddler.tags, mergeReadOnly(config.defaultCustomFields, tiddler.fields), false, tiddler.created)
+  }
 
+  function mergeReadOnly(first, second) {
+    var merged = {};
+    for (var field in first) { merged[field] = first[field]; }
+    for (var field in second) { merged[field] = second[field]; }
+    return merged;
+  }
 
 /***
 !InclusifierStyleSheet
 .inclusifier li { list-style-type: none; }
 !(end of StyleSheet)
 ***/
+
+}
+
+})();
 
