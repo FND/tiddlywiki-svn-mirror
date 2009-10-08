37265dd420ba2c050b8b99727b94db43cacec29c
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 13:08:18 +0000

    tiddlydocs - adding changes to saveNewSection
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10850 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/saveNewSection.js
+++ verticals/tiddlydocs/Plugins/saveNewSection.js
@@ -22,40 +22,21 @@ window.addToToc = function(sectionTitle, docTitle) {
 	}
 }
 
-Story.prototype.saveTiddler = function(title,minorUpdate)
-{
-	var tiddlerElem = this.getTiddler(title);
-	if(tiddlerElem) {
-		var fields = {};
-		this.gatherSaveFields(tiddlerElem,fields);
-		var newTitle = fields.title || title;
-		if(!store.tiddlerExists(newTitle))
-			newTitle = newTitle.trim();
-		if(store.tiddlerExists(newTitle) && newTitle != title) {
-			if(!confirm(config.messages.overwriteWarning.format([newTitle.toString()])))
-				return null;
-		}
-		if(newTitle != title)
-			this.closeTiddler(newTitle,false);
-		tiddlerElem.id = this.tiddlerId(newTitle);
-		tiddlerElem.setAttribute("tiddler",newTitle);
-		tiddlerElem.setAttribute("template",DEFAULT_VIEW_TEMPLATE);
-		tiddlerElem.setAttribute("dirty","false");
-		if(config.options.chkForceMinorUpdate)
-			minorUpdate = !minorUpdate;
-		if(!store.tiddlerExists(newTitle))
-			minorUpdate = false;
-		var newDate = new Date();
-		var extendedFields = store.tiddlerExists(newTitle) ? store.fetchTiddler(newTitle).fields : (newTitle!=title && store.tiddlerExists(title) ? store.fetchTiddler(title).fields : merge({},config.defaultCustomFields));
-		for(var n in fields) {
-			if(!TiddlyWiki.isStandardField(n))
-				extendedFields[n] = fields[n];
-		}
-		var tiddler = store.saveTiddler(title,newTitle,fields.text,minorUpdate ? undefined : config.options.txtUserName,minorUpdate ? undefined : newDate,fields.tags+'[[doc_'+window.activeDocument+']]',extendedFields, false, newDate);
-		window.addToToc(newTitle, window.activeDocument);
-		autoSaveChanges(null,[tiddler, window.activeDocument]);
-		return newTitle;
-	}
-	return null;
+//(function() {
+
+var _saveTiddler = Story.prototype.saveTiddler;
+Story.prototype.saveTiddler = function(title, minorUpdate) {
+	var autosave = config.options.chkAutoSave;
+	config.options.chkAutoSave = false;
+	var _title = _saveTiddler.apply(this, arguments);
+	console.log("foo", title, _title);
+	config.options.chkAutoSave = autosave;
+
+	addToToc(_title, window.activeDocument);
+	var tiddler = store.getTiddler(_title);
+	tiddler.tags.push("[[doc_" + window.activeDocument + "]]");
+	autoSaveChanges(null, [tiddler, window.activeDocument]);
+	return _title;
 };
 
+//})();
