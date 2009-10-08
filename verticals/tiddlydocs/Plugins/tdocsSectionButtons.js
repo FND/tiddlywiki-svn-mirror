7af69f84c20c47a9269a7de991d0aec7ff8a895b
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 13:03:04 +0000

    tiddlydocs - minor tweaks ;
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10815 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -20,7 +20,7 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 a2.appendChild(img);
                 createTiddlyText(a2, ' RSS');
                 var a3 = createTiddlyElement(place, "a", null, 'button');
-                a3.setAttribute('href',  tiddler.fields['server.host']+'/recipes/'+tiddler.fields['server.recipe']+'/tiddlers.atom?select=root:'+encodeURIComponent(tiddler.title));
+                a3.setAttribute('href',  tiddler.fields['server.host']+"/"+tiddler.fields['server.workspace']+'/tiddlers.atom?select=root:'+encodeURIComponent(tiddler.title));
         		a3.setAttribute('target', 'tiddlydocs_link');
         		var img = document.createElement('img');
                 img.src = '/doccollab/static/mydocs_images/icon_rss.png';
