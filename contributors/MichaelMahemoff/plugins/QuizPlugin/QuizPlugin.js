076ee00c3928f1b2d87f73a6bf837c23c4694057
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:39:00 +0000

    QuizPlugin refactoring/enhancements - "finale" class used to mark/show/hide anything shown after results checked - "question" data structure introduced, to decouple rendering and parsing - upon reveal, correct options now boldfaced/underlined instead of tick - question now shows how many correct options
    To do:
    - enforce no. of correct options (don't let user choose more than that)
    - make "question" object prototype with behaviour eg to check if answer is right
    
    From: mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10645 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/plugins/QuizPlugin/QuizPlugin.js
+++ contributors/MichaelMahemoff/plugins/QuizPlugin/QuizPlugin.js
@@ -25,92 +25,138 @@ More options in the future and a reset quiz button. Maybe even a secure tiddlywe
 /*{{{*/
 if(!version.extensions.QuizPlugin) {
 
-  jQuery.fn.create = function(html) { return this.append(html).children(":last"); };
-
   version.extensions.QuizPlugin = {installed:true};
 
-  config.macros.quiz = {
+  (function() {
 
-    init: function() {
-      var stylesheet = store.getTiddlerText(tiddler.title + "##StyleSheet");
-      config.shadowTiddlers["StyleSheetQuizPlugin"] = stylesheet;
-      store.addNotification("StyleSheetQuizPlugin", refreshStyles);
-    },
-
-    handler: function(place,macroName,params,wikifier,paramString,tiddler) {
-      $ = jQuery;
-      console.log(store.getTaggedTiddlers("question"));
-      var quizEl = $(createTiddlyElement(place, "div", null, "quiz"));
-      $.each(shuffle(store.getTaggedTiddlers("question")), function(i, question) {
-        var questionEl = buildQuestionEl(question);
-        quizEl.append(questionEl);
-      });
-      quizEl.append($("<button>check</button>").click(function() { checkQuiz(quizEl); }));
-      quizEl.append($("<div class='quizStatus' />"));
-    }
-
-  }
-
-  function buildQuestionEl(question) {
     var $ = jQuery;
-    var questionText = question.text.replace(/^\*.*/mg, "");
-    var questionEl = $("<div/>").
-      addClass("question")
-      .html(questionText)
-    // var wikitext = question.text.replace(/^\*\*/m,"*");
-    var wrongOptions = question.text.match(/^\* .*$/mg);
-    var rightOptions = question.text.match(/^\*\* .*$/mg);
-    var options = shuffle(wrongOptions.concat(rightOptions)); // todo randomise
-    for (i=0; i<options.length; i++) {
-      var isRight = (rightOptions.indexOf(options[i])!=-1);
-      var optionText = options[i].replace(/^\*+ /, "");
-      $("<div class='option'/>")
-      .append("<input type='checkbox' />")
-      .create("<span>"+optionText+"</span>")
-        .click(function() { $(this).prev().click() })
-      .end()
-      .create("<span class='rightness'>"+(isRight? "&#10004" : "")+"</span>")
-        .css("visibility", "hidden")
-      .end()
-      .data("right", isRight)
-      .appendTo(questionEl);
+    jQuery.fn.attach = function(html) { return this.append(html).children(":last"); };
+
+    config.macros.quiz = {
+
+      init: function() {
+        var stylesheet = store.getTiddlerText(tiddler.title + "##StyleSheet");
+        config.shadowTiddlers["StyleSheetQuizPlugin"] = stylesheet;
+        store.addNotification("StyleSheetQuizPlugin", refreshStyles);
+      },
+
+      handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+        var quizEl = $(createTiddlyElement(place, "div", null, "quiz"));
+        $.each(shuffle(store.getTaggedTiddlers("question")), function(i, questionTiddler) {
+          quizEl.append(buildQuestionEl(parseQuestion(questionTiddler.text)));
+        });
+        quizEl
+          .append($("<button>check</button>").click(function() { checkQuiz(quizEl); }))
+          .append($("<div class='quizStatus finale' />"));
+      }
+
+    };
+
+    function parseQuestion(questionText) {
+      var question = {
+        challenge: wikifyStatic(trim(questionText.replace(/^\*.*/mg, ""))),
+        wrongOptions: questionText.match(/^\* .*$/mg),
+        rightOptions: questionText.match(/^\*\* .*$/mg),
+      };
+      for (i=0; i<question.wrongOptions.length; i++) {
+        question.wrongOptions[i] = question.wrongOptions[i].replace(/^\*+ /,"");
+      }
+      for (i=0; i<question.rightOptions.length; i++) {
+        question.rightOptions[i] = question.rightOptions[i].replace(/^\*+ /,"");
+      }
+      question.options = shuffle(question.wrongOptions.concat(question.rightOptions));
+      return question;
     }
-    questionEl.append("<div class='status'>&nbsp;</div>");
-    return questionEl;
-    // return $("<div/>")
-      // .html(wikifyStatic(wikitext));
-  }
 
-  function checkQuiz(quizEl) {
-    var tally = 0;
-    quizEl.find(".question").each(function() {
-      var isCorrect = true;
-      var $question = $(this);
-      $question.find(".option").each(function() {
-        var optionIsRight = $(this).data("right");
-        var userSaysItsRight = $(this).find(":checkbox").is(":checked");
-        if (optionIsRight!=userSaysItsRight) isCorrect = false;
-        $(this).find(".rightness").hide().fadeIn().css("visibility", "visible");
+    function buildQuestionEl(question) {
+      var questionEl = $("<div class='question'/>")
+        // .append("<div class='challenge'>"+question.challenge+"</div>")
+        // .append("<div class='choose'>Choose " + question.rightOptions.length + "</div>");
+        .append("<div class='challenge'>"+question.challenge + " " +
+                "<span class='choose'>(choose " + question.rightOptions.length + ")</span></div>");
+      for (i=0; i<question.options.length; i++) {
+        var isRight = (question.rightOptions.indexOf(question.options[i])!=-1);
+        $("<div class='option'/>")
+        .attach("<input type='checkbox' />")
+          /*
+          .click(function() {
+            // console.log($(this).parent().siblings(".option"));
+            if (! $(this).is(":checked")) return;
+            var checksRemaining = question.rightOptions.length-1;
+            $(this).parent().siblings(".option").find(":checkbox").each(function() {
+              console.log(this);
+              if ($(this).is(":checked")) {
+                if (checksRemaining-- <= 0) $(this).attr("checked", false);
+              }
+            });
+          })
+          */
+        .end()
+        .addClass(isRight ? "right" : "wrong")
+        .attach("<span>"+question.options[i]+"</span>")
+          .click(function() { $(this).prev().click(); })
+        .end()
+        .appendTo(questionEl);
+      }
+      questionEl.append("<div class='status'></div>");
+      return questionEl;
+    }
+
+    function checkQuiz(quizEl) {
+      console.log("check");
+      var tally = 0;
+      quizEl.find(".question").each(function() {
+        var isCorrect = true;
+        var $question = $(this);
+        $question.find(".option").each(function() {
+          var optionIsRight = $(this).hasClass("right");
+          var userSaysItsRight = $(this).find(":checkbox").is(":checked");
+          if (optionIsRight!=userSaysItsRight) isCorrect = false;
+        });
+        if (isCorrect) tally++;
+        $question.find(".status")
+          .html(isCorrect ? "<span class='correct finale'>Correct</span>" :
+                            "<span class='incorrect finale'>Incorrect</span>");
       });
-      if (isCorrect) tally++;
-      $question.find(".status")
-        .hide()
-        .fadeIn()
-        .html(isCorrect ? "<span class='correct'>Correct</span>" :
-                          "<span class='incorrect'>Incorrect</span>");
-    });
-    var total = quizEl.find(".question").length;
-    quizEl.find(".quizStatus").hide().fadeIn()
-      .empty()
-      .append(tally + " of " +  total + " correct. ")
-      .append((tally==total) ? " Well done!" : "");
-  }
+      var total = quizEl.find(".question").length;
+      quizEl.find(".quizStatus")
+        .empty()
+        .append(tally + " of " +  total + " correct. ")
+        .append((tally==total) ? " Well done!" : "")
+        .attach("<span class='pseudolink'>Reset</span>")
+          .click(function() {
+            $(quizEl).find(".finale").fadeOut(function() { $(this).empty(); });
+            $(quizEl).find(":checkbox").attr("checked", false);
+          })
+        .end();
+      console.log("*** reveal finale");
+      revealFinale();
+    }
 
-// http://jsfromhell.com/array/shuffle [v1.0]
-  var shuffle = function(o){ //v1.0
-  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-  };
+    function revealFinale() {
+      console.log("reveal finale");
+      $(".right span").addClass("rightFinale");
+      $(".finale").fadeIn();
+    }
+
+    function clearFinale() {
+      $(".right span").removeClass("rightFinale");
+      $(".finale").hide();
+    }
+
+    // adapted fro mhttp://jsfromhell.com/array/shuffle
+    function shuffle(o) {
+      for (var j, x, i = o.length; i;
+           j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x)
+      ;
+      return o;
+    }
+
+    config.macros.quiz.init();
+
+    function trim(s) { return s.replace(/^[ \n\t]*/, "").replace(/[ \n\t]*$/, ""); }
+
+  })();
 
 //################################################################################
 //# CUSTOM STYLESHEET
@@ -119,23 +165,23 @@ if(!version.extensions.QuizPlugin) {
 /***
 !StyleSheet
 
+.quiz .finale { display: none; }
+.quiz .pseudolink { text-decoration: underline; color:[[ColorPalette::SecondaryDark]]; cursor: pointer; }
 .quizStatus { margin: 1em 0; font-weight: bold; }
 
 .question { margin: 1em 0; }
+.question .choose { font-style: italic; }
 .question .correct { color: #080; }
 .question .incorrect { color: #b00; }
 
 .option .rightness { margin-left: 0.5em; }
 .option span { cursor: pointer; }
 
+.rightFinale { font-weight: bold; text-decoration: underline; }
+
 !(end of StyleSheet)
 
 ***/
-
-  config.macros.quiz.init();
-
-  // var $ = jQuery;
-  // $.fn.push = function(html) { return this.append(html).children(":last"); };
 
 } // end of 'install only once'
 /*}}}*/
--- contributors/MichaelMahemoff/plugins/QuizPlugin/up.sh
+++ contributors/MichaelMahemoff/plugins/QuizPlugin/up.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+scp QuizPlugin.html addy@useng.com:tiddlywiki/public
