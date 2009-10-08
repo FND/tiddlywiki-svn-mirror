6c98fbce4f3418ddb727280899ecf3e7e1a9f26f
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:47:30 +0000

    [tiddlydocs] updated gadget to list proper rooms based on user groups and removed /doccollab dependency in URLs
    From: bengillies <bengillies@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10708 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/BenGillies/TiddlyDocs/gadget.py
+++ contributors/BenGillies/TiddlyDocs/gadget.py
@@ -1,16 +1,25 @@
 """
-placeholder gadget for MyPage inclusion
+gadget for MyPage inclusion
 """
-
+from tiddlyweb.web.http import HTTP403
 
 
 def get_gadget(environ, start_response):
     """
     return a simple page for gadget testing
     """
-    gadget_page = '<html><head><title>MyDocs</title></head><body><a href="/doccollab/recipes/tiddlydocs/tiddlers.wiki" target="_blank">MyDocs</a></body></html>'
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return gadget_page
+    if getattr(environ['tiddlyweb.usersign'], 'roles', None):
+        gadget_links = ['<a href="/%s/recipes/%s/tiddlers.wiki" target="_blank">%s Docs</a>' % (environ['tiddlyweb.config']['server_prefix'], role, role) for role in environ['tiddlyweb.usersign'].roles]
+        gadget_page = '''<html>
+        <head><title>MyDocs</title></head>
+        <body>
+        %s
+        </body>
+        </html>''' % '\n'.join(gadget_links)
+        start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+        return gadget_page
+    else:
+        raise HTTP403()
 
 def init(config):
     config['selector'].add('/gadget', GET=get_gadget)
