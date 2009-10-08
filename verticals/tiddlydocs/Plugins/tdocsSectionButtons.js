55e14f5001bc718ddba65b45d9dfb9ca5a883209
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:35:39 +0000

    tiddlydocs- css tweaks
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10621 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -5,26 +5,26 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 a.setAttribute('href', tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.wiki');
                 var img = document.createElement('img');
                 img.src = '/static/mydocs_images/icon_link.png';
-				img.height = '15';
-				img.width = '15';
+				img.height = '9';
+				img.width = '9';
                 a.appendChild(img);
-                createTiddlyText(a, 'Permalink');
+                createTiddlyText(a, ' Permalink');
                 var a2 = createTiddlyElement(place, "a", null, 'button');
                 a2.setAttribute('href',  tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+tiddler.title+'.atom');
                 var img = document.createElement('img');
                 img.src = '/static/mydocs_images/icon_rss.png';
-				img.height = '15';
-				img.width = '15';
+				img.height = '9';
+				img.width = '9';
                 a2.appendChild(img);
-                createTiddlyText(a2, 'RSS');
+                createTiddlyText(a2, ' RSS');
                 var a3 = createTiddlyElement(place, "a", null, 'button');
                 a3.setAttribute('href',  tiddler.fields['server.host']+'recipes/'+tiddler.fields['server.	recipe']+'/tiddlers.atom?select=root:'+tiddler.title);
                 var img = document.createElement('img');
                 img.src = '/static/mydocs_images/icon_rss.png';
-				img.height = '15';
-				img.width = '15';
+				img.height = '9';
+				img.width = '9';
                 a3.appendChild(img);
-                createTiddlyText(a3, 'Comments RSS');
+                createTiddlyText(a3, ' Comments RSS');
         }
 };
 
