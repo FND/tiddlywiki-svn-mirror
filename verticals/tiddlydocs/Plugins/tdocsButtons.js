ad31b1c38d1b30bc7d0ccf0541ee08a7d38b0b31
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:42:12 +0000

    tiddlydocs - ie tweaks
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10669 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsButtons.js
@@ -2,9 +2,9 @@ config.macros.tdButtons = {};
 config.macros.tdButtons.handler=function(place,macroName,params,wikifier,paramString,tiddler){
 	var buttonHolder = createTiddlyElement(place, "div", "buttonHolder");
 	wikify("<<docPrint "+window.activeDocuement+">>", buttonHolder);
-	var previewClick = function(place) {
-		story.displayTiddler(null, "Current Document Preview");
-	}
+//	var previewClick = function(place) {
+//		story.displayTiddler(null, "Current Document Preview");
+//	}
 //	var btn = createTiddlyButton(buttonHolder, "preview", "preview current document", previewClick, null, null, null, null, "/static/mydocs_images/icon_preview.jpg");
 	var btn = createTiddlyButton(buttonHolder, "new", "New Section", config.macros.newTiddler.onClickNewTiddler, null, null, null, null, "/static/mydocs_images/icon_new.jpg");
 	btn.setAttribute("newTitle","enter here...");
