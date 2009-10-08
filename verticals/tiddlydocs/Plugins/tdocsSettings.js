33c7f52ba3f0d12fbd85f08dbbb575b29d27c640
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:35:47 +0000

    tiddlydocs- css tweaks
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10622 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSettings.js
+++ verticals/tiddlydocs/Plugins/tdocsSettings.js
@@ -21,6 +21,7 @@ CKEDITOR.config.toolbar_Basic =
     ['Cut','Copy','Paste', 'SpellChecker'],
     ['Undo','Redo','-','Find','Replace'],
     ['Bold','Italic','Underline','Strike'],
+	'/',
     ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
     ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
     ['Image', 'Table','HorizontalRule','SpecialChar'],
