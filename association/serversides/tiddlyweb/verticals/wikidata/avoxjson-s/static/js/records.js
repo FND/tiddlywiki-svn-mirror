d3b83cd576ddfc77d67e7958c120508951a4a427
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 12:41:29 +0000

    avox: got the hide/show column interface working
    From: jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10664 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/records.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/records.js
@@ -1,25 +1,6 @@
 var oTable;
 $(document).ready(function() {
 	// set up records table
-	function hideColumn(col) {
-		var visible = [];
-		var columns = oTable.fnSettings().aoColumns;
-		for(var i=0; i<columns.length; i++) {
-			if(columns[i].bVisible===true) {
-				visible.push(i);
-			}
-		}
-		if(visible.length===1) {
-			return;
-		}
-		var toHide = visible[col];
-		oTable.fnSetColumnVis(toHide, false);
-	}
-	$('#recordsTable tfoot th').click(function() {
-		var pos = $("#recordsTable tfoot th").index(this);
-		hideColumn(pos);
-		return false;
-	});
 	$('table').show();
 	var $table = $('#recordsTable');
 	if($table.length!==0) {
@@ -47,6 +28,59 @@ $(document).ready(function() {
 				{ sClass: "center" }
 			],
 			sDom: 't'
+		});
+		var columns = oTable.fnSettings().aoColumns;
+		var titles = [];
+		for(var i=0;i<columns.length;i++) {
+			titles.push(columns[i].sTitle);
+		}
+		function hideColumn(col) {
+			if(columns[col].bVisible) {
+				oTable.fnSetColumnVis(col, false);
+			}
+		}
+		function showColumn(col) {
+			if(!columns[col].bVisible) {
+				oTable.fnSetColumnVis(col, true);
+			}
+		}
+		$('#recordsTable tfoot th').click(function() {
+			var pos = $("#recordsTable tfoot th").index(this);
+			hideColumn(pos);
+			return false;
+		});
+		var $labels = $('#columnPicker span.label');
+		var $controls = $('#columnPicker span.controls');
+		var updateControlList = function() {
+			$controls.each(function(i) {
+				if(!columns[i].bVisible) {
+					$(this).addClass("invisible");
+				} else {
+					$(this).removeClass("invisible");
+				}
+			});
+		};
+		$('#columnPicker .hideControl').click(function() {
+			var i = $('#columnPicker .hideControl').index(this);
+			var label = $labels[i].innerHTML;
+			var pos = $.inArray(label, titles);
+			hideColumn(pos);
+			updateControlList();
+			return false;
+		});
+		$('#columnPicker .showControl').click(function() {
+			var i = $('#columnPicker .showControl').index(this);
+			var label = $labels[i].innerHTML;
+			var pos = $.inArray(label, titles);
+			showColumn(pos);
+			updateControlList();
+			return false;
+		});
+		$('#columnPicker').hover(function() {
+			updateControlList();
+			$('#columnPicker .columns').show();
+		}, function() {
+			$('#columnPicker .columns').hide();
 		});
 	}
 });
