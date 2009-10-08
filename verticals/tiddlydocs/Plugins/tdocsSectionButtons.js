113d39c7e29860e110e9f4abf227cf9d57f4b7f1
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-08 13:02:50 +0000

    tiddlydocs - minor tweaks ;
    From: simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10813 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
+++ verticals/tiddlydocs/Plugins/tdocsSectionButtons.js
@@ -2,7 +2,7 @@ config.macros.docSectionButtons = {};
 config.macros.docSectionButtons.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
         if(typeof(tiddler.fields['server.host']) === "string") {
                 var a = createTiddlyElement(place, "a", null, 'button');
-                a.setAttribute('href', "/"+tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+encodeURIComponent(tiddler.title)+'.wiki');
+                a.setAttribute('href', tiddler.fields['server.host']+"/"+tiddler.fields['server.workspace']+'/tiddlers/'+encodeURIComponent(tiddler.title)+'.wiki');
 				a.setAttribute('target', 'tiddlydocs_link');
                 var img = document.createElement('img');
                 img.src = '/doccollab/static/mydocs_images/icon_link.png';
@@ -11,7 +11,7 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 a.appendChild(img);
                 createTiddlyText(a, ' Permalink');
                 var a2 = createTiddlyElement(place, "a", null, 'button');
-                a2.setAttribute('href',  "/"+tiddler.fields['server.host']+tiddler.fields['server.workspace']+'/tiddlers/'+encodeURIComponent(tiddler.title)+'.atom');
+                a2.setAttribute('href',  tiddler.fields['server.host']"/"+tiddler.fields['server.workspace']+'/tiddlers/'+encodeURIComponent(tiddler.title)+'.atom');
 				a2.setAttribute('target', 'tiddlydocs_link');
                 var img = document.createElement('img');
                 img.src = '/doccollab/static/mydocs_images/icon_rss.png';
@@ -20,7 +20,7 @@ config.macros.docSectionButtons.handler = function(place,macroName,params,wikifi
                 a2.appendChild(img);
                 createTiddlyText(a2, ' RSS');
                 var a3 = createTiddlyElement(place, "a", null, 'button');
-                a3.setAttribute('href',  "/"+tiddler.fields['server.host']+'recipes/'+tiddler.fields['server.recipe']+'/tiddlers.atom?select=root:'+encodeURIComponent(tiddler.title));
+                a3.setAttribute('href',  tiddler.fields['server.host']+'/recipes/'+tiddler.fields['server.recipe']+'/tiddlers.atom?select=root:'+encodeURIComponent(tiddler.title));
         		a3.setAttribute('target', 'tiddlydocs_link');
         		var img = document.createElement('img');
                 img.src = '/doccollab/static/mydocs_images/icon_rss.png';
