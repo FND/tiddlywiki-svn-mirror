1dc9ac2b413dd0d6c61054ed917877849602eb12
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-14 17:21:48 +0000

    avox: test commit to see if this fixes searching by country_of_registration on the server
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10942 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/app.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/app.js
@@ -32,7 +32,7 @@ function addAdvSearchLine() {
 	/* keeping this "Any Field" option back until we can support it in search
 	<option>Any Field</option>\n
 	*/
-	var s = '<div class="advSearchLine">\n<select name="adv_'+i+'_field">\n<option>Legal Name</option>\n<option>Previous Name(s)</option>\n<option>Trades As Name(s)</option>\n<option>Trading Status</option>\n<option>Company Website</option>\n<option>Registered Country</option>\n<option>Operational PO Box</option>\n<option>Operational Floor</option>\n<option>Operational Building</option>\n<option>Operational Street 1</option>\n<option>Operational Street 2</option>\n<option>Operational Street 3</option>\n<option>Operational City</option>\n<option>Operational State</option>\n<option>Operational Country</option>\n<option>Operational Postcode</option>\n</select></div>';
+	var s = '<div class="advSearchLine">\n<select name="adv_'+i+'_field">\n<option>Legal Name</option>\n<option>Previous Name(s)</option>\n<option>Trades As Name(s)</option>\n<option>Trading Status</option>\n<option>Company Website</option>\n<option>Country of Registration</option>\n<option>Operational PO Box</option>\n<option>Operational Floor</option>\n<option>Operational Building</option>\n<option>Operational Street 1</option>\n<option>Operational Street 2</option>\n<option>Operational Street 3</option>\n<option>Operational City</option>\n<option>Operational State</option>\n<option>Operational Country</option>\n<option>Operational Postcode</option>\n</select></div>';
 	var t ='<input name="adv_'+i+'_value" id="adv_'+i+'_value" size="35" type="text" />';
 	var u ='<a href="javascript:;" class="advanced" id="add_new_adv_'+i+'"><button onclick="return false;">+</button><!--<img src="/static/images/plus_small.gif" />--></a>';
 	var $advSearchLine = $(s).appendTo($('#advancedSearchLines')).append(t).append(u);
@@ -51,7 +51,8 @@ function addAdvSearchLine() {
 		}
 	};
 	var countryFieldsToMap = [
-		'Registered Country',
+		//'Registered Country',
+		'Country of Registration',
 		'Operational Country'
 	];
 	$advSearchLine.find('select').change(function() {
