508bf497203d7c0bc12a288b013b32aa998f9689
jdlrobson <jdlrobson@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-20 10:33:48 +0000

    [newideas] moved to svn tiddlywiki
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10981 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/newideas/tiddlywebconfig.py
@@ -0,0 +1,17 @@
+# A default config, make your own changes here.
+config = {
+    'secret': 'f1348429e0b12eb24bc2f63688a30ead59e7160a',
+    'log_level': 'DEBUG',
+    'system_plugins': ['static','tiddlywebwiki','ideas_staticpages','form'],
+    'twanager_plugins': ['get_mail','static','tiddlywebwiki'],
+    'static_dir': 'static',
+    'server_prefix': '/ideas',
+    'server_host': {
+        'scheme': 'http',
+        'host': 'localhost',
+        'port': '80'
+    },
+    'css_uri':'/ilga/static/css/main_stylesheet.css'
+
+    
+}
