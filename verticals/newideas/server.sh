57d51800f38e87f607e959c4a23c2ef6d815ebaa
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-20 11:25:34 +0000

    server.sh for ease of starting
    git-svn-id: http://svn.tiddlywiki.org/Trunk@10985 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/newideas/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "******************************************************************************"
+echo "WELCOME TO TIDDLYIDEAS"
+echo "******************************************************************************"
+echo "Point browser to http://localhost:8080/ideas"
+
+twanager server localhost 8080
