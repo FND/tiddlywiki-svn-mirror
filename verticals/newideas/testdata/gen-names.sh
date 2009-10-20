abf3d2ce91d68d534cf4e2ac40f5cd31cde5b33b
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-20 14:23:50 +0000

    NewIdeas - add 200 users
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11019 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/newideas/testdata/gen-names.sh
+++ verticals/newideas/testdata/gen-names.sh
@@ -2,13 +2,12 @@
 # uses Project Guttenberg RDF/XML dump:
 # http://www.gutenberg.org/feeds/catalog.rdf.zip
 
-
 #grep 'dc:creator rdf:parse' catalog.rdf | head -50 ; exit
 
 grep 'dc:creator rdf:parse' catalog.rdf |
-    head -50 | 
     sed -e 's/^.*">//' -e 's/<.*$//' | 
     awk -F, '{ print $2 " " $1 }' |
     sed -e 's/(.*)//g' -e 's/[0-9-]*//g' -e 's/^ *//' |
-    sed '/Various/d' |
+    sed -e '/Various/d' -e '/Unknown/d' -e '/[^A-Za-z ]/d' |
+    head -200 |
     sort -u 
