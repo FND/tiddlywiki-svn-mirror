e607d080f788e53823c245d3af7996fd0e9c204a
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-20 14:53:24 +0000

    ideas - fix server.sh user list
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11030 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/newideas/server.sh
+++ verticals/newideas/server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-port=8080
+port=9333
 echo "******************************************************************************"
 echo "WELCOME TO TIDDLYIDEAS"
 echo "******************************************************************************"
 echo "Point browser to http://localhost:$port/ideas"
-echo "All these users have password 'pass':"
-ls store/users
+echo "All these users have password 'pass' (showing first 5 users):"
+ls store/users | head -5
 
 twanager server localhost $port
