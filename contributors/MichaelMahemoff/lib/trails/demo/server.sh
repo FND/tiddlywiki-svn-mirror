12d0f6a533827e2769491fa5b7554d6642c682ec
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-22 15:30:27 +0000

    trails - improved editor UI
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11078 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/lib/trails/demo/server.sh
+++ contributors/MichaelMahemoff/lib/trails/demo/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo 'starting server at http://trails.dev:8080/static/demo.html'
-twanager wserver trails.dev 8080
+if [ "$tiddlyweb_port" == "" ] ; then tiddlyweb_port=8080 ; fi
+echo "starting server at http://trails.dev:$tiddlyweb_port/static/demo.html"
+twanager wserver trails.dev $tiddlyweb_port
