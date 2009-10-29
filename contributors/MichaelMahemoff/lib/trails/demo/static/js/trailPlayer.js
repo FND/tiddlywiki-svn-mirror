86097ae0b8888a913d1a51070fd1d4764b86e63a
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-29 23:39:38 +0000

    trails cleanup - UI updates and refactoring - commented and restructured javascript for readability - got rid of separate logo and made new logo as background image - re-layout navigation for clarity
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11132 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
+++ contributors/MichaelMahemoff/lib/trails/demo/static/js/trailPlayer.js
@@ -1,7 +1,77 @@
 
 var $resources, $predictedTitle, $markers, selectedIndex, $hovered;
 
-function onMouseEnter() {
+/*******************************************************************************
+ * Initialise - build and wire the UI
+ * - Reads resources list, which is a simple gracefully degrading link list,
+ *   and builds and wires the UI from those resources.
+ * - Each element declares a "predict" function indicating what will happen when
+ *   the element is clicked/selected. Precisely, the "predict" function returns
+ *   the index of the resource which to be opened, within the list of resources.
+ *******************************************************************************/
+
+$(function() {
+
+  $resources = $("#resources a");
+  $predictedTitle = $("#predictedTitle");
+
+  $resources.each(function(count) {
+    $resource = $(this);
+    (function(count) {
+      $("<a class='marker'>&#9679;</a>")
+      .attr("href", $resources.eq(count).attr("href"))
+      .wireClickAndHover(predictForListItem)
+      .appendTo($("#markers"));
+
+      $("<option/>")
+      .html($resource.html())
+      .wireClickAndHover(predictForListItem)
+      .appendTo($("#dropdown"));
+    })(count);
+  });
+  $markers = $(".marker");
+  $resources.hide();
+
+  $("#start").wireClickAndHover(function() { return 0; });
+  $("#prev").wireClickAndHover(function() { return Math.max(0, selectedIndex-1); });
+  $("#next").wireClickAndHover(function() { return Math.min(selectedIndex+1, $resources.length-1); });
+  $("#end").wireClickAndHover(function() { return $resources.length-1; });
+
+  $("#closer").click(function() { document.location.href = $(".selected").attr("href"); });
+
+  switchResource(0);
+});
+
+$.fn.wireClickAndHover = function(predictor) {
+
+  $(this).data("predictor", predictor);
+  return $(this)
+    .click(function() { 
+      var predictedIndex = predictor.apply(this);
+      switchResource(predictedIndex);
+      if ($hovered) {
+        showPrediction.apply($hovered);
+        hidePrediction.apply($hovered);
+      }
+      return false;
+    })
+    .mouseenter(showPrediction)
+    .mouseout(hidePrediction)
+}
+
+// There's no need to create a new function dynamically for each of the dropdown and 
+// marker items; each of these always returns the same value, which can be predicted 
+// purely from the element's identity. Thus each of those elements' "predict" function
+// is the same function, which uses "this" to determine the element's identity.
+function predictForListItem() {
+  return $(this).prevAll().length;
+}
+
+/*******************************************************************************
+ * Changing UI - these update UI in response to user events
+ *******************************************************************************/
+
+function showPrediction() {
   $hovered = $(this);
   var predictedIndex = $(this).data("predictor").apply(this);
 
@@ -13,42 +83,13 @@ function onMouseEnter() {
     "&laquo; " + resourceTitle : resourceTitle + " &raquo");
 }
 
-function onMouseOut() {
+function hidePrediction() {
   $predictedTitle.empty();
   $markers.removeClass("hovered");
 }
 
-$.fn.shallOpen = function(predictor) {
-
-  $(this).data("predictor", predictor);
-  return $(this)
-    .click(function() { 
-      var predictedIndex = predictor.apply(this);
-      switchResource(predictedIndex);
-      if ($hovered) {
-        onMouseOut.apply($hovered);
-        onMouseEnter.apply($hovered);
-      }
-    })
-    .mouseenter(onMouseEnter)
-    .mouseout(onMouseOut)
-}
-
-function predictForListItem() {
-  return $(this).prevAll().length;
-}
-
-function switchResourceForList() {
-  switchResource($(this).prevAll().length);
-}
-
-function showpredictedTitleForList() {
-  var index = $(this).prevAll().length;
-  var description = $("#resources a").eq(index).html();
-  $("#predictedTitle").html(description);
-}
-
 function switchResource(index) {
+  if (selectedIndex==index) return;
   updateControls(selectedIndex = index);
   var url = $("#resources a").eq(index).attr("href");
   $('#resourceView').empty().create("<iframe/>").attr("src", url);
@@ -83,38 +124,9 @@ function showFullNote() {
   $.modal.show(message);
 }
 
-$(function() {
-
-  $resources = $("#resources a");
-  $predictedTitle = $("#predictedTitle");
-
-  $resources.each(function(count) {
-    $resource = $(this);
-    (function(count) {
-      $("<span class='marker'>&#9679;</span>")
-      .shallOpen(predictForListItem)
-      .appendTo($("#markers"));
-
-      $("<option/>")
-      .data("index", count)
-      .html($resource.html())
-      // .shallOpen(function() { return count; })
-      .shallOpen(predictForListItem)
-      .appendTo($("#dropdown"));
-    })(count);
-  });
-  $markers = $(".marker");
-  $resources.hide();
-
-  $("#start").shallOpen(function() { return 0; });
-  $("#prev").shallOpen(function() { return Math.max(0, selectedIndex-1); });
-  $("#next").shallOpen(function() { return Math.min(selectedIndex+1, $resources.length-1); });
-  $("#end").shallOpen(function() { return $resources.length-1; });
-
-  $("#closer").click(function() { document.location.href = $(".selected").attr("href"); });
-
-  switchResourceForList(0);
-});
+/*******************************************************************************
+ * Utils
+ *******************************************************************************/
 
 function log() { if (window.console) console.log.apply(console, arguments); }
 $.fn.create = function(html) { return this.append(html).children(":last"); };
