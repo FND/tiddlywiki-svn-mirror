dd32087543e580351c6310495da1c714c36d843e
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-10-30 17:39:40 +0000

    avox: fixing error when stringifying request
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11146 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/emailAvox.py
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/emailAvox.py
@@ -55,6 +55,6 @@ def emailAvox(query):
     else:
        to = 'jnthnlstr@googlemail.com'
        subject = 'Unknown contact type'
-       body = 'Query: '+str(query)
+       body = 'Query: '+print(str(query))
     logging.debug('to:'+to+', subject:'+subject+', '+'body: '+body)
     send(to,subject,body)
