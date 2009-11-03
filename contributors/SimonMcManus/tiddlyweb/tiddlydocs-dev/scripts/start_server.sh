9efafa1a1acdfc6e2146be430467c8f0396abadf
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-11-03 14:59:28 +0000

    tiddlydocs - removing dev instance
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11169 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/tiddlyweb/tiddlydocs-dev/scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-../
-port=$1
-echo "******************************************************************************"
-echo "Welcome to TiddlyDocs"
-echo "******************************************************************************"
-echo "Point browser to http://localhost:$port"
-
-twanager server localhost $port
