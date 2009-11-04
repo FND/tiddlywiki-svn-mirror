17e25e036644c18f31db1186fc46dff878b943c2
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-11-04 12:38:47 +0000

    Remove a bunch of old tired stuff now superceded by http://github.com/tiddlyweb
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11178 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/ChrisDent/experimental/tags/TiddlyWeb-20080404/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-setup(name='tiddlyweb',
-        version='0.1',
-        packages=['tiddlyweb',])
-
--- contributors/ChrisDent/experimental/tiddlywebweb/tiddlywebconfig.py
+++
@@ -1,3 +0,0 @@
-config = {
-        'server_store': ['tiddlywebweb.tiddlywebstore', {'server_base': 'http://localhost:8000'}],
-        }
