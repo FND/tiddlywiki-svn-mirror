35bdd67aea61b2c396810e0292f087e69124b4b4
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-11-11 16:14:34 +0000

    TiddlyPocketBook - added moz transforms
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11236 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
+++ contributors/PaulDowney/verticals/TiddlyPocketBook/deploy.sh
@@ -2,7 +2,7 @@
 scp index.html pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/
 
 #ssh pauldowney@amp.dreamhost.com mkdir -p /home/pauldowney/tiddlypocketbook.com/books/psd/
-#scp TiddlyWikiPocketBook.pdf pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/books/psd
+scp TiddlyWikiPocketBook.pdf pauldowney@amp.dreamhost.com:/home/pauldowney/tiddlypocketbook.com/books/psd
 
 (
 	cd ..
