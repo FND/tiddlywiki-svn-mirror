f925b692b14eabfa0e21b603dbf022e9f3b5684e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-11-13 13:37:01 +0000

    tiddlydocs- adding reserved bag names for tiddlydocs
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11242 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/tiddlyweb/tiddlydocs/tiddlywebconfig.py
+++ contributors/SimonMcManus/tiddlyweb/tiddlydocs/tiddlywebconfig.py
@@ -3,6 +3,7 @@ config = {
     'secret': '7d72e845d6ba23821a76c6ff3d7d3be17220a409',
     'twanager_plugins': ['tiddlywebwiki'],
     'log_level':'DEBUG',
+	'reserved_bag_names': ['tdocs', 'system'],
     'wikklytext.safe_mode': False,
  	'static_url_dir': 'doccollab/static',
     'server_host' : {
