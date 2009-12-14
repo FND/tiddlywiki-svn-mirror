e3e297dd271b6ed169b4c36ba973ef41b67184be
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-12-14 21:50:45 +0000

    TiddlySlidy - added SinglePagePlugin and MainMenuOrderPlugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11333 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/TiddlySlidy/shadows/settings.js
+++ verticals/TiddlySlidy/shadows/settings.js
@@ -10,7 +10,24 @@ config.options.txtTheme = "PresenterMode";
 /*
  *  Faster fading messages!
  */
-config.options.txtFadingMessagesTimeout = 1;
+config.options.txtFadingMessagesTimeout = 1; 
+
+/*
+ *  Animations
+ */ 
+config.options.chkAnimate = 0;
+
+/*
+ *  SinglePagedMode
+ */
+config.options.chkSinglePageMode = 1; //Display one tiddler at a time
+config.options.chkSinglePagePermalink = 1; //Automatically permalink current tiddler
+config.options.chkSinglePageKeepFoldedTiddlers = 0; //Don't close tiddlers that are folded
+config.options.chkSinglePageKeepEditedTiddlers = 1; //Don't close tiddlers that are being edited
+config.options.chkTopOfPageMode = 1; //Open tiddlers at the top of the page
+//config.options.chkBottomOfPageMode = 0; //Open tiddlers at the bottom of the page
+//config.options.chkSinglePageAutoScroll = 1; //Automatically scroll tiddler into view (if needed)
+
 
 /*
  *  Lingo
