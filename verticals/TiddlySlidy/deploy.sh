e0ffba37e4b736fb0bbca8185c8460c8cc152c44
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-12-15 13:57:59 +0000

    TiddlySlidy - TitleMaster slide
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11339 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/TiddlySlidy/deploy.sh
+++ verticals/TiddlySlidy/deploy.sh
@@ -2,9 +2,10 @@
 #  script to deploy to server
 #  - you need to run build.sh and follow instructions first
 #
+host=pauldowney@amp.dreamhost.com
+dir=/home/pauldowney/tiddlyslidy.com
 
-#host=osmodot@osmosoft.com 
-#dir=/data/vhost/www.mediawikiunplugged.com/html
-
+ssh $host mkdir $dir/images
 scp index.html $host:$dir
+scp images/* $host:$dir/images
 scp TiddlySlidy.zip  $host:$dir
