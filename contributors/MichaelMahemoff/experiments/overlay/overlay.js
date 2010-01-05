6c45babad699bfbaef38e4badd1eb7f9b60d35a1
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-05 17:29:28 +0000

    Converted to plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11373 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/experiments/overlay/overlay.js
+++ contributors/MichaelMahemoff/experiments/overlay/overlay.js
@@ -9,66 +9,74 @@
 |~CoreVersion|2.2|
 
 !StyleSheet
-* html { overflow-y: hidden; }
-* html body { height: 100%; overflow: auto; }
-#overlay {
-  position: fixed; top: 0; left: 0;
-  width: 100%; height: 100%;
-  zIndex: 1000;
-  background: black
+
+html, body {
+  height: 100%;
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
 }
-* html #overlay { position: absolute; }
+
+#overlay {
+  position: absolute; top: 0; left: 0;
+  width: 100%; 
+  min-height: 100%;
+  height: auto !important;
+  height: 100%;
+  zIndex: 1000;
+  overflow: visible;
+}
 #overlay button { margin: 10px; }
+
 !code
 {{{
 ***/
 
 (function($) {
 
-  version.extensions.portal = {installed:true};
-  var $overlay;
-  var allStylesFrag;
+  version.extensions.overlay = {installed:true};
+  var $overlay, allStylesFrag, overlayStyleSheetContent;
 
   // Dummy Macro so we can ensure a function will be run
   // after page has loaded (the init() function)
-  var macro = config.macros.portal = {
-
-    init: function() {
+  var macro = config.macros.overlay = {
+    handler: function(place,macroName,params,wikifier,paramString,tiddler) {
+      var macroParams = paramString.parseParams();
+      createTiddlyButton(place,getParam(macroParams,"launchLabel")||"launch",null,version.extensions.overlay.toggle);
+      var content = store.getTiddlerText(getParam(macroParams, "contentTiddler"));
       makeOverlay();
-    },
-
-    handler: function() {
-      $("<button>portal</button>")
-      .click(toggle)
-      .appendTo(place);
+      if (content) $("#overlay").html(content);
+      $("#closeOverlay").click(version.extensions.overlay.toggle);
+      overlayStyleSheetContent = store.getTiddlerText("overlay##StyleSheet");
+      var customOverlayStyleSheetContent = store.getTiddlerText(getParam(macroParams, "styleSheetTiddler"));
+      if (customOverlayStyleSheetContent) overlayStyleSheetContent+=customOverlayStyleSheetContent;
     }
   };
 
   function makeOverlay() {
     $overlay = $("<div id='overlay'/>").hide()
     .appendTo($("body"));
-
-    $("<button>Admin</button>")
-    .appendTo($overlay)
-    .click(toggle);
   }
 
-  function toggle() {
+  version.extensions.overlay.toggle = function() {
 
     if ($overlay.css("display")=="none") {
-      setStylesheet(store.getTiddlerText("overlay##StyleSheet"));
+      setStylesheet(overlayStyleSheetContent);
+      overlayBaseStylesheet = $("style:last")[0];
       $overlay.fadeIn(1000, function() {
         allStylesFrag = document.createDocumentFragment();
         $("style").each(function() {
-          if (this!=$("style:last")[0]) allStylesFrag.appendChild(this);
+          if (this!=overlayBaseStylesheet) allStylesFrag.appendChild(this);
         });
+        $.twStylesheet("#copyright,#saveTest,#backstage,#backstageButton,#storeArea,#shadowArea,#contentWrapper { display: none; }", {id:"main"});
       });
     } else {
+      $.twStylesheet.remove({id:"main"});
       $(allStylesFrag).children().each(function() {
         $("head").append(this);
       });
       $overlay.fadeOut(function() {
-        $("style:first").remove();
+        $(overlayBaseStylesheet).remove();
       });
     }
 
