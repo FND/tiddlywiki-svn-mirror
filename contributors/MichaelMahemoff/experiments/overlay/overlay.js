bfca3ccf197fc32cdf80395c5eba4c5f5ec1ceff
mahemoff <mahemoff@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-09 13:28:49 +0000

    overlay - fixes while making trail player. now more flexible model
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11392 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/MichaelMahemoff/experiments/overlay/overlay.js
+++ contributors/MichaelMahemoff/experiments/overlay/overlay.js
@@ -37,50 +37,60 @@ html, body {
   version.extensions.overlay = {installed:true};
   var $overlay, allStylesFrag, overlayStyleSheetContent;
 
-  // Dummy Macro so we can ensure a function will be run
-  // after page has loaded (the init() function)
   var macro = config.macros.overlay = {
     handler: function(place,macroName,params,wikifier,paramString,tiddler) {
       var macroParams = paramString.parseParams();
       createTiddlyButton(place,getParam(macroParams,"launchLabel")||"launch",null,version.extensions.overlay.toggle);
-      var content = store.getTiddlerText(getParam(macroParams, "contentTiddler"));
-      makeOverlay();
-      if (content) $("#overlay").html(content);
-      $("#closeOverlay").click(version.extensions.overlay.toggle);
-      overlayStyleSheetContent = store.getTiddlerText("overlay##StyleSheet");
-      var customOverlayStyleSheetContent = store.getTiddlerText(getParam(macroParams, "styleSheetTiddler"));
-      if (customOverlayStyleSheetContent) overlayStyleSheetContent+=customOverlayStyleSheetContent;
-    }
+    },
   };
 
   function makeOverlay() {
-    $overlay = $("<div id='overlay'/>").hide()
-    .appendTo($("body"));
+    if (! $("#overlay").length) {
+      var overlays = store.getTaggedTiddlers("overlay");
+      if (!overlays.length) return;
+      // var content = store.getTiddlerText(getParam(macroParams, "contentTiddler"));
+      overlay = overlays[0];
+      $overlay = $("<div id='overlay'/>").hide().html(store.getTiddlerText(overlay.title+"##Content")).appendTo("body");
+      config[overlay.title].onLoad();
+      // $("#closeOverlay").click(version.extensions.overlay.toggle);
+      overlayStyleSheetContent = store.getTiddlerText("overlay##StyleSheet");
+      var customOverlayStyleSheetContent = store.getTiddlerText(overlay.title+"##StyleSheet");
+      if (customOverlayStyleSheetContent) overlayStyleSheetContent+=customOverlayStyleSheetContent;
+    }
   }
 
   version.extensions.overlay.toggle = function() {
 
+    makeOverlay();
+    var overlayTiddlerTitle = store.getTaggedTiddlers("overlay")[0].title;
     if ($overlay.css("display")=="none") {
-      setStylesheet(overlayStyleSheetContent);
+      $.twStylesheet(overlayStyleSheetContent, {id: "overlayStyleSheet"});
       overlayBaseStylesheet = $("style:last")[0];
+      config[overlayTiddlerTitle].onOpen();
       $overlay.fadeIn(1000, function() {
         allStylesFrag = document.createDocumentFragment();
         $("style").each(function() {
           if (this!=overlayBaseStylesheet) allStylesFrag.appendChild(this);
         });
         $.twStylesheet("#copyright,#saveTest,#backstage,#backstageButton,#storeArea,#shadowArea,#contentWrapper { display: none; }", {id:"main"});
+        $("#backstageButton").hide();
       });
     } else {
       $.twStylesheet.remove({id:"main"});
       $(allStylesFrag).children().each(function() {
         $("head").append(this);
       });
+      $("#backstageButton").show();
       $overlay.fadeOut(function() {
-        $(overlayBaseStylesheet).remove();
+        $.twStylesheet.remove({id:"overlayStyleSheet"});
+        // $(overlayBaseStylesheet).remove();
       });
     }
 
   }
+
+  version.extensions.overlay.toggle();
+
 
 })(jQuery);
 
