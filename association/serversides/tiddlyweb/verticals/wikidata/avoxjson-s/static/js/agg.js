340645473cd0a72be323dc310458ae4d77b77644
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-11 13:12:07 +0000

    avox: tweak
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11414 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/agg.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/agg.js
@@ -1074,42 +1074,7 @@ DependentInputs.fields = [
 	'Operational State',
 	'Operational Country',
 	'Operational Postcode'
-];/* to move tabs into a clickable tab interface */
-$(document).ready(function() {
-	var tabWidth, tabMargin, newWidth;
-	$('#recordcontainer .record').each(function() {
-		var $elem = $(this);
-		$elem.css({'float':'left'});
-		$('.entitycontent', $elem).css({
-			"position":"absolute",
-			"left":"0"
-		});
-		if(!$elem.hasClass("selected")) {
-			$('.entitycontent',$elem).hide();
-		}
-	});
-	$('#recordcontainer .tab').click(function() {
-		//var i = $('#recordcontainer .tab').index(this);
-		$('#recordcontainer .record.selected').removeClass('selected').find('.entitycontent').hide();
-		$(this).parent().addClass('selected').end().next().show();
-	}).each(function(i) {
-		if(i!==0) {
-			$(this).css("margin-left","5px");
-		}
-	});
-	$('#recordcontainer .tab').eq(0).click();
-	var $companyDiv = $('#recordcontainer');
-	if($companyDiv.length) {
-		$companyDiv.css("visibility","visible");
-		var addressText = $.trim((//$companyDiv.find('.adr .street-address').text() +
-			$companyDiv.find('.adr .locality').text() +
-			$companyDiv.find('.adr .region').text() +
-			$companyDiv.find('.adr .country-name').text() +
-			$companyDiv.find('.adr .postal-code').text()).replace(/[\n|\r]/g,"").replace(/(\s)+/g," "));
-		console.log(addressText);
-		window.gMaps.op_address = addressText;
-	}
-});/* app.js */
+];/* app.js */
 // override search links to use ajax_search as soon as possible
 $('a[href^="/search"]').each(function() {
 	var href = $(this).attr('href');
@@ -1250,7 +1215,42 @@ $(document).ready(function() {
 	// now show hidden things
 	$('.onlyjs').css('visibility','visible');
 });
-/*
+/* to move tabs into a clickable tab interface */
+$(document).ready(function() {
+	var tabWidth, tabMargin, newWidth;
+	$('#recordcontainer .record').each(function() {
+		var $elem = $(this);
+		$elem.css({'float':'left'});
+		$('.entitycontent', $elem).css({
+			"position":"absolute",
+			"left":"0"
+		});
+		if(!$elem.hasClass("selected")) {
+			$('.entitycontent',$elem).hide();
+		}
+	});
+	$('#recordcontainer .tab').click(function() {
+		//var i = $('#recordcontainer .tab').index(this);
+		$('#recordcontainer .record.selected').removeClass('selected').find('.entitycontent').hide();
+		$(this).parent().addClass('selected').end().next().show();
+	}).each(function(i) {
+		if(i!==0) {
+			$(this).css("margin-left","5px");
+		}
+	});
+	$('#recordcontainer .tab').eq(0).click();
+	var $companyDiv = $('#recordcontainer');
+	if($companyDiv.length) {
+		$companyDiv.css("visibility","visible");
+		var addressText = $.trim((//$companyDiv.find('.adr .street-address').text() + " " +
+			$companyDiv.find('.adr .locality').text() + " " +
+			$companyDiv.find('.adr .region').text() + " " +
+			$companyDiv.find('.adr .country-name').text() + " " +
+			$companyDiv.find('.adr .postal-code').text()).replace(/[\n|\r]/g,"").replace(/(\s)+/g," "));
+		console.log(addressText);
+		window.gMaps.op_address = addressText;
+	}
+});/*
  * File:        jquery.dataTables.js
  * Version:     1.5.1
  * CVS:         $Id$
--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/comprec.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/comprec.js
@@ -25,10 +25,10 @@ $(document).ready(function() {
 	var $companyDiv = $('#recordcontainer');
 	if($companyDiv.length) {
 		$companyDiv.css("visibility","visible");
-		var addressText = $.trim((//$companyDiv.find('.adr .street-address').text() +
-			$companyDiv.find('.adr .locality').text() +
-			$companyDiv.find('.adr .region').text() +
-			$companyDiv.find('.adr .country-name').text() +
+		var addressText = $.trim((//$companyDiv.find('.adr .street-address').text() + " " +
+			$companyDiv.find('.adr .locality').text() + " " +
+			$companyDiv.find('.adr .region').text() + " " +
+			$companyDiv.find('.adr .country-name').text() + " " +
 			$companyDiv.find('.adr .postal-code').text()).replace(/[\n|\r]/g,"").replace(/(\s)+/g," "));
 		console.log(addressText);
 		window.gMaps.op_address = addressText;
