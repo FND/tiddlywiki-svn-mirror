46f918f885e21c61709379d57845a0cb7e4bb675
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-13 15:31:18 +0000

    avox: updating text on suggest page to ask people to search first
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11448 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/records.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/lib/records.js
@@ -174,7 +174,7 @@ $(document).ready(function() {
 					var str = 'There are '+count+' results';
 					switch(count) {
 						case 0:
-							str = 'There are no results - try <span class="filter"><a href="#">adding a filter</a></span> to include other fields in your search or <a href="/pages/suggest_new">suggest a new record</a></span>';
+							str = 'There are no results - try <span class="filter"><a href="#">adding a filter</a></span> to include other fields in your search or <a href="/pages/suggest_new">suggest a new record</a>';
 							break;
 						case 1:
 							str = 'There is 1 result';
