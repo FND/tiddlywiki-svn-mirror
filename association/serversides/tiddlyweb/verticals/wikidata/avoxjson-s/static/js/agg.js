cfdabdc9d0021277b48dfb19ae70639a413410eb
jayfresh <jayfresh@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-13 14:13:41 +0000

    avox: fixed company record layout in IE6/7
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11438 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/agg.js
+++ association/serversides/tiddlyweb/verticals/wikidata/avoxjson-s/static/js/agg.js
@@ -1226,30 +1226,30 @@ $(document).ready(function() {
 /* to move tabs into a clickable tab interface */
 $(document).ready(function() {
 	var tabWidth, tabMargin, newWidth;
-	$('#recordcontainer .record').each(function() {
-		var $elem = $(this);
-		$elem.css({'float':'left'});
-		$('.entitycontent', $elem).css({
-			"position":"absolute",
-			"left":"0"
-		});
-		if(!$elem.hasClass("selected")) {
-			$('.entitycontent',$elem).hide();
-		}
-	});
-	$('#recordcontainer .tab').click(function() {
-		//var i = $('#recordcontainer .tab').index(this);
-		$('#recordcontainer .record.selected').removeClass('selected').find('.entitycontent').hide();
-		$(this).parent().addClass('selected').end().next().show();
-	}).each(function(i) {
-		if(i!==0) {
-			$(this).css("margin-left","5px");
-		}
-	});
-	$('#recordcontainer .tab').eq(0).click();
 	var $companyDiv = $('#recordcontainer');
 	if($companyDiv.length) {
-		$companyDiv.css("visibility","visible");
+		$('#recordcontainer .record').each(function() {
+			var $elem = $(this);
+			$elem.css({'float':'left'});
+			$('.entitycontent', $elem).css({
+				"position":"absolute",
+				"left":"0"
+			});
+			if(!$elem.hasClass("selected")) {
+				$('.entitycontent',$elem).hide();
+			}
+		});
+		$('#recordcontainer .tab').click(function() {
+			//var i = $('#recordcontainer .tab').index(this);
+			$('#recordcontainer .record.selected').removeClass('selected').find('.entitycontent').hide();
+			$(this).parent().addClass('selected').end().next().show();
+		}).each(function(i) {
+			if(i!==0) {
+				$(this).css("margin-left","5px");
+			}
+		});
+		$('#recordcontainer .tab').eq(0).click();
+		$companyDiv.removeClass('hide');
 		var addressText = $.trim((//$companyDiv.find('.adr .street-address').text() + " " +
 			$companyDiv.find('.adr .locality').text() + " " +
 			$companyDiv.find('.adr .region').text() + " " +
