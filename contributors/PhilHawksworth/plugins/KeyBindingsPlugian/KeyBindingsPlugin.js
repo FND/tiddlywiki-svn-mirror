8875b4e6639b5464a8b5a94d113dbdf3015ccfac
PhilHawksworth <PhilHawksworth@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-18 16:59:45 +0000

    INitial checkin of keybindings plugin. This will eventually provide a UI for customising the keybindings to different funcitons
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11476 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PhilHawksworth/plugins/KeyBindingsPlugian/KeyBindingsPlugin.js
@@ -0,0 +1,53 @@
+/***
+|''Name:''| keybindings |
+|''Description:''|Keybindings|
+|''Author:''|Phil Hawksworth|
+|''Version:''|0.1|
+|''Date:''|dd|
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''~CoreVersion:''|2.4.1|
+***/
+
+//{{{
+	
+	
+	// create macro object
+	config.macros.keybindings = {
+		
+		init: function() {			
+			jQuery(document).bind('keypress', function(ev){				
+				if(config.macros.keybindings.keyCodes[ev.which] && config.macros.keybindings.keyCodes[ev.which] !== undefined) {
+					config.macros.keybindings.keyCodes[ev.which].call();
+				}
+			});
+		},
+		
+		// Add a handler function to be invoked by <<keybindings>> 
+		// handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+		// 	
+		// 	// to do: display the options panel for customisation.
+		// 	
+		// },
+		
+		
+		// some sample functions to invoke.
+		forward: function() {
+			alert('forward');
+		},
+		
+		back: function() {
+			alert('back');
+		}
+		
+	};
+	
+	// key mappings
+	config.macros.keybindings.keyCodes = {
+		32: config.macros.keybindings.forward, 	//space
+		46: config.macros.keybindings.forward, 	// .
+		44: config.macros.keybindings.back		// ,
+	};
+	
+	
+//}}}
