f71249e537dbaf27d874d4ed247e8596ea561203
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-22 05:30:09 +0000

    tiddlydocs - updates to minimal theme
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11500 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
+++ verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
@@ -39,4 +39,47 @@ config.macros.docTabs.refresh = function() {
 }
 
 
+// NEW 
+
+config.macros.docTabsNew = {};
+config.macros.docTabsNew.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+	var html = '<div id="tab_wrapper"><ul class="tabs" id="tab_bar"><li class="tab spacer"/></li></ul></div>';
+	var d = createTiddlyElement(null, 'div', '', '', '');
+	d.innerHTML  = html;
+	place.appendChild(d);
+	config.macros.docTabsNew.refresh();
+}
+
+
+config.macros.docTabsNew.switchDoc = function (title) {
+	window.activeDocument = title;
+	refreshAll();
+	var spec = jQuery.parseJSON(store.getTiddlerText(title));
+	if(spec.content[0] != undefined){
+		story.displayTiddler(null, spec.content[0].title);
+	}
+}
+
+config.macros.docTabsNew.refresh = function() {
+	var values = store.getTaggedTiddlers('document');
+	var selectedHtml = '';
+	for (var i=0; i < values.length; i++) {
+		if(values[i].title == window.activeDocument){
+			selectedHtml += '<li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801">'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
+		} else {
+			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"  onclick="config.macros.docTabsNew.switchDoc(\''+values[i].title.replace("'", "\\'")+'\');"><div><a class="thickbox mis" href="#">'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
+	
+		}
+	selectedHtml += '<li class="tab spacer"/>';
+	}
+	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue" href="#"><img style="border: medium none ;" src="/doccollab/static/mydocs_images/new_document.jpg" class="addGadgetCatImg"/></a>';
+	html = selectedHtml  + newdoc;
+	jQuery('#tab_bar:first-child').html(html);
+	var addClick = function() {
+			story.displayTiddler(null, "Create New Document", config.options.txtTheme+'##wizardViewTemplate');
+	}
+	jQuery('#AddFromCatalogueDialogue').click(addClick);
+}
+
+
 
