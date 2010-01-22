3ab25d790728aed8632e220d5927419c1a33fa93
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-22 16:52:57 +0000

    RawViewMacroPlugin - made into conventional plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11509 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/RawViewMacroPlugin/RawViewMacroPlugin.js
+++ contributors/PaulDowney/plugins/RawViewMacroPlugin/RawViewMacroPlugin.js
@@ -1,24 +1,30 @@
 /***
 |''Name:''|RawViewMacroPlugin|
-|''Description:''|Add extra view to the view macro for raw HTML||
-|''Version:''|0.0.1|
-|''Date:''|Mar 2, 2008|
-|''Source:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/RawViewMacroPlugin|
-|''Author:''|PaulDowney (psd (at) osmosoft (dot) com)|
-|''License:''|[[BSD open source license]]|
-|''~CoreVersion:''|2.1.0|
-|''Browser:''|Firefox 1.0.4+; Firefox 1.5; InternetExplorer 6.0|
-
+|''Description:''|Extend the view macro to display raw, unwikified HTML markup |
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
+|''Source:''|http://whatfettle.com/2008/07/RawViewMacroPlugin/ |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/RawViewMacroPlugin/ |
+|''Version:''|0.1|
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.4|
+!!Documentation
 Include raw HTML in a template, etc:
+
+&lt;&lt;view text raw&gt;&gt;
+
 <<view text raw>>
 
+!!Code
 ***/
-
 //{{{
+/*jslint onevar: false nomen: false plusplus: false */
+/*global config createTiddlyElement */
+if (!version.extensions.RawViewMacroPlugin) {
+    version.extensions.RawViewMacroPlugin = {installed: true};
 
-config.macros.view.views.raw = function(value,place,params,wikifier,paramString,tiddler) {
-                createTiddlyElement(place,"span",null,null).innerHTML = value;
-        };
-
-
+    config.macros.view.views.raw = function (value, place, params, wikifier, paramString, tiddler) {
+        createTiddlyElement(place, "span", null, null).innerHTML = value;
+    };
+}
 //}}}
