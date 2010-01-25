e13dd2cb36418bb631eb4ab707db88037cc89446
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-25 16:31:08 +0000

    tiddlydocs - moving minimal theme to css3 dir
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11537 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
+++ verticals/tiddlydocs/Plugins/tdocsDocumentTabs.js
@@ -43,43 +43,50 @@ config.macros.docTabs.refresh = function() {
 
 config.macros.docTabsNew = {};
 config.macros.docTabsNew.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	var html = '<div id="tab_wrapper"><ul class="tabs" id="tab_bar"><li class="tab spacer"/></li></ul></div>';
-	var d = createTiddlyElement(null, 'div', '', '', '');
-	d.innerHTML  = html;
-	place.appendChild(d);
-	config.macros.docTabsNew.refresh();
-}
+		var tagged = store.getTaggedTiddlers('document').reverse();
+		var cookie = "documentTabs";
+		var wrapper = createTiddlyElement(null,"div",null,"tabsetWrapper taggedTabset" + cookie);
+		var tabset = createTiddlyElement(wrapper,"div",null,"tabset");
+		var validTab = false;
+		tabset.setAttribute("cookie",cookie);
+		for(var t=0; t<tagged.length; t++) {
+			if(tagged[t].title == window.activeDocument){
+				console.log(tagged[t].title, "is selecyed");
+				var tabSelected = "tab tabSelected";
+			}else{
+				var tabSelected = "tab tabUnselected";
+			}
+			var label = tagged[t].title;
+				tabLabel = label;
+			var prompt = tagged[t].title;
+			var tab = createTiddlyButton(tabset,tabLabel,prompt,config.macros.docTabsNew.onTabClick,tabSelected);
+			tab.setAttribute("tab",label);
+			if(config.options[cookie] == label)
+				validTab = true;
+		}
+		if(!validTab)
+			config.options[cookie] = tagged[0].title;
+		place.appendChild(wrapper);
+		var newDoc = function() {
+			story.displayTiddler(null, "Create New Document", config.options.txtTheme+'##wizardViewTemplate')
+		}
+		createTiddlyButton(wrapper,'Create New Document',prompt, newDoc,"newDocumentButton");
+};
+
+config.macros.docTabsNew.onTabClick = function() {
+	config.macros.docTabsNew.switchDoc(this.title);
+	refreshAll();
+	
+};
 
 
 config.macros.docTabsNew.switchDoc = function (title) {
 	window.activeDocument = title;
 	refreshAll();
-	var spec = jQuery.parseJSON(store.getTiddlerText(title));
+	var spec = jQuery.parseJSON(store.getTiddlerText(title));	
 	if(spec.content[0] != undefined){
 		story.displayTiddler(null, spec.content[0].title);
 	}
 }
-
-config.macros.docTabsNew.refresh = function() {
-	var values = store.getTaggedTiddlers('document');
-	var selectedHtml = '';
-	for (var i=0; i < values.length; i++) {
-		if(values[i].title == window.activeDocument){
-			selectedHtml += '<li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801">'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
-		} else {
-			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"  onclick="config.macros.docTabsNew.switchDoc(\''+values[i].title.replace("'", "\\'")+'\');"><div><a class="thickbox mis" href="#">'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
-	
-		}
-	selectedHtml += '<li class="tab spacer"/>';
-	}
-	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue" href="#"><img style="border: medium none ;" src="/doccollab/static/mydocs_images/new_document.jpg" class="addGadgetCatImg"/></a>';
-	html = selectedHtml  + newdoc;
-	jQuery('#tab_bar:first-child').html(html);
-	var addClick = function() {
-			story.displayTiddler(null, "Create New Document", config.options.txtTheme+'##wizardViewTemplate');
-	}
-	jQuery('#AddFromCatalogueDialogue').click(addClick);
-}
-
 
 
