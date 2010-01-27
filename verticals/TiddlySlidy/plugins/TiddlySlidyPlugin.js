df6a03164d86aabccd06f30ab34522efab9261f9
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-27 12:00:12 +0000

    TiddlySlidy - use MainMenuUpdatePlugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11554 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/TiddlySlidy/plugins/TiddlySlidyPlugin.js
+++ verticals/TiddlySlidy/plugins/TiddlySlidyPlugin.js
@@ -20,7 +20,8 @@
     };
 
     /*
-     *  newSlide
+     *  newSlide button
+     *  TBD := replace with selection from master slide thumbnails
      */
     config.macros.newSlide = {};
     config.macros.newSlide.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
@@ -32,62 +33,17 @@
             var template = theme + "##EditTemplate";
             var tags = ['slide'];
             var customFields = {
-                'theme': theme,
-                //'subtitle': "subtitle text ..",
-                //'url': "http://example.com"
+                'theme': theme
             };
             var tiddler = store.createTiddler(title);
             merge(tiddler.fields, customFields);
             merge(tiddler.tags, tags);
-            story.displayTiddler(null,title,template,false,null,customFields,false,place)
+            story.displayTiddler(null,title,template,false,null,null,false,place)
             return false;
         };
 
-        return createTiddlyButton(place,theme,tip,onClick,null,null,"1");
+        return createTiddlyButton(place,theme,tip,onClick,null,null,null);
     };
 
-    /*
-     *  ensure slides are in the main menu
-     */
-
-    //TBD: move to using store saveTiddler
-    //TBD: handle delete tiddler
-
-    var saveTiddler = story.saveTiddler;
-    story.saveTiddler = function(title,minorUpdate) {
-        var newTitle = saveTiddler.apply(this, arguments);
-        var tiddler = store.getTiddler(newTitle);
-        if (tiddler.tags.indexOf("slide") == -1) {
-            return newTitle;
-        }
-        var t = store.getTiddler("MainMenu");
-        var text = t.text;
-
-        if (newTitle != title) {
-            text = text.replace("[[" + title + "]]", "[[" + newTitle + "]]");
-        }
-        if (text.indexOf("[[" + newTitle  + "]]") == -1) {
-            text = text + "*[[" + newTitle + "]]\n";
-        }
-        store.saveTiddler(t.title,t.title,text,t.modifier,t.modified,t.tags,t.fields,true,t.created,t.creator);
-        story.refreshTiddler("MainMenu",null,true);
-        autoSaveChanges();
-        return newTitle;
-    };
-
-    var removeTiddler = store.removeTiddler;
-    store.removeTiddler = function(title) {
-
-        var t = store.getTiddler("MainMenu");
-        var text = t.text;
-        text = text.replace("*[[" + title + "]]\n", "");
-        store.saveTiddler(t.title,t.title,text,t.modifier,t.modified,t.tags,t.fields,true,t.created,t.creator);
-
-        story.closeTiddler(title,true);
-        n = removeTiddler.apply(store, arguments);
-        story.refreshAllTiddlers(true);
-        refreshAll();
-        return n;
-    }
 })();
 //}}}
