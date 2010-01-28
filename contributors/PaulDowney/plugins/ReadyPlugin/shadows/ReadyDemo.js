cae006f4968b81df00a7698c6ed46376551fe1f8
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-01-28 07:51:04 +0000

    ReadyPlugin - tidied demo
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11565 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/ReadyPlugin/shadows/ReadyDemo.js
+++ contributors/PaulDowney/plugins/ReadyPlugin/shadows/ReadyDemo.js
@@ -1,14 +1,16 @@
 //{{{
 (function($) {
 
-    config.macros.readyDemoFade = {};
-    config.macros.readyDemoFade.ready = function () {
-        $(this).fadeOut("slow").fadeIn("slow");
+    config.macros.readyDemoFade = {
+        ready: function () {
+            $(this).fadeOut("slow").fadeIn("slow");
+        }
     };
 
-    config.macros.readyDemoSlide = {};
-    config.macros.readyDemoSlide.ready = function () {
-        $(this).slideUp("slow").slideDown("slow");
+    config.macros.readyDemoSlide = {
+        ready: function () {
+            $(this).slideUp("slow").slideDown("slow");
+        }
     };
 
 })(jQuery);
