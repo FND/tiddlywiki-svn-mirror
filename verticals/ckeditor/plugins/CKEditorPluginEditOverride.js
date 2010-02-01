a820fda37c8c65e8ad7dade7538e3568888e4e5e
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-02-01 14:17:59 +0000

    ckeditor - tab now switches back to wikimarkup mode when in ckeditor
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11597 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/ckeditor/plugins/CKEditorPluginEditOverride.js
+++ verticals/ckeditor/plugins/CKEditorPluginEditOverride.js
@@ -2,78 +2,21 @@
 var oldEditHandler = config.macros.edit.handler;
 config.macros.edit.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
-	
 	var editHolder = createTiddlyElement(place, "div", "editHolder");
-	
-	
-	
 	if(paramString.indexOf("text") === -1)  {// if paramsString does not contain "text" 
 		oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 
 	}else {  // We are dealing with the main text area 	
 		var markupSwitch = function() {
 			removeChildren(this.parentNode);
-			oldEditHandler(place,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 		
-			
+			oldEditHandler(place,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 			
 		};
-		createTiddlyButton(editHolder,'WikiMarkup', 'revert to wiki markup', markupSwitch, null, null, null, {tiddlerTitle:tiddler.title});
-
-		if(tiddler.text.substring(0, 6)==="<html>")
+		if(tiddler.text.substring(0, 6)==="<html>" || tiddler.text == config.views.editor.defaultText.format([tiddler.title])) {
 			config.macros.editHtml.handler(editHolder,macroName,params,wikifier,paramString,tiddler);
-		else
-			oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 		
+			createTiddlyButton(editHolder,'WikiMarkup', 'revert to wiki markup', markupSwitch, null, null, null, {tiddlerTitle:tiddler.title});	
+		} else {
+			oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 
+		}		
 	}
 };
 
-
-
-config.macros.edit.ckRefresh = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-	var editHolder = createTiddlyElement(place, "div", "editHolder");
-
-	if(paramString.indexOf("text") === -1)  {// if paramsString does not contain "text" 
-		oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 
-	}else {  // We are dealing with the main text area 
-		if(tiddler.text.substring(0, 6)==="<html>")
-			config.macros.editHtml.handler(editHolder,macroName,params,wikifier,paramString,tiddler);
-		else
-			oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);  // use old edit handler 		
-	}
-		
-/*		if(!paramString.indexOf("text")) {
-		
-		var markupSwitch = function(a, b, c, place) {
-			console.log('boo', place);		
-		};
-		createTiddlyButton(editHolder,'tiddlerText', 'revert to wiki markup', markupSwitch, null, null, null, {tiddlerTitle:tiddler.title});
-		config.macros.editHtml.handler(editHolder,macroName,params,wikifier,paramString,tiddler);
-	} else {
- 		oldEditHandler(editHolder,macroName,params,wikifier,paramString,tiddler);
-	}
-	*/
-};
-
-/*
-config.macros.edit.ckRefresh = function(place,macroName,params,wikifier,paramString,tiddler)
-{
-
-	if((tiddler.text.substring(0, 6)==="<html>" || !tiddler.isTagged('markup')) && !paramString.indexOf("text")) {
-			var markupSwitch = function(place) {
-						console.log(arguments);
-				var title = this.getAttribute('tiddlerTitle');
-				console.log(title);
-			//	story.setTiddlerTag(title,'markup',0);
-			//	story.saveTiddler(title);
-			//	refreshAll();
-			}
-	
-		createTiddlyButton(place,'tiddlerText', 'revert to wiki markup', markupSwitch, null, null, null, {tiddlerTitle:tiddler.title});
-		config.macros.editHtml.handler(place,macroName,params,wikifier,paramString,tiddler);
-
-	} else {
- 		oldEditHandler(place,macroName,params,wikifier,paramString,tiddler);
-	}
-};
-
-
-*/
 
