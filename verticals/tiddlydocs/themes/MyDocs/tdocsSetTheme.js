4af9906631aebba510910b7c8498d747a2d096fc
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-02-11 17:13:46 +0000

    tiddlydocs - removing some unused themse and settings ckeditor value
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11671 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
+++ verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
@@ -1,6 +1,7 @@
 //{{{
 	config.options.txtTheme = "MyDocsTheme";
 	config.macros.TableOfContent.editTemplate = 'MyDocsTheme##tiddlerEditTemplate';
+	config.macros.editHtml.showButton = false;
 
 //}}}
 
