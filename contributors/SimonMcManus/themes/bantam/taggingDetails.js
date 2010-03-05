da374b81dd4c0df59bad5bc5d53050050c23dedc
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-05 19:40:50 +0000

    bantam theme - company view taking shape
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11797 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/SimonMcManus/themes/bantam/taggingDetails.js
@@ -0,0 +1,25 @@
+config.macros.taggingDetails = {};
+config.macros.taggingDetails.handler = function(place,macroName,params,wikifier,paramString,tiddler)
+{
+	params = paramString.parseParams("anon",null,true,false,false);
+	var title = getParam(params,"anon","");
+	if(title == "" && tiddler instanceof Tiddler)
+			title = tiddler.title;
+	
+	var ul = createTiddlyElement(place,"ul");
+	var tagged = store.getTaggedTiddlers(title);
+	createTiddlyElement(ul,"li",null,"listTitle",null);
+	for(var t=0; t<tagged.length; t++) {
+		var altClass = (t%2 == 0)? 'alternateItem':'';
+		var li = createTiddlyElement(ul,"li", null, 'detailList companyListItem '+altClass);
+		li.onclick = function() {
+			story.displayTiddler(this, [this.firstChild.text]);
+		}
+		createTiddlyText(li, tagged[t].title);
+		var uri = store.getTiddlerText(tagged[t].title+"_URL");
+		if(uri!=null)
+			createExternalLink(li,uri, uri);
+	
+	}
+};
+
