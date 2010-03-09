161434eb32a3aa3ada27a7c1e0b08ac53fbb4e54
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-09 15:56:43 +0000

    tiddlydocs - adding minimal recipe
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11813 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/jquery/iNestedSortablesExtension.js
+++ verticals/tiddlydocs/jquery/iNestedSortablesExtension.js
@@ -1,3 +1,9 @@
+/*
+
+Allows the nexted sortable list to be given a class of notHoverable which will mean items cannot be dragged into the list from other droppable lists. 
+
+*/
+
 var oldCheckHover = jQuery.iNestedSortable.checkHover;
 jQuery.iNestedSortable.checkHover = function(e,o) {
 	if(!jQuery(e).hasClass('notHoverable'))
