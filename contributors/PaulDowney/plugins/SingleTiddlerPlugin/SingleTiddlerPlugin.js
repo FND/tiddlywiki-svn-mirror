d1f4a89b8692d67be9ca95410317e670929f234f
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-09 14:05:32 +0000

    SingleTiddlerPlugin - less refresh agressive single page mode plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11811 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/SingleTiddlerPlugin/SingleTiddlerPlugin.js
+++ contributors/PaulDowney/plugins/SingleTiddlerPlugin/SingleTiddlerPlugin.js
@@ -1,6 +1,6 @@
 /***
 |''Name:''|SingleTiddlerPlugin|
-|''Description:''| Simple Single Tiddler Display |
+|''Description:''|Display one tiddler at one time |
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
 |''Source:''|http://whatfettle.com/2008/07/SingleTiddlerPlugin/ |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/SingleTiddlerPlugin/ |
@@ -9,18 +9,23 @@
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.4|
 !!Documentation
-Display one tiddler at one time.
+This plugin closes all tiddlers and displays one tiddler at one time. 
+The URL is updated to point to the current tiddler.
 !!Code
 ***/
 //{{{
-if(!version.extensions.SingleTiddlerPlugin) {
-version.extensions.SingleTiddlerPlugin = {installed:true};
+/*jslint onevar: false nomen: false plusplus: false */
+/*global jQuery window config Story story Tiddler */
+(function ($) {
+    version.extensions.SingleTiddlerPlugin = {installed: true};
 
 	Story.prototype._displayTiddler = Story.prototype.displayTiddler;
-	Story.prototype.displayTiddler = function()
-	{
+	Story.prototype.displayTiddler = function (src, t) {
+        var title = t instanceof Tiddler ? t.title : t;
 		story.closeAllTiddlers();
 		this._displayTiddler.apply(this, arguments);
-	}
-}
+        window.location.hash = encodeURIComponent(String.encodeTiddlyLink(title));
+	};
+
+}(jQuery));
 //}}}
