9b7dae94037d465dd204904c30e6e01273d2c098
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-15 14:04:46 +0000

    tiddlydocs - mydocs - mydocs tweaks
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11840 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/Plugins/DocumentTabsPlugin.js
+++ verticals/tiddlydocs/Plugins/DocumentTabsPlugin.js
@@ -22,51 +22,10 @@ Provides tabs which users can use to change the current active document to any d
 
 //{{{
 	
-config.macros.docTabs = {};
-config.macros.docTabs.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	var html = '<div id="tab_wrapper" style="background-color: rgb(128, 128, 128);"><ul class="tabs" id="tab_bar" style="visibility: visible;"><li class="tab spacer"/></li></ul></div>';
-	var d = createTiddlyElement(null, 'div', '', '', '');
-	d.innerHTML  = html;
-	place.appendChild(d);
-	config.macros.docTabs.refresh();
-}
-
-
-config.macros.docTabs.switchDoc = function (title) {
-	window.activeDocument = title;
-	refreshAll();
-	var spec = jQuery.parseJSON(store.getTiddlerText(title));
-	if(spec.content[0] != undefined){
-		story.displayTiddler(null, spec.content[0].title);
-	}
-}
-
-config.macros.docTabs.refresh = function() {
-	var values = store.getTaggedTiddlers('document');
-	var selectedHtml = '';
-	for (var i=0; i < values.length; i++) {
-		if(values[i].title == window.activeDocument){
-			selectedHtml += '<li class="tab tab_l tab_l_selected"/><li class="tab selectedtab" id="tab_6801"><div class="tabsDiv"><span id="tab_name_6801">'+values[i].title+'</span></div></li><li class="tab tab_r tab_r_selected"/>';
-		} else {
-			selectedHtml +=  '<li class="tab tab_l tab_l_add" id="add_tab_l"></li><li class="tab tab_add tabalignment2 tabalignment2OP" id="add_tab"  onclick="config.macros.docTabs.switchDoc(\''+values[i].title.replace("'", "\\'")+'\');"><div><a class="thickbox mis" href="#">'+values[i].title+'</a></div></li><li class="tab tab_r tab_r_add"></li>';
-	
-		}
-	selectedHtml += '<li class="tab spacer"/>';
-	}
-	var newdoc = '<li class="tab tabalignment3OP tabalignment3" id="add_gadget"><a  class="thickbox mis addFromCatalogue"  id="AddFromCatalogueDialogue" href="#"><img style="border: medium none ;" src="/doccollab/static/mydocs_images/new_document.jpg" class="addGadgetCatImg"/></a>';
-	html = selectedHtml  + newdoc;
-	jQuery('#tab_bar:first-child').html(html);
-	var addClick = function() {
-			story.displayTiddler(null, "Create New Document", config.options.txtTheme+'##wizardViewTemplate');
-	}
-	jQuery('#AddFromCatalogueDialogue').click(addClick);
-}
-
-
 // NEW 
 
-config.macros.docTabsNew = {};
-config.macros.docTabsNew.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+config.macros.docTabs = {};
+config.macros.docTabs.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
 		var tagged = store.getTaggedTiddlers('document').reverse();
 		var cookie = "documentTabs";
 		var wrapper = createTiddlyElement(null,"div",null,"docTabset tabsetWrapper taggedTabset" + cookie);
@@ -82,7 +41,7 @@ config.macros.docTabsNew.handler = function(place,macroName,params,wikifier,para
 			var label = tagged[t].title;
 				tabLabel = label;
 			var prompt = tagged[t].title;
-			var tab = createTiddlyButton(tabset,tabLabel,prompt,config.macros.docTabsNew.onTabClick,tabSelected);
+			var tab = createTiddlyButton(tabset,tabLabel,prompt,config.macros.docTabs.onTabClick,tabSelected);
 			tab.setAttribute("tab",label);
 			if(config.options[cookie] == label)
 				validTab = true;
@@ -92,14 +51,14 @@ config.macros.docTabsNew.handler = function(place,macroName,params,wikifier,para
 		place.appendChild(wrapper);
 };
 
-config.macros.docTabsNew.onTabClick = function() {
-	config.macros.docTabsNew.switchDoc(this.title);
+config.macros.docTabs.onTabClick = function() {
+	config.macros.docTabs.switchDoc(this.title);
 	refreshAll();
 	
 };
 
 
-config.macros.docTabsNew.switchDoc = function (title) {
+config.macros.docTabs.switchDoc = function (title) {
 	window.activeDocument = title;
 	refreshAll();
 	var spec = jQuery.parseJSON(store.getTiddlerText(title));	
--- verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
+++ verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
@@ -1,6 +1,7 @@
 //{{{
 	config.options.txtTheme = "MyDocsTheme";
 	config.macros.TableOfContent.editTemplate = 'MyDocsTheme##tiddlerEditTemplate';
+	if(config.macros.editHtml)
 	config.macros.editHtml.showButton = false;
 
 //}}}
