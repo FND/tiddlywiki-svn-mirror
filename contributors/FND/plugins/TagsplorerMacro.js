7879c365c3a4f2bf17d3c85f072cb98ff6e02424
FND <FND@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-21 16:44:27 +0000

    TagsplorerMacro: added "intelligent" tag selection
    This led to some significant refactoring in many places, including
    turning some methods into private utility functions.
    
    TiddlyWiki v2.6 is required due to the use of an object with jQuery's
    data method.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11884 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/FND/plugins/TagsplorerMacro.js
+++ contributors/FND/plugins/TagsplorerMacro.js
@@ -2,12 +2,12 @@
 |''Name''|TagsplorerMacro|
 |''Description''|tag-based tiddler navigation|
 |''Author''|FND|
-|''Version''|0.1.0|
+|''Version''|1.0.0|
 |''Status''|@@experimental@@|
 |''Source''|http://svn.tiddlywiki.org/Trunk/contributors/FND/plugins/TagsplorerMacro.js|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/contributors/FND/|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
-|''CoreVersion''|2.5.0|
+|''CoreVersion''|2.6.0|
 |''Keywords''|navigation tagging|
 !Usage
 {{{
@@ -16,11 +16,11 @@
 !!Examples
 <<tagsplorer systemConfig>>
 !Revision History
-!!v0.1 (2010-03-21)
+!!v1.0 (2010-03-21)
 * initial release
 !To Do
-* interactive tag selection instead of prompt
 * refresh handling
+* sorting for tag/tiddler lists
 * animations for new/removed tags/tiddlers (requires array diff'ing)
 !StyleSheet
 .tagsplorer ul {
@@ -56,16 +56,15 @@ var macro = config.macros.tagsplorer = {};
 
 config.macros.tagsplorer = $.extend(macro, {
 	locale: {
+		newTagLabel: "new",
+		newTagTooltip: "add tag to filter",
 		delTagTooltip: "remove tag",
-		addTagLabel: "new",
-		addTagTooltip: "add tag to filter",
-		addTagLabel: "new",
-		addTagPrompt: "Enter new tag:"
+		noTagLabel: "N/A"
 	},
 
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
 		var tags = params;
-		var tiddlers = this.getTiddlers(tags);
+		var tiddlers = getTiddlers(tags);
 
 		var container = $('<div class="tagsplorer" />').
 			append('<ul class="tagSelection" />').
@@ -76,25 +75,48 @@ config.macros.tagsplorer = $.extend(macro, {
 
 		container.appendTo(place);
 	},
-	addTag: function(ev) {
-		var tag = prompt(macro.locale.addTagPrompt);
-		if(tag) {
-			var btn = $(this);
-			var container = btn.closest(".tagsplorer");
-			var tags = container.find(".tagSelection").data("tags");
-			var tiddlers = container.find(".tiddlerList").data("tiddlers");
-			tiddlers = macro.filterTiddlers(tiddlers, tag);
-			tags.pushUnique(tag);
-			macro.refreshTags(tags, container);
-			macro.refreshTiddlers(tiddlers, container);
+	newTagClick: function(ev) {
+		var btn = $(this);
+		var container = btn.closest(".tagsplorer");
+
+		var tags = container.find(".tagSelection").data("tags");
+		var tiddlers = container.find(".tiddlerList").data("tiddlers");
+		var tagSelection = getTagSelection(tiddlers, tags);
+
+		var popup = Popup.create(this, "ul");
+		if(tagSelection.length) {
+			$.each(tagSelection, function(i, tag) {
+				createTagElement(popup, tag, macro.locale.newTagTooltip, macro.onTagClick);
+			});
+		} else {
+			createTagElement(popup, macro.locale.noTagLabel);
 		}
+		$(popup).data({
+			container: container,
+			tags: tags,
+			tiddlers: tiddlers
+		});
+		Popup.show();
+		ev.stopPropagation();
+	},
+	onTagClick: function(ev) {
+		var btn = $(this);
+		var popup = btn.closest(".popup");
+		var container = popup.data("container");
+		var tags = popup.data("tags");
+		var tiddlers = popup.data("tiddlers");
+		var tag = btn.text();
+		tags.pushUnique(tag);
+		tiddlers = filterTiddlers(tiddlers, tag);
+		macro.refreshTags(tags, container);
+		macro.refreshTiddlers(tiddlers, container);
 	},
 	delTag: function(ev) {
 		var btn = $(this);
 		var container = btn.closest(".tagsplorer");
 		var tags = container.find(".tagSelection").data("tags");
 		tags.remove(btn.text());
-		var tiddlers = macro.getTiddlers(tags);
+		var tiddlers = getTiddlers(tags);
 		btn.parent().remove();
 		macro.refreshTags(tags, container);
 		macro.refreshTiddlers(tiddlers, container);
@@ -104,19 +126,11 @@ config.macros.tagsplorer = $.extend(macro, {
 		var clone = orig.clone().empty();
 		clone.data("tags", tags);
 
-		addTag = function(label, tooltip, action) {
-			var el = $("<li />").appendTo(clone);
-			return $('<a href="javascript:;" />').
-				text(label).
-				attr("title", tooltip).
-				click(action).
-				appendTo(el);
-		};
 		var self = this;
 		$.each(tags, function(i, tag) {
-			addTag(tag, self.locale.delTagTooltip, self.delTag);
+			createTagElement(clone, tag, self.locale.delTagTooltip, self.delTag);
 		});
-		addTag(this.locale.addTagLabel, this.locale.addTagTooltip, this.addTag).
+		createTagElement(clone, this.locale.newTagLabel, this.locale.newTagTooltip, this.newTagClick).
 			addClass("button");
 
 		orig.replaceWith(clone);
@@ -132,22 +146,47 @@ config.macros.tagsplorer = $.extend(macro, {
 		});
 
 		orig.replaceWith(clone);
-	},
-	getTiddlers: function(tags) {
-		var tiddlers = store.getTiddlers();
-		for(var i = 0; i < tags.length; i++) {
-			tiddlers = this.filterTiddlers(tiddlers, tags[i]);
-		}
-		return tiddlers;
-	},
-	filterTiddlers: function(collection, tag) {
-		return $.map(collection, function(item, i) {
-			if(item.tags.contains(tag)) {
-				return item;
-			}
-		});
 	}
 });
+
+var getTiddlers = function(tags) {
+	var tiddlers = store.getTiddlers();
+	for(var i = 0; i < tags.length; i++) {
+		tiddlers = filterTiddlers(tiddlers, tags[i]);
+	}
+	return tiddlers;
+};
+
+var filterTiddlers = function(tiddlers, tag) {
+	return $.map(tiddlers, function(item, i) {
+		if(item.tags.contains(tag)) {
+			return item;
+		}
+	});
+};
+
+var getTagSelection = function(tiddlers, exclude) {
+	var tags = [];
+	for(var i = 0; i < tiddlers.length; i++) {
+		var _tags = tiddlers[i].tags;
+		for(var j = 0; j < _tags.length; j++) {
+			var tag = _tags[j];
+			if(!exclude.contains(tag)) {
+				tags.pushUnique(tag);
+			}
+		}
+	}
+	return tags;
+};
+
+var createTagElement = function(container, label, tooltip, action) {
+	var el = $("<li />").appendTo(container);
+	return $('<a href="javascript:;" />').
+		text(label).
+		attr("title", tooltip || "").
+		click(action || null).
+		appendTo(el);
+};
 
 })(jQuery);
 //}}}
