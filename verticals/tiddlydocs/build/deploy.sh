76444dca862c1c0d237f1508f104e501f0f9d4b8
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-22 19:14:16 +0000

    tiddlydocs - renaming pieces to recipes
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11900 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/build/deploy.sh
+++ verticals/tiddlydocs/build/deploy.sh
@@ -1,17 +1,14 @@
 #ck parts
 
 ck tiddlydocs.html.recipe
-scp  tiddlydocs.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/pieces/tiddlydocs.html
+scp  tiddlydocs.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/recipes/tiddlydocs.html
 rm tiddlydocs.html
 
 ck ../../ckeditor/split.html.recipe
-scp ../../ckeditor/split.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/pieces/fckeditor.html
+scp ../../ckeditor/split.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/recipes/fckeditor.html
 rm ../../ckeditor/split.html
 
-ck ../themes/css3/split.html.recipe
-scp ../themes/css3/split.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/pieces/css3Theme.html
-rm ../themes/css3/split.html
 
 ck ../themes/minimal/split.html.recipe
-scp ../themes/minimal/split.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/pieces/minimalTheme.html
+scp ../themes/minimal/split.html root@tiddlydocs.com:/data/vhost/tiddlydocs.com/recipes/minimalTheme.html
 rm ../themes/minimal/split.html
