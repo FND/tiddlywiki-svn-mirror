193712633ba269f66210bd1085d9a3486d03dd27
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-03-30 10:21:35 +0000

    commentsSlider - updates to commentsSlider plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11929 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/plugins/commentSlider.js
+++ contributors/SimonMcManus/plugins/commentSlider.js
@@ -1,8 +1,14 @@
-config.macros.commentSlider = {};
-config.macros.commentSlider.handler = function(place,macroName,params)
+config.macros.commentSlider = {
+	commentLabel: "comments"
+};
+config.macros.commentSlider.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
-	var commentCount = wikifyStatic("<<commentsCount "+arguments[5].title+">>")
+	if(paramString!='noCount')
+		var label = config.macros.commentSlider.commentLabel+"("+wikifyStatic("<<commentsCount "+arguments[5].title+">>")+")";
+	else
+		var label = config.macros.commentSlider.commentLabel;
 	var holder = createTiddlyElement(place, "div", "", "commentStretch", " ");
-	var slider = config.macros.slider.createSlider(holder, "chkSlider"+arguments[5].title, "comments("+commentCount+")");
+	
+	var slider = config.macros.slider.createSlider(holder, "chkSlider"+arguments[5].title, label);
 	wikify("<<comments tiddler:'"+arguments[5].title+"'>>\n", slider);
 }
--- contributors/SimonMcManus/plugins/failedSaveNotification.js
+++ contributors/SimonMcManus/plugins/failedSaveNotification.js
@@ -4,41 +4,41 @@
 !Code
 ***/
 //{{{
-	
-	
-config.extensions.ServerSideSavingPlugin.reportSuccess = function() {
-}
-
-config.extensions.ServerSideSavingPlugin.reportFailure = function(msg, tiddler, context){
-	if(context.httpStatus == 403)
-		jQuery.modal.show("Your changes were not saved. Your session may have expired. Please try logging in again.");
-	else 
-		jQuery.modal.show("Your changes were not saved.");
-	jQuery('.savingNotificationsDiv').html('Some changes were not saved. Please refresh the page.');
-}
-
-
 config.macros.saveNotification = {};
-
-config.macros.saveNotification.displayStatus = function(dirty) {
-	if(dirty) {
-			jQuery('.savingNotificationsDiv').show();
-	} else {
-			jQuery('.savingNotificationsDiv').hide();
+if(config.extensions.ServerSideSavingPlugin) { 	
+	config.extensions.ServerSideSavingPlugin.reportSuccess = function() {
 	}
-};
 
-config.macros.saveNotification.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
-	var div = createTiddlyElement(place, "div", "", "savingNotificationsDiv", "");
-	config.macros.saveNotification.displayStatus(store.isDirty());
-};
+	config.extensions.ServerSideSavingPlugin.reportFailure = function(msg, tiddler, context){
+		if(context.httpStatus == 403)
+			jQuery.modal.show("Your changes were not saved. Your session may have expired. Please try logging in again.");
+		else 
+			jQuery.modal.show("Your changes were not saved.");
+		jQuery('.savingNotificationsDiv').html('Some changes were not saved. Please refresh the page.');
+	}
+	config.macros.saveNotification.displayStatus = function(dirty) {
+		if(dirty) {
+				jQuery('.savingNotificationsDiv').show();
+		} else {
+				jQuery('.savingNotificationsDiv').hide();
+		}
+	};
 
-var original = TiddlyWiki.prototype.setDirty;
-TiddlyWiki.prototype.setDirty = function(dirty) {
-	original.apply(arguments);
-	config.macros.saveNotification.displayStatus(dirty);
-};
+	config.macros.saveNotification.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+		var div = createTiddlyElement(place, "div", "", "savingNotificationsDiv", "");
+		config.macros.saveNotification.displayStatus(store.isDirty());
+	};
 
+	var original = TiddlyWiki.prototype.setDirty;
+	TiddlyWiki.prototype.setDirty = function(dirty) {
+		original.apply(arguments);
+		config.macros.saveNotification.displayStatus(dirty);
+	};
+}else {
+	config.macros.saveNotification.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
+		displayMessage("No ~ServerSideSavingPlugin detected. Changes will not be saved to a server.");
+	};
+}
 
 
 //}}}
