100c475fa8784fb41ca6df53f9747274d49c90b2
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-04-12 12:51:21 +0000

    tiddlydocs/mydocs - fixing open in edit mode option
    git-svn-id: http://svn.tiddlywiki.org/Trunk@11995 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
+++ verticals/tiddlydocs/themes/MyDocs/tdocsSetTheme.js
@@ -1,8 +1,7 @@
 //{{{
 	config.options.txtTheme = "MyDocsTheme";
-	config.macros.TableOfContent.editTemplate = 'MyDocsTheme##tiddlerEditTemplate';
 	if(config.macros.editHtml)
-	config.macros.editHtml.showButton = false;
+		config.macros.editHtml.showButton = false;
 
 //}}}
 
