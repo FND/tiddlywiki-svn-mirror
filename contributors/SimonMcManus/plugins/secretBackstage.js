df268196c411bde1a6a249244b7ee823863f2943
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-04-21 11:07:59 +0000

    tiddlydocs - secretBackstage plugin
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12038 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/plugins/secretBackstage.js
+++ contributors/SimonMcManus/plugins/secretBackstage.js
@@ -1,11 +1,38 @@
+/***
+|''Name''|secretBackstage|
+|''Authors''|Simon McManus|
+|''Version''|0.2|
+|''Source''|http://svn.tiddlywiki.org/Trunk/contributors/SimonMcManus/plugins/secretBackstage.js|
+|''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
 
-config.macros.secretBackstage = {};
+!Description
+
+Provides a link to the backstage. 
+
+!Usage
+{{{
+<<secretBackstage>>
+}}}
+
+
+!History 
+0.1 - initial release.
+0.2 - removed shift and alt key detection as it did not work in IE. 
+
+!Code
+***/
+
+//{{{
+	
+config.macros.secretBackstage = {
+	linkText:"****"
+};
 config.macros.secretBackstage.handler = function(place,macroName,params)
 {
-	createTiddlyButton(place,"****",null,function(e) {
-		if(e.altKey && e.shiftKey) {
-			backstage.show();
-		}
+	createTiddlyButton(place,config.macros.secretBackstage.linkText,null,function(e) {
+		backstage.show();
 		return false;
 	}, 'secretBackstage');
 };
+
+//}}}
