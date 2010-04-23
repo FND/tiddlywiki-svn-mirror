a96c4e5644bf2b23748fc83e4bf110b8cb8bdc58
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-04-23 12:03:08 +0000

    tiddlydocs - adding settings
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12049 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ verticals/tiddlydocs/themes/MyDocs/CKEditorSettings.js
@@ -0,0 +1,17 @@
+CKEDITOR.config.baseHref = '/doccollab/static/ckeditor/';
+// making this point to a config file which contains the below may speed up CKEditors performance. 
+CKEDITOR.config.customConfig = 'null';
+
+
+    CKEDITOR.editorConfig = function( config )
+    {
+    	// Define changes to default configuration here. For example:
+    	// config.language = 'fr';
+    	 config.uiColor = '#AADC6E';
+         config.toolbar = 
+         [
+             [ 'Source', '-', 'Bold', 'Italic', 'fileicon' ]
+         ];
+         config.extraPlugins='fileicon';
+    };
+
