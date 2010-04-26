46d28d0be9c43cdbbbe485ef539e6b6caa060343
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-04-26 12:30:19 +0000

    mydocs - adding custom ckeditor bits
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12054 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- verticals/tiddlydocs/themes/MyDocs/CKEditorSettings.js
+++ verticals/tiddlydocs/themes/MyDocs/CKEditorSettings.js
@@ -1,17 +1,12 @@
 CKEDITOR.config.baseHref = '/doccollab/static/ckeditor/';
 // making this point to a config file which contains the below may speed up CKEditors performance. 
-CKEDITOR.config.customConfig = 'null';
+CKEDITOR.config.customConfig = '/doccollab/static/ckeditor/config.js';
 
 
     CKEDITOR.editorConfig = function( config )
     {
     	// Define changes to default configuration here. For example:
     	// config.language = 'fr';
-    	 config.uiColor = '#AADC6E';
-         config.toolbar = 
-         [
-             [ 'Source', '-', 'Bold', 'Italic', 'tw_uploader' ]
-         ];
-         config.extraPlugins='tw_uploader';
+  
     };
 
