d903d8c08af333d4bc0eb18daeca159ebfccfaf7
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-05-06 14:13:33 +0000

    tw_uploader - ckeditor plugin now supports basic image insertion.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12107 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/tiddlyweb/CKEditor/tw_uploader/dialogs/tw_uploader.js
+++ contributors/SimonMcManus/tiddlyweb/CKEditor/tw_uploader/dialogs/tw_uploader.js
@@ -7,54 +7,60 @@
  */
 /*********************************************************************************************************/
 
-CKEDITOR.dialog.add('tw_uploader',function(a){
-	page1={type:'html'};
-	page1.html = "<div id='insert-div' class='tw_uplaoder_container'>insert</div>";
-	page2={type:'html'};
-	page2.html = "<div id='browser-div' class='tw_uplaoder_container'>browse me </div>";
-	page3={type:'html'};
-	page3.html = "<div id='upload-div' class='tw_uplaoder_container'>upload </div>";
-	return {
-		title:"Image Upload",
-		minWidth:180,
-		minHeight:150,
-		onShow: function() {
-		    console.log('on show ', arguments);
-		    jQuery(".tw_uplaoder_container").children().remove();
-			wikify('<<binaryUpload edit:"tags" tags:"image">>', jQuery('#container-div'));
-		},
-	    changeFocus  : function() {
-	        alert('bb');
-	    },
-		contents:[
-			{   
-			    id:'insertTab',
-				label:'Insert',
-    			expand:true,
-                accessKey: 'i',
-    			elements:
-    			        [
-    			            page1  
-    			        ]
-    			
-			},{
-                id:'browseTab',
-                label:'Browse', 
-                accessKey: 'b',
-                elements:[page2]
-            },{
-                id:'uploadTab',
-                label:'Upload', 
-                accessKey: 'u',
-                elements:[page3]
-            }],
-		buttons:[    {
-                type:'button',
-                id:'binaryUpload', /* note: this is not the CSS ID attribute! */
-                label: 'Upload',
-                onClick: function(click){
-                    jQuery('#container-div').children('form').submit();
-                }
-            },CKEDITOR.dialog.cancelButton]
-	};
+CKEDITOR.dialog.uiElementDefinition = function()
+ {
+    alert('opng');
+}
+CKEDITOR.dialog.add('tw_uploader',
+function(a) {
+    page1 = {
+        type: 'html'
+    };
+    page1.html = "<div id='browser-div' class='tw_uplaoder_container'>browse me </div>";
+    page2 = {
+        type: 'html'
+    };
+    page2.html = "<div id='upload-div' class='tw_uplaoder_container'>upload </div>";
+    return {
+        title: "Image Upload",
+        onLoad: function() {
+            wikify('<<imageSelector>>', jQuery('#browser-div')[0]);
+            wikify('<<timeline>>', jQuery('#insert-div')[0]);
+            wikify('<<binaryUpload>>', jQuery('#upload-div')[0]);
+
+        },
+        onOk: function() {
+				var html = [];
+				jQuery('.selectedImage').each(function() {
+					html.push("<img src='"+this.src+"' />");
+				});
+				jQuery('.selectedImage').removeClass('selectedImage');
+				 this.getParentEditor().insertHtml(html.join(' '));
+        },
+        contents: [
+        {
+            id: 'browseTab',
+            label: 'Browse',
+            expand: true,
+            elements: [page1]
+        },
+        {
+            id: 'uploadTab',
+            label: 'Upload',
+            elements: [page2]
+        }
+        ],
+        buttons: [{
+            type: 'button',
+            id: 'binaryUpload',
+            label: 'Upload',
+            onClick: function(click) {
+                jQuery('#container-div').children('form').submit();
+            }
+        },
+        CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton
+        ]
+    };
 });
+
+
--- contributors/SimonMcManus/tiddlyweb/CKEditor/tw_uploader/plugin.js
+++ contributors/SimonMcManus/tiddlyweb/CKEditor/tw_uploader/plugin.js
@@ -12,15 +12,15 @@ CKEDITOR.plugins.add('tw_uploader',
     requires: ['tw_uploader'],
 	lang : ['en'], 
     init:function(a) { 
-	var b="tw_uploader";
-	var c=a.addCommand(b,new CKEDITOR.dialogCommand(b));
+		var b="tw_uploader";
+		var c=a.addCommand(b,new CKEDITOR.dialogCommand(b));
 		c.modes={wysiwyg:1,source:0};
 		c.canUndo=false;
-	a.ui.addButton("tw_uploader",{
-					label:a.lang.tw_uploader.title,
-					command:b,
-					icon:this.path+"fileicon.gif"
-	});
+		a.ui.addButton("tw_uploader",{
+			label:a.lang.tw_uploader.title,
+			command:b,
+			icon:this.path+"fileicon.gif"
+		});
 	CKEDITOR.dialog.add(b,this.path+"dialogs/tw_uploader.js")}
 });
 
