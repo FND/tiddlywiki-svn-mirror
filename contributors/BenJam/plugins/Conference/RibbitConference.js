409c57e2dca17f26b749d5b947b73ea760af3b49
BenJam <BenJam@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-05-19 16:03:22 +0000

    BUMP v1.0
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12148 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/BenJam/plugins/Conference/RibbitConference.js
+++ contributors/BenJam/plugins/Conference/RibbitConference.js
@@ -1,10 +1,34 @@
+/***
+|''Name:''|Confernece|
+|''Description:''|In-tiddler conferencing app powered by Ribbit, to be used with verticals like TeamTasks to improve collaboration. Requires a Ribbit for Mobile account. CAUTION calls are charged per leg (caller) on the conference bridge, this can get expensive!|
+|''Author:''|BenJam|
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/BenJam/plugins/Conference/RibbitConference.js |
+|''Version:''|0.1|
+|''Date:''|May 20th 2010|
+|''Comments:''|Please comment at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.3|
+
+TODO
+Poll the status of the call
+Window-dressing
+Learn Japanese
+
+TOFIX
+Can hangup the call on click
+
+TOTHINK 
+Running more than one conference in each TW, perhaps create a conference object with details inside?
+Keeping a record of conferenced Tiddlers
+Linking a hCard implementation to select collaborators from a dorp down meunu
+Putting hCard into TeamTasks and doing above
+
+Usage:
+{{{
+<<Conference>>
+
+***/
 //{{{
-	
-//TODO lots
 
-//TOFIX none
-
-//TOTHINK running more than one conference in each TW, perhaps create a conference object with details inside.
 (function($) {
     
     var log = console.log;
@@ -17,11 +41,11 @@
         handler: function(place, macroName, params, wikifier, paramString, tiddler){
 
             var mParams = paramString.parseParams();
-			ui(place,mParams);
+			init(place,mParams);
 		}        
 	};
 	
-	function ui(place, mParams){
+	function init(place, mParams){
 		
 		conference = createTiddlyElement(place, "div", null, "conference", "");
 		createTiddlyButton(conference, "Start Conference", null, function(){
@@ -32,21 +56,38 @@
 	}
 	
 	function build(place, mParams){
-		var f = createTiddlyElement(place, "form", null);
+		var f = createTiddlyElement(place, 'form', null);
+		var t = createTiddlyElement(f,'ul',null);
 		for(var i= 0; i<2; i++){
-			createTiddlyElement(f,'span',null,null,"Participant:");
-			var input = createTiddlyElement(f,'input',null);
-			input.setAttribute('name', "Participant ");
+			var l = createTiddlyElement(t,'li',null);
+			createTiddlyElement(l,'span',null,null,"Participant:");
+			var input = createTiddlyElement(l,'input',null);
+			input.setAttribute('name', "participant");
 		}
+		var add = createTiddlyButton(f,'Add', null, function(){
+			var l = createTiddlyElement(t,'li',null);
+			createTiddlyElement(l,'span',null,null,"Participant:");
+			var input = createTiddlyElement(l,'input',null);
+			input.setAttribute('name', "participant");
+		});
+		
 		var start = createTiddlyButton(f,'Start', null,function(){
 			var legs = f.getElementsByTagName('input');
-			conferenceStart(legs, place, mParams);
+			conferenceStart(legs, place.parentNode, mParams);
 		});
 	}
 	
-	function addParticipant(container,macrParams){
-		
-	} 
+	// function addParticipant(place,macroParams){
+	// 	var f = createTIdlyElement(place, "form", null);
+	// 	createTiddlyElement(f,'span',null,null,"Add participant:")
+	// 	var input = createTiddlyElement(f,'input',null);
+	// 	input.setAttribute('name', "participant");
+	// 	var add = craeteTiddlyButton(f,'Add',null,function(){
+	// 		var leg = f.getElementsByTagName('input');
+	// 		niceLeg = formatLegs(leg);
+	// 		addLeg(niceLeg); //TODO carry a callId here for multiple conferences on one page
+	// 	});
+	// } 
 	
 	function formatLegs(legs){
 		//TODO format an array of numbers into a properly formatted ribbit-ish string
@@ -68,34 +109,47 @@
 			return
 		}
 		var legStr = formatLegs(legs);
-		var result = Ribbit.Calls().createCall(callback, legStr);
+		var callId = Ribbit.Calls().createCall(callback, legStr);
 		
 		//call success
 		if(conference){
 			$('.conference').remove();
-			inProgressUI(place, mParams);
+			var d = createTiddlyElement(place, "div", null, "conference", "Conference in progress ");
+			createTiddlyButton(d,"End",null,function(){
+				alert("Clicked end");
+				Ribbit.Calls().hangupCall(function(result){
+					if(result.hasError){
+						alert("Bah! Couldn't hang up the call");
+					}
+				},callId);
+				callId=null;
+				endConference(place, mParams);
+			});
+			pollState(callId)
 		}
-	}
-	
-	function inProgressUI(place, mPrams){
-		theConference = createTiddlyElement(place, "div", null, "conference", "Confernece in progress...");
 	}
 	
 	function callback(result){
 		if(result.hasError){
 			alert("Bah! "+result.errorMessage);
-			return
+			return null
 		}
+		alert(result);
 		conference = true;
 		return result
 	}
 	
-	function conferenceEnd(){
-		//TODO close down the conference
+	function endConference(place, mParams){
+		alert("Ending conference");
+		var t = place.parentNode;
+		$('.conference').remove();
+		init(t, mParams);
 	}
 	
 	function pollState(result){
-		//TODO poll the state of the conference (on/off, no. participants etc)
+		setTimeout(function(){
+			return
+		},5000)//TODO poll the state of the conference (on/off, no. participants etc)
 		
 	}
 	
