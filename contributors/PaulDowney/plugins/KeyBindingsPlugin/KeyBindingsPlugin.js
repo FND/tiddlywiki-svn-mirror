bc2b6bf279a9b053f037f5cb0c20ed3e7c438827
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-06-09 22:10:54 +0000

    KeyBindingsPlugin - forked from Phil Hawksworth for Cecily
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12197 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/KeyBindingsPlugin/KeyBindingsPlugin.js
@@ -0,0 +1,42 @@
+/***
+|''Name:''|KeyBindingsPlugin|
+|''Description:''| capture document keyboard events |
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
+|''Source:''|http://whatfettle.com/2008/07/KeyBindingsPlugin/ |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/KeyBindingsPlugin/ |
+|''Version:''|0.1|
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.4|
+!!Documentation
+!!Code
+***/
+//{{{
+/*jslint onevar: false nomen: false plusplus: false */
+/*global jQuery document config */
+(function ($) {
+    version.extensions.KeyBindingsPlugin = {installed: true};
+
+	jQuery(document).bind('keydown', function (e) {
+		if (e.target.nodeName.toLowerCase() == "html") {
+			var keyCode = e.keyCode || e.which;
+			console.log(keyCode);
+			if (config.macros.keybindings.keyCode[keyCode]) {
+				config.macros.keybindings.keyCode[keyCode].call(e);
+			}
+		}
+	});
+	
+	// key mappings
+	config.macros.keybindings = {
+		keyCode: {
+			32: function (e) { story.nextTiddler() },	//space
+			39: function (e) { story.nextTiddler() },	//cursor-right
+			37: function (e) { story.prevTiddler() },	//cursor-left
+			49: function (e) { story.firstTiddler() },	//1
+			61: function (e) { story.lastTiddler() }	//=
+		}
+	};
+
+})(jQuery);
+//}}}
