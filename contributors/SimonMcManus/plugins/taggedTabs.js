a47b188c6ecd39e055801a6977c8cb97bff1b8b1
simonmcmanus <simonmcmanus@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-06-10 13:52:27 +0000

    tiddlydocs - simplified taggedTabs plugin for tiddlyspace
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12203 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/SimonMcManus/plugins/taggedTabs.js
+++ contributors/SimonMcManus/plugins/taggedTabs.js
@@ -1,3 +1,24 @@
+/***
+|''Name''|taggedTabs|
+|''Authors''|Simon McManus|
+|''Version''|0.1|
+|''Status''|stable|
+|''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
+|''Requires''||
+!Description
+
+Provides tabs containing all the tiddlers with the tag specified. 
+!Usage
+{{{
+
+<<taggedTabs tag>>
+
+}}}
+
+!Code
+***/
+
+
 config.macros.taggedTabs={};
 
 config.macros.taggedTabs.handler=function(place,macroName,params,wikifier,paramString,tiddler,errorMsg){
@@ -10,10 +31,7 @@ config.macros.taggedTabs.handler=function(place,macroName,params,wikifier,paramS
 	tabset.setAttribute("cookie",cookie);
 	for(var t=0; t<tagged.length; t++) {
 		var label = tagged[t].title;
-		if(label=='ccLogin') 
-			tabLabel = config.macros.ccLogin.buttonLogin;
-		else
-			tabLabel = label;
+		tabLabel = label;
 		var prompt = tagged[t].title;
 		var tab = createTiddlyButton(tabset,tabLabel,prompt,config.macros.tabs.onClickTab,"tab tabUnselected");
 		tab.setAttribute("tab",label);
@@ -25,19 +43,6 @@ config.macros.taggedTabs.handler=function(place,macroName,params,wikifier,paramS
 		config.options[cookie] = tagged[0].title;
 	place.appendChild(wrapper);
 	config.macros.tabs.switchTab(tabset, config.options[cookie]);
-	
-	setStylesheet("div.tiddler .tab {font-size:1.2em;  font-weight:bold;padding-left:2em; padding-right:2em; margin-left:0px; margin-right:1em; padding-bottom:2px}"+
-	"div.tiddler .wizard { margin:0px; }"+
-	" div.tabContents .wizard { margin:0px; }"+
-	".tabsetWrapper .wizard h1 {display:none}"+
-	".tabsetWrapper .wizard h2 {padding:0.5em}"+
-	".viewer {float:right; width:90%;}"+
-
-"div.viewer  div.tabsetWrapper{width:90%}"+
-	"a.tabSelected{ filter:'alpha(opacity:60)'; }"+
-	"div.tabset {padding:0px}"+
-	"div.tabContents {padding:0px; background:transparent}",
-	 "taggedTabs");
 	
 };
 
