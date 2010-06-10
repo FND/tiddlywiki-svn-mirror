19889a3c58be7bd12b3abed9b2ac2908d3dc2755
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-06-10 10:14:46 +0000

    KeyBindingsPlugin - body tag for Webkit
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12200 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/KeyBindingsPlugin/KeyBindingsPlugin.js
+++ contributors/PaulDowney/plugins/KeyBindingsPlugin/KeyBindingsPlugin.js
@@ -4,7 +4,7 @@
 |''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
 |''Source:''|http://whatfettle.com/2008/07/KeyBindingsPlugin/ |
 |''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/KeyBindingsPlugin/ |
-|''Version:''|0.1|
+|''Version:''|0.2|
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.4|
@@ -13,12 +13,15 @@
 ***/
 //{{{
 /*jslint onevar: false nomen: false plusplus: false */
-/*global jQuery document config */
+/*global jQuery document config story */
 (function ($) {
     version.extensions.KeyBindingsPlugin = {installed: true};
 
 	jQuery(document).bind('keydown', function (e) {
-		if (e.target.nodeName.toLowerCase() == "html") {
+		console.log(e.target, e.target.nodeName.toLowerCase());
+		switch (e.target.nodeName.toLowerCase()) {
+		case "html":
+		case "body":
 			var keyCode = e.keyCode || e.which;
 			//console.log(keyCode);
 			if (config.macros.keybindings.keyCode[keyCode]) {
@@ -30,11 +33,11 @@
 	// key mappings
 	config.macros.keybindings = {
 		keyCode: {
-			32: function (e) { story.nextTiddler() },	//space
-			39: function (e) { story.nextTiddler() },	//cursor-right
-			37: function (e) { story.prevTiddler() },	//cursor-left
-			49: function (e) { story.firstTiddler() },	//1
-			61: function (e) { story.lastTiddler() }	//=
+			32: function (e) { story.nextTiddler(); },	//space
+			39: function (e) { story.nextTiddler(); },	//cursor-right
+			37: function (e) { story.prevTiddler(); },	//cursor-left
+			49: function (e) { story.firstTiddler(); },	//1
+			61: function (e) { story.lastTiddler(); }	//=
 		}
 	};
 
