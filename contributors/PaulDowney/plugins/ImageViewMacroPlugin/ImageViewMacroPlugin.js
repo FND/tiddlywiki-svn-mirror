74877b73f33cc0ed35c1b37649428e78f471c695
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-06-10 15:45:19 +0000

    ImageViewMacroPlugin - image tiddlers using a view template
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12204 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/PaulDowney/plugins/ImageViewMacroPlugin/ImageViewMacroPlugin.js
@@ -0,0 +1,27 @@
+/***
+|''Name:''|ImageViewMacroPlugin|
+|''Description:''| @@@Description@@@ |
+|''Author:''|PaulDowney (psd (at) osmosoft (dot) com) |
+|''Source:''|http://whatfettle.com/2008/07/ImageViewMacroPlugin/ |
+|''CodeRepository:''|http://svn.tiddlywiki.org/Trunk/contributors/PaulDowney/plugins/ImageViewMacroPlugin/ |
+|''Version:''|0.1|
+|''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
+|''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
+|''~CoreVersion:''|2.4|
+!!Documentation
+@@@blah blah@@@
+!!Code
+***/
+//{{{
+/*jslint onevar: false nomen: false plusplus: false */
+/*global jQuery config */
+(function ($) {
+    version.extensions.ImageViewMacroPlugin = {installed: true};
+
+	// view macro for SVG image tiddlers
+	config.macros.view.views.image = function (value, place, params, wikifier, paramString, tiddler) {
+		config.macros.image.handler(place, "image", [tiddler.title, tiddler.fields.width, tiddler.fields.height, tiddler.fields.transform]);
+	};
+
+}(jQuery));
+//}}}
---
+++ contributors/PaulDowney/plugins/ImageViewMacroPlugin/test/ImageViewMacroPluginTests.js
@@ -0,0 +1,7 @@
+/*global story, jQuery, document, module, test, same */
+(function ($) {
+    jQuery(document).ready(function () {
+        module("ImageViewMacroPlugin");
+
+    });
+})(jQuery);
