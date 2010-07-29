5cf5750577fd11268fcc3ded5c8d70af9adf00a0
jdlrobson <jdlrobson@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-07-29 19:57:59 +0000

    [RCP] updated RandomColorPalette to take arguments to bias the results. Now outputs to hex rather than rgb. Rgb available as parameter rgb
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12322 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/JonRobson/plugins/RandomColorPalettePlugin/plugins/RandomColorPalettePlugin.js
+++ contributors/JonRobson/plugins/RandomColorPalettePlugin/plugins/RandomColorPalettePlugin.js
@@ -2,7 +2,7 @@
 |''Name''|RandomColorPalettePlugin|
 |''Description''|Adds a random color palette to TiddlyWiki|
 |''Author''|Jon Robson|
-|''Version''|<1.0.0>|
+|''Version''|<1.1.0>|
 |''Status''|stable|
 |''Source''|http://svn.tiddlywiki.org/Trunk/contributors/JonRobson/plugins/RandomColorPalettePlugin/RandomColorPalettePlugin.js|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
@@ -14,10 +14,29 @@ Sets and saves a random color palette on execution
 <<RandomColorPaletteButton>>
 Creates a button, which when clicked will change the color palette
 }}}
+!Parameters
+rgb: yes
+By default the ColorPalette is defined in hex. You can output the colours as rgb by simply including this parameter.
+hue:[0,360]
+Seeds the randomiser with this hue.
+saturation:[0,1]
+Seeds the randomiser with a given saturation
+lightness:[0,1]
+Seeds the randomiser with a value for the lightest color tone (0 being darkest, 1 being lightest).
+
+darkest:[0,1]
+Seeds the randomiser with a value for the darkest color tone (0 being darkest, 1 being lightest).
+
+huevariance: [0,90]
+Given a certain hue, specify the angle from the secondary colour to which the secondary and tertiary colours should be determined.
 !Code
 ***/
 /*{{{*/
 (function($){
+	RGB.prototype.toRGBString = function() {
+		return "rgb(%0,%1,%2)".format([parseInt(this.r * 255, 10), 
+			parseInt(this.g * 255, 10), parseInt(this.b * 255, 10)])
+	}
 	function HSL_TO_RGB(h, s, l){ // h (hue) between 0 and 360, s (saturation) & l (lightness) between 0 and 1
 		var c;
 		if(l <= 0.5) {
@@ -62,16 +81,14 @@ Creates a button, which when clicked will change the color palette
 		r += m;
 		g += m;
 		b += m;
-		r = parseInt(r * 255,10);
-		g = parseInt(g * 255,10);
-		b = parseInt(b * 255,10);
-		return "rgb(" + r + "," + g + "," + b + ")";
+		return new RGB(r, g, b);
 	}
 
 	var macro = config.macros.RandomColorPalette = {
 		messagesOn: false, 
 		changedPaletteText: "We have assigned you a random theme by adjusting the [[ColorPalette]] tiddler.\nDon't like it? Click <<RandomColorPalette>> for another one.", 
-		generate_palette: function() {
+		generate_palette: function(options) {
+			var outputRGB = options.rgb && options.rgb[0];
 			if(this.inprogress) { 
 				return;
 			}
@@ -90,17 +107,16 @@ Creates a button, which when clicked will change the color palette
 				TertiaryPale: "#eee",
 				TertiaryLight: "#ccc",
 				TertiaryMid: "#999",
-				TertiaryDark: "#666",
-				Error: "#f88"
+				TertiaryDark: "#666"
 			};
-			var hue = Math.floor(Math.random() * 359);
-			var saturation = (Math.random() * 1);
-			var dark = (Math.random() * 0.5); // this will be the darkest color.
-			var pale = 1;
-			var lightness_values = {Dark:dark, Mid:(pale - dark) / 2, Light:pale - ((pale - dark) / 4), Pale:pale};
+			var hue = options.hue ? parseInt(options.hue[0]) : Math.floor(Math.random() * 359);
+			var saturation = options.saturation ? parseFloat(options.saturation[0]) : (Math.random() * 1);
+			var pale = options.lightness ? parseFloat(options.lightness[0]) : (Math.random() * 1); 
+			var dark = options.darkest ? parseFloat(options.darkest[0]) : 0;
+			var lightness_values = {Dark:dark, Mid:pale - ( ( pale - dark ) / 2 ), Light:pale - ( ( pale - dark ) / 4 ), Pale:pale};
 
 			var opposite_hue = (hue + 180) % 360;
-			var seed = Math.floor((85 * Math.random()) + 5); // we want it to be at least 5 degrees
+			var seed = options.huevariance ? options.huevariance[0] : Math.floor((85 * Math.random()) + 5); // we want it to be at least 5 degrees
 			var huetwo = (opposite_hue + seed) % 360;
 			var huethree = (opposite_hue - seed) % 360;
 			if(huetwo < 0) {
@@ -120,27 +136,41 @@ Creates a button, which when clicked will change the color palette
 			palette.Foreground = HSL_TO_RGB(hue, saturation, 0.08);
 			
 			// construct new ColorPalette
-			var text= "/*{{{*/\n";
+			var text = ["/*{{{*/\n"];
+			var colorcode;
 			for(var id in palette) {
 				if(true) {
-					text += id + ": " + palette[id] + "\n";
+					var color = palette[id];
+					if(outputRGB) {
+						colorcode = color.toRGBString();
+					} else {
+						colorcode = color.toString();
+					}	
+					text.push("%0: %1\n".format([id, colorcode]));
 				}
 			}
-			text += "/*}}}*/";
+			text.push("/*}}}*/");
+			
 			var tid = store.getTiddler('ColorPalette');
 			if(!tid) {
 				tid = new Tiddler('ColorPalette');
 				tid.fields = merge({}, config.defaultCustomFields);
 				tid.modifier ='RandomColorPalette Macro';
 			}
-			tid.text = text;
+			tid.text = text.join("");
 			this.inprogress = false;
 			return tid;
 		},
 		saveColorPalette: function(tid) {
 			// save the color palette in tid
 			store.saveTiddler(tid.title, tid.title, tid.text, tid.modifier, tid.modified, tid.tags, tid.fields, false, tid.created, '');
-			autoSaveChanges(null, [tid]);
+			// an interval is used to cope with users clicking on the palette button quickly.
+			if(macro._nextSave) {
+				window.clearTimeout(macro._nextSave);
+			}
+			macro._nextSave = window.setTimeout(function() {
+					autoSaveChanges(null, [tid]);
+				}, 2000);
 			refreshAll();
 			macro.reportChange();
 		},
@@ -152,18 +182,22 @@ Creates a button, which when clicked will change the color palette
 				msgPlace.appendChild(tempPlace);
 			}
 		},
-		handler: function(place) {
-			var tiddler = macro.generate_palette();
+		handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+			paramString = paramString || "";
+			var options = paramString.parseParams("name", null, true, false, true)[0];
+			var tiddler = macro.generate_palette(options);
 			macro.saveColorPalette(tiddler);
 		}
-
 	};
 
 	config.macros.RandomColorPaletteButton = {
 			text: "New ColorPalette",
 			tooltip: "Generate a random colour scheme for your TiddlyWiki",
-			handler: function(place) {
-				createTiddlyButton(place, this.text, this.tooltip, config.macros.RandomColorPalette.handler);
+			handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+				var btnHandler = function() {
+					config.macros.RandomColorPalette.handler(place, macroName, params, wikifier, paramString, tiddler);
+				};
+				createTiddlyButton(place, this.text, this.tooltip, btnHandler);
 			}
 	};
 })(jQuery);
