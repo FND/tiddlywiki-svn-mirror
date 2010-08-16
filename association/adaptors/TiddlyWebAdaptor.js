7dec7f858e049d7bb947db0829c32985371d8aae
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-08-16 15:01:07 +0000

    TiddlyWebAdaptor - added support for setting http headers.
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12364 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- association/adaptors/TiddlyWebAdaptor.js
+++ association/adaptors/TiddlyWebAdaptor.js
@@ -3,7 +3,7 @@
 |''Description''|adaptor for interacting with TiddlyWeb|
 |''Author:''|FND|
 |''Contributors''|Chris Dent, Martin Budden|
-|''Version''|1.3.3|
+|''Version''|1.3.4|
 |''Status''|stable|
 |''Source''|http://svn.tiddlywiki.org/Trunk/association/adaptors/TiddlyWebAdaptor.js|
 |''CodeRepository''|http://svn.tiddlywiki.org/Trunk/association/|
@@ -97,7 +97,7 @@ adaptor.prototype.getTiddlerList = function(context, userParams, callback) {
 	var uri = uriTemplate.format([context.host, workspace.type + "s",
 		adaptor.normalizeTitle(workspace.name), params]);
 	var req = httpReq("GET", uri, adaptor.getTiddlerListCallback,
-		context, { accept: adaptor.mimeType }, null, null, null, null, true);
+		context, merge({ accept: adaptor.mimeType }, context.headers), null, null, null, null, true);
 	return typeof req == "string" ? req : true;
 };
 
@@ -158,7 +158,7 @@ adaptor.prototype.getTiddlerRevisionList = function(title, limit, context, userP
 	var uri = uriTemplate.format([context.host, workspace.type + "s",
 		adaptor.normalizeTitle(workspace.name), adaptor.normalizeTitle(title)]);
 	var req = httpReq("GET", uri, adaptor.getTiddlerRevisionListCallback,
-		context, { accept: adaptor.mimeType }, null, null, null, null, true);
+		context, merge({ accept: adaptor.mimeType }, context.headers), null, null, null, null, true);
 	return typeof req == "string" ? req : true;
 };
 
@@ -230,7 +230,7 @@ adaptor.prototype.getTiddler = function(title, context, userParams, callback) {
 		adaptor.normalizeTitle(workspace.name), adaptor.normalizeTitle(title),
 		context.revision]);
 	var req = httpReq("GET", uri, adaptor.getTiddlerCallback, context,
-		{ accept: adaptor.mimeType }, null, null, null, null, true);
+		merge({ accept: adaptor.mimeType }, context.headers), null, null, null, null, true);
 	return typeof req == "string" ? req : true;
 };
 
