c25c023bee6709c7e830f7ee79aa37e74580fb48
MartinBudden <MartinBudden@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-08-25 17:04:29 +0000

    TiddlySpace testing
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12383 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ contributors/MartinBudden/verticals/testTiddlySpace/settings.js
@@ -0,0 +1,27 @@
+/***
+|''Name:''|settings|
+|''Description:''|set preferences|
+|''~CoreVersion:''|2.4.0|
+***/
+
+/*{{{*/
+config.views.editor.defaultText = '';
+config.options.chkAnimate = false;
+config.options.chkSaveBackups = false;
+config.options.chkAutoSave = false;
+config.options.txtBackupFolder = 'backup';
+config.options.txtMaxEditRows = 20;
+config.options.chkDisableWikiLinks = true;
+
+config.maxTiddlerImportCount = 500;
+
+//merge(config.defaultCustomFields,{
+//	'server.host':'wiki.openmoko.org'
+//});
+
+config.options.chkSinglePageMode = false;
+config.options.chkTopOfPageMode = false;
+config.options.chkUseHostImages = true;
+
+//#config.displayInstrumentation = true;
+/*}}}*/
