a04ffb6f37e15cbbe7b365afca351df66c881132
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-10-02 07:53:13 +0000

    TiddlySpaceLinkPlugin - changed description
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12435 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/TiddlySpaceLinkPlugin/TiddlySpaceLinkPlugin.js
+++ contributors/PaulDowney/plugins/TiddlySpaceLinkPlugin/TiddlySpaceLinkPlugin.js
@@ -27,10 +27,10 @@ and these may be changed into a space link using {{{@@}}}:
   {{{[[Link to an another space|@@space]]}}} -- [[Link to another space|@@space]]
   {{{[[@space|@@space]]}}} -- [[@space|@@space]]
 <<<
-TiddlySpace includes the [[TiddlySpaceLinkPlugin]] which provides WikiText markup for linking to other spaces on the same server. For example @glossary is a link to the {{{glossary}}} space and [[Small Trusted Group]]@glossary a link to an individual tiddler in the @glossary space. Prefixing the link with a tilde escapes the link, for example {{{~@space}}}. 
+TiddlySpace includes the [[TiddlySpaceLinkPlugin]] which provides WikiText markup for linking to other spaces on the same server. For example @glossary is a link to the {{{glossary}}} space and [[Small Trusted Group]]@glossary a link to an individual tiddler in the @glossary space. Prefixing the link with a tilde escapes the link, for example {{{~@space}}}.
 Email addresses, for example joe.bloggs@example.com and mary@had.a.little.lamb.org should be unaffected.
 !!Features
-A bare {{{@space}}} link forllowed by a period is ignored e.g. @space. Use {{{[[@@space]]}}} as a work-round: [[@@space]].
+A bare {{{@space}}} link followed by a period is ignored e.g. @space. Use {{{[[@@space]]}}} as a workaround: [[@@space]].
 The plugin provides external links decorated so that other plugins may be inclided to add features such as the ability to dynamically pull externally linked tiddlers into the current TiddlyWiki.
 Wikitext linking to a space on another server, for example from a tiddler in a space on tiddlyspace.com to a tiddler or a space on example.com, isn't supported. 
 !!Code
