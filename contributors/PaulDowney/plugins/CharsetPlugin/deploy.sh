e9744fb0de23a2d7159afe2d34485d34e4a0cf0e
psd <psd@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2010-10-19 17:55:31 +0000

    CharsetPlugin - deploy
    git-svn-id: http://svn.tiddlywiki.org/Trunk@12485 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- contributors/PaulDowney/plugins/CharsetPlugin/deploy.sh
+++ contributors/PaulDowney/plugins/CharsetPlugin/deploy.sh
@@ -1,2 +1,2 @@
-ssh pauldowney@amp.dreamhost.com mkdir /home/pauldowney/whatfettle.com/2008/07/CharsetPlugin/
-scp index.html pauldowney@amp.dreamhost.com:/home/pauldowney/whatfettle.com/2008/07/CharsetPlugin/
+ssh ${TW_DEPLOY_HOST:?} mkdir ${TW_DEPLOY_DIR?:?}/CharsetPlugin/
+scp index.html ${TW_DEPLOY_HOST:?}:${TW_DEPLOY_DIR?:?}/CharsetPlugin/
