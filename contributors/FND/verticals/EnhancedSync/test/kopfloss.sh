4995240f58adc12ad08c49a5043a5f4b795ca5f8
FND <fnd@fnd-oda.(none)>
2010-11-05 15:10:41 +0100

    EnhancedSync added Kopfloss for headless JavaScript testing
        $ make kopfloss
        $ make test
    
    NB: Source files currently need to be defined in both index.html and
    test/kopfloss.sh.

---
+++ contributors/FND/verticals/EnhancedSync/test/kopfloss.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+cd "`dirname $0`/kopfloss"
+./run.sh $@ "fixtures/browser.js" \
+	"../../../../../../core/jquery/jquery-1.4.3.min.js" \
+	"../fixtures/kopfloss.js" "../fixtures/tiddlywiki.js" \
+	"../../../../plugins/EnhancedSyncPlugin.js" \
+	"../candidates.js"
