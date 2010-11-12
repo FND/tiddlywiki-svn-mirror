12e5aa3dc1268f6f51b0d359fdd1fff347dd9100
FND <fnd@fnd-oda.(none)>
2010-11-12 15:12:35 +0100

    updated test suite
    TiddlyWiki core no longer provides multiple jQuery versions

--- contributors/FND/verticals/EnhancedSync/test/kopfloss.sh
+++ contributors/FND/verticals/EnhancedSync/test/kopfloss.sh
@@ -2,7 +2,7 @@
 
 cd "`dirname $0`/kopfloss"
 ./run.sh $@ "fixtures/browser.js" \
-	"../../../../../../core/jquery/jquery-1.4.3.min.js" \
+	"../../../../../../core/jquery/jquery.js" \
 	"../fixtures/kopfloss.js" "../fixtures/tiddlywiki.js" \
 	"../../../../plugins/EnhancedSyncPlugin.js" \
 	"../candidates.js" "../tasks.js" "../remotes.js" "../processing.js"
